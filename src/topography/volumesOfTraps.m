a8ae338c8c266ff9382ead777f47e12b1e1164ca
Halvor Møll Nilsen <Halvor.M.Nilsen@sintef.no>
2013-11-01 12:52:26 +0100

    Corrected calculation in volumes, due to hight of formation.

--- src/topography/volumesOfTraps.m
+++ src/topography/volumesOfTraps.m
@@ -11,6 +11,9 @@ for i = 1:numel(trap)
     z = G.cells.z(ind);
 %     h = max(G.cells.z) - z;
     fill_z = res.trap_z(trap(i));
-    v(i) = sum(max(eps, G.cells.volumes(ind).*(fill_z - z)));
+    H=G.cells.H(ind);
+    assert(all((fill_z - z)>=0));
+    h_plume=min((fill_z - z),H);    
+    v(i) = sum(max(eps, G.cells.volumes(ind).*h_plume));
 end
 end
