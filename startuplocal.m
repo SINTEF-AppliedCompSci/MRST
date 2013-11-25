88605285ebbadce37544fdeb0a2606b9bd82cced
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2013-11-25 19:07:58 +0100

    Remove aiigrid, which is not there anymore.

--- startuplocal.m
+++ startuplocal.m
@@ -1,7 +1,6 @@
 mrstModule add co2lab
 mrstModule add deckformat
 mrstModule add gridtools
-mrstModule add internal/aiigrid
 mrstModule add mex/matlab_bgl
 mrstModule add streamlines
 mrstModule add mimetic
