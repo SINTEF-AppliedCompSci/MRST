2e819a7a44fd5fbfeca75ad62f16cdfa5ebd11ae
Halvor Møll Nilsen <Halvor.M.Nilsen@sintef.no>
2014-03-12 11:06:30 +0100

    Addes spessific slover for OW with temperature.

---
+++ ad-fi/eqsfiOWT.m
@@ -0,0 +1,250 @@
+function [eqs, state, hst] = eqsfiOWT(state0, state, dt, G, W, s, f, varargin)
+
+opt = struct('Verbose', mrstVerbose, ...
+             'reverseMode', false,...
+             'scaling', [],...
+             'resOnly', false,...
+             'history', [],...
+             'temperature', false,...
+             'minerals', false, ...
+             'iteration', -1, ...
+             'stepOptions', []);  % Compatibility only
+
+opt = merge_options(opt, varargin{:});
+
+if ~isempty(opt.scaling)
+    scalFacs = opt.scaling;
+else
+    scalFacs.rate = 1; scalFacs.pressure = 1;
+end
+
+hst = opt.history;
+
+% current variables: ------------------------------------------------------
+p    = state.pressure;
+sW   = state.s(:,1);
+T    = state.T;
+pBH = vertcat(state.wellSol.bhp);
+qWs  = vertcat(state.wellSol.qWs);
+qOs    = vertcat(state.wellSol.qOs);
+%mixs  = vertcat(state.wellSol.mixs);
+%mixWs = mixs(:,1);
+
+% previous variables ------------------------------------------------------
+p0  = state0.pressure;
+sW0 = state0.s(:,1);
+T0  = state0.T;
+%--------------------------------------------------------------------------
+
+
+%Initialization of independent variables ----------------------------------
+
+if ~opt.resOnly,
+    % ADI variables needed since we are not only computing residuals.
+    if ~opt.reverseMode,
+        [p, sW, T, qWs, qOs, pBH] = ...
+            initVariablesADI(p, sW, T, qWs, qOs, pBH);
+    else
+        [p0, sW0, T0, tmp, tmp, tmp] = ...
+            initVariablesADI(p0, sW0,  T0,        ...
+            zeros(size(qWs)), ...
+            zeros(size(qOs)), ...
+            zeros(size(pBH)));                          %#ok
+    end
+end
+clear tmp
+g  = norm(gravity);
+
+%--------------------
+%check for p-dependent tran mult:
+trMult = 1;
+if isfield(f, 'tranMultR'), trMult = f.tranMultR(p); end
+
+%check for p-dependent porv mult:
+pvMult = 1; pvMult0 = 1;
+if isfield(f, 'pvMultR')
+    pvMult =  f.pvMultR(p);
+    pvMult0 = f.pvMultR(p0);
+end
+transMult=1;
+if isfield(f, 'transMult')
+   transMult=f.transMult(p); 
+end
+%check for capillary pressure (p_cow)
+pcOW = 0;
+if isfield(f, 'pcOW')
+    pcOW  = f.pcOW(sW);
+end
+
+trans=s.T.*transMult;
+% -------------------------------------------------------------------------
+[krW, krO] = f.relPerm(sW);
+%krW = f.krW(sW);
+%krO = f.krO(1-sW);
+
+% water props (calculated at oil pressure OK?)
+%bW     = f.bW(p);
+bW     = f.bW(p-pcOW);
+rhoW   = bW.*f.rhoWS;
+% rhoW on face, avarge of neighboring cells (E100, not E300)
+rhoWf  = s.faceAvg(rhoW);
+%mobW   = trMult.*krW./f.muW(p);
+mobW   = trMult.*krW./f.muW(p-pcOW);
+dpW     = s.grad(p-pcOW) - g*(rhoWf.*s.grad(G.cells.centroids(:,3)));
+% water upstream-index
+upc = (double(dpW)>=0);
+bWvW = s.faceUpstr(upc, bW.*mobW).*trans.*dpW;
+
+
+% oil props
+bO     = f.bO(p);
+rhoO   = bO.*f.rhoOS;
+rhoOf  = s.faceAvg(rhoO);
+dpO    = s.grad(p) - g*(rhoOf.*s.grad(G.cells.centroids(:,3)));
+% oil upstream-index
+upc = (double(dpO)>=0);
+if isfield(f, 'BOxmuO')
+    % mob0 is already multplied with b0
+    mobO   = trMult.*krO./f.BOxmuO(p);
+    bOvO   = s.faceUpstr(upc, mobO).*trans.*dpO;
+else
+    mobO   = trMult.*krO./f.muO(p);
+    bOvO   = s.faceUpstr(upc, bO.*mobO).*trans.*dpO;
+end
+
+
+% %WELLS ----------------------------------------------------------------
+% bWw     = bW(wc);
+% bOw     = bO(wc);
+% mobWw  = mobW(wc);
+% mobOw  = mobO(wc);
+%
+% %producer mobility
+% bWmobWw  = bWw.*mobWw;
+% bOmobOw  = bOw.*mobOw;
+%
+% %set water injector mobility: mobw = mobw+mobo+mobg, mobo = 0;
+%
+% bWmobWw(iInxW) = bWw(iInxW).*(mobWw(iInxW) + mobOw(iInxW));
+% bOmobOw(iInxW) = 0;
+% bWmobWw(iInxO) = 0;
+% bOmobOw(iInxO) = bOw(iInxO).*(mobWw(iInxO) + mobOw(iInxO));
+%
+% pw  = p(wc);
+%
+% bWqW  = -bWmobWw.*Tw.*(pBHP(perf2well) - pw + 0.0*pcOWw + g*dzw.*rhoW(wc));
+% bOqO  = -bOmobOw.*Tw.*(pBHP(perf2well) - pw + g*dzw.*rhoO(wc));
+
+% EQUATIONS ---------------------------------------------------------------
+% oil:
+sO=(1-sW);sO0=(1-sW0);
+bO0=f.bO(p0);
+eqs{1} = (s.pv/dt).*( pvMult.*bO.*sO - pvMult0.*bO0.*sO0 ) + s.div(bOvO);
+
+
+% water:
+bW0=f.bW(p0);
+eqs{2} = (s.pv/dt).*( pvMult.*bW.*sW - pvMult0.*bW0.*sW0 ) + s.div(bWvW);
+
+% temprature
+rhoS={f.rhoOS,f.rhoWS};
+    bFsF={bO.*sO,bW.*sW};
+    bFsF0={bO0.*sO0,bW0.*sW0};
+
+    bFvF={bOvO,bWvW};
+    eF={f.uO(p,T) , f.uW(p,T)};
+    eF0={f.uO(p0,T0) , f.uW(p0,T0)};
+    hF={f.hO(p,T), f.hW(p,T)};
+
+    uR=f.uR(T);uR0=f.uR(T0);
+    vQ = s.T_r .* s.grad(T);
+    %wT=[W.T]';
+    %vQqQ=W.WI_r*(wT-T(wc));
+    %vQqQ = 0*wT;
+    %bFqF={bOqO,bWqW,bGqG};
+    % well contributions is taken at the end
+
+
+    eqs{3} = (s.pv/dt).*(  (1-pvMult).*uR-(1-pvMult0).*uR0) + s.div( vQ);
+    %eqs{3}=eqs{3};
+    %eqs{5}(wc) = eqs{5}(wc)-vQqQ;
+    for i=1:numel(eF)       
+        eqs{3}  =  eqs{3} + ((s.pv/dt).*( pvMult.*eF{i}.*rhoS{i}.*bFsF{i} - pvMult0.*eF0{i}.*rhoS{i}.*bFsF0{i} )...
+                +  s.div( s.faceUpstr(bFvF{i}>0, rhoS{i}.*hF{i}) .* bFvF{i}));         
+    end
+
+
+% well equations
+if ~isempty(W)
+    if ~opt.reverseMode
+        wc    = vertcat(W.cells);
+        pw   = p(wc);
+        rhos = [f.rhoWS, f.rhoOS];
+        bw   = {bW(wc), bO(wc)};
+        rw   = {};
+        mw   = {mobW(wc), mobO(wc)};
+        [eqs(4:6), cqs, state.wellSol, Rw] = getWellContributions(...
+            W, state.wellSol, pBH, {qWs, qOs}, pw, rhos, bw, rw, rw, mw, ...
+            'iteration', opt.iteration);
+
+        [wc, cqs] = checkForRepititions(wc, cqs);
+        eqs{1}(wc) = eqs{1}(wc) - cqs{2};
+        eqs{2}(wc) = eqs{2}(wc) - cqs{1};
+    else
+        % in reverse mode just gather zero-eqs of correct size
+        for eqn = 4:6
+            nw = numel(state0.wellSol);
+            zw = double2ADI(zeros(nw,1), p0);
+            eqs(4:6) = {zw, zw, zw};
+        end
+    end
+else % no wells
+    eqs(4:6) = {pBH, pBH, pBH};  % empty  ADIs
+end
+
+bFqF={cqs{2},cqs{1}};
+hFwp=cell(2,1);
+
+%for i=1:numel(W)
+    %{
+    hFwp{1}=[hFwp{1};f.rhoOS*repmat(W(i).hO,numel(W(i).cells),1)];
+    hFwp{2}=[hFwp{2};f.rhoWS*repmat(W(i).hW,numel(W(i).cells),1)];
+    %}
+    
+    
+%end
+Tw=[W.T]';
+hFwp{2}=f.rhoWS*f.hW(Rw*pBH,Rw*Tw);
+hFwp{1}=f.rhoOS*f.hO(Rw*pBH,Rw*Tw);
+HqH=cell(3,1);
+for i=1:2;
+    HqH{i}  = rhoS{i}.*hF{i}(wc).*bFqF{i};
+    ind=bFqF{i}>0;
+    HqH{i}(ind)  = hFwp{i}(ind).*bFqF{i}(ind);
+end
+%% add well contributioin
+for i=1:2
+    eqs{3}(wc) = eqs{3}(wc)  -  HqH{i};
+end
+eqs{3}=eqs{3}/1e6;
+
+end
+%--------------------------------------------------------------------------
+
+
+
+function [wc, cqs] = checkForRepititions(wc, cqs)
+[c, ia, ic] = unique(wc, 'stable');
+if numel(c) ~= numel(wc)
+    A = sparse(ic, (1:numel(wc))', 1, numel(c), numel(wc));
+    wc = c;
+    for k=1:numel(cqs)
+        cqs{k} = A*cqs{k};
+    end
+end
+end
+
+
+
+
+
---
+++ ad-fi/stepOWT.m
@@ -0,0 +1,158 @@
+function [state, meta] = stepOWT(state0, state, meta, dt, G, W, system, fluid, varargin)
+% Do a single step of a nonlinear solve for a Oil-Water system
+% This function should in general not be called directly and is as such not
+% documented with regards to input/output: See solvefiADI for an
+% explanation of how the ad-fi solvers are implemented.
+
+%{
+Copyright 2009-2014 SINTEF ICT, Applied Mathematics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
+
+opt = struct('Verbose', mrstVerbose, 'temperature', false, 'minerals', false);
+opt = merge_options(opt, varargin{:});
+s = system.s;
+
+% if ~isempty(system.podbasis)
+%     solve = @(eqs) SolveEqsADIPOD(eqs, opt.podbasis);
+% else
+[eqs, state] = system.getEquations(state0, state, dt, G, W, s, fluid, ...
+    'temperature', opt.temperature, ...
+    'minerals', opt.minerals, ...
+    'iteration', meta.iteration);
+
+if system.nonlinear.cpr && isempty(system.podbasis)
+    p  = mean(state0.pressure);
+    bW = fluid.bW(p); bO = fluid.bO(p);
+    sc = [1./bO, 1./bW];
+
+    vargs = { 'ellipSolve', system.nonlinear.cprEllipticSolver, ...
+              'cprType'   , system.nonlinear.cprType          , ...
+              'relTol'    , system.nonlinear.cprRelTol        , ...
+              'eqScale'   , sc};
+
+    [dx, gmresits, solver_diverged] = cprGeneric(eqs, system, vargs{:});
+
+else
+
+    dx = SolveEqsADI(eqs, system.podbasis);
+    solver_diverged = false;
+    gmresits = [0 0];
+end
+
+
+
+% dx = solve(eqs);
+
+% [state, nInc] = updateState(state, dx);
+
+
+searchfail = true;
+if system.nonlinear.linesearch
+    getEqs = @(state) system.getEquations(state0, state, dt, G, W, s, fluid, 'resOnly', true,...
+        'temperature', opt.temperature,...
+        'minerals',opt.minerals);
+    upState = @(dx) updateState(state, dx, opt);
+    [state, dx, searchfail] = linesearchADI(state, dx, system, getEqs, upState, false);
+end
+
+% Update reservoir conditions once a delta has been found.
+if searchfail
+    [dx, meta] = stabilizeNewton(dx, meta, system);
+    % If the line search failed, uncritically accept the first step and
+    % pray the other measures (relaxation / dampening) handle the error.
+    [state, nInc] = updateState(state, dx, opt);
+end
+
+[converged CNV MB] = getConvergence(state, eqs, fluid, system, dt);
+[meta, residuals] = getResiduals(meta, eqs, system, solver_diverged);
+
+
+%%{
+    %residuals = cellfun(@(x) norm(x.val, 'inf'), eqs);
+    meta.converged = all(residuals < system.nonlinear.tol);
+%}
+
+
+if opt.Verbose
+    
+    if meta.iteration == 1
+        eqnnames = {'Oil', 'Water', 'T', 'qOs', 'qWs', 'pBHP'};
+        if(opt.temperature)
+            eqnnames{end+1} = 'T';
+        end
+        
+        if(opt.minerals)
+            eqnnames{end+1} = 'MI';
+        end
+        fprintf('%-9s', eqnnames{:})
+        fprintf('\n');
+    end
+    fprintf('%8.2e ', residuals);
+    fprintf('\n');
+end
+
+
+meta.stopped = meta.iteration == system.nonlinear.maxIterations && ~converged;
+
+end
+
+%--------------------------------------------------------------------------
+
+function [state, nInc] = updateState(state, dx, opt)
+dsMax = .2;
+dpMax = .3;
+% if ~isempty(phi)
+%     for i = 1:numel(dx)
+%         dx{i} = phi.basis{i}*dx{i};
+%     end
+% end
+
+dp = dx{1};
+ds = dx{2};
+nInc = max( norm(dp,'inf')/norm(state.pressure, 'inf'), ...
+            norm(ds,'inf')/norm(state.s(:,1), 'inf') );
+
+%maxch = norm(ds, 'inf');
+%step = min(1, maxSatStep./maxch);
+
+ds = sign(ds).*min(abs(ds), dsMax);
+dp = sign(dp).*min(abs(dp), abs(dpMax.*state.pressure));
+
+state.pressure = state.pressure + dp;
+sw = state.s(:,1) + ds;
+% Cap values
+sw = min(sw, 1); sw = max(sw, 0);
+
+state.s = [sw, 1-sw];
+
+
+%if(opt.temperature)
+    state.T=state.T+dx{3};
+%end
+dqWs    = dx{4};
+dqOs    = dx{5};
+dpBHP   = dx{6};
+
+dpBHP = sign(dpBHP).*min(abs(dpBHP), abs(dpMax.*vertcat(state.wellSol.bhp)));
+for w = 1:numel(state.wellSol)
+    state.wellSol(w).bhp      = state.wellSol(w).bhp + dpBHP(w);
+    state.wellSol(w).qWs      = state.wellSol(w).qWs + dqWs(w);
+    state.wellSol(w).qOs      = state.wellSol(w).qOs + dqOs(w);
+end
+end
