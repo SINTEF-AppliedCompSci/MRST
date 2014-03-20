487db60fd642381c2977f65ec4e491ec97958b6a
Halvor Møll Nilsen <Halvor.M.Nilsen@sintef.no>
2014-03-20 15:38:34 +0100

    Added assert for zero timestep.

--- ad-fi/solvefiADI.m
+++ ad-fi/solvefiADI.m
@@ -82,7 +82,8 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
     % If the stepFunction admets additional arguments, pass them along,
     % otherwise ignore them @@ TODO: We should find a consistent solution for
     % this.
-    if nargin(system.stepFunction) == 8
+    assert(dt>0);
+    if nargin(system.stepFunction) == 7
         % Step function has a fixed number of arguments (8), and doesn't allow
         % additional arguments
         step = @(state, meta) ...
