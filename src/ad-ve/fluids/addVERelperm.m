91b5bed8e5e2712dbba8134a27331b4cb5cfb52b
kalie <Knut-Andreas.Lie@sintef.no>
2014-03-25 14:41:16 +0100

    Whitespace changes only

--- src/ad-ve/fluids/addVERelperm.m
+++ src/ad-ve/fluids/addVERelperm.m
@@ -8,7 +8,7 @@ function fluid = addVERelperm(fluid, Gt, varargin)
 % Add VE-upscaled rel.perm. (and related functions) for a two-phase fluid
 % object.  The two phases are described as oil and gas, but could also be
 % interpreted as water and gas for a CO2 system.
-% 
+%
 % PARAMETERS:
 %   fluid    - Fluid object to modify
 %   Gt       - Top surface grid
@@ -29,13 +29,13 @@ function fluid = addVERelperm(fluid, Gt, varargin)
 %   krG       - upscaled rel.perm. of gas as a function of (gas) saturation
 %   krOG      - upscaled rel.perm. of oil as a function of (oil) saturation
 %   pcOG      - upscaled 'capillary pressure' as a function of gas saturation
-%   invPc3D   - Fine-scale oil saturation as function of cap. pressure 
+%   invPc3D   - Fine-scale oil saturation as function of cap. pressure
 %   cutValues - Function to ensure consistency and nonzero values in
 %               saturation fields of the 'state' variable ('state.s' and
 %               'state.rs')
 %   kr3D      - Dummy function, returning a rel.perm. value that is simply
 %               equal to the input saturation.
-    
+
 
     opt=struct('res_oil',       0,...
                'res_gas',       0,...
@@ -43,8 +43,8 @@ function fluid = addVERelperm(fluid, Gt, varargin)
                'krg',           1,...
                'top_trap',      [],...
                'surf_topo',     'smooth');
-   
-    opt = merge_options(opt, varargin{:});        
+
+    opt = merge_options(opt, varargin{:});
 
     fluid.krG=@(sg, p, varargin) krG(sg, Gt, opt, varargin{:});
     fluid.krOG=@(so, p, varargin) krOG(so,opt,varargin{:});
@@ -70,30 +70,30 @@ end
 function kr= krG(sg, Gt, opt,varargin)
 
     % Check for records of residual saturation
-    loc_opt=struct('sGmax',[]);    
+    loc_opt=struct('sGmax',[]);
     loc_opt=merge_options(loc_opt,varargin{:});
 
     % Determine how much of the gas saturation that  can be considered 'free'
     % (and how much is locked up as residual saturation)
-    if(~isempty(loc_opt.sGmax))                
+    if(~isempty(loc_opt.sGmax))
         sg_free = free_sg(sg,loc_opt.sGmax,opt);
     else
         sg_free = sg;
     end
-    
+
     switch opt.surf_topo
         case 'inf_rough'
             % for infinite rough upper surface: kr = (h-dh)/H
-            kr = (sg_free .* Gt.cells.H - opt.top_trap) ./ Gt.cells.H; 
-      
+            kr = (sg_free .* Gt.cells.H - opt.top_trap) ./ Gt.cells.H;
+
       case 'sinus'
             kr2 = ((sg_free .* Gt.cells.H).^2 - opt.top_trap.^2) ./ ...
                   (Gt.cells.H.^2 - opt.top_trap.^2);
-            
-            kr2=kr2+1e-4*sg_free;  % @@ Adding small 'fudge factor' 
+
+            kr2=kr2+1e-4*sg_free;  % @@ Adding small 'fudge factor'
             kr2(kr2<0)=0*sg_free(kr2<0);  % @@ Really necessary??
             kr=(kr2).^(0.5);
-      
+
       case 'square'
            kr_s=(sg_free.^2 - (opt.top_trap ./ Gt.cells.H).^2) ./ ...
                 (sg_free .* (1 - (opt.top_trap ./ Gt.cells.H).^2));
@@ -101,14 +101,14 @@ function kr= krG(sg, Gt, opt,varargin)
            kr=kr_s;
            kr((opt.top_trap ./ Gt.cells.H) > sg_free) = ...
                0 * sg_free((opt.top_trap ./ Gt.cells.H) > sg_free);
-      
+
       case 'smooth'
            kr = sg_free;
-      
+
       otherwise
            error('Unknown surface topology')
     end
-    
+
     if(any(double(kr)<0))
         kr(kr<0)=0.0.*sg_free(kr<0);
     end
@@ -119,7 +119,7 @@ end
 % ----------------------------------------------------------------------------
 function kr= krOG(so,opt,varargin)
 
-    loc_opt=struct('sGmax',[]);    
+    loc_opt=struct('sGmax',[]);
     loc_opt=merge_options(loc_opt,varargin{:});
 
     if(~isempty(loc_opt.sGmax))
@@ -128,7 +128,7 @@ function kr= krOG(so,opt,varargin)
         ineb = (sg) > loc_opt.sGmax;
         sg_res = (loc_opt.sGmax - sg);
 
-        so_free = 1 - (loc_opt.sGmax / (1 - opt.res_oil));     
+        so_free = 1 - (loc_opt.sGmax / (1 - opt.res_oil));
         kr=so_free+(1-opt.res_gas)*sg_res;
         % this to avoid errors in ADI derivative
 
@@ -148,7 +148,7 @@ end
 
 % ----------------------------------------------------------------------------
 function pc = pcOG(sg, p, fluid, Gt, opt, varargin)
-    loc_opt = struct('sGmax',[]);    
+    loc_opt = struct('sGmax',[]);
     loc_opt = merge_options(loc_opt, varargin{:});
     if(~isempty(loc_opt.sGmax))
         % could been put in separate function
@@ -172,8 +172,8 @@ function state = cutValues(state, opt)
     %sg=min(sg,1-opt.res_oil);
     sg = max(sg,0);
     state.s = [1-sg, sg];
-    state.rs = max(state.rs, 0);  % @@ Anything missing?  state.rs capped, 
-                                  %    but not otherwise updated     
+    state.rs = max(state.rs, 0);  % @@ Anything missing?  state.rs capped,
+                                  %    but not otherwise updated
     %state.sGmax=min(1,state.sGmax);
     %state.sGmax=max(0,state.sGmax);
     %state.rs=min(state.rs,max_rs);
