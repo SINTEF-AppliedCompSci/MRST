96d96ed8e926e78605d66557895415c20d96ec14
Halvor Møll Nilsen <Halvor.M.Nilsen@sintef.no>
2014-03-25 08:35:33 +0100

    Added fluid conversion to full temperature dependence. 2

---
+++ ad-props/fluid2BlackOilTemp.m
@@ -0,0 +1,28 @@
+function fluid = fluid2BlackOilTemp(bo_fluid,fluids,p_ref,T_ref)
+  %bo_fluid = fluid2BlackOil(bo_fluid,fluids,p_ref,T_ref)
+  % relperm needs to be defined on bo_fluids
+    names = {'W', 'O', 'G'};
+   for i = 1:numel(names)
+       n = names{i};       
+       fluid.(['rho', n, 'S']) =  fluids{i}.density(p_ref,T_ref);
+       fluid.(['b', n]) =@(p,T) fluids{i}.density(p,T)./fluid.(['rho', n, 'S']);
+       %fluid.(['B', n]) =@(p) 1./fluid.(['b', n])(p);
+       try
+            [pa,Ta] = initVariablesADI(p_ref,T_ref);
+            fluids{i}.viscosity(pa,Ta);
+            fluid.(['mu', n]) = @(p,T) fluids{i}.viscosity(p,T);
+       catch
+            warning('Derivatives of viscosity not present: use constant')
+            fluid.(['mu', n]) = @(p,T) fluids{i}.viscosity(p_ref,T_ref);
+       end
+       fluid.(['h', n]) =@(p,T) fluids{i}.enthalpy(p,T);
+       fluid.(['u', n]) =@(p,T) fluids{i}.enthalpy(p,T)-p./fluids{i}.density(p,T);
+       fluid.(['kr',n]) = bo_fluid.(['kr',n]);
+       fluid.(['krO', n]) = bo_fluid.(['kr',n]);
+   end
+   fluid.rsSat = @(varargin) varargin{1}*0;
+   fluid.relPerm = bo_fluid.relPerm;
+end
+function T=extend(T,p)
+    T=repmat(T,numel(double(p)),1);
+end
