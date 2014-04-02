8fbec89efb2b807d387917a00d5baeab022a39eb
Halvor Møll Nilsen <Halvor.M.Nilsen@sintef.no>
2014-04-02 09:28:16 +0200

    Change formationg of table to more intuitive?

--- ad-fi/utils/interp2DTable.m
+++ ad-fi/utils/interp2DTable.m
@@ -1,6 +1,6 @@
 function y = interp2DTable(T, x, y)
-    in_range_x = ( min(T.x)<=min(x) && max(x) <= max(T.x));
-    in_range_y = ( min(T.y)<=min(y) && max(y) <= max(T.y));
+    in_range_x = ( min(T.x)<=min(double(x)) && max(double(x)) <= max(T.x));
+    in_range_y = ( min(T.y)<=min(double(y)) && max(double(y)) <= max(T.y));
     if(~(in_range_x && in_range_y))
        warning('value out of range of tensor table') 
     end
@@ -8,12 +8,24 @@ function y = interp2DTable(T, x, y)
     if(isa(x,'ADI') || isa(y,'ADI'))
         xi=double(x);vi=double(y);
         [yi, dyidxi, dyidvi] = interp2DTableLoc(T, xi, vi);
-         tmp1 = lMultdiag(dyidxi, x.jac);
-         tmp2 = lMultdiag(dyidvi, y.jac);
-            for i=1:numel(tmp2)
-                tmp1{i}=tmp1{i}+tmp2{i};
-            end                       
-            y = ADI(yi, tmp1);
+        tmp={};
+        if(isa(x,'ADI'))
+         tmp{end+1} = lMultdiag(dyidxi, x.jac);
+        end
+        if(isa(y,'ADI'))
+         tmp{end+1} = lMultdiag(dyidvi, y.jac);
+        end
+        if(numel(tmp)==1)
+           y = ADI(yi, tmp{1});
+           return
+        elseif( numel(tmp)==2)   
+            for i=1:numel(tmp{2})
+                tmp{1}{i}=tmp{1}{i}+tmp{2}{i};
+            end
+        else
+           error('Not possible')
+        end                       
+        y = ADI(yi, tmp{1});
         
     else
        y = interp2DTableLoc(T, x, y);
@@ -27,8 +39,8 @@ compDer = (nargout>1);
 yil = zeros(size(xi));
 yir = zeros(size(xi));
 if compDer
-    dyidvil = zeros(size(xi));
-    dyidvir = zeros(size(xi));
+    dyidxil = zeros(size(xi));
+    dyidxir = zeros(size(xi));
     dwdvi   = zeros(size(xi));
 end
 
@@ -36,12 +48,12 @@ end
     xval=T.x;
     yval=T.y;
     %v = T{k}.key;  pos = T{k}.pos;
-    v=xval;
+    v=yval;
     
     lims = v; lims(1)=-inf; lims(end)=inf;
-    vi_k = vi;
-    [bin,bin] = histc(xi, lims);                                     %#ok
-    w = (xi-v(bin))./(v(bin+1)-v(bin));
+    vi_k = xi;
+    [bin,bin] = histc(vi, lims);                                     %#ok
+    w = (vi-v(bin))./(v(bin+1)-v(bin));
     if compDer
         dwdvi = 1./(v(bin+1)-v(bin));
     end
@@ -51,18 +63,18 @@ end
         %ixl = (bin==(tn-1));
         ixr = (bin==tn);
         if(any(ixr))
-            yil(ixr) = interpTable(yval, tab(:,1), vi_k(ixr));        
-            yir(ixr) = interpTable(yval, tab(:,2), vi_k(ixr));
+            yil(ixr) = interpTable(xval, tab(:,1), vi_k(ixr));        
+            yir(ixr) = interpTable(xval, tab(:,2), vi_k(ixr));
         end
         if compDer
-            dyidvil(ixr) = dinterpTable(yval, tab(:,1), vi(ixr));
-            dyidvir(ixr) = dinterpTable(yval, tab(:,2), vi(ixr));
+            dyidxil(ixr) = dinterpTable(xval, tab(:,1), vi_k(ixr));
+            dyidxir(ixr) = dinterpTable(xval, tab(:,2), vi_k(ixr));
         end
     end
 %end
 yi = yil.*(1-w) + yir.*w;
 if compDer
-    dyidvi = dyidvil.*(1-w) + dyidvir.*w;
-    dyidxi = -(yil-yir).*dwdvi;
+    dyidxi = dyidxil.*(1-w) + dyidxir.*w;
+    dyidvi = -(yil-yir).*dwdvi;
 end
 end
