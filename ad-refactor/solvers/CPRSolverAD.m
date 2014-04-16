a4ecdbbfe19e3ac6b9f9871a62ea8a231ece4c95
Olav Møyner <olav.moyner@sintef.no>
2014-04-16 13:11:06 +0200

    Completely rewritten CPR solver that is reasonably easy to read and work with. Supports: - N phases - Pressure variable not being the first variable implicitly - New style linear solvers (so we can have a MG solver with actual setup phase) - Model scaling based on b factors using auto discovery of equation ordering
    It is a re-implementation/intepretation of the current best model in cprGeneric.
    
    Works on SPE9.

--- ad-refactor/solvers/CPRSolverAD.m
+++ ad-refactor/solvers/CPRSolverAD.m
@@ -7,22 +7,19 @@ classdef CPRSolverAD < linearSolverAD
     methods
         function solver = CPRSolverAD(varargin)
             solver.ellipticSolver = mldivideSolverAD();
+            % Todo fill in these options
             solver.relativeTolerance = 1e-3;
             solver.pressureScaling = 1/(200*barsa);
-%             solver.pressureScaling = 1;
-
-            
-            disp('OK!')
         end
         
         function result = solveLinearSystem(solver, A, b) %#ok
-            error('Not supported')
+            error('Not supported directly - this is a preconditioner')
         end
         
-        function [dx, result] = solveLinearProblem(solver, problem)
-
+        function [dx, result] = solveLinearProblem(solver, problem, model)
+            % Solve a linearized problem using a constrained pressure
+            % residual preconditioner
             
-            % Solve a linearized problem
             isPressure = problem.indexOfPrimaryVariable('pressure');
             pressureIndex = find(isPressure);
             
@@ -35,7 +32,7 @@ classdef CPRSolverAD < linearSolverAD
             cellEqNo = numel(cellIndex);
             
             % Find number of "cell" like variables
-            n = problem.getEquationVarNum(cellIndex(1));
+            nCell = problem.getEquationVarNum(cellIndex(1));
             nP = numel(problem);
             
             % Eliminate non-cell variables (well equations etc)
@@ -47,58 +44,62 @@ classdef CPRSolverAD < linearSolverAD
             for i = 1:numel(notCellIndex)
                 [problem, eliminated{i}] = problem.eliminateVariable(elimNames{i});
             end
-            
-            
+
             
             eqs = problem.equations;
-            diagMod = zeros(n, cellEqNo);
+                        
+            % Get and apply scaling
+            scale = getScaling(problem, model);
+            for eqn = 1:cellEqNo
+                eqs{eqn} = eqs{eqn}.*scale(eqn);
+            end
+            
+            isElliptic = false(nCell, cellEqNo);
             for i = 1:cellEqNo
                 % Find the derivative of current cell block w.r.t the
                 % pressure
-                pressureJacobi = eqs{cellIndex(i)}.jac{isPressure};
+                pressureJacobi = eqs{i}.jac{isPressure};
                 pressureDiag  = diag(pressureJacobi);
                 
                 sod = sum(abs(pressureJacobi), 2) - abs(pressureDiag);
                 % Find "bad" equations, i.e. equations where the current
                 % pressure index does not give a good elliptic jacobian
-                diagMod(pressureDiag./sod > edd, i) = pressureIndex;
+                isElliptic(pressureDiag./sod > edd, i) = true;
             end
             
-            diagMod(all(diagMod == 0, 2), 1) = 1;
-            bad = diagMod(:, 1) == 0;
+            isElliptic(all(isElliptic == 0, 2), pressureIndex) = true;
             
+            bad = ~isElliptic(:, pressureIndex);
+
             if any(bad)
                 % Switch equations for non-elliptic jacobian components with
                 % some other equation that has an elliptic pressure jacobian
-                [r, c] = find(diagMod(bad, :));
-                firstIndex = accumarray(r,c,[sum(bad), 1], @min);
-                diagMod(bad, 1) = firstIndex;
-                diagMod = diagMod(:, 1);
+                [r, c] = find(isElliptic(bad, :));
+                replacementInd = accumarray(r,c,[sum(bad), 1], @min);
                 
                 
-                for i = unique(diagMod)'
+                for i = unique(replacementInd)'
                     if i == pressureIndex
                         continue
                     end
                     % Standard switching with some overloaded indexing in
                     % the ad objects
-                    isCurrent = diagMod == i;
+                    isCurrent = replacementInd == i;
                     tmp = eqs{pressureIndex}(isCurrent);
                     eqs{pressureIndex}(isCurrent) = eqs{i}(isCurrent);
                     eqs{i}(isCurrent) = tmp;
                 end
             end
             
-            ok = diagMod(:, 1) == pressureIndex;
-            ok = ok | true;
+            tmp = 0*eqs{pressureIndex};
             for i = 1:cellEqNo
-                % Add together all equations to create the pressure-like
-                % equations that captures all jacobians
-                if i == pressureIndex
-                    continue
-                end
-                eqs{pressureIndex}(ok) = eqs{pressureIndex}(ok) + eqs{i}(ok);
+                % Add together all the equations to get a "pressure
+                % equation" where we know that all submatrices should be as
+                % close as possible to M-matrices (because of switching,
+                % which does not actually alter the solution)
+                tmp = tmp + eqs{i};
             end
+            eqs{pressureIndex} = tmp;
             problem.equations = eqs;
             
             % Set up storage for all variables, including those we
@@ -110,10 +111,6 @@ classdef CPRSolverAD < linearSolverAD
             recovered(cellIndex) = true;
             
             % Solve cell equations
-                        
-%             refSolver = mldivideSolverAD();
-%             dxCell = refSolver.solveLinearProblem(problem);
-            
             if solver.pressureScaling ~= 1
                 for i = 1:numel(problem)
                     problem.equations{i}.jac{pressureIndex} =...
@@ -121,33 +118,40 @@ classdef CPRSolverAD < linearSolverAD
                 end
             end
 
-            % ILU0 preconditioner
+            % Set up linear system
             [A, b] = problem.getLinearSystem();
             
+            % ILU0 preconditioner for the non-elliptic part
             [L, U] = ilu(A, struct('type', 'nofill'));
 
-            Ap = problem.equations{pressureIndex}.jac{pressureIndex};
+            Ap = -problem.equations{pressureIndex}.jac{pressureIndex};
             % We have only cell variables present, and these will have
             % offsets of cellnum long each
-            pInx = (1:n).' + n*(pressureIndex-1);
-            % solver
-            ellipSolve = @(A, b) solver.ellipticSolver.solveLinearSystem(A, b);
+            pInx = (1:nCell).' + nCell*(pressureIndex-1);
+            
+            
+            % Set up elliptic solver
+            solver.ellipticSolver = solver.ellipticSolver.setupSolver(Ap, b(pInx));
+            ellipSolve = @(b) solver.ellipticSolver.solveLinearSystem(Ap, b);
 
-            prec = @(r) applyTwoStagePreconditioner(r, A, L, U, Ap, pInx, ellipSolve);
+            prec = @(r) applyTwoStagePreconditioner(r, A, L, U, pInx, ellipSolve);
             [cprSol, fl, relres, its] = gmres(A, b, [], solver.relativeTolerance, 40, prec);
+            
+            % Undo pressure scaling
             if solver.pressureScaling ~= 1;
                 cprSol(pInx) = cprSol(pInx) ./ solver.pressureScaling;
             end
+            % Clean up elliptic solver
+            solver = solver.cleanupSolver(Ap, b(pInx));
+            
             dispif(mrstVerbose(), 'GMRES converged in %d iterations\n', its(2));
             dxCell = solver.storeIncrements(problem, cprSol);
             
             dx(recovered) = dxCell;
             
-            %
+            assert(all(diff(cellIndex) == 1), 'This solver currently assumes that the cell variables comes first!')
             for i = numel(eliminated):-1:1
                 pos = notCellIndex(i);
-                % OBS: cellEqNo + 1 is a hack WHICH ASSUMES THAT ALL
-                % ELIMINATED VARIABLES COMES AFTER CELL VARIABLES
                 dVal = recoverVars(eliminated{i}, cellEqNo + 1, dx(recovered));
                 dx{pos} = dVal;
                 recovered(pos) = true;
@@ -161,10 +165,44 @@ classdef CPRSolverAD < linearSolverAD
     end
 end
 
-function x = applyTwoStagePreconditioner(r, A, L, U, Ap, pInx, ellipticSolver)
+function x = applyTwoStagePreconditioner(r, A, L, U, pInx, ellipticSolver)
    x = zeros(size(r));
-   x(pInx) = ellipticSolver(Ap, r(pInx));
+   x(pInx) = ellipticSolver(r(pInx));
 
    r = r - A*x;
    x = x + U\(L\r);
+end
+
+function scale = getScaling(problem, model)
+    state = problem.state;
+    fluid = model.fluid;
+    p  = mean(state.pressure);
+    bW = fluid.bW(p);
+    
+    scale = zeros(numel(problem), 1);
+    
+    isBO = isa(model, 'threePhaseBlackOilModel');
+    
+    if isBO && model.disgas
+        rs = fluid.rsSat(p);
+        bO = fluid.bO(p, rs, true);
+    else
+        bO = fluid.bO(p);
+    end
+    if isBO && model.vapoil
+        rv = fluid.rvSat(p);
+        bG = fluid.bG(p, rv, true);
+    else
+        bG = fluid.bG(p);
+    end
+
+    if model.oil
+        scale(problem.indexOfEquationName('oil')) = 1./bO;
+    end
+    if model.gas
+        scale(problem.indexOfEquationName('gas')) = 1./bG;
+    end
+    if model.water
+        scale(problem.indexOfEquationName('water')) = 1./bW;
+    end
 end
