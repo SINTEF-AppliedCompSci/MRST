424b94371d52762c7445be9db2f3450db7836508
Xavier Raynaud <xavier.raynaud@sintef.no>
2014-05-06 10:58:31 +0200

    Shut down wells in updateSwitchedControls.

--- ad-fi/wells/updateSwitchedControls.m
+++ ad-fi/wells/updateSwitchedControls.m
@@ -1,31 +1,50 @@
-function W = updateSwitchedControls(sol, W)
-if isempty(W)
-    return
+function [W, wells_shut] = updateSwitchedControls(sol, W, varargin)
+
+   if isempty(W) || isempty(sol)
+      wells_shut = false;
+      return
+   end
+
+   opt = struct('allowWellSignChange', false);
+   opt = merge_options(opt, varargin{:});
+
+   % Check if producers are becoming injectors and vice versa
+   wsg = vertcat(W(:).sign);
+   ssg = sign(getTotalRate(sol));
+   inx = find(wsg~=ssg);
+
+   wells_shut = false;
+   if ~opt.allowWellSignChange
+      for k = 1:numel(inx)
+         wells_shut = true;
+         tps  = {'injector', 'producer'};
+         tpIx = [1 2];
+         if wsg(inx(k)) < 1, tpIx = [2 1];end
+         W(inx(k)).status = false;
+         fprintf(['Well ', W(inx(k)).name, ' has been shut down.\n']);
+      end
+   end
+
+   % Check if well-controls have been switch, if so, update W
+   inx = find(~arrayfun(@(x,y)strcmp(x.type,y.type), W(:), sol(:)));
+   for k = 1:numel(inx)
+      fromTp = W(inx(k)).type;
+      toTp   = sol(inx(k)).type;
+      fprintf(['Well ', W(inx(k)).name, ' has switched from ', fromTp, ' to ', toTp, '.\n']);
+      W(inx(k)).type = toTp;
+      W(inx(k)).val  = sol(inx(k)).val;
+   end
 end
-% Check if producers are becoming injectors and vice versa
-% Nothing is done here for now, these should be shut down
-wsg = vertcat(W(:).sign);
-ssg = vertcat(sol(:).sign);
-inx = find(wsg~=ssg);
-for k = 1:numel(inx)
-    tps  = {'injector', 'producer'};
-    tpIx = [1 2];
-    if wsg(inx(k)) < 1, tpIx = [2 1];end
 
-    warning(['Well ', W(inx(k)).name, ' has switched from ',tps(tpIx(1)), ' to ' tps(tpIx(2)),'.']);
+
+function qt = getTotalRate(sol)
+   typelist = {'qWs', 'qOs', 'qGs'};
+   typeindex = isfield(sol(1), typelist);
+   types = typelist(typeindex);
+   qt = zeros(numel(sol), 1);
+   for w = 1:numel(sol)
+      for i = 1:numel(types)
+         qt(w) = qt(w) + sol(w).(types{i});
+      end
+   end
 end
-
-% Check if well-controls have been switch, if so, update W
-inx = find(~arrayfun(@(x,y)strcmp(x.type,y.type), W(:), sol(:)));
-for k = 1:numel(inx)
-    fromTp = W(inx(k)).type;
-    toTp   = sol(inx(k)).type;
-    fprintf(['Well ', W(inx(k)).name, ' has switched from ', fromTp, ' to ', toTp, '.\n']);
-    W(inx(k)).type = toTp;
-    W(inx(k)).val  = sol(inx(k)).val;
-end
-end
-
-
-
-
