79a0da7971ecdb6673c8f9db796d412640b59453
Xavier Raynaud <xavier.raynaud@sintef.no>
2014-05-07 15:22:37 +0200

    Started adding comments for spe3.

--- ad-fi/examples/SPE3/run_spe3.m
+++ ad-fi/examples/SPE3/run_spe3.m
@@ -1,3 +1,9 @@
+%% SPE3 case for fully implicit black oil solver
+% This example solves the SPE1 problem which consists of gas injection in a
+% small ($9\times9\times4$) reservoir with a single producer and
+% injector. The problem is parsed and solved from the problem file
+% "SPE3.DATA" 
+
 require ad-fi deckformat
 
 % Read and process file.
@@ -18,19 +24,25 @@ rock  = compressRock(rock, G.cells.indexMap);
 % Create a special ADI fluid which can produce differentiated fluid
 % properties.
 fluid = initDeckADIFluid(deck);
-%fluid.bO  = @(p, rs, flag, varargin)fluid.bO(p, varargin{:});
-%fluid.muO = @(p, rs, flag, varargin)fluid.muO(p, varargin{:}); 
+
 % The case includes gravity
 gravity on
 
 %%
+% The initial state is a pressure field and  a
+%  mixture of water  and free gas (So=0.88) which are constant in each layer. The gas cannot
+%  dissolve in oil but ith no initial free
+% gas (Sg=0.0) and a constant dissolved gas/oil ratio ("Rs") throughout the
+% model.
+
 p0  = deck.SOLUTION.PRESSURE;
 sw0 = deck.SOLUTION.SWAT;
 sg0 = deck.SOLUTION.SGAS;
 s0  = [sw0, 1-sw0-sg0, sg0];
 rv0 = deck.SOLUTION.RV;
 rs0 = 0;
-state = struct('s', s0, 'rs', rs0, 'rv', rv0, 'pressure', p0);   clear k p0 s0
+state = struct('s', s0, 'rs', rs0, 'rv', rv0, 'pressure', p0);   
+clear k p0 s0 rv0 rs0
 
 %fluid.rsSat = @(p)rs0;
 %% Initialize schedule and system before solving for all timesteps
