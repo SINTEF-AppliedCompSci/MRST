3fbfb5623aa8fc86d01f82e5fac2477a9fa2b71f
kalie <Knut-Andreas.Lie@sintef.no>
2014-05-07 09:58:13 +0200

    Changed functionality of the routine and renamed it. Previously, the   columns of the partition vector 'pf' had to be refinements of each   other, i.e., pf(:,i+1) was a refined partition of pf(:,i). In the new   version, this requirement is relaxed. This means that one for a 2D   Cartesian grid can, e.g., first apply a 2x1 partition and then a   1x2 partition. In the previous version, the same effect could only be   achieved by first a 2x1 and then a 2x2 partition.

--- agglom/applySuccessivePart.m
+++ agglom/applySuccessivePart.m
@@ -1,46 +1,55 @@
-function p = refineSuccessivePart(p, G, indicator, NU, pfixed)
+function p = applySuccessivePart(p, G, indicator, NU, pfixed)
 %Refine blocks by successively applying static background partitions
 %
 % SYNOPSIS:
-%   p = refineSuccessivePart(p, G, I, NU, pf)
+%   p = applySuccessivePart(p, G, I, NU, pf)
 %
 % DESCRIPTION:
-%   Given a partition 'p' of a grid 'G', this function applies a succession
-%   of fixed subdivisions of each coarse block in which accumulated cell
-%   indicators 'I' exceed an upper limit.
-%
-%   Here, 'I' is interpreted as a density and multiplied by the cell volume
-%   to derive the total indicator value for each cell.  No block should
-%   have a total accumulated indicator value exceeding "NU / G.cells.num"
-%   times the total accumulated indicator value for the whole grid,
+%   Given a partition 'p' of a grid 'G', this function applies a set of
+%   fixed subdivisions pf(:,1) to pf(:,end) successively to coarse blocks
+%   in the grid. In each block, the next subdivision in the succession is
+%   only applied if the accumulative indicator inside the block exceeds a
+%   prescribed threshold. The indicator function 'I' is interpreted as a
+%   density and multiplied by the cell volume to derive the total indicator
+%   value for each cell. No block should have a total accumulated indicator
+%   value exceeding "NU / G.cells.num" times the total accumulated
+%   indicator value for the whole grid,
 %
 %       SUM(I .* G.cells.volumes)
 %
 % PARAMETERS:
 %   p  - Partition vector.
 %
-%   G  - Grid data structure discretising the reservoir model.
+%   G  - Grid data structure
 %
-%   I  - Cell-wise value of some measure/indicator function used for
-%        deciding which blocks to refine.
+%   I  - Cell-wise value of some indicator density function used to
+%        decide whether the next partition should be applied inside
 %
 %   NU - Upper bound on indicator values per block.  Positive scalar.
 %
-%   pf - A predefined partition vector which is substituted into each
-%        coarse block with indicator values that are too large.  If the
-%        fixed partition is a (G.cells.num)-by-m matrix, the columns are
-%        each applied once, in sequence of increasing column numbers.
+%   pf - A predefined set of partition vectors which are substituted into
+%        each coarse block with indicator values that are too large.  If
+%        the fixed partition is a (G.cells.num)-by-m matrix, the columns
+%        are each applied once, in sequence of increasing column numbers.
 %
 % NOTE:
-%   This function uses features of the 'coarsegrid' module so that module
-%   must be active in order to use function 'refineSuccessivePart'.
+%   This function uses features of the 'coarsegrid' module, which must be
+%   loaded before using the function.
 %
 % RETURNS:
-%   p - Partition vector after refinement.  The partition vector contains
-%       connected, non-empty blocks only because the final step of function
-%       'refineSuccessivePart' is to pass the vector through the functions
-%       'compressPartition' and 'processPartition' from the 'coarsegrid'
-%       module.
+%   p -  Partition vector after refinement.  The partition vector contains
+%        connected, non-empty blocks only because the final step of
+%        function 'applySuccessivePart' is to pass the vector through the
+%        functions 'compressPartition' and 'processPartition' from the
+%        'coarsegrid' module.
+%
+% EXAMPLES:
+%   G = computeGeometry(cartGrid([5 5]));
+%   p1 = partitionCartGrid(G.cartDims, [2 1]);
+%   p2 = partitionCartGrid(G.cartDims, [1 2]);
+%   p = ones(G.cells.num,1);
+%   p = applySuccessivePart(p, G, p, 6, [p1 p2]);
+%   plotCellData(G,p,'EdgeColor','k');
 %
 % SEE ALSO:
 %   compressPartition, processPartition
@@ -92,8 +101,9 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
       bfix = false([1, max(p)]);
       bfix(blockIndicator > upper_bound) = true;
 
-      p(bfix(p)) = pfixed(bfix(p), i) + max(p);
+      p(bfix(p)) = pfixed(bfix(p), i)*max(p) + p(bfix(p));
 
+      p = compressPartition(p);
       blockIndicator = blkInd(p);
    end
 
