a4afff0b323238d96b1ac4f0915e46055767ea74
Xavier Raynaud <xavier.raynaud@sintef.no>
2014-05-08 11:38:47 +0200

    Added extra check in updateSwitchedControls in case control rate in well is zero but the well is nevertheless not shut down.

--- ad-fi/wells/updateSwitchedControls.m
+++ ad-fi/wells/updateSwitchedControls.m
@@ -8,34 +8,45 @@ function [W, wells_shut] = updateSwitchedControls(sol, W, varargin)
    opt = struct('allowWellSignChange', false, 'allowControlSwitching', true);
    opt = merge_options(opt, varargin{:});
 
-   % Check if producers are becoming injectors and vice versa
+   % Check if producers are becoming injectors and vice versa. The indexes of
+   % such wells are stored in inx.
    wsg = vertcat(W(:).sign);
    ssg = sign(getTotalRate(sol));
-   inx = find(wsg~=ssg);
+   inx = wsg ~= ssg;
+
+   % A well can be set to zero rate without beeing shut down. We update inx to
+   % take into account this fact.
+   wval = vertcat(W(:).val);
+   wtype = {W(:).type}';
+   inx = inx & ~strcmp('bhp', wtype) & (wval ~= 0);
+
+   inx = find(inx);
    wells_shut = false;
    if ~opt.allowWellSignChange & opt.allowControlSwitching
       if any(inx)
          wells_shut = true;
          ostring = 'Wells ';
+
          for k = 1:numel(inx)
             W(inx(k)).status = false;
             ostring = [ostring,  W(inx(k)).name, ', '];
          end
-         fprintf([ostring(1:end - 2) ' shut down.\n']);
+
+         fprintf([ostring(1:end - 2), ' shut down.\n']);
       end
    end
 
-   
    % Check if well-controls have been switch, if so, update W
    inx = find(~arrayfun(@(x,y)strcmp(x.type,y.type), W(:), sol(:)));
    for k = 1:numel(inx)
       fromTp = W(inx(k)).type;
       toTp   = sol(inx(k)).type;
-      fprintf(['Well ', W(inx(k)).name, ' has switched from ', fromTp, ' to ', toTp, '.\n']);
+      fprintf(['Well ', W(inx(k)).name,       ...
+               ' has switched from ', fromTp, ...
+               ' to ', toTp, '.\n']);
       W(inx(k)).type = toTp;
       W(inx(k)).val  = sol(inx(k)).val;
    end
-   
 end
 
 
