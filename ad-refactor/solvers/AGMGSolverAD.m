99b9a82636b9262606c6d519e3748b679dc4e43a
Olav Møyner <olav.moyner@sintef.no>
2014-06-10 11:16:12 +0200

    AGMG based solver for the new style AD solvers. This actually uses the setup/cleanup features so it should be faster than regular CPR-AGMG.

---
+++ ad-refactor/solvers/AGMGSolverAD.m
@@ -0,0 +1,28 @@
+classdef AGMGSolverAD < linearSolverAD
+    % Base class for a nonlinear solver
+   properties
+       
+   end
+   methods
+       function solver = AGMGSolverAD(varargin)
+            solver = solver@linearSolverAD(varargin{:});
+       end
+       
+       function result = solveLinearSystem(solver, A, b)
+           % Solve the linear system to a given tolerance
+           result = agmg(A, b, [], solver.tolerance, solver.maxiterations, [], [], 2);
+       end
+       
+       function solver = setupSolver(solver, A, b, varargin) %#ok 
+           % Run setup on a solver for a given system
+           disp 'Setting up AGMG!'
+           agmg(A,[],[],[],[],[],[], 1);
+       end
+       
+       function  solver = cleanupSolver(solver, A, b, varargin) %#ok 
+           % Clean up solver after use (if needed)
+           disp 'Cleaning up AGMG!'
+           agmg(A,[],[],[],[],[],[], -1);
+       end
+   end
+end
