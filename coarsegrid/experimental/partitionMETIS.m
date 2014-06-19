285f75e4fea6445a30389b9a12ea2d155034a54d
Olav Møyner <olav.moyner@sintef.no>
2014-06-19 09:52:42 +0200

    Allow partitionMETIS to pass on more options.   Allow file to set METIS seed. Use default seed = 0.

--- coarsegrid/experimental/partitionMETIS.m
+++ coarsegrid/experimental/partitionMETIS.m
@@ -1,4 +1,4 @@
-function p = partitionMETIS(G, T, n, varargin)
+    function p = partitionMETIS(G, T, n, varargin)
 %Partition grid according to connection strengths
 %
 % SYNOPSIS:
@@ -23,6 +23,11 @@ function p = partitionMETIS(G, T, n, varargin)
 %                         equal to or less than 50%. (DEFAULT)
 %
 %  no2hop  - Do not use multiple hops when coarsening. Passed onto METIS.
+%
+%  seed    - METIS seeding parameter to create consistent results for given
+%            platform. Default: 0.
+%
+%  ncuts   - Passed onto METIS.
 %
 % RETURNS:
 %   p - Partition vector.  Contains no empty or multiply connected blocks.
@@ -53,6 +58,8 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
    require coarsegrid
 
    opt = struct('ufactor', 1.5, ...
+                'ncuts',   1, ...
+                'seed',    0, ...
                 'no2hop',  false);
    opt = merge_options(opt, varargin{:});
 
@@ -73,8 +80,8 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
    %  - 'ufactor' set to 500 to allow for 150% variation on coarse block
    %    size
    %  - 'cut' is default objtype
-   opts = sprintf('-minconn -contig -ufactor=%d -objtype=cut', ...
-                  floor(1000 * (opt.ufactor - 1)));
+   opts = sprintf('-minconn -contig -ufactor=%d -objtype=cut -ncuts=%d -seed=%d', ...
+                  floor(1000 * (opt.ufactor - 1)), opt.ncuts, opt.seed);
    if opt.no2hop
        opts = [opts, ' -no2hop'];
    end
