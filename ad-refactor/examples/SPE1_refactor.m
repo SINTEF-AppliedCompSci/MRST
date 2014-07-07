d8a9a9c6c8735674d97feb4ed1ffbbf28aef882a
Olav Møyner <olav.moyner@sintef.no>
2014-07-07 16:32:22 +0200

    Some timestep testing

--- ad-refactor/examples/SPE1_refactor.m
+++ ad-refactor/examples/SPE1_refactor.m
@@ -50,30 +50,73 @@ schedule = convertDeckScheduleToMRST(G, rock, deck);
 clear boModel
 clear nonlinear
 
-boModel = ThreePhaseBlackOilModel(G, rock, fluid, 'inputdata', deck);
-%%
+boModel = ThreePhaseBlackOilModel(G, rock, fluid, 'inputdata', deck, ...
+                                                  'useCNVConvergence', false, ...
+                                                  'nonlinearTolerance', 1e-10);
+% boModel.useCNVConvergence = false;
+% boModel.nonlinearTolerance = 1e-12;
 
 
 ellipSolver = BackslashSolverAD();
 linsolve = CPRSolverAD('ellipticSolver', ellipSolver);
 
-
-timer = tic();
-[wellSols, states] = simulateScheduleAD(state, boModel, schedule, 'linearSolver', linsolve);
-time_ms = toc(timer);
-
 %%
+timer = tic();
+[wellSols_base, states_base, report_base] = simulateScheduleAD(state, boModel, schedule, 'linearSolver', linsolve);
+time_base = toc(timer);
+%%
+% res.dt = schedule.step.val(2:end);
+clear res
+res.dt = cumsum(schedule.step.val(2:end));
+res.itcount = report_base.Iterations(2:end);
+res.target = nan;
+res.time = time_base;
+rbase = res;
+%%
+results = [];
+for i = [4, 8, 10, 15, 20]
 
+    scheduleOnestep = schedule;
+    scheduleOnestep.step.val = [1*day; sum(scheduleOnestep.step.val)];
+    scheduleOnestep.step.control = [1; 1];
 
-scheduleOnestep = schedule;
-scheduleOnestep.step.val = [1*day; sum(scheduleOnestep.step.val)/10];
-scheduleOnestep.step.control = [1; 1];
+    % timestepper = SimpleTimeStepSelector('maxTimestep', 5*day, 'verbose', true);
+    % timestepper = IterationCountTimeStepSelector('maxTimestep', 5*day, 'verbose', true);
+    timestepper = GustafssonLikeStepSelector('targetIterationCount', i,...
+                                             'minRelativeAdjustment', sqrt(eps),...
+                                             'maxRelativeAdjustment', inf);
 
-% timestepper = SimpleTimeStepSelector('maxTimestep', 5*day, 'verbose', true);
-timestepper = IterationCountTimeStepSelector('maxTimestep', 5*day, 'verbose', true);
+    nonlinear = NonLinearSolver('timeStepSelector', timestepper, 'verbose', true);
 
-nonlinear = NonLinearSolver('timeStepSelector', timestepper, 'verbose', true);
+    timer = tic();
+    [ws, s, reports] = simulateScheduleAD(state, boModel, scheduleOnestep, 'nonlinearSolver', nonlinear);
+    time_dynamic = toc(timer);
+    deltat = cellfun(@(x) x.LocalTime, reports.ControlstepReports{2}.StepReports);
+    itcount = cellfun(@(x) x.Iterations, reports.ControlstepReports{2}.StepReports);
+    
+    
+    good = cellfun(@(x) x.Converged, reports.ControlstepReports{2}.StepReports);
+    deltat = deltat(good);
+    itcount = itcount(good);
+    
+    res.dt = deltat;
+    res.itcount = itcount;
+    res.target = i;
+    res.time = time_dynamic;
+    results = [results; res];
+end
+%%
+close all
 
-[ws, s, reports] = simulateScheduleAD(state, boModel, scheduleOnestep, 'nonlinearSolver', nonlinear);
+pl = @(r, c) stairs(r.dt, r.itcount, 'linewidth', 2, 'color', c);
+pl(rbase, 'k')
 
+hold on
+nr = numel(results);
+c = lines(nr);
+for i = 1:nr
+    pl(results(i), c(i, :))
+end
+legend(['base'; arrayfun(@(x) num2str(x.target), results, 'unif', false)])
+%%
 
