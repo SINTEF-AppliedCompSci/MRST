12fcfa4c9231a38cd3e0796f84b9e7dd7cbe6856
Olav Møyner <olav.moyner@sintef.no>
2014-07-07 19:17:15 +0200

    Make a new schedule based on the report from the ad solver

---
+++ ad-refactor/utils/convertReportToSchedule.m
@@ -0,0 +1,11 @@
+function schedule = convertReportToSchedule(report, schedule)
+    [timesteps, controls] = deal([]);
+    for i = 1:numel(report.ControlstepReports)
+        cr = report.ControlstepReports{i};
+        steprep = cr.StepReports(cellfun(@(x) x.Converged, cr.StepReports));
+        timesteps = [timesteps; cellfun(@(x) x.Timestep, steprep)]; %#ok
+        controls = [controls; repmat(schedule.step.control(i), numel(cr), 1)]; %#ok
+    end
+    schedule.step.val = timesteps;
+    schedule.step.control = controls;
+end
