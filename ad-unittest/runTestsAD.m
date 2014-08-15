77a71219096ccb11fc42f7cb83ff553bab9c7390
Olav Møyner <olav.moyner@sintef.no>
2014-08-15 15:50:33 +0200

    Minor changes to the way output is written

--- ad-unittest/runTestsAD.m
+++ ad-unittest/runTestsAD.m
@@ -23,15 +23,17 @@ function results = runTestsAD(varargin)
     results = [];
     for i = 1:numel(suites)
         runner = TestRunner.withTextOutput;
-        results = [results, runner.run(suites{i})]; %#ok
+        res = runner.run(suites{i});
+        results = [results, res]; %#ok
+        if opt.writeToDisk
+            outd = fullfile(mrstPath('query', 'ad-unittest'), 'output', 'TAP');
+            if exist(outd, 'dir') == 0
+                mkdir(outd);
+            end
+            tapFile = fullfile(outd, [names{i}, '.tap']);
+            writeTestsTAP_YAMLISH(res, tapFile)
+        end
     end
     
-    if opt.writeToDisk
-        outd = fullfile(mrstPath('query', 'ad-unittest'), 'output', 'TAP');
-        if exist(outd, 'dir') == 0
-            mkdir(outd);
-        end
-        tapFile = fullfile(outd, [names{i}, '.tap']);
-        writeTestsTAP_YAMLISH(results, tapFile)
-    end
+
 end
