08f6fe02186d0ecf8ea63f6f8318e6c6f80dea96
Olav Møyner <olav.moyner@sintef.no>
2014-09-03 11:05:57 +0200

    Fixing a bug in generation of schedule from ministeps when failed ministeps are present in the report.

--- ad-core/utils/convertReportToSchedule.m
+++ ad-core/utils/convertReportToSchedule.m
@@ -53,7 +53,7 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
         steprep = cr.StepReports(cellfun(@(x) x.Converged, cr.StepReports));
         timesteps = [timesteps; cellfun(@(x) x.Timestep, steprep)]; %#ok
         controls = [controls;...
-            repmat(schedule.step.control(i), numel(cr.StepReports), 1)]; %#ok
+            repmat(schedule.step.control(i), numel(steprep), 1)]; %#ok
     end
     schedule.step.val = timesteps;
     schedule.step.control = controls;
