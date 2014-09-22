424149c49da682998d617eb251d4e4bf3eabd488
Odd Andersen <odd.andersen@gmail.com>
2014-09-22 15:42:26 +0200

    Added support for calling the new operator constructor.
    The new operator constructor setupOperatorsTPFA in mrst-autodiff intends
    to fix an inconsistency in the sign of the discretized 'div' and 'grad'
    operators.  'setupSimCompVE' can now call this new constructor instead
    of the old 'setupSimComp' if an optional argument is passed.  To
    encourage migration to the new standard, a warning is displayed whenever
    the old standard is used.

--- co2lab-ad/solving/setupSimCompVe.m
+++ co2lab-ad/solving/setupSimCompVe.m
@@ -1,21 +1,32 @@
 function s = setupSimCompVe(Gt, rock2D, varargin)
 % Wrapper for setupSimComp to produce a vertically-integrated version.
-    
-    % Compute vertially-integrated transmissibilities
-    rock_tmp = rock2D;
-    rock_tmp.perm = rock2D.perm .* Gt.cells.H;
-    T = computeTrans(Gt,rock_tmp);
-    cf = Gt.cells.faces(:,1); 
-    nf = Gt.faces.num;
-    T  = 1 ./ accumarray(cf, 1./T, [nf, 1]);
-    T_all = T;
-    
-    % Computing vertically-integrated pore-volume
-    pv = poreVolume(Gt,rock2D);
-    pv = pv.*Gt.cells.H;    
-    
-    % Calling setupSimComp, substituting the pore-volume and transmissiblity
-    % values it computes with our vertically-integrated quantities.
-    s = setupSimComp(Gt, rock2D, 'porv', pv,'trans',T);
-    s.T_all=T_all;
+   
+   opt.useNewStandard = false;
+   opt = merge_options(opt, varargin{:});
+   
+   % Compute vertially-integrated transmissibilities
+   rock_tmp = rock2D;
+   rock_tmp.perm = rock2D.perm .* Gt.cells.H;
+   T = computeTrans(Gt,rock_tmp);
+   cf = Gt.cells.faces(:,1); 
+   nf = Gt.faces.num;
+   T  = 1 ./ accumarray(cf, 1./T, [nf, 1]);
+   T_all = T;
+   
+   % Computing vertically-integrated pore-volume
+   pv = poreVolume(Gt,rock2D);
+   pv = pv.*Gt.cells.H;    
+   
+   % Calling setupSimComp, substituting the pore-volume and transmissiblity
+   % values it computes with our vertically-integrated quantities.
+   if opt.useNewStandard
+      s = setupOperatorsTPFA(Gt, rock2D, 'porv', pv,'trans',T);
+   else
+      warning(['Using old standard for the operator structure, which uses ' ...
+               'different signs for gradient and divergence term.  Consider ' ...
+               'migrating to the new standard, as implemented in ' ...
+               '''setupOperatorsTPFA''']);
+      s = setupSimComp(Gt, rock2D, 'porv', pv,'trans',T); 
+   end
+   s.T_all=T_all;
 end
