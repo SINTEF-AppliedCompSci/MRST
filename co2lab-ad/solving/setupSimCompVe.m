fc657a1f6a5d27f74a95d42b9d799a8785f4af4d
Odd Andersen <odd.andersen@sintef.no>
2014-09-30 10:09:03 +0200

    Added support for sending externally-computed transmissibilities into setupSimCompVE

--- co2lab-ad/solving/setupSimCompVe.m
+++ co2lab-ad/solving/setupSimCompVe.m
@@ -2,15 +2,21 @@ function s = setupSimCompVe(Gt, rock2D, varargin)
 % Wrapper for setupSimComp to produce a vertically-integrated version.
    
    opt.useNewStandard = false;
+   opt.trans = [];
    opt = merge_options(opt, varargin{:});
    
-   % Compute vertially-integrated transmissibilities
-   rock_tmp = rock2D;
-   rock_tmp.perm = rock2D.perm .* Gt.cells.H;
-   T = computeTrans(Gt,rock_tmp);
-   cf = Gt.cells.faces(:,1); 
-   nf = Gt.faces.num;
-   T  = 1 ./ accumarray(cf, 1./T, [nf, 1]);
+   % Compute vertially-integrated transmissibilities if not provided
+   if isempty(opt.trans)
+      rock_tmp = rock2D;
+      rock_tmp.perm = rock2D.perm .* Gt.cells.H;
+      T = computeTrans(Gt,rock_tmp);
+      cf = Gt.cells.faces(:,1); 
+      nf = Gt.faces.num;
+      T  = 1 ./ accumarray(cf, 1./T, [nf, 1]);
+   else
+      T = opt.trans;
+   end
+   
    T_all = T;
    
    % Computing vertically-integrated pore-volume
