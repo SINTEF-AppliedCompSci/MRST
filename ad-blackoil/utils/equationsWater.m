30d1d1e16f8a73a365a85ce0fe0f68d60b302660
Halvor Møll Nilsen <Halvor.M.Nilsen@sintef.no>
2014-10-03 15:21:25 +0200

    Added single phase water model.

---
+++ ad-blackoil/utils/equationsWater.m
@@ -0,0 +1,147 @@
+function [problem, state] = equationsWater(state0, state, model, dt, drivingForces, varargin)
+% Get linearized problem for oil/water system with black oil-style
+% properties
+
+opt = struct('Verbose', mrstVerbose, ...
+             'reverseMode', false,...
+             'scaling', [],...
+             'resOnly', false,...
+             'history', [],...
+             'iteration', -1, ...
+             'stepOptions', [],...
+             'addflux',false);  % Compatibility only
+
+opt = merge_options(opt, varargin{:});
+
+if ~isempty(opt.scaling)
+    scalFacs = opt.scaling;
+else
+    scalFacs.rate = 1; scalFacs.pressure = 1;
+end
+
+W = drivingForces.Wells;
+assert(isempty(drivingForces.bc) && isempty(drivingForces.src))
+
+s = model.operators;
+G = model.G;
+f = model.fluid;
+
+hst = opt.history;
+
+[p, wellSol] = model.getProps(state, 'pressure', 'wellsol');
+
+[p0] = model.getProps(state0, 'pressure');
+
+
+pBH    = vertcat(wellSol.bhp);
+qWs    = vertcat(wellSol.qWs);
+%qOs    = vertcat(wellSol.qOs);
+
+%Initialization of independent variables ----------------------------------
+
+if ~opt.resOnly,
+    % ADI variables needed since we are not only computing residuals.
+    if ~opt.reverseMode,
+        [p, qWs, pBH] = ...
+            initVariablesADI(p, qWs, pBH);
+    else
+        [p0, tmp, tmp] = ...
+            initVariablesADI(p0, sW0,          ...
+            zeros(size(qWs)), ...
+            zeros(size(qOs)), ...
+            zeros(size(pBH)));                          %#ok
+    end
+end
+primaryVars = {'pressure','qWs', 'bhp'};
+
+clear tmp
+g  = norm(gravity);
+
+%--------------------
+%check for p-dependent tran mult:
+trMult = 1;
+if isfield(f, 'tranMultR'), trMult = f.tranMultR(p); end
+
+%check for p-dependent porv mult:
+pvMult = 1; pvMult0 = 1;
+if isfield(f, 'pvMultR')
+    pvMult =  f.pvMultR(p);
+    pvMult0 = f.pvMultR(p0);
+end
+transMult=1;
+if isfield(f, 'transMult')
+   transMult=f.transMult(p); 
+end
+
+trans=s.T.*transMult;
+% -------------------------------------------------------------------------
+% water props (calculated at oil pressure OK?)
+%bW     = f.bW(p);
+bW     = f.bW(p);
+rhoW   = bW.*f.rhoWS;
+% rhoW on face, avarge of neighboring cells (E100, not E300)
+rhoWf  = s.faceAvg(rhoW);
+%mobW   = trMult.*krW./f.muW(p);
+mobW   = trMult./f.muW(p);
+dpW     = s.grad(p) - g*(rhoWf.*s.grad(G.cells.centroids(:,end)));
+% water upstream-index
+upc = (double(dpW)>=0);
+bWvW = s.faceUpstr(upc, bW.*mobW).*trans.*dpW;
+if(opt.addflux)
+    state.bWvW = bWvW;
+    state.WvW = s.faceUpstr(upc, mobW).*trans.*dpW;
+end
+% EQUATIONS ---------------------------------------------------------------
+% water:
+eqs{1} = (s.pv/dt).*( pvMult.*bW - pvMult0.*f.bW(p0) ) - s.div(bWvW);
+
+names = {'water'};
+types = {'cell'};
+% well equations
+if ~isempty(W)
+    if ~opt.reverseMode
+        wc    = vertcat(W.cells);
+        pw   = p(wc);
+        rhos = [f.rhoWS];
+        bw   = {bW(wc)};
+        mw   = {mobW(wc)};
+        s = {1};
+
+        wm = WellModel();
+        [cqs, weqs, ctrleqs, wc, state.wellSol]  = wm.computeWellFlux(model, W, wellSol, ...
+                                             pBH, {qWs}, pw, rhos, bw, mw, s, {},...
+                                             'nonlinearIteration', opt.iteration,'referencePressureIndex', 1);
+        eqs(2) = weqs;
+        eqs{3} = ctrleqs;
+        
+        eqs{1}(wc) = eqs{1}(wc) - cqs{1};        
+        names(2:3) = {'waterWells', 'closureWells'};
+        types(2:3) = {'perf', 'well'};
+    else
+        % in reverse mode just gather zero-eqs of correct size
+        for eqn = 2:3
+            nw = numel(state0.wellSol);
+            zw = double2ADI(zeros(nw,1), p0);
+            eqs(2:3) = {zw, zw};
+        end
+        names(2:3) = {'empty', 'empty'};
+        types(2:3) = {'none', 'none'};
+    end
+else % no wells
+    eqs(2:3) = {pBH, pBH};  % empty  ADIs
+    names(2:3) = {'empty', 'empty'};
+    types(2:3) = {'none', 'none'};
+end
+problem = LinearizedProblem(eqs, types, names, primaryVars, state, dt);
+problem.iterationNo = opt.iteration;
+end
+%--------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
