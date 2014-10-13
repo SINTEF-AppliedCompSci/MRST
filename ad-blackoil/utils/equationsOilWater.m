009a3502a5a7353d0ea0387d88d7589d21a11c3c
Stein Krogstad <stein.krogstad@sintef.no>
2014-10-13 11:14:12 +0200

    Updated code such that operators Div and Grad have correct signs. Equations should now resemble what is found in textbooks. Also added functionality for general gradient vector.

--- ad-blackoil/utils/equationsOilWater.m
+++ ad-blackoil/utils/equationsOilWater.m
@@ -64,10 +64,10 @@ if isfield(f, 'pcOW')
 end
 % Gravity contribution, assert that it is aligned with z-dir
 grav = gravity();
-assert(grav(1) == 0 && grav(2) == 0);
-g  = norm(grav);
-dz = s.grad(G.cells.centroids(:,3));
-
+%assert(grav(1) == 0 && grav(2) == 0);
+%g  = norm(grav);
+%dz = s.Grad(G.cells.centroids(:,3));
+gdz = s.Grad(G.cells.centroids) * grav';
 
 % Compute transmissibility
 T = s.T.*transMult;
@@ -81,10 +81,10 @@ rhoW   = bW.*f.rhoWS;
 % rhoW on face, average of neighboring cells
 rhoWf  = s.faceAvg(rhoW);
 mobW   = trMult.*krW./f.muW(p);
-dpW     = s.grad(p-pcOW) - g*(rhoWf.*dz);
+dpW     = s.Grad(p-pcOW) - rhoWf.*gdz;
 % water upstream-index
-upcw = (double(dpW)>=0);
-vW   = s.faceUpstr(upcw, mobW).*T.*dpW;
+upcw = (double(dpW)<=0);
+vW   = - s.faceUpstr(upcw, mobW).*T.*dpW;
 bWvW = s.faceUpstr(upcw, bW).*vW;
 if any(bW < 0)
     warning('Negative water compressibility present!')
@@ -94,17 +94,17 @@ end
 bO     = f.bO(p);
 rhoO   = bO.*f.rhoOS;
 rhoOf  = s.faceAvg(rhoO);
-dpO    = s.grad(p) - g*(rhoOf.*dz);
+dpO    = s.Grad(p) - rhoOf.*gdz);
 % oil upstream-index
-upco = (double(dpO)>=0);
+upco = (double(dpO)<=0);
 if isfield(f, 'BOxmuO')
     % mob0 is already multplied with b0
     mobO   = trMult.*krO./f.BOxmuO(p);
-    bOvO   = s.faceUpstr(upco, mobO).*T.*dpO;
+    bOvO   = - s.faceUpstr(upco, mobO).*T.*dpO;
     vO = bOvO./s.faceUpstr(upco, bO);
 else
     mobO   = trMult.*krO./f.muO(p);
-    vO = s.faceUpstr(upco, mobO).*T.*dpO;
+    vO = - s.faceUpstr(upco, mobO).*T.*dpO;
     bOvO   = s.faceUpstr(upco, bO).*vO;
 end
 if any(bO < 0)
@@ -123,10 +123,10 @@ end
 
 % EQUATIONS ---------------------------------------------------------------
 % oil:
-eqs{1} = (s.pv/dt).*( pvMult.*bO.*(1-sW) - pvMult0.*f.bO(p0).*(1-sW0) ) - s.div(bOvO);
+eqs{1} = (s.pv/dt).*( pvMult.*bO.*(1-sW) - pvMult0.*f.bO(p0).*(1-sW0) ) + s.Div(bOvO);
 
 % water:
-eqs{2} = (s.pv/dt).*( pvMult.*bW.*sW - pvMult0.*f.bW(p0).*sW0 ) - s.div(bWvW);
+eqs{2} = (s.pv/dt).*( pvMult.*bW.*sW - pvMult0.*f.bW(p0).*sW0 ) + s.Div(bWvW);
 
 names = {'oil', 'water'};
 types = {'cell', 'cell'};
