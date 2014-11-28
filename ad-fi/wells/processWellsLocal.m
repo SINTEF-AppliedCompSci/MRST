9491ccc46fea8ea556e7b8a6555568137306bde7
Olav Møyner <olav.moyner@sintef.no>
2014-11-28 13:26:19 +0100

    Treat cstatus field in same manner as other perforation values

--- ad-fi/wells/processWellsLocal.m
+++ ad-fi/wells/processWellsLocal.m
@@ -597,15 +597,18 @@ end
 
 function W = reorderByDepth(G, W)
    for i = 1:numel(W)
-      cells = W(i).cells;
-      WI = W(i).WI;
-      dZ = W(i).dZ;
+      cells   = W(i).cells;
+      WI      = W(i).WI;
+      dZ      = W(i).dZ;
+      cstatus = W(i).cstatus;
+      
       % depth = G.cells.centroids(cells, 3);
       index = (1:numel(cells))';
-      new_index = sortrows(horzcat(dZ, index, cells, WI));
-      W(i).dZ = new_index(:, 1);
-      W(i).cells = new_index(:, 3);
-      W(i).WI = new_index(:, 4);
+      new_index = sortrows(horzcat(dZ, index, cells, WI, cstatus));
+      W(i).dZ      = new_index(:, 1);
+      W(i).cells   = new_index(:, 3);
+      W(i).WI      = new_index(:, 4);
+      W(i).cstatus = new_index(:, 5);
    end
 end
 
