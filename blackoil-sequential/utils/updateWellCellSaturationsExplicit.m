1447815fc93bb432ab3c5b648b0f560e38a877ff
Olav Møyner <olav.moyner@sintef.no>
2014-12-17 13:10:34 +0100

    Adding routine for (explicit) updates of saturation in injection cells.

---
+++ blackoil-sequential/utils/updateWellCellSaturationsExplicit.m
@@ -0,0 +1,40 @@
+function state = updateWellCellSaturationsExplicit(model, state, problem, dx, drivingForces)
+    
+    W = drivingForces.Wells;
+    
+    inj = vertcat(drivingForces.Wells.sign) > 0;
+%     perf2well = getPerforationToWellMapping(W);
+%     active = inj(perf2well);
+    
+    cells = vertcat(W(inj).cells);
+    
+    dt = problem.dt;
+    
+    b = state.bfactor(cells, :);
+    b0 = state.bfactor0(cells, :);
+    
+    s = state.s(cells, :);
+    s0 = state.s0(cells, :);
+
+    
+    intx = model.operators.internalConn;
+    div = model.operators.C(:, cells)';
+    
+    
+    pv = repmat(model.operators.pv(cells), 1, size(s, 2));
+    
+    cqs = vertcat(state.wellSol(inj).cqs);
+    
+    
+    divV = div*state.flux(intx, :);
+    
+    if model.disgas
+        rs = state.rs(cells);
+        divV(:, 3) = divV(:, 2)*rs;
+    end
+    
+    
+    cap = @(x) min(max(x, 0), 1);
+    state.s(cells, :) = cap((dt*(cqs - divV)./pv + s0.*b0)./b);
+    
+end
