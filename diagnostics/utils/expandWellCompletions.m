88ad857a75564ee1bd59bce0a1040f5c74f12dbb
Olav Møyner <olav.moyner@sintef.no>
2015-02-05 09:18:31 +0100

    Catch up to rename from master (x -> state).

--- diagnostics/utils/expandWellCompletions.m
+++ diagnostics/utils/expandWellCompletions.m
@@ -101,6 +101,6 @@ for i=1:nw
       Wc(n).WI    = W(i).WI(b==j);
       Wc(n).dZ    = W(i).dZ(b==j);
       Wc(n).name  = [W(i).name ':' num2str(j)];
-      xc.wellSol(n).flux = x.wellSol(i).flux(b==j);
+      xc.wellSol(n).flux = state.wellSol(i).flux(b==j);
    end
 end
