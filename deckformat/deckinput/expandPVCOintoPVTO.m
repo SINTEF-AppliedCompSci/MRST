f299300c8f32080211892ac30044cce2cebd5a46
Olav Møyner <olav.moyner@sintef.no>
2015-03-06 17:12:11 +0100

    Minor doc.

--- deckformat/deckinput/expandPVCOintoPVTO.m
+++ deckformat/deckinput/expandPVCOintoPVTO.m
@@ -1,4 +1,5 @@
 function T = expandPVCOintoPVTO(tbl, ntab)
+% Utility for expanding a PVCO table into a PVTO table
     T = cell([ntab, 1]);
     
     for i = 1:ntab
@@ -10,11 +11,12 @@ function T = expandPVCOintoPVTO(tbl, ntab)
                    'data', zeros(npts*2, 3));
         t.key = tloc(:, 1);
         
+        % We have data for saturated regions, need to get interpolated
+        % values for undersaturated retiongs
         satix = 1:2:(2*npts-1);
         usatix = 2:2:(2*npts);
         
         sat_tbl = tloc(:, 2:4);
-        
         
         t.data(satix, :) = sat_tbl;
         t.data(usatix, :) = makeUsatTable(sat_tbl, tloc(:, 5:6));
@@ -28,6 +30,7 @@ function tbl = makeUsatTable(sat_tbl, cv)
     p0 = sat_tbl(:, 1);
     p = 0.75*p0;
     
+    % Interpolate using constants
     b  = sat_tbl(:, 2).*exp(-(p - p0).*cv(:, 1));
     mu = sat_tbl(:, 3).*exp(-(p - p0).*cv(:, 2));
     
