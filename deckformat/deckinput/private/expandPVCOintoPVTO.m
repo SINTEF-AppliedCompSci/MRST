28348d49eb5f0299d78a4929892ae77396c9b4c1
Olav Møyner <olav.moyner@sintef.no>
2015-03-09 13:32:19 +0100

    Rewrite PVCO support to include several data points.

--- deckformat/deckinput/private/expandPVCOintoPVTO.m
+++ deckformat/deckinput/private/expandPVCOintoPVTO.m
@@ -2,37 +2,50 @@ function T = expandPVCOintoPVTO(tbl, ntab)
 % Utility for expanding a PVCO table into a PVTO table
     T = cell([ntab, 1]);
     
+    % Relative increments used to evaluate undersaturated curves
+    dp = [1.01, 1.05, 1.1, 1.25];
+    ndp = numel(dp);
+    
     for i = 1:ntab
         tloc = tbl{i};
         npts = size(tloc, 1);
         
+        nd = npts*(1 + ndp);
         t = struct('key' , [], ...
                    'pos' , 1, ...
-                   'data', zeros(npts*2, 3));
+                   'data', zeros(nd, 3));
         t.key = tloc(:, 1);
         
-        % We have data for saturated regions, need to get interpolated
-        % values for undersaturated retiongs
-        satix = 1:2:(2*npts-1);
-        usatix = 2:2:(2*npts);
+        % We have data for saturated regions, need to evaluate the
+        % undersaturated curves
+        satix = false(nd, 1);
+        satix(1:(ndp+1):(nd - ndp)) = true;
         
         sat_tbl = tloc(:, 2:4);
         
         t.data(satix, :) = sat_tbl;
-        t.data(usatix, :) = makeUsatTable(sat_tbl, tloc(:, 5:6));
-        t.pos = (1:2:(2*npts + 1))';
+        t.data(~satix, :) = makeUsatTable(sat_tbl, tloc(:, 5:6), dp);
+        t.pos = (1:(1+ndp):(nd + 1))';
         
         T{i} = t;
     end
 end
  
-function tbl = makeUsatTable(sat_tbl, cv)
-    p0 = sat_tbl(:, 1);
-    p = 1.25*p0;
+function tbl = makeUsatTable(sat_tbl, cv, dp)
+    % Compute b/mu for undersaturated curves
+    p0 = repmat(sat_tbl(:, 1), 1, numel(dp));
+    p = bsxfun(@times, p0, dp);
     
-    % Interpolate using constants
-    b  = sat_tbl(:, 2).*exp(-(p - p0).*cv(:, 1));
-    mu = sat_tbl(:, 3).*exp(-(p - p0).*cv(:, 2));
+    b  = constantPressureDependence(sat_tbl(:, 2), p, p0, cv(:, 1));
+    mu = constantPressureDependence(sat_tbl(:, 3), p, p0, cv(:, 2));
     
-    tbl = [p, b, mu];
+    fix = @(x) reshape(x', [], 1);
+    tbl = [fix(p), fix(b), fix(mu)];
+end
+
+function v = constantPressureDependence(v0, p, p0, c)
+    % v(p) = v(p_0) e ^ -(p - p_0) C
+    pdiff = p0 - p;
+    e = exp(bsxfun(@times, pdiff, c));
+    v = bsxfun(@times, v0, e);
 end
