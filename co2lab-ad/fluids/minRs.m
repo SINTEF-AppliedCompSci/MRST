6331ef3b72aba6c1de8a4e8bab2be83f1405352b
Odd Andersen <odd.andersen@sintef.no>
2015-04-05 17:53:08 +0200

    Added a little bit of documentation

--- co2lab-ad/fluids/minRs.m
+++ co2lab-ad/fluids/minRs.m
@@ -1,4 +1,8 @@
 function min_rs= minRs(p,sG,sGmax,f, G)
+% Determine the minimal amount of dissolved CO2 in each cell, based on the
+% maximum historical CO2 saturation in each cell.  The brine in the part of
+% the column that contains CO2 (whether free-flowing or residual) is assumed
+% to be saturated with CO2.
     
     % computing |g|.(rho_w - rho_g)
     drho=norm(gravity)*(f.rhoWS.*f.bW(p)-f.rhoGS.*f.bG(p));
@@ -23,19 +27,3 @@ function min_rs= minRs(p,sG,sGmax,f, G)
 
 end
 
-% -------------------------------- DEPRECATED --------------------------------
-%     %{
-%     pcWG=f.pcWG(sG,p,'sGmax',double(sGmax));
-%     h=pcWG./drho;
-%     assert(all(double(h)>=0));
-%     h_max(h<=0)=sG(h<=0).*G.cells.H(h<=0)/(f.res_gas);
-%     h(h<=0)=0*h(h<=0);
-%     min_rs1=(1-f.res_gas).*(h_max-h).*f.dis_max...% rs in the oil/water sone
-%                 +(f.res_water).*h.*f.dis_max;
-%     min_rs1=min_rs1/G.cells.H;        
-%     %}
-
-%     %assume all water above h_max is saturated
-%     %min_rs=((1-sG)-(G.cells.H-h_max)./G.cells.H).*f.dis_max;   
-%     %min_rs=min_rs./G.cells.H;
-% end
