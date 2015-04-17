f30f4a0f0e856c134c76d3638ce9e6219a25afab
Olav Møyner <olav.moyner@sintef.no>
2015-04-17 11:45:09 +0200

    Delete empty file and delete developer specific mystartup.m

--- ad-props/mystartup.m
+++
@@ -1,2 +0,0 @@
-addpath('/home/hnil/heim/BITBUCKET/mrst-bitbucket/my_modules/mrst-external/fluid/coolprops/')
-addpath('/home/hnil/heim/BITBUCKET/mrst-bitbucket/my_modules/coolprops_git/wrappers/MATLAB/')
