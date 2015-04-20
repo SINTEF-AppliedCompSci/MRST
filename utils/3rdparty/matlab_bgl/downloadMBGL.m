0d228017c9cbd88caa5b1725cf411c475f820c76
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2015-04-20 19:15:28 +0200

    MatlabBGL: Chase new location
    This commit fixes the automatic download script for 3rd-party module
    'matlab_bgl' to reference the current location of the .ZIP file.  As
    a fall-back we echo the FEX location in the command window in case
    we fail to download the bits.
    
    While here, also prune the original modload script.  This was
    renamed into modload_fallback and not properly removed when the
    mechanism was first designed.

--- utils/3rdparty/matlab_bgl/downloadMBGL.m
+++ utils/3rdparty/matlab_bgl/downloadMBGL.m
@@ -1,26 +1,37 @@
 function downloadMBGL
-   fex = 'http://www.mathworks.com/matlabcentral/fileexchange/';
-   pth = 'downloads/6038';  % Probably not stable over time...
-   zip = 'matlab_bgl-4.0.1.zip';
-   url = [fex, pth];
+   fex = 'http://www.mathworks.com/matlabcentral/fileexchange';
+   pth = 'submissions/10922/v/2/download';  % Probably not stable over time
+   zip = 'zip';                             % Possibly not stable over time
+   url = [fex, '/', pth, '/', zip];
 
    % Ouput directory.
    dest = fullfile(ROOTDIR, 'utils', '3rdparty', 'matlab_bgl');
 
-   fprintf(['Downloading ', zip, ' ... '])
-   t = tic;
-   unzip(url, dest);
-   t = toc(t);
-   fprintf('done (%.02f [s])\n\n', t);
+   try
+      fprintf('Downloading MatlabBGL ZIP archive ... ')
+      t = tic;
+      unzip(url, dest);
+      t = toc(t);
+      fprintf('done (%.02f [s])\n\n', t);
+   catch
+      fallback  = [fex, '/', '10922-matlabbgl'];
+      print_url = ['<a href="', fallback, '">', fallback, '</a>'];
 
-   % MatlabBGL contains an 'assert.m' file in its 'test' directory that
-   % conflicts with the built-in function of the same name.  Just use the
-   % built-in, because the semantics of 'assert.m' are the same as for
-   % ASSERT.
-   %
-   fprintf('Patching MatlabBGL for compatibility with recent MATLAB ... ');
-   delete(fullfile(dest, 'matlab_bgl', 'test', 'assert.m'));
-   fprintf('done\n');
+      error(['Failed to retrieve MatlabBGL package from known URL\n', ...
+             'Please see fall-back location\n  * %s'], print_url);
+   end
+
+   if exist('assert', 'builtin'),
+      % MatlabBGL contains an 'assert.m' file in its 'test' directory that
+      % conflicts with the built-in function of the same name.  Just use
+      % the built-in, because the semantics of 'assert.m' are the same as
+      % for ASSERT.
+      %
+      fprintf(['Patching MatlabBGL for compatibility ', ...
+               'with recent MATLAB ... ']);
+      delete(fullfile(dest, 'matlab_bgl', 'test', 'assert.m'));
+      fprintf('done\n');
+   end
 
    % Rewrite the module path for matlab_bgl to point to the correct
    % directory
