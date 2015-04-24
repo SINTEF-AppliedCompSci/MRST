bede6d1966d91cf83baab7865716196de95a543e
Odd Andersen <odd.andersen@sintef.no>
2015-04-24 09:10:05 +0200

    Added the source files for paper 3, overlooked in last commit.

---
+++ co2lab/examples/publication_code/paper3/plotTopSurfaceExample.m
@@ -0,0 +1,293 @@
+function plotTopSurfaceExample(simres)
+
+   gravity on;
+   
+%% Produce plots for figure 11
+   
+   % Simulation indices for simulations with dissolution, caprock
+   % oscillations, residual saturation and one of the fluid types 'sharp
+   % interface', 'linear cap.' and 'P-scaled table'.
+   ix = [10 12 14];
+   %   top_surface_reconstruction(simres(ix));
+   
+%% Produce graphs for figure 12
+
+   plot_plume_shapes(simres);
+
+end
+
+% ----------------------------------------------------------------------------
+
+function plot_plume_shapes(res)
+
+   legendtext = {'No dissolution (A=0)', 'Dissolution (A=0)', ...
+                 'No dissolution (A=2)', 'Dissolution (A=2)'};
+   
+   line_colors={'k','b','m','g'};
+   
+   Gt_rough = res{9}.Gt;
+   Gt       = res{1}.Gt;
+   
+   z = Gt_rough.cells.z;
+   zt = max(z) * ones(size(z));
+   for i = 2:numel(zt) - 1
+      zt(i) = max(z(i:end)); 
+   end
+   zt(end) = max(zt(end - 1), z(end)); 
+   ht = zt - z; 
+   ff = exp( - linspace( - 25, 25, 501).^2); ff = ff' / sum(ff); 
+   hts = filter2(ff, ht);
+   
+   % Now setting z to the smooth version of the caprock
+   z  = Gt.cells.z;   
+   
+   fluid_types = {'sharp interface','linear cap','P-scaled table', ...
+                  'P-K-scaled table'};   
+   residual = true;
+   count = 1;
+   for smooth = [true false]
+
+      f = figure(); clf; hold on;
+      set(f, 'Position', [0, 600, 700, 500]);
+      
+      for i = 1:4 % fluid type
+      
+         for dissolution = [false, true]
+         
+            state = res{count}.states{end-70};
+            fluid = res{count}.fluid;
+            sG = free_sg(state.s(:,2),state.sGmax, ...
+                         struct('res_gas',fluid.res_gas, ...
+                                'res_water', fluid.res_water));
+            hold on;
+            if (dissolution)
+               mline = [line_colors{i}, '--'];
+            else
+               mline = [line_colors{i}, '-'];
+            end
+            
+            xc = Gt.cells.centroids(:,1)/1e3;
+            xx = xc(150:650);
+            ff = exp(-((xx-xc(400))/(0.3)).^2);
+            ff = ff/sum(ff);                        
+            
+            plot(xc, filter2(ff, sG .* Gt.cells.H), mline, 'LineWidth', 2);
+
+            drawnow;
+            count = count + 1;
+         end
+         
+      end
+      if ~smooth
+         plot(xc(50:end-50), hts(50:end-50), 'r-', 'LineWidth', 2);
+      end
+      
+      axis tight;
+      ax = axis(); 
+      set(gca, 'YDir', 'reverse', 'FontSize', 16); 
+      % Add to legends one for color and one for linestyle
+      ivec = [1, 2, 4, 3]; 
+      if smooth
+         addLegends(gca, {fluid_types{ivec}}, {line_colors{ivec}}, {legendtext{1:2}}, ...
+                    {' - ', ' -- '}); 
+      else
+         addLegends(gca, {fluid_types{ivec}}, {line_colors{ivec}}, {legendtext{3:4}}, ...
+                    {' - ', ' -- '});
+    end
+   end
+end
+
+% ----------------------------------------------------------------------------
+
+function top_surface_reconstruction(res)
+
+   Gt = res{1}.Gt;
+   z  = Gt.cells.z;
+   zt = max(z)*ones(size(z));
+   for i=2:numel(zt)-1
+      zt(i)=max(z(i:end));
+   end
+   zt(end) = max(zt(end-1),z(end));
+   ht  = zt - z;
+   h_trap = ht;
+   
+   rock = struct('perm', 100 * milli * darcy * ones(Gt.parent.cells.num, 1),...
+                 'poro', 0.2 * ones(Gt.parent.cells.num, 1)); 
+   rock2D = averageRock(rock, Gt); 
+   
+   
+   
+   for f_ix = 1:3 % fluid type
+
+      fluid = res{f_ix}.fluid;
+      Gt    = res{f_ix}.Gt;
+      
+      for nn = [50 80] % time step
+         state = res{f_ix}.states{nn};
+         
+         sG_free = free_sg(state.s(:,2),state.sGmax, ...
+                           struct('res_gas',fluid.res_gas, ...
+                                  'res_water', fluid.res_water));
+         
+         f = figure(); clf, hold on; 
+         set(f, 'Position', [0, 600, 700, 500]); 
+         p          = state.pressure; 
+         sG         = state.s(:,2);
+         sGmax      = state.sGmax;
+         drho       = fluid.rhoWS .* fluid.bW(p) - fluid.rhoGS .* fluid.bG(p); 
+         h          = (fluid.pcWG(sG, p, 'sGmax', sGmax)) ./ (drho * norm(gravity())); 
+         h_max      = (fluid.pcWG(sGmax, p, 'sGmax', sGmax)) ./ (drho * norm(gravity()));
+         mm         = minRs(p, sG, sGmax, fluid, Gt); 
+         h_res_diff = Gt.cells.H .* ((1 - sG) .* state.rs - mm) / fluid.dis_max;
+         
+         assert(all(h_res_diff >= -1e-5)); 
+         h_res = h_max + h_res_diff;
+
+         zt = Gt.cells.z; 
+         zb = zt + Gt.cells.H; 
+         xc = Gt.cells.centroids(:,1)/1e3;
+
+         patch(xc([1 1:end end]), [zt(end) - 20; zt; zt(end) - 20],.7 * [1 1 1]); 
+         patch(xc([1 1:end end]), [zb(1) + 20; zb; zb(1) + 20],.7 * [1 1 1]); 
+         
+         patch(xc([1:end end: - 1:1]),...
+               [zt + h_trap; zt(end: - 1:1)], myCOColor(1))
+         patch(xc([1:end end: - 1:1]),...
+               [zt + h; zt(end: - 1:1) + h_trap(end: - 1:1)], myCOColor(2))
+         patch(xc([1:end end: - 1:1]),...
+               [zt + h; zt(end: - 1:1) + h_max(end: - 1:1)], myCOColor(3))
+         patch(xc([1:end end: - 1:1]),...
+               [zt + h_max; zt(end: - 1:1) + h_res(end: - 1:1)], myCOColor(4))
+         patch(xc([1:end end: - 1:1]),...
+               [zt + h_res; zb(end: - 1:1)], myCOColor(5))
+         assert(all(h_max >= h)); 
+         % line(xc([1:end]), zt + h_max, 'LineWidth', 2, 'Color', 'k')
+         patch(xc([1 1:end end]), [zt(end) - 20; zt; zt(end) - 20], 0.7 * [1 1 1]); 
+         
+         set(gca, 'YDir', 'reverse'), axis tight
+
+         drawnow;   
+         hold on
+         
+         set(gca, 'FontSize', 19)
+         set(gca, 'Ydir', 'reverse')
+         xlabel('X km', 'FontSize', 16)
+         ylabel('Depth m', 'FontSize', 16)
+         set(gca, 'Color', 'none')
+
+         h1 = gca;
+         if(nn == 50)
+            cells = [89, 135, 200, 270, 89];
+         else% (nn == 80)
+            cells = [89, 270, 350, 405, 89];
+         end
+         
+         for ii = 1:numel(cells)
+            cell = cells(ii);
+            ha{ii} = axes();%#ok
+            if(ii == 1)
+               set(ha{ii}, 'Position', get(h1, 'Position') * 0.3 + [0.4, 0.78,- 0.05,- 0.15])   
+            elseif(ii == 2)
+               set(ha{ii}, 'Position', get(h1, 'Position') * 0.3 + [0.30, 0.13, 0.0, 0.0]); 
+            elseif(ii == 3)
+               set(ha{ii}, 'Position', get(h1, 'Position') * 0.3 + [0.60, 0.13, 0.0, 0.0]); 
+            elseif(ii == 4)
+               set(ha{ii}, 'Position', get(h1, 'Position') * 0.3 + [0.60, 0.44, 0.00, 0])
+            elseif(ii == 5)
+               set(ha{ii}, 'Position', get(h1, 'Position') * 0.3 + [0.15, 0.635, 0.0, 0.0]); 
+            else
+               error('Wrong cell');
+            end        
+         
+            hold on
+            rock = rock2D; 
+            kscale = sqrt(rock.poro(cell) ./ (rock.perm(cell))) * fluid.surface_tension; 
+            H = Gt.cells.H(cell);
+
+            h_int = h(cell);
+            h_int_max = h_max(cell); 
+            h_int_res_b = h_res(cell); 
+            
+            ss = []; hh = []; krk = []; ss_max = []; hh_max = []; 
+            
+            [s, pc, kr, s_max, s_free, fval] = veRelpermTesterCell(h_int, drho(cell), fluid, H, ....
+                                                              'samples', 1000, ...
+                                                              'hs_max', h_int_max, ...
+                                                              'kscale', kscale); 
+            mtol = 1e-2; 
+            assert(abs(s_free - sG_free(cell))<mtol); 
+            assert(abs(s - sG(cell))<mtol); 
+            assert(abs(s_max - sGmax(cell))<mtol); 
+            pc_cells = fluid.pcWG(sG, p, 'sGmax', sGmax); 
+            assert(abs(pc - pc_cells(cell))<mtol); 
+            krG = fluid.krG(sG, p, 'sGmax', sGmax); 
+            assert(abs(kr - krG(cell))<mtol); 
+            
+            ss = [ss, 1 - fval.s_h];%#ok
+            ss_max = [ss_max, 1 - fval.s_hmax];%#ok
+            hh_max = [hh_max, h_int_max - fval.h_max];%#ok
+            krk = [krk, fval.kr_h];%#ok
+            hh = [hh, h_int - fval.h];%#ok
+            
+            s_max = @(d) interp1(hh_max, ss_max, d, 'linear', 0);
+            if(h_int>0)
+               s = @(d) interp1(hh, ss, d, 'linear',0);
+            else
+               s = @(d) d*0;
+            end
+
+            res_s = @(d) double(d<(h_int_res_b));
+            w_s = @(d) ones(size(d)); 
+            d = linspace(0, 50, 1345)';
+            
+            ptch =@(x,y) [x;y(end:-1:1);x(1)];
+            axes(ha{ii});%#ok
+            
+            assert(all(s(d) >= 0))
+            s_res = (s_max(d) - s(d)) .* fluid.res_gas; % 'non flowing co2'  
+            patch(ptch(s_res, s(d) * 0), ptch(d, d), myCOColor(3))  
+            s_real = s(d) + s_res; % total saturation
+            ind = find(d>h_trap(cell), 1, 'first'); 
+            patch(ptch(s_real(1:ind), s_res(1:ind)), ptch(d(1:ind), d(1:ind)), myCOColor(1))        
+            patch(ptch(s_real(ind + 1:end), s_res(ind + 1:end)), ptch(d(ind + 1:end), d(ind + 1:end)), myCOColor(2))        
+            
+            fix = res_s(d); 
+            patch(ptch(fix, s_real), ptch(d, d), myCOColor(4))
+            patch(ptch(w_s(d), fix),ptch(d,d),myCOColor(5))
+            
+            line([0.0 1.0],[h_trap(cell) h_trap(cell)],'Color',myCOColor(1),'LineWidth',1);            
+            
+            ss_max = s_max(d); 
+            ss_max(d>h_int_max) = nan; 
+            ind = find(d>h_int_max, 1, 'first');
+            ss_max(ind) = 0; 
+            plot(ss_max, d, 'k'); % , 'LineWidth', 2) 
+
+            set(gca, 'YDir', 'reverse')
+            box on
+            set(gca, 'LineWidth', 2)
+            set(gca, 'FontSize', 16)
+            axes(h1);%#ok
+            line([xc(cell), xc(cell)], [zt(cell) - 50, zb(cell) + 50], 'LineWidth', 4, 'Color', 'r')
+            axes(ha{ii})%#ok
+         end
+         top = @(x) interp1(xc, zt, x);
+         for i = [5 1 2 3 4]
+            axs = ha{i}; 
+            if(i<5)
+               xpos = xc(cells(i)); 
+               bpos = [xpos, top(xpos) + 25, 1 1]; 
+               pos5 = dsxy2figxy_new(axs, [0 25 1 1]); 
+               pos1 = dsxy2figxy_new(h1, bpos + [0 0 0 0]); 
+               annotation('arrow', [pos1(1) pos5(1)], [pos1(2) pos5(2)], 'LineWidth', 2)
+            else
+               axes(ha{i});%#ok
+               ax = axis();%#ok 
+               axis([0 1, 0, h_trap(cells(i)) * 5]); 
+            end
+            axes(ha{i});%#ok
+            axis off;
+         end         
+      end
+   end
+end
---
+++ co2lab/examples/publication_code/paper3/plotVEReconstruction_new.m
@@ -0,0 +1,165 @@
+function plotVEReconstruction_new(fig_num)
+
+   moduleCheck('co2lab','ad-fi','ad-props');
+   gravity on;
+
+   switch fig_num
+     case 6
+       plot_reconstruction(0, 0);
+     case 8
+       plot_reconstruction(0.0, 0.2);
+     otherwise
+       error('This function only generates plots for figure 6 and 8');
+   end
+end
+
+% ----------------------------------------------------------------------------
+
+function plot_reconstruction(srw, src)
+
+   T_ref     = 277 + 1000 * 30 / 1e3;
+   mu        = [6e-2 * milli 8e-4] * Pascal * second;
+   rho       = [760 1200] .* kilogram / meter^3;
+   plot_hyst = (src>0);
+
+   H = 25;
+   sample = 100;
+   G = cartGrid([sample 1 1], [100 100 H]);
+   G = computeGeometry(G);
+   Gt = topSurfaceGrid(G);
+   rock = struct('perm', 1000 * milli * darcy * ones(G.cells.num, 1), ...
+                 'poro', 0.4 * ones(G.cells.num, 1));
+   rock.parent = rock;
+
+   %% Defining fluid
+   fluidADI = initSimpleADIFluid('mu', [mu(2) mu(2) mu(1)],...
+                                 'rho', [rho(2) rho(2), rho(1)],...
+                                 'n', [1 1 1]);
+   wfields = {'krW', 'krO', 'krG', 'pcOG', 'pcOW'};
+   for i = 1:numel(wfields)
+      if(isfield(fluidADI, wfields{i}))
+         fluidADI = rmfield(fluidADI, wfields{i});
+      end
+   end
+   fluidADI.pvMultR = @(p) 1 + (1e-5 / barsa) * (p - 100 * barsa);
+   fluidADI.bW      = @(p) 1 + (4.3e-5 / barsa) * (p - 100 * barsa);
+   fluidADI.BW      = @(p) 1 ./ fluidADI.bW(p);
+   fluidADI.bG      = boCO2(T_ref, fluidADI.rhoGS);
+   fluidADI.BG      = @(p) 1 ./ fluidADI.bG(p);
+   fluidADI.bG      = @(p) 1 + 0 * p; fluidADI.BG = @(p)  1 + 0 * p;
+   fluidADI.bW      = @(p) 1 + 0 * p;fluidADI.BW  = @(p)  1 + 0*p;
+   fluidADI.rhoGS   = 600;
+   fluidADI.rhoWS   =1000;
+
+   fluidADI.surface_tension = 30e-3;
+   fluid_names = makeVEFluidsForTest(); % Get names for the fluids
+   fluid_names = {fluid_names{[2, 3, 4, 5, 6]}}; %#ok
+   ff_names = {'sharp interface', 'linear cap.', ...
+               'P - scaled table', 'P - K - scaled table', 'S table'};
+   leg = {};
+   fluid = {};
+   for i=1:numel(fluid_names)
+      fluid{end+1} = makeVEFluidsForTest(fluidADI    , fluid_names{i} , ...
+                                         'res_gas'   , 0              , ...
+                                         'res_water' , 0              , ...
+                                         'Gt'        , Gt             , ...
+                                         'rock'      , rock           , ...
+                                         'res_water' , srw            , ...
+                                         'res_gas'   , src);%#ok
+      fluid{i}.name = ff_names{i}; %#ok
+   end
+
+   %% Plotting figure
+
+   s = linspace(0, 1, sample)';
+   p = 100 * barsa * ones(size(s));
+   sco_max = 0.6 * ones(size(s));
+
+   krG = []; krGm = []; pcWG = []; pcWGm = [];
+   for i = 1:numel(fluid)
+      krG      = [krG, fluid{i}.krG(s, p, 'sGmax', s) + i * 1e-3]; %#ok
+      krGm     = [krGm, fluid{i}.krG(s, p, 'sGmax', sco_max)]; %#ok
+      pcWG     = [pcWG, fluid{i}.pcWG(s, p, 'sGmax', s)]; %#ok
+      pcWGm    = [pcWGm, fluid{i}.pcWG(s, p, 'sGmax', sco_max)]; %#ok
+      leg{i}   = fluid{i}.name;%#ok
+    end
+
+
+   figure(1), clf, hold on;
+   plot(s, krG, '-', 'LineWidth', 2);
+   legend(leg{:}, 'Location', 'NorthWest')
+   if(plot_hyst)
+      plot(s, krGm, '--', 'LineWidth', 2)
+   end
+   box on
+   set(gca, 'FontSize', 16)
+   set(gca, 'LineWidth', 2)
+
+   %%
+   dpH = 9.8 * 400 * H / barsa;
+   figure(2), clf, hold on;
+   plot(s, pcWG / barsa, '-', 'LineWidth', 2)
+   legend(leg{:}, 'Location', 'NorthWest')
+   if(plot_hyst)
+      plot(s, pcWGm / barsa, '--', 'LineWidth', 2)
+   end
+   box on
+   line([0 1], [dpH dpH], 'LineWidth', 4, 'Color', 'k')
+   axis([0 1 0 2 * dpH])
+   set(gca, 'FontSize', 16)
+   set(gca, 'LineWidth', 2)
+
+   %%
+   dpH = 9.8 * 400 * H;
+   figure(3), clf, hold on;
+   plot(s, pcWG / dpH, '-', 'LineWidth', 2)
+   legend(leg{:}, 'Location', 'NorthWest')
+   if(plot_hyst)
+      plot(s, pcWGm / dpH, '--', 'LineWidth', 2)
+   end
+   box on
+   line([0 1], [1 1], 'LineWidth', 4, 'Color', 'k')
+   axis([0 1 0 2])
+   set(gca, 'FontSize', 16)
+   set(gca, 'LineWidth', 2)
+
+   %%
+   sco_max = mean(sco_max);
+
+   %%
+   figure(4), clf, hold on
+   kscale = sqrt(rock.poro ./ (rock.perm)) * fluidADI.surface_tension;
+   if(plot_hyst)
+      h_int = 0.5 * sco_max * H;
+      h_int_max = sco_max * H;
+   else
+      h_int = sco_max * H;
+      h_int_max = sco_max * H;
+   end
+   leg = {};
+   ss = []; hh = []; krk = []; ss_max = []; hh_max = [];
+   for i = 1:numel(fluid)
+      [s, pc, kr, SH, krH, s_max, fval] = ...
+          veRelpermTester(h_int, p, fluid{i}, H, ...
+                          'samples' , 100       , ...
+                          'hs_max'  , h_int_max , ...
+                          'kscale'  , kscale); %#ok
+      ss = [ss, 1 - fval.s_h]; %#ok
+      ss_max = [ss_max, 1 - fval.s_hmax]; %#ok
+      hh_max = [hh_max, h_int_max - fval.h_max]; %#ok
+      krk = [krk, fval.kr_h]; %#ok
+      hh = [hh, h_int - fval.h]; %#ok
+      leg{i} = fluid{i}.name; %#ok
+   end
+   plot(ss, hh, 'LineWidth', 2);
+   if(plot_hyst)
+      plot(ss_max, hh_max, ' -- ', 'LineWidth', 2)
+   end
+   legend(leg{:}, 'Location', 'NorthWest')
+   set(gca, 'YDir', 'reverse')
+   axis([0 1.02, 0 H])
+   box on
+   set(gca, 'LineWidth', 2)
+   set(gca, 'FontSize', 16)
+
+end
