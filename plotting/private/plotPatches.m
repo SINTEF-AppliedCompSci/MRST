e261d28dfc4de5e4cee5f9a1755d20332e35d02b
Olav Møyner <olav.moyner@sintef.no>
2015-05-02 23:48:35 +0200

    PlotPatches: Use getSortedCellNodes utility instead of internal duplicated code. This restores fast 2D plotting functionality for CO2-lab.

--- plotting/private/plotPatches.m
+++ plotting/private/plotPatches.m
@@ -105,7 +105,8 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
    end
 
    if dim == 2,
-      [nodes, pos] = getCellNodes(G);
+      nodes = getSortedCellNodes(G);
+      pos   = G.cells.facePos;
    else
       nodes = G.faces.nodes;
       pos   = G.faces.nodePos;
@@ -215,57 +216,5 @@ function rgb = get_rgb(colour)
       case {'w', 'white'  }, rgb = [1, 1, 1];
       case {'k', 'black'  }, rgb = [0, 0, 0];
       otherwise            , rgb = [0, 0, 1]; % Unknown colour -> 'blue'.
-   end
-end
-
-%--------------------------------------------------------------------------
-% For 2d only.
-function [cn, pos] = getCellNodes(G)
-   % Construct n x 2 table of cell edges with edges oriented the same
-   % direction around the cell boundary. Return first column.
-   cellNo    = rldecode(1:G.cells.num, diff(G.cells.facePos), 2).';
-   edges     = reshape(G.faces.nodes, 2, [])';
-   cellEdges = edges(G.cells.faces(:,1),:);
-   ind       = G.faces.neighbors(G.cells.faces(:,1), 1) ~= cellNo;
-   cellEdges(ind, :) = cellEdges(ind, [2,1]);
-
-   % Sort edges in each cell:
-   for c = 1 : G.cells.num,
-      ind = G.cells.facePos(c) : G.cells.facePos(c + 1) - 1;
-      cellEdges(ind, :) = sortEdges(cellEdges(ind,:));
-   end
-   cn = reshape(cellEdges(:,1), 1, [])';
-   pos = G.cells.facePos;
-end
-
-%--------------------------------------------------------------------------
-% For 2d only.
-function edges = sortEdges(edges)
-   % Assume edges vectors are oriented in the same direction around cell.
-   % Sort edges such that they are back-to-back.
-   % Then cellNodes are edges(:,1).
-
-   for i = 1 : size(edges, 1) - 1,
-      for j = i + 1 : size(edges,1),
-
-         % Check if j follows edges(i)
-         if any(edges(i, 2) == edges(j, :), 2),
-            if edges(i,2) == edges(j,2), edges(j,:) = edges(j,[2,1]);end
-            % Add j to end of list by swapping edges i+1 and j
-            tmp = edges(i+1,:);
-            edges(i+1, :) = edges(j,:);
-            edges(j,   :) = tmp;
-            break
-         end
-
-         % Check if j precedes edges(1)
-         if any(edges(1, 1) == edges(j, :), 2),
-            if edges(1,1) == edges(j,1), edges(j,:) = edges(j,[2,1]);end
-            % Add j to front of list
-            edges = edges([j,1:j-1,j+1:end],:);
-            break
-         end
-
-      end
    end
 end
