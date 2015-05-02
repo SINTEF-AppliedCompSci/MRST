2393de4d1723234b7b57a4ca8aef853a6d90fb42
Sindre Hilden <sindre.k.hilden@gmail.com>
2015-05-02 13:45:24 +0200

    Cleanup shear multiplier function.
    Better documented.

--- ad-blackoil/utils/getPolymerShearMultiplier.m
+++ ad-blackoil/utils/getPolymerShearMultiplier.m
@@ -1,43 +1,49 @@
-function mult = getPolymerShearMultiplier(model, VW0, muWmultf)
-% Compute the velocity multiplier due to polymer shear thinning/thickening
-% 
+function [mult, VW] = getPolymerShearMultiplier(model, VW0, muWmult)
+% Compute the flux multiplier due to polymer shear thinning/thickening
+%
 % SYNOPSIS:
-%   mult = getPolymerShearMultiplier(model, vW0, muWmultf)
+%   mult      = getPolymerShearMultiplier(model, VW0, muWmult)
+%   [mult,VW] = getPolymerShearMultiplier(model, VW0, muWmult)
 %
-% PARAMETERS:
+% DESCRIPTION:
+%   The viscoisty of a polymer solution may be changed by the shear rate,
+%   which is related to the flow velocity. This function returns a flux
+%   multiplier mult, such that
+%     vWsh = vW.*mult,   and,   vPsh = vP.*mult,
+%   where vWsh and vPsh are the shear modified fluxes for pure water and
+%   water with polymer, respectively.
+%
+%   The input water face velocity is computed as
+%     VW = vW/(poro*area)
+%   where poro is the average porosity between neighboring cells, and area
+%   is the face area.
+%
+% REQUIRED PARAMETERS:
 %   model    - Model structure
-%   vW0      - Water velocity on faces without shear thinning (see below)
-%   muWmultf - Viscosity multiplier on faces
+%   VW0      - Water velocity on faces without shear thinning
+%   muWmult  - Viscosity multiplier on faces
 %
-% The viscoisty of a polymer solution may be changed by the shear rate,
-% which is related to the flow velocity. This function returns a velocity 
-% multiplier mult, such that
-%   vWsh = vW.*mult,   and,   vPsh = vP.*mult,
-% where vWsh and vPsh are the shear modified velocities of pure water and 
-% water with polymer, respectively.
-% 
-% The water given water face velocity is computed as
-%   VW = vW/(poro*area)
-% where poro is the average porosity between neighboring cells, and area is
-% the face area.
-% 
+% RETURNS:
+%   mult     - Flux multiplier (reciprocal of the viscosity multiplier)
+%   VW       - Water velocity as solution of nonlinear shear equation
+%
 
 f = model.fluid;
 
 % Compute the flow velocity
-VW0      = abs(double(VW0));
-muWmultf = double(muWmultf);
+VW0     = abs(double(VW0));
+muWmult = double(muWmult);
 
 % Setup equation for the shear modified velocity. This is an equation on
 % the faces of the domain.
-eqn = @(VW) VW .* (1 + f.plyshearMult(VW).*(muWmultf-1)) - VW0.*muWmultf;
+eqn = @(VW) VW .* (1 + f.plyshearMult(VW).*(muWmult-1)) - VW0.*muWmult;
 
 % Solve for shear modified velocity
 VW  = solveNonlinearEqn(eqn, VW0);
 
 % Compute velocity multiplier from solution
 % Note that this is the reciprocal of the viscosity multiplier
-mult = muWmultf ./ ( 1 + f.plyshearMult(VW).*(muWmultf-1) );
+mult = muWmult ./ ( 1 + f.plyshearMult(VW).*(muWmult-1) );
 
 end
 
@@ -48,6 +54,7 @@ end
 %--------------------------------------------------------------------------
 
 function x = solveNonlinearEqn(fun, x0)
+% Simple Newton solver to find the root of the nonlienar shear equation
 
 % Hardcoded settings
 maxit   = 30;
@@ -59,26 +66,27 @@ x    = x0;
 
 % Newton loop
 while iter <= maxit
-   
-   x   = initVariablesADI(x);
-   eqs = fun(x);
-   
-   resnorm = norm(double(eqs), 'inf');
-   if resnorm < abstol
-      break
-   end
-   
-   J   = -eqs.jac{1};
-   dx  = J\eqs.val;
-   x   = x.val + dx;
-   
-   iter = iter + 1;
-   
+    
+    x   = initVariablesADI(x);
+    eqs = fun(x);
+    
+    resnorm = norm(double(eqs), 'inf');
+    if resnorm < abstol
+        break
+    end
+    
+    J   = -eqs.jac{1};
+    dx  = J\eqs.val;
+    x   = x.val + dx;
+    
+    iter = iter + 1;
+    
 end
 
 if resnorm > abstol
-  error('Polymer shear convergence failure. Residual=%1.2e', ...
-     maxit, resnorm);
+    % Tolerence not met. Issue warning.
+    warning('Polymer shear convergence failure. Residual=%1.2e', ...
+        maxit, resnorm);
 end
 
 x = double(x);
