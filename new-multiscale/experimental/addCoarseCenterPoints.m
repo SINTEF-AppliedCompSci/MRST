c8d5ec2f9fd469754b5009e19d87502d33329e59
Olav Møyner <olav.moyner@sintef.no>
2015-05-04 16:03:38 +0200

    Allow passing of subindices for adjustment of centers near boundary.

--- new-multiscale/experimental/addCoarseCenterPoints.m
+++ new-multiscale/experimental/addCoarseCenterPoints.m
@@ -1,6 +1,7 @@
 function CG = addCoarseCenterPoints(CG, varargin)
     opt = struct('adjustCenters', true, ...
                  'centerOverride',    [], ...
+                 'adjustDims',        1:CG.parent.griddim, ...
                  'edgeBoundaryCenters', true);
     G = CG.parent;
     if opt.adjustCenters
@@ -19,12 +20,13 @@ function CG = addCoarseCenterPoints(CG, varargin)
             f = bf(i);
             c = sum(CG.faces.neighbors(f, :), 2);
             
-            cc = blockPts(c, :);
-            fc = CG.faces.centroids(f, :);
-            N = CG.faces.normals(f, :);
+            dims = opt.adjustDims;
+            cc = blockPts(c, dims);
+            fc = CG.faces.centroids(f, dims);
+            N = CG.faces.normals(f, dims);
             d = norm(cc - fc, 2);
             N = N./norm(N, 2);
-            blockPts(c, :) = cc - N*d;
+            blockPts(c, dims) = cc - N*d;
         end
     end
     
