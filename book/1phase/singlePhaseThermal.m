30e992b6dd38cfc668a688fced04911694e9c5fe
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2015-05-19 17:53:24 +0200

    Cleaned up 1-phase thermal example and renamed script

--- book/1phase/singlePhaseThermal.m
+++ book/1phase/singlePhaseThermal.m
@@ -1,3 +1,13 @@
+%% Single-phase compressible AD solver with thermal effects
+% The purpose of the example is to give demonstrate rapid prototying in
+% MRST using the automatic differentiation (AD) class. To this end, we
+% extend the <singlePhaseAD.m flow simulator for compressible single-phase
+% flow> to include temperature effects, which are modeled by incorporating
+% a second conservation equation for energy. Except for this, the
+% computational setup is the same with a single horizontal well draining
+% fluids from a simple box-geometry reservoir.
+
+
 %% Set up model: grid, perm and poro
 [nx,ny,nz] = deal( 10,  10, 10);
 [Dx,Dy,Dz] = deal(200, 200, 50);
@@ -8,31 +18,30 @@ G = computeGeometry(G);
 rock.perm = repmat(30*milli*darcy, [G.cells.num, 1]);
 rock.poro = repmat(0.3, [G.cells.num, 1]);
 cr   = 1e-6/barsa;
-pref = 200*barsa;
-pvr  = poreVolume(G, rock);
-pv   = @(p) pvr .* exp( cr * (p - pref) );
-spv  = @(p) G.cells.volumes - pv(p);
+p_r  = 200*barsa;
+pv_r = poreVolume(G, rock);
+pv   = @(p) pv_r .* exp( cr * (p - p_r) );
+sv   = @(p) G.cells.volumes - pv(p);
 
 %% Define model for compressible fluid
-mu   = 5*centi*poise;
-cf   = 1e-3/barsa;
-ch   = 1*1e-4;    % nloop*1e-4;
-rhor = 850*kilogram/meter^3;
-rhoS = 750*kilogram/meter^3;
-Tref = 300;
-rho  = @(p,T) rhor .* (1+(cf*(p-pref))).*exp(-ch*(T-Tref) );
+mu0   = 5*centi*poise;
+cmup  = 2e-3/barsa;
+cmut  = 1e-3;
+T_r   = 300;
+mu    = @(p,T)  mu0*(1+cmup*(p-p_r)).*exp(-cmut*(T-T_r));
 
+cf    = 1e-3/barsa;
+ch    = 1*1e-4;    % nloop*1e-4;
+rho_r = 850*kilogram/meter^3;
+rho_S = 750*kilogram/meter^3;
+rho   = @(p,T) rho_r .* (1+(cf*(p-p_r))).*exp(-ch*(T-T_r) );
+
+%% Quantities for energy equation
 Cw = 1e3;
 Cr = Cw/2;
-Hf = @(p,T) Cr*T+(1-Tref*ch).*(p-pref)./rho(p,T);
+Hf = @(p,T) Cr*T+(1-T_r*ch).*(p-p_r)./rho(p,T);
 Ef = @(p,T) Hf(p,T) - p./rho(p,T);
 Er = @(T)   Cr*T;
-
-% Plot density as function of pressure and temperature
-% [p,T]=meshgrid(linspace(100*barsa,200*barsa,50),linspace(290,310,50));
-% mesh(p/barsa,T,rho(p,T));
-% xlabel('pressure'), ylabel('Temperature'),zlabel('density')
-
 
 %% Assume a single horizontal well
 nperf = floor(G.cartDims(2)*(ny-2)/ny);
@@ -45,9 +54,9 @@ W = addWell([ ], G, rock, cellInx, 'Name', 'P1', 'Dir', 'y');
 %% Impose vertical equilibrium
 gravity reset on; g = norm(gravity);
 [z_0, z_max] = deal(0, max(G.cells.centroids(:,3)));
-equil  = ode23(@(z,p) g.* rho(p,Tref), [z_0, z_max], pref);
+equil  = ode23(@(z,p) g.* rho(p,T_r), [z_0, z_max], p_r);
 p_init = reshape(deval(equil, G.cells.centroids(:,3)), [], 1);  clear equil
-T_init = ones(G.cells.num,1)*Tref;
+T_init = ones(G.cells.num,1)*T_r;
 
 %% Plot well and initial pressure
 figure
@@ -66,16 +75,17 @@ N   = double(G.faces.neighbors);
 intInx = all(N ~= 0, 2);
 cf  = G.cells.faces(:,1);
 nf  = G.faces.num;
-N   = N(intInx, :);                          % Interior neighbors
+N   = N(intInx, :);                           % Interior neighbors
 
-hT  = computeTrans(G, rock);                 % Half-transmissibilities
-Tr  = 1 ./ accumarray(cf, 1 ./ hT, [nf, 1]); % Harmonic average
-Tr  = Tr(intInx);                            % Restricted to interior
+hT  = computeTrans(G, rock);                  % Half-transmissibilities
+Tp  = 1 ./ accumarray(cf, 1 ./ hT, [nf, 1]);  % Harmonic average
+Tp  = Tp(intInx);                             % Restricted to interior
 
-tmp = struct('perm', 4*ones(G.cells.num,1)); % heat conduction of granit
-hT  = computeTrans(G, tmp);                  % Half-transmissibilities
-Th  = 1 ./ accumarray(cf, 1 ./ hT, [nf, 1]); % Harmonic average
-Th  = Th(intInx);                            % Restricted to interior
+kap = 4;                                      % Heat conduction of granite
+tmp = struct('perm',kap*ones(G.cells.num,1)); % Temporary rock object
+hT  = computeTrans(G, tmp);                   % Half-transmissibilities
+Th  = 1 ./ accumarray(cf, 1 ./ hT, [nf, 1]);  % Harmonic average
+Th  = Th(intInx);                             % Restricted to interior
 
 %% Define discrete operators
 n    = size(N,1);
@@ -86,29 +96,17 @@ avg  = @(x) 0.5 * (x(N(:,1)) + x(N(:,2)));
 upw  = @(x,flag) x(N(:,1)).*double(flag)+x(N(:,2)).*double(~flag);
 
 %% Define flow equations
-if ~exist('testcase','var')
-   testcase=2;
-end
-gdz = grad(G.cells.centroids)*gravity()';
-switch testcase
-   case 1,
-      v  = @(p,T)  -(Tr/mu).*( grad(p) - avg(rho(p,T)).*gdz );
-   case 2,
-      [mu0, cmup, cmut]= deal(mu,2e-3/barsa,1e-3);
-      mu = @(p,T)  mu0*(1+cmup*(p-pref)).*exp(-cmut*(T-Tref));
-      v  = @(p,T)  -(Tr./mu(avg(p),avg(T))).*(grad(p) - avg(rho(p,T)).*gdz);
-end
-
-%% Define flow equations
 % Writing in functional form means that v(p,T) is evaluated two
 % times more than strictly needed if the whole definition of
 % discrete equations written as a single function
-presEq = @(p,T, p0,T0, dt) ...
+gdz = grad(G.cells.centroids)*gravity()';
+v   = @(p,T)  -(Tp./mu(avg(p),avg(T))).*(grad(p) - avg(rho(p,T)).*gdz);
+pEq = @(p,T, p0,T0, dt) ...
      (1/dt)*(pv(p).*rho(p,T) - pv(p0).*rho(p0,T0)) ...
       + div( avg(rho(p,T)).*v(p,T) );
-heatEq = @(p, T, p0, T0, dt) ...
-     (1/dt)*(pv(p ).*rho(p, T ).*Ef(p ,T ) + spv(p ).*Er(T ) ...
-           - pv(p0).*rho(p0,T0).*Ef(p0,T0) - spv(p0).*Er(T0)) ...
+hEq = @(p, T, p0, T0, dt) ...
+     (1/dt)*(pv(p ).*rho(p, T ).*Ef(p ,T ) + sv(p ).*Er(T ) ...
+           - pv(p0).*rho(p0,T0).*Ef(p0,T0) - sv(p0).*Er(T0)) ...
      + div( upw(Hf(p,T),v(p,T)>0).*avg(rho(p,T)).*v(p,T) ) ...
      + div( -Th.*grad(T));
 
@@ -123,7 +121,7 @@ p_conn  = @(bhp,bhT) bhp + g*dz.*rho(bhp,bhT); %connection pressures
 q_conn  = @(p,T, bhp) ...
     WI .* (rho(p(wc),T(wc))./mu(p(wc),T(wc))) .* (p_conn(bhp,bhT)-p(wc));
 
-rateEq = @(p,T, bhp, qS)  qS-sum(q_conn(p, T, bhp))/rhoS;
+rateEq = @(p,T, bhp, qS)  qS-sum(q_conn(p, T, bhp))/rho_S;
 ctrlEq = @(bhp)           bhp-100*barsa;
 
 %% Initialize for solution loop
@@ -148,8 +146,8 @@ t = 0; step = 0;
 while t < totTime,
    t = t + dt;
    step = step + 1;
-   fprintf('\nTime step %d: Time %.2f -> %.2f days\n', ...
-      step, convertTo(t - dt, day), convertTo(t, day));
+%   fprintf('\nTime step %d: Time %.2f -> %.2f days\n', ...
+%      step, convertTo(t - dt, day), convertTo(t, day));
 
    % Newton loop
    resNorm = 1e99;
@@ -159,13 +157,13 @@ while t < totTime,
    while (resNorm > tol) && (nit < maxits)
 
       % Add source terms to homogeneous pressure equation:
-      eq1      = presEq(p_ad,T_ad, p0, T0,dt);
+      eq1      = pEq(p_ad,T_ad, p0, T0,dt);
       qw       = q_conn(p_ad,T_ad, bhp_ad);
       eq1(wc)  = eq1(wc) - qw;
-      hq       = Hf(bhp_ad,bhT).*qw; %in flow not in this example
+      hq       = Hf(bhp_ad,bhT).*qw;    %inflow not in this example
       Hcells   = Hf(p_ad,T_ad);
       hq(qw<0) = Hcells(wc(qw<0)).*qw(qw<0);
-      eq2      = heatEq(p_ad,T_ad, p0, T0,dt);
+      eq2      = hEq(p_ad,T_ad, p0, T0,dt);
       eq2(wc)  = eq2(wc) - hq;
 
       % Collect all equations
@@ -185,7 +183,7 @@ while t < totTime,
 
       resNorm = norm(res);
       nit     = nit + 1;
-      fprintf('  Iteration %3d:  Res = %.4e\n', nit, resNorm);
+ %     fprintf('  Iteration %3d:  Res = %.4e\n', nit, resNorm);
    end
 
    if nit > maxits,
@@ -196,7 +194,7 @@ while t < totTime,
 			    'T', double(T_ad), 'qH', sum(double(hq)));
    end
 end
-
+%{
 %% Plot production rate
 clf, set(gca,'FontSize',20);
 stairs([sol(2:end).time]/day,-[sol(2:end).qS]*day,'LineWidth',2);
@@ -217,13 +215,13 @@ for i=1:4
    axis tight off; zoom(1.4)
    text(200,170,-8,[num2str(round(steps(i)*dt/day)) ' days'],'FontSize',12);
 end
-h=colorbar('horiz','Position',[.1 .05 .8 .025]);
+colorbar('horiz','Position',[.1 .05 .8 .025]);
 colormap(jet(55));
 
 %%
 figure(),clf;
 steps = [2 5 10 numel(sol)];
-T = vertcat(sol(:).T); %-Tref;
+T = vertcat(sol(:).T); %-T_r;
 cax=[min(T) max(T)];
 for i=1:numel(steps)
    subplot(2,2,i);
@@ -260,6 +258,7 @@ legend('min(T)', 'max(T)', 'avg(T)', 'wells');
 %subplot(2,5,nloop); plot(t,Tm,t,TM,t,Ta,'LineWidth',2);
 %title(['ct=', num2str(nloop) 'e-4']);
 %axis tight; drawnow
+%}
 
 %{
 Copyright 2009-2015 SINTEF ICT, Applied Mathematics.
