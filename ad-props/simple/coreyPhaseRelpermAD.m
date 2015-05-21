6f72c6eedfbe3d4b98a63882c91eeecdeee48e37
Olav Møyner <olav.moyner@sintef.no>
2015-05-21 12:20:20 +0200

    Adding a few simple routines for making fluid functions for AD solvers (simple tabulated pressure dependent stuff, corey-type relperms with sr/kwm)

---
+++ ad-props/simple/coreyPhaseRelpermAD.m
@@ -0,0 +1,36 @@
+function fn = coreyPhaseRelpermAD(n, sr, kwm, sr_tot)
+% Make function handle for AD style fluids
+
+    % n - exponent
+    % sr - residual saturation
+    % kwm - endpoint relperm
+    % sr_tot - sum of sr for all phases present
+    
+    if nargin < 1
+        n = 1;
+    end
+    if nargin < 2
+        sr = 0;
+    end
+    if nargin < 3
+        kwm = 1;
+    end
+    if nargin < 4
+        sr_tot = sr;
+    end
+%     den = 1 - sr_tot;
+%     fn = @(s) kwm*max(min(((s - sr)./den), 1), 0).^n;
+    fn = @(s) coreyRelperm(s, n, sr, kwm, sr_tot);
+end
+
+function kr = coreyRelperm(s, n, sr, kwm, sr_tot)
+    den = 1 - sr_tot;
+    sat = ((s - sr)./den);
+    if isa(sat, 'ADI')
+        sat.val = max(min(sat.val, 1), 0);
+    else
+        sat = min(max(sat, 1), 0);
+    end
+    
+    kr = kwm*sat.^n;
+end
