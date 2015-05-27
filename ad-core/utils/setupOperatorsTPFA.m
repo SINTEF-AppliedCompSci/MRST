5b0da2ad1ac67f5ddacc04a048a023a9d7fab05e
Olav Møyner <olav.moyner@sintef.no>
2015-05-27 12:55:12 +0200

    Avoid constructing a sparse matrix for upstream weighting when input is a regular double matrix. This greatly speeds up the routine for larger cases (i.e. 100k+ cells).

--- ad-core/utils/setupOperatorsTPFA.m
+++ ad-core/utils/setupOperatorsTPFA.m
@@ -177,7 +177,13 @@ function xu = faceUpstr(flag, x, N, sz)
         'One logical upstream flag must'' be supplied per interface.');
     upCell       = N(:,2);
     upCell(flag) = N(flag,1);
-    xu = sparse((1:sz(1))', upCell, 1, sz(1), sz(2))*x;
+    if isnumeric(x)
+        % x is a simple matrix, we just extract values using the cells
+        xu = x(upCell, :);
+    else
+        % x is likely AD, construct a matrix to achieve upstream weighting
+        xu = sparse((1:sz(1))', upCell, 1, sz(1), sz(2))*x;
+    end
 end
 
 
