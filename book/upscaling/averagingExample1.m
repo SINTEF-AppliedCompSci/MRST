f783c3f937d5a8b021f5cd19586a7355eec418dd
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2015-06-02 23:05:34 +0200

    First full draft of example

--- book/upscaling/averagingExample1.m
+++ book/upscaling/averagingExample1.m
@@ -1,46 +1,80 @@
-%% Arithmetic-harmonic averaging
-% To illustrate the implementation of this type of averaging, we consider a
-% single layer of the SPE10 model
-fine = [45 60];
+%% Averaging of SPE10 layer
+% To illustrate the implementation of various types of averaging
+% techniques, we consider a single layer of the SPE10 model and compare
+% the effective permeabilities obtained by arithmetic, harmonic, and
+% harmonic-arithmetic averaging.
+fine = [40 60];
 G    = cartGrid(fine, fine);
 G    = computeGeometry(G);
 rock = SPE10_rock(1:fine(1),1:fine(2),46);
 rock.perm = convertFrom(rock.perm(:,1:2), milli*darcy);
 
 % The coarse grid
-coarse = [15 15];
+coarse = [10 15];
 q = partitionUI(G,coarse);
-CG = generateCoarseGrid(G, q);
+cG = cartGrid(coarse,fine);
 
-%%
-%
+%% Compute the averages
+% The arithmetic and harmonic averages are straightforward. To compute the
+% harmonic-arithmetic average, we  temporary partition that coincides with
+% the coarse grid in along the given axial direction and with the original
+% fine grid in the other directions. Then we map the averaged values back
+% onto the fine grid and perform a standard arithmetic averaging.
+clear crock*
 vol = G.cells.volumes;
 for i=1:size(rock.perm,2)
    crock1.perm(:,i) = accumarray(q,vol.*rock.perm(:,i))./accumarray(q,vol);
    
    crock2.perm(:,i) = accumarray(q,vol)./accumarray(q,vol./rock.perm(:,i));
 
-   dims = fine; dims(i)=coarse(i);
+   dims = G.cartDims; dims(i)=coarse(i);
    qq = partitionUI(G, dims);
    K = accumarray(qq,vol)./accumarray(qq,vol./rock.perm(:,i));
    crock3.perm(:,i) = accumarray(q,K(qq).*vol)./accumarray(q,vol);
 end
 
-%%
+%% Plot the resulting effective permeabilities
+% To improve the visualization we enhance the colorbar by adding a small
+% histogram of permeability values
 px = log10([min(rock.perm(:,1)) max(rock.perm(:,1))]);
-clf
+clf, set(gcf,'Position',[500 400 880 410]);
 subplot(1,4,1); 
 plotCellData(G,log10(rock.perm(:,2)),'EdgeColor','none');
-plotGrid(CG,'FaceColor','none','EdgeColor','k'); caxis(px); axis tight off
+plotGrid(cG,'FaceColor','none','EdgeColor','k'); caxis(px); axis tight off
+title('Fine scale');
 
 subplot(1,4,2);
-plotCellData(G,log10(crock1.perm(q,2)),'EdgeColor','none');
-plotGrid(CG,'FaceColor','none','EdgeColor','k'); caxis(px); axis tight off
+plotCellData(cG,log10(crock1.perm(:,2)),'EdgeColor','k');
+caxis(px); axis tight off
+title('Arithmetic');
 
 subplot(1,4,3);
-plotCellData(G,log10(crock2.perm(q,2)),'EdgeColor','none');
-plotGrid(CG,'FaceColor','none','EdgeColor','k'); caxis(px); axis tight off
+plotCellData(cG,log10(crock2.perm(:,2)),'EdgeColor','k');
+caxis(px); axis tight off
+title('Harmonic');
 
 subplot(1,4,4);
-plotCellData(G,log10(crock3.perm(q,2)),'EdgeColor','none');
-plotGrid(CG,'FaceColor','none','EdgeColor','k'); caxis(px); axis tight off
+plotCellData(cG,log10(crock3.perm(:,2)),'EdgeColor','k');
+caxis(px); axis tight off
+title('Arithmetic-harmonic');
+
+perm{1} = log10(rock.perm(:,2));
+perm{2} = log10(crock1.perm(:,2));
+perm{3} = log10(crock2.perm(:,2));
+perm{4} = log10(crock3.perm(:,2));
+
+for i=1:4
+   subplot(1,4,i); ha = gca;
+   h=colorbar('SouthOutside');
+   apos = get(ha,'Position');
+   pos=get(h,'Position'); set(h,'Position',pos - [0 .09 0 .03]);
+   set(h,'XTick',-16:2:-12,'XTickLabel',{'.1', '10', '1000'});
+   
+   set(ha,'Position',apos);
+   axes('Position',pos + [0 -.03 0 .03]);
+   hist(perm{i}, linspace(-18,-10,51));
+   axis tight off, set(gca,'XLim',[-18 -10]);
+   h = findobj(gca,'Type','patch');
+   set(h,'FaceColor','none','EdgeColor',[.1 .1 .1]);
+   set(gcf, 'CurrentAxes', ha)
+end
