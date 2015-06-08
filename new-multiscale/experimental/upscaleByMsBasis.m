38c95506822ecfb86bea64b9800ec54e8a999296
Olav Møyner <olav.moyner@sintef.no>
2015-06-08 14:58:31 +0200

    Utility to extract (approximate) transmissibilities from MS basis.

---
+++ new-multiscale/experimental/upscaleByMsBasis.m
@@ -0,0 +1,24 @@
+function Tc = upscaleByMsBasis(CG, T, varargin)
+    G = CG.parent;
+    A = getIncomp1PhMatrix(G, T);
+    basis = getMultiscaleBasis(CG, A, varargin{:});
+    
+    A_c = basis.R*A*basis.B;
+    
+    [ic, jc, vc] = find(A_c);
+    Tc = zeros(CG.faces.num, 1);
+
+    for i = 1:CG.cells.num
+        fa = gridCellFaces(CG, i);
+        for j = 1:numel(fa)
+            f = fa(j);
+            
+            c = CG.faces.neighbors(f, :);
+            c = c(c~=i);
+            if c == 0
+                continue
+            end
+            Tc(f) = abs(vc(ic == i & jc == c));
+        end
+    end
+end
