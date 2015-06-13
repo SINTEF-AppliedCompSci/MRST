b6d6a0cac38d46e5fbb006610c5e653ce14d58b3
Olav Møyner <olav.moyner@sintef.no>
2015-06-13 15:24:51 +0200

    Adding utility for plotting well rates (WOG) + bhp in a single plot for a given well.

---
+++ ad-core/utils/terniaryWellPlot.m
@@ -0,0 +1,57 @@
+function terniaryWellPlot(wellSols, T, ix, varargin)
+    opt = struct('plotBHP', true);
+    opt = merge_options(opt, varargin{:});
+    
+    ws0 = wellSols{1};
+    if nargin < 2
+        T = 1:numel(wellSols);
+        xname = 'Step #';
+    else
+        T = T/day;
+        xname = 'Time [day]';
+    end
+    if nargin < 3
+        ix = 1:numel(ws0);
+    end
+    flds = {'bhp', 'qWs', 'qOs', 'qGs'};
+     [data, names] = getWellOutput(wellSols, flds, ix);
+    for i = 1:numel(ix)
+        figure('Color', 'w');
+        hold on
+        qw = squeeze(data(:, i, 2:4));
+        
+        c = 'brg';
+        for j = 1:3
+            d = abs(qw(:, j))*day;
+            plot(T, d, [c(j), '--'], 'linewidth', 2)
+        end
+        ax = gca;
+        set(ax, 'Color','none');
+        xlabel(xname);
+        legend('Water', 'Oil', 'Gas', 'Location', 'NorthWest');
+        ylabel('Rates [m^3/day]');
+        if opt.plotBHP
+            axpos = get(ax, 'Position');
+            ax2 = axes('Position',axpos,...
+                        'XAxisLocation','top',...
+                        'YAxisLocation','right',...
+                        'Color','none');
+            hold on
+            plot(T, data(:, i, 1)/mega, 'k--', 'Parent', ax2, 'linewidth', 2);
+            set(ax2, 'XTickLabel', []);
+            ylabel('Bottom hole pressure [MPa]');
+            legend('BHP', 'Location', 'NorthEast');
+            axes(ax);
+        end
+        grid on
+        
+        
+        % Title
+        if ws0(i).sign == -1
+            title(['Production data for ', names{i}]);
+        else
+            title(['Injection data for ', names{i}]);
+        end
+    end
+    
+end
