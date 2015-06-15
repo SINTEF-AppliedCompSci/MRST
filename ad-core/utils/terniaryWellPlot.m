0f7b344db4e73557c1f6fee69faac5164234c505
Stein Krogstad <stein.krogstad@sintef.no>
2015-06-16 00:09:57 +0200

    Use years rather than days on x-axis to match diagnostics plots. Use 1000m^3/day unit for gas. Fix title.

--- ad-core/utils/terniaryWellPlot.m
+++ ad-core/utils/terniaryWellPlot.m
@@ -2,13 +2,13 @@ function terniaryWellPlot(wellSols, T, ix, varargin)
     opt = struct('plotBHP', true);
     opt = merge_options(opt, varargin{:});
     
-    ws0 = wellSols{1};
+    ws0 = wellSols{end};
     if nargin < 2
         T = 1:numel(wellSols);
         xname = 'Step #';
     else
-        T = T/day;
-        xname = 'Time [day]';
+        T = T/year;
+        xname = 'Time [year]';
     end
     if nargin < 3
         ix = 1:numel(ws0);
@@ -21,15 +21,17 @@ function terniaryWellPlot(wellSols, T, ix, varargin)
         qw = squeeze(data(:, i, 2:4));
         
         c = 'brg';
+        fac = 1;
         for j = 1:3
+            if j == 3, fac = 1000; end
             d = abs(qw(:, j))*day;
-            plot(T, d, [c(j), '--'], 'linewidth', 2)
+            plot(T, d/fac, [c(j), '-'], 'linewidth', 2)
         end
         ax = gca;
         set(ax, 'Color','none');
         xlabel(xname);
         legend('Water', 'Oil', 'Gas', 'Location', 'NorthWest');
-        ylabel('Rates [m^3/day]');
+        ylabel('Rates [m^3/day] (liquid),  [1000m^3/day] (gas)');
         if opt.plotBHP
             axpos = get(ax, 'Position');
             ax2 = axes('Position',axpos,...
@@ -37,7 +39,7 @@ function terniaryWellPlot(wellSols, T, ix, varargin)
                         'YAxisLocation','right',...
                         'Color','none');
             hold on
-            plot(T, data(:, i, 1)/mega, 'k--', 'Parent', ax2, 'linewidth', 2);
+            plot(T, data(:, i, 1)/mega, 'k-', 'Parent', ax2, 'linewidth', 2);
             set(ax2, 'XTickLabel', []);
             ylabel('Bottom hole pressure [MPa]');
             legend('BHP', 'Location', 'NorthEast');
@@ -47,7 +49,7 @@ function terniaryWellPlot(wellSols, T, ix, varargin)
         
         
         % Title
-        if ws0(i).sign == -1
+        if ws0(ix).sign == -1
             title(['Production data for ', names{i}]);
         else
             title(['Injection data for ', names{i}]);
