34d8b4f42a5d57bb170d6401e04d7df586b6322a
kalie <Knut-Andreas.Lie@sintef.no>
2015-06-19 15:37:27 +0200

    Multiple changes: no outline if second model is empty, use area plot rather than bars if more than 20 completions, structured code differently

--- diagnostics/utils/plotWellAllocationComparison.m
+++ diagnostics/utils/plotWellAllocationComparison.m
@@ -49,14 +49,12 @@ You should have received a copy of the GNU General Public License
 along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
 opt = struct('useZ', true,...
-             'plotOnly', NaN);
+             'plotOnly', NaN, ...
+             'bwidth', .98);
 opt = merge_options(opt, varargin{:});
 
-if (isempty(D2) || isempty(WP2))
-    D2 = D1;
-    WP2 = WP1;
-end
-
+% -------------------------------------------------------------------------
+% check what we should plot
 if (isnan(opt.plotOnly))
     num_wells = numel(WP1.inj) + numel(WP1.prod);
     opt.plotOnly = 1:num_wells;
@@ -66,28 +64,42 @@ else
         return;
     end
 end
+if (isempty(D2) || isempty(WP2))
+    plotFlag = false;
+else
+    plotFlag = true;
+end
 
+% -------------------------------------------------------------------------
+% Parameterization along well path
+% Create fake z values based on the cell numbering in the well. Useful
+% whenever the well isn't vertical.
 WP1 = addDummyData(WP1);
-WP2 = addDummyData(WP2);
 if ~opt.useZ
-    % Create fake z values based on the cell numbering in the well. Useful
-    % whenever the well isn't vertical.
     for i = 1:numel(WP1.inj)
         n = numel(WP1.inj(i).z);
         WP1.inj(i).z = (1:n)./n;
-
-        n = numel(WP2.inj(i).z);
-        WP2.inj(i).z = (1:n)./n;
     end
-
     for i = 1:numel(WP1.prod)
         n = numel(WP1.prod(i).z);
         WP1.prod(i).z = (1:n)./n;
-
-        n = numel(WP2.prod(i).z);
-        WP2.prod(i).z = (1:n)./n;
     end
 end
+if plotFlag
+    WP2 = addDummyData(WP2);
+    if ~opt.useZ
+        for i = 1:numel(WP1.inj)
+            n = numel(WP2.inj(i).z);
+            WP2.inj(i).z = (1:n)./n;
+        end
+        for i = 1:numel(WP1.prod)
+            n = numel(WP2.prod(i).z);
+            WP2.prod(i).z = (1:n)./n;
+        end
+    end
+end
+
+% -------------------------------------------------------------------------
 % Extract and format the well-allocation factors for model 1
 nit = numel(D1.inj);
 npt = numel(D1.prod);
@@ -106,28 +118,32 @@ end
 wp1(D1.inj)  = WP1.inj(:);
 wp1(D1.prod) = WP1.prod(:);
 
+% -------------------------------------------------------------------------
 % Extract and format the well-allocation factors for model 2
-nit = numel(D2.inj);
-npt = numel(D2.prod);
-for i=1:numel(WP2.inj)
-   nc   = numel(WP2.inj(i).z);
-   atmp = zeros(nc,nit+npt);
-   atmp(:,D2.prod) = WP2.inj(i).alloc;
-   WP2.inj(i).alloc  = atmp;
+if plotFlag
+    nit = numel(D2.inj);
+    npt = numel(D2.prod);
+    for i=1:numel(WP2.inj)
+        nc   = numel(WP2.inj(i).z);
+        atmp = zeros(nc,nit+npt);
+        atmp(:,D2.prod) = WP2.inj(i).alloc;
+        WP2.inj(i).alloc  = atmp;
+    end
+    for i=1:numel(WP2.prod)
+        nc   = numel(WP2.prod(i).z);
+        atmp = zeros(nc,nit+npt);
+        atmp(:,D2.inj) = WP2.prod(i).alloc;
+        WP2.prod(i).alloc  = atmp;
+    end
+    wp2(D2.inj)  = WP2.inj(:);
+    wp2(D2.prod) = WP2.prod(:);
 end
-for i=1:numel(WP2.prod)
-   nc   = numel(WP2.prod(i).z);
-   atmp = zeros(nc,nit+npt);
-   atmp(:,D2.inj) = WP2.prod(i).alloc;
-   WP2.prod(i).alloc  = atmp;
-end
-wp2(D2.inj)  = WP2.inj(:);
-wp2(D2.prod) = WP2.prod(:);
 
-%Calculate number of plots in 2D grid
+% -------------------------------------------------------------------------
+% Calculate number of plots in 2D grid
 num_plots = numel(opt.plotOnly);
 if (numel(opt.plotOnly) < numel(wp1))
-    clf(); %Since we most probably may change number of subplots, clear figure...
+    clf();
     num_plots = num_plots + 1;
 end
 nx = ceil(sqrt(num_plots));
@@ -140,39 +156,67 @@ current_fig = gcf();
 wb = waitbar(0, 'Well 1');
 set(0, 'CurrentFigure', current_fig);
 
+% -------------------------------------------------------------------------
 % Plot the well-allocation factors for fine/coarse scale models
 for j=1:numel(opt.plotOnly)
    subplot(nx,ny,j);
    i = opt.plotOnly(j);
    
+   % --------------------------------
+   % Plot data for model 1
    [~,ix]   = sort(wp1(i).z);
    wp1(i).z = wp1(i).z(ix);
    wp1(i).alloc = wp1(i).alloc(ix,:);
-   if numel(wp1(i).z) == 1  % Need to trick Matlab
+   nseg = numel(wp1(i).z);
+   if nseg == 1  % Need to trick Matlab
       z = [wp1(i).z; wp1(i).z+1];
       a = [cumsum(wp1(i).alloc,1); zeros(1,numel(wp1(i).alloc))];
       bwidth = 2*numel(wp2(i).z)+3;
+      h = barh(z, a,'stacked','BarWidth',bwidth, 'EdgeColor','none');
    else
       z = flipud(wp1(i).z);
       a = cumsum(flipud(wp1(i).alloc),1);
-      bwidth=.98;
+      if nseg<21
+          aargs = {'YDir', 'YLim'};
+          h = barh(z, a,'stacked','BarWidth',opt.bwidth, 'EdgeColor','none');
+      else
+          h = area(z, a, eps,'EdgeColor','none'); axis tight;
+          view(90,-90);
+          aargs = {'XDir', 'XLim'};
+      end
    end
-   h = barh(z, a,'stacked','BarWidth',bwidth, 'EdgeColor','none');
-   hold on
-   if numel(wp2(i).z) == 1  % Need to trick Matlab
-      z = [wp2(i).z; wp2(i).z+1];
-      a = [cumsum(wp2(i).alloc,1); zeros(1,numel(wp2(i).alloc))];
-   else
-      z = flipud(wp2(i).z);
-      a = cumsum(flipud(wp2(i).alloc),1);
-   end
-   %barh(z,a,'stacked','FaceColor','none','EdgeColor','k','LineWidth',2);
-   barh(z,a,'stacked','FaceColor','none');
-   hold off
-   zm = min(min(wp1(i).z),min(wp2(i).z));
-   zM = max(max(wp1(i).z),max(wp2(i).z));
+   zm = min(wp1(i).z);
+   zM = max(wp1(i).z);
    
-   set(gca,'YDir','reverse', 'YLim', [zm zM + sqrt(eps)] + [-.2 .2]*(zM-zm));
+   % --------------------------------
+   % Plot data for model 2
+   if plotFlag
+       hold on
+       nseg = numel(wp2(i).z);
+       if nseg == 1  % Need to trick Matlab
+           z = [wp2(i).z; wp2(i).z+1];
+           a = [cumsum(wp2(i).alloc,1); zeros(1,numel(wp2(i).alloc))];
+           barh(z,a,'stacked','FaceColor','none','BarWidth',opt.bwidth);
+       else
+           z = flipud(wp2(i).z);
+           a = cumsum(flipud(wp2(i).alloc),1);
+           if nseg<21
+               aargs = {'YDir', 'YLim'};
+               barh(z,a,'stacked','FaceColor','none','BarWidth',opt.bwidth);
+           else
+               h = area(z, a, eps,'FaceColor','none'); axis tight;
+               view(90,-90);
+               aargs = {'XDir', 'XLim'};
+           end
+       end
+       hold off
+       zm = min(zm,min(wp2(i).z));
+       zM = max(zM,max(wp2(i).z));
+   end
+   
+   % ---------------------------------
+   % Set axis and add legend
+   set(gca,aargs{1},'reverse', aargs{2}, [zm zM + sqrt(eps)] + [-.1 .1]*(zM-zm));
 
    if max(wp1(i).alloc(:))>0
       hl=legend(h(i),wp1(i).name,4); set(hl,'FontSize',8); legend boxoff
@@ -182,10 +226,10 @@ for j=1:numel(opt.plotOnly)
    waitbar(j/numel(opt.plotOnly),wb,['Well ', num2str(i+1)])
 end
 
-
-%Final plot contains all non-plotted colors in a legend
+% -------------------------------------------------------------------------
+% Show legend for wells that are not plotted
 if (numel(opt.plotOnly) < numel(wp1))
-    subplot(nx,ny,numel(opt.plotOnly)+1);
+    subplot(nx,ny,max(numel(opt.plotOnly)+1,(nx-1)*ny+1):nx*ny);
     cla('reset');
     hold on;
     axis off;
@@ -212,13 +256,17 @@ if (numel(opt.plotOnly) < numel(wp1))
     set(hl,'FontSize',8);
 end
 
-%Set colormap
+% -------------------------------------------------------------------------
+% Set colormap
 cmap = jet(nit+npt);
 c = 0.9*cmap + .1*ones(size(cmap)); colormap(c);
 drawnow;
 delete(wb);
 
 end
+
+
+% =========================================================================
 
 function WP = addDummyData(WP)
     ni = numel(WP.inj);
