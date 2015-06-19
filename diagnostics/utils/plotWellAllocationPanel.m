42b98c592e830266ebc3b9d500311176abc7c3fb
kalie <Knut-Andreas.Lie@sintef.no>
2015-06-19 18:31:34 +0200

    Function for plotting many well allocation factors. Not yet finished, will add callback that enables user to click on axis and enlarge one of the plots in a new figure with axes etc properly included

--- diagnostics/utils/plotWellAllocationPanel.m
+++ diagnostics/utils/plotWellAllocationPanel.m
@@ -1,31 +1,25 @@
-function plotWellAllocationComparison(D1, WP1, D2, WP2, varargin)
+function plotWellAllocationPanel(D, WP, varargin)
 %Plot a panel comparing well-allocation from models with different resolution
 %
 % SYNOPSIS
-%   plotWellWallocationComparision(D1, WP1, D2, WP2)
+%   plotWellAllocationPanel(D, WP)
 %
 % PARAMETERS:
-%   D1, D2   - data structure with basic data for flow diagnostics computed
-%              by a call to 'computeTOFandTracer' for model 1 and model 2
+%   D  - data structure with basic data for flow diagnostics computed
+%        by a call to 'computeTOFandTracer'
 %
-%   WP1, WP2 - data structure containing information about well pairs,
-%              computed by a call to 'computeWellPairs'
+%   WP - data structure containing information about well pairs,
+%        computed by a call to 'computeWellPairs'
 %
 % DESCRIPTION:
 %   The routine makes a bar plot for each well segment that is represented
-%   in the input data D/WP. (There is no check that the well segments in
-%   D1 and D2 are the same). For injection wells, the bars represent
+%   in the input data D/WP.  For injection wells, the bars represent
 %   the cumulative outfluxes, from the bottom to the top of the segment,
 %   that have been attributed to the different producers. The bars are
-%   shown in color for model 1, with a unique color representing each of
-%   the segments of the producers,  and in solid black lines for model 2.
+%   shown in color with a unique color representing each of
+%   the segments of the producers.
 %   For the production wells, the bars represent influxes that can be
-%   attributed to different injector segments. If the two models predict
-%   the same flux allocation, the color bars and the solid lines should be
-%   matching.
-%
-%   If D2 and WP2 are empty, they are set equal D1 and WP1, and a graph is
-%   produced which shows the well allocation for the wells in WP1.
+%   attributed to different injector segments.
 %
 % SEE ALSO:
 %   computeTOFandTracer, computeWellParis, expandWellCompletions
@@ -56,7 +50,7 @@ opt = merge_options(opt, varargin{:});
 % -------------------------------------------------------------------------
 % check what we should plot
 if (isnan(opt.plotOnly))
-    num_wells = numel(WP1.inj) + numel(WP1.prod);
+    num_wells = numel(WP.inj) + numel(WP.prod);
     opt.plotOnly = 1:num_wells;
 else
     if (numel(opt.plotOnly) == 0)
@@ -64,87 +58,47 @@ else
         return;
     end
 end
-if (isempty(D2) || isempty(WP2))
-    plotFlag = false;
-else
-    plotFlag = true;
-end
 
 % -------------------------------------------------------------------------
 % Parameterization along well path
 % Create fake z values based on the cell numbering in the well. Useful
 % whenever the well isn't vertical.
-WP1 = addDummyData(WP1);
+WP = addDummyData(WP);
 if ~opt.useZ
-    for i = 1:numel(WP1.inj)
-        n = numel(WP1.inj(i).z);
-        WP1.inj(i).z = (1:n)./n;
+    for k = 1:numel(WP.inj)
+        n = numel(WP.inj(k).z);
+        WP.inj(k).z = (1:n)./n;
     end
-    for i = 1:numel(WP1.prod)
-        n = numel(WP1.prod(i).z);
-        WP1.prod(i).z = (1:n)./n;
-    end
-end
-if plotFlag
-    WP2 = addDummyData(WP2);
-    if ~opt.useZ
-        for i = 1:numel(WP1.inj)
-            n = numel(WP2.inj(i).z);
-            WP2.inj(i).z = (1:n)./n;
-        end
-        for i = 1:numel(WP1.prod)
-            n = numel(WP2.prod(i).z);
-            WP2.prod(i).z = (1:n)./n;
-        end
+    for k = 1:numel(WP.prod)
+        n = numel(WP.prod(k).z);
+        WP.prod(k).z = (1:n)./n;
     end
 end
 
 % -------------------------------------------------------------------------
-% Extract and format the well-allocation factors for model 1
-nit = numel(D1.inj);
-npt = numel(D1.prod);
-for i=1:numel(WP1.inj)
-   nc   = numel(WP1.inj(i).z);
+% Extract and format the well-allocation factors
+nit = numel(D.inj);
+npt = numel(D.prod);
+for k=1:numel(WP.inj)
+   nc   = numel(WP.inj(k).z);
    atmp = zeros(nc, nit+npt);
-   atmp(:,D1.prod) = WP1.inj(i).alloc;
-   WP1.inj(i).alloc = atmp;
+   atmp(:,D.prod) = WP.inj(k).alloc;
+   WP.inj(k).alloc = atmp;
 end
-for i=1:numel(WP1.prod)
-   nc = numel(WP1.prod(i).z);
+for k=1:numel(WP.prod)
+   nc = numel(WP.prod(k).z);
    atmp = zeros(nc, nit+npt);
-   atmp(:,D1.inj) = WP1.prod(i).alloc;
-   WP1.prod(i).alloc = atmp;
+   atmp(:,D.inj) = WP.prod(k).alloc;
+   WP.prod(k).alloc = atmp;
 end
-wp1(D1.inj)  = WP1.inj(:);
-wp1(D1.prod) = WP1.prod(:);
-
-% -------------------------------------------------------------------------
-% Extract and format the well-allocation factors for model 2
-if plotFlag
-    nit = numel(D2.inj);
-    npt = numel(D2.prod);
-    for i=1:numel(WP2.inj)
-        nc   = numel(WP2.inj(i).z);
-        atmp = zeros(nc,nit+npt);
-        atmp(:,D2.prod) = WP2.inj(i).alloc;
-        WP2.inj(i).alloc  = atmp;
-    end
-    for i=1:numel(WP2.prod)
-        nc   = numel(WP2.prod(i).z);
-        atmp = zeros(nc,nit+npt);
-        atmp(:,D2.inj) = WP2.prod(i).alloc;
-        WP2.prod(i).alloc  = atmp;
-    end
-    wp2(D2.inj)  = WP2.inj(:);
-    wp2(D2.prod) = WP2.prod(:);
-end
+wp(D.inj)  = WP.inj(:);
+wp(D.prod) = WP.prod(:);
 
 % -------------------------------------------------------------------------
 % Calculate number of plots in 2D grid
 num_plots = numel(opt.plotOnly);
-if (numel(opt.plotOnly) < numel(wp1))
+if (numel(opt.plotOnly) < numel(wp))
     clf();
-    num_plots = num_plots + 1;
 end
 nx = ceil(sqrt(num_plots));
 ny = nx-1;
@@ -155,105 +109,74 @@ end
 current_fig = gcf();
 wb = waitbar(0, 'Well 1');
 set(0, 'CurrentFigure', current_fig);
+[xs,ys]=meshgrid(linspace(0,.85,nx+1),linspace(0,1,ny+1));
+dx=diff(xs,[],2); mx=.05*max(dx(:));
+dy=diff(ys,[],1); my=.05*max(dy(:));
+
+% -------------------------------------------------------------------------
+% Add color legend
+axes('position',[.87 .025 .11 .95]);
+nw = numel(wp);
+nc = 2;
+nr = ceil(nw/nc);
+[x,y]=meshgrid(1:nc,1:nr);
+im=nan(1,nr*nc); im(1:nw)=1:nw; im = reshape(im,nc,nr)';
+pcolor(im([end:-1:1 1],[1:end end]));
+txt = cell(nc,nr); txt(1:nw) = {wp.name}; txt=flipud(txt'); txt=txt(:);
+ht=text(x(:)+.5,y(:)+.5,txt);
+set(ht,'Color','w','HorizontalAlignment','center','FontWeight','bold','FontSize',8);
+%ht2=text(x(:)+.5,y(:)+.5,txt); 
+%set(ht2,'Color','w','HorizontalAlignment','center','FontSize',8);
+axis off
+
 
 % -------------------------------------------------------------------------
 % Plot the well-allocation factors for fine/coarse scale models
-for j=1:numel(opt.plotOnly)
-   subplot(nx,ny,j);
-   i = opt.plotOnly(j);
+for n=1:numel(opt.plotOnly)
+   i = rem(n-1,nx)+1;
+   j=ny-fix((n-1)/nx);
+   axes('position', ...
+       [xs(j,i)+mx ys(j,i)+my dx(j,i)-2*mx  dy(j,i)-2*my]);
+   %subplot(nx,ny+1,n);
+   k = opt.plotOnly(n);
    
    % --------------------------------
-   % Plot data for model 1
-   [~,ix]   = sort(wp1(i).z);
-   wp1(i).z = wp1(i).z(ix);
-   wp1(i).alloc = wp1(i).alloc(ix,:);
-   nseg = numel(wp1(i).z);
+   % Plot data
+   [~,ix]   = sort(wp(k).z);
+   wp(k).z = wp(k).z(ix);
+   wp(k).alloc = wp(k).alloc(ix,:);
+   nseg = numel(wp(k).z);
    if nseg == 1  % Need to trick Matlab
-      z = [wp1(i).z; wp1(i).z+1];
-      a = [cumsum(wp1(i).alloc,1); zeros(1,numel(wp1(i).alloc))];
-      bwidth = 2*numel(wp2(i).z)+3;
+      z = [wp(k).z; wp(k).z+1];
+      a = [cumsum(wp(k).alloc,1); zeros(1,numel(wp(k).alloc))];
+      bwidth = 2*numel(wp2(k).z)+3;
       h = barh(z, a,'stacked','BarWidth',bwidth, 'EdgeColor','none');
    else
-      z = flipud(wp1(i).z);
-      a = cumsum(flipud(wp1(i).alloc),1);
+      z = flipud(wp(k).z);
+      a = cumsum(flipud(wp(k).alloc),1);
       if nseg<21
-          aargs = {'YDir', 'YLim'};
+          args = {'YDir', 'YLim'};
           h = barh(z, a,'stacked','BarWidth',opt.bwidth, 'EdgeColor','none');
       else
           h = area(z, a, eps,'EdgeColor','none'); axis tight;
           view(90,-90);
-          aargs = {'XDir', 'XLim'};
+          args = {'XDir', 'XLim'};
       end
    end
-   zm = min(wp1(i).z);
-   zM = max(wp1(i).z);
-   
-   % --------------------------------
-   % Plot data for model 2
-   if plotFlag
-       hold on
-       nseg = numel(wp2(i).z);
-       if nseg == 1  % Need to trick Matlab
-           z = [wp2(i).z; wp2(i).z+1];
-           a = [cumsum(wp2(i).alloc,1); zeros(1,numel(wp2(i).alloc))];
-           barh(z,a,'stacked','FaceColor','none','BarWidth',opt.bwidth);
-       else
-           z = flipud(wp2(i).z);
-           a = cumsum(flipud(wp2(i).alloc),1);
-           if nseg<21
-               aargs = {'YDir', 'YLim'};
-               barh(z,a,'stacked','FaceColor','none','BarWidth',opt.bwidth);
-           else
-               h = area(z, a, eps,'FaceColor','none'); axis tight;
-               view(90,-90);
-               aargs = {'XDir', 'XLim'};
-           end
-       end
-       hold off
-       zm = min(zm,min(wp2(i).z));
-       zM = max(zM,max(wp2(i).z));
-   end
+   zm = min(wp(k).z);
+   zM = max(wp(k).z);
    
    % ---------------------------------
    % Set axis and add legend
-   set(gca,aargs{1},'reverse', aargs{2}, [zm zM + sqrt(eps)] + [-.1 .1]*(zM-zm));
+   set(gca,args{1},'reverse', args{2}, [zm zM + sqrt(eps)] + [-.1 .1]*(zM-zm));
 
-   if max(wp1(i).alloc(:))>0
-      hl=legend(h(i),wp1(i).name,4); set(hl,'FontSize',8); legend boxoff
+   if max(wp(k).alloc(:))>0
+      hl=legend(h(k),wp(k).name,4); set(hl,'FontSize',8); legend boxoff
    else
-      hl=legend(h(i),wp1(i).name,3); set(hl,'FontSize',8); legend boxoff
+      hl=legend(h(k),wp(k).name,3); set(hl,'FontSize',8); legend boxoff
    end
-   waitbar(j/numel(opt.plotOnly),wb,['Well ', num2str(i+1)])
-end
-
-% -------------------------------------------------------------------------
-% Show legend for wells that are not plotted
-if (numel(opt.plotOnly) < numel(wp1))
-    subplot(nx,ny,max(numel(opt.plotOnly)+1,(nx-1)*ny+1):nx*ny);
-    cla('reset');
-    hold on;
-    axis off;
-    
-    h = [];
-    
-    %Make a simple patch for each color
-    for i=1:numel(wp1)
-       h1 = patch([0.25, 0.75], [0.25, 0.75], i);
-       %Store the colors we actually want in legend in h
-       if (not(any(opt.plotOnly == i)))
-           h = [h, h1];
-       end
-    end
-    
-    %Find labels
-    labels_sel = ones(1, numel(wp1));
-    labels_sel(opt.plotOnly) = 0;
-    labels={wp1(find(labels_sel)).name};
-    
-    %Create legend
-    pos = get(gca(), 'Position');
-    hl=legend(h, labels, 'Location', 'none', 'Position', pos); 
-    set(hl,'FontSize',8);
+   set(gca,'XTickLabel',[],'YTickLabel',[]);
+   waitbar(n/numel(opt.plotOnly),wb,['Well ', num2str(k+1)])
 end
 
 % -------------------------------------------------------------------------
