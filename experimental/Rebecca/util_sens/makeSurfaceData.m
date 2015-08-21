6cc3a19fc21ae414a813ae1a04ae877017b48bee
Halvor Møll Nilsen <Halvor.M.Nilsen@sintef.no>
2015-08-21 12:45:03 +0200

    made makeSurfaceData.m also work with grids without proper cartdims

--- experimental/Rebecca/util_sens/makeSurfaceData.m
+++ experimental/Rebecca/util_sens/makeSurfaceData.m
@@ -1,15 +1,27 @@
-function [plumes,co2_int] = makeSurfaceData(plumes,Gt)
+function [plumes,topsurface, topfit, hCO2] = makeSurfaceData(plumes,Gt)
+if(isfield(Gt,'cartDims') && Gt.cells.num==prod(Gt.cartDims))
 X=reshape(Gt.cells.centroids(:,1),Gt.cartDims(1),Gt.cartDims(2));
 Y=reshape(Gt.cells.centroids(:,2),Gt.cartDims(1),Gt.cartDims(2));
 Z=reshape(Gt.cells.z,Gt.cartDims(1),Gt.cartDims(2));
 topsurface=@(coord) interp2(X',Y',Z',coord(:,1),coord(:,2));
+else
+    F = scatteredInterpolant(Gt.cells.centroids(:,1),Gt.cells.centroids(:,2),Gt.cells.z);
+    topsurface =@(coord) F(coord(:,1),coord(:,2));
+end
 for i=1:numel(plumes)
     line_coord=plumes{i}.outline;
+    %%{
     nd=size(line_coord,1);
     A=[ones(nd,1),line_coord];
     rhs=topsurface(line_coord);
     vec=A\rhs;
     topfit=@(coord) [ones(size(coord,1),1),coord]*vec;
+    %}
+%{
+    [ydata,ia,ic]=unique(line_coord(:,2));
+    zt =@(y) interp1(ydata,topsurface(line_coord(ia,:)),y);
+    topfit =@(coord)  zt(coord(:,2));
+%}
     hCO2_tmp =@(coord)(topfit(coord)-topsurface(coord));
     hCO2 =@(coord) hCO2_tmp(coord).*(hCO2_tmp(coord)>0).*insidePolygon(line_coord,coord);
     plumes{i}.hCO2=hCO2;
