f797236b6687f2b6f2130d6563b2535d415bd037
Rebecca Allen <allen.rebecca.d@gmail.com>
2015-09-21 11:23:31 +0200

    fixed typo in function name

--- experimental/SleipnerBenchmarkInvestigations/util_sens/addHeightData.m
+++ experimental/SleipnerBenchmarkInvestigations/util_sens/addHeightData.m
@@ -1,4 +1,4 @@
-function states = addHightData(states,Gt,fluid)
+function states = addHeightData(states,Gt,fluid)
     for i=1:numel(states)
        states{i}.h=Gt.cells.H.*states{i}.s(:,2)./(1-fluid.res_water); 
     end
