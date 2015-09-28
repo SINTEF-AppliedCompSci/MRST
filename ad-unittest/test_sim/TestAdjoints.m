479967b9d644a41f0f5b41a9f16a123b01c236fb
Olav Møyner <olav.moyner@sintef.no>
2015-09-28 12:46:56 +0200

    Update TestAdjoints to account for changes in objective function syntax.

--- ad-unittest/test_sim/TestAdjoints.m
+++ ad-unittest/test_sim/TestAdjoints.m
@@ -44,7 +44,7 @@ classdef TestAdjoints < matlab.unittest.TestCase
             % Setup function handles for NPV for oil/water system
             obj_adj = @(tstep)NPVOW(G, wellSols, schedule, ...
                             'ComputePartials', true, 'tStep', tstep);
-            obj_num = @(wellSols)NPVOW(G, wellSols, schedule);
+            obj_num = @(wellSols, states)NPVOW(G, wellSols, schedule);
         end
     end
     
