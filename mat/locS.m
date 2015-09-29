9f70780e5b9e6528bebe4fb6f36c9807bfc8a26f
oystein klemetsdal <oystein.klemetsdal@gmail.com>
2015-09-29 18:01:52 +0200

    Implementation of locS.m

--- mat/locS.m
+++ mat/locS.m
@@ -1,94 +1,94 @@
-function [lS]=locS(X)
+function [Sl]=locS(X)
 %--------------------------------------------------------------------------
 %   Generates local matrix for diffusion problem
 %
 %   X:  mx2 matrix containing the m vertices of the polygon.
 %       X(i,:) = (x,y) coordinates of vertex i. Vertices ordered counter
 %       clockwise.
-%   Sl: m x m local stifness matrix.
+%   Sl: NK x NK local stifness matrix.
 %--------------------------------------------------------------------------
 
-                            %   Number of vertices.                       %
-n = size(X,1);
-k = 2;
-nk = 0.5*(k+1)*(k+2);
-NK = n*k + 0.5*k*(k-1);
-alf=1;
-[~,BB]=baric(X);            %   Baricenter coordinates.                  %
-xV = X(:,1); yV = X(:,2);
-xK = BB(1); yK = BB(2);
+%%  DIMENSIONS, BARICENTER, MONOMIALS.                                   %% 
 
-% -------
-% BASIS for the VEM space is value at first
-% vertex, then second vertex, etc ... 
-% BASIS for the polynomial space is 
-% {1,x,y} with coordinates x,y scaled with the baricenter
-                            
-                            %   Build matrix D.                           %
-hK = sqrt(2);               %   Element Diameter
-                            %   Monomials and their gradients and         %
-                            %   Laplacians.                               %
-m1 = @(x,y) 1*(x+1)./(x+1);                      %   m_(0,0)
-m2 = @(x,y) (x-xK)./hK;             %   m_(1,0)
-m3 = @(x,y) (y-yK)./hK;             %   m_(0,1)
-m4 = @(x,y) (x-xK).^2/hK^2;         %   m_(2,0)
-m5 = @(x,y) (x-xK).*(y-yK)./hK^2;   %   m_(1,1)
-m6 = @(x,y) (y-yK).^2./hK^2;        %   m_(0,2)
+n = size(X,1);              %   Number of vertices.
+k = 2;                      %   Method order.
+nk = 0.5*(k+1)*(k+2);       %   Dimension of polynomial space.
+NK = n*k + 0.5*k*(k-1);     %   Dimension of V^K (degrees of freedom).
+[~,BB]=baric(X);            %   Baricenter of K
+xK = BB(1); yK = BB(2);        
+hK = sqrt(2);               %   Element Diameter (NG)
 
-grad_m2 = @(x,y) 1/hK*[1,0];        %   \nabla m_(1,0)
-grad_m3 = @(x,y) 1/hK*[0,1];        %   \nabla m_(0,1)
-grad_m4 = @(x,y) 1/hK^2*[x-xK,0];   %   \nabla m_(2,0)
-grad_m5 = @(x,y) 1/hK^2*[1,1];      %   \nabla m_(1,1)
-grad_m6 = @(x,y) 1/hK^2*[0,y-yK];   %   \nabla m_(0,2)
+                            %   Monomials. m(i) = m_i
+m =      @(X) 1/hK.*[X(:,1)-xK, ...                             %   (1,0)
+                     X(:,2)-yK, ...                             %   (0,1)
+                     (X(:,1)-xK).^2./hK, ....                   %   (2,0)
+                     (X(:,1)-xK).*(X(:,2)-yK)./hK, ....         %   (1,1)
+                     (X(:,2)-yK).^2./hK];                       %   (0,2)
+                 
+                            %   Gradients of monomials.
+                            %   grad_m(i,:) = \nabla m_i.
+grad_m = @(X) 1/hK.*[1                 , 0;                     %   (1,0)
+                     0                 , 1;                     %   (0,1)
+                     2/hK.*(X(:,1)-xK) , 0;                     %   (2,0)
+                     1/hK.*(X(:,2)-yK) , 1/hK.*(X(:,1)-xK);     %   (1,1)
+                     0                 , 2/hK.*(X(:,2)-yK)   ]; %   (0,2)
 
-malpha = (X - ones(n,1)*BB)./hK;
+%%  BUILD MATRIX D                                                       %%
+
 D = zeros(NK, nk);
-m2(xV,yV)
-                            %   Monomial values at the vertices.          %
-D(1:n,:) = [m1(xV,yV), m2(xV,yV), m3(xV,yV), ...
-            m4(xV,yV), m5(xV,yV), m6(xV,yV)];
-                            %   Monomial values at the midpoints.         %
-mid = (X(1:end,:) + [X(2:end,:);X(1,:)])/2;
-xMid = mid(:,1); yMid = mid(:,2);
-D(n+1:2*n,:) = [m1(xMid,yMid), m2(xMid,yMid), m3(xMid,yMid), ...
-                m4(xMid,yMid), m5(xMid,yMid), m6(xMid,yMid)];
-                            %   Integral of monomials over the element    %
-                            %   using Gauss-Lobatto quadrature.           %
+                            %   Monomial values at the vertices.
+D(1:n,:) = [ones(n,1), m(X)];
+                            %   Monomial values at the midpoints.
+Xmid = (X(1:end,:) + [X(2:end,:);X(1,:)])/2;
+D(n+1:2*n,:) = [ones(n,1), m(Xmid)];
+                            %   Integral of monomials over K using
+                            %   Gauss-Lobatto quadrature. (NG)
 D(NK,:) = [1/36*sum(D(1:n,:)) + 1/9*sum(D(n+1:2*n,:))];
-D(NK,1) = D(NK,1) + 4/9
+D(NK,1) = D(NK,1) + 4/9;
 
+%%  BUILD MATRIX B.
 
-                            %   Build matrix B (initially as transpose).  %
-B=zeros(NK,nk);
-for j=1:nk                  %   Add contribution of each edge             %
-                            %   Kernel part (first column).
-                            %   See projection below.                     %
-   B(j,1) = (1/n); 
-                            %   Non kernel part (second and third columns)%
-                            %   Outward normal.                           %
-   n = [ X(mod(j,m)+1,2) - X(j,2) , - X(mod(j,m)+1,1) + X(j,1) ];
-   B(j,2:3) = B(j,2:3) + (1/2)*n;
-   B(mod(j,m)+1,2:3) = B(mod(j,m)+1,2:3) + (1/2)*n; 
+B=zeros(nk,NK);
+                            %   Contribution from boundary integrals.
+                            %   Basisfunctions with non-zero degrees of
+                            %   freedom on edge j are \phi_j,
+                            %   \phi_{mod(j,n)+1} (endpoints) and
+                            %   \phi_{j+4} (midpoint). For each edge, 
+                            %   \int_\patrial K \nabla m_\alpha \cdot nVec
+                            %   is evaluated by Gauss-Lobatto.
+for j=1:n                  
+                            %   Outward normal of edge j, nVec
+    nVec = [X(mod(j,n)+1,2) - X(j,2) , - X(mod(j,n)+1,1) + X(j,1) ]';
+                            %   First point of edge j.
+    grad_mDOTnVec = grad_m(X(j,:))*nVec;
+    B(2:nk,j) = B(2:nk,j) + 1/6.*grad_mDOTnVec;
+                            %   Last point of edge j.
+    grad_mDOTnVec = grad_m(X(mod(j,n)+1,:))*nVec;
+    B(2:nk,mod(j,n)+1) = B(2:nk,mod(j,n)+1) + 1/6.*grad_mDOTnVec;
+                            %   Midpoint of edge j.
+    grad_mDOTnVec = grad_m(Xmid(j,:))*nVec;
+    B(2:nk,j+4) = B(2:nk,j+4) + 2/3.*grad_mDOTnVec;
 end
-B=B'; % transpose 
+                            %   Contribution from surface integrals.
+                            %   \Dela m_\alpha = 2/hK^2 for \alpha = (2,0)
+                            %   and (0,2), 0 otherwise. (NG)
+B(4,NK) = B(4,NK) - 2/hK^2;
+B(6,NK) = B(6,NK) - 2/hK^2;
+B(1,NK) = 1;                %   First row (!?). (NG)
 
-% --- build projectors
-G = B*D;          % G matrix from Hitchhikers paper
-PNs = inv(G)*B;   % PiNabla star projector (polynomial basis)
-PN = D*PNs;      % PiNabla projector (Vh basis)  
-%  --- build local matrix ---
-Gt(1,:)=zeros(1,3);   % matrix G tilde (null on the kernel)
-Gt(2:3,:)=G(2:3,:);
+%%  CONSTRUCTION OF LOCAL STIFFNESS MATRIX.                              %% 
 
-M = PNs'*Gt*PNs;  % consistent part
-Sl = M + alf*(trace(M)/2)*(eye(m)-PN)'*(eye(m)-PN);
+G = B*D;
+PNstar = G\B;               %   \Pi^\Nabla in the monomial basis.
+PN = D*PNstar;              %   \Pi^\Nabla in the V^K basis.
+Gtilde = zeros(nk,nk);
+Gtilde(2:nk,:)=G(2:nk,:);
 
+                            %   Local stiffness matrix.
+Sl = PNstar'*Gtilde*PNstar + (eye(NK)-PN)'*(eye(NK)-PN);
 
-% -----------------------
-% KERNEL PROJECTION
-% -----------------------
-% Let the vertex based scalar product 
-% <v,w> = (1/m) sum_{i=1}^m v(vertex_i)\cdot w(vertex_i) .
-% Then, the projector on the kernel is
-% P(v) := <1,v>
-% -----------------------
+D
+B
+G
+
+end
--- mat/runLoc.m
+++ mat/runLoc.m
@@ -1,19 +1,27 @@
 run('../../matlab/project-mechanics-fractures/mystartup.m')
 
-% G = cartGrid([1,1]);
-% G = mrstGridWithFullMappings(G);
-% G = computeGeometry(G);
-% 
-% iNodes = [G.cells.nodePos(1):G.cells.nodePos(2)-1];
-% nodes = G.cells.nodes(iNodes);
-% X = G.nodes.coords(nodes,:);
-% 
-% Sl = locS(X);
+                            %   Build cartesian grid and compute geometry.
+nx = 3; ny = 3;             %   Grid dimensions.
+G = cartGrid([nx, ny]);
+G = mrstGridWithFullMappings(G);
+G = computeGeometry(G);
+n = G.cells.num;            %   Number of cells in G.
 
-d = sqrt(2);
-X = [0.0, 0.0
-     1.0, 0.0;
-     1.0, 1.0;
-     0.0, 1.0];
-
-locS(X)
+pos = 0;
+for c = 1:n
+    
+    nodeNum = G.cells.nodePos(c):G.cells.nodePos(c+1)-1
+    nodes = G.cells.nodes(nodeNum)
+    X = G.nodes.coords(nodes,:)
+    Sl = locS(X);
+    
+    dofvec=mcolon(2*(nodes-1)+1,2*(nodes-1)+2);
+    iindl=repmat(dofvec',1,numel(dofvec));
+    jindl=repmat(dofvec,numel(dofvec),1);
+    nnzl = numel(iindl);
+    iglob(pos+1:pos+nnzl) = iindl;
+    jglob(pos+1:pos+nnzl) = jindl;
+    mglob(pos+1:pos+nnzl) = Sl;
+    pos = pos+nnzl;
+end
+S=sparse(iglob(:),jglob(:),mglob(:),ndof,ndof)
