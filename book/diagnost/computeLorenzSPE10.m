5d2586f10fe4b70ff01252ff7dadaf214e2a91dd
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2015-10-08 23:30:36 +0200

    Script that computes Lorenz coefficient for all layers of SPE10 and then changes the well pattern in one of the layers manually to show how one can improve the Lorenz coefficient and the sweep.

--- book/diagnost/computeLorenzSPE10.m
+++ book/diagnost/computeLorenzSPE10.m
@@ -59,29 +59,68 @@ for n=1:85
     waitbar(n/85);
 end
 close(h);
+clf; set(gcf,'Position',[470 420 960 400]);
 h=bar(Lc,'Style','hist'); 
 axis tight; set(h,'FaceColor',[.95 .95 1],'EdgeColor',[0 0 .7]);
 
 % [nmin, nmax] = deal(22,61);
 
 %% Look at case with lowest Lorenz coeffficient
-n=22;
+minCase = false;
+if minCase
+    [~,n]=min(Lc);
+else
+    [~,n]=max(Lc);
+end
 rock = SPE10_rock(1:cartDims(1),1:cartDims(2),n);
 rock.perm = convertFrom(rock.perm, milli*darcy);
 rock.poro = max(rock.poro, 1e-4);
-W = [];
-for w = 1 : numel(wtype),
-    W = verticalWell(W, G, rock, wloc(1,w), wloc(2,w), 1, ...
-        'Type', wtype{w}, 'Val', wtarget(w), ...
-        'Radius', wrad(w), 'Name', wname{w}, ...
-        'InnerProduct', 'ip_tpf');
+pv = poreVolume(G, rock);
+
+nwloc = wloc;
+for nstep=1:2
+    W = [];
+    for w = 1 : numel(wtype),
+        W = verticalWell(W, G, rock, nwloc(1,w), nwloc(2,w), 1, ...
+            'Type', wtype{w}, 'Val', wtarget(w), ...
+            'Radius', wrad(w), 'Name', wname{w}, ...
+            'InnerProduct', 'ip_tpf');
+    end
+
+    rS = initState(G, W, 0);
+    T  = computeTrans(G, rock);
+    rS = incompTPFA(rS, G, T, fluid, 'wells', W);
+    D  = computeTOFandTracer(rS, G, rock, 'wells', W, 'maxTOF', inf);
+    WP = computeWellPairs(rS, G, rock, W, D);
+    [F,Phi] = computeFandPhi(pv, D.tof);
+    computeLorenz(F,Phi)
+
+    figure('Position',[710   420   720   400]);
+    plotCellData(G,rock.poro,'EdgeColor','none');
+    plotGrid(Gbb,'FaceColor','none');
+    plotWell(G,W,'height',10,'LineWidth',4);
+    plotWellPairConnections(G,WP,D,W,pv,1e-4);
+    cmap=jet(128); colormap(.6*cmap + .4*ones(size(cmap))); clear cmap;
+    view(0,70); set(gca,'DataAspectRatio',[1 1 .5]); axis off
+    cax = caxis;
+
+    pos = [.05 .025 .25 .35;  .725 .025 .25 .35; ...
+        .05 .625 .25 .35; .725 .625 .25 .35];
+    for i=1:4
+        axes('position', pos(i,:));
+        rad = sum(bsxfun(@minus,G.cells.centroids,....
+            G.cells.centroids(W(i).cells,:)).^2,2);
+        plotCellData(G,rock.poro,rad<3500,'EdgeColor','k','EdgeAlpha',.1);
+        plotWell(G,W(i),'height',10,'LineWidth',10);
+        caxis(cax);
+        view(0,70); set(gca,'DataAspectRatio',[1 1 .5]); axis off tight;
+    end
+    
+    if minCase,
+        nwloc     = [  1,   53,     1,   59,  30;
+                       1,    2,   218,  220, 111];
+    else
+        nwloc     = [  1,   60,     7,   60,  30;
+                       6,   11,   220,  219, 111];
+    end
 end
-interactiveDiagnostics(G, rock, W);
-
-
-
-wloc     = [  1,   60,     1,   60,  30;
-              1,    1,   220,  220, 111];
-% show tof and/or tracer partition + well allocation
-% try to move wells manually to equalize sweep regions
-% check well allocation & Lorenz coefficient
