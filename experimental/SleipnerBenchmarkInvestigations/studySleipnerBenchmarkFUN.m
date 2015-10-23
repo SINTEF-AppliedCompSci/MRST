37331e417ab39f021ae05b22a304eb699fa6cd4b
Rebecca Allen <allen.rebecca.d@gmail.com>
2015-10-23 11:28:45 +0200

    delete unused lines of code

--- experimental/SleipnerBenchmarkInvestigations/studySleipnerBenchmarkFUN.m
+++ experimental/SleipnerBenchmarkInvestigations/studySleipnerBenchmarkFUN.m
@@ -72,63 +72,6 @@ opt = merge_options(opt, varargin{:});
     var.wellYcoord      = 6.47121e6;
 
 
-%     %% Select well injection rate data:
-%     fprintf(['Your rate case is set to ' opt.ratecase '.\n'])
-%     switch opt.ratecase 
-%         % Note, inj_rates are in terms of reservoir rates (i.e., the
-%         % volumetric rate of CO2 entering layer 9, not the volumetric
-%         % surface rate). Seismic imaging provided estimates of how much CO2
-%         % accumlated in the pore space of layer 9. These volumes were
-%         % likely converted into a mass using an infered CO2 density, and
-%         % then into a surface rate using the CO2 density at the surface.
-%         % Specifying the inj_rates in terms of reservoir volume instead of
-%         % surface volume allows one to test other CO2 densities without the
-%         % need to modify a surface volume injection rate.
-%         
-%         case 'SPE'
-%             % See Singh et al 2010 for more info about how they determined
-%             % these rates. Note: the injection rates were reported as
-%             % surface rates. Both volume and mass were given, thus surface
-%             % density can be calculated (=1.87 kg/m3). The CO2 density at
-%             % reservoir conditions was reported as 760 kg/m3.
-%             var.inj_year   = [1999; 2000; 2001; 2002; 2003; 2004; 2005; 2006; 2007; 2008; 2009];
-%             var.inj_rates  = [2.91e4; 5.92e4; 6.35e4; 8.0e4; 1.09e5; 1.5e5; 2.03e5; 2.69e5; 3.47e5; 4.37e5; 5.4e5] .* meter^3/day;
-%             % inj_rates is in meter^3/s
-%             % Convert to rate at reservoir conditions
-%             var.inj_rates  = var.inj_rates.*(1.87/760);       
-%         case 'original' 
-%             % See "Injection rates Layer 9.xls" under
-%             % co2lab/data/sleipner/original for more info about these
-%             % rates. Note: the CO2 density at reservoir conditions was
-%             % reported as 695.89 kg/m3, and the surface density 1.87 kg/m3.
-%             [ var.inj_year, var.inj_rates ] = getSleipnerOriginalInjectionRates();
-%             % inj_rates is in meter^3/s   
-%         otherwise
-%             error('The injection rate option was either invalid or not selected.')     
-%     end
-% 
-%     % limit the number of injection years (optional)
-%     if ~isempty(opt.num_years)
-%         fprintf('\n Only using first %d years of injection data. \n', opt.num_years)
-%         var.inj_year  = var.inj_year(1:opt.num_years);
-%         var.inj_rates = var.inj_rates(1:opt.num_years);
-%     end
-%         
-% 
-%     %% Specify and compute time step size for injection period.
-%     % ***Note: inj_time and inj_steps are applied to each inj_rate given***
-%     inj_time    = 1 * year; % DEFAULT. CAN ONLY ADJUST NUMBER OF STEPS.
-%     inj_steps   = 2;
-%     dTi         = inj_time / inj_steps; % timestep size in seconds
-% 
-%     % Specify and compute time step size for migration period.
-%     % Note: migration period is not relevant for Sleipner benchmarking,
-%     % however could be implemented if desired.
-%     %mig_time    = 0 * year; % CAN ADJUST.
-%     %mig_steps   = 0;        % CAN ADJUST.
-%     %dTm         = mig_time / mig_steps; % timestep size in seconds
-
-
     %% Specify fluid properties:
     % TODO: implement as opt
     [rho, mu, sr, sw]   = getValuesSPE134891();
@@ -150,7 +93,7 @@ opt = merge_options(opt, varargin{:});
 
 
 
-    %% 1. Load grid with given refinement/coarsening specified (if any):
+    %% Load grid with given refinement/coarsening specified (if any):
     % makeSleipnerModelGrid() looks for file or generates it from grdecl files
     % and writes .mat file. Output is the variables G, Gt, rock, rock2D.
     % Grid options: 'ORIGINALmodel', 'IEAGHGmodel', 'INHOUSEmodel'
@@ -248,7 +191,7 @@ opt = merge_options(opt, varargin{:});
     var.wellCoord_z = 0;
     
     
-    %% Schedule: add discrete rates, time steps, well controls
+    %% Schedule: set up time steps, well controls, and get discrete rates
    
     % First get time step sizes and associated control well numbers:
     [ schedule, var_tmp ] = ...
@@ -287,24 +230,6 @@ opt = merge_options(opt, varargin{:});
         schedule.control(i).bc = bc;
     end
 
-%     % Time step:
-%     % TODO: implement dynamic time-stepping
-%     istepvec                = repmat( ones(inj_steps, 1) * var.dTi , [numel(var.inj_rates) 1] );
-%     %mstepvec                = ones(mig_steps, 1) * dTm;    
-%     schedule.step.val       = var.dTi; %istepvec; %[istepvec; mstepvec];
-%     schedule.step.control   = [ 1:1:numel(var.dTi) ]';
-%     schedule.step.control   = [];
-%     for i = 1:numel(schedule.control)
-%         schedule.step.control = [schedule.step.control; ];
-%         %if schedule.control(i).W.val ~= 0
-%             % an injection period
-%             %schedule.step.control = [schedule.step.control; ones(inj_steps, 1) * i];
-%         %elseif schedule.control(i).W.val == 0
-%             % a migration period
-%         %    schedule.step.control = [schedule.step.control; ones(mig_steps, 1) * i];
-%         %end
-%     end
-
 
     %% Create fluid:
     caprock_temperature = 273.15 + seafloor_temp + ...
@@ -325,6 +250,7 @@ opt = merge_options(opt, varargin{:});
         'pvMult_fac'  , pvMult, ...
         'residual'    , [sw, sr] , ...
         'dissolution' , isDissOn, 'dis_max', dis_max);
+    
 
     %% Solver testing:
     if opt.testSolverOptions
