cd9647ffac8d3661f7e40300fe188c8f93185dda
oystein klemetsdal <oystein.klemetsdal@gmail.com>
2015-10-27 20:38:06 +0100

    Notes from Equivalent Projectors paper added to vemTheory.tex.

--- mat/runVEM2D.m
+++ mat/runVEM2D.m
@@ -2,31 +2,33 @@ clc; close all;
 
 run('../../matlab/project-mechanics-fractures/mystartup.m')
 
-n = 25;
+n = 40;
 nx = n; ny = n;
 G = unitSquare(nx, ny);
 G = sortEdges(G);
 G = mrstGridWithFullMappings(G);
 G = computeGeometry(G);
 
-delta = 0.001;
-gD = @(X) 10*ones(size(X,1),1);
-gN = @(X) zeros(size(X,1),1);
+% delta = 0.001;
+% gD = @(X) 10*ones(size(X,1),1);
+% gN = @(X) zeros(size(X,1),1);
+% f = @(X) 1/sqrt(2*delta*pi).*exp(-((X(:,1)-0.5).^2 + (X(:,2)-0.5).^2)./(2*delta));
+% 
+% boundaryEdges = find((G.faces.neighbors(:,1) == 0) + (G.faces.neighbors(:,2) == 0));
+% bNeu = zeros(numel(boundaryEdges),1);
+% for e = 1:numel(boundaryEdges)
+%     X = G.nodes.coords(G.faces.nodes(G.faces.nodePos(e):G.faces.nodePos(e +1)-1));
+%     if X(:,1) < 0.001
+%         bNeu(e) = 1;
+%     end
+% end
+% bc = struct('bcFunc', {{gN,gD}}, 'bcFaces', {{boundaryEdges(bNeu == 1), boundaryEdges(bNeu == 0)}}, 'bcType', {{'neu', 'dir'}});
 
-f = @(X) 1/sqrt(2*delta*pi).*exp(-((X(:,1)-0.5).^2 + (X(:,2)-0.5).^2)./(2*delta));
-
-boundaryEdges = find((G.faces.neighbors(:,1) == 0) + (G.faces.neighbors(:,2) == 0));
-bNeu = zeros(numel(boundaryEdges),1);
-for e = 1:numel(boundaryEdges)
-    X = G.nodes.coords(G.faces.nodes(G.faces.nodePos(e):G.faces.nodePos(e +1)-1));
-    if X(:,1) < 0.001
-        bNeu(e) = 1;
-    end
-end
-bc = struct('bcFunc', {{gN,gD}}, 'bcFaces', {{boundaryEdges(bNeu == 1), boundaryEdges(bNeu == 0)}}, 'bcType', {{'neu', 'dir'}});
-
-% g = @(X) 1/4.*((X(:,1)-0.5).^2 + (X(:,2)-0.5).^2);
+% gD = @(X) 1/4.*((X(:,1)-0.5).^2 + (X(:,2)-0.5).^2);
 % f = @(X) -ones(size(X,1),1);
+% boundaryEdges = find((G.faces.neighbors(:,1) == 0) + (G.faces.neighbors(:,2) == 0));
+% bc = struct('bcFunc', {{gD}}, 'bcFaces', {{boundaryEdges}}, 'bcType', {{'dir'}});
+
 
 % g = @(X) 1/6.*((X(:,1)-0.5).^3 + (X(:,2)-0.5).^3);
 % f = @(X) -(X(:,1)-0.5) - (X(:,2)-0.5);
@@ -34,8 +36,10 @@ bc = struct('bcFunc', {{gN,gD}}, 'bcFaces', {{boundaryEdges(bNeu == 1), boundary
 % g = @(X) X(:,2).*(1-X(:,2)).*X(:,1).^3;
 % f = @(X) -6.*X(:,1).*X(:,2).*(1-X(:,2)) + 2.* X(:,1).^3;
 
-% g = @(X) -log(1./((X(:,1)+0.1).^2 + (X(:,2)+0.1).^2));
-% f = @(X) zeros(size(X,1),1);
+gD = @(X) -log(1./((X(:,1)+0.1).^2 + (X(:,2)+0.1).^2));
+f = @(X) zeros(size(X,1),1);
+boundaryEdges = find((G.faces.neighbors(:,1) == 0) + (G.faces.neighbors(:,2) == 0));
+bc = struct('bcFunc', {{gD}}, 'bcFaces', {{boundaryEdges}}, 'bcType', {{'dir'}});
 
 U = VEM2D(G,f,bc);
 
@@ -49,9 +53,10 @@ for c = 1:Nc
 end
 
 X = [G.nodes.coords ; G.faces.centroids ; baricenters];
-Uexact = g(X);
+Uexact = gD(X);
 
-err = norm(U - Uexact, inf);
+errVec = U - Uexact;
+err = norm(U - Uexact, Inf);
 fprintf('Error: %e', err);
 
 fig1 = figure;
