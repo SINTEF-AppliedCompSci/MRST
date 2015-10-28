d2c8e8274bf5f0e3b2efa0e13cacda4dc9123924
Rebecca Allen <allen.rebecca.d@gmail.com>
2015-10-28 14:05:43 +0100

    New function to add heterogeneous rock properties for grid generation

---
+++ co2lab/utils/atlasgrid/updateWithHeterogenVals.m
@@ -0,0 +1,11 @@
+function [  ] = updateWithHeterogenVals(  )
+% Update deck and petroinfo to include heterogeneous rock properties.
+% This overwrites the petroinfo.avgperm and petroinfo.avgporo with the
+% averaged values of the heterogeneous data.
+
+
+
+
+
+end
+
