da0bc61aa1c2a8b361d2fe61723769915f997542
Rebecca Allen <allen.rebecca.d@gmail.com>
2015-11-04 15:13:15 +0100

    Update to documentation in func that returns formation names

--- co2lab/utils/atlasgrid/getNorwegianSeaNames.m
+++ co2lab/utils/atlasgrid/getNorwegianSeaNames.m
@@ -1,6 +1,9 @@
 function names = getNorwegianSeaNames()
 % Returns the formation names present in the Norwegian Sea.
 %
+% According to the Compiled CO2 Atlas, Not and Ror are sealing formations
+% and are not evaluated as an aquifer for CO2 storage or a large injection
+% potential, respectively.
 
 
 names = {   'Arefm',...
