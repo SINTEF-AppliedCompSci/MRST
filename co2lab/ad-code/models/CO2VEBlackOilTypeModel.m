8f9a53a6f7dace5fb730dbaf810c687591a8c3d5
Odd Andersen <odd.andersen@sintef.no>
2015-11-05 08:41:16 +0100

    Small fix in the update of gas saturations
    Saturations are not allowed to rise above 1-rw, where rw is the residual
    saturation of water.

--- co2lab/ad-code/models/CO2VEBlackOilTypeModel.m
+++ co2lab/ad-code/models/CO2VEBlackOilTypeModel.m
@@ -137,7 +137,7 @@ function [state, report] = updateState(model, state, problem, dx, drivingForces)
                                                 drivingForces);
 
    sg          = state.s(:,2);
-   sg          = min(1, max(0,sg));
+   sg          = min(1-model.fluid.res_water, max(0,sg));
    state.s     = [1-sg, sg];    
    state.sGmax = min(1,state.sGmax);
    state.sGmax = max(0,state.sGmax);
