d9bfeefe2aa26fd6c319a7a1fd2bee6640316249
oystein klemetsdal <oystein.klemetsdal@gmail.com>
2015-11-18 12:24:07 +0100

    Some more on chapter 2. Fixing references.

--- mat/runVEM2D.m
+++ mat/runVEM2D.m
@@ -4,8 +4,8 @@ run('../../matlab/project-mechanics-fractures/mystartup.m')
 
 n = 10;
 nx = n; ny = n;
-G = unitSquare(nx, ny);
-% G = cartGrid([nx,ny],[1,1]);
+% G = unitSquare(nx, ny);
+G = cartGrid([nx,ny],[1,1]);
 G = sortEdges(G);
 G = mrstGridWithFullMappings(G);
 G = computeGeometry(G);
@@ -37,10 +37,35 @@ G = computeGeometry(G);
 % g = @(X) X(:,2).*(1-X(:,2)).*X(:,1).^3;
 % f = @(X) -6.*X(:,1).*X(:,2).*(1-X(:,2)) + 2.* X(:,1).^3;
 
-gD = @(X) -log(1./((X(:,1)+0.1).^2 + (X(:,2)+0.1).^2));
+% gD = @(X) -log(1./((X(:,1)+0.1).^2 + (X(:,2)+0.1).^2));
+% f = @(X) zeros(size(X,1),1);
+% boundaryEdges = find((G.faces.neighbors(:,1) == 0) + (G.faces.neighbors(:,2) == 0));
+% bc = struct('bcFunc', {{gD}}, 'bcFaces', {{boundaryEdges}}, 'bcType', {{'dir'}});
+
+gD = @(X) 1/2.*(X(:,2).^2 - X(:,1).^2);
+gN = @(X) X(:,2);
 f = @(X) zeros(size(X,1),1);
 boundaryEdges = find((G.faces.neighbors(:,1) == 0) + (G.faces.neighbors(:,2) == 0));
-bc = struct('bcFunc', {{gD}}, 'bcFaces', {{boundaryEdges}}, 'bcType', {{'dir'}});
+bNeu = zeros(numel(boundaryEdges),1);
+bDir = zeros(numel(boundaryEdges),1);
+for e = 1:numel(boundaryEdges)
+    X = G.faces.centroids(boundaryEdges(e),:);
+    if X(:,1) < 0 + eps 
+        bDir(e) = 1;
+    end
+    if X(:,1) > 1 - eps
+        bDir(e) = 1;
+    end
+    if X(:,2) < 0 + eps 
+        bNeu(e) = 1;
+    end
+    if X(:,2) > 1-eps
+        bNeu(e) = 1;
+    end
+    
+end
+
+bc = struct('bcFunc', {{gN,gD}}, 'bcFaces', {{boundaryEdges(bNeu == 1), boundaryEdges(bDir == 1)}}, 'bcType', {{'neu', 'dir'}});
 
 U = VEM2D(G,f,bc);
 
