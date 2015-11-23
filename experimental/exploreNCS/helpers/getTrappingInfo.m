ad37082e9295e2a2efbfa7cf7cdf3e2908b6c68a
Rebecca Allen <allen.rebecca.d@gmail.com>
2015-11-23 16:09:56 +0100

    Content of one function put into another function
    Content of getTrappingCapacities_specificTraps() put into
    getTrappingInfo(), thus is removed from repo

--- experimental/exploreNCS/helpers/getTrappingInfo.m
+++ experimental/exploreNCS/helpers/getTrappingInfo.m
@@ -1,9 +1,10 @@
-function [ res ] = getTrappingInfo(name, coarsening, varargin)
+function [ varargout ] = getTrappingInfo(name, coarsening, varargin)
 % Computes structural trapping info and makes plots (optional).
 %
 % SYNOPSIS
 %   res = getTrappingInfo('Stofm',3)
 %   res = getTrappingInfo('Stofm',3, 'plotsOn',false)
+%   res = getTrappingInfo('Stofm',3, 'cells',trapcells)
 %
 % DESCRIPTION
 %   Parameter values (seafloor_depth, etc.) are obtained using
@@ -19,11 +20,16 @@ function [ res ] = getTrappingInfo(name, coarsening, varargin)
 %       Bulk rock volume * NTG = Net rock volume
 %       Net rock volume * poro = volume which fluid can occupy
 %
+%   If varargin cells is specified, trapping info is returned for those
+%   specific cells. If not specified, default is cells = 1:Gt.cells.num
+%
 % See also
 %   exploreCapacity.m
     
 
-    opt.plotsOn = true;
+    opt.plotsOn     = true;
+    opt.cells       = [];
+    opt.trapName    = {'default'};
     opt = merge_options(opt, varargin{:});
     
     
@@ -32,6 +38,7 @@ function [ res ] = getTrappingInfo(name, coarsening, varargin)
     
     
     %% Get formation grid and info
+    % entire formation grid
     [var.Gt, var.rock2D] = getFormationTopGrid(name, coarsening);
     var.ta               = trapAnalysis(var.Gt,'false');
     var.co2              = CO2props('sharp_phase_boundary', true, ...
@@ -39,10 +46,12 @@ function [ res ] = getTrappingInfo(name, coarsening, varargin)
     info                 = getSeaInfo(name, rhoCref);
     
     
+    
+    
     %% make plots (optional) and get trapping capacity results (res)
     draw();
     
-    res.Gt      = var.Gt;
+    res.Gt      = var.Gt;       
     res.rock2D  = var.rock2D;
     res.ta      = var.ta;
 
@@ -51,7 +60,24 @@ function [ res ] = getTrappingInfo(name, coarsening, varargin)
     res.caprock_pressure    = compute_pressure();               % Pascals
     res.caprock_temperature = compute_temperature() + 273.15;   % Kelvin
     
+    
+    %% trapping info for specific cells
+    % 'cells' is intended to belong to the same structural trap (whole or
+    % portion of trap)
+    if ~isempty(opt.cells)
+        assert( unique(var.ta.traps(opt.cells)) ~= 0 , ...
+            'Cells are not part of a structural trap.')
+        assert( numel( unique(var.ta.traps(opt.cells)) ) == 1 , ...
+            'Cells contain more than one unique trap ID.')
+        
+        [output] = compute_trapcap(opt.cells, opt.trapName);
+    end
 
+    %% function returns:
+    varargout{1} = res;
+    if exist('output','var')
+       varargout{2} = output;
+    end
 
     % ---------------------------------------------------------------------
     % ------------------------- Helper Functions --------------------------
@@ -278,6 +304,130 @@ function [ res ] = getTrappingInfo(name, coarsening, varargin)
         end
 
         %plotGrid(Gt);
+
+    end
+
+    % ---------------------------------------------------------------------
+
+    function [output, H1, strap, btrap_res, btrap_dis] = compute_trapcap(cells, trapName)
+    % computes trapping capacity for specific grid cells
+    % 'cells' is intended to belong to the same structural trap
+
+        %assert()
+
+        p = compute_pressure();
+        t = compute_temperature() + 273.15; % Kelvin
+
+        Gt   = var.Gt;
+        ta   = var.ta;
+        poro = var.rock2D.poro;
+        perm = var.rock2D.perm;
+        if isfield(var.rock2D,'ntg')
+            ntg = var.rock2D.ntg;
+            fprintf(['\n Note: net-to-gross rock property exists.',...
+               ' Net rock volume is (on avg) %d percent of bulk rock volume. \n'], mean(ntg)*100);
+        else
+            ntg = ones(numel(poro),1);
+        end
+
+        % computing structural trap heights (H1) for each cell
+        trapcells     = cells;
+        H1            = zeros(Gt.cells.num, 1);
+        if ~isempty(trapcells)
+         H1(trapcells) = ta.trap_z(ta.traps(trapcells)) - Gt.cells.z(trapcells);
+        end
+        H1=min(H1,Gt.cells.H);
+        H2 = Gt.cells.H - H1;
+        assert(all(H1<=Gt.cells.H));
+
+        % Computing total trapping volume in structural traps (dissolved
+        % and structurally trapped
+        strap_pvol_tot       = Gt.cells.volumes .* H1 .* poro .* ntg;
+        strap_pvol_co2_plume = strap_pvol_tot .* (1 - info.res_sat_wat);
+        strap_pvol_co2_diss  = strap_pvol_tot .* info.res_sat_wat .* info.dis_max;
+
+        strap = strap_pvol_co2_plume .* var.co2.rho(p,t) + ...
+              strap_pvol_co2_diss  .* rhoCref;
+        %fprintf('\nAverage strap is %d kg.\n', mean(strap))
+
+        % Computing total trapping volume below structural traps (dissolved
+        % and residually trapped
+        btrap_pvol_tot          = Gt.cells.volumes .* H2 .* poro .* ntg;
+        btrap_pvol_co2_residual = btrap_pvol_tot .* info.res_sat_co2;
+        btrap_pvol_co2_dissol   = btrap_pvol_tot .* (1-info.res_sat_co2) .* info.dis_max;
+
+        btrap_res = btrap_pvol_co2_residual .* var.co2.rho(p,t);
+        btrap_dis = btrap_pvol_co2_dissol   .* rhoCref;
+
+
+        % Other info about The Structural Trap that might be useful for
+        % comparions later:
+        output.numtrapcells     = numel(cells);
+        output.rockVol_m3       = sum(Gt.cells.volumes .* H1);
+        output.netVol_m3        = sum(Gt.cells.volumes .* H1 .* ntg);
+        output.poreVol_m3       = sum(Gt.cells.volumes .* H1 .* ntg .* poro);
+                                % cubic meters, total in structural trap
+
+        % av ntg, poro, perm, and their ranges:
+        output.ntg_min = min(ntg(trapcells));
+        output.ntg_max = max(ntg(trapcells));
+        output.ntg_av  = mean(ntg(trapcells));
+
+        output.perm_min = min(perm(trapcells));
+        output.perm_max = max(perm(trapcells));
+        output.perm_av  = mean(perm(trapcells));
+
+        output.poro_min = min(poro(trapcells));
+        output.poro_max = max(poro(trapcells));
+        output.poro_av  = mean(poro(trapcells));
+
+        % min/max depth of structural trap (not formation), and average:
+        output.depth_min = min(Gt.cells.z(trapcells));
+        output.depth_max = max(Gt.cells.z(trapcells));
+        output.depth_av  = mean(Gt.cells.z(trapcells));
+
+
+        % storage capacity in structure, in volume (m3), and in mass (kg,
+        % Mt) assuming 100% of pore volume is capable of storing CO2 (no
+        % storage efficiency factor used here):
+        output.storageCap_m3 = output.poreVol_m3;
+        output.storageCap_kg = sum( Gt.cells.volumes(trapcells) ...
+            .* H1(trapcells) .* ntg(trapcells) .* poro(trapcells) ...
+            .* var.co2.rho(p(trapcells),t(trapcells)) ); % m3 * kg/m3 = kg
+        output.storageCap_Mt = output.storageCap_kg / 1e9; % 10^9 kg = 1 Mt
+
+
+        % corresponding storage efficiency in structural trap:
+        output.Seff = output.storageCap_m3 / output.poreVol_m3;
+
+
+        output.T = table(  [output.numtrapcells;        ...
+                    output.rockVol_m3;          ...
+                    output.netVol_m3;           ...
+                    output.poreVol_m3;          ...
+                    output.ntg_min;             ...
+                    output.ntg_max;             ...
+                    output.ntg_av;              ...
+                    output.perm_min;            ...
+                    output.perm_max;            ...
+                    output.perm_av;             ...
+                    output.poro_min;            ...
+                    output.poro_max;            ...
+                    output.poro_av;            ...
+                    output.depth_min;           ...
+                    output.depth_max;           ...
+                    output.depth_av;            ...
+                    output.storageCap_Mt],       ...
+            'RowNames', {'NumTrapCells' ...
+            'RockVolm3' 'NetVolm3' 'PoreVolm3' ...
+            'ntgmin' 'ntgmax' 'ntgav' ...
+            'permmin' 'permmax' 'permav' ...
+            'poromin' 'poromax' 'poroav' ...
+            'depthmin' 'depthmax' 'depthav' ...
+            'StorageCapacityMt'}, ...
+            'VariableNames', trapName  );
+
+        output.trapName = trapName;
 
     end
 
