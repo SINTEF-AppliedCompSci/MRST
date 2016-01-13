caa676fbab82a8a250a9a56a2d3c1a7d783c18c4
Rebecca Allen <allen.rebecca.d@gmail.com>
2016-01-13 16:00:54 +0100

    Bug-fix in makeReports_extras: use qGs not qGr
    for mass balance, the masses must be computed using the correct
    injection rate, that is the surface rate.
    
    Note: wellSols.qGs is used instead of W.val since wells might be
    pressure controlled, in which case W.val is bhp, not an injection
    rate.

--- experimental/optimization_part2/helpers/makeReports_extras.m
+++ experimental/optimization_part2/helpers/makeReports_extras.m
@@ -19,8 +19,14 @@ function reports = makeReports_extras(Gt, states, rock, fluid, schedule, ...
          reports(i).W = schedule.control(schedule.step.control(i-1)).W;
          
          %assert(all(cellfun(@(x) strcmpi(x, 'rate'), {reports(i).W.type})));
-         tot_inj = tot_inj + sum([wellSols{i-1}.qGr]) * schedule.step.val(i-1) * fluid.rhoGS;
          %tot_inj = tot_inj + sum([reports(i).W.val]) * schedule.step.val(i-1) * fluid.rhoGS;
+         
+         % since bhp-controlled wells may have been used, we cannot rely on
+         % using W.val for the rate. Thus, we use the SURFACE RATE found in
+         % the wellSols structure.
+         % NB: for rate-controlled wells, wellSols.val == wellSols.qGs, not wellSols.qGr !!!
+         tot_inj = tot_inj + sum([wellSols{i-1}.qGs]) * schedule.step.val(i-1) * fluid.rhoGS;
+         
       end
       
       reports(i).sol       = states{i};
@@ -35,7 +41,11 @@ function reports = makeReports_extras(Gt, states, rock, fluid, schedule, ...
                                                         dh);
       leaked = tot_inj - sum(reports(i).masses);
       reports(i).masses = [reports(i).masses, leaked];
+
    end
+   
+   % hack @@ put well cell indexes in reports(1) using reports(2)
+      reports(1).W = reports(2).W;
    
 end
 
