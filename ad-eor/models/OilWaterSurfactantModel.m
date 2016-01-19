7d144f41e203a5cf7937551fd8d55c3cd93fcb21
Xavier Raynaud <xavier.raynaud@sintef.no>
2016-01-19 14:58:33 +0100

    Fixes in velocity computations.

--- ad-eor/models/OilWaterSurfactantModel.m
+++ ad-eor/models/OilWaterSurfactantModel.m
@@ -2,62 +2,62 @@ classdef OilWaterSurfactantModel < TwoPhaseOilWaterModel
 % Oil/water/Surfactant system
 % This model is a two phase oil/water model, extended with the surfactant
 % component in addition.
-   
+
    properties
       % Surfactant present
       surfactant
-      
+
    end
-   
+
    methods
       function model = OilWaterSurfactantModel(G, rock, fluid, varargin)
-         
+
          model = model@TwoPhaseOilWaterModel(G, rock, fluid);
-         
+
          % This is the model parameters for oil/water/surfactant
          model.surfactant = true;
-         
+
          model.wellVarNames = {'qWs', 'qOs', 'qWSft', 'bhp'};
-         
+
          model = merge_options(model, varargin{:});
-         
+
+         model = model.setupOperators(G, rock, varargin{:});
+
       end
-      
+
       function [problem, state] = getEquations(model, state0, state, ...
                                                       dt, drivingForces, varargin)
          [problem, state] = equationsOilWaterSurfactant(state0, state, ...
                                                         model, dt, drivingForces, varargin{:});
       end
-      
+
       function [state, report] = updateState(model, state, problem, ...
                                              dx, drivingForces)
          [state, report] = updateState@TwoPhaseOilWaterModel(model, ...
                                                            state, problem,  dx, drivingForces);
-         
+
          if model.surfactant
             c = model.getProp(state, 'surfactant');
             state = model.setProp(state, 'surfactant', max(c, 0) );
          end
       end
-      
+
       function model = setupOperators(model, G, rock, varargin)
-      % Set up divergence/gradient/transmissibility operators
-         operators = model.setupOperators(G, rock, varargin{:});
-         operators.veloc = computeVelocTPFA(G, rock, 'operators', operators);
+         model.operators.veloc = computeVelocTPFA(G, rock, 'operators', model.operators);
       end
 
       function varargout = evaluateRelPerm(model, sat, varargin)
          error('function evaluateRelPerm is not implemented for surfactant model')
       end
-      
-      
+
+
       function [state, report] = updateAfterConvergence(model, state0, state, dt, drivingForces)
          [state, report] = updateAfterConvergence@TwoPhaseOilWaterModel(model, state0, state, ...
                                                            dt, drivingForces);
-         % Nothing extra to be done here for the surfactant case. 
+         % Nothing extra to be done here for the surfactant case.
       end
 
-      
+
       function [fn, index] = getVariableField(model, name)
       % Get the index/name mapping for the model (such as where
       % pressure or water saturation is located in state)
@@ -70,7 +70,7 @@ classdef OilWaterSurfactantModel < TwoPhaseOilWaterModel
                 model, name);
          end
       end
-      
+
       function scaling = getScalingFactorsCPR(model, problem, names)
          nNames = numel(names);
 
--- ad-eor/models/computeVelocTPFA.m
+++ ad-eor/models/computeVelocTPFA.m
@@ -3,9 +3,12 @@ function veloc = computeVelocTPFA(G, rock, varargin)
    opt = struct('verbose', false, 'operators', [] );
    opt = merge_options(opt, varargin{:});
 
-   if isempty(operator)
-      operator = setupOperatorsTPFA(G, rock);
+   if isempty(opt.operators)
+      operators = setupOperatorsTPFA(G, rock);
+   else
+      operators = opt.operators;
    end
+
    T = operators.T;
    N = operators.N;
    intInx = operators.internalConn;
@@ -19,16 +22,20 @@ function veloc = computeVelocTPFA(G, rock, varargin)
    cfn    = bsxfun(@times, G.faces.normals(G.cells.faces(:,1), :), sgn);
    dim    = size(G.nodes.coords, 2);
    [K, r, c] = permTensor(rock, dim);
-   Kn = zeros(size(cellNo, 1), dim);
    Kn = K(cellNo,:).*cfn(:, c);
-   Kn = reshape(Kn',3, []);
+   Kn = reshape(Kn', dim, []);
    Kn = sum(Kn);
-   Kn = reshape(Kn, 3, [])';
-   Kn = Kn(intInx);
+   Kn = reshape(Kn, dim, [])';
+   Kn = Kn(intInx, :);
    Af = G.faces.areas(G.cells.faces(:, 1));
-   vol = G.cells.volume(CellNo);
-   wKn = 1./vol(intInx).*bsxfun(@times, Kn, Af./T);
-   sumFaceOper = sparse(cellNo(intInx), 1:numel(intInx), 1, G.cells.num, numel(intInx));
-   veloc = @(v)(sumFaceOper*bsxfun(wKn, v)); 
-   
+   Af = Af(intInx);
+   vol = G.cells.volumes(cellNo);
+   wKn = bsxfun(@times, Kn, 1./vol(intInx).*Af./T);
+   sumFaceOper = sparse(cellNo(intInx), 1 : nnz(intInx), 1, G.cells.num, nnz(intInx));
+   dim = G.griddim;
+   veloc = cell(dim, 1);
+   for i = 1 : dim
+      veloc{i} = @(v)(sumFaceOper*(wKn(:, i).*v));
+   end
+
 end
