8e4bac8b954a334b612be4de1383470f719b6ccf
Xavier Raynaud <xavier.raynaud@sintef.no>
2016-01-19 15:17:18 +0100

    Fixed in computation of surfactant relperm.

--- ad-eor/utils/computeCapillaryNumber.m
+++ ad-eor/utils/computeCapillaryNumber.m
@@ -2,8 +2,12 @@ function Nc = computeCapillaryNumber(p, c, fluid, operators)
 
    s = operators;
    v = -s.T.*s.Grad(p);
-   veloc = s.veloc(v);
-   abs_veloc = (sum(veloc.^2, 2)).^(1/2);
+   veloc = s.veloc;
+   abs_veloc = 0;
+   for i = 1 : numel(veloc)
+      abs_veloc = abs_veloc + veloc{i}(v).^2;
+   end
+   abs_veloc = (abs_veloc).^(1/2);
    sigma = fluid.ift(c);
    Nc = abs_veloc./sigma;
 
--- ad-eor/utils/computeRelPermSft.m
+++ ad-eor/utils/computeRelPermSft.m
@@ -1,22 +1,23 @@
-function [krW, krO] = computeRelPermSft(fluid, sW, Nc)
+function [krW, krO] = computeRelPermSft(sW, Nc, fluid)
 
-   m = fluid.surfcapd(Nc);
+   logNc = log(Nc)/log(10);
+   % We cap logNc (as done in Eclipse)
+   logNc = min(max(-20, logNc), 20);
+   m = fluid.miscfact(logNc);
 
-   sWcon     = fluid.sWcon;
-   sOres     = fluid.sOres;
-   sWconSurf = fluid.sWconSurf;
-   sOresSurf = fluid.sOresSurf;
+   sWcon    = fluid.sWcon;
+   sOres    = fluid.sOres;
+   sWconSft = fluid.sWconSft;
+   sOresSft = fluid.sOresSft;
 
-   sNcWcon = m.*sWcon + (1 - m).*sWconSurf;
-   sNcOres = m.*sOres + (1 - m).*sOresSurf;
+   sNcWcon = m.*sWcon + (1 - m).*sWconSft;
+   sNcOres = m.*sOres + (1 - m).*sOresSft;
 
    sNcWnosurf = ((1 - sNcWcon - sNcOres)./(1 - sWcon - sOres)).*(sW - sWcon) + sNcWcon;
-   krNcWnosurf = fluid.krW(sNcWnosurf);
-   krNcOnosurf = fluid.krO(sNcWnosurf);
+   [krNcWnosurf, krNcOnosurf] = fluid.relPerm(sNcWnosurf);
 
-   sNcWsurf = ((1 - sNcWcon - sNcOres)./(1 - sWconSurf - sOresSurf)).*(sW - sWconSurf) + sNcWcon;
-   krNcWsurf = fluid.krW(sNcWsurf);
-   krNcOsurf = fluid.krO(sNcWsurf);
+   sNcWsurf = ((1 - sNcWcon - sNcOres)./(1 - sWconSft - sOresSft)).*(sW - sWconSft) + sNcWcon;
+   [krNcWsurf, krNcOsurf] = fluid.relPermSft(sNcWsurf);
 
    krW = m.*krNcWnosurf + (1 - m).*krNcWsurf;
    krO = m.*krNcOnosurf + (1 - m).*krNcOsurf;
