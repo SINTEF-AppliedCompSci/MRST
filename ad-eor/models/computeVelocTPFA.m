85e35443f231d7cd50bd2474ca55c949bc1660c4
Xavier Raynaud <xavier.raynaud@sintef.no>
2016-01-21 14:44:33 +0100

    fixed computeVelocTPFA

--- ad-eor/models/computeVelocTPFA.m
+++ ad-eor/models/computeVelocTPFA.m
@@ -1,5 +1,8 @@
 function veloc = computeVelocTPFA(G, rock, varargin)
 
+% Compute approximation of velocity for TPFA
+% Note: The approximation method has flaws at well cells.
+
    opt = struct('verbose', false, 'operators', [] );
    opt = merge_options(opt, varargin{:});
 
@@ -14,28 +17,32 @@ function veloc = computeVelocTPFA(G, rock, varargin)
    intInx = operators.internalConn;
    K = rock.perm;
 
-   % We compute value of product K by outer normal n on each half-faces. Obtain array of
-   % dimension intInx-by-dim
+   % We compute value of product K by outer normal n on each half-faces.
    cellNo = rldecode(1 : G.cells.num, diff(G.cells.facePos), 2).';
-   sgn    = 2*double(G.faces.neighbors(G.cells.faces(:,1), 1) == cellNo) - 1;
-   % cfn  = cell-face normal = *outward* face normals on each cell.
-   cfn    = bsxfun(@times, G.faces.normals(G.cells.faces(:,1), :), sgn);
+   cfn    = G.faces.normals(G.cells.faces(:,1), :);
    dim    = size(G.nodes.coords, 2);
    [K, r, c] = permTensor(rock, dim);
-   Kn = K(cellNo,:).*cfn(:, c);
+   Kn = K(cellNo, :).*cfn(:, c);
    Kn = reshape(Kn', dim, []);
    Kn = sum(Kn);
    Kn = reshape(Kn, dim, [])';
-   Kn = Kn(intInx, :);
-   Af = G.faces.areas(G.cells.faces(:, 1));
-   Af = Af(intInx);
    vol = G.cells.volumes(cellNo);
-   wKn = bsxfun(@times, Kn, 1./vol(intInx).*Af./T);
-   sumFaceOper = sparse(cellNo(intInx), 1 : nnz(intInx), 1, G.cells.num, nnz(intInx));
-   dim = G.griddim;
+
+   % Define mapping from internal faces to half faces.
+   nhf = size(G.cells.faces, 1); % number of half faces
+   nf  = G.faces.num;            % number of faces
+   nif = nnz(intInx);            % number of internal faces
+   nc  = G.cells.num;            % number of cells
+   fromIntfacesToFaces = sparse(find(intInx), 1 : nif, 1, nf, nif);
+   fromFacesToHalffaces = sparse(1 : nhf, G.cells.faces(:, 1), 1, nhf, nf);
+   fromIntfacesToHalffaces = fromFacesToHalffaces*fromIntfacesToFaces;
+
+   hfinvT = fromIntfacesToHalffaces*(1./T);
+   wKn = bsxfun(@times, Kn, hfinvT./vol);
+   sumHalffaces = sparse(cellNo, 1 : nhf, 1, nc, nhf);
    veloc = cell(dim, 1);
    for i = 1 : dim
-      veloc{i} = @(v)(sumFaceOper*(wKn(:, i).*v));
+      veloc{i} = @(v)(sumHalffaces*(wKn(:, i).*(fromIntfacesToHalffaces*v)));
    end
 
 end
