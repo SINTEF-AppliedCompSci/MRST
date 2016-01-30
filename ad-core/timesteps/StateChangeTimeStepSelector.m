cb0455ff07be8bcc37883ed2cc87f5bd334b4da4
Olav Møyner <olav.moyner@sintef.no>
2016-01-29 20:09:06 -0800

    Better output from step selector

--- ad-core/timesteps/StateChangeTimeStepSelector.m
+++ ad-core/timesteps/StateChangeTimeStepSelector.m
@@ -44,8 +44,9 @@ classdef StateChangeTimeStepSelector < IterationCountTimeStepSelector
                     prev = {prev};
                 end
                 
-                assert(numel(curr) == numel(prev), ['Mismatch in property size: ', fn]);
-                for jj = 1:numel(curr)
+                nvals = numel(curr);
+                assert(nvals == numel(prev), ['Mismatch in property size: ', fn]);
+                for jj = 1:nvals
                     c = curr{jj};
                     p = prev{jj};
                     if iscell(c)
@@ -56,6 +57,11 @@ classdef StateChangeTimeStepSelector < IterationCountTimeStepSelector
                         p = [p{:}];
                     end
                     
+                    if nvals == 1
+                        sname = '';
+                    else
+                        sname = [' (', num2str(jj), ')'];
+                    end
                     changeAbs = abs(c - p);
                     changeRel = changeAbs./abs(p);
                     changeRel(isnan(changeRel)) = 0;
@@ -73,8 +79,8 @@ classdef StateChangeTimeStepSelector < IterationCountTimeStepSelector
                     f_rel(isnan(f_rel)) = inf;
                     
                     dt_next = min([dt_next, dt_prev*f_rel, dt_prev*f_abs]);
-                    dispif(selector.verbose, 'Property ''%s'': Rel change %1.2f, abs change: %1.2f -> adjustment of %1.2f\n', ...
-                        fn, maxRel, maxAbs, min(f_rel, f_abs));
+                    dispif(selector.verbose, 'Property ''%s%s'': Rel change %1.2f [%1.2f], abs change: %1.2f [%1.2f]-> adjustment of %1.2f\n', ...
+                        fn, sname, maxRel, targetRel, maxAbs, targetAbs, min(f_rel, f_abs));
                 end
                 dt = min(dt, dt_next);
             end
