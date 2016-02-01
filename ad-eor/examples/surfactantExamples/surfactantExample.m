f8ed90938607161237063b956dc05d2cfce509d9
Xavier Raynaud <xavier.raynaud@sintef.no>
2016-02-01 15:47:54 +0100

    set same input values as in Jørgensen MSc thesis.

--- ad-eor/examples/surfactantExamples/surfactantExample.m
+++ ad-eor/examples/surfactantExamples/surfactantExample.m
@@ -50,7 +50,7 @@ switch simul_case
   case '1D'
 
     nc = G.cells.num;
-    state0 = initResSol(G, 300*barsa, [ .1, .9]);
+    state0 = initResSol(G, 300*barsa, [ .2, .8]);
 
     % Add zero surfactant concentration to the state.
     state0.c    = zeros(G.cells.num, 1);
