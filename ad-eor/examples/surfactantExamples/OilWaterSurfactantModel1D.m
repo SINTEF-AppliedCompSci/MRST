366ec82a832f8e6510e6616dffdee4c80f0da6b4
Xavier Raynaud <xavier.raynaud@sintef.no>
2016-02-10 15:12:21 +0100

    Minor changes in OilWaterSurfactantModel1D

--- ad-eor/examples/surfactantExamples/OilWaterSurfactantModel1D.m
+++ ad-eor/examples/surfactantExamples/OilWaterSurfactantModel1D.m
@@ -6,23 +6,47 @@ classdef OilWaterSurfactantModel1D < OilWaterSurfactantModel
       end
 
       function [state, report] = updateAfterConvergence(model, state0, state, dt, drivingForces)
-         [state, report] = updateAfterConvergence@OilWaterSurfactantModel(model, state0, state, ...
-                                                           dt, drivingForces);
-         try
-            set(0, 'CurrentFigure', 1);
-         catch
-            figure(1);
-         end
+         [state, report] = updateAfterConvergence@OilWaterSurfactantModel(model, state0, ...
+                                                           state, dt, drivingForces);
+
+         setFigure(1)
+         clf
          subplot(2, 1, 1)
-         plot(state.s(:, 1));
+         plot(state.s(:, 1), '*-');
          axis([0, 100, 0, 1]);
          title('Saturation');
          subplot(2, 1, 2)
-         plot(state.c);
-         axis([0, 100, 0, 50]);
+         plot(state.c, '*-');
+         % axis([0, 100, 0, 50]);
          title('Concentration');
          drawnow
 
+         setFigure(2)
+         clf
+         plot(state.c, '*-');
+         axis([0, 100, 0, 51]);
+         title('Concentration');
+         drawnow
+
+         setFigure(3)
+         clf
+         plot(state.ads, '*-');
+         title('SURFADS');
+         drawnow
+
+         % setFigure(4)
+         % clf
+         % plot(state.SURFCNM, '*-');
+         % title('SURFCNM');
+         % drawnow
+
+         % setFigure(5)
+         % clf
+         % plot(state.SURFST, '*-');
+         % title('SURFST');
+         % drawnow
+
+         % waitforbuttonpress;
       end
    end
 end
