3cd907d90ab973e39e9f6d83b9ed5e40948e0670
Xavier Raynaud <xavier.raynaud@sintef.no>
2016-02-10 15:14:44 +0100

    Fixed updateExplicitAds

--- ad-eor/utils/updateExplicitAds.m
+++ ad-eor/utils/updateExplicitAds.m
@@ -1,59 +1,51 @@
-function state = updateExplicitAds(state0, state, model, dt)
+function state = updateExplicitAds(state, model, varargin)
 % Explicit update of the concentration for the adsorption term
-% It is important
+% We update the surfactant concentration ensuring mass conservation
+
+   opt = struct('desorptionThreshold', -inf);
+   opt = merge_options(opt, varargin{:});
 
    s = model.operators; % shortcut
    fluid = model.fluid; % shortcut
 
+   [p, sW, c, cmax, ads, adsmax ] = model.getProps(state, 'pressure', 'water', 'surfactant', ...
+                                                          'surfactantmax', 'ads', 'adsmax');
 
-   [p, sW, c ] = model.getProps(state, 'pressure', 'water', 'surfactant', 'surfactantmax');
-
-   [p0, cmax0, ads0, adsmax0] = model.getProps(state0, 'pressure', 'surfactantmax', 'ads', ...
-                                                       'adsmax');
-
-   ads = fluid.surfads(c);
-   if fluid.adsInxSft == 2
-      ads = max(ads, adsmax0);
-   end
-
+   new_ads = computeEffAds(c, opt.desorptionThreshold, fluid);
 
    % Multipliers for properties
-   [pvMult, transMult, mobMult, pvMult0] = getMultipliers(model.fluid, p, p0);
+   [pvMult, ~, ~] = getMultipliers(model.fluid, p, p);
    bW   = fluid.bW(p);
    poro = model.rock.poro;
-   ads_term = fluid.rhoRSft.*((1-poro)./poro).*(ads - ads0);
+
    % Solve the equation
    %
-   %   (s.pv/dt).*(pvMult.*bW.*sW.*(new_c - c) + ads_term )= 0
+   %   (s.pv/dt).*(pvMult.*bW.*sW.*(new_c - c) + fluid.rhoRSft.*((1-poro)./poro).*(new_ads - ads))= 0
    %
    % We remove values where sW is zero. At those points, the concentration is not defined and we
    % let it unchanged.
    %
    ind = sW > 1e-12;
-   coef = 1./(pvMult.*bW.*sW);
-   coef(~ind) = 0;
-   newc  = c - coef.*ads_term;
+   coef1 = fluid.rhoRSft.*((1-poro)./poro);
+   coef2 = pvMult.*bW.*sW;
+   coef = coef1./coef2;
+   coef(~ind) = 0; % if sW=0, concentration is in fact not well-defined and we let it unchanged.
+   new_c  = c - coef.*(new_ads - ads);
 
-   negc = newc < 0;
+   negc = new_c < 0;
    % For the terms where the concentration becomes negative, we set the adsorption term so that
    % surfactant mass conservation is ensured, that is
    %
-   %   (s.pv/dt).*(pvMult.*bW.*sW.*(0 - c) + fluid.rhoRSft.*((1-poro)./poro).*(ads - ads0) )= 0
+   %   (s.pv/dt).*(pvMult.*bW.*sW.*(0 - c) + fluid.rhoRSft.*((1-poro)./poro).*(new_ads - ads) )= 0
    %
    if any(negc)
-      coef1 = fluid.rhoRSft.*((1-poro)./poro);
-      coef2 = pvMult.*bW.*sW;
-      ads(negc) = ads0(negc) + coef2(negc)./coef1(negc).*c(negc);
-      c = max(0, newc);
-   else
-      c = newc;
+      new_ads(negc) = ads(negc) + coef2(negc)./coef1(negc).*c(negc);
+      new_c = max(0, new_c);
    end
 
-   state = model.setProp(state, 'ads', ads);
-   adsmax = max(ads, adsmax0);
-   state = model.setProp(state, 'adsmax', adsmax);
-   state = model.setProp(state, 'surfactant', max(c, 0));
-   state = model.setProp(state, 'surfactantmax', max(c, cmax0));
-
+   state = model.setProp(state, 'ads', new_ads);
+   state = model.setProp(state, 'adsmax', max(new_ads, adsmax));
+   state = model.setProp(state, 'surfactant', max(new_c, 0));
+   state = model.setProp(state, 'surfactantmax', max(new_c, cmax));
 
 end
