cccfd192b571e024cd653d78cd6ed983949764f5
Xavier Raynaud <xav.raynaud@gmail.com>
2016-03-01 08:55:29 +0100

    removed special treatment for adsorption.
    when c=0.

--- ad-eor/utils/computeEffAds.m
+++ ad-eor/utils/computeEffAds.m
@@ -1,14 +1,7 @@
 function ads = computeEffAds(c, adsmax, fluid)
 % Compute effective adsorption, depending of desorption is present or not
 
-% Treat differently case where no surfactant is present.  Physically, it does not make sense not to
-% have fluid.surfads(0) = 0 and the input data should satisfied this identity However, we treat
-% separatly the case c = 0 because, usually, adsorption increases very rapidly and this can create
-% unnecessary convergence problem in the case where there is no surfactant.
-
-   isSft = (double(c) > 0);
-   ads = 0*c;
-   ads(isSft) = fluid.surfads(c(isSft));
+   ads = fluid.surfads(c);
    if fluid.adsInxSft == 2
       ads = max(ads, adsmax);
    end
