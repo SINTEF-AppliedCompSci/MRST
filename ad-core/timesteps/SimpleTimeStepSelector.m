2e9c49cd8d29289a5e3587faf68e17614206ce77
Olav Møyner <olav.moyner@sintef.no>
2016-04-15 13:40:16 +0200

    Ensure that reset actually sets all flags to default values.

--- ad-core/timesteps/SimpleTimeStepSelector.m
+++ ad-core/timesteps/SimpleTimeStepSelector.m
@@ -109,15 +109,16 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
             assert(selector.firstRampupStepRelative <= 1 && ...
                    selector.firstRampupStepRelative >  0);
             
-            selector.isStartOfCtrlStep = true;
-            selector.isFirstStep       = true;
-            selector.controlsChanged = true;
-            selector.stepLimitedByHardLimits = true;
+            selector.reset();
         end
         
         function reset(selector)
             selector.history = [];
             selector.previousControl = [];
+            selector.isStartOfCtrlStep = true;
+            selector.isFirstStep       = true;
+            selector.controlsChanged   = true;
+            selector.stepLimitedByHardLimits = true;
         end
         
         function storeTimestep(selector, report)
