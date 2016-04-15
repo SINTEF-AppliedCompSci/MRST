862fde84371fde7f229fe2fe204baf80ca4ab2ef
Olav Møyner <olav.moyner@sintef.no>
2016-04-15 14:25:06 +0200

    Add optional argument to formatTimeRange.
    By sending in a second argument, strings will be limited to only
    significant values, by turning:
    "10 Years, 54 Days, 1 Hour, 2031 Seconds"
    into
    "31710 Years, 54.07 Days"
    when two significant units are specified.

--- utils/formatTimeRange.m
+++ utils/formatTimeRange.m
@@ -1,10 +1,15 @@
-function s = formatTimeRange(time)
+function s = formatTimeRange(time, limit)
 % Small utility which returns a human readable string from seconds.
+    if nargin < 2
+        % Second argument can be a limit of time units to output
+        limit = inf;
+    end
     s = '';
 
     timesys = {year, day, hour, second, second/1000};
     timen = {'Year', 'Day', 'Hour', 'Second', 'Millisecond'};
     added = false;
+    count = 0;
     for i = 1:numel(timesys)
         a = floor(time/timesys{i});
         if a > 0,
@@ -13,11 +18,18 @@ function s = formatTimeRange(time)
             else
                 space = '';
             end
+            count = count + added;
 
             plural = '';
             if a ~= 1, plural = 's'; end
-
-            s = [s, sprintf([space, '%d ', timen{i}, plural], a)];     %#ok
+            if count == limit
+                % Max depth reached, just print decimal
+                s = [s, sprintf([space, '%1.2f ', timen{i}, plural], ...
+                                                    time/timesys{i})]; %#ok
+                break
+            else
+                s = [s, sprintf([space, '%d ', timen{i}, plural], a)]; %#ok
+            end
 
             time  = time - a*timesys{i};
             added = true;
