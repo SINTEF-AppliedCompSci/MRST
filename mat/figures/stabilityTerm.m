470f2a4b8729d37836adbf684520878b40d44470
strene <oystein.klemetsdal@gmail.com>
2016-06-09 16:14:13 +0200

    Proofread until section 'FEm for quadrilaterals and hexahedra'.

--- mat/figures/stabilityTerm.m
+++ mat/figures/stabilityTerm.m
@@ -113,8 +113,20 @@ dest = '../../tex/thesis/fig/';
 
 %%
 
-gDl2 = sqrt(sum(polygonInt(G, 1:G.cells.num, @(X) gD(X).*gD(X), 7)));
-gD2 = norm(uChi,2);
+%%
+X = G.nodes.coords(G.cells.nodes(G.cells.nodePos(1):G.cells.nodePos(2)-1),:);
+hx = abs(max(X(:,1))-min(X(:,1)));
+hy = abs(max(X(:,2))-min(X(:,2)));
+Q = sqrt(9/(4*hx*hy))*[-1,1,-1,1]';
+
+r= hy;
+trustCells = find(sum(G.cells.centroids.^2,2) > r^2);
+trustNodes = unique(G.cells.nodes(mcolon(G.cells.nodePos(trustCells),G.cells.nodePos(trustCells+1)-1)));
+
+%%
+
+gDl2 = sqrt(sum(polygonInt(G, trustCells, @(X) gD(X).*gD(X), 7)));
+gD2 = norm(uChi(trustNodes),2);
 
 l2err1 = sqrt(sum(l2Error(G, sol1, gD, 1)))/gDl2;
 err1 = norm((sol1.nodeValues-uChi),2)/gD2;
@@ -137,48 +149,49 @@ fprintf('Error with 2norm stability term: %f, %f, %f \n', err3, l2err3, err3+l2e
 fprintf('Error with FEM stability term: %f, %f, %f \n', err4, l2err4, err4+l2err4);
 
 %%
+X = G.nodes.coords(G.cells.nodes(G.cells.nodePos(1):G.cells.nodePos(2)-1),:);
+hx = abs(max(X(:,1))-min(X(:,1)));
+hy = abs(max(X(:,2))-min(X(:,2)));
+Q = sqrt(9/(4*hx*hy))*[-1,1,-1,1]';
 
-% X = G.nodes.coords(G.cells.nodes(G.cells.nodePos(1):G.cells.nodePos(2)-1),:);
-% hx = abs(max(X(:,1))-min(X(:,1)));
-% hy = abs(max(X(:,2))-min(X(:,2)));
-% Q = sqrt(9/(4*hx*hy))*[-1,1,-1,1]';
-% 
-% uChi = gD(G.nodes.coords);
-% 
-% wLower = .06; wUpper = .07;
-% tol = 1e-4;
-% while abs(wLower - wUpper) > tol;
-% 
-% n = 10;
-% w = linspace(wLower,wUpper,n);
-% errVec = zeros(n,1);
-% 
-% for i = 1:n
-%     
-%     lambda = 3*w(i)*(1/hx^2+1/hy^2);
-%     sigma = 1/(Q'*Q)^2*lambda*ones(G.cells.num,1);
-% 
-%     [sol,G] = VEM2D(G, 0, bc, 1, 'projectors', true, 'sigma', sigma, 'cartGridQ',  true, 'src', src);
-%     
-% %     errVec(i) = norm(sol.nodeValues-uChi,2);
-%     errVec(i) = sqrt(sum(l2Error(G, sol, gD, 1)));
-%     
-% end
-% 
-% i = find(min(errVec) == errVec);
-% i = i(1);
-% wMin = w(i);
-% iLower = max(1,i-1);
-% wLower = w(iLower);
-% iUpper = min(n, i+1);
-% wUpper = w(iUpper);
-% 
-% end
-% 
-% lambda = 3*wMin*(1/hx^2+1/hy^2);
-% sigma = 1/(Q'*Q)^2*lambda*ones(G.cells.num,1);
-% fprintf('Minimum attained at w = %f', wMin);
-% fprintf('Minimum attained at sigma = %f', sigma(1));
+uChi = gD(G.nodes.coords);
+
+wLower = 0; wUpper = 6;
+tol = 1e-4;
+while abs(wLower - wUpper) > tol;
+
+n = 10;
+w = linspace(wLower,wUpper,n);
+errVec = zeros(n,1);
+
+for i = 1:n
+    
+    lambda = 3*w(i)*(1/hx^2+1/hy^2);
+    sigma = 1/(Q'*Q)^2*lambda*ones(G.cells.num,1);
+
+    [sol,G] = VEM2D(G, 0, bc, 1, 'projectors', true, 'sigma', sigma, 'cartGridQ',  true, 'src', src);
+    
+%    err = sol.nodeValues - uChi;    
+%     errVec(i) = norm(err(trustNodes),2);
+    l2Err = l2Error(G, sol, gD, 1);
+    errVec(i) = sqrt(sum(l2Err(trustCells)));
+    
+end
+
+i = find(min(errVec) == errVec);
+i = i(1);
+wMin = w(i);
+iLower = max(1,i-1);
+wLower = w(iLower);
+iUpper = min(n, i+1);
+wUpper = w(iUpper);
+
+end
+
+lambda = 3*wMin*(1/hx^2+1/hy^2);
+sigma = 1/(Q'*Q)^2*lambda*ones(G.cells.num,1);
+fprintf('Minimum attained at w = %f', wMin);
+fprintf('Minimum attained at sigma = %f', sigma(1));
 
 %%
 % 
@@ -217,49 +230,50 @@ fprintf('Error with FEM stability term: %f, %f, %f \n', err4, l2err4, err4+l2err
 
 %%
 
-gDl2 = sqrt(sum(polygonInt(G, 1:G.cells.num, @(X) gD(X).*gD(X), 7)));
-
-X = G.nodes.coords(G.cells.nodes(G.cells.nodePos(1):G.cells.nodePos(2)-1),:);
-hx = abs(max(X(:,1))-min(X(:,1)));
-hy = abs(max(X(:,2))-min(X(:,2)));
-Q = sqrt(9/(4*hx*hy))*[-1,1,-1,1]';
-
-uChi = gD(G.nodes.coords);
-
-wLower = 0; wUpper = .3;
-n = 20;
-w = linspace(wLower,wUpper,n);
-errVec = zeros(n,1);
-
-for i = 1:n
-    
-    lambda = 3*w(i)*(1/hx^2+1/hy^2);
-    sigma = 1/(Q'*Q)^2*lambda*ones(G.cells.num,1);
-
-    [sol,G] = VEM2D(G, 0, bc, 1, 'projectors', true, 'sigma', sigma, 'cartGridQ',  true, 'src', src);
-    
-%     errVec(i) = norm(sol.nodeValues-uChi,2);
-    errVec(i) = sqrt(sum(l2Error(G, sol, gD, 1)));
-    
-end
-
-%%
-
-plot(w, errVec/gDl2);
-
-set(gcf, 'defaulttextinterpreter', 'latex');
-xlabel('$w$'); ylabel('$\|e_h\|_{0,K}/\|u\|_{0,K}$')
-
-w = 0;
-h = 0;
-ps = get(gcf, 'Position');
-ratio = 1;
-paperWidth = 10;
-paperHeight = paperWidth*ratio;
-set(gcf, 'paperunits', 'centimeters');
-set(gcf, 'papersize', [paperWidth-w paperHeight-h]);
-set(gcf, 'PaperPosition', [-w    -h   paperWidth+w paperHeight+h]);
-print(gcf, '-dpdf', '../../tex/thesis/fig/wStabilityL2.pdf');
+% gDl2 = sqrt(sum(polygonInt(G, 1:G.cells.num, @(X) gD(X).*gD(X), 7)));
+% 
+% X = G.nodes.coords(G.cells.nodes(G.cells.nodePos(1):G.cells.nodePos(2)-1),:);
+% hx = abs(max(X(:,1))-min(X(:,1)));
+% hy = abs(max(X(:,2))-min(X(:,2)));
+% Q = sqrt(9/(4*hx*hy))*[-1,1,-1,1]';
+% 
+% uChi = gD(G.nodes.coords);
+% 
+% wLower = 0; wUpper = 10;
+% n = 20;
+% w = linspace(wLower,wUpper,n);
+% errVec = zeros(n,1);
+% 
+% for i = 1:n
+%     
+%     lambda = 3*w(i)*(1/hx^2+1/hy^2);
+%     sigma = 1/(Q'*Q)^2*lambda*ones(G.cells.num,1);
+% 
+%     [sol,G] = VEM2D(G, 0, bc, 1, 'projectors', true, 'sigma', sigma, 'cartGridQ',  true, 'src', src);
+%     
+% %     errVec(i) = norm(sol.nodeValues-uChi,2);
+%     l2Err = l2Error(G, sol, gD, 1);
+%     errVec(i) = sqrt(sum(l2Err(trustCells)));
+%     
+% end
+% 
+% %%
+% 
+% plot(w, errVec/gDl2);
+% 
+% set(gcf, 'defaulttextinterpreter', 'latex');
+% xlabel('$w$'); ylabel('$\|e_h\|_{0,K}/\|u\|_{0,K}$')
+% 
+% w = 0;
+% h = 0;
+% ps = get(gcf, 'Position');
+% ratio = 1;
+% paperWidth = 10;
+% paperHeight = paperWidth*ratio;
+% set(gcf, 'paperunits', 'centimeters');
+% set(gcf, 'papersize', [paperWidth-w paperHeight-h]);
+% set(gcf, 'PaperPosition', [-w    -h   paperWidth+w paperHeight+h]);
+% print(gcf, '-dpdf', '../../tex/thesis/fig/wStabilityL2.pdf');
 
 %%
 
@@ -273,7 +287,7 @@ uChi = gD(G.nodes.coords);
 wLower = 0; wUpper = 10;
 n = 20;
 w = linspace(wLower,wUpper,n);
-errVec = zeros(n,1);
+errVec = zeros(n,2);
 
 for i = 1:n
     
@@ -281,28 +295,29 @@ for i = 1:n
     sigma = 1/(Q'*Q)^2*lambda*ones(G.cells.num,1);
 
     [sol,G] = VEM2D(G, 0, bc, 1, 'projectors', true, 'sigma', sigma, 'cartGridQ',  true, 'src', src);
-    
-    errVec(i) = norm(sol.nodeValues-uChi,2);
-    %errVec(i,2) = sqrt(sum(l2Error(G, sol, gD, 1)));
+    err = sol.nodeValues - uChi;
+    errVec(i,1) = norm(err(trustNodes),2);
+    l2Err = l2Error(G, sol, gD, 1);
+    errVec(i,2) = sqrt(sum(l2Err(trustCells)));
     
 end
 
 %%
 
-plot(w, errVec/gD2);
+plot(w, errVec(:,1)/gD2, w, errVec(:,2)/gDl2);
 
 set(gcf, 'defaulttextinterpreter', 'latex');
 xlabel('$w$'); ylabel('$|\mathbf{\hat{e}}_h|/|\mathbf{\hat{u}}|$')
 
-w = 0;
+width = 0;
 h = 0;
 ps = get(gcf, 'Position');
 ratio = 1;
 paperWidth = 10;
 paperHeight = paperWidth*ratio;
 set(gcf, 'paperunits', 'centimeters');
-set(gcf, 'papersize', [paperWidth-w paperHeight-h]);
-set(gcf, 'PaperPosition', [-w    -h   paperWidth+w paperHeight+h]);
+set(gcf, 'papersize', [paperWidth-width paperHeight-h]);
+set(gcf, 'PaperPosition', [-width    -h   paperWidth+width paperHeight+h]);
 print(gcf, '-dpdf', '../../tex/thesis/fig/wStability2.pdf');
 
 
