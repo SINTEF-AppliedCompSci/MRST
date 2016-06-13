afbc3a44d3e754ea6b54f1fecc18c515c58bdd79
Olav Møyner <olav.moyner@sintef.no>
2016-06-13 09:50:36 +0200

    Removing duplicate file introduced by error back in the SVN days.

--- ad-props/props/assignPLYADSm.m
+++
@@ -1,7 +0,0 @@
-function f = assignPLYADSm(f, plyvisc, reg)
-   regmap = @(c, varargin) ...
-      getRegMap(c, reg.PVTNUM, reg.PVTINX, varargin{:});
-
-   f.muWMult = @(c, varargin) ...
-      interpReg(plyvisc, c, regmap(c, varargin{:}));
-end
