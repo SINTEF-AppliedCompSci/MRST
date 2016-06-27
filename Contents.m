ffb56901f907709f4730904fdda3d222aba53f57
Runar_Berge <runarlb@stud.ntnu.no>
2016-06-27 17:36:16 +0200

    Updated contents file

--- Contents.m
+++ Contents.m
@@ -12,6 +12,10 @@
 %  COPYRIGHT.txt         - Copyright file
 %
 % /voronoi2D
+%   README              - README for files in voronoi2D folder
+%   clippedPebi2D       - 2D PEBI-grid clipped against polygon boundary
+%   createCVD           - Centroidal Voronoi Diagram
+%   faultSufCond        - Enforce suficient fault condition
 %   compositePebiGrid   - Creates a conforming PEBI-grid embedded in a
 %                         Cartesian background grid.
 %   pebiGrid            - Creates a fully unstructured PEBI-grid by using
@@ -20,11 +24,23 @@
 %                           both sides of a fault
 %   createWellGridPoints - Creates a set of Voronoi sites tracing a well
 %   plotLinePath         - Plots a fault or a well path
-%   README              - Readme file for voronoi2D
+%   README2d             - Readme file for voronoi2D
 %   removeConflictPoints2 - Remove sites that are too close to each other
 %   splitAtInt          - Split a fault or well paths at the intersections.
 %   /examples
+%     intersectingWellAndFaults         - one well intersected of several
+%                                         faults
+%     reservoirWithComplexFaultNetwork  - A reservoir with many faults
+%     showOptionValuesCompositePebiGrid - Show the different parameters
+%                                         that you can pass to
+%                                         compositePebiGrid
+%     showOPtionValuesPebiGrid          - Show the different parameters you
+%                                         can pass to pebiGrid
+%     statisticalFractures              - Reservoir with many fractures
+%     threeIntersectingFaults           - Thre faults that intersects
+%     wellBranching                     - Intersecting wells
+%
 %
 % /distMesh:            - Contains a modifed version of distMesh.
-%  
+% /util:                - Contains some utility functions.  
 %
---
+++ README.m
@@ -0,0 +1,19 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This matlab code was written as a part of my master thesis during the 
+% fall of 2015 and spring of 2016. My masters thesis is written in 
+% cooperation with SINTEF ICT.
+% Runar Lie Berge                                    (runarlb@stud.ntnu.no)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% UPR contains three subfolders with functions. voronoi2D/ contains functions
+% for generate a grid adapting to surfaces in 2D. Please see the README file 
+% in this folder for more information. The subfolder util/ contains some 
+% utility functions, while the subfolder distmesh/ contains a modified 
+% version of the software distmesh: Per-Olof Persson and Gilbert Strang, 
+% "A Simple Mesh Generator in MATLAB," SIAM Review Vol. 46 (2) 2004.
+%
+% For a detailed review about UPR we have included the master thesis 
+% "Unstructured PEBI-grids Adapting to Geological Features in Subsurface
+% Reservoirs".
+%
+% Please see COPYRIGHT for the use of this software.
