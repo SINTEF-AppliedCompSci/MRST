c30da07199721e8e41b4b23a953779806fbb5b43
Xavier Raynaud <xavier.raynaud@sintef.no>
2016-06-28 14:59:26 +0200

    cleaned up in grid_utils.

--- vemmech/grid_utils/verticalGrdecl.m
+++ vemmech/grid_utils/verticalGrdecl.m
@@ -1,18 +1,19 @@
-function grdecl=verticalGrdecl(grdecl,varargin)
-    opt=struct('method','all')
-    opt=merge_options(opt,varargin{:});
-    top=[1,2];bot=[4,5];
-    [xyz,zcorn]=grdeclXYZ(grdecl);
+function grdecl = verticalGrdecl(grdecl, varargin)
+    opt = struct('method', 'all')
+    opt = merge_options(opt, varargin{:});
+    top = [1, 2];
+    bot = [4, 5];
+    [xyz, zcorn] = grdeclXYZ(grdecl);
     switch opt.method
-        case 'sides'
-        xyz(bot,1,:)= xyz(top,1,:);% x-sides
-        xyz(bot,end,:)= xyz(top,end,:);
-        xyz(bot,:,1)= xyz(top,:,1);%y-sides
-        xyz(bot,:,end)= xyz(top,:,end)
-        case 'all'
-           xyz(top,:,:)=xyz(bot,:,:); 
-        otherwise
-       error('no such method');     
+      case 'sides'
+        xyz(bot, 1, :) = xyz(top, 1, :); % x-sides
+        xyz(bot, end, :) = xyz(top, end, :);
+        xyz(bot, :, 1) = xyz(top, :, 1); % y-sides
+        xyz(bot, :, end) = xyz(top, :, end)
+      case 'all'
+        xyz(top, :, :) = xyz(bot, :, :); 
+      otherwise
+        error('no such method');     
     end
-    grdecl.COORD=xyz(:);
+    grdecl.COORD = xyz(:);
 end
