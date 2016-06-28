96c9eca5dc60c122588c55c0787ed8fc78d60275
Xavier Raynaud <xavier.raynaud@sintef.no>
2016-06-28 14:10:06 +0200

    Cleaned up divergence codes.

--- vemmech/utils/VEM3D_div.m
+++ vemmech/utils/VEM3D_div.m
@@ -3,34 +3,22 @@ function [div] = VEM3D_div(G)
 % volum, area, length
 
 %{ 
-   Copyright 2009 - 2014 SINTEF ICT, Applied Mathematics
-   %}
-   assert(G.griddim == 3)
-   % ncl = diff(G.cells.nodePos
-   % svdiv = struct('iind', [], 'jind', [], 'matel', []); 
-   % nn = sum(diff(G.faces.nodePos)) * G.griddim; 
-   cells     = 1:G.cells.num; 
-   ifaces    = mcolon(G.cells.facePos(cells), (G.cells.facePos(cells + 1) - 1)); 
-   faces     = G.cells.faces(ifaces, 1); 
-   inodes    = mcolon(G.faces.nodePos(faces), (G.faces.nodePos(faces + 1) - 1)); 
-   % edges   = G.faces.edges(iedges); 
-   nodes     = G.faces.nodes(inodes); 
-   cellfaces = rldecode(cells', diff(G.cells.facePos)); 
-   nvec      = bsxfun(@times, G.faces.normals(faces, :), ...
-                      (2 * (G.faces.neighbors(faces, 1) == cellfaces) - 1)); 
-   nfl       = G.faces.nodePos(faces + 1) - G.faces.nodePos(faces); 
-   if(false)
-      nvec = reshape(rldecode(bsxfun(@rdivide, nvec, nfl), nfl)', [], 1); % simple ???
-      matel = reshape(nvec', [], 1); 
-   else
-      [qc, qf] = calculateQC_vec(G); 
-      nvec = bsxfun(@rdivide, nvec, G.faces.areas(faces)); 
-      matel = reshape(bsxfun(@times, rldecode(nvec, nfl), qf(inodes))', [], 1); 
-   end
-   % facenodes = rldecode(faces, diff(G.faces.nodePos)); 
-   jind = mcolon(G.griddim * (nodes - 1) + 1, G.griddim * (nodes - 1) + G.griddim); 
-   iind = rldecode(cellfaces, nfl * 3); 
-   % assert(nn == numel(matel(:))); 
-   div = sparse( iind', jind', matel(:), G.cells.num, G.nodes.num .* G.griddim);
+Copyright 2009 - 2014 SINTEF ICT, Applied Mathematics
+%}
+    assert(G.griddim == 3);
+    cells     = 1:G.cells.num; 
+    ifaces    = mcolon(G.cells.facePos(cells), (G.cells.facePos(cells + 1) - 1)); 
+    faces     = G.cells.faces(ifaces, 1); 
+    inodes    = mcolon(G.faces.nodePos(faces), (G.faces.nodePos(faces + 1) - 1)); 
+    nodes     = G.faces.nodes(inodes); 
+    cellfaces = rldecode(cells', diff(G.cells.facePos)); 
+    nvec      = bsxfun(@times, G.faces.normals(faces, :), ...
+                       (2 * (G.faces.neighbors(faces, 1) == cellfaces) - 1)); 
+    nfl       = G.faces.nodePos(faces + 1) - G.faces.nodePos(faces); 
+    nvec  = bsxfun(@rdivide, nvec, G.faces.areas(faces)); 
+    matel = reshape(bsxfun(@times, rldecode(nvec, nfl), qf(inodes))', [], 1); 
+    jind  = mcolon(G.griddim * (nodes - 1) + 1, G.griddim * (nodes - 1) + G.griddim); 
+    iind  = rldecode(cellfaces, nfl * 3); 
+    div   = sparse( iind', jind', matel(:), G.cells.num, G.nodes.num .* G.griddim);
 
 end
