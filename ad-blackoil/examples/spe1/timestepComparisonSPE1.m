abfe04384a5d2cb5f6ec33de6b38a4ac924f3a9b
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-06-30 00:38:43 +0200

    Added discussions to each of the section of the script. Also improved the plotting slightly.

--- ad-blackoil/examples/spe1/timestepComparisonSPE1.m
+++ ad-blackoil/examples/spe1/timestepComparisonSPE1.m
@@ -1,5 +1,22 @@
+%% Automated Time-Step Control
+% The ad-core module offers several options for automatic time-step
+% control. The simplest approach is to run the time steps as prescribed and
+% use a heuristic algorithm to reduce the time step if the number of
+% nonlinear iterations exceeds a prescribed limit; this is commonly
+% referred to as the Appleyard chop. In the modified version, one also
+% monitors the saturation increments and reduces the time step if the
+% saturation changes more than a prescribed limit within a single time
+% step.
+%
+% MRST also offers more a sophisticated time-step control that monitors the
+% convergence of the nonlinear solves and adjusts the time-step so that the
+% number of nonlinear iterations stays close to a prescribed target. In
+% this example, we will use the SPE1 benchmark to demonstrate this time
+% step control.
+
 mrstModule add ad-props deckformat mrst-gui ad-core ad-blackoil
 
+%% Load and run the SPE1 model
 % Because several examples use the SPE1 dataset, the initial setup is
 % delegated to a helper function. See the inside for documentation.
 [G, rock, fluid, deck, state] = setupSPE1();
@@ -12,20 +29,30 @@ model %#ok, intentional display
 
 % Convert the deck schedule into a MRST schedule by parsing the wells
 schedule = convertDeckScheduleToMRST(model, deck);
-%% Run base case 
+
+% Run base case 
 [wellSols, states, report] = simulateScheduleAD(state, model, schedule);
 
-%% Make a smaller schedule
-% Because the SPE1 benchmark only has a single well configuration during
+%% Time-step control with iteration targets
+% Because the SPE1 benchmark uses the same well configuration throughout
 % the entire simulation, we are (relatively) free to choose timesteps. To
 % demonstrate this, we create a simpler schedule consisting of a single
-% very long timestep. The compressSchedule routine reduces a schedule to
-% the minimal number of timesteps required to honor the distinct control
-% steps. In this case, we only have a single control step.
+% very long timestep and then let the time-step control automatically
+% select the length of the individual time steps so that the number of
+% iterations for each step stays close to a prescribed target. We will run
+% four simulations with target values 4, 8, 15, and 25.
+%
+% Use the compressSchedule routine to reduce the schedule to the minimal
+% number of timesteps required to honor the distinct control steps. In this
+% case, we only have a single control step. To get a stable solution, we
+% also need to start with a small time step to get the solver started.
+% Here, we start with a time step of one day and then gradually increase
+% the length of the time step to stay close to our iteration target.
 
+% Reduce to a minimal schedule
 schedule_small = compressSchedule(schedule);
-%% Run various number of target iterations
-% Small step to get the solver started
+
+% Run all four simulations
 rampup = 1*day;
 targetIts = [4 8 15 25];
 [reports, ws] = deal(cell(numel(targetIts), 1));
@@ -38,7 +65,7 @@ for i = 1:numel(targetIts)
                                       'firstRampupStep',       rampup, ...
                                       'verbose', true);
 
-    % Instansiate a non-linear solver with the timestep class as a
+    % Instantiate a nonlinear solver with the timestep class as a
     % construction argument.
     nonlinear = NonLinearSolver('timeStepSelector', timestepper, ...
                                 'maxiterations', 4*targetIts(i));
@@ -48,10 +75,11 @@ for i = 1:numel(targetIts)
 end
 
 %% Combine output
-% We now have reports and solutions for both base case and the cases using
-% automatic timesteping. Combine the results into 
+% We now have reports and solutions for the base case and the cases using
+% automatic time-stepping. We extract the length of each time step from the
+% simulation reports and plot versus time
 l = arrayfun(@(x) ['Target: ', num2str(x), ' its'], targetIts, 'UniformOutput', false);
-l = horzcat('Basecase', l);
+l = horzcat('Base case', l);
 
 wsols = vertcat({wellSols}, ws);
 
@@ -70,17 +98,25 @@ figure;
 hold on
 c = lines(numel(timesteps));
 for i = 1:numel(timesteps);
-    plot(time{i}, timesteps{i}, '--o', 'linewidth', 2, 'color', c(i, :))
+    plot(time{i}/day, timesteps{i}/day, '-o', 'linewidth', 2, 'color', c(i, :))
     grid on
 end
-legend(l)
+legend(l,2)
 
-%% Compare the solutions interactively
+%% Compare solution accuracy (well curves)
+% Looking at the various well curves, we see that whereas the water rate
+% and water production is accurately resolved by all simulations, the
+% larger time steps fail to accurately resolve the strong gradients in the
+% oil and gas production as the gas front approaches the producer. On the
+% other hand, using a target of 15 or 25 iterations allows time steps
+% between one and two years, which are extremely, and one should generally
+% not expect that this will give very accurate solutions.
 plotWellSols(wsols, time, 'datasetnames', l);
 
-%% Find the number of iterations and simulation time taken for all cases
-% Since the timesteps produce substeps we have to find report->control step
-% reports.
+%% Compare computational efficiency
+% To get an overall view of the computational cost of each simulation, we
+% need to extract the number of iterations and computational time of each
+% substep. These can be extracted from the report->control step reports.
 reps = vertcat({report}, reports);
 
 [iterations, timesteps] = deal(zeros(numel(reps), 1));
@@ -99,3 +135,10 @@ figure;
 bar([iterations, timesteps])
 legend('Non-linear iterations', 'Time taken')
 set(gca, 'XTicklabel', l)
+
+%% Conclusion
+% The automatic time-step control enables us to run very large time steps.
+% However, since this will also introduce large errors in the temporal
+% discretization, a reasonable compromise between accuracy and efficiency
+% would most likely be to select an iteration target somewhere in the
+% interval 3 to 8.
