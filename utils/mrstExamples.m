2b3783dde6d1baa3dcc4bf5a881b5350b9edb69d
Olav Møyner <olav.moyner@sintef.no>
2016-06-30 21:45:08 +0200

    mrstExamples: Less complicated version of folder output that also does not give warnings on windows.

--- utils/mrstExamples.m
+++ utils/mrstExamples.m
@@ -90,9 +90,10 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
             end
             for j = 1:nex
                 % Split by path + examples to get base folder path
-                spl = strsplit(allfiles{j}, fullfile(pth, ['examples', filesep]));
+                spl = strsplit(allfiles{j}, filesep);
+                sub = find(strcmpi(spl, 'examples')) + 1;
                 fprintf('    <a href="matlab: edit %s">%s\n</a>',...
-                                                allfiles{j}, spl{end});
+                                                allfiles{j}, fullfile(spl{sub:end}));
             end
         end
         examplePaths{i} = allfiles;
