88aa40c0b0f2077aba4a3929516a9139af5fc6dd
Olav Møyner <olav.moyner@sintef.no>
2016-06-30 21:46:26 +0200

    Adding work in progress WAG example for sequential module

--- blackoil-sequential/examples/sequentialSPE10.m
+++ blackoil-sequential/examples/sequentialSPE10.m
@@ -1,4 +1,5 @@
 %%
+mrstModule add ad-core ad-blackoil blackoil-sequential spe10
 [state, model, schedule] = setupSPE10_AD('layers', 15);
 seqModel = getSequentialModelFromFI(model);
 
@@ -17,3 +18,24 @@ t_fi = toc(timer);
 
 %%
 plotWellSols({wsSeq, wsFIMP}, cumsum(schedule.step.val), 'datasetnames', {'Sequential', 'FIMP'})
+
+%%
+fluid = initSimpleADIFluid('phases', 'WG', 'c', [1/barsa, 2/barsa], 'cR', 2),
+
+%% Compute solution with refined time steps
+% We will now compute a solution with refined time steps. As the time-steps
+% become smaller, the solution becomes more accurate. In order to achieve
+% increased accuracy without manually changing the timesteps, we can use
+% a automatic timestep selector based on saturation change targets. We let
+% the solver aim for a maximum saturation change of 1% in each cell during
+% the timesteps to get very small steps.
+stepSel = StateChangeTimeStepSelector(...
+          'targetProps', 's',...            % Saturation as change target
+          'targetChangeAbs', 0.01,...       % Target change of 0.01
+          'firstRampupStepRelative', 0.01); % Initial rampup step is dt0/100
+solver = NonLinearSolver('timeStepSelector', stepSel);
+% Simulate with small timesteps. As the resulting timesteps will be very
+% small, it will take some time (about six minutes on the workstation where
+% the example was written).
+[wsFine, statesFine, repFine] = simulateScheduleAD(state, model, schedule, ...
+                        'nonlinearsolver', solver, 'outputMinisteps', true);
