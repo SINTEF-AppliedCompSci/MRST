304f5dc1eb88f2250284c05145a71855e0215a98
Xavier Raynaud <xavier.raynaud@sintef.no>
2016-06-30 10:36:36 +0200

    Fixed comments in SURFACTANT1D

--- ad-eor/examples/surfactantExamples/surfactantExample1D.m
+++ ad-eor/examples/surfactantExamples/surfactantExample1D.m
@@ -1,8 +1,15 @@
-%% 
-% This 1D 
+%%
+% 1D test case oil-water-surfactant
 %
-% data from deck
+% The input data is read from deck (SURFACTANT1D.DATA). The surfactant property,
+% which are read from file surfact.inc, are taken from SPE paper 145036 (see
+% more details in input file).
 %
+% Water and surfactant are injected at the left-hand side and oil is produced
+% at the right-hand side at given pressure.
+%
+% In a first period, only water is injected. Then, for a second period,
+% surfactant is added to the water.
 
 try
     require ad-core ad-blackoil ad-eor ad-props deckformat mrst-gui
@@ -11,7 +18,6 @@ catch
 end
 
 current_dir = fileparts(mfilename('fullpath'));
-
 fn = fullfile(current_dir, 'SURFACTANT1D.DATA');
 gravity off
 
@@ -19,46 +25,32 @@ deck = readEclipseDeck(fn);
 deck = convertDeckUnits(deck);
 
 fluid = initDeckADIFluid(deck);
-
 G = initEclipseGrid(deck);
 G = computeGeometry(G);
-
 rock  = initEclipseRock(deck);
 rock  = compressRock(rock, G.cells.indexMap);
 
-
-
-%% Set up simulation parameters
-% We want a layer of oil on top of the reservoir and water on the bottom.
-% To do this, we alter the initial state based on the logical height of
-% each cell. The resulting oil concentration is then plotted.
-
+%% Set up initial state
+% Constant pressure, residual water saturation, no surfactant
 
 nc = G.cells.num;
-state0 = initResSol(G, 300*barsa, [ .2, .8]);
-
-% Add zero surfactant concentration to the state.
+state0 = initResSol(G, 300*barsa, [ .2, .8]); % residual water saturation is 0.2
 state0.c    = zeros(G.cells.num, 1);
 state0.cmax = state0.c;
+state0.ads = computeEffAds(state0.c, 0, modelSurfactant.fluid); % adsorption
+state0.adsmax = state0.ads;
 
-
-%% Set up systems.
+%% Set up the model
 
 modelSurfactant = FullyImplicitOilWaterSurfactantModel(G, rock, fluid, ...
                                                   'inputdata', deck, ...
                                                   'extraStateOutput', true);
 
-% Convert the deck schedule into a MRST schedule by parsing the wells
+%% Convert the deck schedule into a MRST schedule by parsing the wells
+
 schedule = convertDeckScheduleToMRST(modelSurfactant, deck);
 
-
 %% Run the schedule
-% Once a system has been created it is trivial to run the schedule. Any
-% options such as maximum non-linear iterations and tolerance can be set in
-% the system struct.
-
-state0.ads = computeEffAds(state0.c, 0, modelSurfactant.fluid);
-state0.adsmax = state0.ads;
 
 resulthandler = ResultHandler('dataDirectory', pwd, 'dataFolder', 'cache', 'cleardir', true);
 [wellSolsSurfactant, statesSurfactant] = simulateScheduleAD(state0, modelSurfactant, ...
