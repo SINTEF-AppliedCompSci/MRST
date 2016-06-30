c6f25aa4867fb8fceedd3bc7d6e59b4d77567882
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-06-30 16:20:49 +0200

    Inserted copyright notice at the end using Bård's sed script

--- agglom/examples/agglomExample5.m
+++ agglom/examples/agglomExample5.m
@@ -153,4 +153,28 @@ axis tight off, title(sprintf('Hybrid vel:\n%d blocks', max(p)),targs{:});
 % that the latter has regular coarse blocks in all zones of low flow
 % because of the static 6x22 partition.
 
-% #COPYRIGHT_EXAMPLE#
+% <html>
+% <p><font size="-1">
+% Copyright 2009-2015 SINTEF ICT, Applied Mathematics.
+% </font></p>
+% <p><font size="-1">
+% This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+% </font></p>
+% <p><font size="-1">
+% MRST is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% </font></p>
+% <p><font size="-1">
+% MRST is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% </font></p>
+% <p><font size="-1">
+% You should have received a copy of the GNU General Public License
+% along with MRST.  If not, see
+% <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses</a>.
+% </font></p>
+% </html>
