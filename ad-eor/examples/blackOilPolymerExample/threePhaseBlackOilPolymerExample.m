60f03cc64cd6dabc1d0cb1654798cb25c8d000b2
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-06-30 12:31:22 +0200

    Added an introduction, compacted the plotting.

--- ad-eor/examples/blackOilPolymerExample/threePhaseBlackOilPolymerExample.m
+++ ad-eor/examples/blackOilPolymerExample/threePhaseBlackOilPolymerExample.m
@@ -1,18 +1,20 @@
-%%
-% This example contains a simple 2D 20*5*1 grid containing one injector
-% well on the bottom layer and one production well on the top layer.
-%
-% The schedule being used contains first a period of injection without
-% polymer, then water flooding period with polymer, followed by a pure
-% water flooding phase without polymer.
+%% 2D Three-Phase Polymer Injection Case
+% This example contains a simple 4000 m × 200 m × 125 m reservoir described
+% on 20 × 1 × 5 uniform Cartesian grid. One injection well is located in
+% the bottom two layers and one production well is located in the top two
+% layers. Hydrostatic equilibration is used for initialization.
 
-try
-    require ad-core ad-blackoil ad-eor ad-fi ad-props deckformat
-catch
-    mrstModule add ad-core ad-blackoil ad-eor ad-fi ad-props deckformat
-end
+% The polymer injection schedule follows a typical polymer waterflooding
+% strategy. The flooding process begins with primary waterflooding for 1260
+% days, followed by a polymer slug injected over 1700 days, and then
+% switching back to water injection. The injection well is under rate
+% control with target rate 1000 m3 /day and upper limit of 450 bar on the
+% bottom-hole pressure (bhp), whereas the production well is under pressure
+% control with target bottom-home pressure 260 bar.
 
+mrstModule add ad-core ad-blackoil ad-eor ad-fi ad-props deckformat
 
+%% Set up model and initial conditions
 % The data required for the example
 % If the data does not exist locally, download it automatically
 fname = { '2D_THREEPHASE_POLY_HETER.DATA', 'POLY.inc' };
@@ -38,22 +40,21 @@ G = computeGeometry(G);
 rock  = initEclipseRock(deck);
 rock  = compressRock(rock, G.cells.indexMap);
 
-%% set the initial conditions with with InitEclipseState
-% for this deck, equil is used to initialize the initial state, while the
-% results of the initialization need to be verified against Eclipse later.
-
 gravity on;
-
 fluid          = initDeckADIFluid(deck);
+modelBOPolymer = ThreePhaseBlackOilPolymerModel(G, rock, fluid, 'inputdata', deck);
+
+% Set initial equilibrium and no polymer present
 state0         = initEclipseState(G, deck, initEclipseFluid(deck));
 state0.c       = zeros([G.cells.num, 1]);
 state0.cmax    = zeros([G.cells.num, 1]);
-modelBOPolymer = ThreePhaseBlackOilPolymerModel(G, rock, fluid, 'inputdata', deck);
 
 % Convert the deck schedule into a MRST schedule by parsing the wells
 schedule = convertDeckScheduleToMRST(modelBOPolymer, deck);
 
-%% Set the non-linear solver
+%% Select nonlinear and linear solvers
+% Use the AGMG algebraic multigrid solver if this is present, and if not,
+% use the default MATLAB solver.
 modelBOPolymer.useCNVConvergence = true;
 
 % Choose the linear solver for pressure equation
@@ -69,174 +70,99 @@ nonlinearsolver.useRelaxation = true;
 
 
 %% Plotting the initial saturations
-h=figure(1);
+h=figure(1); clf
 set(h, 'Position', [100, 100, 900, 600]);
-clf;
 
 subplot(2,2,1);
-plotGrid(G, 'FaceColor', 'none');
-axis tight off;
-view(26,18);
-W = schedule.control(1).W;
-plotWell(G,W);
+plotGrid(G, 'FaceColor', 'none'); plotWell(G,schedule.control(1).W);
+axis tight off; view(26,18);
 
-subplot(2,2,2);
-plotCellData(G, state0.s(:,1));
-plotGrid(G, 'FaceColor', 'none')
-title('Initial Water saturation')
-axis tight off
-view(26,18);
-colorbar;
-
-subplot(2,2,3);
-plotCellData(G, state0.s(:,2));
-plotGrid(G, 'FaceColor', 'none')
-title('Initial Oil saturation')
-axis tight off
-view(26,18);
-colorbar;
-
-subplot(2,2,4);
-plotCellData(G, state0.s(:,3));
-plotGrid(G, 'FaceColor', 'none')
-title('Initial Gas saturation')
-axis tight off
-view(26,18);
-colorbar;
-
-pause(0.5);
+phases = {'water','oil','gas'};
+for i=1:3
+    subplot(2,2,i+1);
+    plotCellData(G, state0.s(:,i), 'EdgeColor','k');
+    title(['Initial ', phases{i}, ' saturation'])
+    axis tight off; view(26,18); colorbar;
+end
 
 %% Run the schedule
 % Once a system has been created it is trivial to run the schedule. Any
 % options such as maximum non-linear iterations and tolerance can be set in
 % the system struct.
 
-
 [wellSolsPolymer, statesPolymer] = ...
-    simulateScheduleAD(state0, modelBOPolymer, schedule, 'NonLinearSolver', nonlinearsolver);
-
+    simulateScheduleAD(state0, modelBOPolymer, schedule, ...
+                       'NonLinearSolver', nonlinearsolver);
 
 %% plotting the well data
-h = figure(3);
-set(h, 'Position', [100, 50, 900, 1300]);
-clf;
-
-T     = convertTo(cumsum(schedule.step.val), day);
-
+figure(2)
+set(gcf,'Position', [100, 50, 900, 1300]);
+T = convertTo(cumsum(schedule.step.val), day);
 [qWs, qOs, qGs, bhp] = wellSolToVector(wellSolsPolymer);
 
-subplot(5,2,1);
+subplot(5,2,1); title('BHPs for injection wells');
 plot(T, convertTo(bhp(:,1), barsa));
-title('BHPs for injection wells');
-ylabel('bhp (bar)');
-xlabel('time (day)');
+ylabel('bhp (bar)'); xlabel('time (day)');
 
-subplot(5,2,2);
+subplot(5,2,2); title('BHPs for production wells');
 plot(T, convertTo(bhp(:,2), barsa));
-title('BHPs for production wells');
-ylabel('bhp (bar)');
-xlabel('time (day)');
+ylabel('bhp (bar)'); xlabel('time (day)');
 
-subplot(5,2,3);
+subplot(5,2,3); title('water injection rate');
 plot(T, convertTo(qWs(:,1), meter^3/day));
-title('water injection rate');
-ylabel('WWIR (m^3/day)');
-xlabel('time (day)');
+ylabel('WWIR (m^3/day)'); xlabel('time (day)');
 
-subplot(5,2,4);
+subplot(5,2,4); title('Water Production Rate');
 plot(T, convertTo(qWs(:,2), meter^3/day));
-title('Water Production Rate');
-ylabel('WWPR (m^3/day)');
-xlabel('time (day)');
+ylabel('WWPR (m^3/day)'); xlabel('time (day)');
 
-subplot(5,2,5);
+subplot(5,2,5); title('oil injection rate');
 plot(T, convertTo(qOs(:,1), meter^3/day));
-title('oil injection rate');
-ylabel('WOIR (m^3/day)');
-xlabel('time (day)');
+ylabel('WOIR (m^3/day)'); xlabel('time (day)');
 
-subplot(5,2,6);
+subplot(5,2,6); title('oil production rate');
 plot(T, convertTo(qOs(:,2), meter^3/day));
-title('oil production rate');
-ylabel('WOPR (m^3/day)');
-xlabel('time (day)');
+ylabel('WOPR (m^3/day)'); xlabel('time (day)');
 
-subplot(5,2,7);
+subplot(5,2,7); title('gas injection rate');
 plot(T, convertTo(qGs(:,1), meter^3/day));
-title('gas injection rate');
-ylabel('WGIR (m^3/day)');
-xlabel('time (day)');
+ylabel('WGIR (m^3/day)'); xlabel('time (day)');
 
-subplot(5,2,8);
+subplot(5,2,8); title('gas production rate');
 plot(T, convertTo(qGs(:,2), meter^3/day));
-title('gas production rate');
-ylabel('WGPR (m^3/day)');
-xlabel('time (day)');
+ylabel('WGPR (m^3/day)'); xlabel('time (day)');
 
 qWPoly = getWellOutput(wellSolsPolymer, 'qWPoly');
 sign = getWellOutput(wellSolsPolymer, 'sign');
 qWPoly = sign .* qWPoly;
 
-subplot(5,2,9);
+subplot(5,2,9); title('polymer injection rate');
 plot(T, convertTo(qWPoly(:,1), kilogram/day));
-title('polymer injection rate');
-ylabel('WCIR (kg/day)');
-xlabel('time (day)');
+ylabel('WCIR (kg/day)'); xlabel('time (day)');
 
-subplot(5,2,10);
+subplot(5,2,10); title('polymer production rate');
 plot(T, convertTo(qWPoly(:,2), kilogram/day));
-title('polymer production rate');
-ylabel('WCPR (kg/day)');
-xlabel('time (day)');
-
-pause(0.5);
-
+ylabel('WCPR (kg/day)'); xlabel('time (day)');
 
 %% plotting the animated saturation evolution
 numStep = size(statesPolymer, 1);
+figure(2); set(gcf,'Position', [700, 100, 900, 600]);
 for iStep = 1:10:numStep
-
-    h = figure(2);
-    set(h, 'Position', [700, 100, 900, 600]);
-    clf;
-
-
     stateStep = statesPolymer{iStep,1};
 
-    subplot(2,2,1);
-    plotCellData(G, stateStep.s(:,1));
-    plotGrid(G, 'FaceColor', 'none')
-    title('Water saturation')
-    axis tight off
-    view(26,18);
-    colorbar;
+    for i=1:3,
+        subplot(2,2,i), cla
+        plotCellData(G, stateStep.s(:,i),'EdgeColor','k');
+        title([phases{i} ' saturation']);
+        axis tight off, view(26,18); colorbar; caxis([0 1]);
+    end
 
-
-    subplot(2,2,2);
-    plotCellData(G, stateStep.s(:,2));
-    plotGrid(G, 'FaceColor', 'none')
-    title('Oil saturation')
-    axis tight off
-    view(26,18);
-    colorbar;
-
-    subplot(2,2,3);
-    plotCellData(G, stateStep.s(:,3));
-    plotGrid(G, 'FaceColor', 'none')
-    title('Gas saturation')
-    axis tight off
-    view(26,18);
-    colorbar;
-
-    subplot(2,2,4);
-    plotCellData(G, stateStep.c);
-    plotGrid(G, 'FaceColor', 'none')
+    subplot(2,2,4); cla
+    plotCellData(G, stateStep.c,'EdgeColor','k');
     title('Polymer concentration')
-    axis tight off
-    view(26,18);
-    colorbar;
+    axis tight off; view(26,18); colorbar; caxis([0 1]);
 
-    pause(0.5);
+    drawnow; pause(0.5);
 
 end
 %%
