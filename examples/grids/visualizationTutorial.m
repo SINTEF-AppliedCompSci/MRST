f48775b29b163761e6ae745a81ee8c8619c11216
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-07-01 11:30:35 +0200

    Revised text, compacted some of the code, combined solution and plotting in the first time loop, and added copyright notice

--- examples/grids/visualizationTutorial.m
+++ examples/grids/visualizationTutorial.m
@@ -2,58 +2,19 @@
 % MRST contains a suite of visualization routines which make it easy to
 % create visualizations of grids and results. This tutorial show how to
 % visualize grids, subsets of grids and details the different routines
-% included in MRST.
-try
-    require incomp
-catch %#ok<CTCH>
-    mrstModule add incomp
-end
+% included in MRST. 
 
-%% We define a simple two phase example
-% The following is only meant as an example for the visualization and will
-% not be covered thourougly.
-% Create a simple 3D Cartesian grid
+%% PLotting grids
+% The |plotGrid| function is an essential part of MRST's visualization
+% routines. It simply draws a grid to a figure with a reversed z axis.
 G = cartGrid([10, 10, 3]);
 G = computeGeometry(G);
-% Disable gravity
-gravity off
-% Set up uniform permeability and constant porosity
-rock = makeRock(G, 100*milli*darcy, 0.5);
-% A simple two phase system.
-fluid = initSimpleFluid('mu' , [   1,  10]*centi*poise     , ...
-                            'rho', [1014, 859]*kilogram/meter^3, ...
-                            'n'  , [   2,   2]);
-% Two wells, an injector at 1 bar and a producer at 0 bar.
-W = verticalWell([], G, rock, 1, 1, [], ...
-            'Type', 'bhp' , 'Val', 1*barsa(), ...
-            'Radius', 0.1, 'InnerProduct', 'ip_tpf', ...
-            'Comp_i', [0, 1]);
-
-W = verticalWell(W, G, rock, 10, 10, [], ...
-            'Type', 'bhp' , 'Val', 0*barsa(), ...
-            'Radius', 0.1, 'InnerProduct', 'ip_tpf', ...
-            'Comp_i', [1, 0]);
-
-% Create a initialized state and set initial saturation to phase 1.
-sol = initState(G, [], 0, [1, 0]);
-
-% Find transmissibility.
-T = computeTrans(G, rock);
-
-% Reference TPFA
-psolve = @(state) incompTPFA(state, G, T, fluid, 'wells', W);
-
-% Implicit transport solver
-tsolve   = @(state, dT) implicitTransport(state, G, dT, rock, ...
-                                                fluid, 'wells', W);
-%% PlotGrid
-% PlotGrid is an essential part of MRST's visualization routines. It simply
-% draws a grid to a figure with a reversed z axis.
 clf;
 plotGrid(G)
 view(30,50)
+
 %% MRST and patch
-% All grid plotting routines are based on MATLAB's patch routine which
+% All grid plotting routines are based on MATLAB's patch routine, which
 % enables plotting of general polygons. Any keyword arguments will be
 % passed on to patch, which makes it possible to alter many attributes.
 % For instance, we can replot the grid with partially transparent edges and
@@ -61,8 +22,9 @@ view(30,50)
 clf;
 plotGrid(G, 'EdgeAlpha', 0.1, 'FaceColor', 'blue')
 view(30,50)
+
 %% plotGrid and subsets
-% plotGrid's second argument corresonds to a list of cells to be plotted.
+% plotGrid's second argument corresponds to a list of cells to be plotted.
 % This can be either logical indices (a logical vector of length
 % G.cells.num will plot Cell i if the ith element of the vector is true) or
 % an explicit list of indices, i.e. the cell numbers to be plotted.
@@ -74,34 +36,62 @@ equal_index = mod(1:G.cells.num,2) == 0;
 plotGrid(G,  equal_index, 'FaceColor', 'red')
 plotGrid(G, ~equal_index, 'FaceColor', 'blue')
 view(30,50)
-%% plotCellData
+
+%% Plotting cell properties
+% To illustrate the visualization of cell properties, we will use
+% routines from the |incomp| module to set up and solve a simple flow
+% problem. This is only meant as an example for the visualization and will
+% not be covered thourougly.
+mrstModule add incomp
+  
+gravity off                                       % Disable gravity
+rock = makeRock(G, 100*milli*darcy, 0.5);         % Uniform rock properties
+fluid = initSimpleFluid( ...                      % A simple fluid
+    'mu' , [   1,  10]*centi*poise     , ...      %  - viscosity
+    'rho', [1014, 859]*kilogram/meter^3, ...      %  - density
+    'n'  , [   2,   2]);                          %  - quadratic Corey relperms
+W = verticalWell([], G, rock, 1, 1, [], ...       % Injector in cell (1,1)
+            'Type', 'bhp' , 'Val', 1*barsa(), ... %   - bhp=1 bar
+            'Radius', 0.1, 'InnerProduct', 'ip_tpf', ...
+            'Comp_i', [0, 1]);
+W = verticalWell(W, G, rock, 10, 10, [], ...      % Producer in cell (10,10)
+            'Type', 'bhp' , 'Val', 0*barsa(), ... %   - bhp=0 bar
+            'Radius', 0.1, 'InnerProduct', 'ip_tpf', ...
+            'Comp_i', [1, 0]);
+sol = initState(G, [], 0, [1, 0]);                % Initial: filled with phase 1
+
+% Compute transmissibility and define function handles for solvers to avoid
+% having to retype parameters that stay constant throught the simulation
+T = computeTrans(G, rock);
+psolve = @(state) incompTPFA(state, G, T, fluid, 'wells', W);
+tsolve = @(state, dT) implicitTransport(state, G, dT, rock, fluid, 'wells', W);
+
+%% Plot the pressure distribution
 % Generally seeing only the grid is not that interesting. If we want to
 % show actual values, we need to use plotCellData. Let us solve the
 % reservoir pressure and plot it:
 sol= psolve(sol);
 clf;
 plotCellData(G, sol.pressure)
-colorbar
-view(30,50)
-%% plotCellData with subsets and plotWell
-% plotCellData is closely related to plotGrid. We can observe that the
-% pressure has its largest values in the wells, which makes it seem that
-% the pressure is almost homogeneous inside the domain. Let us then plot
-% pressure values for the middle of the domain. We also plot an empty grid
-% to see where we are actually plotting.
+colorbar, view(30,50)
+
+%% Plot subset of data
+% We can observe that the pressure has its largest values in the wells,
+% which makes it seem that the pressure is almost homogeneous inside the
+% domain. Let us then plot pressure values for the middle of the domain. We
+% also plot an empty grid to see where we are actually plotting. In
+% addition, we use the |plotWell| function to show wells.
 [i j k] = ind2sub(G.cartDims, 1:G.cells.num);
 clf;
 plotGrid(G, 'FaceAlpha', 0, 'EdgeAlpha', .1)
 plotCellData(G, sol.pressure, j == round(G.cartDims(2)/2))
-% Plot the wells
-plotWell(G, W);
-view(30,50)
-%% plotFaces
+plotWell(G, W); view(30,50)
+
+%% plotFaces - plotting face values
 % Some data is given at a per face basis instead of cells. For this purpose
 % we have plotFaces and plotFaceData which are analogous to plotGrid and
-% plotCellData respectively.
-
-% Plot Faces with positive normals in z direction
+% plotCellData respectively. To illustrate this function, we plot all faces
+% having positive normal in the z-direction
 clf;
 plotFaces(G, find(G.faces.normals(:,3)>0));
 view(30,50);
@@ -115,37 +105,40 @@ plotFaceData(G, G.faces.centroids(:,3));
 view(30,50);
 
 %% Animated example
-% We will now simulate a simple transport problem between the wells. We
-% will plot the empty grid and the wells, and in each time step we will
-% visualize the saturation in the cells where the inejcted phase is above a
-% threshold, here 0.05.
+% We will now simulate a simple transport problem between the wells and
+% create an animation that shows how the solution develops over time. To
+% this end, we plot the empty grid and the wells, and in each time step
+% only show the saturation in the cells where the saturation of the
+% injected phase is above a threshold, here 0.05. Here, we solve and plot
+% the solution simulaneously. We also store the time steps in a cell array
+% so that they can be extracted after the simulation has finished
 
-% We first solve the problem
+% Set up static parts of the plot
+clf
+plotGrid(G, 'FaceAlpha', 0, 'EdgeAlpha', .1)
+plotWell(G, W);
+view(30,50); pause(1)
+hs = [];  % handle for saturation plot, empty initially
+
+% Perform simulation
 dT = 10*day;
 solutions = cell(20,1);
 for i = 1:20
     sol = tsolve(sol, dT);
     sol = psolve(sol);
     solutions{i} = sol;
+    delete(hs)
+    hs = plotCellData(G, sol.s(:,2), sol.s(:,2)>0.05);
+    drawnow, pause(.5)
 end
 
-% Then plot
-for i = 1:20
-    clf;
-    sol = solutions{i};
-    plotCellData(G, sol.s(:,2), sol.s(:,2)>0.05)
-    plotGrid(G, 'FaceAlpha', 0, 'EdgeAlpha', .1)
-    plotWell(G, W);
-    view(30,50);
-    pause(.5)
-end
 %% Alternatively, use plotGridVolumes for the same purpose
-% plotGridVolumes is an alternative to plotCellData. While more
-% computationally intensive, it plots interpolated surfaces corresponding
-% to specific values of the saturation, giving a better indication of the
-% saturation front and a less clear indication of the saturation mixture in
-% sationary areas. These functions can be combined depending on the nature
-% of the data set.
+% The function |plotGridVolumes| is an alternative to |plotCellData|. While
+% more computationally intensive, it plots interpolated surfaces
+% corresponding to specific values of the saturation, giving a better
+% indication of the saturation front and a less clear indication of the
+% saturation mixture in sationary areas. These functions can be combined
+% depending on the nature of the data set.
 
 for i = 1:20
     clf;
@@ -156,3 +149,8 @@ for i = 1:20
     view(30,50);
     pause(.5)
 end
+
+%%
+displayEndOfDemoMessage(mfilename)
+
+% #COPYRIGHT_EXAMPLE#
