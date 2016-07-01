f298c17dfb0d829778e81f85c2ca2ff4fc1b602f
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-07-01 11:39:37 +0200

    Wrote an introduction to this example

--- examples/grids/perturbedAndStructuredGrid.m
+++ examples/grids/perturbedAndStructuredGrid.m
@@ -1,8 +1,13 @@
 %% Basic Grid Operations and Manipulations
-%
-% 
+% In this tutorial, we describe how you can use MRST to construct Cartesian
+% and rectilinear grids for rectangular and non-rectangular domains and
+% show how you can populate your grid with petrophysical properties. To
+% this end, we will grid a rectangular domain, from which we cut out a half
+% circle. This example is discussed in more detail in 
+% <http://www.sintef.no/projectweb/mrst/jolts/ Jolt 2>
 
 %% Tensor grid
+clf
 dx = 1 - 0.5*cos((-1:0.1:1)*pi);
 x = -1.15 + 0.1*cumsum(dx);
 y = 0:0.05:1;
@@ -46,3 +51,7 @@ nI(Gs.faces.nodes(n)) = false;
 
 Gs.nodes.coords(nI,:) = Gs.nodes.coords(nI,:) + 0.007*randn([sum(nI), 2]);
 cla, plotCellData(Gs, rock.perm);
+
+%% Copyright Notice
+%
+% #COPYRIGHT_EXAMPLE#
