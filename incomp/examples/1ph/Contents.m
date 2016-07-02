cac2d1257337010e4bc2dc83e279dc7c679e4d9c
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-07-02 03:08:00 +0200

    Moved files here from mrst-core/examples

---
+++ incomp/examples/1ph/Contents.m
@@ -0,0 +1,33 @@
+% One-phase flow examples.
+%
+% Files
+%   gravityColumn.m            - My First Flow Solver: Gravity Column
+%   monotonicityComparison.m   - Compare Mimetic, MPFA and TPA Pressures Solvers
+%   realField1phExample.m      - Pressure Solver: Example of a Real Field Model
+%   saigupField1phExample.m    - Pressure Solver: Example of a realistic Field Model
+%   secondTPFA.m               - Two-Point Flux Approximation Solvers with gravity
+%   simpleBC.m                 - Basic Flow-Solver Tutorial
+%   simpleCornerPointExample.m - Pressure Solver: Simple Corner-Point Grid with Linear Pressure Drop
+%   simpleSRCandBC.m           - How to Specify Sources and Boundary Conditions
+%   simpleTPFA.m               - Two-Point Flux Approximation Solvers
+%   simpleTimeOfFlight.m       - Script to calcuate timeof flight for a simple example
+%   simpleWellExample.m        - Using Peacemann Well Models
+
+%{
+Copyright 2009-2015 SINTEF ICT, Applied Mathematics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
---
+++ incomp/examples/2ph/inclinedGravityColumn.m
@@ -0,0 +1,98 @@
+%% Inclined Gravity Column
+% In this example, we simulate the injection of a light fluid (CO2) into
+% a heavier fluid (brine) inside an inclined sandbox.
+try
+    require incomp
+catch %#ok<CTCH>
+    mrstModule add incomp
+end
+
+n      = [ 20,  2,  100];
+box_sz = [100, 10, 200];
+G = cartGrid(n, box_sz);
+G = computeGeometry(G);
+
+%% Set rock and fluid parameters
+rock = makeRock(G, 0.1*darcy, 0.3);
+           
+fluid = initSimpleFluid('mu' , [  0.307,   0.049] .* centi*poise     , ...
+                        'rho', [973    , 617    ] .* kilogram/meter^3, ...
+                        'n'  , [  2    ,   2    ]);
+
+%% Set gravity direction
+rot   = @(theta) makehgtform('xrotate',  theta(1), ...
+                             'yrotate', -theta(2), ...
+                             'zrotate', -theta(3));
+mul   = @(a,b,n) a(1:n,1:n) * reshape(b(1:n), [], 1);
+angle = [0, pi/4, 0];
+gravity reset on
+gravity(mul(rot(angle), gravity(), 3));
+
+%% Assemble linsys components
+T = computeTrans(G, rock, 'verbose', true);
+
+%% Solve flow problem
+% Put region of CO2 at bottom of reservoir.
+xr = initResSol(G, 1*barsa, 1);
+d  = gravity() ./ norm(gravity);
+c  = G.cells.centroids * d.' > 110;
+xr.s(c) = 0;
+xr = incompTPFA(xr, G, T, fluid);
+
+%% Plot flow output
+cla reset
+ax = gca;
+h = plotCellData(G, convertTo(xr.pressure(1:G.cells.num), barsa), ...
+                 'EdgeColor', 'k', 'EdgeAlpha', 0.1, 'FaceAlpha', 0.625);
+trans = hgtransform;
+set(trans, 'Parent', ax, 'Matrix', ...
+    makehgtform('translate', [ box_sz(1), 0,  box_sz(3)],    ...
+                'xrotate'  , -angle(1), 'yrotate', angle(2), ...
+                'translate', [-box_sz(1), 0, -box_sz(3)]));
+set(h, 'Parent', trans);
+view([0,0]), grid on, %camproj perspective
+colorbar
+
+mat = get(trans, 'Matrix');
+
+%%
+dT = [1, 2, 2, 5, 5, 10, 15, 20, 40, 50, 50, ...
+      100, 100, 200, 200, 300, 400, 500] .* day();
+dT = [dT, [2, 2, 2, 4, 5, 5, 10, 10, repmat(15, [1, 34])].*year()]/200;
+
+clf, ax = gca;
+h = plotGrid(G, 'FaceColor', 'none', 'EdgeAlpha', 0.1);
+trans = hgtransform;
+set(trans, 'Parent', ax, 'Matrix', mat);
+set(h, 'Parent', trans);
+view(0,0);
+thresh = 2;
+hs = plotCellData(G, xr.s, find(xr.s < thresh), 'EdgeColor', 'k', ...
+                  'EdgeAlpha', 0.1, 'FaceAlpha', 0.25);
+set(hs, 'Parent', trans)
+
+s  = linspace(0, 1, 8192).';
+cm = [1-s.^(13/16), 1-s.^6, s.^6];
+colormap(cm)
+colorbar
+%%
+t = 0;
+for k = 1 : numel(dT),
+   xr = implicitTransport(xr, G, dT(k), rock, fluid, 'Verbose', false);
+
+   % Check for inconsistent saturations
+   assert (max(xr.s) < 1+eps && min(xr.s) > -eps);
+
+   % Increase time and plot saturation
+   t = t + dT(k);
+   delete(hs)
+   hs = plotCellData(G, xr.s, find(xr.s < thresh), 'EdgeColor', 'k', ...
+                     'EdgeAlpha', 0.1, 'FaceAlpha', 0.25);
+   set(hs, 'Parent', trans)
+   caxis([0, 1]);
+    view(0,0)
+   drawnow
+
+   % Compute new flow field.
+   xr = incompTPFA(xr, G, T, fluid);
+end
