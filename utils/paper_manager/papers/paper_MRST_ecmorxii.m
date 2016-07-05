b315725d32cbbfda1b1bf1521bc1606ed86210ea
Olav Møyner <olav.moyner@sintef.no>
2016-07-05 14:24:24 +0200

    Adding more papers: MRST book, old ecmor MRST paper, various msrsb papers and a diagnostics paper.

---
+++ utils/paper_manager/papers/paper_MRST_ecmorxii.m
@@ -0,0 +1,28 @@
+function paper = paper_MRST_ecmorxii()
+%{
+Copyright 2009-2015 SINTEF ICT, Applied Mathematics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+paper = createPaperStruct('mrst-book', ...
+                          'An Introduction to Reservoir Simulation Using MATLAB', ...
+                          'authors', 'K.-A. Lie, S. Krogstad, I. S. Ligaarden, J. R. Natvig, H. M. Nilsen, and B. Skaflestad', ...
+                          'published', 'Proceedings of ECMOR XII, Oxford, UK, 6-9 September 2010', ...
+                          'year', 2010, ...
+                          'modules', {'incomp', 'mimetic', 'mpfa'}, ...
+                          'url', 'http://www.earthdoc.org/publication/publicationdetails/?publication=41326', ...
+                          'fileurl', 'http://folk.uio.no/kalie/papers/ecmor-xii-mrst.pdf');
+end
