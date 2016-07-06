57561feabc2c4d6305b5a94f9b6914779b09f4e5
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-07-06 21:05:51 +0200

    Added copyright notice

--- ad-blackoil/utils/getFluxAndPropsGas_BO.m
+++ ad-blackoil/utils/getFluxAndPropsGas_BO.m
@@ -34,3 +34,22 @@ function [vG, bG, mobG, rhoG, pG, upcg, dpG] = getFluxAndPropsGas_BO(model, pO, 
     upcg    = (double(dpG)<=0);
     vG = - s.faceUpstr(upcg, mobG).*T.*dpG;
 end
+
+%{
+Copyright 2009-2016 SINTEF ICT, Applied Mathematics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
--- ad-blackoil/utils/getFluxAndPropsOil_BO.m
+++ ad-blackoil/utils/getFluxAndPropsOil_BO.m
@@ -34,3 +34,22 @@ function [vO, bO, mobO, rhoO, p, upco, dpO] = getFluxAndPropsOil_BO(model, p, sO
     upco = (double(dpO)<=0);
     vO   = - s.faceUpstr(upco, mobO).*T.*dpO;
 end
+
+%{
+Copyright 2009-2016 SINTEF ICT, Applied Mathematics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
