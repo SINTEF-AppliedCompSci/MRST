8e2778862b9bb976001f6986c9835f60bf42b341
Xavier Raynaud <xav.raynaud@gmail.com>
2016-07-06 23:42:21 +0200

    commented function mrstGridWithFullMappings

--- vemmech/grid_utils/mrstGridWithFullMappings.m
+++ vemmech/grid_utils/mrstGridWithFullMappings.m
@@ -1,70 +1,62 @@
 function G = mrstGridWithFullMappings(G)
-% Add all mappings to mrst grid. This is needed for VEM of finite element
-% type methods. 
+%
 %
 % SYNOPSIS:
-%   G = mrstGridWithFullMappings(G)
-%   
+%   function G = mrstGridWithFullMappings(G)
+%
+% DESCRIPTION: The grid structure as described by grid_structure lacks some
+% mappings and structures that are needed for the assembly of the VEM
+% method. Those are added by calling the function mrstGridWithFullMappings.
 %
 % PARAMETERS:
-%   G       - Grid structure as described by grid_structure.
+%   G - Grid structure as described by grid_structure.
 %
-% OPTIONAL PARAMETERS:
+% RETURNS:
+%   G - Grid structure as described by full_grid_structure (call the
+%   function full_grid_structure to display the documentation).
 %
-%   'hingenodes'  - A struct with fields 'faces' and 'nodes'.  A hinge node
-%                   is an extra center node for a face, that is used to
-%                   triangulate the face geometry.  For each face number F
-%                   in 'faces', there is a row in 'nodes' which holds the
-%                   node coordinate for the hinge node belonging to face F.
+%   The method assumes that the edges are sorted. The sorting rule can be found
+%   found the documentation displayed by calling full_grid_structure. The
+%   function sortEdges.m outputs a grid for which the edges are guaranted to
+%   be sorted.
 %
-%
-% COMMENTS:
-%   At the moment, mappings that are not needed may be constructed. The mapping
-%   from local nodes to global is not calculated and needs to be made localy
-%   if needed,  for example by using a sparse matrix.
-%   The methods assume that edges and faces are sorted.
+% EXAMPLE:
 %
 % SEE ALSO:
-%   grid_structure. calulateGeometryCalc,  VEM_linElast
 %
 
-%{ 
-Copyright 2009-2014 SINTEF ICT, Applied Mathematics
-%} 
 
-    if(G.griddim == 3)
-        ind1 = mcolon(G.faces.nodePos(1:end-1), G.faces.nodePos(2:end)-1, 1);
-        ind2 = mcolon(G.faces.nodePos(1:end-1)+1, G.faces.nodePos(2:end), 1);
-        ind2(G.faces.nodePos(2:end)-1) = G.faces.nodePos(1:end-1);
+    if (G.griddim == 3)
+        ind1 = mcolon(G.faces.nodePos(1 : end - 1), G.faces.nodePos(2 : end) - 1, 1);
+        ind2 = mcolon(G.faces.nodePos(1 : end - 1) + 1, G.faces.nodePos(2 : end), 1);
+        ind2(G.faces.nodePos(2 : end) - 1) = G.faces.nodePos(1 : end - 1);
         eh = [G.faces.nodes(ind1), G.faces.nodes(ind2)];
         ehs = sort(eh, 2);
         [e, ~, j] = unique(ehs, 'rows', 'sorted');
         assert(all(all(e(j, :) == ehs)))
         
         G.faces.edges = j;
-        G.faces.edgePos = [1;cumsum(diff(G.faces.nodePos))+1];
-        G.faces.edgeSign = 2*(e(j, 1) == eh(:, 1))-1;
+        G.faces.edgePos = [1;cumsum(diff(G.faces.nodePos)) + 1];
+        G.faces.edgeSign = 2*(e(j, 1) == eh(:, 1)) - 1;
         
-        % make ting si pos format
         e = e';
         ne = size(e', 1);
         G.edges.nodes = e(:);
         nPos = cumsum(repmat(2, ne, 1));
-        G.edges.nodePos = [1;nPos+1];
+        G.edges.nodePos = [1;nPos + 1];
         G.edges.num = ne;
     else
         faces = G.cells.faces(:, 1);
-        inodes = mcolon(G.faces.nodePos(faces), G.faces.nodePos(faces+1)-1);
+        inodes = mcolon(G.faces.nodePos(faces), G.faces.nodePos(faces + 1) - 1);
         assert(2*numel(faces) == numel(inodes))
-        cell = rldecode([1:G.cells.num]', diff(G.cells.facePos));%#ok
-        sign = 2*(cell == G.faces.neighbors(faces, 1))-1;
-        nf = [1:2:numel(inodes)]'-(sign-1)/2;
+        cell = rldecode([1 : G.cells.num]', diff(G.cells.facePos));%#ok
+        sign = 2*(cell == G.faces.neighbors(faces, 1)) - 1;
+        nf = [1 : 2 : numel(inodes)]' - (sign - 1)/2;
         nodes = G.faces.nodes(inodes(nf));
         G.cells.nodes = nodes;
         G.cells.nodePos = G.cells.facePos;
     end
     w = createGridMappings(G);
-
 
     %% Setup node to face mapping
     n2f = unique([w(:, 2), w(:, 4)], 'rows', 'sorted');
@@ -72,9 +64,8 @@ Copyright 2009-2014 SINTEF ICT, Applied Mathematics
     G.nodes.facePos = pos;
     G.nodes.faces = val;
 
-
     %% Setup cell to node mapping
-    if(G.griddim == 3)
+    if (G.griddim == 3)
         c2n = unique([w(:, 1), w(:, 2)], 'rows', 'sorted');
         [pos, val] = map2Pos(c2n);
         G.cells.nodePos = pos;
@@ -92,7 +83,7 @@ end
 
 function [pos, val] = map2Pos(nn)
     ind = find(diff(nn(:, 1)) == 1);
-    pos = [1;ind+1;size(nn, 1)+1];
+    pos = [1; ind + 1; size(nn, 1) + 1];
     val = nn(:, 2);
 end
 
