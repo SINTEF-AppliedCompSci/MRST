3d16420c88d5d7ff90da0d71ba5f83b04c28599b
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-07-06 21:26:13 +0200

    Added copyright notice

--- ad-props/simple/coreyPhaseRelpermAD.m
+++ ad-props/simple/coreyPhaseRelpermAD.m
@@ -1,10 +1,29 @@
 function fn = coreyPhaseRelpermAD(n, sr, kwm, sr_tot)
 % Make function handle for AD style fluids
+%
+% n - exponent
+% sr - residual saturation
+% kwm - endpoint relperm
+% sr_tot - sum of sr for all phases present
 
-    % n - exponent
-    % sr - residual saturation
-    % kwm - endpoint relperm
-    % sr_tot - sum of sr for all phases present
+%{
+Copyright 2009-2016 SINTEF ICT, Applied Mathematics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
     
     if nargin < 1
         n = 1;
