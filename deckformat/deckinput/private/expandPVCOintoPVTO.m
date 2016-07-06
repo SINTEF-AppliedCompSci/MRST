0c979de25e3bc1100e3f56b8a87faa40e6019c9b
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-07-06 22:05:39 +0200

    Added copyright notice

--- deckformat/deckinput/private/expandPVCOintoPVTO.m
+++ deckformat/deckinput/private/expandPVCOintoPVTO.m
@@ -1,6 +1,24 @@
 function T = expandPVCOintoPVTO(tbl, ntab)
 % Utility for expanding a PVCO table into a PVTO table
-    T = cell([ntab, 1]);
+ %{
+Copyright 2009-2016 SINTEF ICT, Applied Mathematics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+   T = cell([ntab, 1]);
     
     % Relative increments used to evaluate undersaturated curves
     dp = [1.01, 1.05, 1.1, 1.25];
--- deckformat/deckinput/readVFPINJ.m
+++ deckformat/deckinput/readVFPINJ.m
@@ -1,4 +1,23 @@
 function [tid, tab] = readVFPINJ(fid)
+%{
+Copyright 2009-2016 SINTEF ICT, Applied Mathematics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
    r1 = { '0', '0.0', 'OIL', 'THP', 'USYS', 'BHP' };
 
    data = readDefaultedRecord(fid, r1);
