3e227597a027520da4270988d00a600a858eede7
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-07-06 21:31:40 +0200

    Added copyright notice

--- blackoil-sequential/examples/sequentialSPE1.m
+++ blackoil-sequential/examples/sequentialSPE1.m
@@ -143,3 +143,32 @@ for i = 1:numel(states)
 end
 
 plotWellSols(wsol, T, 'datasetnames', wnames)
+
+
+%% Copyright notice
+
+% <html>
+% <p><font size="-1">
+% Copyright 2009-2016 SINTEF ICT, Applied Mathematics.
+% </font></p>
+% <p><font size="-1">
+% This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+% </font></p>
+% <p><font size="-1">
+% MRST is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% </font></p>
+% <p><font size="-1">
+% MRST is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% </font></p>
+% <p><font size="-1">
+% You should have received a copy of the GNU General Public License
+% along with MRST.  If not, see
+% <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses</a>.
+% </font></p>
+% </html>
