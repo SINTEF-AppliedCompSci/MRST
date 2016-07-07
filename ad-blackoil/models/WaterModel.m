f9391be1eefd79dac5501c09bca674dd6c83054e
Olav Møyner <olav.moyner@sintef.no>
2016-07-07 19:06:24 +0200

    WaterModel: Remove copy/paste documentation and replace with correct statements.

--- ad-blackoil/models/WaterModel.m
+++ ad-blackoil/models/WaterModel.m
@@ -1,30 +1,22 @@
 classdef WaterModel < ReservoirModel
-    % Two phase oil/water system without dissolution
+    % Single phase water model. 
     properties
 
     end
     
     methods
         function model = WaterModel(G, rock, fluid, varargin)
-            
             model = model@ReservoirModel(G, rock, fluid);
-            
-            % This is the model parameters for oil/water
+            % Only enable water
             model.oil = false;
             model.gas = false;
             model.water = true;
-            %model.addflux=true;
-            
-            % Blackoil -> use CNV style convergence 
             model.useCNVConvergence = false;
             
             model.saturationVarNames = {'sw'};
             model.wellVarNames = {'qWs', 'bhp'};
             
             model = merge_options(model, varargin{:});
-            
-            % Setup operators
-            %model = model.setupOperators(G, rock, 'deck', model.inputdata);
         end
         
         function [problem, state] = getEquations(model, state0, state, dt, drivingForces, varargin)
@@ -38,8 +30,6 @@ classdef WaterModel < ReservoirModel
         function [state, report] = updateState(model, state, problem, dx, drivingForces)
             % Parent class handles almost everything for us
             [state, report] = updateState@ReservoirModel(model, state, problem, dx, drivingForces);
-            
-            % Update wells based on black oil specific properties
             saturations = model.saturationVarNames;
             wi = strcmpi(saturations, 'sw');
             oi = strcmpi(saturations, 'so');
@@ -47,7 +37,6 @@ classdef WaterModel < ReservoirModel
 
             W = drivingForces.W;
             state.wellSol = assignWellValuesFromControl(model, state.wellSol, W, wi, oi, gi);
-
         end
     end
 end
