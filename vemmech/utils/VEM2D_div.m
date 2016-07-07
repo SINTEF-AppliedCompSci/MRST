62ddd749ca8e1fc48eae80ec2feaf158685ee4e7
Xavier Raynaud <xav.raynaud@gmail.com>
2016-07-07 15:07:26 +0200

    Reintroduced option about extra face-degrees of freedom.

--- vemmech/utils/VEM2D_div.m
+++ vemmech/utils/VEM2D_div.m
@@ -16,6 +16,11 @@ function [div] = VEM2D_div(G)
 % PARAMETERS:
 %   G        - Grid structure
 %
+% OPTIONAL PARAMETERS (supplied in 'key'/value pairs ('pn'/pv ...)):
+%
+%   'extraFaceDof' - This option has to be set to get a stable divergence
+%                    operator when extra degrees of freedom have been introduced
+%                    on the edges to avoid numerical locking.
 % RETURNS:
 %   div - matrix corresponding to the discrete div operator.
 %
@@ -24,12 +29,27 @@ function [div] = VEM2D_div(G)
 % SEE ALSO:
 %
 
+    opt = struct('extraFaceDof', false); 
+    opt = merge_options(opt, varargin{:}); 
+
     qf     = calculateQF(G);
     qf     = reshape(qf', [], 1);
     dofs   = mcolon(G.griddim * (G.cells.nodes - 1) + 1, ...
                     G.griddim * (G.cells.nodes - 1) + G.griddim);
     ndofs  = G.nodes.num * G.griddim;
     cellno = rldecode([1:G.cells.num]', diff(G.cells.nodePos) * G.griddim);
+    
+    if (opt.extraFaceDof)
+        ncf        = diff(G.cells.facePos); 
+        fcellno    = rldecode([1:G.cells.num]', ncf); 
+        faces      = G.cells.faces(:, 1); 
+        fasgn      = (2 * (G.faces.neighbors(faces, 2) == fcellno) - 1) .* ...
+            G.faces.areas(faces); 
+        cellno     = [cellno; fcellno]; dofs = [dofs'; ndofs + faces]; 
+        qf         = [qf; fasgn]; 
+        ndofs      = ndofs + G.faces.num; 
+    end    
+    
     div = sparse(cellno, dofs, qf, G.cells.num, ndofs);
 
 end
