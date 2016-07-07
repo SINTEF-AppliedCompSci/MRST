303ca618beee8b78c28bcf9837becea1826d17b2
Xavier Raynaud <xav.raynaud@gmail.com>
2016-07-08 01:33:16 +0200

    Cleaned up and documented example_2D_layers

--- vemmech/examples/example_2D_layers.m
+++ vemmech/examples/example_2D_layers.m
@@ -1,62 +1,61 @@
-%% Example using a complex grid to test mechanical solver
-%  It is possible to vary the material parametes in layers
-%  
-E = [1; 10; 1]; % define youngs modulo of layres
-nu = [0.3; 0.3; 0.3]; % define Poison ratio of layers
-zlayers = [0.3; 0.3; 0.3]; % values of layers
-zlayers = 1/sum(zlayers)*zlayers; 
-L = [1 1]; % define domain size
-dims = [10 10]; % define grid dimensions
+%% Example using three layers with different parameters in each layer
+%
+% Use the utility function squareLayersTest to set up the problem. You may look
+% at the documentation of this function and try the other settings presented there
+% by modifying this example.
 
-% Using the utiliy function squareLayerTests several types of grids are
-% possible se this function for definitions
+%% Set up the rock parameters and the grid
+%
 
-% define grids
-% grid_type = 'cartgrid'; % define type of trid
-% grid_type = 'triangle';
-grid_type = 'mixed4';dims= [4 4]; %this needs small grid size  
+E  = [1; 10; 1];      % Define the Youngs moduli in the layers
+nu = [0.3; 0.3; 0.3]; % Define the Poisson's ratio in the layers
 
-% also define testcase type i.e. boundary conditions and forces
-% test_name = 'original_2D_test';
-test_name = 'hanging_rod_2D'
+zlayers = [0.3; 0.3; 0.3]; % Define the width of the layers.
+zlayers = 1/sum(zlayers)*zlayers;
+L    = [1 1];   % Define the domain size
 
-disturb = 0.042;% use twister to distrube grid
+% Define the grid type, for example 'mixed4'
+grid_type = 'mixed4';
+dims= [4 4];
 
-% define test cases by a utility function
+% Define test case type. It determines the boundary conditions and the forces
+test_name = 'hanging_rod_2D';
+% Parameter used to twist the grid so that the Cartesian symmetries are
+% broken and we can test the method on more irregular grid.
+disturb = 0.042;
+
+% The utility function squareLayersTest sets up the problem.
 [G, bc, test_cases] = squareLayersTest('E', E, 'nu', nu, 'zlayers', zlayers, 'cartDims', ...
                                        dims, 'L', L, 'grid_type', grid_type, 'disturb', ...
                                        disturb, 'make_testcases', false, 'test_name', ...
                                        test_name);
+G = computeGeometry(G);
 
-                                   G = computeGeometry(G);
 
-
-%  get the test case paramters
+% We recover the problem parameters using the structure test_cases
 testcase = test_cases{1};
-% boundary condition for mechanics
-el_bc = testcase.el_bc;
-% load
-load = testcase.load;
-% material parameters in Voigts notation
-C = testcase.C;
-% Solve the linear elastic system using virtual element methods
+el_bc    = testcase.el_bc; % The boundary conditions
+load     = testcase.load;  % The load
+C        = testcase.C;     % The material parameters in Voigts notations
+
+%% We assemble and solve the equations
+%
 [uVEM, extra] = VEM_linElast(G, C, el_bc, load);
 
-% plot results
+%% We plot the results
+%
 plotopts = {'EdgeAlpha', 0.0, 'EdgeColor', 'none'};
 plotopts1 = {'EdgeAlpha', 0.1};
-figure(1), clf
-subplot(3, 1, 1)
+figure();
+subplot(3, 1, 1);
 plotNodeData(G, uVEM(:, 1), plotopts{:}); colorbar();
-title('x displacement')
-subplot(3, 1, 2)
+title('x displacement');
+subplot(3, 1, 2);
 plotNodeData(G, uVEM(:, 2), plotopts{:}); colorbar();
-title('y displacement')
-subplot(3, 1, 3)
-% calculate div operator
+title('y displacement');
+subplot(3, 1, 3);
+% We compute the divergence
 vdiv = VEM2D_div(G);
 mdiv = vdiv*reshape(uVEM', [], 1)./G.cells.volumes;
-title('div')
+title('Divergence');
 plotCellDataDeformed(G, mdiv, uVEM, plotopts1{:}); colorbar();
-
-
