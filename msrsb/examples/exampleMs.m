7a948ac57d96e8945a480f4e7cea4283178f8047
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-07-07 11:57:34 +0200

    Revised and expanded introduction text. No functional changes

--- msrsb/examples/exampleMs.m
+++ msrsb/examples/exampleMs.m
@@ -1,19 +1,25 @@
-%% Simple conceptual multiscale example
+%% Simple Conceptual Multiscale Example (Layers of SPE10)
 % This example goes through the construction of a simple multiscale solver
-% based on smoothed basis functions. We begin by defining the fine scale
-% problem and plotting the permeability field.
+% based on restriction-smoothed basis functions. We begin by defining the
+% fine-scale problem and plotting the permeability field, which represents
+% a number of layers sampled from the SPE10 model. Layers from 1 to 35 will
+% result in smooth, lognormal permeabilities, whereas layers 36 to 85 will
+% result in channelized formations that are considered to be significantly
+% more challenging for upscaling and multiscale methods.
+%
+% We solve the flow problem using three methods: a fine-scale TPFA solver
+% from the 'incomp' module, the multiscale finite-volume (MsFV) method from
+% the 'msfv' module, and the MsRSB method implemented in this module.
 
-% This value corresponds to the layers of the SPE10 dataset we are going to
-% use. It can either be a single value or multiple values. Values from 1 to
-% 35 will result in smooth, lognormal values while the values from 36 to 85
-% will result in channelized formations that are considered to be
-% significantly more challenging to upscale.
-
+%%  Set up the fine-scale model
 layerNo = 85;
 % layerNo = 1;
 
 mrstModule add mrst-gui spe10 coarsegrid incomp
 [G, ~, rock] = SPE10_setup(layerNo);
+
+% Transmissibilities on fine scale
+T = computeTrans(G, rock);
 
 % Boundary conditions corresponding to unit pressure drop over the entire
 % domain
@@ -21,11 +27,10 @@ bc = [];
 bc = pside(bc, G, 'ymin', 0);
 bc = pside(bc, G, 'ymax', 1);
 
-% Transmissibilities on fine scale
-T = computeTrans(G, rock);
-% Single phase unit fluid model. Since we are solving a single-phase
-% incompressible problem without gravity, the values here does not matter.
+% Single-phase unit fluid model. Since we are solving a single-phase
+% incompressible problem without gravity, the values here do not matter.
 fluid = initSingleFluid('rho', 1, 'mu', 1);
+
 % Set up empty solution structure.
 state = initResSol(G, 0);
 
@@ -36,7 +41,7 @@ axis equal tight off
 colormap jet
 view(90, 90);
 
-%% Solve fine scale problem
+%% Solve fine-scale problem
 state = incompTPFA(state, G, T, fluid, 'MatrixOutput', true, 'bc', bc);
 
 %% Set up coarsegrid
@@ -58,6 +63,7 @@ mrstModule add msfvm
 DG = partitionUIdual(CG, coarsedims);
 DG = makeExplicitDual(CG, DG);
 CG.dual = DG;
+
 %% Set up basis functions
 A = getIncomp1PhMatrix(G, T);
 % B = iteratedJacobiBasis(A, CG);
@@ -117,3 +123,31 @@ names = cellfun(@(x) x.type, basises, 'uniformoutput', false);
 semilogy(tmp, '*-')
 legend(names)
 title('Convergence of GMRES')
+
+%% Copyright notice
+
+% <html>
+% <p><font size="-1">
+% Copyright 2009-2016 SINTEF ICT, Applied Mathematics.
+% </font></p>
+% <p><font size="-1">
+% This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+% </font></p>
+% <p><font size="-1">
+% MRST is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% </font></p>
+% <p><font size="-1">
+% MRST is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% </font></p>
+% <p><font size="-1">
+% You should have received a copy of the GNU General Public License
+% along with MRST.  If not, see
+% <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses</a>.
+% </font></p>
+% </html>
