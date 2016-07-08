8a76458f3ca8b1de4759ade1d4c15390c527f9dd
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2016-07-08 13:58:53 +0200

    Restart Examples: Support automatic output download from GitHub
    Reimplement restart examples in terms of new function
    
        makeSPE9OutputAvailable
    
    and getDatasetPath('spe9').  This obviates the hard-coded pathnames
    that were previously employed.

--- deckformat/resultinput/examples/diagnosticsFromEclipseOutput.m
+++ deckformat/resultinput/examples/diagnosticsFromEclipseOutput.m
@@ -1,10 +1,17 @@
 %% Read/convert ECLIPSE output and visualize using interactive diagnostics
-% This example reads ECLIPSE unformatted output files from a simulation based 
-% on the SPE9 benchmark. The data is converted to MRST grid and soluion
-% structures and used as input to the interactive diagnostics functionality. 
+% This example reads ECLIPSE unformatted output files from a simulation
+% based on the SPE-9 benchmark. The data is converted to MRST grid and
+% soluion structures and used as input to the interactive diagnostics
+% functionality.
+
 mrstModule add mrst-gui ad-core diagnostics
 
-prefix =  '/Users/steink/data/opm-data/spe9/eclipse-simulation/SPE9_CP';
+if ~ makeSPE9OutputAvailable,
+   error('SPE9Download:Failure', ...
+         'Failed to download ECLIPSE output for SPE-9 benchmark case');
+end
+
+prefix = fullfile(getDatasetPath('spe9'), 'Simulation-Output', 'SPE9_CP');
 
 %% Read INIT/EGRID-files and construct MRST-grid
 init = readEclipseOutputFileUnFmt([prefix, '.INIT']);
@@ -21,14 +28,18 @@ state = states{1};
 % The wellSol-field of state contains sufficient data to work as a
 % well-struct (W).
 W = state.wellSol;
+
 %%
-% If flux field is not given in Eclipse restart (as here), the converted 
-% state will not contain fluxes either. In this case, interactiveDiagnostics 
-% will need solve an incompressible pressure equation. We therefore need to
-% adjust W such that its control settings are compatible with the
-% incompressible solvers. Here we set W(1) (the injector) to be controlled by bhp, 
-% and W(2:end) (producers) to be controlled by rate. We set the rate values to the
-% reservoir volume rates computed by ECLIPSE (resv)
+% If flux field is not given in ECLIPSE restart (as here), the converted
+% state will not contain fluxes either. In this case,
+% interactiveDiagnostics will need to solve an incompressible pressure
+% equation in order to reconstruct a reasonable flux field. We therefore
+% need to adjust |W| such that its control settings are compatible with the
+% incompressible solvers. Here we set |W(1)| (the injector) to be
+% controlled by bottom-hole pressure target (BHP), and |W(2:end)| (the
+% producers) to be controlled by rate. We set the rate values to the
+% reservoir volume rates computed by ECLIPSE (resv).
+
 [W(1).type, W(1).val] = deal('bhp', W(1).bhp);
 for wnum = 2:numel(W)
     [W(wnum).type, W(wnum).val] = deal('rate', W(wnum).resv);
@@ -36,4 +47,4 @@ end
 
 %% Run Interactive diagnostics
 close all, interactiveDiagnostics(G, rock, W, 'state', state);
-figure(1), daspect([1 1 .3]), view([1 -.5 1])
+figure(1), daspect([1, 1, 0.3]), view([1, -0.5, 1])
--- deckformat/resultinput/examples/readAndConvertEclipseOutput.m
+++ deckformat/resultinput/examples/readAndConvertEclipseOutput.m
@@ -1,14 +1,20 @@
 %% Read ECLIPSE output and convert to MRST grid and solution structures
-% This example reads ECLIPSE unformatted output files from a simulation based 
-% on the SPE9 benchmark. The MRST grid structure (G) is generated using output
-% from *.INIT and *.EGRID. The solution structure (states and states{i}.wellSol)
-% are generated using output from *.UNRST.
+% This example reads ECLIPSE unformatted output files from a simulation
+% based on the SPE-9 benchmark. The MRST grid structure (G) is generated
+% using output from *.INIT and *.EGRID. The solution structure (states and
+% |states{i}.wellSol|) are generated using output from *.UNRST.
 
 mrstModule add mrst-gui ad-core
-prefix =  '/Users/steink/data/opm-data/spe9/eclipse-simulation/SPE9_CP';
+
+if ~ makeSPE9OutputAvailable,
+   error('SPE9Download:Failure', ...
+         'Failed to download ECLIPSE output for SPE-9 benchmark case');
+end
+
+prefix = fullfile(getDatasetPath('spe9'), 'Simulation-Output', 'SPE9_CP');
 
 %% Read INIT/EGRID-files and generate MRST-grid
-init =  readEclipseOutputFileUnFmt([prefix, '.INIT']);
+init  = readEclipseOutputFileUnFmt([prefix, '.INIT']);
 egrid = readEclipseOutputFileUnFmt([prefix, '.EGRID']);
 [G, rock] = eclOut2mrst(init, egrid);
 G = computeGeometry(G);
@@ -24,9 +30,9 @@ time     = cellfun(@(x)x.time, states);
 
 %% Plot model with wells
 figure, plotToolbar(G, states);
-plotWellData(G, wellSols{1}, 'labelFontSize', 10, 'color', [.3 .3 .3]);
-axis off vis3d tight, view([1 -.5 1]), camproj perspective
+plotWellData(G, wellSols{1}, 'labelFontSize', 10, ...
+             'color', [0.3, 0.3, 0.3]);
+axis off vis3d tight, view([1, -0.5, 1]), camproj perspective
 
 %% Plot well solutions for restart time-steps
 plotWellSols(wellSols, time);
-
