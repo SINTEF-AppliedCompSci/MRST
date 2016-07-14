6cda9502fe77413ab1fb93211cae3b0c93fcb23d
Rebecca Allen <allen.rebecca.d@gmail.com>
2016-07-14 18:45:07 +0200

    Bug-fix in top_cell_of_trap.m
    - error occurs when top of trap is located at more than one cell. The
    purpose of this routine is simply to return one cell per trap for trap
    labeling, thus we only take one cell.

--- co2lab/examples/leakage_forecast/utils/top_cell_of_trap.m
+++ co2lab/examples/leakage_forecast/utils/top_cell_of_trap.m
@@ -1,15 +1,17 @@
-function top = top_cell_of_trap(Gt, ta)
+function tops = top_cell_of_trap(Gt, ta)
 % explicitly get top cells of each trap region
 % (in case ta.tops is not indexed properly according to trap number)
 
     num_traps = numel(ta.trap_z);
-    top = [];
+    tops = [];
     for i = 1:num_traps
         tcells = find(ta.traps == i);
         %tz = zeros(Gt.cells.num,1);
         tz = NaN(Gt.cells.num,1);
         tz(tcells) = Gt.cells.z(tcells);
-        top(i,1) = find(tz == min(tz)); % tops at at the min elevations of each trap region
+        top = find(tz == min(tz)); % top at the min elevation of trap i
+        tops(i,1) = top(1);  % the top elevation may occur at more than one cell, so we only
+                             % need to take one cell to get the top
         clear tcells
     end
 
