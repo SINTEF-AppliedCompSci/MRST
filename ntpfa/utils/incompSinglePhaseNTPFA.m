d423489a67558805e5b620d09abfb97f2dc10a4f
Olav Møyner <olav.moyner@sintef.no>
2016-10-31 10:50:13 +0100

    Improve compatability for single phase solver with incomp solvers used for paper.

--- ntpfa/utils/incompSinglePhaseNTPFA.m
+++ ntpfa/utils/incompSinglePhaseNTPFA.m
@@ -1,4 +1,10 @@
 function state = incompSinglePhaseNTPFA(model, varargin)
+    if mod(numel(varargin), 2) > 0
+        state0 = varargin{1};
+        varargin = varargin(2:end);
+    else
+        state0 = initResSol(model.G, 0, [1, 0]);
+    end
     opt = struct('Wells', [], 'src', [], 'bc', []);
     opt = merge_options(opt, varargin{:});
     
@@ -14,9 +20,16 @@ function state = incompSinglePhaseNTPFA(model, varargin)
     solver = NonLinearSolver('maxIterations', 1000);
 
     schedule = simpleSchedule(1, 'W', opt.Wells, 'bc', opt.bc, 'src', opt.src);
-    state0 = initResSol(model.G, 0, [1, 0]);
+    
     
 %     solver.minIterations = 100;
     [~, states] = simulateScheduleAD(state0, model, schedule, 'nonlinearsolver', solver);
     state = states{1};
+    state.flux = sum(state.flux, 2);
+    if isfield(state, 'wellSol')
+        for i = 1:numel(state.wellSol)
+            state.wellSol(i).flux = sum(state.wellSol(i).flux, 2);
+            state.wellSol(i).pressure = state.wellSol(i).bhp;
+        end
+    end
 end
