3dce92fa2db8ab2b257a472dbe5a08534966de12
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-11-20 12:44:21 +0100

    Updated function to work with multiple perforations and report perforation rates as well as surface rates

--- book/in2ph/utils/getWellSol.m
+++ book/in2ph/utils/getWellSol.m
@@ -5,14 +5,21 @@ wellSol(numel(W))=struct;
 for i=1:numel(W)
     out = min(x.wellSol(i).flux,0); iout = out<0;
     in  = max(x.wellSol(i).flux,0); iin  = in>0;
-    lamc = fluid.relperm(x.s(W(i).cells,:))./mu; 
+    
+    swc  = x.s(W(i).cells,:);
+    lamc = bsxfun(@rdivide,fluid.relperm(swc), mu); 
     fc   = lamc(:,1)./sum(lamc,2);
-    lamw = fluid.relperm(W(i).compi)./mu;
+    
+    sww  = W(i).compi;
+    lamw = bsxfun(@rdivide,fluid.relperm(sww), mu);
     fw   = lamw(:,1)./sum(lamw,2);
+    
     wellSol(i).name = W(i).name;
     wellSol(i).bhp  = x.wellSol(i).pressure;
     wellSol(i).wcut = iout.*fc + iin.*fw;
-    wellSol(i).Sw   = iout.*x.s(W(i).cells,1) + iin.*W(i).compi(1);
-    wellSol(i).qWs  = sum(out.*fc) + sum(in.*fw);
-    wellSol(i).qOs  = sum(out.*(1-fc)) + sum(in.*(1-fw));
+    wellSol(i).Sw   = iout.*swc(:,1) + iin.*sww(:,1);
+    wellSol(i).qW   = out.*fc + in.*fw;
+    wellSol(i).qWs  = sum(wellSol(i).qW);
+    wellSol(i).qO   = out.*(1-fc) + in.*(1-fw);
+    wellSol(i).qOs  = sum(wellSol(i).qO);
 end
