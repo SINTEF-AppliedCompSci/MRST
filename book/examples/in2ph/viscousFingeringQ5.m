64d8bbcea307631bccd99d56c06c7513d3ab168c
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-11-24 14:53:53 +0100

    Reinstate correct loop index, update to new SPE10 syntax, add a short text about plotWellSols, ensure all required modules are loaded

--- book/examples/in2ph/viscousFingeringQ5.m
+++ book/examples/in2ph/viscousFingeringQ5.m
@@ -6,15 +6,14 @@
 % water is less than the viscosity of the resident oil) leads to pronounced
 % fingers, whereas a piston-like displacement with minimal fingering effects
 % is observed for a favorable mobility ratio.
-mrstModule add incomp
+mrstModule add incomp ad-core spe10
 
 %% Set up model and parameters
-T        = 20*year;
-dims     = [60 120 1];
-domain   = dims.*[20 10 2]*ft;
-G        = computeGeometry(cartGrid(dims,domain));
-rock     = SPE10_rock((1:dims(1)),(1:dims(2))+60,1);
-rock.perm = rock.perm*milli*darcy;
+T         = 20*year;
+dims      = [60 120 1];
+domain    = dims.*[20 10 2]*ft;
+G         = computeGeometry(cartGrid(dims,domain));
+rock      = getSPE10rock((1:dims(1)),(1:dims(2))+60,1);
 rock.poro = max(rock.poro,.0005);
 gravity reset off
 
@@ -43,8 +42,8 @@ mu      = [1 N; 1 1 ; N 1];
 [dt,dT] = deal(zeros(M,1), T/M);
 is      = 1;
 wellSol = cell(M,3);
-oip      = zeros(M,3);
-for n=1:1
+oip     = zeros(M,3);
+for n=1:3
     fluid = initSimpleFluid('mu', mu(n,:).* centi*poise, ...
         'rho', [1000,1000].*kilogram/meter^3, 'n',  [2 2]);
     [x,ip] = deal(x0,1);
@@ -71,5 +70,9 @@ for n=1:1
 end
 
 %% Plot well responses
+% This plotting only works for predefined quantities like surface rates,
+% bottom-hole pressure, etc. If the well structure contains other
+% quantities, these will appear in the list of available fields but cannot
+% be plotted.
 plotWellSols({wellSol(:,1),wellSol(:,2),wellSol(:,3)},...
     cumsum(dt),'datasetnames',{'Ratio 1:10','Ratio 1:1','Ratio 10:1'});
