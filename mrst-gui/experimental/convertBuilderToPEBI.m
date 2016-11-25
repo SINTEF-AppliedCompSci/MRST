120bc33d61c309085900c75571855a612836896e
Olav Møyner <olav.moyner@sintef.no>
2016-11-25 16:40:18 +0100

    Remove hacks for experimental grid builder no longer needed after UPR updates

--- mrst-gui/experimental/convertBuilderToPEBI.m
+++ mrst-gui/experimental/convertBuilderToPEBI.m
@@ -1,4 +1,4 @@
-function G = convertBuilderToPEBI(out, n, varargin)
+function [G,pts,F] = convertBuilderToPEBI(out, n, varargin)
     require upr
     fault = out.faults;
     bdr = out.outline;
@@ -7,31 +7,8 @@ function G = convertBuilderToPEBI(out, n, varargin)
         wells{i} = out.points(i, :);
     end
     wells = [wells, out.wells];
-    if isempty(fault)
-        L = {};
-    else
-        L = fault;
-    end
-    % Trick the gridder to make outline of the domain as faces so we can
-    % remove the final cells
-    if ~isempty(bdr)
-        bdr_closed = [bdr; bdr(1, :)];
-        for i = 1:size(bdr, 1);
-            L = [L, bdr_closed([i, i+1], :)];
-        end
-    end
-    G = pebiGrid(1/sqrt(n), [1, 1], ...
-        'faultLines', L, 'wellLines', wells, ...
+		varargin = [varargin, {'polyBdr', bdr}];
+		[G,pts,F] = pebiGrid(1/sqrt(n), [1, 1], ...
+        'faultLines', fault, 'wellLines', wells, ...
         varargin{:});
-    
-    if ~isempty(bdr)
-        % Extract subgrid defined by bounding box
-        G = computeGeometry(G);
-        G0 = G;
-        keep = inpolygon(G.cells.centroids(:, 1), G.cells.centroids(:, 2),...
-                         bdr_closed(:, 1), bdr_closed(:, 2));
-        [G, gc, gf] = extractSubgrid(G, keep);
-        G.cells.tag = G0.cells.tag(gc);
-        G.faces.tag = G0.faces.tag(gf);
-    end
 end
