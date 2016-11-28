507c8bff74faa95e82c870677eb03e441e609221
Olav Møyner <olav.moyner@sintef.no>
2016-11-28 11:29:43 +0100

    Fix dependency on expensive toolboxes.

--- examples/utils/producerInjectorGrid.m
+++ examples/utils/producerInjectorGrid.m
@@ -48,7 +48,7 @@ if ~isempty(wellPts)
     for i = 1:size(resPtsInit,1)
         res = [res; mlqt(resPtsInit(i,:), wellPts, [d,d], varArg{:})];
     end
-    resPts = vec2mat([res{:,1}],2);
+    resPts = vertcat(res{:,1});
     %resGridSize = 0.5*[res{:,2}]';
 else
     resPts = resPtsInit;
