5e2ab1cef27bd46199c215e78176ba3da3e862d5
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2016-11-29 22:37:16 +0100

    Turn this script into a function

--- book/examples/in2ph/experimental/showGridErrors.m
+++ book/examples/in2ph/experimental/showGridErrors.m
@@ -1,34 +1,39 @@
-%% Grid-orientation effects for the five-spot
-% With a two-point type discretization (and with many other discretizations
-% as well), evolving displacement profiles will preferrentially move along
-% the axial directions, i.e., in the direction of the normals to the cell
-% faces. To illustrate this, we contrast approximate solutions for the
-% repeated five-spot well pattern computed using the original quarter
-% five-spot setup and with a rotated setup with injectors in the SW and NE
-% corners and producers in the SE and NW corners. The two setups will have
-% different preferrential flow directions and hence generally give
-% different solutions.
+function runQ5DiagParal(cartDim, nstep, fluid, pvi)
+%Solve and plot quarter five-spot problem on diagonal and parallel grid
+%
+% SYNOPSIS:
+%   runQ5DiagParal(nstep, cartDim, fluid, pvi)
+%
+% DESCRIPTION:
+%   This function runs two quarter five-spot problems and plots them in the
+%   same axes. The first is the original quarter-five spot with injector
+%   and producer in diagonally opposite corners. Here, the main flow
+%   direction will be along the diagonal of the grid cells. This solution
+%   will be plotted using contour lines. The second has a grid that is
+%   rotated 45 degrees so that we simulate a case with four wells with
+%   injectors in the SW and NE corners and producers in the SE and NW
+%   corners. Here the main flow direction will align with the axial
+%   directions of the grid. This is plotted as filled contours.
+%
+% REQUIRED PARAMETERS:
+%   cartDim - Dimension of Cartesian grid used for the original Q5 setup
+%
+%   nstep   - Number of time steps in sequential solution
+%
+%   fluid   - Fluid object as defined by function 'initSimpleFluid'
+%
+%   pvi     - Dimensionless end time for simulation 
 
-mrstModule add incomp
-
-%% Common parts of the two simulation setups
-T = 5*year; 
-% nstep=1;
-% cartDim = [16 16 1];
-pvi = 0.6;
 gravity reset off
-fluid = initSimpleFluid('mu' , [   1,     1] .* centi*poise     , ...
-                        'rho', [1000,  850] .* kilogram/meter^3, ...
-                        'n'  , [   2,    2]);
 
-%% Original quater-five spot
-% This is a standard quarter-five spot with injector and producer in
-% diagonally opposit corners. Here the main flow direction will be along
-% the diagonal of the grid cells.
+
+% -------------------------------------------------------------------------
+% Original quarter-five spot
+% This is a standard 
 domain = [1000 1000 20];
 G      = computeGeometry(cartGrid(cartDim,domain));
 rock   = makeRock(G, 450*milli*darcy, 0.2);
-rate   = pvi*sum(poreVolume(G,rock))/T;
+rate   = pvi*sum(poreVolume(G,rock));
 W = addWell([],G, rock, 1, 'Type', 'rate', ...
     'Val', rate, 'name', 'I', 'radius', .1, 'Comp_i', [1 0]);
 W = addWell(W,G, rock, G.cells.num, 'Type', 'rate', ...
@@ -38,13 +43,8 @@ x  = initState(G,W,100*barsa, [0 1]);
 hT = computeTrans(G, rock);
 for i=1:nstep
     x  = incompTPFA(x, G, hT, fluid, 'wells', W);
-    x  = explicitTransport(x, G, T/nstep, rock, fluid, 'wells', W);
+    x  = explicitTransport(x, G, 1/nstep, rock, fluid, 'wells', W);
 end
-%{
-clf
-plotCellData(G,x.s(:,1));
-plotWell(G, W);
-%}
 
 % Set plotting function for later
 N = 10;
@@ -54,11 +54,8 @@ plotData = @(G,x) contour(reshape(G.cells.centroids(:,1),G.cartDims), ...
                           reshape(x,G.cartDims), cval, '-k', 'LineWidth',1.5);
 colormap(flipud(.5*jet(N)+.5*ones(N,3)));
 
-%% Rotated quater-five spot
-% This is a grid that is rotated 45 degrees so that we simulate a case with
-% four wells with injectors in the SW and NE corners and producers in the
-% SE and NW corners. Here the main flow direction will align with the axial
-% directions of the grid.
+% -------------------------------------------------------------------------
+% Rotated quater-five spot
 theta = pi/4;
 R = [cos(theta) -sin(theta); sin(theta) cos(theta)];
 Gr = cartGrid([round(G.cartDims(1:2).*sqrt(2)) 1], domain);
@@ -79,21 +76,12 @@ xr  = initState(Gr, Wr, 100*barsa, [0 1]);
 hTr = computeTrans(Gr, rockr);
 for i=1:nstep
     xr  = incompTPFA(xr, Gr, hTr, fluid, 'wells', Wr);
-    xr  = explicitTransport(xr, Gr, T/nstep, rockr, fluid, 'wells', Wr);
+    xr  = explicitTransport(xr, Gr, 1/nstep, rockr, fluid, 'wells', Wr);
 end
-%{
-clf
-plotCellData(Gr,xr.s(:,1),'EdgeColor',[.6 .6 .6]);
-plotWell(Gr,Wr);
-i = G.cells.centroids(:,1)>=G.cells.centroids(:,2);
-plotCellData(G,x.s(:,1),i,'EdgeColor','k');
-plotWell(G, W);
-axis tight equal;
-c = jet; colormap([1 1 1; c(2:end,:)]);
-%}
 
-%% Compare the two solutions
+% -------------------------------------------------------------------------
+% Plot and compare the two solutions
 remapAndPlot(Gr,xr.s(:,1),domain([1 2]),cval);
 hold on; plotData(G,x.s(:,1)); hold off;
 axis equal tight; axis([0 domain(1) 0 domain(2)]);
-%legend('Original','Rotated',4);
+end
