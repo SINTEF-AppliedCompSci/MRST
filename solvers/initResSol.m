b725e5292bb9adfb85f7b04df82d0770bd0dc602
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2016-11-30 15:56:59 +0100

    initResSol: Reference 'incompTPFA'
    Function 'solveIncompFlow' was renamed long ago and is, moreover, no
    longer our primary solver for incompressible two-phase flow problems.

--- solvers/initResSol.m
+++ solvers/initResSol.m
@@ -32,7 +32,7 @@ function resSol = initResSol(G, p0, s0, varargin)
 %   The initial Darcy flux is zero throughout the reservoir.
 %
 % SEE ALSO:
-%   initWellSol, solveIncompFlow.
+%   initWellSol, incompTPFA.
 
 %{
 Copyright 2009-2016 SINTEF ICT, Applied Mathematics.
