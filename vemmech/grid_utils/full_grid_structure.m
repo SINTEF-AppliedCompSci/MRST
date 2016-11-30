b85afd6680da5853507ffe08722ce6e930cfa712
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2016-11-30 11:06:09 +0100

    Release Preparation: Update Copyright Notice Where Needed

--- vemmech/grid_utils/full_grid_structure.m
+++ vemmech/grid_utils/full_grid_structure.m
@@ -242,7 +242,7 @@
 %             more on, help grid_structure, more off
 
 %{
-Copyright 2009-2015 SINTEF ICT, Applied Mathematics.
+Copyright 2009-2016 SINTEF ICT, Applied Mathematics.
 
 This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
 
