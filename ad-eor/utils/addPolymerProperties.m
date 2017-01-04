db089a31907e226750958323e026b393d204066a
Olav Møyner <olav.moyner@sintef.no>
2017-01-04 17:33:38 +0100

    Adding utility for adding polymer props to a fluid (currently only uses the old POLY.DATA values)

---
+++ ad-eor/utils/addPolymerProperties.m
@@ -0,0 +1,11 @@
+function fluid = addPolymerProperties(fluid, varargin)
+    polyDeck  = readEclipseDeck('POLYMER.DATA');
+    polyDeck  = convertDeckUnits(polyDeck);
+    polyFluid = initDeckADIFluid(polyDeck);
+
+    fns = {'muWMult','dps','rrf','rhoR','adsInx','adsMax','ads',...
+        'mixPar','cmax'};
+    for i=1:numel(fns)
+        fluid.(fns{i}) = polyFluid.(fns{i});
+    end
+end
