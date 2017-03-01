90f4b7282ea2eb752576e80fd120e91437bde885
Olav Møyner <olav.moyner@sintef.no>
2017-03-01 14:49:38 +0100

    Adding a very simple routine for incompressible pressure distribution

---
+++ utils/incompHydrostaticPressure.m
@@ -0,0 +1,26 @@
+function p = incompHydrostaticPressure(G, contacts, densities, varargin)
+    opt = struct('topPressure', 0, ...
+                 'nsamples', 1000);
+    opt = merge_options(opt, varargin{:});
+    ns = opt.nsamples;
+    pdist = zeros(ns, 1);
+    zi = zeros(ns, 1);
+    zmax = max(G.cells.centroids(:, 3));
+    zmin = min(G.cells.centroids(:, 3));
+    h = (zmax - zmin)./(ns-1);
+    pdist(1) = opt.topPressure;
+    zi(1) = zmin;
+    for i = 2:ns
+        zi(i) = h*i + zmin;
+        ix = find(contacts < zi(i), 1, 'last');
+        if isempty(ix)
+            ix = 0;
+        end
+        rho = densities(ix+1);
+
+        pdist(i) = pdist(i-1) + rho*h*norm(gravity);
+    end
+   
+    z = G.cells.centroids(:, 3);
+    p = interp1(zi, pdist, z);
+end
