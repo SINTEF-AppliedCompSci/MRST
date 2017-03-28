6bddb99fc3def2915a876f3af0767c89fa048fa3
Stein Krogstad <stein.krogstad@sintef.no>
2017-03-28 13:57:46 +0200

    Add functionality for permeablity sensitivities

--- ad-core/simulators/computeSensitivitiesAdjointAD.m
+++ ad-core/simulators/computeSensitivitiesAdjointAD.m
@@ -60,12 +60,14 @@ function sens = computeSensitivitiesAdjointAD(state0, states, model, schedule, g
         linsolve = opt.LinearSolver;
     end
     
-    param  = opt.Parameters;
+    param  = checkParam(opt.Parameters);
     pTypes = opt.ParameterTypes;
     sens = cell(1, numel(param));
     [sens{:}] = deal(0);
+  
+    
     % inititialize parameters to ADI
-    modelParam = initModelParametersADI(model, param, pTypes);
+    modelParam = initModelParametersADI(model, schedule, param);
     
     nstep = numel(schedule.step.val);
     lambda = [];
@@ -84,11 +86,6 @@ function sens = computeSensitivitiesAdjointAD(state0, states, model, schedule, g
         end
     end
     
-    % set ADI back to double
-    opnms = {'T', 'pv'};
-    for k  =1:numel(opnms)
-        model.operators.(opnms{k}) = double(model.operators.(opnms{k}));
-    end
     
    
     % add regularization term
@@ -111,6 +108,11 @@ function eqdth = partialWRTparam(model, getState, schedule, step)
     % get forces and merge with valid forces
     forces = model.getDrivingForces(lookupCtrl(step));
     forces = merge_options(validforces, forces{:});
+    model = model.validateModel(forces);
+    % Initial state typically lacks wellSol-field, so add if needed
+    if step == 1
+        before = model.validateState(before);
+    end
     
     problem = model.getEquations(before, current, dt, forces, 'iteration', inf, 'resOnly', true);
     eqdth   = problem.equations;
@@ -126,45 +128,76 @@ function state = getStateFromInput(schedule, states, state0, i)
     end
 end
 
-function model = initModelParametersADI(model, param, pTypes)
+function [model, schedule] = initModelParametersADI(model, schedule, param)
 np = numel(param);
 m = cell(1, np);
-for k = 1:np
-    switch param{k}
-        case 'transmissibility'
-            if strcmp(pTypes{k}, 'multiplyer')
-                m{k} = ones(size(model.operators.T));
-            else
-                m{k} = model.operators.T;
-            end
-        case 'porevolume'
-            if strcmp(pTypes{k}, 'multiplyer')
-                m{k} = ones(size(model.operators.pv));
-            else
-                m{k} = model.operators.pv;
-            end
-        otherwise
-            error(['Unknown parameter: ', param{k}]);
+% in stage 1, set values, in stage 2, initialize ADI
+for stage = 1:2
+    for k = 1:np
+        switch param{k}
+            case 'transmissibility'
+                if stage == 1
+                    m{k} = model.operators.T; 
+                else
+                    model.operators.T = m{k};
+                end
+            case 'porevolume'
+                if stage == 1
+                    m{k} = model.operators.pv;
+                else
+                    model.operators.pv = m{k};
+                end
+            case 'permx'
+                % always compute sensitivity for perm in X,Y and Z
+                % -directions, input must be organized such that permy and
+                % permz appear at index k+1 and k+2, resp.
+                if stage == 1
+                    nd = size(model.rock.perm, 2);
+                    if nd == 1
+                        ix = [1 1 1];
+                    elseif nd ==3
+                        ix = [1 2 3];
+                    else
+                        error('Sensitivity code assumes either isotropic or diagnoal (3-elem) perm');
+                    end
+                    for ii = 1:3
+                        m{k+ii-1} = model.rock.perm(:,ix(ii));
+                    end
+                else
+                    th = cell(1,3);
+                    for ii = 1:3
+                        % half -transmissibilities projected along each
+                        % coordinate direction
+                        th{ii} = perm2directionalTrans(model, m{k+ii-1}, ii);
+                    end
+                    cf = model.G.cells.faces(:,1);
+                    nf = model.G.faces.num;
+                    % mapping from from cell-face to face
+                    M = sparse(cf, (1:numel(cf))', 1, nf, numel(cf));
+                    % consider only internal faces
+                    ie = model.operators.internalConn;
+                    model.operators.T = 1./(M(ie,:)*(1./(th{1}+th{2}+th{3})));
+                end
+            case 'welltrans'
+                % assume wells are the same throughout sim
+                if stage == 1
+                    m{k} = vertcat(schedule.control(1).W.WI);
+                else
+                    schedule = updateWellTrans(schedule, m{k});
+                end
+            case {'swcr', 'swu', 'sowcr'}  
+                if stage == 1
+                    m{k} = model.fluid.(param{k});
+                else
+                    model.fluid.(param{k}) = m{k};
+                end
+        end
     end
-end
-[m{:}] = initVariablesADI(m{:});
-for k = 1:np
-    switch param{k}
-        case 'transmissibility'
-            if strcmp(pTypes{k}, 'multiplyer')
-                model.operators.T = m{k}.*model.operators.T;
-            else
-                model.operators.T = m{k};
-            end
-        case 'porevolume'
-            if strcmp(pTypes{k}, 'multiplyer')
-                model.operators.pv = m{k}.*model.operators.pv;
-            else
-                model.operators.pv = m{k};
-            end
-    end
+    % after setting values, initialize ADI:
+    [m{:}] = initVariablesADI(m{:});
 end
 end
+
 
 function sens = addRegularizationSens(sens, param, pTypes, model, modelBase, reg)
 np = numel(param);
@@ -188,3 +221,44 @@ for k = 1:np
     sens{k} = sens{k} + reg{k}*dparam;
 end
 end
+
+function ti = perm2directionalTrans(model, p, cdir)
+% special utility function (typically for sensitivity calculations)
+% for calculating transmissibility along coordinate direction cdir
+% In particular:
+% trans = t1+t2+t2, where ti = perm2dirtrans(model, perm(:, i), i);
+assert(size(p,2)==1, ...
+       'Input p should be single column representing permeability in direction cdir');
+   
+% make perm represent diag perm tensor with zero perm orthogonal to cdir
+
+dp = double(p);
+r.perm = zeros(numel(dp), 3);
+r.perm(:, cdir) = dp;
+ti = computeTrans(model.G, r);
+if isa(p, 'ADI')
+    % make ti ADI (note ti is linear function of p)
+    p = p./dp;
+    cellno = rldecode(1 : model.G.cells.num, diff(model.G.cells.facePos), 2).';
+    ti = ti.*p(cellno);
+end
+end
+
+function param_proc = checkParam(param)
+param_proc = param;
+pix = 0;
+for k = 1:numel(param)
+    p = param{k};
+    pix = pix+1;
+    switch p
+        case {'transmissibility', 'porevolume'}
+            param_proc{pix} = param{k};
+        case 'permeability'
+            param_proc(pix:(pix+2)) = {'permx', 'permy', 'permz'};
+            pix = pix+2;
+        otherwise
+            error('Unknown parameter: %s\n', param{k});
+    end
+end
+end
+
