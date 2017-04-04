5574068b280e0d4ed5c5ba9ca4493e0fe4a1f644
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2017-04-04 06:52:41 +0200

    Walkthrough of SPE1, SPE3 and SPE9 fluids as used in book

--- book/examples/mphase/showSPEfluids.m
+++ book/examples/mphase/showSPEfluids.m
@@ -12,46 +12,60 @@
 % If you have used the mrstDatasetGUI functionality to download the SPE 1
 % and SPE 9 data set, it should be possible to find them on a standard
 % path. We read the data and convert to SI units.
-mrstModule add deckformat
+mrstModule add deckformat ad-props ad-core
 
-pth  = getDatasetPath('spe1');
-fn   = fullfile(pth, 'BENCH_SPE1.DATA');
-%pth  = getDatasetPath('spe9');
-%fn   = fullfile(pth, 'BENCH_SPE9.DATA');
-deck = readEclipseDeck(fn);
-deck = convertDeckUnits(deck);
-
-%% Create fluid objects
-mrstModule add ad-props ad-core
-f = initDeckADIFluid(deck);
+nr   = [1 3 9];
+f    = cell(3,1);
+col  = lines(3);
+name = cell(3,1);
+for i=1:3
+    name{i} = sprintf('spe%d',nr(i));
+    pth  = getDatasetPath(name{i});
+    fn   = fullfile(pth, sprintf('BENCH_SPE%d.DATA',nr(i)));
+    deck = readEclipseDeck(fn);
+    deck = convertDeckUnits(deck);
+    f{i} = initDeckADIFluid(deck);
+end
 
 %% Plot two-phase relative permeability curves
 % For a three-phase model we have four relative permeability curves. One
 % for both gas and water and two curves for the oil phase. The oil relative
 % permeability is tabulated for both water-oil and oil-gas systems.
 s = linspace(0,1,51)';
+for i=1:numel(name), name{i} = upper(name{i}); end
 
-figure;
-plot(s, f.krW(s), 'linewidth', 2)
+figure; hold all, set(gca,'FontSize',14)
+for i=1:3
+    plot(s, f{i}.krW(s), 'linewidth', 2, 'Color',col(i,:))
+end
 grid on
+legend(name{:},2);
 xlabel('Water saturation');
 title('Water relative permeability curve')
-ylabel('k_r')
 
-figure;
-plot(s, [f.krOW(s), f.krOG(s)], 'linewidth', 2)
+figure; hold all, set(gca,'FontSize',14)
+for i=1:3
+    krOW = f{i}.krOW(s); krOW(s>1-f{i}.sWcon)=nan;
+    krOG = f{i}.krOG(s); krOG(s>1-f{i}.sWcon)=nan;
+    plot(s, krOW, '-','Color',col(i,:), 'linewidth',2);
+    plot(s, krOG, '--', 'linewidth', 4, 'Color',col(i,:))
+end
 grid on
+h = findobj(gca,'Type','line');
+legend(h([6 5 6 4 2]),'oil/water system', 'oil/gas system', name{:},2);
 xlabel('Oil saturation');
-legend('Oil-Water system', 'Oil-Gas system', 'location', 'northwest')
 title('Oil relative permeability curves')
-ylabel('k_r')
 
-figure;
-plot(s, f.krG(s), 'linewidth', 2)
+figure; hold all, set(gca,'FontSize',14)
+for i=1:3
+    krG = f{i}.krG(s); krG(s>1-f{i}.sWcon)=nan;
+    plot(s, krG, 'linewidth', 2,'Color',col(i,:))
+end
 grid on
+legend(name{:},4);
 xlabel('Gas saturation');
 title('Gas relative permeability curve')
-ylabel('k_r')
+
 
 %% Plot three-phase relative permeability for oil
 % When all three phases are present simultaneously in a single cell, we
@@ -60,15 +74,18 @@ ylabel('k_r')
 % permeability model. Herein, we use a simple linear interpolation, which
 % is also the default in Eclipse
 %
-
-[x, y] = meshgrid(linspace(0,1,201));
-[~, krO] = f.relPerm(x(:),y(:));
-krO = reshape(krO,size(x));
-krO(x+y>1)=nan;
-z=1-x-y;
-figure;
-[mapx,mapy] = ternaryAxis('names',{'S_w';'S_g';'S_o'});
-contourf(mapx(x,y,z), mapy(x,y,z), krO, 20)
-set(gca,'YLim',[0 sqrt(3)/2]);
-text(mapx(.45,.45,.1),mapy(.45,.45,.1),'Oil relative permeability', ...
-    'BackgroundColor','w','HorizontalAlignment','center'); 
+[sw, sg] = meshgrid(linspace(0,1,201));
+so=1-sw-sg;
+for i=1:3
+    [~, krO] = f{i}.relPerm(sw(:),sg(:));
+    krO = reshape(krO,size(sw)); krO(sw+sg>1)=nan;
+    
+    figure; set(gca,'FontSize',14);
+    [mapx,mapy] = ternaryAxis('names',{'S_w';'S_g';'S_o'},'FontSize',14);
+    contourf(mapx(sw,sg,so), mapy(sw,sg,so), krO, 20)
+    
+    set(gca,'YLim',[0 sqrt(3)/2]);
+    text(mapx(.45,.45,.1),mapy(.45,.45,.1),'Oil relative permeability', ...
+        'BackgroundColor','w','HorizontalAlignment','center','FontSize',14);
+    caxis([0 1]);
+end
