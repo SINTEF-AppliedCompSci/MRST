a115fd5b6e52e75968a1585aa9f6aeb3e9dfdfa2
Øystein <oystein.klemetsdal@gmail.com>
2017-05-22 19:54:52 +0200

    Some tweaks to speed up computations (e.g. ADI arithmetics computed only once)

--- code/FourPhaseSolvent/computeViscositiesAndDensities.m
+++ code/FourPhaseSolvent/computeViscositiesAndDensities.m
@@ -12,25 +12,10 @@ function [muW_eff, muO_eff, muG_eff, muS_eff, rhoW_eff, rhoO_eff, rhoG_eff, rhoS
     sOn = max(sO - sOres ,0);
     sGn = max(sG - sSGres,0);
     sSn = max(sS - sSGres,0);
-    
-%     % Add small value to avoid 0/0-type expressions
-%     tol = 10*eps;
-%     sOn = sOn + (abs(sOn) < tol).*tol;
-%     sGn = sGn + (abs(sGn) < tol).*tol;
-%     sSn = sSn + (abs(sSn) < tol).*tol;
-    
     sNn = sOn + sGn + sSn;
     sOSn = sOn + sSn;
     sSGn = sSn + sGn;
     
-    % Calculate mixed viscosities
-%     a = 1/4;
-%     muMOS = muO.*muS./(sOn./sOSn.*muS.^a + sSn./sOSn.*muO.^a).^4;
-%     muMSG = muS.*muG./(sSn./sSGn.*muG.^a + sGn./sSGn.*muS.^a).^4;
-%     muM   = muO.*muS.*muG./(sOn./sNn.*muS.^a.*muG.^a ...
-%                           + sSn./sNn.*muO.^a.*muG.^a ...
-%                           + sGn./sNn.*muO.^a.*muS.^a).^4;
-%       
     sOnsOSn = saturationFraction(sOn, sOSn);
     sSnsSGn = saturationFraction(sSn, sSGn);
     sOnsNn = saturationFraction(sOn, sNn);
@@ -38,11 +23,14 @@ function [muW_eff, muO_eff, muG_eff, muS_eff, rhoW_eff, rhoO_eff, rhoG_eff, rhoS
     
     % Calculate mixed viscosities
     a = 1/4;
-    muMOS = muO.*muS./(sOnsOSn.*muS.^a + (1 - sOnsOSn).*muO.^a).^4;
-    muMSG = muS.*muG./(sSnsSGn.*muG.^a + (1 - sSnsSGn).*muS.^a).^4;
-    muM   = muO.*muS.*muG./(sOnsNn.*muS.^a.*muG.^a ...
-                          + sSnsNn.*muO.^a.*muG.^a ...
-                          + (1 - (sOnsNn + sSnsNn)).*muO.^a.*muS.^a).^4;
+    muSa = muS.^a;
+    muGa = muG.^a;
+    muOa = muO.^a;
+    muMOS = muO.*muS./(sOnsOSn.*muSa + (1 - sOnsOSn).*muOa).^4;
+    muMSG = muS.*muG./(sSnsSGn.*muGa + (1 - sSnsSGn).*muSa).^4;
+    muM   = muO.*muS.*muG./(sOnsNn.*muSa.*muGa ...
+                          + sSnsNn.*muOa.*muGa ...
+                          + (1 - (sOnsNn + sSnsNn)).*muOa.*muSa).^4;
   
     
     
@@ -59,36 +47,25 @@ function [muW_eff, muO_eff, muG_eff, muS_eff, rhoW_eff, rhoO_eff, rhoG_eff, rhoS
     rhoS = fluid.bS(p).*fluid.rhoSS;
     
     % Effective fractional saturations
-    sOsN_Oeff = muO.^a.*(muO_eff.^a - muS.^a)./(muO_eff.^a.*(muO.^a - muS.^a));
-    sOsN_Geff = muS.^a.*(muG_eff.^a - muG.^a)./(muG_eff.^a.*(muS.^a - muG.^a));
-%     sOGn      = sOn + sGn;
-%     sOf       = sOn./sOGn;
-%     sGf       = sGn./sOGn;
-%     sSsN_Seff = (muS.^a.*(sGf.*muO.^a + sOf.*muG.^a) - muO.^a.*muG.^a.*(muS./muS_eff).^a)...
-%                   ./(muS.^a.*(sGf.*muO.^a + sOf.*muG.^a) - muO.^a.*muG.^a);
+    
+    muOmuS = (muO./muS).^a;
+    muSmuO = 1./muOmuS;
+    muSmuG = (muS./muG).^a;
+    sOsN_Oeff = (muOmuS - (muO./muO_eff).^a)./(muOmuS-1);
+    sOsN_Geff = (muSmuG - (muS./muG_eff).^a)./(muSmuG-1);
 
     sOGn = sOn + sGn;
-    tol = eps;
-    if all(sGn < tol)
-        sGf = 0;
-    else
-        sGf = saturationFraction(sGn, sOGn);
-    end
+    sGf = saturationFraction(sGn, sOGn);
     
-    sSsN_Seff = (muS.^a.*(sGf.*muO.^a + (1-sGf).*muG.^a) - muO.^a.*muG.^a.*(muS./muS_eff).^a)...
-                  ./(muS.^a.*(sGf.*muO.^a + (1-sGf).*muG.^a) - muO.^a.*muG.^a);
-
+    sSsN_Seff = (muSmuG.*sGf + muSmuO.*(1-sGf) - (muS./muS_eff).^a)...
+               ./(muSmuG.*sGf + muSmuO.*(1-sGf) - 1);
+    
     % Expressions are sinuglar if muO == muG, in which case we replace the
     % by a simple interpolation rho*(1-omega) + rhoM*omega
     tol = 1e-10;
     eq = abs(muO - muS) < tol | abs(muS - muG) < tol;
-    
-%     sO = sO + (abs(sO) < tol).*tol;
-%     sG = sG + (abs(sG) < tol).*tol;
-%     sS = sS + (abs(sS) < tol).*tol;
-%     rhoM = rhoO.*sO./sN + rhoG.*sG./sN + rhoS.*sS./sN;
-    sN = sO + sG + sS;
 
+    sN = sO + sG + sS;
     sOsN = saturationFraction(sO, sN);
     sGsN = saturationFraction(sG, sN);
     rhoM = rhoO.*sOsN + rhoG.*sGsN + rhoS.*(1 - (sOsN + sGsN));
@@ -108,9 +85,5 @@ function f = saturationFraction(sNum, sDen)
 
     tol = eps;
     f = sNum./(sDen + (sDen < tol).*tol);
-    
-%     num = sNum - sDen;
-%     f = 1 - (num + (num < tol).*tol)./(sDen + (sDen < tol).*tol);
-    
-    
+       
 end
