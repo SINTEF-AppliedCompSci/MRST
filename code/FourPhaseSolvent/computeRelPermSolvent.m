3ad9558e773d8e50cdb104dc5be600241cff5af4
Øystein <oystein.klemetsdal@gmail.com>
2017-05-22 19:54:25 +0200

    Cleaned up computeRelPermSolvent.

--- code/FourPhaseSolvent/computeRelPermSolvent.m
+++ code/FourPhaseSolvent/computeRelPermSolvent.m
@@ -1,31 +1,21 @@
 function [krW_eff, krO_eff, krG_eff, krS_eff] = computeRelPermSolvent(fluid, p, sW, sO, sG, sS, sWres, sOres, sSGres, mobMult)
-    % Calulates effective relative permeabilities.
-    
-
-    
-%     % Relperm of hydrocarbon to water as a funciton of total HC saturation
-%     krN = fluid.krO(sO + sG + sS);
-%     
-%     % Immiscible relperms for oil and total gas
-%     krO_i = fluid.krO(sO);
-%     krGT_i = fluid.krG(sG + sS);
+% Calulates effective relative permeabilities.
 
     n = 2;
-
     sr_tot = sWres + sOres + sSGres;
     
+    % Relperm of hydrocarbon to water as a funciton of total HC saturation
     krN = coreyRelperm(sO + sG + sS, ...
                   n, ...
                   sOres + sSGres, ...
                   fluid.krO(1 - sWres), ...
                   sr_tot);
-              
+    % Immiscible relperms for oil and total gas          
     krO_i = coreyRelperm(sO, ...
                   n, ...
                   sOres, ...
                   fluid.krO(1 - (sWres + sSGres)), ...
                   sr_tot);
-              
     krGT_i = coreyRelperm(sG + sS, ...
                   n, ...
                   sSGres, ...
@@ -34,26 +24,12 @@ function [krW_eff, krO_eff, krG_eff, krS_eff] = computeRelPermSolvent(fluid, p, 
 
 
     M = fluid.Msat(sG, sS).*fluid.Mpres(p);
-    
-%     % Add small value to avoid 0/0-type expressions
-%     tol = eps;
-%     sO = sO + (sO < tol).*tol;
-%     sG = sG + (sG < tol).*tol;
-%     sS = sS + (sS < tol).*tol;
-%     
-%     tol = eps;
-%     sGsS = (sG + (sG < tol).*tol)./(sG + (sG < tol).*tol + sS);
 
     sGsGT = saturationFraction(sG, sG + sS);
     
     % Immiscible gas and solvent relperms
     krG_i = sGsGT.*krGT_i;
     krS_i = (1-sGsGT).*krGT_i;
-
-    
-%     % Immiscible gas and solvent relperms
-%     krG_i = sG./(sS + sG).*krGT_i;
-%     krS_i = sS./(sS + sG).*krGT_i;
     
     sOn = max(sO - sOres, 0);
     sGn = max(sG - sSGres,0);
@@ -71,12 +47,6 @@ function [krW_eff, krO_eff, krG_eff, krS_eff] = computeRelPermSolvent(fluid, p, 
     krGT_m = sGTnsNn.*krN;
     krG_m = sGnsGTn.*krGT_m;
     krS_m = sSnsGTn.*krGT_m;
-    
-%     % Miscible relperms
-%     krO_m = sO./(sO + sG + sS).*krN;
-%     krGT_m = (sG + sS)./(sO + sG + sS).*krN;
-%     krG_m = sG./(sS + sG).*krGT_m;
-%     krS_m = sS./(sS + sG).*krGT_m;
     
     % Interpolate between miscible and immiscible cases (water relperm
     % not affected by the solvent)
