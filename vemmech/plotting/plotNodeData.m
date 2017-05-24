74a1b8954b03ba7fcd3d52acded20db733848a48
Olav Møyner <olav.moyner@sintef.no>
2017-05-24 10:34:56 +0200

    Clean up copy-pasted parts of plotNodeData

--- vemmech/plotting/plotNodeData.m
+++ vemmech/plotting/plotNodeData.m
@@ -81,17 +81,6 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
     %colour = colour(verts, :);
     fc     = 'interp';
     
-    if size(f, 1) == size(v, 1)
-        % plotFaces uses FaceVertexCData to plot face data. The internal MATLAB
-        % patch function decides if the data is given per vertex or face based
-        % on a check of lengths. However, if the sizes are exactly *equal* it
-        % defaults to vertex data. This can happen when dynamically plotting
-        % subsets of fronts. In this case, we add a dummy vertex consisting of
-        % NaN which has no influence on the plot, but ensures that the data is
-        % interpreted as face data.
-        v = [v; NaN(1, size(v, 2))];
-    end
-
     % Build final patch for graphical output (Note: added to GCA).
     
     h = patch('Faces'          , f      , 'Vertices' , v , ...
@@ -182,79 +171,3 @@ function [f, present] = get_face_topo_3d(G, faces)  %#ok
 end
 
 %--------------------------------------------------------------------------
-
-function [plotOutline, varargin] = do_outline_p(varargin)
-% Does caller of 'plotFaces' request an outline plot?
-
-    plotOutline = false;
-    if numel(varargin) > 0, 
-        i = 1 + isnumeric(varargin{1});
-        v = find(strcmpi(varargin(i : 2 : end), 'outline'));
-        if ~isempty(v), 
-            % Find argument following last 'outline'
-            plotOutline = varargin{i + 2*v(end) - 1};
-
-            % Remove pairs of 'outline'/value from varargin.
-            varargin([i + 2*v - 2, i + 2*v - 1]) = [];
-        end
-    end
-end
-
-%--------------------------------------------------------------------------
-
-function rgb = get_rgb(colour)
-    switch lower(colour), 
-      case {'y', 'yellow' }, rgb = [1, 1, 0];
-      case {'m', 'magenta'}, rgb = [1, 0, 1];
-      case {'c', 'cyan'   }, rgb = [0, 1, 1];
-      case {'r', 'red'    }, rgb = [1, 0, 0];
-      case {'g', 'green'  }, rgb = [0, 1, 0];
-      case {'b', 'blue'   }, rgb = [0, 0, 1];
-      case {'w', 'white'  }, rgb = [1, 1, 1];
-      case {'k', 'black'  }, rgb = [0, 0, 0];
-      otherwise            , rgb = [0, 0, 1]; % Unknown colour -> 'blue'.
-    end
-end
-
-%--------------------------------------------------------------------------
-
-function pts = findFaceOutline(g, faces)
-    assert (size(g.nodes.coords, 2) == 3);
-    % Find points on border of collection of grid faces.
-    if numel(faces) == 0, pts = zeros(0, 3); return; end
-
-    cellNo = rldecode(1 : g.cells.num, diff(g.cells.facePos), 2) .';
-    sgn    = 2*(cellNo == g.faces.neighbors(g.cells.faces(:, 1), 1)) - 1;
-    faceNo = rldecode(1 : g.faces.num, diff(g.faces.nodePos), 2) .';
-
-    fi     = false(g.faces.num, 1); fi(faces) = true;
-    fn     = g.faces.nodes(fi(faceNo));
-
-    nodeflag     = false([g.nodes.num, 1]);
-    nodeflag(fn) = true;
-
-    fe = faceEdges(g);
-    fe(sgn(faceNo)<0, :) = fe(sgn(faceNo)<0, [2, 1]);
-
-    fe = fe (fi(faceNo), :);
-    nodes = double(fe(all(nodeflag(fe), 2), :));
-
-    if numel(nodes) > 0, 
-        % Remove edges which appear more than once.  These edges are
-        % considered internal in the collection of faces.  The remaining
-        % edges are on the outer boundary.
-        [nodes, n] = rlencode(sortrows(sort(nodes, 2)));
-        nodes(n>1, :) = [];
-    end
-
-    pts = nan(size(nodes, 1)*3, 3);
-    pts (1 : 3 : end, :) = g.nodes.coords(nodes(:, 1), :);
-    pts (2 : 3 : end, :) = g.nodes.coords(nodes(:, 2), :);
-end
-
-%--------------------------------------------------------------------------
-
-function fe = faceEdges(g)
-    fe = [g.faces.nodes, g.faces.nodes([2 : end, 1])];
-    fe(g.faces.nodePos(2 : end) - 1, 2) = fe(g.faces.nodePos(1 : end-1), 1);
-end
