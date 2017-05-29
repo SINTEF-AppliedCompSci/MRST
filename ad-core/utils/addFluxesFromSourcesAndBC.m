d9439eca1b8e5d168bc205ff6e8ab4ab7116d2d2
Olav Møyner <olav.moyner@sintef.no>
2017-05-29 11:13:21 +0200

    BC/SRC: Add warning that vapoil/disgas support is not properly implemented.

--- ad-core/utils/addFluxesFromSourcesAndBC.m
+++ ad-core/utils/addFluxesFromSourcesAndBC.m
@@ -59,7 +59,12 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
     [qBC, qSRC, qRes] = deal(cell(numel(mob), 1));
     [BCTocellMap, srcCells, bcCells] = deal([]);
-    
+    if (isprop(model, 'disgas') && model.disgas) ||...
+       (isprop(model, 'vapoil') && model.vapoil)
+        warning(['Boundary conditions and source terms do not fully support', ...
+                 ' problems with vapoil or disgas active!']);
+    end
+        
     if isempty(forces.bc) && isempty(forces.src)
         return
     end
