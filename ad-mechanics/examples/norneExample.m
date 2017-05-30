16a87de75f20dcb9dbe5c7fbae2de338d8638d77
Xavier Raynaud <xavier.raynaud@sintef.no>
2017-05-30 16:31:14 +0200

    worked on ad-mechanics example.

--- ad-mechanics/examples/norneExample.m
+++ ad-mechanics/examples/norneExample.m
@@ -1,95 +1,89 @@
-mrstModule add ad-core ad-props ad-blackoil vemmech 
-
-% OPTIONS : They are set by opt, default values are given below.
-%
-% method_case:
-%               'no split'
-%               'drained split'
-%               'stress split'
-%
-% norne_case
-%               'full'
-%               'mini Norne'
-
-% bc_cases
-%               'no displacement'
-%
+%% Example: Coupling between fluid flow (blackoil model) and rock mechanics (elasticity)
+% We use the same blackoil equations as in the ad-blackoil model. For rock
+% mechanics, we use the VEM discretization that is given in the vemmech module
 %
 
+mrstModule add ad-core ad-props ad-blackoil vemmech deck-format
 
-opt = struct('method_case', 'no split', ...
-             'norne_case', 'mini Norne', ...
-             'bc_cases', 'no displacement');
 
+%% Options for this example
+
+opt.norne_case = 'mini Norne'; % 'full' or 'mini Norne'
+%
+% 'full'       : 7392 cells
+% 'mini Norne' :  605 cells
+
+opt.bc_case    = 'no displacement'; % 'no displacement' or 'bottom fixed'
+%
+% 'no displacement' : All nodes belonging to external faces have displacement
+%                     equal to zero
+% 'bottom fixed'    : The nodes that belong to the bottom have zero
+%                     displacement, while a given pressure is imposed on
+%                     the external faces that are not bottom faces.
 
 %% Load Norne grid
+
 if ~ (makeNorneSubsetAvailable() && makeNorneGRDECL()),
     error('Unable to obtain simulation model subset');
 end
 
-is_grid_constructed = false;
-if ~is_grid_constructed
-    grdecl = fullfile(getDatasetPath('norne'), 'NORNE.GRDECL');
-    grdecl = readGRDECL(grdecl);
-    fullCartDims = grdecl.cartDims;
-    usys   = getUnitSystem('METRIC');
-    grdecl = convertInputUnits(grdecl, usys);
-    switch opt.norne_case
-      case 'full'
-        grdecl = cutGrdecl(grdecl, [10 25; 35 55; 1 22]);
-      case 'mini Norne'
-        grdecl = cutGrdecl(grdecl, [10 20; 35 45; 1 5]);
-      otherwise
-        error('norne_case not recognized');
-    end
-    %act_org=grdecl.ACTNUM
-    grdecl.ACTNUM = ones(size(grdecl.ACTNUM));
-    G = processGRDECL(grdecl);
-    G = G(1);
-    G = computeGeometry(G);
+
+grdecl = fullfile(getDatasetPath('norne'), 'NORNE.GRDECL');
+grdecl = readGRDECL(grdecl);
+fullCartDims = grdecl.cartDims;
+usys   = getUnitSystem('METRIC');
+grdecl = convertInputUnits(grdecl, usys);
+switch opt.norne_case
+  case 'full'
+    grdecl = cutGrdecl(grdecl, [10 25; 35 55; 1 22]);
+  case 'mini Norne'
+    grdecl = cutGrdecl(grdecl, [10 20; 35 45; 1 5]);
+  otherwise
+    error('norne_case not recognized');
 end
+grdecl.ACTNUM = ones(size(grdecl.ACTNUM));
+G = processGRDECL(grdecl);
+G = G(1);
+G = computeGeometry(G);
 
 
-%% Setup material parameters from deck for fluid
+%% Setup fluid parameters from SPE1
+
 perm = [grdecl.PERMX, grdecl.PERMY, grdecl.PERMZ];
 rock.perm = perm(G.cells.indexMap, :);
 rock.poro = max(grdecl.PORO(G.cells.indexMap), 0.1);
 
+pth = getDatasetPath('spe1');
+fn  = fullfile(pth, 'BENCH_SPE1.DATA');
+deck = readEclipseDeck(fn);
+deck = convertDeckUnits(deck);
+fluid = initDeckADIFluid(deck);
+fluid = rmfield(fluid, 'relPerm');
+fluid = rmfield(fluid, 'pcOW');
+fluid = rmfield(fluid, 'pcOG');
 
-is_fluid_initiated = false;
-if ~is_fluid_initiated
-    pth = getDatasetPath('spe1');
-    fn  = fullfile(pth, 'BENCH_SPE1.DATA');
-    mrstModule add deckformat
-    deck = readEclipseDeck(fn);
-    deck = convertDeckUnits(deck);
-    fluid = initDeckADIFluid(deck);
-    fluid = rmfield(fluid, 'relPerm');
-    fluid = rmfield(fluid, 'pcOW');
-    fluid = rmfield(fluid, 'pcOG');
-    % Setup quadratic relative permeabilities, since SPE1 relperm are a bit rough.
-    fluid.krW = @(s) s.^2;
-    fluid.krG = @(s) s.^2;
-    fluid.krOW = @(s) s.^2;
-    fluid.krOG = @(s) s.^2;
-    pRef = deck.PROPS.PVTW(1);
-end
+% Setup quadratic relative permeabilities, since SPE1 relperm are a bit rough.
+fluid.krW = @(s) s.^2;
+fluid.krG = @(s) s.^2;
+fluid.krOW = @(s) s.^2;
+fluid.krOG = @(s) s.^2;
+pRef = deck.PROPS.PVTW(1);
 
 
 %% Setup material parameters for Biot and mechanics
 
-E     = 1 * giga * Pascal; % Young's module
-nu    = 0.3;               % Poisson's ratio
-alpha = 1;                 % Biot's coefficient
-Ev = repmat(E, G.cells.num, 1);
-nuv = repmat(nu, G.cells.num, 1);
+E          = 1 * giga * Pascal; % Young's module
+nu         = 0.3;               % Poisson's ratio
+alpha      = 1;                 % Biot's coefficient
+Ev         = repmat(E, G.cells.num, 1);
+nuv        = repmat(nu, G.cells.num, 1);
 rock.alpha = repmat(alpha, G.cells.num, 1);
 
 
 %% Setup boundary conditions for mechanics (no displacement)
 
 switch opt.bc_cases
-  
+
   case 'no displacement'
 
     ind = (G.faces.neighbors(:, 1) == 0 | G.faces.neighbors(:, 2) == 0);
@@ -104,12 +98,15 @@ switch opt.bc_cases
     m = ones(nn, 3);
     disp_bc = struct('nodes', bcnodes, 'uu', u, 'mask', m);
     force_bc = [];
-  
+
   case 'bottom fixed'
 
     nx = G.cartDims(1);
     ny = G.cartDims(2);
     nz = G.cartDims(3);
+
+    % Find the bottom nodes. On these nodes, we impose zero displacement
+
     c = zeros(nx*ny*nz, 1);
     c(G.cells.indexMap) = (1 : numel(G.cells.indexMap))';
     bottomcells = c(nx*ny*(nz - 1) +  (1 : (nx*ny))');
@@ -127,16 +124,19 @@ switch opt.bc_cases
     u = zeros(nn, 3);
     m = ones(nn, 3);
     disp_bc = struct('nodes', bcnodes, 'uu', u, 'mask', m);
-    
+
+    % Find outer faces that are not at the bottom. On these faces, we impose
+    % a given pressure.
+
     is_outerface1 = (G.faces.neighbors(:, 1) == 0);
     is_outerface1(bottomfaces) = false;
     is_outerface2 = G.faces.neighbors(:, 2) == 0;
     is_outerface2(bottomfaces) = false;
-    
+
     is_outerface = is_outerface1 | is_outerface2;
 
     outer_faces = find(is_outerface);
-    
+
     outer_pressure = pRef;
     signcoef = (G.faces.neighbors(outer_faces, 1) == 0) - ...
         (G.faces.neighbors(outer_faces, 2) == 0);
@@ -146,7 +146,7 @@ switch opt.bc_cases
 
     force_bc = struct('faces', outer_faces, 'force', force);
 
-    
+
   otherwise
     error('bc_cases not recognized')
 end
@@ -156,33 +156,25 @@ el_bc = struct('disp_bc' , disp_bc, ...
 
 
 %% Setup load for mechanics
+
+% In this example we do not impose any volumetric force
 loadfun = @(x) (0*x);
 
+
 %% Gravity is on - Only for fluid.
-gravity on; 
+gravity on;
 g = gravity;
 if norm(g) == 0
     warning('Gravity off!');
 end
 
 %% Setup mechanic struct
+
 mech = struct('Ev', Ev, 'nuv', nuv, 'el_bc', el_bc, 'load', loadfun);
 
 %% Setup model
 
-is_model_setup = false;
-if ~is_model_setup
-    switch opt.method_case
-      case  'no split'
-        model = MechBlackOilModel(G, rock, fluid, mech);
-      case 'drained split'
-        model = MechFluidDrainedSplitModel(G, rock, fluid, mech, 'fluidModelType', 'blackoil');
-      case 'stress split'
-        model = MechFluidFixedStressSplitModel(G, rock, fluid, mech, 'fluidModelType', 'blackoil');
-      otherwise
-        error('method_case not recognized.')
-    end
-end
+model = MechBlackOilModel(G, rock, fluid, mech);
 
 
 %% Setup wells
@@ -239,4 +231,3 @@ title('Second stress component');
 figure
 plotCellData(G, state.stress(:, 3));
 title('Third stress component');
-
