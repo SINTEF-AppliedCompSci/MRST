8393b6b7047256f7d96ea2423fff3f4d71a14fad
Olav Møyner <olav.moyner@sintef.no>
2017-05-31 16:07:27 +0200

    First implementation of dissolution in BC/SRC

--- ad-core/utils/computeSourcesAndBoundaryConditionsAD.m
+++ ad-core/utils/computeSourcesAndBoundaryConditionsAD.m
@@ -38,8 +38,37 @@ function src = getContributionsStruct(model, force, q_s, b, rhoS, cells, dissolv
         q_r{i} = q_r{i}./b{i}(cells);
     end
     
-    if ~isempty(dissolved)
-        assert(0);
+    if ~isempty(dissolved) && ~isempty(force)
+        q_s0 = q_s;
+        q_t = 0;
+        for i = 1:numel(q_s)
+            q_t = q_t + double(q_s{i});
+        end
+        isInj = q_t > 0;
+        for i = 1:nPh
+            for j = 1:nPh
+                % Add dissolution of component i into phase j
+                r_cell = dissolved{i}{j};
+                if isempty(r_cell)
+                    continue
+                end
+                
+                if isfield(force, 'dissolution')
+                    ds_mat = force.dissolution;
+                    if ndims(ds_mat) == 3
+                        % One value per cell, per phase and component
+                        r_inj = ds_mat(:, i, j);
+                    else
+                        % One value for all cells, per phase and component
+                        r_inj = ds_mat(i, j);
+                    end
+                else
+                    r_inj = 0;
+                end
+                r = isInj.*r_inj + ~isInj.*r_cell(cells);
+                q_s{i} = q_s{i} + r.*q_s0{j};
+            end
+        end
     end
     
     srcMass = q_s;
