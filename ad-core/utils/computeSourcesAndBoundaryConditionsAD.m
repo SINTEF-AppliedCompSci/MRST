e7552050d71d1b08642df68be52214d04a3c46fc
Olav Møyner <olav.moyner@sintef.no>
2017-05-31 17:06:44 +0200

    New BC: Avoid computing values that are not needed, other cleanup

--- ad-core/utils/computeSourcesAndBoundaryConditionsAD.m
+++ ad-core/utils/computeSourcesAndBoundaryConditionsAD.m
@@ -1,37 +1,27 @@
 function [src, bc] = computeSourcesAndBoundaryConditionsAD(model, pressure, s, mob, rho, dissolved, forces)
-
-    if isempty(forces.bc) && isempty(forces.src)
-        return
-    end
+    hasBC  = ~isempty(forces.bc);
+    hasSRC = ~isempty(forces.src);
     rhoS = model.getSurfaceDensities();
-    b = phaseDensitiesTobfactor(rho, rhoS, dissolved);
-    
-    [qVolBC, qVolSRC] = deal(cell(1, numel(rhoS)));
-    [bcCells, srcCells] = deal([]);
-    
-    if ~isempty(forces.bc)
-        % Setup the fluxes from the boundary condition
-        [qVolBC, BCTocellMap, bcCells] = getBoundaryConditionFluxesAD(model, pressure, b, mob, s, forces.bc);
-                
-        qVolBC = fixRepeats(qVolBC, BCTocellMap, bcCells);
+    if hasBC || hasSRC
+        b = phaseDensitiesTobfactor(rho, rhoS, dissolved);
+        if hasBC
+            % Setup the fluxes from the boundary condition
+            [qVolBC, BCTocellMap, bcCells] = getBoundaryConditionFluxesAD(model, pressure, b, mob, s, forces.bc);
+        end
+
+        if hasSRC
+            % Fluxes from source terms
+            [qVolSRC, srcCells] = getSourceFluxesAD(model, mob, s, forces.src);
+        end
+    else
+        [qVolBC, qVolSRC, b] = deal(cell(1, numel(mob)));
+        [bcCells, srcCells, BCTocellMap] = deal([]);
     end
-    
-    if ~isempty(forces.src)
-        % Fluxes from source terms
-        [qVolSRC, srcCells] = getSourceFluxesAD(model, mob, s, forces.src);
-    end
-    src = getContributionsStruct(model, forces.src, qVolSRC, b, rhoS, srcCells, dissolved);
-    bc = getContributionsStruct(model, forces.bc, qVolBC, b, rhoS, bcCells, dissolved);
+    src = getContributionsStruct(forces.src, qVolSRC, b, rhoS, srcCells, dissolved);
+    bc = getContributionsStruct(forces.bc, qVolBC, b, rhoS, bcCells, dissolved, BCTocellMap);
 end
 
-function val = fixRepeats(val, map, cells)
-    for i = 1:numel(val)
-        v = map*val{i};
-        val{i} = v(cells);
-    end
-end
-
-function src = getContributionsStruct(model, force, q_s, b, rhoS, cells, dissolved)
+function src = getContributionsStruct(force, q_s, b, rhoS, cells, dissolved, map)
     nPh = numel(q_s);
     q_r = q_s;
     for i = 1:nPh
@@ -70,13 +60,20 @@ function src = getContributionsStruct(model, force, q_s, b, rhoS, cells, dissolv
             end
         end
     end
+    if nargin > 6
+        mm = map(cells, :);
+        for i = 1:numel(q_s)
+            q_s{i} = mm*q_s{i};
+            q_r{i} = mm*q_r{i};
+        end
+    end
     
     srcMass = q_s;
     for i = 1:nPh
         srcMass{i} = srcMass{i}.*rhoS(i);
     end
     src = struct('phaseMass',   {srcMass}, ...
-                     'phaseVolume', {q_r}, ...
-                     'components',  {[]}, ...
-                     'sourceCells', cells);
+                 'phaseVolume', {q_r}, ...
+                 'components',  {[]}, ...
+                 'sourceCells', cells);
 end
