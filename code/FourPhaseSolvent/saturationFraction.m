2b89773f17214f7443d5dd0b2d12a03d3c48356d
Øystein <oystein.klemetsdal@gmail.com>
2017-06-02 19:30:22 +0200

    Fixed saturation fraction. SOlver seems to converge quite well for small tolerance (tol) values, but may preoduce singular results if tol is too small.

--- code/FourPhaseSolvent/saturationFraction.m
+++ code/FourPhaseSolvent/saturationFraction.m
@@ -1,23 +1,10 @@
 function f = saturationFraction(sa, sb)
 
-%     tol = 10*eps;
-%     zz = num < tol & den < tol;
-% %     num = num - (num < tol).*num;
-% %     den = den - (den < tol).*den;
-%     f = (num./(den + (den < tol).*tol)).*(~zz) + 0.*zz;
-    
-%     tol = 10*eps;
-%     num = num - (num < tol).*num;
-%     den = den - (den < tol).*den;
-
-%     num = num - (num < 0).*num;
-%     den = den - (den < 0).*den;
-
-%     f = num./den;
+    tol = 1e-4;
+%     tol = 0;
+    sb = sb+tol;
+    c = 1+tol;  
+    f = sa./(sa + sb).*c;
 %     f(isnan(double(f))) = 0;
 
-    tol = 1e-2;
-    sb(sb < tol) = tol;
-    f = sa./(sa + sb);
-    
 end
