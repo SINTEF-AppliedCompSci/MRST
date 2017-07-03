083b52bde79a20a97572e17be25272bcfe1dfdb5
Colin Joseph McNeece <colinjmcneece@gmail.com>
2017-07-03 18:00:14 +0200

    copyright

--- models/ChemicalModel.m
+++ models/ChemicalModel.m
@@ -1,5 +1,25 @@
 classdef ChemicalModel < PhysicalModel
+ %ChemicalModel A model for solving equilibrium geochemistry.
+ 
+ %{
+            Copyright 2009-2017 SINTEF DIGITAL, Applied Mathematics and Cybernetics and The University of Texas at Austin.
 
+            This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+            MRST is free software: you can redistribute it and/or modify
+            it under the terms of the GNU General Public License as published by
+            the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+
+            MRST is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+            GNU General Public License for more details.
+
+            You should have received a copy of the GNU General Public License
+            along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+            %}
+ 
     properties
         MasterCompNames      % Names of the master components as first master component.
         logMasterCompNames   % Names of the log master components (= 'log' in
@@ -167,6 +187,25 @@ classdef ChemicalModel < PhysicalModel
             %       The capacitance is followed by the name of each surface
             %       species associated with '>FeO' and the species charge
             %       contribution to the surface.       
+            
+            %{
+            Copyright 2009-2017 SINTEF DIGITAL, Applied Mathematics and Cybernetics and The University of Texas at Austin.
+
+            This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+            MRST is free software: you can redistribute it and/or modify
+            it under the terms of the GNU General Public License as published by
+            the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+
+            MRST is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+            GNU General Public License for more details.
+
+            You should have received a copy of the GNU General Public License
+            along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+            %}
             
             model = model@PhysicalModel([]);
             model.maxIterations = 50;
