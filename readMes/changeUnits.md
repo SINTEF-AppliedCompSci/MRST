99ed214f0f3a4f46de5b422307a21ba650cbf71d
Colin Joseph McNeece <colinjmcneece@gmail.com>
2017-07-12 09:33:16 -0500

    new readme

---
+++ readMes/changeUnits.md
@@ -0,0 +1,53 @@
+# changeUnits
+
+Changes the units of fields in the state variable
+
+## SYNOPSIS
+
+~~~~
+[state] = changeUnits(state, field, unit)
+~~~~
+
+changes the unit of the the specified field in state. All variables in state are by defualt in SI units. 
+
+## OUTPUT
+ 
+### state
+
+a field with the values of state.field changed to the specified units
+
+## EXAMPLE
+
+change the units of a single field
+
+~~~~
+[state] = changeUnits(state, 'activities', mol/litre)
+~~~~
+
+change the units of a multiple fields
+
+~~~~
+fields = {'acitivities', 'surfaceCharges'}
+units = [mol/litre, mili*Coulumb/(nano*meter)^2];
+
+state = chem.getProps(state, fields, units);
+~~~~
+
+If state is a cell array of structures the function will loop over each cell. 
+
+The numeric value of the unit conversion has to be defined. The is a bank of unit conversions within the release. 
+
+## REQUIRED PARAMETERS
+   
+### state        
+
+The state structure produced by ChemicalModel/initstate. State must be populated by the named variable before it can be retrived.
+
+### field
+
+Name of the field for which the unit change is to occur. Can be a string or cell array of strings.
+
+### unit
+
+Numeric value of unit conversion. If numel(unit) == 1 and field has multple entries then the one value of unit will be applied to all fields.
+Otherwise if field is a cell with multple entries then field and unit must have the same size, numel(unit) == numel(field).
--- utils/changeUnits.m
+++ utils/changeUnits.m
@@ -1,29 +1,85 @@
-function [ states ] = changeUnits( states, unit_conv )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [ states ] = changeUnits( states, field, unit_conv )
+%changeUnits converts the specified field in states to the specified units. 
+% 
+% SYNOPSIS:
+% 
+%         [state] = changeUnits(state, field, unit)
+% 
+% OUTPUT:
+%  
+%	state           -a field with the values of state.field changed to the specified units
+% 
+% REQUIRED PARAMETERS:
+%
+%	state           -The state structure produced by ChemicalModel/initstate. 
+%                       State must be populated by the named variable before
+%                       it can be retrived.
+%
+%	field           -Name of the field for which the unit change is to occur. 
+%                       Can be a string or cell array of strings.
+%
+%	unit            -Numeric value of unit conversion. If numel(unit) == 1 
+%                       and field has multple entries then the one value of 
+%                       unit will be applied to all fields. Otherwise if field
+%                       is a cell with multple entries then field and unit must 
+%                       have the same size, numel(unit) == numel(field).unit_conv = unit_conv.^-1;
+%
+% EXAMPLE:
+% 
+%   change the units of a single field
+% 
+%         [state] = changeUnits(state, 'activities', mol/litre)
+% 
+%   change the units of a multiple fields
+% 
+%         fields = {'acitivities', 'surfaceCharges'}
+%         units = [mol/litre, mili*Coulumb/(nano*meter)^2];
+% 
+%         state = chem.getProps(state, fields, units);
+% 
+%   If state is a cell array of structures the function will loop over each cell. 
+% 
+%   The numeric value of the unit conversion must be defined. The is a bank 
+%   of unit conversions within the release. 
+% 
 
-unit_conv = unit_conv^-1;
+assert(~isnumeric(unit_conv), 'The variable "unit_conv" must be numeric.');
 
 if numel(states) == 1
-        states.components = states.components*unit_conv;
-        states.masterComponents = states.masterComponents*unit_conv;
-        if isfield(states, 'activities')
-           states.activities = states.activities*unit_conv;
+    
+    if ~iscell(field)
+        states.(field) = states.(field)*unit_conv;
+    else
+        assert(numel(field) == numel(unit_conv) | numel(unit_conv) == 1, 'The number of elements in unit_conv must be 1 or equal to the number of cells in field.');
+        if numel(unit_conv) == numel(field)
+            for i = 1 : numel(field)
+                states.(field{i}) = states.(field{i})*unit_conv(i);
+            end
+        else
+            for i = 1 : numel(field)
+                states.(field{i}) = states.(field{i})*unit_conv;
+            end
         end
+    end
         
-        states.logcomponents = log(states.components);
-        states.logmasterComponents = log(states.masterComponents);
 else
 
     for i = 1 : numel(states)
     
-        states{i}.components = states{i}.components*unit_conv;
-        states{i}.masterComponents = states{i}.masterComponents*unit_conv;
-        if isfield(states, 'activities')
-           states.activities{i} = states.activities{i}*unit_conv;
+        if ~iscell(field)
+            states{i}.(field) = states{i}.(field)*unit_conv;
+        else
+            assert(numel(field) == numel(unit_conv) | numel(unit_conv) == 1, 'The number of elements in unit_conv must be 1 or equal to the number of cells in field.');
+            if numel(unit_conv) == numel(field)
+                for j = 1 : numel(field)
+                    states{i}.(field{j}) = states{i}.(field{j})*unit_conv(j);
+                end
+            else
+                for j = 1 : numel(field)
+                    states{i}.(field{j}) = states{i}.(field{j})*unit_conv;
+                end
+            end
         end
-        states{i}.logcomponents = log(states{i}.components);
-        states{i}.logmasterComponents = log(states{i}.masterComponents);
         
     end
 end
