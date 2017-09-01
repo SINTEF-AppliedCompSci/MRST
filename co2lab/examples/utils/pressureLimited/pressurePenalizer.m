32dc2e71b23cd17c89848c95ba4c9ece3d57d0af
Rebecca Allen <allen.rebecca.d@gmail.com>
2017-09-01 15:02:55 +0200

    pressurePenalizer: add documentation, add ability to pass in specific cells which are to be penalized.

--- co2lab/examples/utils/pressureLimited/pressurePenalizer.m
+++ co2lab/examples/utils/pressureLimited/pressurePenalizer.m
@@ -1,18 +1,57 @@
 function obj = pressurePenalizer(model, states, schedule, penalty, plim, varargin)
-% states.pressure is a cell array.
-% schedule is only used for time steps.
-% penalty is a scalar.
-% plim is a cell array.
+% Penalize pressure of specific or all cells.
+%
+% SYNOPSIS:
+%   obj = pressurePenalizer(model, states, schedule, penalty, plim)
+%   obj = pressurePenalizer(model, states, schedule, penalty, plim, 'cells', cells)
+%
+% DESCRIPTION:
+%   Evaluate an objective function which penalizes specific or all cell
+%   pressures that surpasses their pressure limit, plim.
+%
+% REQUIRED PARAMETERS:
+%   model       - The model.
+%   states      - Structure containing pressure field.
+%   schedule    - Structure containing time step and well details.
+%   penalty     - The pressure penalty factor. A scalar value.
+%   plim        - Pressure limit for all cells (size must correspond to
+%                 size of state.pressure)
+%
+% OPTIONAL:
+%   cells               - Cell indexes of cells to be assessed in objective
+%                       function. If empty, all cells are considered.
+%                       Default is empty.
+%   ComputePartials     - true or false flag used to determine whether or
+%                       not to compute the partial derivatives of obj.
+%                       function w.r.t state variables, etc. Default is
+%                       false.
+%   tStep               - time step at which to evaluate obj. If empty, all
+%                       timesteps will be used to computed obj. Default is
+%                       empty.
+%
+% RETURNS:
+%   obj         - cell array of size Nx1 (where N is number of time steps
+%               that function gets evaluated at)
+%
+% SEE ALSO:
+%   
 
-
-% format of objective function:
+% Format of objective function:
 %   obj = max(0, sign(p - plim)) * penalty * (p - plim)^2
-% obj is computed for each time step (for both inj and mig periods).
+% obj is computed for each or for a specific time step
 
 
-   opt.ComputePartials = false;
-   opt.tStep = [];
+   opt.cells            = [];
+   opt.ComputePartials  = false;
+   opt.tStep            = [];
    opt = merge_options(opt, varargin{:});
+   
+   if isempty(opt.cells)
+      cells = 1:model.G.cells.num;
+   else
+      cells = opt.cells;
+      plim  = plim(cells);
+   end
    
    num_timesteps = numel(schedule.step.val);
    tSteps = opt.tStep;
@@ -34,21 +73,25 @@ function obj = pressurePenalizer(model, states, schedule, penalty, plim, varargi
    min_amount_under_cinx= zeros(numSteps, 1);
    for step = 1:numSteps     
       state = states{tSteps(step)}; %@@ +1?      
-      p = state.pressure;
+      p = state.pressure(cells);
       % keep track of amount over or amount under plim at each time step
       max_amount_surp(step) = max(0,max(p-plim));
       if max_amount_surp(step) > 0
           [~,cinx] = max(p-plim);
-          max_amount_surp_cinx(step) = cinx;
+          max_amount_surp_cinx(step) = cinx; % this cinx does not correspond
+                                             % to the grid's cell index,
+                                             % rather it is the index of p
+                                             % that surpassed its plim the
+                                             % most
       end
       min_amount_under(step) = max(0,min(plim-p));
       if min_amount_under(step) > 0
          [~,cinx] = min(max(0,(plim-p)));
-         min_amount_under_cinx(step) = cinx;
+         min_amount_under_cinx(step) = cinx; % same as note above
       end
       if opt.ComputePartials
-        sG = state.s(:,2);   % place holders
-        sGmax = state.sGmax; % place holders
+        sG = state.s(cells,2);      % place holders
+        sGmax = state.sGmax(cells); % place holders
         nW = numel(schedule.control(1).W);
         pBHP = zeros(nW, 1); % place holders
         qGs = pBHP;          % place holders
@@ -57,8 +100,8 @@ function obj = pressurePenalizer(model, states, schedule, penalty, plim, varargi
       end
       dt = dts(step);
       tmp = max(0, sign(p - plim)) .* penalty .* (p - plim).^k/1e12; % scaling to MPa
-      tmp = tmp .* model.G.cells.volumes;
-      obj{step} = sum( tmp )./sum(model.G.cells.volumes);
+      tmp = tmp .* model.G.cells.volumes(cells);
+      obj{step} = sum( tmp )./sum(model.G.cells.volumes(cells));
       obj{step} = obj{step} * dt;
       if (tSteps(step) == num_timesteps)
       % no need to compute another portion of the obj fun here
