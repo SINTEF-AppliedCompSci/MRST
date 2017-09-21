80bee9e6c56c742a21b5251a1a73c8b9ba441292
Runar <runar.berge@uib.no>
2017-09-21 09:38:29 +0200

    Added intersection of 3 fractures

--- voronoi3D/fault_intersections.m
+++ voronoi3D/fault_intersections.m
@@ -10,4 +10,43 @@ for f1 = 1:numel(faults)
        end
     end
 end
+
+% then split intersection of intersections
+
+for f = 1:numel(faults)
+    f_pts = faults{f};
+    center = mean(f_pts, 1);
+    f_center = bsxfun(@minus, f_pts, center);
+    R = rotation_matrix_from_plane(f_center);
+    f_xy = f_center * R';
+    assert(sum(abs(f_xy(:, 3)))<1e-6);
+    f_xy = f_xy(:,1:2);
+    internal_pts_xy = zeros(0, 2);
+    int_in_plane = false(numel(intersections),1);
+    for j = 1:numel(intersections)
+        if intersections{j}{2}==f || intersections{j}{3}==f 
+            int_in_plane(j) = true;
+            pts_i = intersections{j}{1};
+            pts_i_center = bsxfun(@minus, pts_i, center);
+            pts_i_xy = pts_i_center * R';
+            assert(all(abs(pts_i_xy(:,3))<1e-6))
+            pts_i_xy = pts_i_xy(:, 1:2);
+            assert(size(pts_i_xy,1)==2)
+            internal_pts_xy = [internal_pts_xy; pts_i_xy];
+        end
+    end
+    num_lines = size(internal_pts_xy,1)/2;
+    internal_pts_xy = mat2cell(internal_pts_xy, 2 * ones(num_lines,1), 2)';
+    [split_pts_xy, ~, ~, ic] = splitAtInt(internal_pts_xy, internal_pts_xy);
+    %split_pts_xy = cell2mat(split_pts_xy)';
+    split_pts = cellfun(@(c) [c, zeros(2, 1)], split_pts_xy, 'un',0);
+    split_pts = cellfun(@(c) c * R + center, split_pts, 'un', 0);
+    
+    % remove old intersection lines and add new
+    new_intersections = intersections(ic);
+    new_intersections = cellfun(@(c1,c2) [c1, c2(2:3)], split_pts, new_intersections, 'un', 0);
+    intersections = [intersections(~int_in_plane), new_intersections];
+
+
+end
 end
