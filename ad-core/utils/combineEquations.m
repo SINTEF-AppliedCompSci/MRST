79629da5d3b8289fd46debad7002f967e1595434
Olav Møyner <olav.moyner@sintef.no>
2017-10-10 14:44:45 +0200

    combineEquations: add a bit of documentation

--- ad-core/utils/combineEquations.m
+++ ad-core/utils/combineEquations.m
@@ -1,3 +1,6 @@
 function h = combineEquations(varargin)
+% Combine equations. For doubles, this is equivialent to a vertical
+% concatenation. Please note that the full implementation used is found as
+% a part of the ADI class. This is the fallback for doubles.
     h = vertcat(varargin{:});
 end
