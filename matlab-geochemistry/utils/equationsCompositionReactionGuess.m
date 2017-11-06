ef6115a601fab30671196374b92da57305cae38c
Colin McNeece <colinjmcneece@gmail.com>
2017-11-06 15:36:25 -0600

    changes surface concentrations to mole fractions

--- matlab-geochemistry/utils/equationsCompositionReactionGuess.m
+++ matlab-geochemistry/utils/equationsCompositionReactionGuess.m
@@ -9,20 +9,41 @@ function [eqs, names, types] = equationsCompositionReactionGuess(model, state, l
     nC = size(CM,2);
     
     components       = cellfun(@(x) exp(x), logComponents, 'UniformOutput', false);
-        
+    masterComponents = cellfun(@(x) exp(x), logMasterComponents,'UniformOutput', false);
+
     logK = model.logReactionConstants;
     
     eqs   = cell(1, model.nMC + model.nR + model.nLC);
     names = cell(1, model.nMC + model.nR + model.nLC);
     types = cell(1, model.nMC + model.nR + model.nLC);
 
+    %% mol fractions
+    surfMat = repmat(model.surfMaster, 1, model.nC).*CM;
+    surfTest = logical(sum(surfMat));
+    
+    moleFraction = components;
+    
+    for i = 1 : model.nC
+        if surfTest(i)
+            surfDen = 0;
+            surfNum = 0;
+            for j = 1 : model.nMC
+                surfNum = surfNum + CM(j,i).*model.surfMaster(j);
+                surfDen = surfDen + double(logical(CM(j,i).*model.surfMaster(j)))*masterComponents{j};
+            end
+            moleFraction{i} = (surfNum./surfDen).*components{i};
+        end
+
+    end
+    logMoleFraction = cellfun(@(x) log(x), moleFraction, 'UniformOutput', false);
+    
     %% reaction matrix
     for i = 1 : model.nR
 
         eqs{i} = -logK{i}(:);
         
         for k = 1 : nC
-            eqs{i} = eqs{i} + RM(i, k).*logComponents{k};
+            eqs{i} = eqs{i} + RM(i, k).*logMoleFraction{k};
         end
         
         % gas reactions
