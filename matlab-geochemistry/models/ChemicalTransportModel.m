b4fd255077bee67a8913c960b989240d518c4744
Colin Joseph McNeece <colinjmcneece@gmail.com>
2017-12-04 13:43:33 -0600

    update to chemicalTransportModel documentation

--- matlab-geochemistry/models/ChemicalTransportModel.m
+++ matlab-geochemistry/models/ChemicalTransportModel.m
@@ -1,21 +1,100 @@
 classdef ChemicalTransportModel < WaterModel
+    % ChemicalTransportModel An object for the the simulation of chemical transport.
+    %
+    % SYNOPSIS:
+    %  model = ChemicalTransportModel(G, rock, fluid, chemicalModel, varargin)
+    %
+    % DESCRIPTION:
+    %   A class of WaterModel which can simulate advective transport of
+    %   chemical through reactive media on arbitary domains. The solver
+    %   uses a fully implicit numerical scheme with upwind discretization.
+    %   The pressure, flow and chemistry system are fully coupled. 
+    %
+    % PARAMETERS:
+    %   G - A geometry strucuture with the domain size and discretization 
+    %
+    %   rock - rock structure containing rock properties
+    %
+    %   fluid - fluid structure containing the properties of the fluid
+    %           phase
+    %
+    %   chemicalModel - the chemical model object to be used during the
+    %                   transport simulation
+    %
+    % RETURNS:
+    %   model - the transport model object containing tools used internally for
+    %           the solution of the coupled chemical, pressure and
+    %           advection system. Useful fields include:
+    %       model.fluidMat - matrix to determine the mass of each element
+    %                       in the fluid phase
+    %       model.plotIter - toggle the plotting of species and element
+    %                        concentration in each newton step within a
+    %                        timestep, [false]
+    %       model.plotFinal - toggle the plotting of species and element
+    %                         concentrations at the end of each time step, 
+    %                         [false]
+    %
+    % EXAMPLES:
+    %   %% define the domain
+    %   G = cartGrid([100, 1, 1]);
+    %   G = computeGeometry(G);
+    % 
+    %   %% Define the rock
+    %   rock.perm = 1*darcy*ones(G.cells.num, 1);
+    %   rock.poro = 0.5*ones(G.cells.num, 1);
+    % 
+    %   %% Define the fluid
+    %   pRef = 0*barsa;
+    %   fluid = initSimpleADIFluid('phases', 'W', 'mu', 1*centi*poise, 'rho', ...
+    %                            1000*kilogram/meter^3, 'c', 1e-10, 'cR', 4e-10, ...
+    %                            'pRef', pRef);
+    % 
+    %   %% Define the chemistry
+    %   elements = {'O', 'H', 'Na*','Cl*'};
+    % 
+    %   species = {'H+*', 'OH-', 'Na+', 'H2O*', 'NaCl','Cl-'};
+    % 
+    %   reactions ={'H2O  = H+  + OH- ',      10^-14*mol/litre, ... 
+    %             'NaCl = Na+ + Cl-',       10^1*mol/litre};
+    % 
+    %   % instantiate chemical model
+    %   chemModel = ChemicalModel(elements, species, reactions);
+    % 
+    %   %% instantiate the transport model
+    %   model = ChemicalTransportModel(G, rock, fluid, chemModel);
+    %
+    % SEE ALSO:
+    %   'simulateScheduleAD', 'ChemicalModel'
 
+    %{
+    Copyright 2009-2017 SINTEF Digital, Mathematics & Cybernetics.
+
+    This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+    MRST is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MRST is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+    %}
+    
     properties
-        % Chemical model for the chemistry
-        chemicalModel
-        % List of all the variable names for the chemistry
-        chemical_fds;
-        % List of all the variable names for the transport
-        transport_fds;
-        % Matrix to compute, for a given component, the amount that is
-        % contained in the fluid (water)
-        fluidMat;
-        % Matrix to compute, for a given component, the amount that is
-        % attached to the surface
-        surfMat;
+        
+        chemicalModel % Chemical model for the chemistry
+        chemical_fds % List of all the variable names for the chemistry
+        transport_fds % List of all the variable names for the transport
+        fluidMat % Matrix to compute, for a given component, the amount that is attached to the surface 
+        surfMat % Matrix to compute, for a given component, the amount that is contained in the fluid (water) 
         plotIter % plot each iteration of the netwon solver
         plotFinal % plot final iteration of the newton solver
-        currentTime
+        currentTime % current time step in seconds
 
     end
 
