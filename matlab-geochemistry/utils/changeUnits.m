6b2a5ae64c2383b05c993ea389cdb36bc2d3c182
Colin Joseph McNeece <colinjmcneece@gmail.com>
2017-12-04 11:21:45 -0600

    update documentation of changeUnits and remove old versions of equation files

--- matlab-geochemistry/utils/changeUnits.m
+++ matlab-geochemistry/utils/changeUnits.m
@@ -1,48 +1,62 @@
 function [ states ] = changeUnits( states, fields, unit_conv )
-%changeUnits converts the specified fields in states to the specified units. 
+% changeUnits converts the specified fields in states to the specified units. 
 % 
 % SYNOPSIS:
-% 
-%         [state] = changeUnits(state, fields, unit)
-% 
-% OUTPUT:
-%  
-%	state           -a fields with the values of state.fields changed to the specified units
-% 
+%   [state] = changeUnits(state, fields, unit)
+%
 % REQUIRED PARAMETERS:
+%   state - The state structure produced by ChemicalModel/initstate. State 
+%           must be populated by the named variable before they can be retrived.
 %
-%	state           -The state structure produced by ChemicalModel/initstate. 
-%                       State must be populated by the named variable before
-%                       it can be retrived.
+%	fields - Name of the fields for which the unit change is to occur. Can 
+%           be a string or cell array of strings.
 %
-%	fields           -Name of the fields for which the unit change is to occur. 
-%                       Can be a string or cell array of strings.
-%
-%	unit            -Numeric value of unit conversion. If numel(unit) == 1 
-%                       and fields has multple entries then the one value of 
-%                       unit will be applied to all fieldss. Otherwise if fields
-%                       is a cell with multple entries then fields and unit must 
-%                       have the same size, numel(unit) == numel(fields).unit_conv = unit_conv.^-1;
+%	unit - Numeric value of unit conversion. If numel(unit) == 1 and fields 
+%           has multple entries then the one value of unit will be applied 
+%           to all fields. Otherwise if fields is a cell with multple entries
+%           then fields and unit must have the same size, 
+%           numel(unit) == numel(fields).unit_conv = unit_conv.^-1;
+% 
+% RETURNS:
+%   state - the state variable with the fields state.fields converted from
+%           SI units to the units specified in units
 %
 % EXAMPLE:
-% 
-%   change the units of a single fields
+%   % change the units of a single fields
 % 
 %         [state] = changeUnits(state, 'activities', mol/litre)
 % 
-%   change the units of a multiple fieldss
+%   %change the units of a multiple fields
 % 
-%         fieldss = {'acitivities', 'surfaceCharges'}
+%         fields = {'acitivities', 'surfaceCharges'}
 %         units = [mol/litre, mili*Coulumb/(nano*meter)^2];
 % 
-%         state = chem.getProps(state, fieldss, units);
+%         state = chem.getProps(state, fields, units);
 % 
 %   If state is a cell array of structures the function will loop over each cell. 
 % 
-%   The numeric value of the unit conversion must be defined. The is a bank 
+%   The numeric value of the unit conversion must be defined. There is a bank 
 %   of unit conversions within the release. 
 % 
 
+%{
+Copyright 2009-2017 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
 assert(isnumeric(unit_conv), 'The variable "unit_conv" must be numeric.');
 unit_conv = unit_conv.^-1;
 
