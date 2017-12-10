aeae444bcad6b0c7f0c09f4b3b6bb474e81bb042
Oystein <oystein.klemetsdal@gmail.com>
2017-12-10 20:33:16 +0100

    Some changes to SPE10 example.

--- code/FourPhaseSolvent/examples/spe10WAGexample.m
+++ code/FourPhaseSolvent/examples/spe10WAGexample.m
@@ -1,28 +1,27 @@
-%% Add necessary modules
-
-mrstModule add ad-blackoil ad-props % AD-moduels
-mrstModule add mrst-gui
-mrstModule add spe10                % SPE10 dataset
-mrstModule add solvent              % Solvent model
-
-gravity reset on
+%% Water-alternating Gas Injection in layer 10 of SPE10
+% In this example, we simulate water-alternating gas injection (WAG), where
+% we inject small volumes of water and solvent gas into the reservoir
+% several cycles. The solvent gas mixes with the reservoir hydrocarbons
+% according to amodified vesrion of the Todd-Longstaff mixing model, which
+% treats the solvent gas as fourth pseudocomponent.
+mrstModule add ad-blackoil ad-props spe10 mrst-gui mrstModule solvent
 
 df = get(0, 'defaultfigureposition');
 close all
 
 %% Set up grid and rock properties
 % We pick up layer 10 of SPE10, and extract the grid and rock properties.
-% We will define our own fluid based on a three-phasespe10 fluid with water, oil
-% and gas
+% We will define our own fluid based on a three-phase fluid with water, oil
+% and gas.
 fluid = initSimpleADIFluid('n'     , [2, 2, 2], ...
                            'rho'   , [1000, 800, 100]*kilogram/meter^3, ...
                            'phases', 'WOG', ...
-                           'mu'    , [2, 3, 0.4]*centi*poise, ...
+                           'mu'    , [1, 3, 0.4]*centi*poise, ...
                            'c'     , [1e-7, 1e-6, 1e-5]/barsa);
 layers = 10;
-[~, model, ~] = setupSPE10_AD('layers', layers);
-G    = model.G;
-rock = model.rock;
+[~, model4Ph, ~] = setupSPE10_AD('layers', layers);
+G    = model4Ph.G;
+rock = model4Ph.rock;
 
 %% Define solvent properties         
 % The solvent model we will use, treats solvent gas as a fourth phase,
@@ -30,8 +29,8 @@ rock = model.rock;
 % the fraction of solvent concentration to total gas concentration,
 % $S_s/(S_g + S_s)$, and the pressure.
 
-sOres_i = 0.36; % Immiscible residual oil saturation
-sOres_m = 0.12; % Miscible residual oil saturation
+sOres_i = 0.38; % Immiscible residual oil saturation
+sOres_m = 0.21; % Miscible residual oil saturation
 fluid   = addSolventProperties(fluid, 'n'    , 2                   , ...
                                     'rho'    , 100*kilogram/meter^3, ...
                                     'mixPar' , 2/3                 , ...
@@ -44,14 +43,14 @@ fluid   = addSolventProperties(fluid, 'n'    , 2                   , ...
 % In regions with only oil, reservoir gas and water, we have traditional
 % black-oil behavior, whereas regions with only water, reservoir oil and
 % solvent gas, the oil is completely miscible with the solvent. In this
-% case, olvent gas mixes with formation oil, effectively altering the
+% case, solvent gas mixes with formation oil, effectively altering the
 % viscosities, densities and relperms according the the Todd-Longstaff
 % model [1]. In the intermediate region, we interpolate between the two
 % extrema depending on the solvent to total gas saturation fraction, and
 % the pressure. We look at terneary plots for the hydrocarbon relperms of
 % the hydrocardon phases when no water is present. Notice how the residual
-% oil saturation (white line) from $S_{or,i}$ (immiscible) to $S_{or,m}$
-% (miscible) with increasing solvent saturation.
+% oil saturation (white line) reduces from $S_{or,i}$ (immiscible) to
+% $S_{or,m}$ (miscible) with increasing solvent saturation.
 
 figure('Position', [df(1:2), 1000, 400]);
 
@@ -93,18 +92,16 @@ end
 % We consider the hello-world of reservoir simulation: A quarter-five spot
 % pattern. The injector in the middle is operater at a fixed injection
 % rate, while the producers in each corner is operated at a fixed
-% bottom-hole pressure of 50 bars. We will simulate a popular injection
-% strategy called water-alternating gas (WAG) injection over a period of
-% four years. During the first 2 years, we inject only water, while the
-% next two, we perform four cycles of equal periods of solvent gas and
-% water injection. This is a common strategy to reduce the growth of
-% viscous fingers associated with gas injection, and uphold a more
-% favourable injected to reservoir fluid mobility ratio.
+% bottom-hole pressure of 50 bars. During the first 2 years, we inject only
+% water, while during the next two, we perform four cycles of equal periods
+% of solvent gas and water injection. This is a common strategy to reduce
+% the growth of viscous fingers associated with gas injection, and uphold a
+% more favourable injected to reservoir fluid mobility ratio.
 
-time = 1*year;                             % Time of the water injection and
-                                           % WAG injection.
-rate = 0.15*sum(poreVolume(G, rock))/time; % We inject a total of 0.3 PVI 
-bhp = 50*barsa;                            % Producer bhp
+time = 1*year;                             % Time of the water injection
+                                           % and WAG injection
+rate = 0.15*sum(poreVolume(G, rock))/time;  % We inject a total of 0.2 PVI 
+bhp = 50*barsa;                            % Producer bottom-hole pressure
 
 producers = {1                                                     , ...
              G.cartDims(1)                                         , ...
@@ -130,39 +127,62 @@ for iNo = 1:numel(injectors)
                 'name'  , ['I', num2str(iNo)]  );
 end
 
-dtWAG       = 15*day; % Timestep size
+dt          = 60*day; % Timestep size for water injection
+dtWAG       = 30*day; % Timestep size for WAG cycles
 useRampUp   = true;   % We use a few more steps each time we change the 
                       % well control to ease the nonlinear solution process
 nCycles     = 4;      % Four WAG cycles
-scheduleWAG = makeWAGschedule(W, nCycles, 'time'     , time     , ...
+scheduleWAG = makeWAGschedule(W, nCycles, 'time'     , 2*time     , ...
                                           'dt'       , dtWAG    , ...
                                           'useRampup', useRampUp);
-schedule = scheduleWAG;
 
-dt                    = 30*day;
 tvec                  = rampupTimesteps(time, dt);
 control               = 2*ones(numel(tvec),1);
-schedule.step.val     = [tvec; scheduleWAG.step.val];
-schedule.step.control = [control; scheduleWAG.step.control];
+scheduleWAG.step.val     = [tvec; scheduleWAG.step.val; tvec];
+scheduleWAG.step.control = [control; scheduleWAG.step.control; control];
 
 %% Define model and initial state, and simulate results
 % The reservoir is initially filled with a mixture of oil, reservoir gas
-% and water.
-model = FourPhaseSolventModel(G, rock, fluid);
-model.extraStateOutput = true;
-sO = 0.75; sG = 0.2;
+% and water. We set up a four-phase solvent model and simulate the
+% schedule.
+model4Ph = FourPhaseSolventModel(G, rock, fluid);
+model4Ph.extraStateOutput = true;
+sO = 0.5; sG = 0.4;
 state0 = initResSol(G, 100*barsa, [1-sO-sG, sO, sG, 0]);
-state0.wellSol = initWellSolAD(W, model, state0);
+state0.wellSol = initWellSolAD(W, model4Ph, state0);
 
-fn = getPlotAfterStepSolvent(state0, model, schedule, ...
+fn = getPlotAfterStepSolvent(state0, model4Ph, schedule, ...
                      'plotWell', true, 'plotReservoir', true);
 
-[ws, states, reports] = simulateScheduleAD(state0, model, schedule, 'afterStepFn', fn);
+[wellSolsWAG, statesWAG, reportsWAG] ...
+    = simulateScheduleAD(state0, model4Ph, scheduleWAG, 'afterStepFn', fn);
 
-%%
+%% Interactive visualization of results
+% We use an interactive plotting tool to visualize reservoir results and
+% the well solutions.
+figure('Position', [df(1:2), 600,800])
+plotToolbar(G, statesWAG); axis equal tight
 
-plotToolbar(G, states)
-% plotWellSols(ws)
+%% Compare with standard water injection
+% We compare the results with standard water injection to see the effects
+% of using WAG.
+for wNo = 1:numel(W)
+    W(wNo).compi = [1,0,0];
+end
 
-%%
+tvec     = rampupTimesteps(4*time, dt);
+schedule = simpleSchedule(tvec, 'W', W);
+model3Ph = ThreePhaseBlackOilModel(G, rock, fluid);
+model3Ph.extraStateOutput = true;
+state0 = initResSol(G, 100*barsa, [1-sO-sG, sO, sG]);
+state0.wellSol = initWellSolAD(W, model3Ph, state0);
 
+[wellSol, states, reports] = simulateScheduleAD(state0, model3Ph, schedule);
+
+%% Visulize well production curves
+% We look at the well production curves to see the difference of the two
+% injection strategies.
+close all
+tWAG = cumsum(scheduleWAG.step.val);
+t    = cumsum(schedule.step.val);
+plotWellSols({wellSolsWAG, wellSol},{tWAG, t})
