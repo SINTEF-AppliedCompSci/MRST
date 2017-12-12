14af016d915724751763f25990c40f262e13ac19
Olav Møyner <olav.moyner@sintef.no>
2017-12-12 12:50:24 +0100

    Remove strange ^V character from example.

--- examples/2D/fineScale2phWells.m
+++ examples/2D/fineScale2phWells.m
@@ -59,7 +59,7 @@ axis equal tight; box on
 % For this example, we will generate the porosity as a Gaussian field. To
 % get a crude approximation to the permeability-porosity relationship, we
 % assume that our medium is made up of uniform spherical grains of diameter
-% dp = 10 m, for which the specic surface area is Av = 6 = dp. With these
+% dp = 10 m, for which the specic surface area is Av = 6 = dp. With these
 % assumptions, using the Carman Kozeny relation, we can then calculate the
 % isotropic permeability (K). The rock properties are then plotted.
 
