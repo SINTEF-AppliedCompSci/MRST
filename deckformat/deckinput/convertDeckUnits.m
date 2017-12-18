ee9e242d5402e942842e898e92c9c1213eeee602
Øystein <oystein.klemetsdal@gmail.com>
2017-12-18 15:11:53 +0100

    Changed to correct units for solvent keywords.

--- deckformat/deckinput/convertDeckUnits.m
+++ deckformat/deckinput/convertDeckUnits.m
@@ -458,7 +458,7 @@ function props = convertPROPS(props, u)
             unt         = [u.gasvol_s/u.liqvol_s, u.press];
             props.(key) = convertFrom(props.(key), unt);
 
-         case {'SGFN', 'SWFN', 'SSFN'},
+         case {'SGFN', 'SWFN'},
             unt = [1, 1, u.press];
 
             for t = 1 : numel(props.(key)),
