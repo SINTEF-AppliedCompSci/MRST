e818cf942944a61ce63817cabe1e2c4f6eda91f1
Øystein <oystein.klemetsdal@gmail.com>
2018-02-14 18:15:02 +0100

    Added functionality to combine 1D polynomials inot nD polynomials.

--- Polynomial/Polynomial.m
+++ Polynomial/Polynomial.m
@@ -12,14 +12,11 @@ classdef Polynomial
             if nargin == 0
                 p.k = [0,0,0];
                 p.w = 1;
-%                 p.w = {0};
             elseif nargin == 1
                 p.k = k;
                 p.w = cell(size(k,1),1);
                 p.w = ones(size(k,1),1);
-%                 [p.w{:}] = deal(1);
             elseif nargin == 2
-%                 assert(isa(w, 'cell'));
                 p.k = k;
                 p.w = w;
             else
@@ -38,15 +35,14 @@ classdef Polynomial
                     for dNo = 1:size(p,2)
                         pk = reshape(repmat(p(dNo).k', size(x,1), 1), p(dNo).dim, [])';
                         pw = reshape(repmat(p(dNo).w', size(x,1), 1), 1, [])';
-                        tmp = pw.*prod(repmat(x, numel(p(dNo)), 1).^pk,2);
-                        val(:, dNo) = accumarray(repmat((1:size(x,1))', numel(p(dNo)), 1), tmp);
+                        tmp = pw.*prod(repmat(x, numel(p(dNo).w), 1).^pk,2);
+                        val(:, dNo) = accumarray(repmat((1:size(x,1))', numel(p(dNo).w), 1), tmp);
                     end
-%                     for wNo = 1:numel(p)
-%                         val = val + prod(x.^p.k(wNo,:),2).*p.w{wNo};
-%                     end
                     [varargout{1:nargout}] = val;
+                    return
                 otherwise
                     [varargout{1:nargout}] = builtin('subsref', p, x);
+                    return
             end
 
         end
@@ -60,10 +56,6 @@ classdef Polynomial
             for dNo = 1:p.dim
                 v(dNo) = dx(p,dNo);
             end
-        end
-        
-        function n = numel(p)
-            n = numel(p.w);
         end
         
         function r = uplus(p)
@@ -96,34 +88,46 @@ classdef Polynomial
             
             if ~isa(p, 'Polynomial')
                 r = q;
-                r.w = cellfun(@(w) w*p, r.w, 'unif', false);
+                r.w = r.w*p;
             elseif ~isa(q, 'Polynomial')
                 r = p;
-                r.w = cellfun(@(w) w*q, r.w, 'unif', false);
+                r.w = r.w*q;
             else
-                pk = repmat(p.k, numel(q), 1);
-                qk = rldecode(q.k, numel(p)*ones(numel(q),1), 1);
+                pk = repmat(p.k, numel(q.w), 1);
+                qk = rldecode(q.k, numel(p.w)*ones(numel(q.w),1), 1);
                 rk = qk + pk;
                 
-                pw = repmat(p.w, numel(q), 1);
-                qw = rldecode(q.w, numel(p)*ones(numel(q),1), 1);
+                pw = repmat(p.w, numel(q.w), 1);
+                qw = rldecode(q.w, numel(p.w)*ones(numel(q.w),1), 1);
                 rw = qw.*pw;
                 
-                
-%                 rw = cell(numel(p)*numel(q),1);
-%                 for i = 1:numel(p)
-%                     for j = 1:numel(q)
-%                         rw{(i-1)*numel(q) + j} = p.w{i}*q.w{j};
-%                     end
-%                 end
-%                 
                 r = Polynomial(rk, rw);
             end
         end
         
-        function r = times(p,q)
+        function r = combine(varargin)
+            pdim = nargin;
+            r = Polynomial(zeros(1,pdim),1);
             
+            for pNo = 1:nargin
+                p = varargin{pNo};
+                pk = zeros(numel(p.w),pdim);
+                pk(:, pNo) = p.k;
+                q = Polynomial(pk, p.w);
+                r = r*q;
+            end
         end
+                
+                
+        
+        function r = rdivide(p,q)
+            r = mtimes(p, power(q,-1));
+        end
+        
+        
+%         function r = times(p,q)
+%             
+%         end
         
         function r = power(p,q)
             if ~isa(q, 'Polynomial')
--- utils/dgBasis.m
+++ utils/dgBasis.m
@@ -1,26 +1,61 @@
-function [k, n, psi, grad_psi] = dgBasis(degree, dim)
+function basis = dgBasis(degree, dim, type)
     
+    nDof = polyDim(degree, dim); 
+        [psi, grad_psi] = deal(cell(nDof,1));
     if degree < 0
-        n = 0;
-        k = [];
+        nDof = 0;
+        k    = [];
     else
-        n = polyDim(degree, dim);
-        
         k = zeros(polyDim(degree, 1).^dim, dim);
         for dNo = 1:dim   
             kk = repmat(0:degree, polyDim(degree, 1).^(dNo-1)    , ...
-                                  polyDim(degree, 1).^(dim - dNo));
+                      polyDim(degree, 1).^(dim - dNo));
             k(:,dNo) = kk(:);
         end
         k = k(sum(k,2) <= degree,:);
         
+        switch type
+            case 'poly'
+                for dofNo = 1:nDof
+                    psi{dofNo}      = Polynomial(k(dofNo,:), 1);
+                    grad_psi{dofNo} = grad(psi{dofNo});
+                end
+
+            case 'legendre'
+                leg = legendre(degree);
+                for dofNo = 1:nDof
+                    l = cell(dim, 1);
+                    for dNo = 1:dim
+                        l{dNo} = leg{k(dofNo,dNo)+1};
+                    end
+                    psi{dofNo} = combine(l{:});
+                    grad_psi{dofNo} = grad(psi{dofNo});
+                end
+                
+            otherwise
+                error('Unknown basis function class');
+        end
+
     end
     
-    [psi, grad_psi] = deal(cell(n,1));
-    for bNo = 1:n
-        psi{bNo} = @(x) prod(x.^k(bNo,:),2);
-        grad_psi{bNo} = @(x) [k(bNo,1).*prod(x.^[max(k(bNo,1)-1,0), k(bNo,2)         ],2) ...
-                              k(bNo,2).*prod(x.^[k(bNo,1)         , max(k(bNo,2)-1,0)],2)];
+    basis = struct('psi'     , {psi}     , ...
+                   'grad_psi', {grad_psi}, ...
+                   'k'       , k         , ...
+                   'nDof'    , nDof      );
+    
+end
+
+function l = legendre(degree)
+    
+    n = polyDim(degree, 1);
+    l = cell(n,1);
+    
+    l{1} = Polynomial(0, 1);
+    if degree > 0
+        l{2} = Polynomial(1,1);
+        for k = 1:n-2
+            l{k+2} = ((2*k+1)*l{2}*l{k+1} - k*l{k})./(k+1);
+        end
     end
     
 end
@@ -34,4 +69,3 @@ function n = polyDim(degree, dim)
     end
     
 end
-        
