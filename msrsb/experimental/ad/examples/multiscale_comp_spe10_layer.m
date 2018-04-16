f4c3dd86fc87efbf9784d45bbbc36266393c4663
Olav Møyner <olav.moyner@sintef.no>
2018-04-16 16:32:30 +0200

    msrsb: Update experimental example

--- msrsb/experimental/ad/examples/multiscale_comp_spe10_layer.m
+++ msrsb/experimental/ad/examples/multiscale_comp_spe10_layer.m
@@ -1,4 +1,4 @@
-mrstModule add mrst-gui ad-props ad-core ad-blackoil blackoil-sequential new-multiscale spe10 coarsegrid
+mrstModule add mrst-gui ad-props ad-core ad-blackoil blackoil-sequential msrsb spe10 coarsegrid
 %% Set up simulation problem
 totTime = 2000*day;
 
@@ -20,7 +20,7 @@ pv = poreVolume(G, rock);
 for i = 1:numel(W)
     isinj = W(i).val > 400*barsa;
     if isinj
-        W(i).val = 3*sum(pv)/totTime;
+        W(i).val = 0.25*sum(pv)/totTime;
         W(i).type = 'rate';
     else
         W(i).val = 4000*psia;
@@ -32,9 +32,7 @@ end
 
 % Set up model with quadratic relperms and constant compressibility in oil
 p0 = 300*barsa;
-fluid = initSimpleADIFluid('mu', [1, 5, 1]*centi*poise, 'rho', [1000, 700, 1], 'n', [2 2 2]);
-c = 0.001/barsa;
-fluid_comp.bO = @(p) exp((p - p0)*c);
+fluid = initSimpleADIFluid('mu', [1, 5, 1]*centi*poise, 'rho', [1000, 700, 1], 'n', [2 2 2], 'c', [1e-6, 1e-6, 1e-6]/barsa);
 
 % Fully implicit model
 modelfi = TwoPhaseOilWaterModel(G, rock, fluid);
@@ -58,7 +56,7 @@ schedule.control.W = W;
 state = initResSol(G, p0, [.2, .8]);
 
 
-state.wellSol = initWellSolAD(W, modelfi, state);
+% state.wellSol = initWellSolAD(W, modelfi, state);
 
 % lim = 2;
 % schedule.step.val     = schedule.step.val(1:lim);
@@ -66,19 +64,17 @@ state.wellSol = initWellSolAD(W, modelfi, state);
 %%
 % split, baseline
 timer = tic();
-[ws_split, states_split, report_split] = simulateScheduleAD(state, model, schedule, ... 
-                                         'outputMinisteps', true);
+[ws_split, states_split, report_split] = simulateScheduleAD(state, model, schedule);
 t_split = toc(timer);
 
-schedule_ms = convertReportToSchedule(report_split, schedule);
 %%
-[wsfi, statesfi] = simulateScheduleAD(state, modelfi, schedule_ms);
+[wsfi, statesfi] = simulateScheduleAD(state, modelfi, schedule);
 %% Plot well curves for fully implicit problem
-plotWellSols({ws_split, wsfi}, cumsum(schedule_ms.step.val), 'datasetnames', {'Sequential', 'FI'})
+plotWellSols({ws_split, wsfi}, cumsum(schedule.step.val), 'datasetnames', {'Sequential', 'FI'})
 
 %% Set up coarse grid
 mrstModule add multiscale-devel coarsegrid mrst-experimental
-coarsefactors = [10, 20, 1];
+coarsefactors = [10, 10, 1];
 cdims = ceil(G.cartDims./coarsefactors);
 
 p = partitionUI(G, cdims);
@@ -94,8 +90,8 @@ else
     s = getSmootherFunction('type', 'jacobi', 'iterations', 10);
 end
 
-tols = 0.1;
-its  = 5;
+tols = 0.01;
+its  = 100;
 
 multiscaleModels = cell(numel(tols), 1);
 
@@ -108,7 +104,8 @@ for i = 1:numel(tols)
                                             'useGMRES', true, ...
                                             'getSmoother', s, ...
                                             'tolerance', tol, ...
-                                            'prolongationType', 'jacobi-mex');
+                                            'useMex', false, ...
+                                            'prolongationType', 'msrsb');
 
     msmodel = MultiscalePressureModel(G, rock, model.fluid, model.pressureModel, mssolver);
 
@@ -128,14 +125,13 @@ for i = 1:nms
     model2.pressureModel.multiscaleSolver.basis = [];
     
     timer = tic();
-    [wellsols{i}, states, reports{i}] = simulateScheduleAD(state, model2, schedule_ms);
+    [wellsols{i}, states, reports{i}] = simulateScheduleAD(state, model2, schedule);
     timing(i) = toc(timer);
 end
 %%
 
 % close all
-plotWellSols({ws_split, wellsols{1}}, cumsum(schedule_ms.step.val), 'datasetnames', {'Sequential', 'Multiscale'})
+plotWellSols({ws_split, wellsols{1}}, cumsum(schedule.step.val), 'datasetnames', {'Sequential', 'Multiscale'})
 
 %%
-plotWellSols({ws_split, wellsols{1}, wsfi}, cumsum(schedule_ms.step.val), 'datasetnames', {'Sequential', 'Multiscale', 'FI'})
-
+plotWellSols({ws_split, wellsols{1}, wsfi}, cumsum(schedule.step.val), 'datasetnames', {'Sequential', 'Multiscale', 'FI'})
