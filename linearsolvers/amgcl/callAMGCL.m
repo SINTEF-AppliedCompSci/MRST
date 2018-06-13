ed62dc124a33c2375ab6413e3c1f15acf1fea758
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2018-06-13 19:58:04 +0200

    AMGCL Solver: Document Calling Interfaces
    This commit adds simple documentation blocks to functions callAMGCL,
    callAMGCL_cpr, and amgcl_matlab.  We also introduce a return value
    for the number of iterations to callAMGCL* to match the expectation
    of the underlying MEX gateway.

--- linearsolvers/amgcl/callAMGCL.m
+++ linearsolvers/amgcl/callAMGCL.m
@@ -1,5 +1,71 @@
-function [x, err] = callAMGCL(A, b, varargin)
-% Call AMGCL
+function [x, err, nIter] = callAMGCL(A, b, varargin)
+%Invoke AMGCL Linear Solver Software
+%
+% DESCRIPTION:
+%   Solves the system of simulatenaous linear equations
+%
+%      Ax = b
+%
+%   in which the coefficient matrix 'A' is sparse.  Single right-hand side
+%   vector.
+%
+% SYNOPSIS:
+%    x              = callAMGCL(A, b)
+%    x              = callAMGCL(A, b, 'pn1', pv1, ...)
+%   [x, err]        = callAMGCL(...)
+%   [x, err, nIter] = callAMGCL(...)
+%
+% PARAMETERS:
+%   A - Coefficient matrix.
+%
+%   b - System right-hand side vector.
+%
+% KEYWORD ARGUMENTS:
+%   isTransposed  - Whether or not the coefficient matrix is transposed on
+%                   input.  This simplifies converting MATLAB's CSC matrix
+%                   format to AMGCL's CSR matrix input format.  Default
+%                   value: `isTransposed = false`.
+%
+%   tolerance     - Linear solver tolerance.  Corresponds to parameter
+%                   'solver.tol' (relative residual reduction) of AMGCL.
+%                   Default value: `tolerance = 1.0e-6`.
+%
+%   maxIterations - Maximum number of linear iterations.  Overrides
+%                   'solver.maxiter' if positive.  Default value:
+%                   `maxIterations = 0` (use AMGCL solver's default,
+%                   typically 100).
+%
+%  Additional keyword arguments passed on to function `getAMGCLMexStruct`.
+%
+% RETURNS:
+%   x     - Solution vector.
+%
+%   err   - Norm of residual at end of solution process.
+%
+%   nIter - Number of linear iterations.
+%
+% SEE ALSO:
+%   `mldivide`, `amgcl_matlab`, `getAMGCLMexStruct`.
+
+%{
+Copyright 2009-2017 SINTEF ICT, Applied Mathematics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
     opt = struct('isTransposed',  false, ...
                  'tolerance',     1e-6, ...
                  'maxIterations',  0);
@@ -12,5 +78,7 @@ function [x, err] = callAMGCL(A, b, varargin)
     if ~opt.isTransposed
         A = A';
     end
-    [x, err] = amgcl_matlab(A, b, amg_opt, opt.tolerance, opt.maxIterations, 1);
+
+    [x, err, nIter] = ...
+       amgcl_matlab(A, b, amg_opt, opt.tolerance, opt.maxIterations, 1);
 end
--- linearsolvers/amgcl/utils/amgcl_matlab.m
+++ linearsolvers/amgcl/utils/amgcl_matlab.m
@@ -1,10 +1,74 @@
 function varargout = amgcl_matlab(varargin)
-% MEX-gateway to AMGCL solver.
+%MEX-gateway to AMGCL Linear Solver Software
+%
 % For more information about AMGCL, please see:
 % Documentation: http://amgcl.readthedocs.io/en/latest/
 % GitHub repository: https://github.com/ddemidov/amgcl/tree/master/examples
 %
-% Tested with commit c1c1fc565f96c1bb3adc0a1bf92429ad744036b4.
+% SYNOPSIS:
+%    x              = amgcl_matlab(A, b, amg_opt, tol, maxIter, id)
+%   [x, err]        = amgcl_matlab(...)
+%   [x, err, nIter] = amgcl_matlab(...)
+%
+% PARAMETERS:
+%   A       - Sparse coefficient matrix of system of simultaneous linear
+%             equations.
+%
+%   b       - System right-hand side.
+%
+%   amg_opt - AMGCL options structure as defined by function
+%             `getAMGCLMexStruct`.
+%
+%   tol     - Relative residual reduction tolerance.  Positive scalar.
+%
+%   maxIter - Maximum number of linear iterations.
+%
+%   id      - Solver method ID.  Integer.  Supported values are `1` for the
+%             regular solver and `2` for the CPR solver.
+%
+% RETURNS:
+%   x     - Solution vector.
+%
+%   err   - Norm of residual at end of solution process.
+%
+%   nIter - Number of linear iterations.
+%
+% NOTE:
+%   This gateway was last tested with commit
+%
+%      946398e535cf2586ca59f37eaf8aa9e72f43fde4
+%
+%   of the AMGCL software (from GitHub).
+%
+%   * Linux (Mint 17.2):
+%       GCC   4.9.4
+%       Boost 1.63.0
+%
+%   * MS Windows 10 (1607):
+%       MSVC  19.14.26430 (Visual Studio 15.7)
+%       Boost 1.67.0
+%
+% SEE ALSO:
+%   `callAMGCL`, `getAMGCLMexStruct`.
+
+%{
+Copyright 2009-2017 SINTEF ICT, Applied Mathematics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
 
    if ~exist('verLessThan', 'file') || verLessThan('matlab', '8.3.0')
       error(['Automated Build Script for ''amgcl_matlab'' is not ', ...
