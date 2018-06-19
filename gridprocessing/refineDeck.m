f819334a4e92eb7bb02c68e96046b24b1b793f37
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2018-06-19 13:34:14 +0200

    refineDeck: Remove Cell Mode

--- gridprocessing/refineDeck.m
+++ gridprocessing/refineDeck.m
@@ -34,6 +34,7 @@ function deck = refineDeck(deck_in, dim, varargin)
 %
 % SEE ALSO:
 %   refineGRDECL
+
 %{
 
 Copyright 2009-2017 SINTEF ICT, Applied Mathematics.
@@ -57,7 +58,7 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
    opt.default_well = false;
    opt = merge_options(opt, varargin{:});
    
-   %% Directly copy specific fields
+   % Directly copy specific fields
    fields = {'SOLUTION','REGIONS','PROPS','RUNSPEC','SUMMARY', ...
              'SCHEDULE', 'UnhandledKeywords'};
    
@@ -67,30 +68,35 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
          deck.(f)=deck_in.(f);
       end
    end
+
    % Fix RUNSPEC
    deck.RUNSPEC.DIMENS = deck_in.RUNSPEC.DIMENS.*dim;
    deck.RUNSPEC.cartDims = deck.RUNSPEC.DIMENS;
       
-   %% Expanding cell-based fields
+   % Expanding cell-based fields
    deck = expand_field(deck, dim, 'SOLUTION', {'EQLNUM', 'PRESSURE', 'SWAT', ...
                                                'SOIL', 'SGAS'});
+
    deck = expand_field(deck, dim, 'REGIONS', {'SATNUM','FIPNUM','PVTNUM', ...
                                               'ROCKNUM', 'IMBNUM', 'EQLNUM'});
+
    deck = expand_field(deck, dim, 'PROPS', {'SWL', 'SWCR', 'SGU', 'SGL', ...
                                             'SGCR', 'SOWCR', 'SOGCR', 'SWU', ...
                                             'ISWCR', 'ISGU', 'ISWL', 'ISWU', ...
                                             'ISGL', 'ISOGCR', 'ISOWCR', ...
                                             'ISGCR', 'SWATINIT'});
-   %% Refine the grid
+
+   % Refine the grid
    deck.GRID = refineGrdecl(deck_in.GRID, dim);
-   
-   %% Refine schedule (i.e. wells)
+
+   % Refine schedule (i.e. wells)
    if isfield(deck, 'SCHEDULE') 
       deck.SCHEDULE = refine_wells(deck.SCHEDULE, dim, opt.default_well);
    end
 end
 
-% ----------------------------------------------------------------------------
+% -------------------------------------------------------------------------
+
 function deck = expand_field(deck, dim, gname, fname)
    cartDims_new = deck.RUNSPEC.cartDims;
    cartDims_orig = cartDims_new ./ dim;
@@ -110,62 +116,63 @@ function deck = expand_field(deck, dim, gname, fname)
    end
 end
 
-% ----------------------------------------------------------------------------
+% -------------------------------------------------------------------------
+
 function schedule = refine_wells(schedule, dim, default_well)
    for j = 1:numel(schedule.control)
       cur_ctrl = schedule.control(j);
       if ~isfield(cur_ctrl, 'WELSPECS')
          continue;
       end
-      
-      %% Updating lateral (i,j) cell indices of well positions
+
+      % Updating lateral (i,j) cell indices of well positions
       for i=1:2
          E = cur_ctrl.WELSPECS(:,2+i);
          schedule.control(j).WELSPECS(:,2+i) = ...
              cellfun(@(x) dim(i).*(x-1) + ceil(dim(i)/2), E, 'unif', false);
       end
-      
-      %% Updating lateral cell indices (i, j) for individual well completions
+
+      % Updating lateral cell indices (i, j) for individual well completions
       for i=1:2
          E = cur_ctrl.COMPDAT(:,1+i);
          schedule.control(j).COMPDAT(:,1+i) = ...
              cellfun(@(x) dim(i).*(x-1) + ceil(dim(i)/2), E, 'unif', false);
       end
-      
+
       % Updating vertical cell indices (k) for individual well completions
       vw_ind = [cur_ctrl.COMPDAT{:,13}] == 'Z';  % indicator for vertical wells
-      
+
       E = cur_ctrl.COMPDAT(:, 4); % start index 
       schedule.control(j).COMPDAT(vw_ind, 4) = ...
           cellfun(@(x) dim(3) .* (x-1) + 1, E(vw_ind), 'unif', false);
       schedule.control(j).COMPDAT(~vw_ind, 4) = ...
           cellfun(@(x) dim(3) .* (x-1) + ceil(dim(3)/2), E(~vw_ind), 'unif', false);
-      
+
       E = cur_ctrl.COMPDAT(:, 5); % end index
       schedule.control(j).COMPDAT(vw_ind ,5) = ...
           cellfun(@(x) dim(3) .* x, E(vw_ind), 'unif', false);
       schedule.control(j).COMPDAT(~vw_ind, 5) = ...
           cellfun(@(x) dim(3) .* (x-1) + ceil(dim(3)/2), E(~vw_ind), 'unif', false);
-      
-      %% Repeat perforations in the right direction and change fields
+
+      % Repeat perforations in the right direction and change fields
       compdat_old = schedule.control(j).COMPDAT;
       schedule.control(j).COMPDAT = [];
-      
+
       % loop over perforations
       for i = 1:size(compdat_old, 1) 
          gg = compdat_old(i,:); % the current perforation
          tmp_str = struct('X', 1, 'Y', 2, 'Z', 3);
          mydim = tmp_str.(cell2mat(gg(13)));
-         
+
          if mydim ~= 3 % z-direction already taken care of above
-            
+
             % repeat the current completion once per refinement
             gg = repmat(gg, dim(mydim), 1); 
             gg(:, 1+mydim) = arrayfun(@(x) x, ...
                                       cell2mat(gg(:, 1+mydim)) + ...
                                       (0:dim(mydim)-1)', 'unif', false);
          end
-         
+
          if (default_well)
             gg(:, [8, 10]) = {'-1'};
          else
@@ -173,15 +180,15 @@ function schedule = refine_wells(schedule, dim, default_well)
             if any(nz) % trans
                gg(nz, 8) = arrayfun(@(x) x, [gg{nz, 8}] / dim(mydim), 'unif', false); 
             end
+
             nz = [gg{:, 10}] > 0;
             if any(nz) % KH
                gg(nz, 10) = arrayfun(@(x) x, [gg{nz, 10}] / dim(mydim), 'unif', false); 
             end
          end
-         
+
          schedule.control(j).COMPDAT=[schedule.control(j).COMPDAT; gg];
-         
+
       end
    end
-      
 end
