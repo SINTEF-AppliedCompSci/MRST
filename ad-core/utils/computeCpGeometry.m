6d5b2eaceb4742ec00b5c3f6d1472cacb8326699
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2018-06-19 15:04:33 +0200

    CP Geometry: Reduce Code Duplication
    In particular, introduce a few helper functions that leverage
    MATLAB's ND-arrays rather than cell arrays.

--- ad-core/utils/computeCpGeometry.m
+++ ad-core/utils/computeCpGeometry.m
@@ -1,90 +1,81 @@
-function [cellCenters, cellFaceCenters, cellDims]=computeCpGeometry(G,grdecl,varargin)
-opt=struct('on_all_cpc',false);
-opt=merge_options(opt,varargin{:});
-[x, y, z] = buildCornerPtNodes(grdecl);
+function [cellCenters, cellFaceCenters, cellDims] = ...
+      computeCpGeometry(G, grdecl, varargin)
+opt = struct('on_all_cpc', false);
+opt = merge_options(opt, varargin{:});
 
-px1 = x(1:2:end, 1:2:end, 1:2:end);
-px2 = x(2:2:end, 1:2:end, 1:2:end);
-px3 = x(1:2:end, 2:2:end, 1:2:end);
-px4 = x(2:2:end, 2:2:end, 1:2:end);
-px5 = x(1:2:end, 1:2:end, 2:2:end);
-px6 = x(2:2:end, 1:2:end, 2:2:end);
-px7 = x(1:2:end, 2:2:end, 2:2:end);
-px8 = x(2:2:end, 2:2:end, 2:2:end);
-clear x
+[fc, cc] = calc_centroids(grdecl);
 
-py1 = y(1:2:end, 1:2:end, 1:2:end);
-py2 = y(2:2:end, 1:2:end, 1:2:end);
-py3 = y(1:2:end, 2:2:end, 1:2:end);
-py4 = y(2:2:end, 2:2:end, 1:2:end);
-py5 = y(1:2:end, 1:2:end, 2:2:end);
-py6 = y(2:2:end, 1:2:end, 2:2:end);
-py7 = y(1:2:end, 2:2:end, 2:2:end);
-py8 = y(2:2:end, 2:2:end, 2:2:end);
-clear y
-
-pz1 = z(1:2:end, 1:2:end, 1:2:end);
-pz2 = z(2:2:end, 1:2:end, 1:2:end);
-pz3 = z(1:2:end, 2:2:end, 1:2:end);
-pz4 = z(2:2:end, 2:2:end, 1:2:end);
-pz5 = z(1:2:end, 1:2:end, 2:2:end);
-pz6 = z(2:2:end, 1:2:end, 2:2:end);
-pz7 = z(1:2:end, 2:2:end, 2:2:end);
-pz8 = z(2:2:end, 2:2:end, 2:2:end);
-clear z
-
-p1 = [px1(:) py1(:) pz1(:)];
-clear px1 py1 pz1
-p2 = [px2(:) py2(:) pz2(:)];
-clear px2 py2 pz2
-p3 = [px3(:) py3(:) pz3(:)];
-clear px3 py3 pz3
-p4 = [px4(:) py4(:) pz4(:)];
-clear px4 py4 pz4
-p5 = [px5(:) py5(:) pz5(:)];
-clear px5 py5 pz5
-p6 = [px6(:) py6(:) pz6(:)];
-clear px6 py6 pz6
-p7 = [px7(:) py7(:) pz7(:)];
-clear px7 py7 pz7
-p8 = [px8(:) py8(:) pz8(:)];
-clear px8 py8 pz8
-
-xfc1 = (p1 + p3 + p5 + p7)/4;
-xfc2 = (p2 + p4 + p6 + p8)/4;
-yfc1 = (p1 + p2 + p5 + p6)/4;
-yfc2 = (p3 + p4 + p7 + p8)/4;
-zfc1 = (p1 + p2 + p3 + p4)/4;
-zfc2 = (p5 + p6 + p7 + p8)/4;
-clear p1 p2 p3 p4 p5 p6 p7 p8
-
-cc = (zfc1 + zfc2)/2;
-
-if ~(opt.on_all_cpc)
-   cellNo     = rldecode(1:G.cells.num, diff(G.cells.facePos), 2)';
-   cartCellNo = G.cells.indexMap(cellNo);
-   fType      = G.cells.faces(:, 2);
-   cellCenters      =  cc( G.cells.indexMap, :);
+if ~opt.on_all_cpc
+   cellNo      = gridCellNo(G);
+   cartCellNo  = G.cells.indexMap(cellNo);
+   fType       = G.cells.faces(:, 2);
+   cellCenters = cc(G.cells.indexMap, :);
 else
-   nc=prod(grdecl.cartDims);
-   cartCellNo = rldecode(1:nc, 6, 2) .';
-   fType =repmat([1:6]',nc,1);
-   cellCenters=cc;
+   nc = prod(grdecl.cartDims);
+   cartCellNo  = rldecode(1:nc, 6, 2) .';
+   fType       = repmat((1:6).', [nc, 1]);
+   cellCenters = cc;
 end
 
+pick_face_centres = @(i) ...
+   bsxfun(@times, double(fType == i), fc{i}(cartCellNo, :));
 
 cellFaceCenters  =  ...
-   ((fType == 1)*[1 1 1]).*xfc1(cartCellNo, :) + ...
-   ((fType == 2)*[1 1 1]).*xfc2(cartCellNo, :) + ...
-   ((fType == 3)*[1 1 1]).*yfc1(cartCellNo, :) + ...
-   ((fType == 4)*[1 1 1]).*yfc2(cartCellNo, :) + ...
-   ((fType == 5)*[1 1 1]).*zfc1(cartCellNo, :) + ...
-   ((fType == 6)*[1 1 1]).*zfc2(cartCellNo, :);
+   pick_face_centres(1) + pick_face_centres(2) + ...
+   pick_face_centres(3) + pick_face_centres(4) + ...
+   pick_face_centres(5) + pick_face_centres(6);
 
 if nargout > 2
     vnorm = create_vector_norm_function();
-    cellDims = [vnorm(xfc2-xfc1), vnorm(yfc2-yfc1), vnorm(zfc2-zfc1)];
+
+    cellDims = [              ...
+       vnorm(fc{2} - fc{1}) , ...
+       vnorm(fc{4} - fc{3}) , ...
+       vnorm(fc{6} - fc{5}) ];
 end
+end
+
+%--------------------------------------------------------------------------
+
+function [fc, cc] = calc_centroids(grdecl)
+   p = corner_points(grdecl);
+
+   make_face_centre = ...
+      @(nodeID) sum(p(:, :, nodeID), 3) ./ numel(nodeID);
+
+   fc{1} = make_face_centre([1, 3, 5, 7]);
+   fc{2} = make_face_centre([2, 4, 6, 8]);
+
+   fc{3} = make_face_centre([1, 2, 5, 6]);
+   fc{4} = make_face_centre([3, 4, 7, 8]);
+
+   fc{5} = make_face_centre([1, 2, 3, 4]);
+   fc{6} = make_face_centre([5, 6, 7, 8]);
+
+   cc = (fc{1} + fc{2}) ./ 2;
+end
+
+%--------------------------------------------------------------------------
+
+function p = corner_points(grdecl)
+   [x, y, z] = buildCornerPtNodes(grdecl);
+
+   [i, j, k] = ndgrid(1 : 2);
+   ijk = [i(:), j(:), k(:)];
+
+   point_coord = @(id) ...
+      [coord(ijk(id,:), x), coord(ijk(id,:), y), coord(ijk(id,:), z)];
+
+   p = arrayfun(point_coord, 1 : 8, 'UniformOutput', false);
+   p = cat(3, p{:});
+end
+
+%--------------------------------------------------------------------------
+
+function c = coord(ijk, v)
+    c = reshape(v(ijk(1) : 2 : end, ...
+                  ijk(2) : 2 : end, ...
+                  ijk(3) : 2 : end), [], 1);
 end
 
 %--------------------------------------------------------------------------
