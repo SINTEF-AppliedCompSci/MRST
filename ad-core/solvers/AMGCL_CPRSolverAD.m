c8029002f186b67f8424a8b0ba09ed7b71b1cda9
Olav Møyner <olav.moyner@sintef.no>
2018-06-22 10:41:18 +0200

    AMGCL_CPR: Fix pressure equation formation for adjoint.

--- ad-core/solvers/AMGCL_CPRSolverAD.m
+++ ad-core/solvers/AMGCL_CPRSolverAD.m
@@ -36,87 +36,98 @@ classdef AMGCL_CPRSolverAD < AMGCLSolverAD
            [result, report] = solver.callAMGCL_MEX(A, b, 2);
        end
        
-        function [dx, result, report] = solveLinearProblem(solver, problem, model)
-            n = model.G.cells.num;
-            if solver.amgcl_setup.block_size == 0
-                % Solver has not been told about block size, try to compute
-                % it from what we are given.
-                s = getSampleAD(problem.equations{:});
-                nv = s.getNumVars();
-                isCell = problem.indexOfType('cell');
-                solver.amgcl_setup.block_size = sum(nv(isCell)/n);
-            end
-            
-            % Get and apply scaling
-            if solver.doApplyScalingCPR
-                scale = model.getScalingFactorsCPR(problem, problem.equationNames, solver);
-                
-                if solver.amgcl_setup.use_drs
-                    % Solver will take the sum for us, we just weight each
-                    % equation. Note: This is not the entirely correct way
-                    % of doing this, as solver could do this by itself.
-                    for i = 1:numel(scale)
-                        if ~strcmpi(problem.types{i}, 'cell')
-                            continue
-                        end
-                        if (numel(scale{i}) > 1 || scale{i} ~= 0)
-                             problem.equations{i} = problem.equations{i}.*scale{i};
-                        end
-                    end
-                else
-                    % We form pressure equation for the solver.
-                    e = 0;
-                    for i = 1:numel(scale)
-                        if ~strcmpi(problem.types{i}, 'cell')
-                            continue
-                        end
-                        if (numel(scale{i}) > 1 || scale{i} ~= 0)
-                             e = e + problem.equations{i}.*scale{i};
-                        end
-                    end
-                    problem.equations{1} = e;
-                end
-            end
-            m = solver.amgcl_setup.block_size;
-            assert(m > 0);
+       function [dx, result, report] = solveLinearProblem(solver, problem, model)
+           problem = solver.prepareProblemCPR(problem, model);
+           [dx, result, report] = solveLinearProblem@LinearSolverAD(solver, problem, model);
+       end
+       
+       function [dx, result, report] = solveAdjointProblem(solver, problemPrev,problemCurr, adjVec, objective, model)
+           if ~isempty(problemPrev)
+                problemPrev = solver.prepareProblemCPR(problemPrev, model);
+           end
+           problemCurr = solver.prepareProblemCPR(problemCurr, model);
+           [dx, result, report] = solveAdjointProblem@LinearSolverAD(solver, problemPrev,problemCurr, adjVec, objective, model);
+       end
 
-            if isempty(solver.keepNumber)
-                if solver.reduceToCell
-                    % Will be reduced to ncell by block_size syste,
-                    ndof = n*m;
-                else
-                    % We have no idea and should check
-                    problem = problem.assembleSystem();
-                    ndof = size(problem.A, 1);
-                    if solver.amgcl_setup.active_rows == 0
-                        % Only the first n*m entries are cell-wise
-                        % variables, tell the solver this
-                        solver.amgcl_setup.active_rows = n*m;
-                    end
-                end
-            else
-                ndof = solver.keepNumber;
-            end
-
-
-            if isempty(solver.variableOrdering) || numel(solver.variableOrdering) ~= ndof
-                if solver.useSYMRCMOrdering
-                    sym_ordering = getGridSYMRCMOrdering(model.G);
-                else
-                    sym_ordering = [];
-                end
-                ordering = getCellMajorReordering(n, m, 'ndof', ndof, 'cell_ordering', sym_ordering);
-                solver.variableOrdering = ordering;
-                if isempty(solver.equationOrdering) || numel(solver.equationOrdering) ~= ndof
-                    solver.equationOrdering = ordering;
-                end
-            end
-            
-            [dx, result, report] = solveLinearProblem@LinearSolverAD(solver, problem, model);
-        end
-        
        function setSRelaxation(solver, v)
            solver.amgcl_setup.s_relaxation = translateOptionsAMGCL('relaxation', v);
+       end
+       
+       function problem = prepareProblemCPR(solver, problem, model)
+           n = model.G.cells.num;
+           if solver.amgcl_setup.block_size == 0
+               % Solver has not been told about block size, try to compute
+               % it from what we are given.
+               s = getSampleAD(problem.equations{:});
+               nv = s.getNumVars();
+               isCell = problem.indexOfType('cell');
+               solver.amgcl_setup.block_size = sum(nv(isCell)/n);
+           end
+           
+           % Get and apply scaling
+           if solver.doApplyScalingCPR
+               scale = model.getScalingFactorsCPR(problem, problem.equationNames, solver);
+               
+               if solver.amgcl_setup.use_drs
+                   % Solver will take the sum for us, we just weight each
+                   % equation. Note: This is not the entirely correct way
+                   % of doing this, as solver could do this by itself.
+                   for i = 1:numel(scale)
+                       if ~strcmpi(problem.types{i}, 'cell')
+                           continue
+                       end
+                       if (numel(scale{i}) > 1 || scale{i} ~= 0)
+                           problem.equations{i} = problem.equations{i}.*scale{i};
+                       end
+                   end
+               else
+                   % We form pressure equation for the solver.
+                   e = 0;
+                   for i = 1:numel(scale)
+                       if ~strcmpi(problem.types{i}, 'cell')
+                           continue
+                       end
+                       if (numel(scale{i}) > 1 || scale{i} ~= 0)
+                           e = e + problem.equations{i}.*scale{i};
+                       end
+                   end
+                   problem.equations{1} = e;
+               end
+           end
+           m = solver.amgcl_setup.block_size;
+           assert(m > 0);
+           
+           if isempty(solver.keepNumber)
+               if solver.reduceToCell
+                   % Will be reduced to ncell by block_size syste,
+                   ndof = n*m;
+               else
+                   % We have no idea and should check
+                   problem = problem.assembleSystem();
+                   ndof = size(problem.A, 1);
+                   if solver.amgcl_setup.active_rows == 0
+                       % Only the first n*m entries are cell-wise
+                       % variables, tell the solver this
+                       solver.amgcl_setup.active_rows = n*m;
+                   end
+               end
+           else
+               ndof = solver.keepNumber;
+           end
+           
+           
+           if isempty(solver.variableOrdering) || numel(solver.variableOrdering) ~= ndof
+               if solver.useSYMRCMOrdering
+                   sym_ordering = getGridSYMRCMOrdering(model.G);
+               else
+                   sym_ordering = [];
+               end
+               ordering = getCellMajorReordering(n, m, 'ndof', ndof, 'cell_ordering', sym_ordering);
+               solver.variableOrdering = ordering;
+               if isempty(solver.equationOrdering) || numel(solver.equationOrdering) ~= ndof
+                   solver.equationOrdering = ordering;
+               end
+           end
        end
    end
 end
