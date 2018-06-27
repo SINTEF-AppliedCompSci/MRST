106fe7a016d771189df5b176752850161c325e15
Olav Møyner <olav.moyner@gmail.com>
2018-06-27 10:11:10 +0200

    getCellMajorReordering: Add option for equation-reordering.

--- ad-core/utils/getCellMajorReordering.m
+++ ad-core/utils/getCellMajorReordering.m
@@ -2,6 +2,7 @@ function ordering = getCellMajorReordering(ncell, block_size, varargin)
     ncell_total = ncell*block_size;
 
     opt = struct('ndof', ncell_total, ...
+                 'equation_ordering', [], ...
                  'cell_ordering', []);
     opt = merge_options(opt, varargin{:});
 
@@ -11,6 +12,9 @@ function ordering = getCellMajorReordering(ncell, block_size, varargin)
     subs = reshape(subs, [], block_size)';
     if ~isempty(opt.cell_ordering)
         subs = subs(:, opt.cell_ordering);
+    end
+    if ~isempty(opt.equation_ordering)
+        subs = subs(opt.equation_ordering, :);
     end
     subs = subs(:);
     ordering(1:ncell_total) = subs;
