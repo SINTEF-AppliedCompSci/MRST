128a0fa844da33177a223abf44695dc94d722977
Stein Krogstad <stein.krogstad@sintef.no>
2018-08-14 13:29:42 +0200

    well-plot class for interactive purposes

---
+++ mrst-gui/utils/wellPlotHandle.m
@@ -0,0 +1,160 @@
+classdef wellPlotHandle < handle
+    properties
+        injectors
+        producers
+    end
+    properties (Dependent, SetObservable)
+        Visible              % 'on' / 'off'
+        visibleInjectors     % logic array   
+        visibleProducers
+        Parent
+    end
+    
+    methods
+        
+        function s = wellPlotHandle(G, W, varargin)
+            opt = struct('Parent',     [], ...
+                         'Visible',  'on');
+            [opt, extraOpt] = merge_options(opt, varargin{:});
+              
+            if isempty(opt.Parent)
+                opt.Parent = gca;
+            end
+            
+            if ~isfield(W(1), 'trajectory')
+                W = addTrajectories(W, G, 10);
+            end
+            %wp = plotWellData(G, W, extraOpt{:}, 'linePlot', true);
+            %wp = setParents(wp, opt.Parent);
+            %wp = wp(:);
+            
+            %s.injectors = wp([W.sign]>0);
+            %s.producers = wp([W.sign]<0);
+            
+            s.injectors = plotWellData(G, W([W.sign]>0), extraOpt{:}, 'linePlot', true, 'color', [.2 .2 .7]);
+            s.injectors = setParents(s.injectors(:), opt.Parent);
+            s.producers = plotWellData(G, W([W.sign]<0), extraOpt{:}, 'linePlot', true, 'color', [.7 .2 .2]);
+            s.producers = setParents(s.producers(:), opt.Parent);
+            s.Visible   = opt.Visible; 
+            set([s.injectors.label], 'interpreter', 'none')
+            set([s.producers.label], 'interpreter', 'none')
+        end
+        
+        function set.Visible(s, val)
+            s.visibleInjectors = val;
+            s.visibleProducers = val;
+        end
+        function val = get.Visible(s)
+            if all([s.visibleInjectors; s.visibleProducers])
+                val = 'on';
+            elseif all([~s.visibleInjectors; ~s.visibleProducers])
+                val = 'off';
+            else
+                val = 'mixed';
+            end
+        end
+        
+        function set.visibleInjectors(s, val)
+            if ischar(val)
+                if strcmp(val, 'on')
+                    val = true(numel(s.injectors), 1);
+                else
+                    val = false(numel(s.injectors), 1);
+                end
+            elseif ~islogical(val)
+                tmp = false(numel(s.injectors), 1);
+                tmp(val) = true;
+                val = tmp;
+            end
+            ix = val~=s.visibleInjectors;
+            if any(ix)
+                ix = find(ix);
+                fn = fieldnames(s.injectors);
+                opts = {'off', 'on'};
+                for k = 1:numel(ix)
+                    for n = 1:numel(fn)
+                        set([s.injectors(ix(k)).(fn{n})], 'Visible', opts{val(ix(k))+1})
+                    end
+                end
+            end
+        end
+        function val = get.visibleInjectors(s)
+            val = arrayfun(@(x)strcmp(x.label.Visible, 'on'), s.injectors);
+            val = val(:);
+        end
+            
+        function set.visibleProducers(s, val)
+            if ischar(val)
+                if strcmp(val, 'on')
+                    val = true(numel(s.producers), 1);
+                else
+                    val = false(numel(s.producers), 1);
+                end
+            elseif ~islogical(val)
+                tmp = false(numel(s.producers), 1);
+                tmp(val) = true;
+                val = tmp;
+            end
+            ix = val~=s.visibleProducers;
+            if any(ix)
+                ix = find(ix);
+                fn = fieldnames(s.producers);
+                opts = {'off', 'on'};
+                for k = 1:numel(ix)
+                    for n = 1:numel(fn)
+                        set([s.producers(ix(k)).(fn{n})], 'Visible', opts{val(ix(k))+1})
+                    end
+                end
+            end
+        end
+        function val = get.visibleProducers(s)
+            val = arrayfun(@(x)strcmp(x.label.Visible, 'on'), s.producers);
+            val = val(:);
+        end       
+    end
+end
+
+function wp = setParents(wp, p)
+fn = fieldnames(wp);
+for k = 1:numel(fn)
+    set([wp.(fn{k})], 'Parent', p)
+end
+end
+    
+function W = addTrajectories(W, G, np)
+% add well trajectory for plotting, currently just np points along a single
+% quadratic curve
+
+for k = 1:numel(W)
+    c  = G.cells.centroids(W(k).cells,:);
+    if ~strcmp(W(k).type, 'aquifer')
+        nc = size(c,1);
+        c0 = c(1,:);
+        % take point furthest away as toe
+        v  = c-ones(nc,1)*c0;
+        d2 = sum(v.*v, 2);
+        [~, ix] = max(d2);
+        c2 = c(ix,:);
+        
+        s = linspace(0, 1, np)';
+        [f0, f1, f2] = deal((1-s).^2, 2*(1-s).*s, s.^2);
+        % x,y,z - coords take second point as mean shifted twice away from c0-cn
+        mc = mean(c);
+        %
+        n   = (c2-c0)/norm(c2-c0);
+        pmc = c0 - dot(c0-mc, n)*n;
+        c1  =  2*mc - pmc;
+        c1(3) = min(c2(3),c1(3));
+        traj  = f0*c0 + f1*c1 + f2*c2;
+        
+        
+        %s = (linspace(0, 1, np).^(1/3))';
+        %[f0, f1, f2] = deal((1-s).^2, 2*(1-s).*s, s.^2);
+        %traj(:, 3) = f0*c0(3) + f1*c1(3) + f2*c2(3);
+        
+        W(k).trajectory = traj;
+    else
+        W(k).trajectory = c(1,:);
+    end
+end
+end 
