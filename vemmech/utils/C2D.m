22966041f8c1b118e402465b72c4f4b51fd10bd6
Xavier Raynaud <xavier.raynaud@sintef.no>
2018-08-17 09:43:10 +0200

    added comment in function

--- vemmech/utils/C2D.m
+++ vemmech/utils/C2D.m
@@ -2,7 +2,10 @@ function D = C2D(C, G,varargin)
 % Compute the matrix of normalized strain energies (D) from the elasticity
 % tensor (C) associated with grid (G).
 %
-% convert form voit notation to matrix in VEM format of vem formulation
+% convert Stiffness tensor from Voigt notation to plain notations (no
+% factors are applied to the off-diagonal coefficients of the strain
+% tensor). The latter is used in our formulation of VEM. The resulting tensor
+% D is such that, for S in plain notation, we have: S'DS = energy. 
 
 %{
 Copyright 2009-2018 SINTEF ICT, Applied Mathematics.
@@ -22,25 +25,24 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
-opt=struct('inv',false);
-opt=merge_options(opt,varargin{:});
 
+    opt = struct('inv', false);
+    opt = merge_options(opt, varargin{:});
+    
+    if(G.griddim == 3)
+        nlin = 6; 
+    else
+        assert(G.griddim == 2)
+        nlin = 3; 
+    end
 
-
-   if(G.griddim == 3)
-      nlin = 6; 
-   else
-      assert(G.griddim == 2)
-      nlin = 3; 
-   end
-
-   factor = [ones(G.griddim, G.griddim),            ones(G.griddim, nlin - G.griddim) * 2; ...
-             ones(nlin - G.griddim, G.griddim) * 2, ones(nlin - G.griddim, nlin - G.griddim) * ...
-             4]; 
-   factor = reshape(factor, 1, []);
-   if(opt.inv)
-       factor=1./factor;
-   end
-   D = bsxfun(@times, C, factor);
+    factor = [ones(G.griddim, G.griddim),            ones(G.griddim, nlin - G.griddim) * 2; ...
+              ones(nlin - G.griddim, G.griddim) * 2, ones(nlin - G.griddim, nlin - G.griddim) * ...
+              4]; 
+    factor = reshape(factor, 1, []);
+    if(opt.inv)
+        factor = 1./factor;
+    end
+    D = bsxfun(@times, C, factor);
 end
 
