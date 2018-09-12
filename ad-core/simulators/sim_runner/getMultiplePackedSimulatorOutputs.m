2ccb6b9076adc54f89cbf818ce5792cbc371881c
Olav Møyner <olav.moyner@sintef.no>
2018-09-12 13:24:49 +0200

    getMultiplePackedSimulatorOutputs: Support for filtering missing outout.

--- ad-core/simulators/sim_runner/getMultiplePackedSimulatorOutputs.m
+++ ad-core/simulators/sim_runner/getMultiplePackedSimulatorOutputs.m
@@ -1,12 +1,24 @@
-function [all_ws, all_states, all_reports, names, T, grids] = getMultiplePackedSimulatorOutputs(problems, varargin)
+function [all_ws, all_states, all_reports, names, T, grids, act] = getMultiplePackedSimulatorOutputs(problems, varargin)
+    opt = struct('minSteps', 0);
+    [opt, arg] = merge_options(opt, varargin{:});
     np = numel(problems);
+    counts = zeros(np, 1);
     [T, all_states, all_ws, all_reports, names, grids] = deal(cell(np, 1));
     for i = 1:np
         problem = problems{i};
-        [all_ws{i}, all_states{i}, all_reports{i}] = getPackedSimulatorOutput(problem, varargin{:});
+        [all_ws{i}, all_states{i}, all_reports{i}] = getPackedSimulatorOutput(problem, arg{:});
         names{i} = problem.Name;
-        n = numel(all_ws{i});
-        T{i} = cumsum(problem.SimulatorSetup.schedule.step.val(1:n));
+        counts(i) = numel(all_ws{i});
+        T{i} = cumsum(problem.SimulatorSetup.schedule.step.val(1:counts(i)));
         grids{i} = problem.SimulatorSetup.model.G;
     end
+    
+    act = counts >= opt.minSteps;
+    
+    all_ws = all_ws(act);
+    all_states = all_states(act);
+    all_reports = all_reports(act);
+    names = names(act);
+    T = T(act);
+    grids = grids(act);
 end
