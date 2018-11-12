9703c935e945e1a3fa765d53c3f05c1666a8aedd
Olav Møyner <olav.moyner@gmail.com>
2018-11-12 22:21:26 +0100

    selectFacilityFromDeck: Switch error to warning for VFP parsing.

--- ad-core/models/facilities/selectFacilityFromDeck.m
+++ ad-core/models/facilities/selectFacilityFromDeck.m
@@ -11,13 +11,16 @@ function facility = selectFacilityFromDeck(deck, model)
        isfield(deck.SCHEDULE.control(1), 'VFPINJ')
         facility.VFPTablesInjector = {};
         facility.VFPTablesProducer = {};
+        hasWarned = false;
         for cNo = 1:numel(deck.SCHEDULE.control)
             vfpinj = deck.SCHEDULE.control(cNo).VFPINJ;
             for tno = 1:numel(vfpinj)
                 if ~isempty(vfpinj{tno})
                     if numel(facility.VFPTablesInjector) >= tno
-                        assert(isempty(facility.VFPTablesInjector{tno}), ...
-                            'MRST does not presently support overwriting VFP tables');
+                        if ~isempty(facility.VFPTablesInjector{tno}) && ~hasWarned
+                            warning('MRST does not presently support overwriting VFP tables');
+                            hasWarned = true;
+                        end
                     end
                     facility.VFPTablesInjector{tno} = VFPTable(vfpinj{tno});
                 end
@@ -27,8 +30,10 @@ function facility = selectFacilityFromDeck(deck, model)
             for tno = 1:numel(vfpprod)
                 if ~isempty(vfpprod{tno})
                     if numel(facility.VFPTablesProducer) >= tno
-                        assert(isempty(facility.VFPTablesProducer{tno}), ...
-                            'MRST does not presently support overwriting VFP tables');
+                        if ~isempty(facility.VFPTablesProducer{tno}) && ~hasWarned
+                            warning('MRST does not presently support overwriting VFP tables');
+                            hasWarned = true;
+                        end
                     end
                     facility.VFPTablesProducer{tno} = VFPTable(vfpprod{tno});
                 end
