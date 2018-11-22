a876674465bd92dde860cf5eec5148cbafc9a919
Olav Møyner <olav.moyner@sintef.no>
2018-11-22 09:30:19 +0100

    New properties: Adding density class for BO

---
+++ ad-core/properties/BlackOilDensity.m
@@ -0,0 +1,48 @@
+classdef BlackOilDensity < GridProperty
+    properties
+    end
+    
+    methods
+
+        function rho = evaluate(prop, model, state)
+            act = model.getActivePhases();
+            nph = sum(act);
+            rho = cell(1, nph);
+            ix = 1;
+            
+            
+            f = model.fluid;
+            p = model.getProp(state, 'pressure');
+            if model.water
+                bW = prop.evaluateFunctionOnGrid(f.bW, p);
+                rho{ix} = f.rhoWS.*bW;
+                ix = ix + 1;
+            end
+            
+            if model.oil
+                if model.disgas
+                    rs = model.getProp(state, 'rs');
+                    flag = false(size(double(p)));
+                    bO = prop.evaluateFunctionOnGrid(f.bO, p, rs, flag);
+                else
+                    bO = prop.evaluateFunctionOnGrid(f.bO, p);
+                end
+                rho{ix} = f.rhoOS.*bO;
+                ix = ix + 1;
+            end
+            
+            if model.gas
+                if model.vapoil
+                    rs = model.getProp(state, 'rv');
+                    flag = false(size(double(p)));
+                    bG = prop.evaluateFunctionOnGrid(f.bG, p, rv, flag);
+                else
+                    bG = prop.evaluateFunctionOnGrid(f.bG, p);
+                end
+                rho{ix} = f.rhoGS.*bG;
+
+                ix = ix + 1;
+            end
+        end
+    end
+end
