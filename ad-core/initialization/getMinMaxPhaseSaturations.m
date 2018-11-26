2cb85d8890e029de6157c33bf7b37b424a9cbd33
Olav Møyner <olav.moyner@gmail.com>
2018-11-26 08:38:39 +0100

    getMinMaxPhaseSaturations: Generalize to varying phases with new setup

--- ad-core/initialization/getMinMaxPhaseSaturations.m
+++ ad-core/initialization/getMinMaxPhaseSaturations.m
@@ -5,12 +5,30 @@ function [s_min, s_max] = getMinMaxPhaseSaturations(model, satnum)
     nc = numel(satnum);
     if isfield(model.fluid, 'krPts')
         pts = model.fluid.krPts;
-        s_min = [pts.w(satnum, 2), max(pts.ow(satnum, 2), pts.og(satnum, 2)), pts.g(satnum, 2)];
-        s_max = [pts.w(satnum, 3), max(pts.ow(satnum, 3), pts.og(satnum, 3)), pts.g(satnum, 3)];
+        phases = model.getPhaseNames();
+        nph = numel(phases);
+        [s_min, s_max] = deal(1, nph);
+        for i = 1:nph
+            ph = lower(phases(i));
+            if strcmp(ph, 'o') && ~isfield(pts, 'o')
+                if model.water && model.gas
+                    s_min(i) = min(pts.ow(satnum, 2), pts.og(satnum, 2));
+                    s_max(i) = max(pts.ow(satnum, 3), pts.og(satnum, 3));
+                elseif model.water
+                    s_min(i) = pts.ow(satnum, 2);
+                    s_max(i) = pts.ow(satnum, 3);
+                elseif model.gas
+                    s_min(i) = pts.og(satnum, 2);
+                    s_max(i) = pts.og(satnum, 3);
+                end
+            else
+                s_min(i) = pts.(ph)(satnum, 2);
+                s_max(i) = pts.(ph)(satnum, 3);
+            end
+        end
     else
         [s_min, s_max] = getMinMaxPhaseSaturationsFromRelPerm(model, 1e-6, cellInx(1));
     end
-%     s_max = ones(size(s_min));
 
     s_min = repmat(s_min, nc, 1);
     s_max = repmat(s_max, nc, 1);
