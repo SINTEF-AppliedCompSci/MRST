2bb1e4856c0e2c4ec6c2861217859568f7d04073
Olav Møyner <olav.moyner@gmail.com>
2018-11-26 19:49:51 +0100

    Props: Add implementations of BO stuff.

--- ad-core/properties/BlackOilFlowPropertyFunctions.m
+++ ad-core/properties/BlackOilFlowPropertyFunctions.m
@@ -7,6 +7,10 @@ classdef BlackOilFlowPropertyFunctions < FlowPropertyFunctions
     methods
         function props = BlackOilFlowPropertyFunctions(model)
             props@FlowPropertyFunctions(model);
+            % Get PVT region from density
+            pvt = props.Density.regions;
+            props.RsMax = RsMax(model, pvt);
+            props.RvMax = RvMax(model, pvt);
         end
         
         function state = evaluateProperty(props, model, state, name)
--- ad-core/properties/RsMax.m
+++ ad-core/properties/RsMax.m
@@ -7,7 +7,7 @@ classdef RsMax < GridProperty
             p = model.getProp(state, 'pressure');
             if model.disgas
                 f = model.fluid;
-                rsSat = prop.evaluateFunctionOnGrid(f.rsSat, ss);
+                rsSat = prop.evaluateFunctionOnGrid(f.rsSat, p);
             else
                 rsSat = 0*p;
             end
