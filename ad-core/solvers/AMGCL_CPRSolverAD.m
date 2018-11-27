6fbe08483b62913ff56ff05aae91fbcd2db5dad2
Olav Møyner <olav.moyner@gmail.com>
2018-11-27 10:48:17 +0100

    AMGCL_CPRSolverAD: Avoid forming pressure equation for regular CPR

--- ad-core/solvers/AMGCL_CPRSolverAD.m
+++ ad-core/solvers/AMGCL_CPRSolverAD.m
@@ -67,31 +67,16 @@ classdef AMGCL_CPRSolverAD < AMGCLSolverAD
            % Get and apply scaling
            if solver.doApplyScalingCPR
                scale = model.getScalingFactorsCPR(problem, problem.equationNames, solver);
-               
-               if solver.amgcl_setup.use_drs
-                   % Solver will take the sum for us, we just weight each
-                   % equation. Note: This is not the entirely correct way
-                   % of doing this, as solver could do this by itself.
-                   for i = 1:numel(scale)
-                       if ~strcmpi(problem.types{i}, 'cell')
-                           continue
-                       end
-                       if (numel(scale{i}) > 1 || any(scale{i} ~= 0))
-                           problem.equations{i} = problem.equations{i}.*scale{i};
-                       end
+               % Solver will take the sum for us, we just weight each
+               % equation. Note: This is not the entirely correct way
+               % of doing this, as solver could do this by itself.
+               for i = 1:numel(scale)
+                   if ~strcmpi(problem.types{i}, 'cell')
+                       continue
                    end
-               else
-                   % We form pressure equation for the solver.
-                   e = 0;
-                   for i = 1:numel(scale)
-                       if ~strcmpi(problem.types{i}, 'cell')
-                           continue
-                       end
-                       if (numel(scale{i}) > 1 || scale{i} ~= 0)
-                           e = e + problem.equations{i}.*scale{i};
-                       end
+                   if (numel(scale{i}) > 1 || any(scale{i} ~= 0))
+                       problem.equations{i} = problem.equations{i}.*scale{i};
                    end
-                   problem.equations{1} = e;
                end
            end
            m = solver.amgcl_setup.block_size;
