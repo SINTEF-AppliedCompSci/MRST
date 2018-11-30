93ef54932ec9472388e9b1526582cc151e704da3
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2018-11-30 16:08:57 +0100

    Started implementing adaptive gridding. Currently only naive approach for finite-volume schemes.

--- gridding/devscripts/devRefine.m
+++ gridding/devscripts/devRefine.m
@@ -1,99 +1,183 @@
-mrstModule add dg vem vemmech ad-props ad-core ad-blackoil ...
-    blackoil-sequential gasinjection mrst-gui reorder matlab_bgl coarsegrid
+mrstModule add dg vem vemmech ad-props ad-core ad-blackoil       ...
+    blackoil-sequential gasinjection mrst-gui reorder matlab_bgl ...
+    coarsegrid incomp
 mrstVerbose on;
 
 %%
 
-n  = 4;
-GF = computeGeometry(cartGrid([n,n]));
-p  = partitionUI(GF, [2, 2]);
+n  = 100;
+l = 1000;
+GF = computeGeometry(cartGrid([n,n], [l,l]));
+% GF = computeGeometry(pebiGrid(l/n, [l,l]));
+rockF = makeRock(GF, 100*milli*darcy, 1);
+
+% T = getFaceTransmissibility(GF, rockF);
+% p = partitionMETIS(GF, T, 25);
+% p = processPartition(GF, p);
+% p = compressPartition(p);
+p = partitionUI(GF, [10,10]);
+
 GC = generateCoarseGrid(GF, p);
 GC = coarsenGeometry(GC);
+GC = assignCoarseNodes(GC);
 
 %%
 
-close all
+[G, map1] = refineGrid(GC, GC, GF, [1, GC.cells.num]');
+plotGrid(G)
 
-G = refineGrid(GC, GF, [55,56]');
-plotToolbar(GF, G.partition);
-colormap(hsv)
-axis equal tight
+% close all
+% [G1, map1] = refineGrid(GC, GF, 1');
+% figure, plotGrid(G1);
+% 
+% [G2, map2] = refineGrid(G1, GF, 3');
+% figure, plotGrid(G2);
+% G = G2;
+
+% axis equal tight
 % G = computeVEMGeometry(G)
-
 %%
 
-rock = makeRock(G, 100*milli*darcy, 1);
-fluid = initSimpleADIFluid('phases', 'WO'                   , ...
-                           'rho'   , [1000, 1]*kilogram/meter^3, ...
-                           'mu'    , [0.5, 0.5]*centi*poise     , ...
-                           'n'     , [1, 1]                 );
+rock  = makeRock(G, 100*milli*darcy, 1);
 
-modelfi = TwoPhaseOilWaterModel(G, rock, fluid);
-modelFV = getSequentialModelFromFI(modelfi);
-modelDG = modelFV;
+
+fluid = initSimpleADIFluid('phases', 'WO'                      , ...
+                           'rho'   , [1000, 1]*kilogram/meter^3, ...
+                           'mu'    , [0.5, 0.5]*centi*poise    , ...
+                           'n'     , [1, 1]                    );
+
+modelFI  = TwoPhaseOilWaterModel(G, rock, fluid);
+modelSI  = getSequentialModelFromFI(modelFI);
+modelASI = AdaptiveSequentialPressureTransportModel(modelSI.pressureModel, modelSI.transportModel, GF, G, rockF);
+
+modelFIF  = TwoPhaseOilWaterModel(GF, rockF, fluid);
+modelSIF  = getSequentialModelFromFI(modelFIF);
 
 %%
 
 time = 2*year;
 rate = 2*sum(poreVolume(G, rock))/time;
+
+xw = [0,0; l,l];
+
 W = [];
-W = addWell(W, G, rock, 1          , 'type', 'rate', 'val', rate    , 'comp_i', [1,0]);
-W = addWell(W, G, rock, G.cells.num, 'type', 'bhp' , 'val', 50*barsa, 'comp_i', [1,0]);
+d = pdist2(G.cells.centroids, xw);
 
-if 0
-    src = addSource([], 20, rate/3, 'sat', [1,0]);
-else
-    src = [];
-end
+[~, ix] = min(d(:,1));
+W = addWell(W, G, rock, ix, 'type', 'rate', 'val', rate    , 'comp_i', [1,0]);
+[~, ix] = min(d(:,2));
+W = addWell(W, G, rock, ix, 'type', 'bhp' , 'val', 50*barsa, 'comp_i', [1,0]);
 
-dt    = 30*day;
+dt    = 7*day;
 dtvec = rampupTimesteps(time, dt, 0);
-schedule = simpleSchedule(dtvec, 'W', W, 'src', src);
+schedule = simpleSchedule(dtvec, 'W', W);
 
 sW     = 0.0;
 state0 = initResSol(G, 100*barsa, [sW,1-sW]);
-% state0.cells = (1:G.cells.num);
+
+WF = [];
+d = pdist2(GF.cells.centroids, xw);
+
+[~, ix] = min(d(:,1));
+WF = addWell(WF, GF, rockF, ix, 'type', 'rate', 'val', rate    , 'comp_i', [1,0]);
+[~, ix] = min(d(:,2));
+WF = addWell(WF, GF, rockF, ix, 'type', 'bhp' , 'val', 50*barsa, 'comp_i', [1,0]);
+
+scheduleF = simpleSchedule(dtvec, 'W', WF);
+
+sW     = 0.0;
+state0F = initResSol(GF, 100*barsa, [sW,1-sW]);
 
 %%
 
-degree = 0:5;
+modelASI.pressureModel.extraStateOutput = true;
+[wsASI, stASI, rep] = simulateScheduleAD(state0, modelASI, schedule);
+
+%%
+
+[wsSI, stSI, rep] = simulateScheduleAD(state0, modelSI, schedule);
+
+%%
+
+[wsSIF, stSIF, rep] = simulateScheduleAD(state0F, modelSIF, scheduleF);
+
+%%
 
 
-% degree = [4];
-degree = [1,2];
-[jt, ot, mt] = deal(Inf);
-% 
-% jt = Inf;
-mt = 1e-6;
-ot = 1e-3;
-% ot = 0.2;
+close all
+fig = figure('Position', [-2000, 0, 1500, 600]);
+M = struct('cdata',[],'colormap',[]);
+d = 10;
 
-
-[wsDG, statesDG, disc] = deal(cell(numel(degree),1));
-nls = NonLinearSolver('maxIterations', 25, 'useLinesearch', false);
-for dNo = 1:numel(degree)
-    disc{dNo} = DGDiscretization(modelDG.transportModel                   , ...
-                                    'degree'               , degree(dNo)  , ...
-                                    'basis'                , 'legendre'   , ...
-                                    'useUnstructCubature'  , true        , ...
-                                    'jumpTolerance'        , jt           , ...
-                                    'outTolerance'         , ot           , ...
-                                    'outLimiter'           , 'orderReduce', ...
-                                    'meanTolerance'        , mt           , ...
-                                    'limitAfterConvergence', false         , ...
-                                    'plotLimiterProgress'  , false        );
-    modelDG.transportModel = TransportOilWaterModelDG(G, rock, fluid, ...
-                                       'disc'    , disc{dNo}        , ...
-                                       'dsMaxAbs', 0.2, ...
-                                       'nonlinearTolerance', 1e-3);
-    
-    modelDG.transportModel.conserveOil   = true;
-    modelDG.transportModel.conserveWater = false;
-    
-    modelDG.pressureModel = PressureOilWaterModelSemiDG(G, rock, fluid, ...
-                                       'disc'    , disc{dNo}        );
-    modelDG.transportNonLinearSolver = nls;
-
-    state0 = assignDofFromState(modelDG.transportModel.disc, state0);
-    [wsDG{dNo}, statesDG{dNo}, rep] = simulateScheduleAD(state0, modelDG, schedule);
+wcut = nan(numel(dt,1),3);
+for sNo = 1:numel(stASI)
+    wcut(sNo,:) = [wsSI{sNo}(2).wcut, wsASI{sNo}(2).wcut, wsSIF{sNo}(2).wcut];
 end
+
+clr = lines(3);
+    
+dt = cumsum(schedule.step.val)/day;
+
+for sNo = 1:numel(stASI)
+    
+    subplot(1,3,1)
+    plotCellData(stASI{sNo}.G, stASI{sNo}.s(:,1), 'edgec', 'none')
+    plotGrid(stASI{sNo}.G, 'facec', 'none', 'edgealpha', 0.2);
+    caxis([0,1]);
+    axis equal tight
+    
+    subplot(1,3,2)
+    plotCellData(G, stSI{sNo}.s(:,1), 'edgec', 'none')
+    plotGrid(G, 'facec', 'none', 'edgealpha', 0.2);
+    caxis([0,1]);
+    axis equal tight
+    
+    colormap(jet)
+    pause(0.1)
+    
+    subplot(1,3,3)
+    hold on
+    wc = wcut;
+    wc(sNo+1:end,:) = nan;
+    plot(dt, wc(:,1), 'linew', 2, 'color', clr(1,:));
+    plot(dt, wc(:,2), 'linew', 2, 'color', clr(2,:));
+    plot(dt, wc(:,3), '--', 'linew', 4, 'color', clr(3,:));
+    xlim([0, dt(end)]);
+    ylim([0,1]);
+    hold off
+    pbaspect([1,1,1])
+    box on
+    ylabel('Water cut')
+    xlabel('Time (days)')
+    legend({'Coarse', 'Adaptive', 'Reference'}, 'location', 'northwest');
+
+    
+    rect = [d, d, fig.Position(3:4) - [d,d]];
+%     rect = pos;
+    M(sNo) = getframe(fig, rect);
+    
+end
+
+%%
+
+
+
+pth = mrstPath('dg');
+name = 'refinement';
+duration = 10;
+vo = VideoWriter(fullfile(pth, name));
+vo.FrameRate = numel(stASI)/duration;
+open(vo);
+
+writeVideo(vo, M);
+
+close(vo)
+
+%%
+
+plotWellSols({wsSI, wsASI, wsSIF})
+
+%%
+
+close all
+plotToolbar(G, st)
--- gridding/refineGrid.m
+++ gridding/refineGrid.m
@@ -1,12 +1,47 @@
-function GH = refineGrid(GC, GF, cells)
+function [G, mappings] = refineGrid(G, GC, GF, cells)
 
-    GH = GC;
-    ix = any(GH.partition == cells',2);
+    % Mappings (fine grid) <-> (old grid)
+    partition = G.partition;
+    fine2old  = partition;
+    
+    GF.cells.num;
+    old2fine  = nan(G.cells.num,1);
+    old2fine(partition) = (1:GF.cells.num);
+    
+    fine2coarse = GC.partition;
+    old2coarse = fine2coarse(old2fine);
+    cells = old2coarse(cells);
+    
     partition     = GC.partition;
-    partition     = partition + nnz(ix) - 1;
-    partition(ix) = 1:nnz(ix);
+    ix = any(partition == cells',2);
     
-    GH = generateCoarseGrid(GF, partition);
-    GH = coarsenGeometry(GH);
+    % Assign new numbers to non-refined cells
+    partition(ix) = max(partition) + 1;
+    map = nan(max(partition), 1);
+    up = unique(partition);
+    map(up) = 1:numel(up);
     
+    partition     = map(partition);
+    partition(ix) = (1:nnz(ix)) + numel(up) - 1;
+    
+    G = generateCoarseGrid(GF, partition);
+    G = coarsenGeometry(G);
+    
+    new2fine = nan(G.cells.num,1);
+    new2fine(partition) = (1:GF.cells.num);
+    fine2new = partition;
+    
+    new2old = fine2old(new2fine);
+    old2new = fine2new(old2fine);
+    
+    G.refined = false(G.cells.num,1);
+    G.refined(old2new(cells)) = true;
+    
+    mappings = struct('new2fine', new2fine, ...
+                      'fine2new', fine2new, ...
+                      'old2fine', old2fine, ...
+                      'fine2old', fine2old, ...
+                      'new2old' , new2old , ...
+                      'old2new' , old2new );
+
 end
