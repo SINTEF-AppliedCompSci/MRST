0b54598e5b62ff5199e37af8d242e974ba160683
hnil <Halvor.M.Nilsen@sintef.no>
2018-12-04 15:21:43 +0100

    Added simulators using opm and mrst and several utils for writing decks from mrst: EXPERIMENTAL

---
+++ deckformat/experimental/reMapWellsols.m
@@ -0,0 +1,27 @@
+function [wellsols,ind]=reMapWellsols(wellsols)
+%% assume uniform
+ws1=wellsols{1}{1};
+ind=nan(numel(ws1),1);
+wnames = {ws1.name};
+for j=2:numel(wellsols)
+    ws2 = wellsols{j}{1};
+    wntmp = {ws2.name};
+    wnames=intersect(wnames,wntmp);
+end
+
+for i=1:numel(wellsols)
+    for j=1:numel(wellsols{i})
+        ws2=wellsols{i}{j};
+        wellsols{i}{j}=ws2(1:numel(wnames));
+        wname2={ws2.name};
+        for k=1:numel(wnames)
+           kk = find([cellfun(@(x) strcmp(wnames{k},x),wname2)]);
+           if(~isempty(kk))
+                wellsols{i}{j}(k)=ws2(kk);
+                ind(k)=kk;
+           else
+               assert(false);
+           end
+        end
+    end
+end
