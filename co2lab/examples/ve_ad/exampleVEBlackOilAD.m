582183b0003653003a085e1a2d7738011f2a3367
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2018-12-20 11:37:56 +0100

    VE Blackoil Example: Use AD-Based Initialisation Routine
    Function 'initEclipseFluid' no longer exists.  Switch the example to
    use module ad-core's wrapper function 'initEclipseProblemAD'.

--- co2lab/examples/ve_ad/exampleVEBlackOilAD.m
+++ co2lab/examples/ve_ad/exampleVEBlackOilAD.m
@@ -18,46 +18,31 @@ depth      = 1000;               % Initial depth
 ipress     = 200;                % Initial pressure
 
 %% Create input deck and construct grid
-% Create an input deck that can be used together with the fully-implicit black
-% oil solver. Since the grid is constructed as part of setting up the input
-% deck, we obtain it directly.
-[deck, G] = sinusDeckAdiVE([nx ny nz], [Lx Ly H], nsteps, dt, ...
-                         -.1*pi/180, depth, phi, perm, ...
-                         (H*phi*Lx*Ly)*0.2*day/year, ipress);
+% Create an input deck that can be used together with the fully-implicit
+% black oil solver. Since the grid is constructed as part of setting up the
+% input deck, we obtain it directly.
+deck = sinusDeckAdiVE([nx, ny, nz], [Lx, Ly, H], nsteps, dt, ...
+                      -.1*pi/180, depth, phi, perm, ...
+                      (H*phi*Lx*Ly)*0.2*day/year, ipress);
 
-% Alternatively, we could read deck from file and construct the grid
-% deck = readEclipseDeck( ...
-%    fullfile(mrstPath('co2lab'),'data','decks','sinusDeckAdiVE.DATA');
-% G = initEclipseGrid(deck);
+deck = convertDeckUnits(deck);
 
-figure, plotGrid(G),view([0 -1 0]), box on
-                      
-                 
 %% Initialize data structures
-% First, we convert the input deck to SI units, which is the unit system used by
-% MRST. Second, we initialize the rock parameters from the deck; the resulting
-% data structure may have to be post-processed to remove inactive cells. Then we
-% set up the fluid object.
-deck  = convertDeckUnits(deck);
-rock  = initEclipseRock(deck);
-rock  = compressRock(rock, G.cells.indexMap);
-fluid = initDeckADIFluid(deck);
+[x0, model, schedule] = initEclipseProblemAD(deck);
 
-%systemOW  = initADISystem({'Oil', 'Water'}, G, rock, fluid);
+%% Show simulation grid
+% This is stored as the |G| data member of the simulation |model|.
+figure, plotGrid(model.G), view([0, -1, 0]), box on
 
 %% Prepare simulation model, schedule and initial state
 % Before we can run the simulation, we make sure that we have an initial
-% hydrostatic pressure distribution.  We proceed by creating a simulation model
-% object that the solver can work with, representing a two-phase oil/water
-% system, where we let 'oil' represent the CO2 phase.  Finally, we convert the
-% schedule from the input deck into MRST format.
-x0 = initEclipseState(G, deck, initEclipseFluid(deck));
-z  = G.cells.centroids(:,3);
+% hydrostatic pressure distribution.  We proceed by creating a simulation
+% model object that the solver can work with, representing a two-phase
+% oil/water system, where we let 'oil' represent the CO2 phase.  Finally,
+% we convert the schedule from the input deck into MRST format.
+z  = model.G.cells.centroids(:,3);
 x0.pressure = ipress*barsa +(z(:)-z(end))*norm(gravity)*deck.PROPS.DENSITY(2);
 x0.sGmax = x0.s(:,2);
-
-model = TwoPhaseOilWaterModel(G, rock, fluid);
-schedule = convertDeckScheduleToMRST(model, deck);
 
 %% Run the schedule setup in the file
 % Before we can run the schedule, we make sure that we have an initial
@@ -65,10 +50,9 @@ schedule = convertDeckScheduleToMRST(model, deck);
 % input deck and start the simulator.
 [wellSols, states] = simulateScheduleAD(x0, model, schedule);
 
-
 %% Plot results
 %figure
-Gt = topSurfaceGrid(G);
+Gt = topSurfaceGrid(model.G);
 xc = Gt.cells.centroids(:,1);
 zt = Gt.cells.z;
 zb = zt + Gt.cells.H;
@@ -78,17 +62,17 @@ for nn=1:numel(states)
     
     subplot(2,2,1),cla
     title('pressure')
-    plotCellData(G,state.pressure/barsa,'EdgeColor','none');
+    plotCellData(model.G,convertTo(state.pressure,barsa),'EdgeColor','none');
     colorbar('horiz'), caxis([100 200])
     
     subplot(2,2,2),cla
     title('saturation')
-    plotCellData(G,state.s(:,1),'EdgeColor','none');
+    plotCellData(model.G,state.s(:,1),'EdgeColor','none');
     colorbar('horiz'), caxis([0 1])
     
     % plot as VE
     subplot(2,2,3),cla
-    plot(xc,state.pressure/barsa); set(gca,'YLim',[100 200]);
+    plot(xc,convertTo(state.pressure,barsa)); set(gca,'YLim',[100 200]);
 
     subplot(2,2,4),cla,hold on
     patch(xc([1 1:end end]), [zt(end)-10; zt; zt(end)-10],.7*[1 1 1]);
