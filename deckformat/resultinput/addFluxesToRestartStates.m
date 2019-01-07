523a1353e4103cd82abc1cf86ef6cb95fbeb874c
Xavier Raynaud <xavier.raynaud@sintef.no>
2019-01-07 15:51:02 +0100

    addFluxesToRestartStates is now compatible with new property treatment

--- deckformat/resultinput/addFluxesToRestartStates.m
+++ deckformat/resultinput/addFluxesToRestartStates.m
@@ -1,47 +1,69 @@
-function states = addFluxesToRestartStates(states, G, T, fluid, deck)
-gravity reset on
-% get model with empty rock for rel-perm computations
-model = selectModelFromDeck(G, [], fluid, deck);
-model.fluid = fluid;
-% setup only the operators we need
-model.operators = setupOperatorsTPFA(G, [], 'trans', sparse(numel(T),1), 'porv', sparse(G.cells.num,1));
-gdz = model.getGravityGradient();
+function states = addFluxesToRestartStates(states, G, T, rock, fluid, deck)
+% The rock structure is needed when the scaling of relative permeability is
+% used.
+    
+    gravity reset on
+    
+    % Set up model for property evaluations
+    model = selectModelFromDeck(G, rock, fluid, deck);
+    % Set up automatically the correct property functions by using validateModel
+    model = model.validateModel(); 
+    props = model.FlowPropertyFunctions;
+    
+    % Set up only the operators we need
+    model.operators = setupOperatorsTPFA(G, [], 'trans', sparse(numel(T),1), ...
+                                            'porv', sparse(G.cells.num,1));
+    op  = model.operators;
+    gdz = model.getGravityGradient();
 
-if ~iscell(states)
-    states = {states};
-end
-for k =1:numel(states)
-    % relperm
-    if model.water && model.oil  && model.gas
-        [kr{1}, kr{2}, kr{3}] = model.evaluateRelPerm({states{k}.s(:,1), states{k}.s(:,2), states{k}.s(:,3)});
-    else
-        [kr{1}, kr{2}] = model.evaluateRelPerm({states{k}.s(:,1), states{k}.s(:,2)});
+
+    if ~iscell(states)
+        states = {states};
     end
-    % fluxes
-    v = cell(1,3);
-    phNum = 0;
-    if model.water
-        phNum = phNum +1;
-        v{phNum} = getFluxAndPropsWater_BO(model, states{k}.pressure, states{k}.s(:,phNum), kr{phNum}, T, gdz);
-    end
-    if model.oil
-        phNum = phNum +1;
-        if model.disgas
-            v{phNum} = getFluxAndPropsOil_BO(model, states{k}.pressure, states{k}.s(:,phNum), kr{phNum}, T, gdz, ...
-                                         states{k}.rs,  states{k}.s(:,phNum+1)>0);
-        else
-            v{phNum} = getFluxAndPropsOil_BO(model, states{k}.pressure, states{k}.s(:,phNum), kr{phNum}, T, gdz);
+    for k =1:numel(states)
+
+        state = states{k};
+        state = model.validateState(state);
+        [fp, fpname] = props.getPropertyContainer();
+        state.(fpname) = fp;
+        
+        pc  = props.getProperty(model, state, 'CapillaryPressure');  
+        rho = props.getProperty(model, state, 'Density');
+        mob = props.getProperty(model, state, 'Mobility');
+        
+        p = model.getProp(state, 'pressure');
+        
+        p_phase = getPhasePressures(p, pc);
+        
+        getflux = @(phnum) computeFluxPhase(phnum, rho, p_phase, mob, gdz, T, op);
+
+        % fluxes
+        v = cell(1,3);
+        phNum = 0;
+        if model.water
+            phNum = phNum + 1;
+            v{phNum} = getflux(phNum);
         end
-    end
-    if model.gas
-        phNum = phNum +1;
-        if model.vapoil
-            v{phNum} = getFluxAndPropsGas_BO(model, states{k}.pressure, states{k}.s(:,phNum), kr{phNum}, T, gdz, ...
-                                         states{k}.rv,  states{k}.s(:,phNum-1)>0);
-        else
-            v{phNum} = getFluxAndPropsGas_BO(model, states{k}.pressure, states{k}.s(:,phNum), kr{phNum}, T, gdz);
+    
+        if model.oil
+            phNum = phNum + 1;
+            v{phNum} = getflux(phNum);
         end
+        
+        if model.gas
+            phNum = phNum + 1;
+            v{phNum} = getflux(phNum);
+        end
+        
+        states{k} = model.storeFluxes(states{k}, v{1}, v{2}, v{3});
     end
-    states{k} = model.storeFluxes(states{k}, v{1}, v{2}, v{3});
 end
+
+function v = computeFluxPhase(phNum, rho, p_phase, mob, gdz, T, op)
+
+    rhof  = op.faceAvg(rho{phNum});
+    pot   = op.Grad(p_phase{phNum}) - rhof.*gdz;
+    upc   = double(pot)<=0;
+    dflux = -T.*pot;
+    v     = dflux.*op.faceUpstr(upc, mob{phNum});
 end
