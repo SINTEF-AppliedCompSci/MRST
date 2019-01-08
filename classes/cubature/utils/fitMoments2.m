d0374cdc1d553ab46c92a36f37a35b1e87f6b2d8
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-01-08 19:36:36 +0100

    Changed to standrad non-negative least-squares solver in moment-fitting

--- classes/cubature/utils/fitMoments2.m
+++ classes/cubature/utils/fitMoments2.m
@@ -7,11 +7,13 @@ function [points,weights,nPts] = fitMoments2(x, basis, moments, varargin)
     nDof = basis.nDof;
     n = size(x,1);
     
-    opts = optimoptions('lsqlin');
-    opts.ConstraintTolerance = 1e-10;
-    opts.OptimalityTolerance = 1e-6;
-    opts.MaxIterations       = 100;
-    opts.Display             = 'off';
+%     opts = optimoptions('lsqlin');
+%     opts.ConstraintTolerance = 1e-12;
+%     opts.OptimalityTolerance = 1e-6;
+%     opts.MaxIterations       = 100;
+%     opts.Display             = 'off';
+    opts = optimset('lsqnonneg');
+    opts = optimset('TolFun', 1e-12);
     
     if ~opt.equal
         nElements = numel(moments{1});
@@ -30,13 +32,15 @@ function [points,weights,nPts] = fitMoments2(x, basis, moments, varargin)
         tic;
         
         M = cellfun(@(m)m(cNo), moments);
+        vol = M(1);
+%         M = M./vol;
         reduced = true;
         x = x0;
         n = n0;
         k = n0;
         w = zeros(n,1);
         
-        while k > 0 && reduced 
+        while k > nDof && reduced 
         
          % Matrix of basis functions evalauted at current quadrature points
             P = reshape(cell2mat(cellfun(@(p) p(x), psi, 'unif', false)), n, nDof)';
@@ -53,9 +57,16 @@ function [points,weights,nPts] = fitMoments2(x, basis, moments, varargin)
                 P = reshape(cell2mat(cellfun(@(p) p(x), psi, 'unif', false)), n, nDof)';
 
                 I = eye(n); o = zeros(n,1); r = Inf(n,1);
-                [w, ~, ~, flag] = lsqlin(I, o, I, r, P  , M, [], [], [], opts);
-%                 [w, ~, ~, flag] = lsqlin(I, o, I, r, P  , moments, o, r, [], opts);
-
+%                 [w, ~, ~, flag] = lsqlin(I, o, I, r, P  , M, [], [], [], opts);
+%                 [w, ~, ~, flag] = lsqlin(I, o, I, r, P  , M, o, r, [], opts);
+                [w, ~, residual, flag] = lsqnonneg(P, M);
+%                 tol = 1e-15;
+                tol = 1e-12;
+                flag = flag && all(abs(residual)<tol);
+%                 PP = P*P';
+%                 w = P'*(PP\M);
+%                 tol = 0.01;
+%                 flag = all(isfinite(w)) && rank(P) == nDof && abs(rcond(PP)) > tol;
                 if flag > 0
                     k       = k-1;
                     reduced = true;
@@ -71,7 +82,7 @@ function [points,weights,nPts] = fitMoments2(x, basis, moments, varargin)
         
         nPts(cNo)    = n;
         points{cNo}  = x;
-        weights{cNo} = w;
+        weights{cNo} = w;%*vol;
         
         time = toc;
         fprintf('Compressed from %d to %d points in %f second\n', n0, n, time);
