bfce41b2a0ec3b8b1ff5019c72fe665f64d609cf
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-01-10 18:43:01 +0100

    Added options to unstructuredContour

--- utils/unstructuredContour.m
+++ utils/unstructuredContour.m
@@ -1,14 +1,17 @@
-function unstructuredContour(G, val, varargin)
+function unstructuredContour(G, val, nl, varargin)
 
+    if nargin == 2
+        nl = 10;
+    end
     fun = scatteredInterpolant(G.cells.centroids(:,1), G.cells.centroids(:,2), val, 'linear', 'none');
     xMax = max(G.nodes.coords);
     xMin = min(G.nodes.coords);
     
     n = 100;
-    [x, y] = ndgrid(linspace(xMin(1), xMax(1), n), linspace(xMin(1), xMax(1), n));
+    [x, y] = ndgrid(linspace(xMin(1), xMax(1), n), linspace(xMin(2), xMax(2), n));
     val = fun(x, y);
     
-    contour(x, y, val, varargin{:});
+    contour(x, y, val, nl, varargin{:});
     
 end
     
