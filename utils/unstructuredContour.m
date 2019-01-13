705495d36cb52ecd73b9e8b7b84af05cb45ff82b
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-01-13 19:20:19 +0100

    Improved unstructuredContour plotting fn

--- utils/unstructuredContour.m
+++ utils/unstructuredContour.m
@@ -4,9 +4,11 @@ function unstructuredContour(G, val, nl, varargin)
         nl = 10;
     end
     fun = scatteredInterpolant(G.cells.centroids(:,1), G.cells.centroids(:,2), val, 'linear', 'none');
-    xMax = max(G.nodes.coords);
-    xMin = min(G.nodes.coords);
-    
+    xMax = max(G.cells.centroids);
+    xMin = min(G.cells.centroids);
+%     xMax = max(G.nodes.coords);
+%     xMin = min(G.nodes.coords);
+%     
     n = 100;
     [x, y] = ndgrid(linspace(xMin(1), xMax(1), n), linspace(xMin(2), xMax(2), n));
     val = fun(x, y);
