2f5194ea1faf902eeea6404f539367bee67ea994
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-01-13 19:17:39 +0100

    Improved qfs-co2 example and plotting

--- examples/rsc-2019/qfs-co2/plot_qfsCO2.m
+++ examples/rsc-2019/qfs-co2/plot_qfsCO2.m
@@ -1,22 +1,28 @@
 %% Load results
 
-[ws, states, reports] = getMultiplePackedSimulatorOutputs(problems, 'readFromDisk', false);
+[ws, states, reports] = getMultiplePackedSimulatorOutputs(problems, 'readFromDisk', true);
 
 %% Get iterations
 
 rIx = 3;
-iterations = cell(3,1);
-for mNo = 3
-    n   = reports{mNo}.numelData;
-    rep = cell(n,1);
-    for sNo = 1:n
-        rep{sNo} = reports{mNo}{sNo};
+iterations = cell(4,1);
+for mNo = 1:4
+%     n   = reports{mNo}.numelData;
+    rep = reports{mNo};
+%     rep = cell(n,1);
+%     for sNo = 1:n
+%         rep{sNo} = reports{mNo}{sNo};
+%     end
+    if mNo == 3
+        iterations{mNo} = getReorderingTransportIterations(rep);
+    elseif mNo > 1
+        iterations{mNo} = cellfun(@(rep) rep.StepReports{1}.NonlinearReport{1}.TransportSolver.Iterations, rep);
     end
-    iterations{mNo} = getReorderingTransportIterations(rep);
-    reports{mNo} = rep;
+    
+%     reports{mNo} = rep;
 end
 
-%%
+%% Common
 
 G = model.G;
 W = problems{1}.SimulatorSetup.schedule.control(1).W;
@@ -43,57 +49,118 @@ end
 hpos = [0.1300 0.1146 0.7750 0.0727];
 cpos = [0.1300 0.07 0.7750 0.03];
 
-%% Plot compositions and 
+gray = [1,1,1]*0.5;
+
+%% Plot co2 compositions for adaptive
 
 realTime = cumsum(schedule.step.val)/day;
 
 close all
 
 name = cellfun(@(p) p.Name, problems, 'unif', false);
-st  = states{rIx};
+st  = states{4};
 timeSteps = [10, 40, 60, 80, 108];
+stref = states{2};
+stc   = states{5};
 
-its = iterations{3};
-itMax = max(cellfun(@max, its));
+% its = iterations{2};
+% itMax = max(cellfun(@max, its));
+G  = computeCellDimensions2(model.G);
+GC = generateCoarseGrid(G, coarsemodel.transportModel.G.partition);
+GC = coarsenGeometry(GC);
+GC = coarsenCellDimensions(GC);
 
 
 for tNo = timeSteps
     
-    it = its{tNo};
     c  = it > 0;
     
     figure('position', pos)
-%     unstructuredContour(model.G, st{tNo}.x(:,1), 20, 'linew', 2);
-    plotCellData(model.G, st{tNo}.x(:,1), 'edgec', 'none');
+    hold on
+    unstructuredContour(model.G, st{tNo}.x(:,1), 5, 'linew', 2);
+    plotGrid(st{tNo}.G, 'facec', 'none', 'edgec', gray);
     colormap(copper);
     axis equal tight
-    hold on
     pw(G, W)
     ax = gca;
     [ax.XTickLabel, ax.YTickLabel] = deal({});
     box on
-    savepng(['qfs-co2-x-', num2str(tNo)]);
+    drawnow();pause(0.5)
+    savepng(['qfs-co2-x-', num2str(tNo), '-adapt']);
     
     figure('position', pos)
-    plotCellData(model.G, it(c), c, 'edgec', 'none');
-    colormap(jet);
-    axis equal tight
     hold on
+    unstructuredContour(model.G, stref{tNo}.x(:,1), 5, 'linew', 2);
+    colormap(copper);
+    axis equal tight
     pw(G, W)
-    caxis([1,itMax]);
     ax = gca;
     [ax.XTickLabel, ax.YTickLabel] = deal({});
     box on
-    savepng(['qfs-co2-its-', num2str(tNo)]);
+    drawnow();pause(0.5)
+    savepng(['qfs-co2-x-', num2str(tNo), '-ref']);
+    
+    figure('position', pos)
+    hold on
+    unstructuredContour(GC, stc{tNo}.x(:,1), 5, 'linew', 2);
+    colormap(copper);
+    axis equal tight
+    pw(G, W)
+    ax = gca;
+    [ax.XTickLabel, ax.YTickLabel] = deal({});
+    box on
+    drawnow();pause(0.5)
+    savepng(['qfs-co2-x-', num2str(tNo), '-coarse']);
+    
     
 end
 
 disp(realTime(timeSteps));
 
+%% Iteration plot
+
+%% Well curves
+
+close all
+
+comp = cell(6,1);
+for mNo = 1:6
+    cmp = cellfun(@(ws) ws(2).components, ws{mNo}, 'unif', false);
+    comp{mNo} = abs(vertcat(cmp{:}));
+end
+
+lt = {'-', '-', '-.', '--', ':', '-o'};
+names = cellfun(@(p) p.Description, problems, 'unif', false);
+
+dtt = cumsum(schedule.step.val);
+clr = copper(3);
+for cNo = 1:size(comp{1},2)
+    figure();
+    hold on
+    for mNo = 2:6
+        cmp = comp{mNo};
+        plot(dtt(3:size(cmp,1)), cmp(3:end,cNo), lt{mNo});
+    end
+    hold off
+    legend(names(2:6));
+end
+
 %%
 
 close all
-plotWellSols(ws(1:2))
+plotWellSols(ws(2:6))
 
 %%
+
+% figure('position', pos)
+%     plotCellData(model.G, it(c), c, 'edgec', 'none');
+%     colormap(jet);
+%     axis equal tight
+%     hold on
+%     pw(G, W)
+%     caxis([1,itMax]);
+%     ax = gca;
+%     [ax.XTickLabel, ax.YTickLabel] = deal({});
+%     box on
+%     savepng(['qfs-co2-its-', num2str(tNo)]);
 
--- examples/rsc-2019/qfs-co2/qfsCO2.m
+++ examples/rsc-2019/qfs-co2/qfsCO2.m
@@ -4,6 +4,8 @@ mrstModule add spe10 vem vemmech dg ad-core ad-props ad-blackoil ...
 
 mrstVerbose on
 
+gravity reset off
+
 %% Common stuff
 
 name = 'qfs_co2_small';
@@ -11,74 +13,125 @@ baseName = 'qfs-CO2';
 dataDir  = fullfile('/media/strene/806AB4786AB46C92/mrst-dg/rsc-2019', baseName);
 [state0, model, schedule] = getReorderingCase(name);
 
-pack = @(model, name, nls, desc) ...
+pack = @(state0, model, name, desc) ...
     packSimulationProblem(state0, model, schedule, baseName, ...
                           'Name'           , name          , ...
-                          'NonLinearSolver', nls           , ...
                           'Directory'      , dataDir       , ...
                           'Description'    , desc          );
 
 
 %% Fully-implicit
 
-nls = NonLinearSolver();
-nls.LinearSolver = selectLinearSolverAD(model);
 model.AutoDiffBackend = DiagonalAutoDiffBackend();
 
-fim = pack(model, 'fim', nls, 'Fully-implicit');
+fim = pack(state0, model, 'fim', 'Fully-implicit');
 
 %% Sequential-implicit
 
 modelseq = getSequential_local(model);
-nls = NonLinearSolver();
-seq = pack(modelseq, 'seq', nls, 'Sequential');
+modelseq.transportNonLinearSolver.LinearSolver = BackslashSolverAD();
+seq = pack(state0, modelseq, 'seq', 'Sequential');
 
 %% Sequential-reordering
 
-nls = NonLinearSolver();
+modelreorder = getSequential_local(model);
+modelreorder.transportNonLinearSolver.LinearSolver = BackslashSolverAD();
+modelreorder.pressureModel.extraStateOutput = true;
+modelreorder.transportModel = ReorderingModel(modelreorder.transportModel);
+modelreorder.transportModel.parent.extraStateOutput = true;
+
+modelreorder.transportModel.chunkSize = 10;
+modelreorder.transportModel.buffer = 0;
+
+reorder = pack(state0, modelreorder, 'reorder', 'Reordering');
+
+%% Sequential-reordering strict tolerance
 
 modelreorder = getSequential_local(model);
 modelreorder.pressureModel.extraStateOutput = true;
 modelreorder.transportModel = ReorderingModel(modelreorder.transportModel);
 modelreorder.transportModel.parent.extraStateOutput = true;
 
-modelreorder.transportModel.chunkSize = 1;
+modelreorder.transportModel.chunkSize = 10;
 modelreorder.transportModel.buffer = 0;
 modelreorder.transportNonLinearSolver.LinearSolver = BackslashSolverAD();
+modelreorder.transportModel.parent.nonlinearTolerance = 1e-5;
 
-reorder = pack(modelreorder, 'reorder', nls, 'Reordering');
+reorderStrict = pack(state0, modelreorder, 'reorder-strict', 'Reordering Strict');
 
-%%
+%% Adaptive
 
-% Cartesian coarse grid
+nls = NonLinearSolver();
+
+modeladapt = getSequential_local(model);
+modeladapt.pressureModel.extraStateOutput = true;
+
+p = partitionCartGrid(model.G.cartDims, [15,15,1]);
+G = model.G;
 G = computeCellDimensions2(G);
-G_cart = cartGrid([100, 100]);
-p_cart = partitionUI(G_cart, [20, 20]);
-p_cart = sampleFromBox(G, reshape(p_cart, G_cart.cartDims));
-GC = generateCoarseGrid(G, p_cart);
-GC = coarsenGeometry(GC);
-GC = coarsenCellDimensions(GC);
+GC = generateCoarseGrid(G, p);
+m = getRefinementMappings(GC, GC, model.G, [1, GC.cells.num]);
 
-mappings = getRefinementMappings(GC, GC, G, [1, GC.cells.num]);
-GC        = generateCoarseGrid(GF, mappings.newPartition);
-GC        = coarsenGeometry(GC);
-GC.cells.refined = mappings.refined;
+GC = generateCoarseGrid(model.G, m.newPartition);
+GC.cells.refined = m.refined;
 
-% [G, map1] = refineGrid(GC, GC, GF, [1, GC.cells.num]');
-CG = coarsenCellDimensions(GC);
+coarsemodel = upscaleModelTPFA(modeladapt.transportModel, m.newPartition);
+state0C = upscaleState(coarsemodel, modeladapt.transportModel, state0);
+modeladapt = AdaptiveSequentialPressureTransportModel(modeladapt.pressureModel, modeladapt.transportModel, GC);
+state0 = ensureDensitiesPresentCompositional(model, state0);
+state0C.rho = repmat(state0.rho(1,:), GC.cells.num,1);
+% state0C.bfactor = state0C.rho./[fluid.rhoOS, fluid.rhoGS];
+% state0C.x = repmat(state0.x(1,:), GC.cells.num, 1);
+% state0C.y = repmat(state0.y(1,:), GC.cells.num, 1);
+% state0C.L = repmat(state0.L(1,:), GC.cells.num, 1);
+% state0C.K = repmat(state0.K(1,:), GC.cells.num, 1);
+% state0C.Z_V = repmat(state0.Z_V(1,:), GC.cells.num, 1);
+% state0C.Z_L = repmat(state0.Z_L(1,:), GC.cells.num, 1);
 
-GC.cells.ghost = false(GC.cells.num,1);
-G.cells.ghost = false(G.cells.num,1);
+state0C.components = repmat(state0C.components(1,:), GC.cells.num, 1);
+state0.transportState = state0C;
+state0.transportState.G = GC;
+state0.transportState.pv = coarsemodel.operators.pv;
+state0.transportModel = modeladapt.transportModel;
+modeladapt.plotProgress = true;
+modeladapt.refineTol = 1e-2;
+modeladapt.coarsenTol = 0.5*1e-2;
 
-modeladapt = AdaptiveSequentialPressureTransportModel(modelseq.pressureModel, modelseq.transportModel, GC);
+adapt = pack(state0, modeladapt, 'adapt', 'Adaptive');
 
 %%
 
-problems = {fim, seq, reorder};
+coarsemodel = upscaleModelTPFA(model, m.newPartition);
+
+fluid = model.fluid;
+state0C.bfactor = state0C.rho./[fluid.rhoOS, fluid.rhoGS];
+state0C.x = repmat(state0.x(1,:), GC.cells.num, 1);
+state0C.y = repmat(state0.y(1,:), GC.cells.num, 1);
+state0C.L = repmat(state0.L(1,:), GC.cells.num, 1);
+state0C.K = repmat(state0.K(1,:), GC.cells.num, 1);
+state0C.Z_V = repmat(state0.Z_V(1,:), GC.cells.num, 1);
+state0C.Z_L = repmat(state0.Z_L(1,:), GC.cells.num, 1);
+
+coarsemodel = getSequential_local(coarsemodel);
+
+W = schedule.control(1).W;
+WC = W;
+d = pdist2(coarsemodel.transportModel.G.cells.centroids, model.G.cells.centroids([W.cells],:));
+[~, c] = min(d);
+for wNo = 1:numel(W)
+    WC(wNo).cells = c(wNo);
+end
+
+coarse = pack(state0C, coarsemodel, 'coarse', 'Coarse');
+coarse.SimulatorSetup.schedule.control(1).W = WC;
+
+%%
+
+problems = {fim, seq, reorder, adapt, coarse, reorderStrict};
 
 %% Simulate problems
 
-runIx = 3;
+runIx = 2:4;
 for pNo = runIx
     [ok, status] = simulatePackedProblem(problems{pNo});
 end
@@ -87,3 +140,11 @@ end
 
 setup = problems{3}.SimulatorSetup;
 [ws, st, rep] = simulateScheduleAD(setup.state0, setup.model, setup.schedule);
+
+
+
+
+
+
+
+
