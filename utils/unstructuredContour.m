236b5661be04f4f1f69640d97b097461d50c2332
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-01-16 18:14:21 +0100

    Added option to use either node or cell coords as min/max in unstructuredContour

--- utils/unstructuredContour.m
+++ utils/unstructuredContour.m
@@ -1,19 +1,24 @@
 function unstructuredContour(G, val, nl, varargin)
 
+    opt = struct('useNodeMax', false);
+    [opt, extra] = merge_options(opt, varargin{:});
+
     if nargin == 2
         nl = 10;
     end
     fun = scatteredInterpolant(G.cells.centroids(:,1), G.cells.centroids(:,2), val, 'linear', 'none');
-    xMax = max(G.cells.centroids);
-    xMin = min(G.cells.centroids);
-%     xMax = max(G.nodes.coords);
-%     xMin = min(G.nodes.coords);
-%     
+    if opt.useNodeMax
+        xMax = max(G.nodes.coords);
+        xMin = min(G.nodes.coords); 
+    else
+        xMax = max(G.cells.centroids);
+        xMin = min(G.cells.centroids);
+    end
     n = 100;
     [x, y] = ndgrid(linspace(xMin(1), xMax(1), n), linspace(xMin(2), xMax(2), n));
     val = fun(x, y);
     
-    contour(x, y, val, nl, varargin{:});
+    contour(x, y, val, nl, extra{:});
     
 end
     
