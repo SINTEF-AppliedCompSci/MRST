22c34f4b81f43b16667f6e571c538541f1a8f0a8
Olav Møyner <olav.moyner@gmail.com>
2019-01-21 20:22:22 +0100

    value: Extend with support for additional fields

--- ad-core/utils/value.m
+++ ad-core/utils/value.m
@@ -1,3 +1,18 @@
 function xv = value(x)
-    xv = x;
+    if isnumeric(x) || islogical(x)
+        xv = x;
+    elseif iscell(x)
+        % Cell arrays are converted to matrices
+        x = cellfun(@value, x, 'UniformOutput', false);
+        xv = [x{:}];
+    elseif isstruct(x)
+        fn = fieldnames(x);
+        for i = 1:numel(fn)
+            f = fn{i};
+            x.(f) = value(x.(f));
+        end
+        xv = x;
+    else
+        xv = x;
+    end
 end
