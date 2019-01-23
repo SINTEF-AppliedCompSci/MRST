7e7367601cb56562426bbb558e359f98e4e59b01
Olav Møyner <olav.moyner@sintef.no>
2019-01-23 11:35:34 +0100

    value: Several updates to handle complex structs and cell arrays

--- ad-core/utils/value.m
+++ ad-core/utils/value.m
@@ -2,14 +2,21 @@ function xv = value(x)
     if isnumeric(x) || islogical(x)
         xv = x;
     elseif iscell(x)
-        % Cell arrays are converted to matrices
-        x = cellfun(@value, x, 'UniformOutput', false);
-        xv = [x{:}];
+        sz = size(x);
+        if sz(1) == 1 && sz(2) > 1
+            % Cell arrays are converted to matrices
+            x = cellfun(@value, x, 'UniformOutput', false);
+            xv = [x{:}];
+        else
+            xv = x;
+        end
     elseif isstruct(x)
         fn = fieldnames(x);
         for i = 1:numel(fn)
             f = fn{i};
-            x.(f) = value(x.(f));
+            for j = 1:numel(x)
+                x(j).(f) = value(x.(f));
+            end
         end
         xv = x;
     else
