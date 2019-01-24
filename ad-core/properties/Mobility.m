09c7ef0a41ceb249112edbcc59bb8ea19b47f43b
Olav Møyner <olav.moyner@gmail.com>
2019-01-24 10:23:11 +0100

    Mobility: Implement tranMultR

--- ad-core/properties/Mobility.m
+++ ad-core/properties/Mobility.m
@@ -5,7 +5,13 @@ classdef Mobility < GridProperty
     methods
         function mob = evaluateOnDomain(prop, model, state)
             [mu, kr] = model.getProps(state, 'Viscosity', 'RelativePermeability');
-            mob = cellfun(@(x, y) x./y, kr, mu, 'unif', false);
+            mob = cellfun(@(x, y) x./y, kr, mu, 'UniformOutput', false);
+            if isfield(model.fluid, 'tranMultR')
+                % Pressure dependent mobility multiplier 
+                p = model.getProp(state, 'pressure');
+                mult = model.fluid.tranMultR(p);
+                mob = cellfun(@(x) x.*mult, mob, 'UniformOutput', false);
+            end
         end
     end
 end
