7223a566ec9442012cf63c4cd79ea964efac4603
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2019-02-04 22:23:25 +0100

    Correct previous check-in: Section 3.3 does not have exercises. Section 3.4 has. Added more material

--- book/exercises/Section3_4.m
+++ book/exercises/Section3_4.m
@@ -1,4 +1,4 @@
-%% Exercise 3.3.1
+%% Exercise 3.4.1
 % Revisit Exercise 3.1.1, but use lower grid resolution 
 dx = .08;
 x = 1-0.5*cos((-1:dx:1)*pi);
@@ -29,9 +29,40 @@ subplot(1,2,2)
 plotCellData(G,G.cells.volumes,'EdgeAlpha',.2); view(-20,25); axis equal tight
 caxis(cx);
 
-%% Exercise 3.3.2
+%% Exercise 3.4.2
 % Simple check to see that the extended triangleGrid function from Exercise
-% 3.2.2 gives reasonable cell and face centroids
+% 3.2.2 gives reasonable cell and face centroids. First in 2D
+load trimesh2d;
+G = computeGeometry(myTriangleGrid([xfe 0*xfe yfe], trife));
+g = computeGeometry(triangleGrid([xfe yfe],trife));
+
+clf
+subplot(2,2,1), 
+plot(G.cells.volumes, g.cells.volumes,'o');
+v = [G.cells.volumes; g.cells.volumes];
+hold on
+plot([min(v) max(v)], [min(v) max(v)],'r-');
+hold off
+axis tight
+
+subplot(2,2,3), plot(G.faces.areas, g.faces.areas, 'o');
+a = [G.faces.areas; g.faces.areas];
+hold on
+plot([min(a) max(a)], [min(a) max(a)],'r-');
+hold off, axis tight
+
+subplot(2,2,[2 4]),
+plotGrid(G); view(3);
+hold on,
+c = G.cells.centroids;
+plot3(c(:,1),c(:,2),c(:,3),'.r');
+f = G.faces.centroids;
+plot3(f(:,1),f(:,2),f(:,3),'.b');
+hold off
+view(0,0); axis([0 100 0 1 0 100]);
+
+%%
+% Then we do a similar test in 3D
 load trimesh3d;
 G = myTriangleGrid([x y z], tri);
 G = computeGeometry(G);
@@ -44,7 +75,7 @@ f = G.faces.centroids;
 plot3(f(:,1),f(:,2),f(:,3),'.b');
 hold off
 
-%% Exercise 3.3.3
+%% Exercise 3.4.3
 % Create grid with invalid nodes
 G = cartGrid([10 10]);
 G.nodes.coords([10 15 20],1)=NaN;
@@ -58,6 +89,7 @@ cn = cellNodes(G);
 nflag = ~any(isnan(G.nodes.coords),2);
 cflag = accumarray(cn(:,1), nflag(cn(:,3))) < accumarray(cn(:,1),1);
 g = extractSubgrid(G,~cflag);
+% Alternative: g = removeCells(G,cflag);
 subplot(1,2,2)
 plotGrid(g);
 title(['# cells: ' num2str(g.cells.num)]);
