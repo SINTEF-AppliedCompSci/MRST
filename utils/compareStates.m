36441d4951d9e30c1394864361d0ba390d3a3c3a
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-02-07 14:51:17 +0100

    Added util func to compare all fileds of two states

---
+++ utils/compareStates.m
@@ -0,0 +1,14 @@
+function stateDiff = compareStates(state1, state2)
+
+    stateDiff = state1;
+    flds = fieldnames(state1);
+    for fNo = 1:numel(flds)
+        v = flds{fNo};
+        if isfield(state2, v)    && ...
+           isnumeric(state2.(v)) && ...
+           all(size(state2.(v)) == size(state1.(v)))
+            stateDiff.(v) = abs(state1.(v) - state2.(v));
+        end
+    end
+       
+end
