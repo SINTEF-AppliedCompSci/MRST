a3b8a4feedaace6bd64bfa46c8cf1e16ac2ea5a4
Olav Møyner <olav.moyner@gmail.com>
2019-02-13 08:09:24 +0100

    AD: Revive the DivergenceTerm class (not currently used)

--- ad-core/backends/diagonal/operators/discreteDivergence.m
+++ ad-core/backends/diagonal/operators/discreteDivergence.m
@@ -30,43 +30,16 @@ function jac = divJac(jac, N, nc, nf, sortIx, C, prelim, useMex)
             end
         end
     elseif jac.isZero
-        if useDivTerm
-            jac = DivergenceTerm([], [], [], nc, prod(jac.dim));
-        else
+%         jac = DiagonalJacobian();
+%         if useDivTerm
+%             jac = DivergenceTerm([], [], [], nc, prod(jac.dim));
+%         else
             jac = sparse([], [], [], nc, prod(jac.dim));
-        end
+%         end
         return
     else
         if useDivTerm
-            % Manual version
-            nD = jac.dim(2);
-
-            if isempty(jac.subset)
-                N = jac.map;
-            else
-                N = jac.map(jac.subset, :);
-            end
-            J = repmat(reshape(N, [], 1), 2, nD);
-
-%             ix = sortIx.J_sorted_index;
-
-            if nD > 1
-                J = bsxfun(@plus, J, (0:nD-1)*jac.dim(1));
-            end
-            I = repmat(sortIx.I_base, 1, nD);
-%             I = [N(:, 1); N(:, 1); N(:, 2); N(:, 2)];
-%             I = repmat(repmat(reshape(N, [], 1), 2, 1), 1, nD);
-            V = [jac.diagonal; -jac.diagonal];
-
-%             V = V(ix, :);
-%             J = J(ix, :);
-
-%             act = V~=0;
-% 
-%             I = I(act);
-%             J = J(act);
-%             V = V(act);
-            jac = DivergenceTerm(I, J, V, nc, prod(jac.dim));
+            jac = DivergenceTerm(jac, N, sortIx.C, prelim, useMex);
         else
             % Sparse version
             if useMex && (isempty(jac.parentSubset) || all(jac.parentSubset == (1:jac.dim(1))'))
