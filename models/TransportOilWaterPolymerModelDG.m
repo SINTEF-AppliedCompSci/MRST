e84f4055c09cca69d41659b139b259fbde500059
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2019-03-05 06:55:55 +0100

    Added updateState to properly treat polymer

--- models/TransportOilWaterPolymerModelDG.m
+++ models/TransportOilWaterPolymerModelDG.m
@@ -53,23 +53,77 @@ classdef TransportOilWaterPolymerModelDG < TransportOilWaterModelDG
         end
         % ----------------------------------------------------------------%
         function [state, report] = updateState(model, state, problem, dx, drivingForces)
-
+            
+            vars = problem.primaryVariables;
+            removed = false(size(vars));
             if model.polymer
                 % Store the polymer from previous iteration temporarily to
                 % use in convergence criteria
                 cdof_prev = model.getProp(state, 'polymer');
             end
+            
+            state = model.updateStateFromIncrement(state, dx, problem, 'pressure', model.dpMaxRel, model.dpMaxAbs);
+            state = model.capProperty(state, 'pressure', model.minimumPressure, model.maximumPressure);
+            [vars, ix] = model.stripVars(vars, 'pressure');
+            removed(~removed) = removed(~removed) | ix;
+            sWdof = model.getProp(state, 'sWdof');
+            dsW   = model.getIncrement(dx, problem, 'sWdof');
+            dsO   = -dsW;
+                        
+            dc = model.getIncrement(dx, problem, 'cDof');
+            state = model.updateStateFromIncrement(state, dc, problem, 'cDof');
 
-            [state, report] = updateState@TransportOilWaterModelDG(model, ...
-               state, problem, dx, drivingForces);
+            nPh = nnz(model.getActivePhases());
+            ds = zeros(numel(sWdof), nPh);
+            phIndices = model.getPhaseIndices();
+            if model.water
+                ds(:, phIndices(1)) = dsW;
+            end
+            if model.oil
+                ds(:, phIndices(2)) = dsO;
+            end
+            state = model.updateStateFromIncrement(state, ds, problem, 'sDof', inf, model.dsMaxAbs);
+            bad = any((state.s > 1 + model.disc.meanTolerance) ...
+                    | (state.s < 0 - model.disc.meanTolerance), 2);    
+                    
+            if any(bad)
+                state.s(bad, :) = min(state.s(bad, :), 1);
+                state.s(bad, :) = max(state.s(bad, :), 0);
+                state.s(bad, :) = bsxfun(@rdivide, state.s(bad, :), ...
+                                              sum(state.s(bad, :), 2));
+                state = dgLimiter2(model.disc, state, bad);
+            end
+%             sT = sum(state.s, 2);
+%             state.sdof = bsxfun(@rdivide, state.sdof, rldecode(sT, state.nDof,1));
+%             state.s    = bsxfun(@rdivide, state.s, sT);
+                
+            if model.disc.limitAfterNewtonStep
+                [sMin, sMax] = model.disc.getMinMaxSaturation(state);
+                bad = sMin < 0 - model.disc.outTolerance | ...
+                      sMax > 1 + model.disc.outTolerance;
+                if any(bad)
+                   state = dgLimiter2(model.disc, state, bad);
+                end
+            end
+            
+            %  We have explicitly dealt with rs/rv properties, remove from list
+            %  meant for autoupdate.
+            [vars, ix] = model.stripVars(vars, {'sWdof', 'sOdof', 'cDof'});
+            removed(~removed) = removed(~removed) | ix;
 
+            % We may have solved for a bunch of variables already if we had
+            % disgas / vapoil enabled, so we remove these from the
+            % increment and the linearized problem before passing them onto
+            % the generic reservoir update function.
+            problem.primaryVariables = vars;
+            dx(removed) = [];
+
+            % Parent class handles almost everything for us
+            [state, report] = updateState@TransportOilWaterModelDG(model, state, problem, dx, drivingForces);
+            
             if model.polymer
                 
                 state = model.capProperty(state, 'c', 0, model.fluid.cmax);
-%                 % Limit polymer concentration to [0, fluid.cmax]
-%                 c = model.getProp(state, 'polymer');
-%                 c = min(c, model.fluid.cmax);
-%                 state = model.setProp(state, 'polymer', max(c, 0) );
                 state.c_prev = cdof_prev;
 
                 % Shear Thinning Report
@@ -82,6 +136,7 @@ classdef TransportOilWaterPolymerModelDG < TransportOilWaterModelDG
                     state = rmfield(state, 'ShearThinningReport');
                 end
             end
+            
         end
         
         function [state, report] = updateAfterConvergence(model, state0, state, dt, drivingForces)
