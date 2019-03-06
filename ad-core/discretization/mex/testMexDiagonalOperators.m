9fef8d48fd4a89a878fbb2ead42a6a796dfe4672
Olav Møyner <olav.moyner@sintef.no>
2019-03-06 13:13:05 +0100

    testMexDiagonalOperators: Add output timings if requested.

--- ad-core/discretization/mex/testMexDiagonalOperators.m
+++ ad-core/discretization/mex/testMexDiagonalOperators.m
@@ -1,9 +1,11 @@
-function testMexDiagonalOperators(model, varargin)
+function results = testMexDiagonalOperators(model, varargin)
     N = model.operators.N;
     
 
     opt = struct('block_size', 5, 'iterations', 5, 'nc', max(N(:)));
     opt = merge_options(opt, varargin{:});
+    
+    results = opt;
     
     % Make test props
     nf = size(N, 1);
@@ -18,20 +20,20 @@ function testMexDiagonalOperators(model, varargin)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     f_avg = @(useMex) faceAverage(N, cell_value, useMex);
     [f_mex, f_matlab] = genFunctions(f_avg);
-    testFunction(f_mex, f_matlab, 'Face average', opt);
+    [favg, results] = testFunction(f_mex, f_matlab, 'faceavg', 'Face average', opt, results);
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % Test single-point upwinding %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     upw = @(useMex) singlePointUpwind(flag, N, cell_value, useMex);
     [f_mex, f_matlab] = genFunctions(upw);
-    face_value = testFunction(f_mex, f_matlab, 'Single-point upwind', opt);
+    [face_value, results] = testFunction(f_mex, f_matlab, 'upwind', 'Single-point upwind', opt, results);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %      Test gradient          %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     grad = @(useMex) twoPointGradient(N, cell_value, [], useMex);
     [f_mex, f_matlab] = genFunctions(grad);
-    testFunction(f_mex, f_matlab, 'Two-point gradient', opt);
+    [gradient, results] = testFunction(f_mex, f_matlab, 'gradient', 'Two-point gradient', opt, results);
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %      Prep divergence        %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -52,17 +54,17 @@ function testMexDiagonalOperators(model, varargin)
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     div = @(useMex) discreteDivergence([], N, face_value, nc, nf, sortIx, gradMat, prelim, useMex);
     [f_mex, f_matlab] = genFunctions(div);
-    testFunction(f_mex, f_matlab, 'Discrete divergence', opt);
+    [divergence, results] = testFunction(f_mex, f_matlab, 'div', 'Discrete divergence', opt, results);
     
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %      Test DivAcc            %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     f_mex = @() discreteDivergence(cell_value, N, face_value, nc, nf, sortIx, gradMat, prelim, true);
     f_matlab = @() cell_value + discreteDivergence([], N, face_value, nc, nf, sortIx, gradMat, prelim, false);
-    testFunction(f_mex, f_matlab, 'Accumulation + divergence', opt);
+    [divacc, results] = testFunction(f_mex, f_matlab, 'accdiv', 'Accumulation + divergence', opt, results);
 end
 
-function out = testFunction(fn_mex, fn_mat, name, opt)
+function [out, results] = testFunction(fn_mex, fn_mat, shortname, name, opt, results)
     its = opt.iterations;
     timer = tic();
     for i = 1:its
@@ -74,9 +76,8 @@ function out = testFunction(fn_mex, fn_mat, name, opt)
     for i = 1:its
         mex = fn_mex();
     end
-    out = matlab;
     t_c = toc(timer);
-    
+    out = matlab;
     v_error = norm(value(matlab) - value(mex))./norm(value(matlab));
     if ~issparse(mex.jac{1})
         mex.jac{1} = mex.jac{1}.sparse();
@@ -89,6 +90,10 @@ function out = testFunction(fn_mex, fn_mat, name, opt)
     fprintf('* %s\n* Matlab: %1.2gs, MEX: %1.2gs (%1.2f speedup)\n', name, t_m/its, t_c/its, t_m/t_c);
     fprintf('Value error %1.2g, Jacobian error %1.2g\n', v_error, j_error)
     fprintf('********************************************************\n');
+    
+    if nargout > 1
+        results.(shortname) = struct('t_matlab', t_m/its, 't_mex', t_c/its, 'description', name);
+    end
 end
 
 function [f_mex, f_matlab] = genFunctions(fn)
