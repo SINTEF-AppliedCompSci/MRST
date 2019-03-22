59fa2c509ad3005afe71e11f6b0cf6915673cd85
Xavier Raynaud <xav.raynaud@gmail.com>
2019-03-22 17:54:37 +0100

    works on simple test case.

--- mpfa/computeMultiPointTrans2.m
+++ mpfa/computeMultiPointTrans2.m
@@ -1,4 +1,4 @@
-function T = computeMultiPointTrans2(G, rock, varargin)
+function [T, div] = computeMultiPointTrans2(G, rock, varargin)
 %Compute multi-point transmissibilities.
 %
 % SYNOPSIS:
@@ -163,9 +163,8 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
    % (done only for internal faces).
    faces = (1 : G.faces.num)';
    isintfaces = (G.faces.neighbors(faces, 1) ~= 0) & (G.faces.neighbors(faces, 2) ~= 0);
-   intfaces = faces(isintfaces);
-   tmptbl.faces = intfaces;
-   S = setupTableMapping(intfacenodetbl, tmptbl, 'faces');
+   intfacetbl.faces = faces(isintfaces);
+   S = setupTableMapping(intfacenodetbl, intfacetbl, 'faces');
    
    % Mapping from internal to internal+external face-nodes faces
    op = setupTableMapping(intfacenodetbl, facenodetbl, 'faces', 'nodes');
@@ -173,6 +172,25 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
    % Assembly of transmissibility
    T = S*op'*iB*op*div';
 
+   % Assembly of standard finite volume divergence operator, mapping from
+   % face to cell. Here, restricted only to internal faces.
+   cellfacetbl = tbls.cellfacetbl;
+   fno = cellfacetbl.faces;
+   intfno = (G.faces.neighbors(fno, 1) ~= 0) & (G.faces.neighbors(fno, 2) ~= 0);
+   intcellfacetbl.faces = cellfacetbl.faces(intfno);
+   intcellfacetbl.cells = cellfacetbl.cells(intfno);
+   intcellfacetbl.num = numel(intcellfacetbl.faces);
+   ifno = intcellfacetbl.faces; %alias
+   icno = intcellfacetbl.cells; %alias
+   sgn = 2*(icno == G.faces.neighbors(ifno, 1)) - 1;
+   div = sparse(intcellfacetbl.cells, ...
+                (1 : intcellfacetbl.num)', ...
+                sgn, ...
+                G.cells.num, ...
+                intcellfacetbl.num);   
+   op = setupTableMapping(intfacetbl, intcellfacetbl, 'faces');
+   div = div*op;
+   
 end
 
 %--------------------------------------------------------------------------
@@ -376,64 +394,3 @@ function M = node_ip(a, v, N, R, K)
     
 end
 
-function bi = blockInverter(opt)
-   if ~ischar(opt.invertBlocks)
-      dispif(opt.verbose, ...
-            ['Unsupported option value of type ''%s'' in ', ...
-             'option ''invertBlocks''. Reset to default ' , ...
-             '(''matlab'')\n'], class(opt.invertBlocks));
-      opt.invertBlocks = 'matlab';
-   end
-
-   switch lower(opt.invertBlocks)
-      case {'matlab', 'm', 'builtin'}
-         bi = @invertDiagonalBlocks;
-      case {'mex', 'c', 'accelerated'}
-         bi = @invertDiagonalBlocksMex;
-      otherwise
-         dispif(opt.verbose, ...
-               ['Unsupported value ''%s'' in option ', ...
-                '''invertBlocks''.\nMust be one of ''matlab'' or ', ...
-                '''mex''.\nReset to default (''matlab'').'], ...
-                opt.invertBlocks);
-
-         bi = @invertDiagonalBlocks;
-   end
-end
-
-%--------------------------------------------------------------------------
-
-% Matlab code calling mex function invertSmallMatrices.c
-function iA = invertDiagonalBlocksMex(A, sz)
-   sz     = int32(sz);
-   blocks = matrixBlocksFromSparse(A, sz);
-   iA     = blockDiagMatrix(invv(blocks, sz), sz);
-end
-
-%--------------------------------------------------------------------------
-
-% Pure Matlab code using inv
-function iA = invertDiagonalBlocks(A, sz)
-   V = zeros([sum(sz .^ 2), 1]);
-   [p1, p2] = deal(0);
-
-   for b = 1 : numel(sz)
-      n  = sz(b);
-      n2 = n * n;
-      i  = p1 + (1 : n);
-
-      V(p2 + (1 : n2)) = inv(full(A(i, i)));
-
-      p1 = p1 + n;
-      p2 = p2 + n2;
-   end
-
-   iA = blockDiagMatrix(V, sz);
-end
-
-%--------------------------------------------------------------------------
-
-function A = blockDiagMatrix(V, sz)
-   [I, J] = blockDiagIndex(sz);
-   A      = sparse(I, J, V);
-end
