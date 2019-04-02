516a65a5463740dfca1dd7ce811cabb964b5761d
Xavier Raynaud <xav.raynaud@gmail.com>
2019-04-02 10:10:39 +0200

    added duplicate functionality in setupTableMapping

--- mpfa/utils/setupTableMapping.m
+++ mpfa/utils/setupTableMapping.m
@@ -1,7 +1,32 @@
-function [map, tbl, map1, map2] = setupTableMapping(tbl1, tbl2, varargin)
+function [map, tbl, map1, map2] = setupTableMapping(tbl1, tbl2, crossfields, varargin)
     
-    fds = varargin;
+    opt = struct('duplicate', []);
+    opt = merge_options(opt, varargin{:});
+    
+    if ~isempty(opt.duplicate)
+        dups = opt.duplicate;
+        ndups = numel(dups);
+        for i = 1 : ndups
+            dup = dups{i};
+            fd = dup{1};
+            fd1 = dup{2}{1};            
+            fd2 = dup{2}{2};
+            tbl1.(fd1) = tbl1.(fd);
+            tbl1 = rmfield(tbl1, fd);
+            tbl2.(fd2) = tbl2.(fd);
+            tbl2 = rmfield(tbl2, fd);
+        end
+    end
+    
+    fds = crossfields; %alias
     nfds = numel(fds);
+    
+    if isempty(crossfields)
+        % handle case when crossfield is empty
+        n1 = tbl1.num;
+        n2 = tbl2.num;
+        map = ones(n2, n1);
+    end
     
     for ifield = 1 : nfds
         fieldname = fds{ifield};
@@ -45,12 +70,13 @@ function [map, tbl, map1, map2] = setupTableMapping(tbl1, tbl2, varargin)
             tbl.(fieldname) = tbl2.(fieldname)(ind2);
         end
         
-        tbl.num = numel(tbl.(fds{1}));
+        % get dimension of table (may not be so robust, to be check...).
+        tbl.num = numel(tbl.(ofields1{1}));
     end
     
     if nargout > 2
-        map1 = setupMapTable(tbl1, tbl, fds);
-        map2 = setupMapTable(tbl2, tbl, fds);
+        map1 = setupTableMapping(tbl1, tbl, fds);
+        map2 = setupTableMapping(tbl2, tbl, fds);
     end
     
 end
