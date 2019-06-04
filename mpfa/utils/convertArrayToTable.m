e5c306bcf3d28bbe2449c61a235b7334c2e39b38
Xavier Raynaud <xav.raynaud@gmail.com>
2019-06-04 10:36:34 +0200

    convertArrayToTable can now discard rows

--- mpfa/utils/convertArrayToTable.m
+++ mpfa/utils/convertArrayToTable.m
@@ -2,8 +2,6 @@ function tbl = convertArrayToTable(A, fds)
     sz = size(A);
     nfds = numel(fds);
     
-    assert(sz(2) == nfds, 'sizes are not matching');
-    
     for ifield = 1 : nfds
         tbl.(fds{ifield}) = A(:, ifield);
     end
