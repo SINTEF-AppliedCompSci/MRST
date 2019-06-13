144124998768d7590e4e5e1084ff39cc5b1c7292
Stein Krogstad <stein.krogstad@sintef.no>
2019-06-13 13:19:28 +0200

    Check that 'sign'-field exists before refering to it

--- diagnostics/utils/computeTOFandTracer.m
+++ diagnostics/utils/computeTOFandTracer.m
@@ -207,7 +207,7 @@ function flux = getTotalWellSolFlux(wellSol)
     for i = 1:nw
         ws = wellSol(i);
         f = sum(ws.flux);
-        if isempty(f) || f == 0 && ~isempty(ws.sign)
+        if isempty(f) || f == 0 && ( isfield(ws, 'sign') && ~isempty(ws.sign) )
             % If we have a sign for well and there is no flux defined, set
             % it to a small value of right sign instead.
             f = sqrt(eps)*ws.sign;
