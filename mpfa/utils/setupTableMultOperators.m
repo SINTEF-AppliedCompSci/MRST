6006360db2933024edac272ff81cb27038896e67
Xavier Raynaud <xav.raynaud@gmail.com>
2019-06-14 20:37:43 +0200

    implementation of table multiplication function

--- mpfa/utils/setupTableMultOperators.m
+++ mpfa/utils/setupTableMultOperators.m
@@ -1,12 +1,19 @@
-function [restbl, map1, map2, reducemap] = setupTableMultOperators(tbl1, tbl2, ...
-                                                                         fds1, ...
-                                                                         fds2, ...
-                                                                         fdscross)
-    afds1 = fieldnames(tbl1);
-    afds2 = fieldnames(tbl2);
+function [prodAB, restbl] = setupTableMultOperators(Acell, Bcell, fds)
+    
+    A    = Acell{1};
+    tblA = Acell{2};
+    B    = Bcell{1};
+    tblB = Bcell{2};
+    
+    fds1 = fds{1};
+    fds2 = fds{2};
+    fdscross = fds{3};
+    
+    afds1 = fieldnames(tblA);
+    afds2 = fieldnames(tblB);
     
     %% sanity checks
-    % first check: we should have fds1 (fds2) equal to ofds1 (ofds2).
+    % first check: we should have fds1 (fds2) included ofds1 (ofds2).
     ofds1 = afds1;
     fdsToRemove = {fdscross{:}, 'ind', 'num'};
     for ifield = 1 : numel(fdsToRemove)
@@ -19,21 +26,23 @@ function [restbl, map1, map2, reducemap] = setupTableMultOperators(tbl1, tbl2, .
         ofds2 = ofds2(~strcmp(ofds2, fdsToRemove{ifield}));
     end
     
-    isdiff = any(setdiff(fds1, ofds1)) | any(setdiff(fds2, ofds2));
+    isdiff = (numel(setdiff(fds1, ofds1)) > 0) | (numel(setdiff(fds2, ofds2)) > 0);
     assert(~isdiff, 'mismatch in table matrix multipication');
     % second check: we do not support for the moment when fds1 and fds2 have
     % common field names (in this case they should be given different names,
     % which we do not do automatically).
     
-    assert(nnz(intersect(ofds1, ofds2)) == 0, ['repeated names in fields are not ' ...
+    assert(numel(intersect(ofds1, ofds2)) == 0, ['repeated names in fields are not ' ...
                         'supported']);
     
     % end of sanity checks
-    [~, prodmattbl] = setupTableMapping(tbl1, tbl2, fdscross);
-    map1 = setupTableMapping(tbl1, prodmattbl, fdscross);
-    map2 = setupTableMapping(tbl2, prodmattbl, fdscross);
+    
+    [~, prodmattbl] = setupTableMapping(tblA, tblB, fdscross);
+    map1 = setupTableMapping(tblA, prodmattbl, {ofds1{:}, fdscross{:}});
+    map2 = setupTableMapping(tblB, prodmattbl, {ofds2{:}, fdscross{:}});
     resfds = {fds1{:}, fds2{:}};
     restbl = projTable(prodmattbl, resfds);
     reducemap = setupTableMapping(prodmattbl, restbl, resfds);
     
+    prodAB = reducemap*((map1*A).*(map2*B));
 end
