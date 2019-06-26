0776288f57e905fdb27bb172204155c30eefb119
Xavier Raynaud <xav.raynaud@gmail.com>
2019-06-26 15:05:51 +0200

    removed unused function

--- mpfa/utils/getcolarray.m
+++
@@ -1,7 +0,0 @@
-function vect = getcolarray(u, celltbl, cellcoltbl, cellfds, coltbl)
-    ind = (1 : celltbl.num)';
-    map = setupTableMapping(celltbl, cellcoltbl, cellfds);
-    ind = map*ind;
-    vect = sparse(ind, cellcoltbl.coldim, u, celltbl.num, coltbl.num);
-    vect = full(vect);
-end
