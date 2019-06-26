75ae46e60c6b38ee162d64c6e1d18fffc5718b13
Xavier Raynaud <xav.raynaud@gmail.com>
2019-06-26 15:35:18 +0200

    table utiliy functions are moved to mrst-core

--- utils/addLocInd.m
+++
@@ -1,3 +0,0 @@
-function tbl = addLocInd(tbl, locindname)
-    tbl.(locindname) = (1 : tbl.num)';
-end
--- utils/projTable.m
+++
@@ -1,7 +0,0 @@
-function projtbl = projTable(tbl, fds)
-    a = convertTableToArray(tbl, fds);
-    nfds = numel(fds);
-    a = a(:, (1 : nfds));
-    a = unique(a, 'rows');
-    projtbl = convertArrayToTable(a, fds);
-end
