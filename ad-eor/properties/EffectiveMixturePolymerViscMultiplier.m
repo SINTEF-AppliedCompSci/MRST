2c35da37c2e042cc311a450695b31429ea7d45e4
Xin Sun <B16020075@s.upc.edu.cn>
2019-08-06 13:27:13 +0200

    rewrote the SurfactantPolymerFlowPropertyFunctions and relevant funtions

---
+++ ad-eor/properties/EffectiveMixturePolymerViscMultiplier.m
@@ -0,0 +1,22 @@
+classdef EffectiveMixturePolymerViscMultiplier < GridProperty
+    properties
+    end
+
+    methods
+        function gp = EffectiveMixturePolymerViscMultiplier(varargin)
+            gp@GridProperty(varargin{:});
+            gp = gp.dependsOn({'polymer'}, 'state'); % check mechanism
+        end
+
+        function muWeffMult = evaluateOnDomain(prop, model, state)
+            cp   = model.getProp(state, 'polymer');            
+            fluid = model.fluid;
+            mixpar = fluid.mixPar;
+            cpbar   = cp/fluid.cmax;
+            a = fluid.muWMult(fluid.cmax).^(1-mixpar);
+            b = 1./(1 - cpbar + cpbar./a);
+            % The viscosity multiplier only results from the polymer mixing.
+            muWeffMult = b.*fluid.muWMult(cp).^mixpar;            
+        end
+    end
+end
---
+++ ad-eor/properties/PolymerViscMultiplier.m
@@ -0,0 +1,20 @@
+classdef PolymerViscMultiplier < GridProperty
+    properties
+    end
+
+    methods
+        function gp = PolymerViscMultiplier(model, varargin)
+            gp@GridProperty(model, varargin{:});
+            gp = gp.dependsOn({'PolymerAdsorption'});
+            gp = gp.dependsOn({'EffectiveMixturePolymerViscMultiplier'});
+        end
+
+        function muWMultp = evaluateOnDomain(prop, model, state)
+            ads = model.getProp(state, 'PolymerAdsorption');
+            muWeffMult = model.getProp(state, 'EffectiveMixturePolymerViscMultiplier');
+            fluid = model.fluid;
+            permRed = 1 + ((fluid.rrf - 1)./fluid.adsMax).*ads;
+            muWMultp  = muWeffMult.*permRed;
+        end
+    end
+end
