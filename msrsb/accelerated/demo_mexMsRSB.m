bbc4c6b9130615032400b5214169174e3ab6075f
Olav Møyner <olav.moyner@sintef.no>
2019-08-30 16:33:27 -0700

    Update demo_mexMsRSB.m
    Improve text

--- msrsb/accelerated/demo_mexMsRSB.m
+++ msrsb/accelerated/demo_mexMsRSB.m
@@ -1,3 +1,10 @@
+%% MsRSB basis function with mex acceleration
+% The MsRSB basis functions can be efficiently computed using a compiled
+% function written in C++. If a C++ compiler is available, this is
+% generally an order of magnitude faster than using the Matlab version.
+%
+% In this example, we demonstrate the syntax for generating basis functions
+% either directly from Matlab or via a flat file format.
 mrstModule add coarsegrid msrsb libgeometry incomp
 % Some different test cases
 testcase = 'simple';
@@ -26,7 +33,7 @@ switch lower(testcase)
         [G, ~, rock] = getSPE10setup(layers);
         p = partitionUI(G, [6, 11, ceil(G.cartDims(3)./5)]);
     otherwise
-        error();
+        error('Unknown test case %s', testcase);
 end
 
 if ~isfield(G.cells, 'centroids')
@@ -39,17 +46,25 @@ A = getIncomp1PhMatrix(G, T);
 CG = generateCoarseGrid(G, p);
 CG = coarsenGeometry(CG);
 CG = storeInteractionRegionCart(CG);
-
+%% Add extra data to coarse grid
+% The function setupMexInteractionMapping adds additional mappings to the
+% coarse grid which are required for the compiled interface. This can be
+% done once for a specific coarse-fine grid pair and reused throughout a
+% simulation.
 CG = setupMexInteractionMapping(CG);
 %% Generate basis functions via regular mex interface
 % Typical usage. We send in the coarsegrid and the mex layer passes values
 % onto the C++ code.
-I = cppMultiscaleBasis(CG, A, 'verbose', true, 'omega', 2/3, 'maxiter', 250, 'tolerance', 0.001);
-
+I = cppMultiscaleBasis(CG, A, 'verbose', true, 'omega', 2/3,...
+                              'maxiter', 250, 'tolerance', 0.001);
+%% Generate basis functions via general interface
+% This is also supported through the general interface for basis functions.
+basis = getMultiscaleBasis(CG, A, 'useMex', true, 'type', 'msrsb');
 %% Write a testcase to flatfiles on disk
 % We can also write basis functions to disk as a series of text files. This
 % is not efficient, but it is useful for setting up and running testcases
-% without Matlab.
+% without running a full Matlab session, or for generating basis functions
+% for some other purpose.
 fn = fullfile(mrstDataDirectory(), 'tmp', ['basis_', lower(testcase)]);
 if ~exist(fn, 'dir')
     cppMultiscaleBasis(CG, A, 'doSolve', false, 'writePath', fn);
