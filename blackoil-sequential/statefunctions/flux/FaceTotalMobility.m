c0dd5f67a41dd5373b2a0761d123eca0f9135d09
Olav Møyner <olav.moyner@sintef.no>
2019-09-01 15:51:26 -0700

    ad-core + blackoil-sequential: A few generalizations to support hybrid upwinding in new framework

--- blackoil-sequential/statefunctions/flux/FaceTotalMobility.m
+++ blackoil-sequential/statefunctions/flux/FaceTotalMobility.m
@@ -1,14 +1,19 @@
 classdef FaceTotalMobility < StateFunction
-    properties
+    properties (Access = private)
+        mobility_name = 'FaceMobility';
     end
     
     methods
-        function gp = FaceTotalMobility(varargin)
-            gp@StateFunction(varargin{:});
-            gp = gp.dependsOn('FaceMobility');
+        function gp = FaceTotalMobility(model, mobility_name)
+            if nargin < 2
+                mobility_name = 'FaceMobility';
+            end
+            gp@StateFunction(model);
+            gp = gp.dependsOn(mobility_name);
+            gp.mobility_name = mobility_name;
         end
         function mobT = evaluateOnDomain(prop, model, state)
-            mob = prop.getEvaluatedDependencies(state, 'FaceMobility');
+            mob = prop.getEvaluatedDependencies(state, prop.mobility_name);
             mobT = 0;
             for i = 1:numel(mob)
                 mobT = mobT + mob{i};
--- blackoil-sequential/statefunctions/flux/PhasePotentialUpwindFlag.m
+++ blackoil-sequential/statefunctions/flux/PhasePotentialUpwindFlag.m
@@ -1,6 +1,6 @@
 classdef PhasePotentialUpwindFlag < StateFunction
     properties
-
+        includeTotalVelocity = true;
     end
     
     methods
@@ -15,7 +15,12 @@ classdef PhasePotentialUpwindFlag < StateFunction
             [G, T, vT] = prop.getEvaluatedDependencies(state, ...
                             'PhaseInterfacePressureDifferences', 'Transmissibility', 'TotalFlux');
             mob = model.getProp(state, 'Mobility');
-            flag_array = multiphaseUpwindIndices(G, vT, T, mob, model.operators.faceUpstr);
+            if prop.includeTotalVelocity
+                v = vT;
+            else
+                v = zeros(size(value(vT)));
+            end
+            flag_array = multiphaseUpwindIndices(G, v, T, mob, model.operators.faceUpstr);
             nph = numel(mob);
             flags = cell(1, nph);
             for i = 1:nph
