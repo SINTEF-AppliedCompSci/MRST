e6dc03db8b70f6cb5cce5d50f257281c145e18c3
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-09-05 10:08:54 +0200

    Updated 1d example to use wells instead of BCs

--- examples/getDGTestCase.m
+++ examples/getDGTestCase.m
@@ -20,29 +20,41 @@ function setup = simple1d(args) %#ok
                                'mu'    , [1,1], ...
                                'n'     , [1,1]);
     
-    modelFV = deal(TransportOilWaterModel(G, rock, fluid));
-    modelDG = cell(numel(opt.degree), 1);
+    model  = GenericBlackOilModel(G, rock, fluid, 'gas', false);
+    pmodel = PressureModel(model);
+    tmodel = TransportModel(model);
+    tmodel.parentModel.useCNVConvergence = false;
     
+    modelFV = SequentialPressureTransportModel(pmodel, tmodel, 'parentModel', model);
+    
+    modelDG = cell(numel(opt.degree), 1);
     for dNo = 1:numel(opt.degree)
         disc         = DGDiscretization(modelFV, ...
                                    'degree', opt.degree(dNo), discArgs{:});
-        modelDG{dNo} = TransportOilWaterModelDG(G, rock, fluid, 'disc', disc);
+        tmodelDG     = TransportModelDG(model, 'disc', disc);
+        tmodelDG.parentModel.useCNVConvergence = false;
+        modelDG{dNo} = SequentialPressureTransportModel(pmodel, tmodelDG, 'parentModel', model);
     end
 
     time  = opt.n;
     dt    = opt.n/100;
     dtvec = rampupTimesteps(time, dt, 0);
-    bc    = [];
-    bc = fluxside(bc, G, 'left' ,  1, 'sat', [1,0]);
-    bc = fluxside(bc, G, 'right', -1, 'sat', [1,0]);
     
-    schedule = simpleSchedule(dtvec, 'bc', bc);
+    W = [];
+    W = addWell(W, G, rock, 1    , 'type', 'bhp', 'val', opt.n-1, 'compi', [1,0], 'WI', 9999);
+    W = addWell(W, G, rock, opt.n, 'type', 'bhp', 'val', 0, 'compi', [1,0], 'WI', 9999);
+%     bc    = [];
+%     bc = fluxside(bc, G, 'left' ,  1, 'sat', [1,0]);
+%     bc = fluxside(bc, G, 'right', -1, 'sat', [1,0]);
+    
+    schedule = simpleSchedule(dtvec, 'W', W);
 
     sW     = 0.0;
     state0 = initResSol(G, 100*barsa, [sW,1-sW]);
-    state0.flux = zeros(G.faces.num,1);
-    state0.flux(modelFV.operators.internalConn) = 1;
-    state0.flux(bc.face) = 1;
+%     state0.flux = zeros(G.faces.num,1);
+%     state0.flux(modelFV.parentModel.operators.internalConn) = 1;
+%     state0.flux(modelFV.transportModel.operators.internalConn) = 1;
+%     state0.flux(bc.face) = 1;
     
     setup = packSetup(state0, schedule, [], modelFV, {modelDG});
    
