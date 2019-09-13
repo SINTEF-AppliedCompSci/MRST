3e742a5deb8b2ff6b282d9ba660be85b9884d076
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-09-13 17:24:36 +0200

    FlowStatebuilderDG now works outside DG model

--- ad/FlowStateBuilderDG.m
+++ ad/FlowStateBuilderDG.m
@@ -2,17 +2,23 @@ classdef FlowStateBuilderDG < FlowStateBuilder
     
     methods
         function flowState = build(builder, fd, model, state, state0, dt)
+            
+            flowState = state.faceStateDG;
             propfn = model.getStateFunctionGroupings();
             for i = 1:numel(propfn)
                 p = propfn{i};
                 struct_name = p.getStateFunctionContainerName();
                 if isfield(state, struct_name)
-                    state = rmfield(state, struct_name);
+                    flowState = rmfield(flowState, struct_name);
                 end
             end
+            flowState = model.initStateFunctionContainers(flowState);
 %             state = model.validateState(state);
-            flowState = state;
             flowState.type = 'face';
+            if ~isfield(flowState, 'cells')
+                flowState.cells = (1:model.G.cells.num)';
+                flowState.faces = (1:model.G.faces.num)';
+            end
         end
     end
 end
