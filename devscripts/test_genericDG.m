09da3a0d339c5ddd5a74a63ec971d76334d1b9ea
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-09-17 08:30:48 +0200

    Using disc.inner with dG(0) now matches FV

--- devscripts/test_genericDG.m
+++ devscripts/test_genericDG.m
@@ -4,14 +4,24 @@ mrstVerbose on
 
 %%
 
-setup   = getDGTestCase('simple1d', 'n', 20);
+gravity reset off
+gravity([1,0]);
+setup   = getDGTestCase('simple1d', 'n', 3);
+
+setup.modelFV.transportModel.parentModel.fluid.transMult = @(p) 0*p + 1;
+setup.modelDG{2}.transportModel.parentModel.fluid.transMult = @(p) 0*p + 1;
 
 %%
 
+% setup.modelFV.transportModel.formulation = 'missingPhase';
 [wsFV, stFV, repFV] = simulateScheduleAD(setup.state0, setup.modelFV, setup.schedule);
 
 %%
 
-for dNo = 2:numel(setup.modelDG)
-    [wsDG, stDG, repDG] = simulateScheduleAD(setup.state0, setup.modelDG{dNo}, setup.schedule);
+for dNo = 1:numel(setup.modelDG)
+%     setup.modelDG{dNo}.transportModel.formulation = 'missingPhase';
+    [wsDG, stDG, repDG] = simulateScheduleAD(setup.state0,setup.modelDG{dNo}, setup.schedule);
 end
+%%
+
+plotWellSols({wsFV, wsDG})
