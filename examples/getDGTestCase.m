24a4356382a2ec49bea0368f1d3d348da4857de8
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-09-19 18:56:10 +0200

    Added 2d qfs example with water and oil

--- examples/getDGTestCase.m
+++ examples/getDGTestCase.m
@@ -52,7 +52,67 @@ function setup = simple1d(args) %#ok
 
     sW     = 0.0;
     state0 = initResSol(G, 1, [sW,1-sW]);
-    setup = packSetup(state0, schedule, [], modelFV, {modelDG});
+    setup = packSetup(state0, schedule, [], {modelFV}, {modelDG});
+   
+end
+
+function setup = qfs_wo_2d(args) %#ok
+    opt = struct('n', 50, 'nkr', 1, 'degree', [0,1,2]);
+    [opt, discArgs] = merge_options(opt, args{:});
+
+    G = computeGeometry(cartGrid([1,1]*opt.n, [500,500]*meter));
+    G = createAugmentedGrid(G);
+    G = computeCellDimensions(G);
+    
+    if 0
+        rng(2019)
+        perm = logNormLayers([opt.n, opt.n, 1])*10*milli*darcy;
+        poro = perm./max(perm)*0.8;
+    else
+        perm = 100*milli*darcy;
+        poro = 0.4;
+    end
+    rock  = makeRock(G, perm, poro);
+    fluid = initSimpleADIFluid('phases', 'WO'                       , ...
+                               'rho'   , [1000,800]*kilogram/meter^3, ...
+                               'mu'    , [0.5,1]*centi*poise        , ...
+                               'n'     , [2,2]                      );
+    
+    model  = GenericBlackOilModel(G, rock, fluid, 'gas', false);
+    pmodel = PressureModel(model);
+    tmodel = TransportModel(model);
+    tmodel.parentModel.useCNVConvergence = false;
+    tmodel.parentModel.nonlinearTolerance = 1e-3;
+    
+    modelFV = SequentialPressureTransportModel(pmodel, tmodel, 'parentModel', model);
+    
+    modelDG = cell(numel(opt.degree), 1);
+    for dNo = 1:numel(opt.degree)
+%         disc         = DGDiscretization(modelFV.transportModel, ...
+%                                    'degree', opt.degree(dNo), discArgs{:});
+        tmodelDG = TransportModelDG(model, 'formulation', 'missingPhase' , ...
+                                           'degree'     , opt.degree(dNo), ...
+                                           discArgs{:});
+        tmodelDG.parentModel.useCNVConvergence = false;
+        tmodelDG.parentModel.nonlinearTolerance = 1e-3;
+        tmodelDG.parentModel.OutputStateFunctions = {};
+        modelDG{dNo} = SequentialPressureTransportModel(pmodel, tmodelDG, 'parentModel', model);
+    end
+
+    time  = 2*year;
+    rate  = sum(poreVolume(G, rock))/time;
+    dt    = 20*day;
+    dtvec = rampupTimesteps(time, dt);
+    
+    W = [];
+    W = addWell(W, G, rock, 1          , 'type', 'rate', 'val', rate     , 'compi', [1,0]);
+    W = addWell(W, G, rock, G.cells.num, 'type', 'bhp' , 'val', 100*barsa, 'compi', [1,0]);
+    
+    schedule = simpleSchedule(dtvec, 'W', W);
+
+    sW     = 0.0;
+    state0 = initResSol(G, 100*barsa, [sW,1-sW]);
+    setup = packSetup(state0, schedule, {{model}}, {{modelFV}}, {modelDG});
    
 end
 
