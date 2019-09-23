03379f014a21586735609d1dc892d4de8d8f43e2
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-09-23 15:28:40 +0200

    Fixed bug in FixedTotalVelocityDG (forgot to multiply by cell volume)

--- ad/FixedTotalVelocityDG.m
+++ ad/FixedTotalVelocityDG.m
@@ -8,9 +8,7 @@ classdef FixedTotalVelocityDG < FixedTotalFluxDG
             gp@FixedTotalFluxDG(model, varargin{:});
         end
         function vT = evaluateOnDomain(prop, model, state)
-            vT = faceFlux2cellVelocity(model.G, sum(state.(prop.fluxfield),2));
-            vT = mat2cell(vT, model.G.cells.num, ones(1,model.G.griddim));
-            vT = SpatialVector(vT{:});
+            vT = model.disc.velocityInterp.faceFlux2cellVelocity(sum(state.(prop.fluxfield),2));
             vT = vT(state.cells,:);
         end
     end
