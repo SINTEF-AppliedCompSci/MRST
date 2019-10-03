9d365b9bdd40a3ee979cf10bc6a99b96323e1b4a
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-10-03 08:04:57 +0200

    Update runDGTest.m

--- devscripts/runDGTest.m
+++ devscripts/runDGTest.m
@@ -4,20 +4,15 @@ mrstVerbose on
 
 %%
 
-setup = getDGTestCase('simple1d', ...
-                      'n'                  , 20        , ...
-                      'nkr'                , 2         , ...
-                      'degree'             , [0,0; 1,0; 2,0; 3,0], ...
-                      'basis'              , 'legendre', ...
-                      'useUnstructCubature', false     ); %#ok
+setup = getDGTestCase('simple1d', 'n', 20, 'nkr', 2 ); %#ok
 
 %%
 
-setup = getDGTestCase('qfs_wo_2d', 'useUnstructCubature', true); %#ok
+setup = getDGTestCase('qfs_wo_2d', 'useMomentFitting', true); %#ok
 
 %%
 
-setup = getDGTestCase('qfs_wog_3d', 'useUnstructCubature', true, 'degree', [1,1,0]);
+setup = getDGTestCase('qfs_wog_3d', 'useMomentFitting', true, 'degree', [1,1,1]);
 
 %%
 
@@ -30,7 +25,7 @@ sim = @(model,inx) simulateScheduleAD(setup.state0, setup.(model){inx}, setup.sc
 
 %%
 
-ix = 4;
+ix = 1:2;
 for i = ix
     [wsDG{i}, stDG{i}, repDG{i}] = sim('modelDG', i);
 end
@@ -41,18 +36,46 @@ plotWellSols(vertcat({wsFV}, wsDG(ix)));
 
 %%
 
-ix = 1:2;
+coords = getPlotCoordinates(setup.modelFV{1}.G, 'n', 100);
+
+%%
 
 close all
+
 for t = 1:numel(stDG{ix(1)})
+    clf
     for i = 1:numel(ix)
         subplot(1, numel(ix), i)
         disc = setup.modelDG{ix(i)}.transportModel.disc;
-        plotSaturationDG(disc, stDG{ix(i)}{t}, 'edgecolor', 'k', 'edgealpha', 0.2);
-        view(3);
+        plotSaturationDG(disc, stDG{ix(i)}{t}, 'edgecolor', 'none', 'edgealpha', 0.2, 'coords', coords, 'phaseNo', 1);
+        axis tight
         zlim([0,1])
         pbaspect([1,1,0.5]);
         view([100,50]);
+        camlight
+        lighting gouraud
+    end
+    pause(0.1);
+end
+
+%%
+
+ix = 1;
+close all
+azel = [13,25];
+for t = 1:numel(stDG{ix(1)})
+    for i = 1:numel(ix)
+        clf;
+        view(azel);
+        camlight;
+        h = camlight;
+        h.Position = [-h.Position(1:2), h.Position(3)];
+        subplot(1, numel(ix), i)
+        disc = setup.modelDG{ix(i)}.transportModel.disc;
+        plotSaturationDG(disc, stDG{ix(i)}{t}, 'edgecolor', 'k', 'edgealpha', 0.2, 'coords', coords, 'phaseNo', 3);
+        ax = gca;
+        ax.ZDir = 'reverse';
+%         pbaspect([1,1,0.5]);
     end
     pause(0.1);
 end
