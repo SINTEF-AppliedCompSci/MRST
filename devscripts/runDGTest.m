10fba5eab641b7cfd30e17427699762b46132985
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-10-04 16:33:06 +0200

    Update runDGTest.m

--- devscripts/runDGTest.m
+++ devscripts/runDGTest.m
@@ -1,5 +1,5 @@
 mrstModule add dg ad-props ad-core ad-blackoil blackoil-sequential ...
-    mrst-gui matlab_bgl vem vemmech vista upr
+    mrst-gui matlab_bgl vem vemmech vista upr compositional
 mrstVerbose on
 
 %%
@@ -8,11 +8,19 @@ setup = getDGTestCase('simple1d', 'n', 20, 'nkr', 2 ); %#ok
 
 %%
 
-setup = getDGTestCase('qfs_wo_2d', 'useMomentFitting', true); %#ok
+setup = getDGTestCase('qfs_wo_2d', 'useMomentFitting', true, 'nkr', 1); %#ok
 
 %%
 
-setup = getDGTestCase('qfs_wog_3d', 'useMomentFitting', true, 'degree', [1,1,1]);
+setup = getDGTestCase('qfs_wog_3d', 'useMomentFitting', true, 'degree', [0,0,0; 1,1,0; 1,1,1]); %#ok
+
+%%
+
+setup = getDGTestCase('spe10_wo', 'useMomentFitting', true); %#ok
+
+%%
+
+setup = getDGTestCase('qfs_co2_2d', 'n', 3, 'useOverall', false);
 
 %%
 
@@ -25,22 +33,29 @@ sim = @(model,inx) simulateScheduleAD(setup.state0, setup.(model){inx}, setup.sc
 
 %%
 
-ix = 1:2;
+[wsFI, stFI, repFI] = sim('modelFI', 1);
+
+%%
+
+ix = 2;
 for i = ix
     [wsDG{i}, stDG{i}, repDG{i}] = sim('modelDG', i);
 end
 
 %%
 
-plotWellSols(vertcat({wsFV}, wsDG(ix)));
+plotWellSols(vertcat(wsDG(ix)));
 
 %%
 
-coords = getPlotCoordinates(setup.modelFV{1}.G, 'n', 100);
+coords = getPlotCoordinates(setup.modelFV{1}.G, 'n', 200);
 
 %%
 
 close all
+figure('Position', [0,0,1000,500])
+
+%%
 
 for t = 1:numel(stDG{ix(1)})
     clf
@@ -50,31 +65,38 @@ for t = 1:numel(stDG{ix(1)})
         plotSaturationDG(disc, stDG{ix(i)}{t}, 'edgecolor', 'none', 'edgealpha', 0.2, 'coords', coords, 'phaseNo', 1);
         axis tight
         zlim([0,1])
-        pbaspect([1,1,0.5]);
+        pbaspect([1,1,0.25]);
         view([100,50]);
         camlight
         lighting gouraud
     end
-    pause(0.1);
+    pause(0.2);
 end
 
 %%
 
-ix = 1;
+ix = 2;
 close all
-azel = [13,25];
+figure('Position', [0,0,1000,500]);
+azel = [64,-10];
+view(azel);
 for t = 1:numel(stDG{ix(1)})
     for i = 1:numel(ix)
-        clf;
-        view(azel);
-        camlight;
-        h = camlight;
-        h.Position = [-h.Position(1:2), h.Position(3)];
+        delete(h);
+%         h.Position = [-h.Position(1:2), h.Position(3)];
+%         h.Position = -h.Position;
         subplot(1, numel(ix), i)
         disc = setup.modelDG{ix(i)}.transportModel.disc;
-        plotSaturationDG(disc, stDG{ix(i)}{t}, 'edgecolor', 'k', 'edgealpha', 0.2, 'coords', coords, 'phaseNo', 3);
+        h = plotSaturationDG(disc, stDG{ix(i)}{t}, 'edgecolor', 'k', 'edgealpha', 0.2, 'coords', coords, 'phaseNo', 3);
         ax = gca;
+        plotWell(setup.modelFV{1}.G, setup.schedule.control(1).W);
+%         plotGrid(setup.modelFV{1}.G, 'facec', 'none', 'facealpha', 0.2);
         ax.ZDir = 'reverse';
+        box on
+        view(azel);
+        if t == 1
+            hl = camlight;
+        end
 %         pbaspect([1,1,0.5]);
     end
     pause(0.1);
