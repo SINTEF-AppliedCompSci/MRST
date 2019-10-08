b3f986f48c5e46dcaf7e6d48269e362d0fb4d0d6
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-10-08 16:27:01 +0200

    Update runDGTest.m

--- devscripts/runDGTest.m
+++ devscripts/runDGTest.m
@@ -1,14 +1,14 @@
 mrstModule add dg ad-props ad-core ad-blackoil blackoil-sequential ...
-    mrst-gui matlab_bgl vem vemmech vista upr compositional
+    mrst-gui matlab_bgl vem vemmech vista upr compositional spe10
 mrstVerbose on
 
 %%
 
-setup = getDGTestCase('simple1d', 'n', 20, 'nkr', 2 ); %#ok
+setup = getDGTestCase('simple1d', 'n', 20, 'nkr', 3); %#ok
 
 %%
 
-setup = getDGTestCase('qfs_wo_2d', 'useMomentFitting', true, 'nkr', 1); %#ok
+setup = getDGTestCase('qfs_wo_2d', 'useMomentFitting', true, 'nkr', 1, 'k', {[0,0], [0,0; 1,0; 0,1; 1,1]}, 'degree', 1); %#ok
 
 %%
 
@@ -16,7 +16,11 @@ setup = getDGTestCase('qfs_wog_3d', 'useMomentFitting', true, 'degree', [0,0,0; 
 
 %%
 
-setup = getDGTestCase('spe10_wo', 'useMomentFitting', true); %#ok
+setup = getDGTestCase('spe10_wo', 'useMomentFitting', true, 'I', 1:30, 'J', 1:110); %#ok
+
+%%
+
+setup = getDGTestCase('spe1', 'subset', false, 'useMomentFitting', true); %#ok
 
 %%
 
@@ -44,11 +48,11 @@ end
 
 %%
 
-plotWellSols(vertcat(wsDG(ix)));
+plotWellSols(vertcat(wsDG(ix)), setup.schedule.step.val);
 
 %%
 
-coords = getPlotCoordinates(setup.modelFV{1}.G, 'n', 200);
+coords = getPlotCoordinates(setup.modelFV{1}.G, 'n', 100);
 
 %%
 
@@ -80,13 +84,14 @@ close all
 figure('Position', [0,0,1000,500]);
 azel = [64,-10];
 view(azel);
+disc = setup.modelDG{ix(1)}.transportModel.disc;
+h = plotSaturationDG(disc, stDG{ix(1)}{1}, 'edgecolor', 'k', 'edgealpha', 0.2, 'coords', coords, 'phaseNo', 3);
 for t = 1:numel(stDG{ix(1)})
     for i = 1:numel(ix)
         delete(h);
 %         h.Position = [-h.Position(1:2), h.Position(3)];
 %         h.Position = -h.Position;
         subplot(1, numel(ix), i)
-        disc = setup.modelDG{ix(i)}.transportModel.disc;
         h = plotSaturationDG(disc, stDG{ix(i)}{t}, 'edgecolor', 'k', 'edgealpha', 0.2, 'coords', coords, 'phaseNo', 3);
         ax = gca;
         plotWell(setup.modelFV{1}.G, setup.schedule.control(1).W);
