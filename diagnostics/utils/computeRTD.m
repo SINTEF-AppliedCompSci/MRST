744d38fca9c8f7e5ff85cc1bf708caca1639a1c0
Stein Krogstad <stein.krogstad@sintef.no>
2019-10-10 11:22:10 +0200

    Include option to compute RTDs in reverse direction of flow field as this faciltates proper calculation of saturation along backward TOFs. Set reverse direction as default.

--- diagnostics/utils/computeRTD.m
+++ diagnostics/utils/computeRTD.m
@@ -54,9 +54,10 @@ opt = struct('injectorIx', [], ...
              'producerIx', [], ...
              'nsteps',     50, ...
              'nbase',       5, ...
-             'computeSaturation', true, ...
+             'computeSaturations', true, ...
              'wbHandle',   [], ...
-             'showWaitbar',  true);
+             'showWaitbar',  true, ...
+             'reverse',    true);
 
 opt = merge_options(opt, varargin{:});
 
@@ -64,19 +65,22 @@ opt = merge_options(opt, varargin{:});
 if isempty(pix), pix = (1:numel(D.prod))'; end
 if isempty(iix), iix = (1:numel(D.inj))'; end       
 
+if opt.nsteps < opt.nbase
+    opt.nsteps = opt.nbase;
+    fprintf('nsteps-option set equal to nbase-option for convenience ...');
+end
 
 % create output struct
 nreg = numel(pix).*numel(iix);
-dist = struct('pairIx',         nan(nreg, 2), ...
-              't',              nan(2*opt.nsteps+1, nreg), ...
-              'volumes',        nan(nreg, 1), ...
-              'allocations',    nan(nreg,1), ...
-              'values',         nan(2*opt.nsteps+1, nreg), ...
-              'injectorFlux',   nan(numel(iix),1), ...
+dist = struct('pairIx',         nan(nreg, 2),       ...
+              't',              [],                 ...
+              'volumes',        nan(nreg, 1),       ...
+              'allocations',    nan(nreg,1),        ...
+              'values',         [],                 ...
+              'injectorFlux',   nan(numel(iix),1),  ...
               'producerFlux',   nan(numel(pix),1)); 
           
-
-% 
+% get well rates from allocations
 for ik = 1:numel(iix)
     dist.injectorFlux(ik) = sum( sum(WP.inj(iix(ik)).alloc) );
 end
@@ -85,11 +89,11 @@ for pk = 1:numel(pix)
     dist.producerFlux(pk) = sum( sum(WP.prod(pix(pk)).alloc) );
 end
 
+% collect relevant data from WP
 ix = 0;          
 for ik = 1:numel(iix)
     for pk = 1:numel(pix)
         ix = ix +1;
-        % collect data from WP
         dist.pairIx(ix,:) = [iix(ik), pix(pk)];
         ixWP = WP.pairIx(:,1)==iix(ik) & WP.pairIx(:,2) == pix(pk);
         dist.volumes(ix)      = WP.vols(ixWP);
@@ -97,18 +101,17 @@ for ik = 1:numel(iix)
     end
 end
 
-
 % index into well-struct
 [inj, prod] = deal(D.inj(iix), D.prod(pix));
 
 % extract relevat subset
-sub = sum(D.itracer(:, iix), 2) > 1e-5;
+sub = sum(D.itracer(:, iix), 2) > 1e-6;
 
-% devide into two or more simulation periods
+% divide into two or more simulation periods
 % 1. 0  <= t <   t1 = min(vol/alloc)  , i.e., minimal interaction mean RT
-% 2. t1 <= t < nbase*t1
+% 2. t1 <= t < (nbase+1)*t1
 %     |
-% n. nbase^(n-2)*t1 <= t <=  maxtof
+% n. (nbase+1)^(n-2)*t1 <= t <  ~maxtof
 
 % only check allocations > total*10^-3
 checkIx = dist.allocations > 1e-3*sum(dist.allocations);
@@ -116,7 +119,7 @@ checkIx = dist.allocations > 1e-3*sum(dist.allocations);
 if ~checkIx
     return
 end
-
+% end of first period
 t1 = min(dist.volumes(checkIx)./dist.allocations(checkIx));
 % total pvi
 pvi = sum(dist.volumes)/sum(dist.allocations);
@@ -128,28 +131,50 @@ else
 end
 itof(~isfinite(itof)) = 0;
 maxTOF = max(max(itof));
-t2 = min(maxTOF, 100*pvi);
+% end time
+t_end = min(maxTOF, 100*pvi);
 
-% find number of simulation periods
-n = ceil(log(t2/t1)/log(opt.nbase));
-t = t2./(opt.nbase.^(n:-1:0));
-dt = diff([0 t], 1, 2)./opt.nsteps;
+% find number of simulation periods and period dt and nsteps
+nperiods    = ceil( log( t_end*(opt.nbase-1)/t1+1 )/(log(opt.nbase)) );
+dts         = (t1/opt.nsteps)*opt.nbase.^((0:(nperiods-1))');
+nsteps      = opt.nsteps*ones(nperiods,1);
+nsteps(end) = ceil( (t_end-sum(dts(1:end-1).*nsteps(1:end-1)))/dts(end) );
+nStepsTot   = sum(nsteps);
 
 % allocate 
-[dist.t, dist.values] = deal(nan((n+1)*opt.nsteps+1, nreg));
-
-
-%t  = [1*pvi, min(maxTOF, 100*pvi)];
-%dt = t/opt.nsteps;
+[dist.t, dist.values] = deal(nan(nStepsTot+1, nreg));
 
 % setup system
-[sysmat, qp_well, tr0] = setupSystemComponents(state, G, pv, W, sub, inj, prod);
+[sysmat, qp_well, tr0, pv] = setupSystemComponents(state, G, pv, W, sub, inj, prod, opt.reverse);
 
 tr = tr0;
-vals = cell(1, numel(iix));
-for k = 1:numel(vals)
-    vals{k} = zeros((n+1)*opt.nsteps+1, numel(pix));
+[ni, np] = deal(numel(iix), numel(pix));
+if opt.reverse
+    [ni, np] = deal(np, ni);
 end
+vals = cell(1, ni);
+for k = 1:numel(vals)
+    vals{k} = zeros(nStepsTot+1, np);
+end
+
+if opt.computeSaturations
+    % get region total and water volumes
+    if ~opt.reverse
+        pv_reg  = bsxfun(@times, D.ptracer(sub,:), pv);
+        pvw_reg = bsxfun(@times, pv_reg, state.s(sub,1));
+    else
+        pv_reg  = bsxfun(@times, D.itracer(sub,:), pv);
+        pvw_reg = bsxfun(@times, pv_reg, state.s(sub,1));
+    end
+    % initial saturation
+    sats = vals; 
+    for tn = 1:numel(sats)
+        curvals  = (tr0(:, tn)'*pvw_reg)./(tr0(:, tn)'*pv_reg);
+        curvals(~isfinite(curvals)) = 0;
+        sats{tn}(1,:) = curvals;
+    end
+end
+
 if opt.showWaitbar
     if isempty (opt.wbHandle)
         h = waitbar(0, 'Computing distribution(s) ...');
@@ -157,60 +182,79 @@ if opt.showWaitbar
         h = waitbar(0, opt.wbHandle, 'Computing distribution(s) ...');
     end
 end
+% Main loop ---------------------------------------------------------------
 cnt = 0;
-for ti = 1:numel(dt)
-    A = sysmat(dt(ti));
-    for k = 1:opt.nsteps
+for ti = 1:numel(dts)
+    A = sysmat(dts(ti));
+    for k = 1:nsteps(ti)
         cnt = cnt+1;
         tr = A\tr;
         for tn = 1:numel(vals)
             curvals  = -(tr(:, tn)'*qp_well);
             curvals(~isfinite(curvals)) = 0;
             vals{tn}(cnt+1,:) = curvals;
+            if opt.computeSaturations
+                curvals  = (tr(:, tn)'*pvw_reg)./(tr(:, tn)'*pv_reg);
+                curvals(~isfinite(curvals)) = 0;
+                sats{tn}(cnt+1,:) = curvals;
+            end
             if opt.showWaitbar
-                waitbar(cnt/((n+1)*opt.nsteps), h);
+                waitbar(cnt/((nperiods+1)*opt.nsteps), h);
             end
         end
     end
 end
 if isempty(opt.wbHandle)&&opt.showWaitbar, close(h); end
 
-t = arrayfun(@(x)repmat(x, [opt.nsteps, 1]), dt, 'UniformOutput', false);
-t = cumsum([0; vertcat(t{:})]);
+% rearrange distributions and possibly scale such that distribution integral
+% equals allocation/injectorflux
+dt = rldecode(dts,nsteps);
+t = cumsum([0; vertcat(dt)]);
 dist.t = repmat(t, [1, nreg]);
 ix = 0;          
 for ik = 1:numel(iix)
     for pk = 1:numel(pix)
         ix = ix +1;
-        dist.values(:, ix)    = vals{ik}(:, pk);
+        if ~opt.reverse
+            dist.values(:, ix)    = vals{ik}(:, pk);
+        else
+            fac = -dist.producerFlux(dist.pairIx(ix,2))/dist.injectorFlux(dist.pairIx(ix,1));
+            dist.values(:, ix)    = vals{pk}(:, ik)*fac;
+        end
     end
 end
-% saturation values
-if opt.computeSaturation
-    dist.sw_cum   = nan(size(dist.values));
+
+% rearrage saturation values and store interaction region water volumes
+if opt.computeSaturations
+    snm = 'sw';
+    if opt.reverse
+        snm = 'swr';
+    end
+    dist.(snm) = nan(nStepsTot+1, nreg);   
     dist.volumesW = nan(size(dist.volumes));
     ix = 0;
     for ik = 1:numel(iix)
         for pk = 1:numel(pix)
             ix = ix +1;
-            V_ip  = D.itracer(:,ik).*D.ptracer(:,pk).*pv;
-            Vw_ip = V_ip.*state.s(:, 1);
-            tof_i = D.itof(:, ik);
-            [tof_i, order] = sort(tof_i);
-            [tof_i, uix] = unique(tof_i);  
-            sw_cum = cumsum(Vw_ip(order(uix)))./cumsum(V_ip(order(uix)));
-            dist.sw_cum(:, ix) = interp1(tof_i, sw_cum, t, 'linear', 1);
-            %
+            V_ip  = D.itracer(sub,ik).*D.ptracer(sub,pk).*pv;
+            Vw_ip = V_ip.*state.s(sub, 1);
+            if ~opt.reverse
+                dist.(snm)(:, ix)    = sats{ik}(:, pk);
+            else
+                dist.(snm)(:, ix)    = sats{pk}(:, ik);
+            end
             dist.volumesW(ix) = sum(Vw_ip);
         end
     end
 end
-
-
 dist.creator = mfilename;
 end
-    
-function [sysmat, qp_well, tr0, pv] = setupSystemComponents(state, G, pv, W, sub, inj, prod)
+
+% ------------------------------------------------------------------------
+function [sysmat, qp_well, tr0, pv] = setupSystemComponents(state, G, pv, W, sub, inj, prod, reverse)
+if reverse
+    [inj, prod] = deal(prod, inj);
+end
 nc = G.cells.num;
 N  = G.faces.neighbors;
 if size(N, 1) ~= numel(state.flux) % flux on interior faces
@@ -230,13 +274,21 @@ remap = zeros(nc,1);
 remap(cix) = (1:nnz(cix))';
 N = remap(N);
 % swap to get upstream in neighbor-list
-v   = sum(state.flux(fix,:),2);
+if ~reverse
+    v =  sum(state.flux(fix,:),2);
+else
+    v = -sum(state.flux(fix,:),2);
+end
 neg = v < 0;
 N(neg,:) = N(neg, [2 1]);
 v(neg)   = -v(neg);
 
 % setup production sources
-q       = sum(vertcat(state.wellSol.flux), 2);
+if ~reverse
+    q =  sum(vertcat(state.wellSol.flux), 2);
+else
+    q = -sum(vertcat(state.wellSol.flux), 2);
+end
 wcells  = vertcat((W.cells));
 qp = sparse(wcells, 1, q.*(q<0), G.cells.num, 1);
 qp = qp(cix);
@@ -266,6 +318,9 @@ for k = 1:numel(prod)
     c = c(c>0);
     qp_well(c, k) = sum(state.wellSol(prod(k)).flux(c>0,:), 2);
 end% distrubute tracer in injector cells according to injector rates
+if reverse
+    [qi_well, qp_well] = deal(-qi_well, -qp_well);
+end
 w   = bsxfun(@rdivide, qi_well, sum(qi_well));
 tr0 = bsxfun(@rdivide, w, pv(cix));
 pv = pv(cix);
