41c6899a63713bdf044411ef646eccbbca20168d
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-10-14 16:53:47 +0200

    Comply with latest name change in Cubature class

--- models/FluxDiscretizationDG.m
+++ models/FluxDiscretizationDG.m
@@ -75,7 +75,7 @@ classdef FluxDiscretizationDG < FluxDiscretization
             
             switch type
                 case 'cells'
-                    [~, ~, cells] = model.discretization.getCubature((1:model.G.cells.num)', 'volume');
+                    [~, ~, cells] = model.discretization.getCubature((1:model.G.cells.num)', 'cell');
                 case 'faces'
                     [~, ~, ~, faces] = model.discretization.getCubature(find(model.operators.internalConn), 'face');
                     cells = [model.G.faces.neighbors(faces,1); model.G.faces.neighbors(faces,2)];
