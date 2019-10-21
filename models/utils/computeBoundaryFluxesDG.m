5df17d564c08b2d972ecf3ac2fb1670e70254d9e
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-10-21 17:55:01 +0200

    Update to work with new framework

--- models/utils/computeBoundaryFluxesDG.m
+++ models/utils/computeBoundaryFluxesDG.m
@@ -1,6 +1,6 @@
-function [q_bc, g] = computeBoundaryFluxesDG(model, state, bc, T_all, g, mob, b, rho, sdof, rdof)
+function [q, rhogKdz] = computeBoundaryFluxesDG(model, state, bc)
 
-    disc = model.disc;
+    disc = model.discretization;
     G    = model.G;
     % Compute total flux
     flux = sum(state.flux,2);
@@ -19,60 +19,103 @@ function [q_bc, g] = computeBoundaryFluxesDG(model, state, bc, T_all, g, mob, b,
     % Determine injection boundaries
     sgn   = 1 - 2*(G.faces.neighbors(faces, 1) == 0);
     isInj = vT(faces) > 0 & sgn < 0;
-    nPh = numel(sdof);
-    [s_bc, mob_bc] = deal(cell(nPh, 1));
-    sT_bc = 0;
-    % Evaluate saturations
-    for phNo = 1:nPh
-        % Saturation on inside of boundary
-        s_r = disc.evaluateDGVariable(x_bc, c_bc, state, sdof{phNo});
-        % Upstream saturation
-        s_bc{phNo}   = s_l(locFaceNo,phNo).*isInj(locFaceNo) + s_r.*(~isInj(locFaceNo));
-        % Total saturation
-        sT_bc = sT_bc + s_bc{phNo};
+    nPh = nnz(model.getActivePhases());
+    
+    hasOutsideMob = isfield(bc, 'mob');
+    hasOutsideRho = isfield(bc, 'rho');
+    
+    [state_r, state_l] = deal(state);
+    
+    state_l.s = bc.sat(locFaceNo,:);
+    isP = reshape(strcmpi(bc.type, 'pressure'), [], 1);
+    if any(isP)
+        error('');
+        val = bc.value(locFaceNo);
+        state_l.pressure = val(isP);
     end
-    s_bc = cellfun(@(s) s./sT_bc, s_bc, 'unif', false);
+    
+    mob_r = model.getProp(state_r, 'Mobility');
+    if hasOutsideMob
+        error('');
+        mob_l = bc.mob(locFaceNo,:);
+    else
+        mob_l = model.getProp(state_l, 'Mobility');
+    end
+    
+    mob = cell(1,nPh);
+    mobT = 0;
+    for i = 1:nPh
+        mob{i} = mob_l{i}.*isInj + mob_r{i}.*(~isInj);
+        mobT   = mobT + mob{i};
+    end
+    
+    rho_r = model.getProp(state_r, 'Density');
+    if hasOutsideRho
+        error('');
+        rho_l = bc.mob(locFaceNo,:);
+    else
+        rho_l = model.getProp(state_l, 'Density');
+    end
+    
+    rho = cell(1, nPh);
+    for i = 1:nPh
+        rho{i} = (rho_l{i} + rho_r{i})/2;
+    end
+    
+    
+    
+%     [s_bc, mob_bc] = deal(cell(nPh, 1));
+%     sT_bc = 0;
+%     % Evaluate saturations
+%     for phNo = 1:nPh
+%         % Saturation on inside of boundary
+%         s_r = disc.evaluateDGVariable(x_bc, c_bc, state, sdof{phNo});
+%         % Upstream saturation
+%         s_bc{phNo}   = s_l(locFaceNo,phNo).*isInj(locFaceNo) + s_r.*(~isInj(locFaceNo));
+%         % Total saturation
+%         sT_bc = sT_bc + s_bc{phNo};
+%     end
+%     s_bc = cellfun(@(s) s./sT_bc, s_bc, 'unif', false);
     % Compute mobilities
-    mobT_bc = 0;
-    for phNo = 1:nPh
-        mob_bc{phNo} = mob{phNo}(c_bc, s_bc);
-        mobT_bc = mobT_bc + mob_bc{phNo};
-    end
+%     mobT_bc = 0;
+%     for phNo = 1:nPh
+%         mob_bc{phNo} = mob{phNo}(c_bc, s_bc);
+%         mobT_bc = mobT_bc + mob_bc{phNo};
+%     end
     % Fractional flow functions
-    f_bc = cellfun(@(mob) mob./mobT_bc, mob_bc, 'unif', false);
+
     % Update gravity fluxes
     dz = G.cells.centroids(sum(G.faces.neighbors(bc.face,:),2), :) - G.faces.centroids(bc.face,:);
     gvec = model.getGravityVector();
     b_bc = cell(nPh,1);
-    c    = sum(G.faces.neighbors(bc.face,:),2);
-    x    = G.faces.centroids(bc.face,:);
-    for phNo = 1:nPh
-        b_bc{phNo} = b{phNo}(c_bc, s_bc{phNo});
-        
-        s      = disc.evaluateDGVariable(x, c, state, sdof{phNo});
-        rho_bc = rho{phNo}(c, s);
-        g{phNo}(bc.face) = rho_bc.*(dz*gvec');
+%     c    = sum(G.faces.neighbors(bc.face,:),2);
+%     x    = G.faces.centroids(bc.face,:);
+    rhogKdz = cell(1, nPh);
+    T = model.operators.T_all(face_bc);
+    for i = 1:nPh
+        rhogKdz{i} = T.*rho{i}.*(dz*gvec');
     end
     
-    [Kg_bc] = deal(cell(nPh,1));
-    for phNo = 1:nPh
-        Kg_bc{phNo} = T_all.*g{phNo};
-%         Kg_c{phNo} = disc.velocityInterp.faceFlux2cellVelocity(Kg_f{phNo});
-        Kg_bc{phNo} = Kg_bc{phNo}./G.faces.areas;
-    end
-    q_bc = deal(cell(nPh,1));
+%     [Kg_bc] = deal(cell(nPh,1));
+%     for phNo = 1:nPh
+%         Kg_bc{phNo} = T_all.*rhogKdz{phNo};
+% %         Kg_c{phNo} = disc.velocityInterp.faceFlux2cellVelocity(Kg_f{phNo});
+%         Kg_bc{phNo} = Kg_bc{phNo}./G.faces.areas;
+%     end
+    q = cell(1,nPh);
+    f = cellfun(@(mob) mob./mobT, mob, 'unif', false);
     for alpha = 1:nPh
 %         q_vc = b_c{alpha}.*f_bc{alpha}.*vT_c(c,:);
-        q_vf = b_bc{alpha}.*f_bc{alpha}.*vT(face_bc);
+        q_vf = rho{alpha}.*f{alpha}.*vT(face_bc);
 %         q_gc = 0;
         q_gf = 0;
         for beta = 1:nPh
             if beta ~= alpha
 %                 q_gc = q_gc + b_c{alpha}.*f_c{alpha}.*mob_c{beta}.*(Kg_c{alpha}(c,:) - Kg_c{beta}(c,:));
-                q_gf = q_gf + b_bc{alpha}.*f_bc{alpha}.*mob_bc{beta}.*(Kg_bc{alpha}(face_bc) - Kg_bc{beta}(face_bc));
+                q_gf = q_gf + rho{alpha}.*f{alpha}.*mob{beta}.*(rhogKdz{alpha}- rhogKdz{beta});
             end
         end
-        q_bc{alpha} = q_vf + q_gf;
+        q{alpha} = q_vf + q_gf;
     end
 
 end
