a7528ba45cca075d4a0efab77739943cce151d36
Olav Møyner <olav.moyner@sintef.no>
2019-10-21 15:04:24 -0700

    ad-core: Rigorous treatment for mass-mixing in cross-flowing wellbores.

---
+++ ad-core/statefunctions/facility/WellComponentTotalFluxDensityMix.m
@@ -0,0 +1,89 @@
+classdef WellComponentTotalFluxDensityMix < StateFunction
+    % Component total flux for wells (with treatment for cross-flow)
+    properties
+
+    end
+    
+    methods
+        function gp = WellComponentTotalFluxDensityMix(varargin)
+            gp@StateFunction(varargin{:});
+            gp = gp.dependsOn({'FacilityWellMapping'});
+            gp = gp.dependsOn({'ComponentPhaseFlux', 'InjectionSurfaceDensity'});
+        end
+        
+        function v = evaluateOnDomain(prop, model, state)
+            ncomp = model.getNumberOfComponents();
+            nph = model.getNumberOfPhases();
+            v = cell(ncomp, 1);
+            phase_flux = prop.getEvaluatedDependencies(state, 'ComponentPhaseFlux');
+            
+            for c = 1:ncomp
+                % Loop over phases where the component may be present
+                for ph = 1:nph
+                    % Check if present
+                    m = phase_flux{c, ph};
+                    if ~isempty(m)
+                        if isempty(v{c})
+                            v{c} = m;
+                        else
+                            v{c} = v{c} + m;
+                        end
+                    end
+                end
+            end
+            % If we have cross-flow and/or we are injecting more than one
+            % component, we need to ensure that injecting perforation
+            % composition mixtures equal the mix entering the wellbore
+            map = prop.getEvaluatedDependencies(state, 'FacilityWellMapping');
+            W   = map.W;
+            massFlux  = value(v');
+            massFluxTotal = sum(massFlux,2);
+            
+%             isInjector = map.isInjector(map.perf2well);
+            injection  = massFluxTotal > 0;
+%             production = ~injection & massFluxTotal ~= 0;
+%             crossflow = (injection & ~isInjector) | ...
+%                         (production & isInjector);
+            if any(injection)
+                rhoS = prop.getEvaluatedDependencies(state, 'InjectionSurfaceDensity');
+
+                ws = state.wellSol(map.active);
+                targetType = arrayfun(@(x) x.type, ws, 'UniformOutput', false);
+                targetValue = vertcat(ws.val);
+%                 isZero = ~strcmpi(targets, 'bhp') & val == 0;
+                nw = numel(W);
+                surfaceComposition = cell(ncomp, nph);
+                for c = 1:ncomp
+                    % Store well injector composition
+                    surfaceComposition(c, :) = model.ReservoirModel.Components{c}.getPhaseComponentFractionWell(model.ReservoirModel, state, W);
+                end
+                rem = cellfun(@isempty, surfaceComposition);
+                [surfaceComposition{rem}] = deal(zeros(nw, 1));
+                % Partial density: Units mass of component per volume of
+                % total surface volumetric surface stream
+                injectionMass = zeros(nw, ncomp);
+                phaseCompi = vertcat(W.compi);
+                isRate = ~(strcmpi(targetType, 'resv') | strcmpi(targetType, 'bhp'));
+                surfaceRates = value(state.FacilityState.surfacePhaseRates);
+                surfaceRates(isRate, :) = phaseCompi(isRate, :).*targetValue(isRate);
+                surfaceMassRates = surfaceRates.*rhoS;
+                for ph = 1:nph
+                    injectionMass = injectionMass + phaseCompi(:, ph).*surfaceMassRates(:, ph).*[surfaceComposition{:, ph}];
+                end
+                % Total volumetric flux in connections
+                volFlux = model.getProp(state, 'PhaseFlux');
+                volumeFluxTotal = sum(value(volFlux), 2);
+                rhoMix = crossFlowMixtureDensity(massFlux, volumeFluxTotal, injectionMass, map);
+                rhoMixPerf = rhoMix(map.perf2well, :);
+
+                vt = zeros(sum(injection), 1);
+                for i = 1:nph
+                    vt = vt + volFlux{i}(injection);
+                end
+                for i = 1:ncomp
+                    v{i}(injection) = vt.*rhoMixPerf(injection, i);
+                end
+            end
+        end
+    end
+end
