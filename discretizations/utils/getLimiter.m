0e11323b877b989a476f8b3dd52803d1aa4bbe9f
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-10-23 10:30:37 +0200

    Bugfixes in limters: TVB now correctly transforms gradients computed in SVD coordinates to dG reference coordinates. Implemented proper support for user-defined basis funcitons. Fixed bug in scale limiter (old version only scaled last column of dofs)

--- discretizations/utils/getLimiter.m
+++ discretizations/utils/getLimiter.m
@@ -47,8 +47,8 @@ function state = tvb(state, model, interpSetup, name, tol, limits)
 
     dof = model.getProp(state, [name, 'dof']);
     disc = model.discretization;
-    dim = min(disc.G.griddim, disc.basis.nDof-1);
-    state0 = state;
+    nLin = nnz(sum(disc.basis.k,2) == 1);
+    dim = min(disc.G.griddim, nLin);
     
     nc = size(dof,2);
     bad = false;
@@ -76,11 +76,6 @@ function state = tvb(state, model, interpSetup, name, tol, limits)
         end
         state = update(model, state, name, dof);
     end
-    
-%     if opt.plot
-%         plotLimiter(disc, state, state0, 'TVB');
-%     end
-    
     
 end
 
@@ -127,10 +122,10 @@ function dofbar = approximatGradient(model, interpSetup, state, dof)
     else
         map = struct('keep', true(G.cells.num,1));
     end
-    q    = nan(G.cells.num,1);
     ix   = disc.getDofIx(state, 1);
+    q    = nan(G.cells.num,1);
     q(map.keep) = dof(ix);
-    
+
     sigma = cell(1, disc.dim);
     [sigma{:}] = deal(0);
      
@@ -138,36 +133,50 @@ function dofbar = approximatGradient(model, interpSetup, state, dof)
         b = interpSetup.tri_basis{dNo};
         for l = 1:disc.dim+1
             loc_cells = interpSetup.C(:, l);
-            ccl = interpSetup.tri_cells(loc_cells);
-            ds = b(:, l).*q(ccl);
+            glob_cells = interpSetup.tri_cells(loc_cells);
+            ds = b(:, l).*q(glob_cells);
             sigma{dNo} = sigma{dNo} + ds;
+            
         end
     end
-
+    supCount = interpSetup.cell_support_count;
+    c = rldecode((1:model.G.cells.num)', supCount);
+    % Transform to physical coordinates
+    s = 0;
+    for dNo = 1:disc.dim
+        sigma{dNo} = sigma{dNo}.*interpSetup.scaling.scale(c, dNo);
+        s = s + sigma{dNo}.*interpSetup.scaling.mapping(c,:,dNo);
+    end
+    % Transform to dG reference coordinates
+    s = s./(G.cells.dx(c,:)/2);
+    sigma = mat2cell(s, size(s,1), ones(size(s,2),1));
     isLin = false(G.cells.num,1);
     isLin(map.keep) = any(state.degree > 0,2);
 
-    nLin = min(G.griddim, disc.basis.nDof-1);
+    nLin = nnz(sum(disc.basis.k,2) == 1);
+    k = disc.basis.k;
     dofbar = zeros(G.cells.num, nLin);
     db     = nan(sum(interpSetup.cell_support_count+1),1);
-    
     a   = [0;cumsum(interpSetup.cell_support_count+1)]+1;
     six = mcolon(a(1:end-1), a(2:end)-2);
     dix = cumsum(interpSetup.cell_support_count+1);
-    for dNo = 1:nLin
-        
-        ix = disc.getDofIx(state, 1+dNo, Inf, true);
-        d = zeros(G.cells.num,1);
-        d(isLin) = dof(ix(ix>0));
-        
-        s = sigma{dNo}(vertcat(interpSetup.cell_support{:}));
-        
-        db(six) = s;
-        db(dix) = d;
-        [dbmin, dbmax, ~, ~, neg, pos] = getMinMax(db, interpSetup.cell_support_count+1);
-        dofbar(:, dNo) = neg.*dbmax + pos.*dbmin;
-    end
-    
+    for dNo = 1:G.griddim
+        if any(k(:,dNo) == 1 & sum(k,2) == 1)
+            dofNo = find(k(:,dNo) == 1 & sum(k,2) == 1);
+            ix = disc.getDofIx(state, dofNo, Inf, true);
+            d = zeros(G.cells.num,1);
+            d(isLin) = dof(ix(ix>0));
+
+            s = sigma{dNo}(vertcat(interpSetup.cell_support{:}));
+            db(six) = s;
+            db(dix) = d;
+            % Assume approximated graidents == 0 is due to boundary effects
+            dd = rldecode(d, interpSetup.cell_support_count+1, 1);
+            db(db==0) = dd(db==0);
+            [dbmin, dbmax, ~, ~, neg, pos] = getMinMax(db, interpSetup.cell_support_count+1);
+            dofbar(:, dofNo-1) = neg.*dbmax + pos.*dbmin;
+        end
+    end    
     if useMap
         dofbar = dofbar(map.keep, :);
     end
@@ -213,22 +222,21 @@ function state = scale(state, model, name, tol, limits)
     nc = size(dof,2);
     
     for i = 1:nc
-        [vMin, vMax] = disc.getMinMax(state, dof(:,nc));
-        theta = [(v(:,nc) - limits(:,1))./(v(:,nc) - vMin), ...
-                 (limits(:,2) - v(:,nc))./(vMax - v(:,nc)), ...
+        [vMin, vMax] = disc.getMinMax(state, dof(:,i));
+        theta = [(v(:,i) - limits(:,1))./(v(:,i) - vMin), ...
+                 (limits(:,2) - v(:,i))./(vMax - v(:,i)), ...
                  ones(G.cells.num,1)          ];
         theta(~isfinite(theta) | abs(theta) > 1) = 1;
-        %             theta(abs(theta) > 1) = 1;
         theta = min(theta, [], 2);
 
         for dofNo = 2:disc.basis.nDof
             ix = disc.getDofIx(state, dofNo, (1:G.cells.num)', true);
-            dof(ix(ix>0),nc) = dof(ix(ix>0),nc).*theta(ix>0);
+            dof(ix(ix>0),i) = dof(ix(ix>0),i).*theta(ix>0);
         end
 
         vix = any(state.degree > 0);
         dix = disc.getDofIx(state, 1, vix);
-        dof(dix,nc) = (dof(dix,nc) - v(vix,nc)).*theta(vix) + v(vix,nc);
+        dof(dix,i) = (dof(dix,i) - v(vix,i)).*theta(vix) + v(vix,i);
     end
     
     state = update(model, state, name, dof);
@@ -236,11 +244,5 @@ function state = scale(state, model, name, tol, limits)
     if any(any(vMin < 0 - eps | vMax > 1 + eps,2)) && 0
         warning('Some values still outside [0,1]')
     end
-
-%     ind = theta < 1;
-%     if disc.degree > 1 && 0
-%         ix = disc.getDofIx(state, (G.griddim+2):nDofMax, ind);
-%         dof(ix,:) = 0;
-%     end
-
+    
 end
