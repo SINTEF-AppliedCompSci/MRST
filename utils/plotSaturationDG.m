6528ec3ffe1a1ef5443a20b4044d5bf30e60f152
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-10-24 16:40:42 +0200

    Update plotSaturationDG.m

--- utils/plotSaturationDG.m
+++ utils/plotSaturationDG.m
@@ -1,10 +1,11 @@
-function h = plotSaturationDG(disc, state, varargin)
+function [h, saturation] = plotSaturationDG(disc, state, varargin)
 
     opt = struct('n'      , 100  , ...
                  'phaseNo', 1    , ...
                  'plot1d' , false, ...
                  'coords' , []   , ...
-                 'contour', false);
+                 'contour', false, ...
+                 'makePlot', true);
     [opt, extra] = merge_options(opt, varargin{:});
     
     coords = opt.coords;
@@ -21,27 +22,29 @@ function h = plotSaturationDG(disc, state, varargin)
         xs{d} = reshape(x(:,d), n);
     end
     saturation = @(state) disc.evaluateDGVariable(x, cells, state, state.sdof(:,opt.phaseNo));
-    
-    s = saturation(state);
-    if opt.plot1d
-        h = plot(x(:,1), s, extra{:});
-    elseif disc.G.griddim == 2
-        s(~keep) = nan;
-        s        = reshape(s, n);
-        if opt.contour
-            h = contourf(xs{1}, xs{2}, s, extra{:});
+    h = [];
+    if opt.makePlot && ~isempty(state)
+        s = saturation(state);
+        if opt.plot1d
+            h = plot(x(:,1), s, extra{:});
+        elseif disc.G.griddim == 2
+            s(~keep) = nan;
+            s        = reshape(s, n);
+            if opt.contour
+                h = contourf(xs{1}, xs{2}, s, extra{:});
+            else
+                h = surf(xs{1}, xs{2}, s, extra{:});
+            end
         else
-            h = surf(xs{1}, xs{2}, s, extra{:});
+            s(~keep) = nan;
+            s        = reshape(s, n);
+            h = patch(isosurface(xs{1}, xs{2}, xs{3}, s, 0.6));
+            h.FaceColor = 'blue';
+            h.EdgeColor = 'none';
+            lighting gouraud
+            axis equal
+            axis([min(x(:,1)), max(x(:,1)), min(x(:,2)), max(x(:,2)), min(x(:,3)), max(x(:,3))]);
         end
-    else
-        s(~keep) = nan;
-        s        = reshape(s, n);
-        h = patch(isosurface(xs{1}, xs{2}, xs{3}, s, 0.6));
-        h.FaceColor = 'blue';
-        h.EdgeColor = 'none';
-        lighting gouraud
-        axis equal
-        axis([min(x(:,1)), max(x(:,1)), min(x(:,2)), max(x(:,2)), min(x(:,3)), max(x(:,3))]);
     end
 
 end
