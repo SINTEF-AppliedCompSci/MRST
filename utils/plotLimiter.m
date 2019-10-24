9d2b14448f46599626c91626b0e9f45b44531351
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2019-10-24 16:40:37 +0200

    Update plotLimiter.m

--- utils/plotLimiter.m
+++ utils/plotLimiter.m
@@ -2,39 +2,72 @@ function fn = plotLimiter(model, varargin)
 
     opt = struct('variable', 's'      , ...
                  'plot1d'  , false    , ...
-                 'view'    , [100, 20]);
+                 'view'    , [100, 20], ...
+                 'position', []       , ...
+                 'n'       , 200      );
     [opt, patchOpt] = merge_options(opt, varargin{:});
-    coords = getPlotCoordinates(model.G, 'n', 200, 'plot1d', opt.plot1d);
-    h = figure();
-    
+    % Get coordinates for plotting
+    coords = getPlotCoordinates(model.G, 'n', opt.n, 'plot1d', opt.plot1d);
+    % Set figure position
+    df = get(0, 'DefaultFigurePosition');
+    if opt.plot1d
+        opt.position = [df(1:2), 800,400];
+    else
+        opt.position = [df(1:2), 800,800];
+    end
+    % Make figure handle
+    fig = figure('Position', opt.position);
     disc = model.discretization;
-    fn = @(model, states, reports, solver, schedule, simtime) afterStepFunction(model, states, reports, solver, schedule, simtime, disc, coords, h, opt, patchOpt);
+    % Make plot functions
+    if opt.plot1d
+        [hu, su] = plotSaturationDG(disc, [], 'plot1d', true, 'coords', coords, 'makePlot', false);
+        [hl, sl] = plotSaturationDG(disc, [], 'plot1d', true, 'coords', coords, 'makePlot', false);
+    else
+        [hu, su] = plotSaturationDG(disc, [], 'coords', coords, 'makePlot', false);
+        [hl, sl] = plotSaturationDG(disc, [], 'coords', coords, 'makePlot', false);
+    end
+    fn = @(model, states, reports, solver, schedule, simtime) ...
+        afterStepFunction(model, states, reports, solver, schedule, simtime, disc, coords, fig, {hu, hl}, {su, sl}, opt, patchOpt);
 end
 
-function [model, states, reports, solver, ok] = afterStepFunction(model, states, reports, solver, schedule, simtim, disc, coords, h, opt, patchOpt)
+function [model, states, reports, solver, ok] ...
+    = afterStepFunction(model, states, reports, solver, schedule, simtime, disc, coords, fig, h, sat, opt, patchOpt)
     computed = cellfun(@(x) ~isempty(x), states);
     current = find(computed, 1, 'last');
     st      = states{current};
-    if ishandle(h)
-        set(0, 'CurrentFigure', h);
+    if ishandle(fig)
+        set(0, 'CurrentFigure', fig);
     else
-        figure(h);
+        error('Figure handle deleted');
     end
-    clf;
+    if current == 1
+        clf;
+    end
+    ax = fig.Children(isa(fig.Children, 'matlab.graphics.axis.Axes'));
     if opt.plot1d
-        hold on
-        plotSaturationDG(disc, st.ul, 'n', 500, 'plot1d', true, 'color', 'k', 'linew', 2);
-        plotSaturationDG(disc, st, 'n', 500, 'plot1d', true, 'color', 'k', 'linew', 4, 'LineStyle', '--'); hold off
-        ylim([-0.2,1.2]);
+        sul = sat{1}(st.ul);
+        sl  = sat{2}(st);
+        if isempty(ax)
+            x = coords.points(:,1); 
+            hold on
+            plot(x(:,1), sul, 'color', 'k', 'lineWidth', 2);
+            plot(x(:,1), sl , 'color', 'k', 'lineWidth', 4, 'LineStyle', '--');
+            ylim([-0.2, 1.2]);
+            hold off
+        else
+            l = ax.Children(arrayfun(@(c) isa(c, 'matlab.graphics.chart.primitive.Line'), ax.Children));
+            l(2).YData = sul;
+            l(1).YData = sl;
+        end
     else
         subplot(1,2,1)
-        plotSaturationDG(disc, st.ul, patchOpt{:});
+        plotSaturationDG(disc, st.ul, patchOpt{:}, 'coords', coords);
         view(opt.view)
         camlight
         lighting gouraud
         zlim([-0.2, 1.2])
         subplot(1,2,2)
-        plotSaturationDG(disc, st, patchOpt{:});
+        plotSaturationDG(disc, st, patchOpt{:}, 'coords', coords);
         view(opt.view)
         camlight
         zlim([-0.2, 1.2])
