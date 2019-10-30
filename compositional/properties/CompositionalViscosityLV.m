93d3a44e8683752299ce334b423adbd00ef3b03f
Olav Møyner <olav.moyner@sintef.no>
2019-10-30 15:04:37 -0700

    Update CompositionalViscosityLV.m
    Use mole fractions as dynamic properties instead of directly from state

--- compositional/properties/CompositionalViscosityLV.m
+++ compositional/properties/CompositionalViscosityLV.m
@@ -6,8 +6,8 @@ classdef CompositionalViscosityLV < StateFunction
     methods
         function gp = CompositionalViscosityLV(model, varargin)
             gp@StateFunction(model, varargin{:});
-            gp = gp.dependsOn({'PhasePressures', 'PhaseCompressibilityFactors'});
-            gp = gp.dependsOn({'pressure', 'temperature', 'x', 'y'}, 'state');
+            gp = gp.dependsOn({'PhasePressures', 'PhaseCompressibilityFactors', 'ComponentPhaseMoleFractions'});
+            gp = gp.dependsOn({'pressure', 'temperature'}, 'state');
         end
         
         function mu = evaluateOnDomain(prop, model, state)
@@ -15,8 +15,15 @@ classdef CompositionalViscosityLV < StateFunction
             nph = sum(act);
             mu = cell(1, nph);
             
-            [p, T, x, y] = model.getProps(state, 'pressure', 'T', 'x', 'y');
-            Z = prop.getEvaluatedDependencies(state, 'PhaseCompressibilityFactors');
+            [p, T] = model.getProps(state, 'pressure', 'T');
+            [Z, mf] = prop.getEvaluatedDependencies(state, 'PhaseCompressibilityFactors',...
+                                                           'ComponentPhaseMoleFractions');
+            oix = phInd == 2;
+            gix = phInd == 3;
+            
+            x = mf((1+model.water):end, oix);
+            y = mf((1+model.water):end, gix);
+
             if model.water
                 p_phase = prop.getEvaluatedDependencies(state, 'PhasePressures');
                 f = model.fluid;
@@ -25,9 +32,7 @@ classdef CompositionalViscosityLV < StateFunction
                 mu{wix} = prop.evaluateFunctionOnDomainWithArguments(f.muW, pw);
             end
             pm = model.EOSModel.PropertyModel;
-            oix = phInd == 2;
             mu{oix} = pm.computeViscosity(p, x, Z{oix}, T, true);
-            gix = phInd == 3;
             
             twoPhase = model.getTwoPhaseFlag(state);
             if prop.useCompactEvaluation && ~all(twoPhase)
