6c8286a72082ad95c5be08fd5b572a910738f291
Xavier Raynaud <xav.raynaud@gmail.com>
2019-11-07 14:56:57 +0100

    fix in computeInitAquifer (function is not tested).

--- ad-blackoil/utils/computeInitAquifer.m
+++ ad-blackoil/utils/computeInitAquifer.m
@@ -5,14 +5,16 @@ function initState = computeInitAquifer(model, initState, initaqvolumes)
     naq      = max(aquid);
     nconn    = size(aquid, 1);
     
-    p_aq = zeros(naq, 1);
+    p_aq   = zeros(naq, 1);
     p_aqAD = initVariablesADI(p_aq);
-    V_aq = initaqvolumes;
     
-    sW   = model.getProp(initState, 'sW');
-    p    = model.getProp(initState, 'pressure');
+    state = initState;
+    state = model.setProp(state, 'aquiferpressures', p_aqAD);
+    state = model.setProp(state, 'aquifervolumes'  , initaqvolumes);
+    
     dt = 0;
-    q = computeAquiferFluxes(model, p, sW, p_aqAD, V_aq, dt);
+    
+    q = computeAquiferFluxes(model, state, dt);
     % We want to compute p_aq that minimizes q'*q. The function q is affine with respect to p_aq,
     % that is, q = M*p_aq + r, for some M and r (see function
     % computeAquiferFluxes). The solution p_aq of the minimization problem is
