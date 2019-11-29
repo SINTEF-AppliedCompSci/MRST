b47c223f3b7ca137233f7b593cbbea498ba285f6
Olav Møyner <olav.moyner@sintef.no>
2019-11-29 10:40:30 -0800

    resetAMGCL: Fit dimensions of valid input.

--- linearsolvers/amgcl/utils/resetAMGCL.m
+++ linearsolvers/amgcl/utils/resetAMGCL.m
@@ -1,4 +1,4 @@
 function resetAMGCL(varargin)
     amg_opt = getAMGCLMexStruct(varargin{:});
-    amgcl_matlab(sparse([], [], []), [], amg_opt, nan, nan, 1000);
+    amgcl_matlab(sparse([], [], []), zeros(0, 1), amg_opt, nan, nan, 1000);
 end
