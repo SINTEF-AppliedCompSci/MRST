98e2b0dc4afaeeee2a339979f85d8e407b3e1fe2
Olav Møyner <olav.moyner@sintef.no>
2019-11-29 08:40:00 -0800

    reconstructPressure: Add optional LU-factorization

--- msrsb/utils/reconstructPressure.m
+++ msrsb/utils/reconstructPressure.m
@@ -1,4 +1,4 @@
-function [p, solvetime] = reconstructPressure(partition, pressure, A, rhs, addNoise)
+function [p, solvetime] = reconstructPressure(partition, pressure, A, rhs, fixMethod)
 % Solve reconstruction problem for multiscale methods
 
 %{
@@ -20,22 +20,31 @@ You should have received a copy of the GNU General Public License
 along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
     if nargin < 5
-        addNoise = true;
+        fixMethod = 1;
     end
     D = formReconstructionMatrix(A, partition);
     n = size(A, 1);
-    % Add a tiny bit of noise to overcome a Matlab UMFPACK issue in MATLAB
-    % 2018b and onwards
-    if addNoise
+    L = [];
+    if fixMethod == 1
+        % Add a tiny bit of noise to overcome a Matlab UMFPACK issue in MATLAB
+        % 2018b and onwards
         d = diag(A);
         D = D - sparse(1:n, 1:n, min(d)*1e-8, n, n);
+    elseif fixMethod == 2
+        % Alternatively: Use LU-factorization to fix.
+        [L, U] = lu(D);
     end
     tmp1 = warning('query','MATLAB:nearlySingularMatrix');
     tmp2 = warning('query','MATLAB:singularMatrix');
     warning('off','MATLAB:nearlySingularMatrix')
     warning('off','MATLAB:singularMatrix')
     t = tic();
-    p = mldivide(D, rhs - (A - D)*pressure);
+    rhs_new = rhs - (A - D)*pressure;
+    if isempty(L)
+        p = mldivide(D, rhs_new);
+    else
+        p = U\(L\rhs_new);
+    end
     assert(all(isfinite(p)))
     solvetime = toc(t);
     warning(tmp1.state,'MATLAB:nearlySingularMatrix')
