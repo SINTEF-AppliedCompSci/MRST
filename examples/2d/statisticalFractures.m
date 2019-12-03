e43df10420e95640282f376a369a18934647a711
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2019-12-03 17:35:41 +0100

    Upgraded plotting: now includes plot of edge-length distance.

--- examples/2d/statisticalFractures.m
+++ examples/2d/statisticalFractures.m
@@ -29,13 +29,15 @@ end
 l = l(ix);
 
 clf;
+subplot(2,1,1);
 patch([0 35 35 0],[0 0 yMax yMax],ones(4,1),'FaceColor',[.95 .95 1])
 plotLinePath(l,'LineWidth',1);
-axis equal
+view(90,90), axis tight
 
 %% Set grid parameters.
-% We need to refine the grid in some areas since some fractures are very
-% close to each other, but not intersecting.
+% We need to refine the grid in some areas, since some fractures are very
+% close to each other, but not intersecting. We mark these points and
+% introduce a local 
 eps = [3,5,1,1,1,2,2,1.5,1,1.5,1.5,1.5,1,1.5,1.5,2.5,1.5];
 refPts = [17.4,79.5; 15.0,71.6; 25.9,98.7; 25.8,97.9; ...
           25.7,97.2; 25.7,96.4; 25.5,94.7; 25.3,92.8; ...
@@ -48,12 +50,23 @@ for i=1:numel(ix)
 end
 [eps,amp,refPts] = deal(eps(ix),amp(ix),refPts(ix,:));
 faultRho =@(p) min(ones(size(p,1),1), ...
-    min(bsxfun(@times, amp,exp(bsxfun(@rdivide, distance(p,refPts),eps))),[],2));
+    min(bsxfun(@times, amp,exp(bsxfun(@rdivide, pdist2(p,refPts),eps))),[],2));
+
+% Plot the distance function
+G = computeGeometry(cartGrid([70 2*yMax],[35 yMax]));
+subplot(2,1,2)
+plotCellData(G, faultRho(G.cells.centroids),'EdgeColor','none'); view(90,90)
+hold on
+plotLinePath(l,'k', 'LineWidth',1);
+plot(refPts(:,1),refPts(:,2),'w.','MarkerSize',12);
+hold off,
+axis tight
 
 %% Generate grid
 G = pebiGrid(15,[35,yMax],'faultLines',l,'faultGridFactor',1/50,...
              'circleFactor',0.62,'faultRefinement',true,'faultEps',5,...
              'faultRho', faultRho,'useMrstPebi',true,'linearize', true);
 
-%% plot grid
-plotGrid(G,'FaceColor','none')
+%% Plot the grid
+subplot(2,1,1)
+plotGrid(G,'FaceColor','none'), axis tight
