f581b1649dd0506c4ae782b71f65cbe9c8f1415b
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2019-12-03 22:32:02 +0100

    MRST-Autodiff: Insert Copyright Notices
    Commit 11/many.

--- ad-fi/orthodecomp/createPod.m
+++ ad-fi/orthodecomp/createPod.m
@@ -1,5 +1,26 @@
-function [basis X_bar] = createPod(values, frac_e, n_e)
-    [X X_bar] = snapshots(values);
+function [basis, X_bar] = createPod(values, frac_e, n_e)
+%Undocumented Utility Function
+
+%{
+Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
+    [X, X_bar] = snapshots(values);
     [V,S,W] = svd(X); % bruk econ?
     eig = diag(S).^2;
     tot_e = sum(eig);
