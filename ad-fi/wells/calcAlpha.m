93e1ef896f8e28486ca91b582b6395f1db9f647f
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2019-12-03 22:39:05 +0100

    MRST-Autodiff: Insert Copyright Notices
    Commit 12/many.

--- ad-fi/wells/calcAlpha.m
+++ ad-fi/wells/calcAlpha.m
@@ -1,6 +1,25 @@
 function alpha = calcAlpha(W, bq, b, rs, perf2well)
 % finally explicit computations of alpha:
 
+%{
+Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
 if nargin < 5 % single well
     perf2well = ones(numel(W.cells), 1);
 end
--- ad-fi/wells/computeWellHead.m
+++ ad-fi/wells/computeWellHead.m
@@ -1,6 +1,26 @@
 function Hw = computeWellHead(w, alpha, rho)
 % compute pressure drop from ref depth to connections of corresponding well
 % assumes input of class double (not ADI)
+
+%{
+Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
 if iscell(rho)
     rho  = cell2mat( cellfun(@double, rho, 'UniformOutput', false) );
 end
