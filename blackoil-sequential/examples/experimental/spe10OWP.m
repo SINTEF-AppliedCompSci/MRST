9c19e774322c4fdeb8afd585ba08466811daf083
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2019-12-03 22:57:48 +0100

    MRST-Autodiff: Insert Copyright Notices
    Commit 15/many.

--- blackoil-sequential/examples/experimental/spe10OWP.m
+++ blackoil-sequential/examples/experimental/spe10OWP.m
@@ -266,4 +266,29 @@ disp(['Speedup pressure: ', num2str(sum(t1(1))/sum(t2(1)))])
 disp(['Speedup solver: ', num2str(sum(t1(1:2))/sum(t2(1:2)))])
 disp(['Speedup simulation: ', num2str(sum(t1(1:3))/sum(t2(1:3)))])
 
-
+%%
+% <html>
+% <p><font size="-1">
+% Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
+% </font></p>
+% <p><font size="-1">
+% This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+% </font></p>
+% <p><font size="-1">
+% MRST is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% </font></p>
+% <p><font size="-1">
+% MRST is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% </font></p>
+% <p><font size="-1">
+% You should have received a copy of the GNU General Public License
+% along with MRST.  If not, see
+% <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses</a>.
+% </font></p>
+% </html>
