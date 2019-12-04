d75f684ba664054c3840a047683b023a05acf35e
Xavier Raynaud <xav.raynaud@gmail.com>
2019-12-04 14:38:55 +0100

    added comment

--- utils/tbl_utils/sortTable.m
+++ utils/tbl_utils/sortTable.m
@@ -1,4 +1,32 @@
 function tbl = sortTable(tbl, fds, varargin);
+%
+%
+% SYNOPSIS:
+%   function tbl = sortTable(tbl, fds, varargin)
+%
+% DESCRIPTION: Sort in increasing order the rows of indices given by the
+% indexing table tbl, following the order given by fds. (We use sortrows)
+%
+% PARAMETERS:
+%   tbl      - Indexing table
+%   fds      - Field names which give the order of the columns for the row
+%              indexing sort
+%   varargin - 
+%
+% KEYWORD ARGUMENTS:
+%
+%   keepAllFields - We keep all the fields for the table, even those that are
+%                   not given in fds but belong to tbl. The default value is false. This
+%                   option has not been properly tested and we recommend not using it for the moment.
+%   
+% RETURNS:
+%   tbl - Indexing table that has been sorted by row.
+%
+% EXAMPLE:
+%
+% SEE ALSO: `setupTableMapping`
+%
+
     opt = struct('keepAllFields', false);
     opt = merge_options(opt, varargin{:});
     
