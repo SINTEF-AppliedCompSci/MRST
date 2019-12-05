8ae5781d704f157945ba3e3e5b3c19b7c3dae13c
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2019-12-06 00:33:58 +0100

    MRST-Visualization: Insert Copyright Notices
    In preparation of MRST 2019b.

--- diagnostics/utils/private/showAllocation.m
+++ diagnostics/utils/private/showAllocation.m
@@ -1,4 +1,25 @@
 function showAllocation(d, src, ax, s2, s3)
+%Undocumented Utility Function
+
+%{
+Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
 ts = s3.tsel.ix;
 [iIx, pIx] = deal(s3.wsel.injectorIx, s3.wsel.producerIx);
 
--- mrst-gui/utils/WellSolSelector.m
+++ mrst-gui/utils/WellSolSelector.m
@@ -196,6 +196,25 @@ classdef WellSolSelector < UIItem
     end
 end
 
+%{
+Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
 % function [names, props, time, name2prop] = processSummary(smry)
 % 
 % if isfield(smry, 'MRST')
@@ -233,6 +252,3 @@ end
 % M(~nmIx, :) = [];
 % name2prop = logical(M);
 % end
-        
-
-
