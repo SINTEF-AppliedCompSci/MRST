43a93d01f6d74144bfc3785efc580fb415bb58d3
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2019-12-06 15:32:32 +0100

    CO2Lab: Run Contents Report
    In preparation of MRST 2019b.

---
+++ co2lab/utils/mappings/Contents.m
@@ -0,0 +1,13 @@
+% MAPPINGS
+%
+% Files
+%   accumulateVertically   - Accumulate quantity vertically per column.
+%   cumulativeHeight       - Compute cumulative height for each column.
+%   cumulativePoreHeight   - Compute cumulative pore height for each column.
+%   fillDegree             - Compute degree of fill (saturation).
+%   height2Sat             - Convert from height to saturation
+%   integrateVertically    - Compute integral of function, vertically per column.
+%   invertVerticalFunction - Solve y(z)=f for piecewise linear, monotonically increasing function y.
+%   normalizeValuesVE      - Normalizes values for different VE grids and formulations.
+%   sat2height             - Convert from saturation to height
+%   upscaledSat2height     - Compute upscaled height, based on upscaled saturation
