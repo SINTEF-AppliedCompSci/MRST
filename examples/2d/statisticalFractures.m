507f54a56912af6a582afade46c1eea54be0d8e8
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2019-12-07 22:10:26 +0100

    Expanded plotting

--- examples/2d/statisticalFractures.m
+++ examples/2d/statisticalFractures.m
@@ -15,29 +15,31 @@ mrstModule add upr
 %% Load dataset and possibly select a subset of the fractures
 % To select a smaller subset of the fractures, you can set yMax to be
 % smaller than the default value of 120.
-yMax = 60;
+yMax = 120;
 pth = fullfile(mrstPath('upr'), 'datasets', 'statistical_fractures.mat');
 load(pth)
-l = mat2cell(fl, ones(size(fl,1),1), size(fl,2)).';
-l = cellfun(@(c) reshape(c', 2,[])' + 2,l,'un',false);
+lines = mat2cell(fl, ones(size(fl,1),1), size(fl,2)).';
+lines = cellfun(@(c) reshape(c', 2,[])' + 2,lines,'un',false);
 
-ix = false(size(l));
+ix = false(size(lines));
 for i=1:numel(ix)
-  ll=l{i}; 
+  ll=lines{i}; 
   ix(i) = max(ll(:,2))<yMax;
 end
-l = l(ix);
+lines = lines(ix);
 
 clf;
 subplot(2,1,1);
 patch([0 35 35 0],[0 0 yMax yMax],ones(4,1),'FaceColor',[.95 .95 1])
-plotLinePath(l,'LineWidth',1);
+plotLinePath(lines,'LineWidth',1);
 view(90,90), axis tight
 
 %% Set grid parameters.
 % We need to refine the grid in some areas, since some fractures are very
 % close to each other, but not intersecting. We mark these points and
-% introduce a local 
+% introduce local grid refinement by setting up a grid density function
+% whose values decay exponentially as we approach any of the points of
+% interest.
 eps = [3,5,1,1,1,2,2,1.5,1,1.5,1.5,1.5,1,1.5,1.5,2.5,1.5];
 refPts = [17.4,79.5; 15.0,71.6; 25.9,98.7; 25.8,97.9; ...
           25.7,97.2; 25.7,96.4; 25.5,94.7; 25.3,92.8; ...
@@ -57,16 +59,42 @@ G = computeGeometry(cartGrid([70 2*yMax],[35 yMax]));
 subplot(2,1,2)
 plotCellData(G, faultRho(G.cells.centroids),'EdgeColor','none'); view(90,90)
 hold on
-plotLinePath(l,'k', 'LineWidth',1);
-plot(refPts(:,1),refPts(:,2),'w.','MarkerSize',12);
+plotLinePath(lines,'k', 'LineWidth',1);
+plot(refPts(:,1),refPts(:,2),'r.','MarkerSize',12);
+patch([0 35 35 0],[0 0 yMax yMax],ones(4,1),'FaceColor','none')
+colormap([parula.^2; 1 1 1])
 hold off,
 axis tight
 
 %% Generate grid
-G = pebiGrid(15,[35,yMax],'faultLines',l,'faultGridFactor',1/50,...
+G = pebiGrid(15,[35,yMax],'faultLines',lines,'faultGridFactor',1/50,...
              'circleFactor',0.62,'faultRefinement',true,'faultEps',5,...
              'faultRho', faultRho,'useMrstPebi',true,'linearize', true);
 
 %% Plot the grid
-subplot(2,1,1)
-plotGrid(G,'FaceColor','none'), axis tight
+clf
+subplot(3,1,1:2)
+patch([0 35 35 0],[0 0 yMax yMax],ones(4,1),'FaceColor',[.95 .95 1]), 
+plotGrid(G,'FaceColor','none')
+plotLinePath(lines)
+hold on
+patch([23.5 25.5 25.5 23.5],[17 17 28 28],ones(4,1), ...
+    'LineWidth',1,'EdgeColor','b','FaceColor','none')
+patch([24.6 27 27 24.6],[65 65 74 74],ones(4,1), ...
+    'LineWidth',1,'EdgeColor','g','FaceColor','none')
+hold off
+view(90,90), axis tight off
+ax1 = gca;
+
+% Zoom 1
+subplot(3,2,5)
+copyobj(get(ax1,'Children'),gca);
+axis tight off, axis([23.5 25.5 17 28]); view(90,90)
+ax=gca; ax.Position = ax.Position + [0 0 .03 .04];
+
+% Zoom 2
+subplot(3,2,6)
+copyobj(get(ax1,'Children'),gca);
+axis tight off, axis([24.6 27 65 74]); view(90,90)
+ax=gca; ax.Position = ax.Position + [-.03 0 .03 .04];
+
