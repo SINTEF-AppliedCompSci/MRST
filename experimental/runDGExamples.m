7d796f6deaec1d4eacb4c42fcf60d3c8042a211b
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2019-12-10 11:56:21 +0100

    Update runDGExamples.m

--- experimental/runDGExamples.m
+++ experimental/runDGExamples.m
@@ -1,6 +1,16 @@
 pth = fullfile(mrstPath('dg'), 'examples');
 
 examples = {'dgExampleDiscretization.m', 'dgExampleBLDisplacement.m', 'dgExampleIFS.m'};
+
+%% Run all
+
 for i = 1:numel(examples)
+    run(fullfile(pth, examples{i}));
+end
+
+%% Run selected
+
+ix = [2,3];
+for i = ix
     run(fullfile(pth, examples{i}));
 end
