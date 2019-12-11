83a8353353e919d2dacb7967f3c43dbf620caa0b
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2019-12-11 22:59:14 +0100

    MRST-dG: Insert Copyright Notices Where Missing
    In preparation of MRST 2019b.

--- discretizations/utils/assignDofFromState.m
+++ discretizations/utils/assignDofFromState.m
@@ -2,6 +2,26 @@ function state = assignDofFromState(disc, state, names)
     % Assign dofs from state (typically initial state). All dofs
     % for dofNo > 0 are set to zero.
     % Set degree to disc.degree in all cells
+
+%{
+Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
     if ~isfield(state, 'degree')
         state.degree = repmat(disc.degree, disc.G.cells.num, 1);
     end
--- utils/plotSaturationDG.m
+++ utils/plotSaturationDG.m
@@ -1,5 +1,25 @@
 function [h, saturation] = plotSaturationDG(disc, state, varargin)
     % Visualize dG saturation with higher-order dG basis functions
+
+%{
+Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
     opt = struct('n'      , 100  , ...
                  'phaseNo', 1    , ...
                  'plot1d' , false, ...
