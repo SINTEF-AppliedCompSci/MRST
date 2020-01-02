69496115c12260263425c499100fc6150bbbe856
Olav Møyner <olav.moyner@gmail.com>
2020-01-02 19:05:44 +0100

    ad-props: Tentative implementation of SOF-type keywords.

--- ad-props/props/assignSOF3.m
+++ ad-props/props/assignSOF3.m
@@ -1,21 +1,33 @@
 function f = assignSOF3(f, sof3, reg)
-f.krOW = @(so, varargin)krOW(so, sof3, reg, varargin{:});
-f.krOG = @(so, varargin)krOG(so, sof3, reg, varargin{:});
+    [f.krOW, f.krOG, f.krPts.ow, f.krPts.og] = getFunctions(f, sof3, reg);
+
 end
 
-function v = krOW(so, sof3, reg, varargin)
-satinx = getRegMap(so, reg.SATNUM, reg.SATINX, varargin{:});
-T = cellfun(@(x)x(:,[1,2]), sof3, 'UniformOutput', false);
-T = extendTab(T);
-v = interpReg(T, so, satinx);
+function [krOW, krOG, pts_ow, pts_og] = getFunctions(f, SOF3, reg)
+    [krOW, krOG] = deal(cell(1, reg.sat));
+    
+    [pts_ow, pts_og] = deal(zeros(reg.sat, 4));
+    for i = 1:reg.sat
+        sof3 = SOF3{i};
+        SO = sof3(:, 1);
+        pts_ow(i, :) = getPoints(SO, sof3(:, 2));
+        pts_og(i, :) = getPoints(SO, sof3(:, 3));
+        sof3 = extendTab(sof3);
+        krow = sof3(:, 2);
+        krog = sof3(:, 3);
+        krOW{i} = @(so) interpTable(SO, krow, so);
+        krOG{i} = @(so) interpTable(SO, krog, so);
+    end
 end
 
-function v = krOG(so, sof3, reg, varargin)
-satinx = getRegMap(so, reg.SATNUM, reg.SATINX, varargin{:});
-T = cellfun(@(x)x(:,[1,3]), sof3, 'UniformOutput', false);
-T = extendTab(T);
-v = interpReg(T, so, satinx);
+function pts = getPoints(so, kro)
+    pts = zeros(1, 4);
+    ii = find(kro == 0, 1, 'first');
+    pts(2) = so(ii);
+    pts(3) = 1;
+    pts(4) = kro(end);
 end
+
 
 
 %{
