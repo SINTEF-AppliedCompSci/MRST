9dfcb21bd9605c2cf9f031fa34c9bb4989354860
Xavier Raynaud <xav.raynaud@gmail.com>
2020-01-28 17:38:38 +0100

    more work in computeMPSATrans

--- computeMPSATrans.m
+++ computeMPSATrans.m
@@ -27,6 +27,7 @@ rock = makeRock(G, 1e-3*darcy, 1);
 
 nc  = G.cells.num;
 nf  = G.faces.num;
+nn  = G.nodes.num;
 dim = G.griddim;
 
 coltbl.coldim = (1 : dim)';
@@ -36,6 +37,9 @@ rowtbl = replacefield(rowtbl, {'coldim', 'rowdim'});
 
 celltbl.cells = (1 : nc)';
 celltbl.num = nc;
+
+nodetbl.nodes = (1 : nn)';
+nodetbl.num = nn;
 
 cellcoltbl = crossTable(celltbl, coltbl, {}); % ordering is cell - col
 
@@ -193,7 +197,6 @@ div_nodeface = div_nodeface.setFromTensorProd(d, prod);
 % are defined below
 %
 
-warning('div_cell to be fixed...')
 fds = {'cells', 'nodes', 'coldim'};
 % note the minus sign below (see formula in paper)
 dreduced = - tblmap(facetNormals, cellnodefacecoltbl, cellnodecoltbl, fds);
@@ -202,33 +205,172 @@ prod = TensorProd();
 prod.tbl1 = cellnodecoltbl;
 prod.tbl2 = cellnodecolrowtbl;
 prod.replacefds1 = {'coldim', 'rowdim'};
+prod.replacefds2 = {'coldim', 'rowdim', 'interchange'};
 prod.reducefds   = {'rowdim', 'nodes'};
 prod.mergefds    = {'cells'};
+prod.prodtbl = cellcoltbl;
 prod = prod.setup();
 
 div_cell = SparseTensor();
 div_cell = div_cell.setFromTensorProd(dreduced, prod);
 
-
 divgrad_nodeface = multSparseTensor(div_nodeface, grad_nodeface);
 
-divgradmat = divgrad_nodeface.getMatrix();
+colrowtbl = crossTable(coltbl, rowtbl, {});
+nodecolrowtbl = crossTable(nodetbl, colrowtbl, {});
+
+col2row2tbl.coldim2 = colrowtbl.coldim;
+col2row2tbl.rowdim2 = colrowtbl.rowdim;
+col2row2tbl.coldim1 = colrowtbl.rowdim;
+col2row2tbl.rowdim1 = colrowtbl.coldim;
+col2row2tbl.num = colrowtbl.num;
+
+prod = TensorProd();
+prod.tbl1 = col2row2tbl;
+prod.tbl2 = nodecolrowtbl;
+prod.replacefds1 = {{'coldim1', 'coldim'}, ...
+                    {'rowdim1', 'rowdim'}};
+prod.replacefds2 = {{'coldim', 'coldim2'}, ...
+                    {'rowdim', 'rowdim2'}};
+prod.reducefds = {'coldim2', 'rowdim2'};
+prod.prodtbl = nodecolrowtbl;
+prod = prod.setup();
+
+sym_op = SparseTensor();
+sym_op = sym_op.setFromTensorProd(ones(col2row2tbl.num, 1), prod);
+
+% get nodal average of cellnode tensor
+prod = TensorProd();
+prod.tbl1 = celltbl;
+prod.tbl2 = cellnodecolrowtbl;
+prod.reducefds = {'cells'};
+prod.prodtbl = nodecolrowtbl;
+
+prod = prod.setup();
+
+nodeaverage_op = SparseTensor();
+nodeaverage_op = nodeaverage_op.setFromTensorProd(ones(celltbl.num), prod);
+
+symnodeaverage_op = multSparseTensor(sym_op, nodeaverage_op);
+
+% Defines stiffness operator
+
+dim = G.griddim;
+vdim = dim*(dim + 1)/2;
+voigttbl.voigt = (1 : vdim)';
+switch dim
+  case 2
+    voigttbl.coldim = [1; 2; 2];
+    voigttbl.rowdim = [1; 2; 1];
+    voigt = [1; 3; 3; 2];
+  case 3
+    voigttbl.coldim = [1; 2; 3; 3; 3; 2];
+    voigttbl.rowdim = [1; 2; 3; 2; 1; 1];
+    voigt = [1; 6; 5; 6; 2; 4; 5; 4; 3];
+end
+voigttbl.num = vdim;
+
+colrowvoigttbl.coldim = colrowtbl.coldim;
+colrowvoigttbl.rowdim = colrowtbl.rowdim;
+colrowvoigttbl.voigt  = voigt;
+colrowvoigttbl.num    = colrowtbl.num;
+
+stensdim = vdim*(vdim + 1)/2;
+
+s = 0;
+voigt1 = [];
+voigt2 = [];
+stensind = [];
+
+
+for i = 1 : vdim
+    for j = i : vdim
+        voigt1(end + 1) = i;
+        voigt2(end + 1) = j;
+        s = s + 1;
+        stensind(end + 1) = s;
+    end
+end
+
+
+stenstbl.voigt1 = voigt1';
+stenstbl.voigt2 = voigt2';
+stenstbl.stensind = stensind';
+stenstbl.num = stensdim;
+fds = {'stensind', 'voigt1', 'voigt2'};
+stenstbl = sortTable(stenstbl, fds);
+
+voigt2tbl = crossTable(voigtbl, voigtbl, 'crossextend', {'voigt1', ...
+                    'voigt2'});
+
+
+for i = 1 : voigt2tbl.num
+    v1 = voigt2tbl.voigt1;
+    v2 = voigt2tbl.voigt2;
+    if v1 < v2;
+        stensind(i) = 
+    
+% stensmat = convertTableToArray(stenstbl, {'stensind', 'voigt1', 'voigt2'});
+
+
+% stensfullmat = [stensmat; ...
+%             [stensmat(:, 1), stensmat(:, 3), stensmat(:, 2)]];
+
+% stensfulltbl = convertArrayToTable(stensfullmat, {'stensind', 'voigt1', ...
+%                     'voigt2'});
+
+% stensfulltbl = crossTable(stensfulltbl, voigttbl, {{'voigt1', 'voigt'}});
+% stensfulltbl = replacefield(stensfulltbl, {{'coldim', 'coldim1'}, {'rowdim', 'rowdim1'}});
+% stensfulltbl = crossTable(stensfulltbl, voigttbl, {{'voigt2', 'voigt'}});
+% stensfulltbl = replacefield(stensfulltbl, {{'coldim', 'coldim2'}, {'rowdim', 'rowdim2'}});
+% stensfulltbl = rmfield(stensfulltbl, {'voigt1', 'voigt2'});
+
+% fds = {'stensind', 'rowdim1', 'coldim1', 'rowdim2', 'coldim2'};
+% stensfulltbl = sortTable(stensfulltbl, fds);
+% stensfullmat = convertTableToArray(stensfulltbl, fds);
+
+crossfds = {{'coldim', {'coldim1', 'coldim2'}}, ...
+            {'rowdim', {'rowdim1', 'rowdim2'}}, ...
+            {'voigt', {'voigt1', 'voigt2'}}};
+
+col2row2voigt2tbl = crossTable(colrowvoigttbl, colrowvoigttbl, {}, 'crossextend', ...
+                               crossfds);
+
+fds = {'rowdim1', 'coldim1', 'voigt1', ...
+       'rowdim2', 'coldim2', 'voigt2', ...
+      };
+col2row2voigt2mat = convertTableToArray(col2row2voigt2tbl, fds);
+
+Cvoigt = (1 : stenstbl.num)';
+
+% prod = TensorProd();
+% prod.tbl1 = stenstbl;
+% prod.tbl2 = voigttbl;
+% prod.replacefds1 = {'stensind', ''};
+% prod.replacefds2 = {{'voigt', 'voigt2'}, {'coldim', ''}, {'rowdim', ''}};
+% prod.reducefds = {'voigt2'};
+% prod = prod.setup();
+% 
+% Cvoigtmat = SparseMatrix();
+% Cvoigtmat = Cvoigtmat.setFromTensorProd(Cvoigt, prod);
+% Cvoigtmat = Cvoigtmat.matrix;
+
+% C = tblmap(Cvoigt, stenstbl, stensfulltbl, {'stensind'});
+
+C = tblmap(Cvoigt, stenstbl, col2row2voigt2tbl, {'voigt1', 'voigt2'});
 
 return
 
-
 prod = TensorProd();
-prod.tbl1 = nodefacecoltbl;
-prod.tbl2 = cellnodefacecoltbl;
-prod.mergefds = {'faces', 'nodes', 'coldim'};
-prod.prodtbl = cellnodefacecoltbl;
+prod.tbl1 = col2row2tbl;
+prod.tbl2 = colrowtbl;
+prod.replacefds1 = {{'coldim1', 'coldim'}, {'rowdim1', 'rowdim'}};
+prod.replacefds2 = {{'coldim', 'coldim2'}, {'rowdim', 'rowdim2'}};
+prod.reducefds = {'coldim2', 'rowdim2'};
 prod = prod.setup();
 
-tens = SparseTensor();
-val = ones(cellnodefacecoltbl.num, 1);
-tens = tens.setFromTensorProd(val, prod, 'argindex', 2);
+Cmat = SparseMatrix();
+Cmat = Cmat.setFromTensorProd(C, prod);
+Cmat = Cmat.matrix;
 
 
-divgrad_nodeface = multSparseTensor(divgrad_nodeface, tens);
-
-mat = divgrad_nodeface.getMatrix();
