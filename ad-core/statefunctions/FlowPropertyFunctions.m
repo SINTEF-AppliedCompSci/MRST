7bfdc635f1208d5146223644b438ff8831f7b14d
pesunxin <b16020075@s.upc.edu.cn>
2020-01-29 16:01:02 +0100

    Reomved getRegionSurfactant function in FlowPropertyFunction for temporirary to avoid the confliction of master branch

--- ad-core/statefunctions/FlowPropertyFunctions.m
+++ ad-core/statefunctions/FlowPropertyFunctions.m
@@ -80,17 +80,7 @@ classdef FlowPropertyFunctions < StateFunctionGrouping
                 end
             end
         end
-        
-        function sat = getRegionSurfactant(props, model)
-            r = model.rock;
-            sat = ones(model.G.cells.num, 1);
-            if isfield(r, 'regions')
-                if isfield(r.regions, 'surfactant')
-                    sat = r.regions.surfactant;
-                end
-            end
-        end
-        
+              
         function pvt = getRegionPVT(props, model)
             r = model.rock;
             pvt = ones(model.G.cells.num, 1);
