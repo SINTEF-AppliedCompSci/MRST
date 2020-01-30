d51bde1b7e497f4ade596757ea50ae502e57014a
Olav Møyner <olav.moyner@gmail.com>
2020-01-30 22:40:40 +0100

    Update WellComponentTotalFluxDensityMix.m
    Work-in-progress

--- ad-core/statefunctions/facility/WellComponentTotalFluxDensityMix.m
+++ ad-core/statefunctions/facility/WellComponentTotalFluxDensityMix.m
@@ -78,16 +78,57 @@ classdef WellComponentTotalFluxDensityMix < StateFunction
                 end
                 % Total volumetric flux in connections
                 volFlux = model.getProp(state, 'PhaseFlux');
-                volumeFluxTotal = sum(value(volFlux), 2);
-                rhoMix = crossFlowMixtureDensity(massFlux, volumeFluxTotal, injectionMass, map);
-                rhoMixPerf = rhoMix(map.perf2well, :);
+%                 volumeFluxTotal = sum(value(volFlux), 2);
+                
+                resdens = model.ReservoirModel.getProp(state, 'Density');
+                resdens = cellfun(@(x) x(map.cells), resdens, 'unif', false);
+                dens = value(resdens);
+                
+                psum = map.perforationSum;
+                volFluxValue = value(volFlux);
+                injVol = max(volFluxValue, 2);
+                surfaceDens = psum*(dens.*injVol);
+                surfaceDens = surfaceDens./(psum*injVol);
+                
+                vi = zeros(nw, ncomp);
+                override = 0;
+                for ph = 1:ph
+                   wellinj = phaseCompi(:, ph).*surfaceMassRates(:, ph).*[surfaceComposition{:, ph}];
+                   override = override + wellinj;
+                   wellinj = max(wellinj, 0); % Into wellbore
+                   for c = 1:ncomp
 
+                        mi = phase_flux{c, ph};
+                        if isempty(mi)
+                            continue
+                        end
+                        qm = value(min(mi, 0)); % Into wellbore
+                        resprod = psum*(qm./dens(:, ph));
+                        % Volume of each component in phase (mass in phase
+                        % divided by volume)
+                        vi(:, c) = vi(:, c) + resprod + wellinj(:, c)./surfaceDens(:, ph);
+                    end
+                end
+                ci = bsxfun(@rdivide, vi, sum(vi, 2));
+                bad = any(isnan(ci), 2);
+                if any(bad)
+                    override = abs(override);
+                    override = max(override, 1e-12);
+                    override = bsxfun(@rdivide, override, sum(override, 2));
+                    ci(bad, :) = override(bad, :);
+                end
                 vt = zeros(sum(replace), 1);
                 for i = 1:nph
                     vt = vt + volFlux{i}(replace);
                 end
+                
+                massFluxTotal = 0;
+                for i = 1:nph
+                    massFluxTotal = massFluxTotal + volFlux{i}.*resdens{i};
+                end
+                ci_perf = ci(map.perf2well, :);
                 for i = 1:ncomp
-                    v{i}(replace) = vt.*rhoMixPerf(replace, i);
+                    v{i}(replace) = massFluxTotal(replace).*ci_perf(replace, i);
                 end
             end
         end
