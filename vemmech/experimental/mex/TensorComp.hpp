218faef127c1cdb5245f3e135ef9174f572bcf80
Odd <odd.andersen@sintef.no>
2020-02-19 00:16:27 +0100

    Added some experimental mex code

---
+++ vemmech/experimental/mex/TensorComp.hpp
@@ -0,0 +1,39 @@
+#ifndef __TENSOR_COMP_HPP
+#define __TENSOR_COMP_HPP
+
+#include <vector>
+#include <string>
+
+template<typename T>
+class TensorComp
+{
+public:
+  TensorComp(const std::vector<std::string>& indexnames,
+             const std::vector<T>& coefs,
+             const std::vector<int>& ixs) :
+    indexnames_(indexnames), coefs_(coefs), ixs_(ixs) {}
+
+  const std::vector<std::string>& indexNames() const {return indexnames_;}
+  const size_t numIndices() const {return indexnames_.size();}
+  const std::vector<T>& coefs() const {return coefs_;}
+  const std::vector<int>& ixs() const { return ixs_;}
+  
+  
+private:
+  std::vector<std::string> indexnames_;
+  std::vector<T> coefs_;
+  std::vector<int> ixs_;
+  
+}; // end class TensorComp
+
+template<typename T>
+TensorComp<T> contract_components(const std::vector<TensorComp<T>>& comps)
+{
+  return comps[0]; // @@ implement properly
+}
+  
+
+
+
+
+#endif
---
+++ vemmech/experimental/mex/tcontract.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+#include "TensorComp.hpp"
+
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+
+using namespace matlab::data;
+using matlab::mex::ArgumentList;
+
+using namespace std;
+
+class MexFunction : public matlab::mex::Function {
+
+public:
+  MexFunction() : matlabPtr(getEngine()) {}
+  
+  void operator() (ArgumentList outputs, ArgumentList inputs) {
+
+    checkArguments(outputs, inputs);
+    const CellArray cells = inputs[0];
+    vector<TensorComp<double>> comps;
+
+    for (const auto c : cells)
+      comps.emplace_back( TensorComp<double>{extract_indexnames_(c),
+                                             extract_numbers_<double>(c, "coefs"),
+                                             extract_numbers_<int>(c, "ixs")});
+
+    cout << "Number of components read: " << comps.size() << endl;
+
+    const TensorComp<double> result = contract_components(comps);
+
+    // convert TensorComp to a return value to put into 'outputs'
+    ArrayFactory factory;
+
+    StructArray matlab_result = factory.createStructArray({1,1},
+                                                          {"indexnames", "coefs", "ixs"});
+    CellArray indexnames = factory.createCellArray({1, result.numIndices()});
+    for (int i = 0; i != result.numIndices(); ++i)
+      indexnames[i] = factory.createCharArray(result.indexNames()[i]);
+
+    TypedArray<double> coefs =
+      factory.createArray<double>(ArrayDimensions{result.coefs().size(), 1},
+                                  &result.coefs()[0],
+                                  &result.coefs()[0] + result.coefs().size());
+
+    vector<double> ixs_double(result.ixs().begin(), result.ixs().end());
+    TypedArray<double> ixs =
+      factory.createArray<double>(
+         ArrayDimensions{result.coefs().size(), result.numIndices()},
+         &ixs_double[0], &ixs_double[0] + ixs_double.size());
+    
+    matlab_result[0]["indexnames"] = indexnames;
+    matlab_result[0]["coefs"] = coefs;
+    matlab_result[0]["ixs"] = ixs;
+
+    outputs[0] = matlab_result;
+
+  }
+
+  // ----------------------------------------------------------------------------
+  vector<string> extract_indexnames_(const StructArray& comp)  {
+    const CellArray ixnames = comp[0][string("indexnames")];
+
+    vector<string> result;    
+    for (const CharArray name : ixnames) 
+      result.emplace_back(string(name.begin(), name.end()));
+    
+    return result;
+  }
+  
+  // ----------------------------------------------------------------------------
+  template<typename T>
+  vector<T> extract_numbers_(const StructArray& comp, const string field) {
+    const TypedArray<double> arr = comp[0][field];
+
+    vector<T> result;
+    copy(arr.begin(), arr.end(), back_inserter(result));
+    return result;
+  }
+  
+  
+  void checkArguments(ArgumentList outputs, ArgumentList inputs) {
+  
+    if (inputs.empty() || inputs[0].getType() != ArrayType::CELL)
+      raiseError("This function takes a cell array.");
+
+  }
+  
+  void raiseError(const string& str) const
+  {
+    ArrayFactory factory;
+    matlabPtr->feval(u"error",
+                     0,
+                     vector<Array>({ factory.createScalar(str)}));
+  }
+
+private:
+  std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr;
+};
+
+  
