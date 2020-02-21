f72436c369aa52fe9ee74fa5d3038d71b98123c8
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2020-02-21 09:45:07 +0100

    Only output fields that hare present in both states

--- experimental/misc/compareStates.m
+++ experimental/misc/compareStates.m
@@ -1,4 +1,7 @@
-function stateDiff = compareStates(state1, state2)
+function stateDiff = compareStates(state1, state2, varargin)
+
+    opt = struct('relative', false);
+    opt = merge_options(opt, varargin{:});
 
     stateDiff = state1;
     flds = fieldnames(state1);
@@ -8,6 +11,11 @@ function stateDiff = compareStates(state1, state2)
            isnumeric(state2.(v)) && ...
            all(size(state2.(v)) == size(state1.(v)))
            stateDiff.(v) = abs(state1.(v) - state2.(v));
+           if opt.relative
+               stateDiff.(v) = stateDiff.(v)./state1.(v);
+           end
+        else
+            stateDiff = rmfield(stateDiff, v);
         end
     end
        
