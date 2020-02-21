8a8c6473c786501b36b0e29ca4d555113425f021
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2020-02-21 17:08:55 +0100

    Use original upstream if required

--- discretizations/utils/setupOperatorsDG.m
+++ discretizations/utils/setupOperatorsDG.m
@@ -22,7 +22,7 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 
     op = setupOperatorsTPFA(G, rock, varargin{:});
     
-    [~, ~, ~, f] = disc.getCubature(find(disc.internalConn), 'face');
+    [~, ~, ~, f] = disc.getCubature(find(disc.internalConn), 'face'); %#ok
     nf = numel(f);
     
     N = [1:nf; nf+1:2*nf]';
@@ -31,7 +31,9 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
     op.M = M;
     op.faceAvg = @(x) M*x;
     
+%     op.faceUpstr0 = @(flag, x) op.faceUpstr(flag, x);
     op.faceUpstr = @(flag, x) faceUpstr(flag, x, N, [nf, 2*nf]);
+%     op.faceUpstr = @(flag, x) fup(op, flag, x, nf);
     
     op.C0 = op.C;
     C = sparse((1:nf)'.*[1,1], N, ones(nf,1)*[1 -1], nf, 2*nf);
@@ -61,4 +63,12 @@ function dv = div(op, v)
     else
         dv = op.C'*x;
     end
+end
+
+function up = fup(op, flag, x, nf)
+%     if numel(flag) 
+%         up = op.faceUpstr0(flag, x);
+%     else
+        up = op.faceUpstr(flag, x);
+%     end
 end
