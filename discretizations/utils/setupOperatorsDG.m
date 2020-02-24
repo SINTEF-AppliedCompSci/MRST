2c89d0df14b09614d4840005920c24f33c8d5db1
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2020-02-24 10:32:47 +0100

    DG operators: Use regular FV operator for grad and div if size of input vector is less than number of quadrature points.

--- discretizations/utils/setupOperatorsDG.m
+++ discretizations/utils/setupOperatorsDG.m
@@ -33,14 +33,32 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
     
     op.faceUpstr = @(flag, x) faceUpstr(flag, x, N, [nf, 2*nf]);
     
+    op.C0 = op.C;
     C = sparse((1:nf)'.*[1,1], N, ones(nf,1)*[1 -1], nf, 2*nf);
     op.C = C;
-    op.Grad = @(x) -C*x;
     
-    op.Div = @(x) -C*x;
+    op.Grad = @(x) grad(op, x);
+    
+    op.Div = @(v) div(op, v);
     
     op.T = op.T_all(f);
     
     op.velocityInterp = velocityInterpolation(G, 'mimetic');
     
+end
+
+function dx = grad(op, x)
+    if size(value(x),1) < size(op.C,2)
+        dx = -op.C0*x;
+    else
+        dx = -op.C*x;
+    end
+end
+
+function dv = div(op, v)
+    if size(value(v),1) < size(op.C',2)
+        dv = op.C0'*x;
+    else
+        dv = op.C'*x;
+    end
 end
