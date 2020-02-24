7e532acb5e73da2bf0435e58f62a7740880fb1e7
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2020-02-24 10:15:11 +0100

    Update ExplicitFlowStateBuilderDG.m

--- statefunctions/flux/flow_state/ExplicitFlowStateBuilderDG.m
+++ statefunctions/flux/flow_state/ExplicitFlowStateBuilderDG.m
@@ -4,8 +4,24 @@ classdef ExplicitFlowStateBuilderDG < ExplicitFlowStateBuilder & FlowStateBuilde
             % Hybridize state
             % The base state is the implicit. Other functions are then
             % assigned.
-            flowState = build@FlowStateBuilderDG(builder, fd, model, state, state0, dt);
-            flowState = build@ExplicitFlowStateBuilderDG(builder, fd, model, flowState, state0.faceState, dt);
+            flowState = state.faceStateDG;
+            propfn = model.getStateFunctionGroupings();
+            for i = 1:numel(propfn)
+                p = propfn{i};
+                struct_name = p.getStateFunctionContainerName();
+                if isfield(state, struct_name)
+                    flowState = rmfield(flowState, struct_name);
+                end
+            end
+            flowState = model.initStateFunctionContainers(flowState);
+            flowState.type = 'face';
+            if ~isfield(flowState, 'cells')
+                flowState.cells = (1:model.G.cells.num)';
+                flowState.faces = (1:model.G.faces.num)';
+            end
+            
+%             flowState = build@FlowStateBuilderDG(builder, fd, model, state, state0, dt);
+            flowState = build@ExplicitFlowStateBuilder(builder, fd, model, flowState, state0.faceStateDG, dt);
         end
     end
 end
