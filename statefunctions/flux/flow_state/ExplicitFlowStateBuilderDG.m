4f37ca88fcedadfa72c66e23d4cd6e693d9ede0a
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2020-02-24 14:12:22 +0100

    Update ExplicitFlowStateBuilderDG.m

--- statefunctions/flux/flow_state/ExplicitFlowStateBuilderDG.m
+++ statefunctions/flux/flow_state/ExplicitFlowStateBuilderDG.m
@@ -1,11 +1,16 @@
 classdef ExplicitFlowStateBuilderDG < ExplicitFlowStateBuilder & FlowStateBuilderDG
     methods
         function flowState = build(builder, fd, model, state, state0, dt, type)
-            % Hybridize state
-            % The base state is the implicit. Other functions are then
-            % assigned.
+            % Hybridize state. The base state is the implicit. Other
+            % functions are then assigned.
             flowState = build@FlowStateBuilderDG(builder, fd, model, state, state0, dt, type);
-            flowState = build@ExplicitFlowStateBuilder(builder, fd, model, flowState, state0.faceStateDG, dt);
+            switch type
+                case 'face'
+                    flowState0 = state0.faceStateDG;
+                case 'cell'
+                    flowState0 = state0.faceStateDG;
+            end
+            flowState = build@ExplicitFlowStateBuilder(builder, fd, model, flowState, flowState0, dt);
         end
     end
 end
