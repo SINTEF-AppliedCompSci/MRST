3729fdc07bcd0ed8288c1113d61026dd06058bca
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2020-02-24 10:15:55 +0100

    Create dgExampleTrailing.m

--- examples/dgExampleTrailing.m
+++ examples/dgExampleTrailing.m
@@ -76,4 +76,12 @@ tmodel3.parentModel.FluxDiscretization = flux;
 plot(G.cells.centroids(:,1),stSPUe{end}.s(:,1),'.','MarkerSize',20);
 legend('exact solution','SPU implicit','dG(1) implicit','SPU explicit');
 %% dG explicit
-% TBA
+tmodelDGe = tmodelDGi;
+flux      = tmodelDGe.parentModel.FluxDiscretization;
+tmodelDGe.parentModel.outputFluxes = true;
+fb        = ExplicitFlowStateBuilderDG();
+%fb    = AdaptiveImplicitFlowStateBuilder();
+flux      = flux.setFlowStateBuilder(fb);
+tmodelDGe.parentModel.FluxDiscretization = flux;
+[~, stDGe, repDGe] = simulateScheduleAD(state0, tmodelDGe, schedule);
+plot(G.cells.centroids(:,1),stDGi{end}.s(:,1),'.','MarkerSize',20);
