dcfdffbdf805d80adef1827890cd07e8e5b5ba0c
Xavier Raynaud <xav.raynaud@gmail.com>
2020-03-03 17:40:14 +0100

    fixes linear ntpfa

--- computeNTPFATrans.m
+++ computeNTPFATrans.m
@@ -1,4 +1,4 @@
-function [M, M_noflow] = computeNTPFATrans(G, rock)
+function [fluxop, fluxop_neumann, A] = computeNTPFATrans(G, rock)
     
     bc.face = [];
     bc_nfvm = convertBC2FlowNTPFA(G, bc);
@@ -6,7 +6,6 @@ function [M, M_noflow] = computeNTPFATrans(G, rock)
     interpFace = findHAP(G, rock, bc_nfvm);
     OSflux = findOSflux(G, rock, bc_nfvm, interpFace);
     
-
     nf = G.faces.num;
     nc = G.cells.num;
 
@@ -21,15 +20,16 @@ function [M, M_noflow] = computeNTPFATrans(G, rock)
         cells2{j} = [];
         vals{j}  = [];
         for i = 1 : nf
-            locneig = G.faces.neighbors(i, 1);
+            locneig = G.faces.neighbors(i, j);
             f = OSflux{i, j};
             if  ~isempty(f)
-                
                 ncells   = size(f, 1) - 1;
                 locfaces = repmat(i, ncells, 1);
                 loccells2 = repmat(locneig, ncells, 1);
                 loccells1 = f(1 : end - 1, 1); 
                 locvals   = f(1 : end - 1, 2);
+                
+                locvals(loccells2 == loccells1) = - locvals(loccells2 == loccells1);
                 
                 faces{j}  = vertcat(faces{j}, locfaces);
                 cells1{j} = vertcat(cells1{j}, loccells1);
@@ -46,31 +46,40 @@ function [M, M_noflow] = computeNTPFATrans(G, rock)
     cell2facetbl.faces = [faces{1}; faces{2}];
     cell2facetbl = IndexTable(cell2facetbl);
 
-    vals = [vals{1}; vals{2}];
+    vals = [0.5*vals{1}; -0.5*vals{2}];
 
     celltbl.cells = (1 : nc)';
     celltbl = IndexTable(celltbl);
 
     facetbl.faces = (1 : nf)';
     facetbl = IndexTable(facetbl);
-
     
+    multicellfacetbl = projTable(cell2facetbl, {'cells1', 'faces'});
+    
+    map = TensorMap();
+    map.fromTbl = cell2facetbl;
+    map.toTbl = multicellfacetbl;
+    map.mergefds = {'cells1', 'faces'};
+    map = map.setup();
+    
+    vals = map.eval(vals);
+
     prod = TensorProd();
-    prod.tbl1 = cell2facetbl;
+    prod.tbl1 = multicellfacetbl;
     prod.tbl2 = celltbl;
     prod.tbl3 = facetbl;
-    prod.replacefds2 = {{'cells', 'cells1'}};
-    prod.reducefds = {'cells1'};
-
+    prod.replacefds1 = {{'cells1', 'cells'}};
+    prod.reducefds = {'cells'};
     prod = prod.setup();
-
-    tens = SparseTensor();
-    tens = tens.setFromTensorProd(vals, prod);
     
-    M = tens.getMatrix();
+    fluxT = SparseTensor();
+    fluxT = fluxT.setFromTensorProd(vals, prod);
+    
+    fluxop = fluxT.getMatrix();
 
-    isBoundary = any(G.faces.neighbors == 0, 2);
-    intfacetbl.faces = find(~isBoundary);
+    N=G.faces.neighbors;
+    intInx = all(N ~= 0, 2);
+    intfacetbl.faces = find(intInx);
     intfacetbl = IndexTable(intfacetbl);
 
     map = TensorMap();
@@ -79,11 +88,35 @@ function [M, M_noflow] = computeNTPFATrans(G, rock)
     map.mergefds = {'faces'};
     map = map.setup();
     
-    tens2 = SparseTensor();
-    tens2 = tens2.setFromTensorMap(map);
+    intT = SparseTensor();
+    intT = intT.setFromTensorMap(map);
     
-    tens = tens2*tens;
+    fluxT_neumann = intT*fluxT;
 
-    M_noflow = tens.getMatrix();
+    fluxop_neumann = fluxT_neumann.getMatrix();
+    
+    cellfacetbl.cells = rldecode((1 : nc)', diff(G.cells.facePos)); 
+    cellfacetbl.faces = G.cells.faces(:, 1);
+    cellfacetbl = IndexTable(cellfacetbl);
+    
+    
+    cno = cellfacetbl.get('cells');
+    fno = cellfacetbl.get('faces');
+    sgn = 2*(cno == G.faces.neighbors(fno, 1)) - 1;
+    
+    prod = TensorProd();
+    prod.tbl1 = cellfacetbl;
+    prod.tbl2 = facetbl;
+    prod.tbl3 = celltbl;
+    prod.reducefds = {'faces'};
+    prod = prod.setup();
+    
+    divT = SparseTensor();
+    divT = divT.setFromTensorProd(sgn, prod);
+    
+    AT = divT*fluxT;
+    
+    A = AT.getMatrix();
+    
     
 end
