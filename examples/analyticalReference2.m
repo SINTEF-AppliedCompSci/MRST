9d0645049b49f7aac1707c2c5c5b83a5c4ff8a3b
Xavier Raynaud <xav.raynaud@gmail.com>
2020-03-05 13:46:11 +0100

    more work on analytical reference

--- examples/analyticalReference2.m
+++ examples/analyticalReference2.m
@@ -1,131 +1,62 @@
-mrstModule add ad-core
-clear all
-
-N = 10;
-
-xtbl.xind = (1 : N)';
-xtbl = IndexTable(xtbl);
-
-ytbl.yind = (1 : N)';
-ytbl = IndexTable(ytbl);
-
-ztbl.zind = (1 : N)';
-ztbl = IndexTable(ztbl);
-
-xyztbl = crossTable(xtbl, ytbl, {}, 'optpureproduct', true);
-xyztbl = crossTable(xyztbl, ztbl, {}, 'optpureproduct', true);
-
-x = linspace(0, 1, N)';
-y = linspace(0, 1, N)';
-z = linspace(0, 1, N)';
-
-map = TensorMap();
-map.fromTbl = xtbl;
-map.toTbl = xyztbl;
-map.mergefds =  {'xind'};
-map = map.setup();
-x = map.eval(x);
-
-map = TensorMap();
-map.fromTbl = ytbl;
-map.toTbl = xyztbl;
-map.mergefds =  {'yind'};
-map = map.setup();
-y = map.eval(y);
-
-map = TensorMap();
-map.fromTbl = ztbl;
-map.toTbl = xyztbl;
-map.mergefds =  {'zind'};
-map = map.setup();
-z = map.eval(z);
-
-
-u1 = @(x,y,z) ((x - 0.5).^2).*(y - 0.5).^2.*(z - 0.5).^2;
-u2 = @(x,y,z) ((x - 0.5).^2).*(y - 0.5).^2.*(z - 0.5).^2;
-u3 = @(x,y,z) -2/3*((x - 0.5).*(y - 0.5).^2 + (x - 0.5).^2.*(y - 0.5)).*(z - 0.5).^3;
-
-
-f1 = @(x,y,z) (0.0625*(2*x - 1)**2*(2*z - 1)**2 + (2*x - 1)*(1.0*y - 0.5)*(2*z ...
-                                                  - 1)**2/4 + 0.125*(2*x - ...
-                                                  1)*(2*y - 1)**2*(2*z - 1) - ...
-               (2*x - 1)*(2*y - 1)*(3.0*z - 1.5)*(x + y - 1)/6 + 0.125*(2*y - ...
-                                                  1)**2*(2*z - 1)**2);
-
-f2 = @(x,y,z)((1.0*x - 0.5)*(2*y - 1)*(2*z - 1)**2/4 + 0.125*(2*x - 1)**2*(2*y ...
-                                                  - 1)*(2*z - 1) + 0.125*(2*x ...
-                                                  - 1)**2*(2*z - 1)**2 - (2*x ...
-                                                  - 1)*(2*y - 1)*(3.0*z - ...
-                                                  1.5)*(x + y - 1)/6 + 0.0625*(2*y ...
-                                                  - 1)**2*(2*z - 1)**2);
-
-f3 =  @(x,y,z) (0.0625*(2*x - 1)**2*(2*z - 1)**2 - (2*x - 1)*(2*y - 1)*(6.0*z ...
-                                                  - 3.0)*(x + y - 1)/6 + (2*x ...
-                                                  - 1)*(2*z - 1)**2*(-1.5*x - ...
-                                                  3.0*y + 2.25)/12 + 0.0625*(2*y ...
-                                                  - 1)**2*(2*z - 1)**2 + (2*y ...
-                                                  - 1)*(2*z - 1)**2*(-3.0*x - ...
-                                                  1.5*y + 2.25)/12);
-
-u1d1 = @(x,y,z) (2*(x - 0.5).*(y - 0.5).^2.*(z - 0.5).^2);
-u1d2 = @(x,y,z) (2*(x - 0.5).^2.*(y - 0.5).*(z - 0.5).^2);
-u1d3 = @(x,y,z) (2*(x - 0.5).^2.*(y - 0.5).^2.*(z - 0.5));
-u2d1 = @(x,y,z) (2*(x - 0.5).*(y - 0.5).^2.*(z - 0.5).^2);
-u2d2 = @(x,y,z) (2*(x - 0.5).^2.*(y - 0.5).*(z - 0.5).^2);
-u2d3 = @(x,y,z) (2*(x - 0.5).^2.*(y - 0.5).^2.*(z - 0.5));
-u3d1 = @(x,y,z) (-2/3*(z - 0.5).^3.*((y - 0.5).^2 + 2*(x - 0.5).*(y - 0.5)));
-u3d2 = @(x,y,z) (-2/3*(z - 0.5).^3.*(2*(x - 0.5).*(y - 0.5) + (x - 0.5).^2));
-u3d3 = @(x,y,z) (-2*(z - 0.5).^2.*((x - 0.5).*(y - 0.5).^2 + (x - 0.5).^2.*(y - 0.5)));
-
-s11 = @(x, y, z) (2*u1d1(x, y, z));
-s22 = @(x, y, z) (2*u2d2(x, y, z));
-s33 = @(x, y, z) (2*u3d3(x, y, z));
-s12 = @(x, y, z) (u1d2(x, y, z) + u2d1(x, y, z));
-s13 = @(x, y, z) (u1d3(x, y, z) + u3d1(x, y, z));
-s23 = @(x, y, z) (u2d3(x, y, z) + u3d2(x, y, z));
-s21 = s12;
-s31 = s13;
-s32 = s23;
-
-
-dotest = false;
-if dotest
+function [u, f] = analyticalReference2(d)
     
-    [xAD, yAD, zAD] = initVariablesADI(x, y, z);
+% d = spatial dimension
+% Reference solution from paper
+%  title={Finite volume methods for elasticity with weak symmetry},
+%  author={Keilegavlen, Eirik and Nordbotten, Jan Martin},
+%  journal={International Journal for Numerical Methods in Engineering},
+%  volume={112},
+%  number={8},
+%  pages={939--962},
+%  year={2017},
+%  publisher={Wiley Online Library}
 
-    u1AD = u1(xAD, yAD, zAD);
-    u2AD = u2(xAD, yAD, zAD);
-    u3AD = u3(xAD, yAD, zAD);
 
-    u1dxAD = diag(u1AD.jac{1});
-    u1dyAD = diag(u1AD.jac{2});
-    u1dzAD = diag(u1AD.jac{3});
-    u2dxAD = diag(u2AD.jac{1});
-    u2dyAD = diag(u2AD.jac{2});
-    u2dzAD = diag(u2AD.jac{3});
-    u3dxAD = diag(u3AD.jac{1});
-    u3dyAD = diag(u3AD.jac{2});
-    u3dzAD = diag(u3AD.jac{3});
 
-    u1dxnum = u1d1(x, y, z);
-    u1dynum = u1d2(x, y, z);
-    u1dznum = u1d3(x, y, z);
-    u2dxnum = u2d1(x, y, z);
-    u2dynum = u2d2(x, y, z);
-    u2dznum = u2d3(x, y, z);
-    u3dxnum = u3d1(x, y, z);
-    u3dynum = u3d2(x, y, z);
-    u3dznum = u3d3(x, y, z);
+    % displacement
+    u = cell(d, 1);
+    % force
+    f = cell(d, 1);
 
-    max(abs(u1dxnum - u1dxAD))
-    max(abs(u1dynum - u1dyAD))
-    max(abs(u1dznum - u1dzAD))
-    max(abs(u2dxnum - u2dxAD))
-    max(abs(u2dynum - u2dyAD))
-    max(abs(u2dznum - u2dzAD))
-    max(abs(u3dxnum - u3dxAD))
-    max(abs(u3dynum - u3dyAD))
-    max(abs(u3dznum - u3dzAD))
-    max(abs(u3dznum - u3dzAD))
+    switch d 
+      case 2
+        
+        u{1} = @(x, y) (x - 0.5).^2.*(y - 0.5).^2;
+        u{2} = @(x, y) -2/3*(x - 0.5).*(y - 0.5).^3;
+        
+        f{1} = @(x, y) 0.25*(2*x - 1).^2 + 0.25*(2*y - 1).^2;
+        f{2} = @(x, y) -2.0*x.*y + 1.0*x + 1.0*y - 0.5;
+        
+      case 3
 
+        u{1} = @(x,y,z) ((x - 0.5).^2).*(y - 0.5).^2.*(z - 0.5).^2;
+        u{2} = @(x,y,z) ((x - 0.5).^2).*(y - 0.5).^2.*(z - 0.5).^2;
+        u{3} = @(x,y,z) -2/3*((x - 0.5).*(y - 0.5).^2 + (x - 0.5).^2.*(y - 0.5)).*(z - 0.5).^3;
+
+        f{1} = @(x,y,z) (0.0625*(2*x - 1).^2.*(2*z - 1).^2 + 1/4*(2*x - 1).*(y ...
+                                                          - 0.5).*(2*z - 1).^2 ...
+                         + 0.125*(2*x - 1).*(2*y - 1).^2.*(2*z - 1) - 1/6*(2*x ...
+                                                          - 1).*(2*y - 1).*(3.0*z ...
+                                                          - 1.5).*(x + y - 1) ...
+                         + 0.125*(2*y - 1).^2.*(2*z - 1).^2);
+
+        f{2} = @(x,y,z)((x - 0.5).*(2*y - 1).*(2*z - 1).^2/4 + 0.125*(2*x - ...
+                                                          1).^2.*(2*y - 1).*(2*z ...
+                                                          - 1) + 0.125*(2*x - ...
+                                                          1).^2.*(2*z - 1).^2 ...
+                        - (2*x - 1).*(2*y - 1).*(3.0*z - 1.5).*(x + y - 1)/6 ...
+                        + 0.0625*(2*y - 1).^2.*(2*z - 1).^2);
+
+        f{3} =  @(x,y,z) (0.0625*(2*x - 1).^2.*(2*z - 1).^2 - (2*x - 1).*(2*y ...
+                                                          - 1).*(6.0*z - ...
+                                                          3.0).*(x + y - 1)/6 ...
+                          + (2*x - 1).*(2*z - 1).^2.*(-1.5*x - 3.0*y + 2.25)/12 ...
+                          + 0.0625*(2*y - 1).^2.*(2*z - 1).^2 + (2*y - 1).*(2*z ...
+                                                          - 1).^2.*(-3.0*x - ...
+                                                          1.5*y + 2.25)/12); ...
+                
+      otherwise
+        error('d not recognized')
+
+    end
 end
--- examples/computederivative.py
+++ examples/computederivative.py
@@ -7,38 +7,44 @@ dervar = [x, y, z]
 
 c1 = sympy.Rational(1, 2)
 c2 = sympy.Rational(2, 3)
-u1 = ((x - c1)**2)*(y - c1)**2*(z - c1)**2
-u2 = ((x - c1)**2)*(y - c1)**2*(z - c1)**2
-u3 = -c2*((x - c1)*(y - c1)**2 + (x - c1)**2*(y - c1))*(z - c1)**3
 
-uvec = list([u1, u2, u3])
+d = 2
+if d == 3:
+    u1 = ((x - c1)**2)*(y - c1)**2*(z - c1)**2
+    u2 = ((x - c1)**2)*(y - c1)**2*(z - c1)**2
+    u3 = -c2*((x - c1)*(y - c1)**2 + (x - c1)**2*(y - c1))*(z - c1)**3
+    uvec = list([u1, u2, u3])
+elif d == 2:
+    u1 = (x - c1)**2*(y - c1)**2
+    u2 = -c2*(x - c1)*(y - c1)**3
+    uvec = list([u1, u2])
+
 
 gradu = []
 
 # gradu[i][j] gets the derivative of the i-th component with respect to the j-th
 # variable
-for i in range(3):
+for i in range(d):
     g = []
-    g.append(sympy.diff(uvec[0], x))
-    g.append(sympy.diff(uvec[1], y))
-    g.append(sympy.diff(uvec[2], z))
+    for j in range(d):
+        g.append(sympy.diff(uvec[i], dervar[j]))
     gradu.append(g)
 
 # stress = 2*mu*(symmetric gradient) + lambda*trace(symmetric gradient)*I
 # The example has been designed such that the trace equal zero.
 stress = []
-for i in range(3):
+for i in range(d):
     scomp = []
-    for j in range(3):
+    for j in range(d):
         s = 0.5*(gradu[i][j] + gradu[j][i])
         scomp.append(s)
     stress.append(scomp)
 
 # We compute the divergence
 div = []
-for j in range(3):
+for j in range(d):
     divcomp = 0
-    for i in range(3):
+    for i in range(d):
         divcomp = divcomp + sympy.diff(stress[i][j], dervar[i])
     divcomp = sympy.simplify(divcomp)
     div.append(divcomp)
