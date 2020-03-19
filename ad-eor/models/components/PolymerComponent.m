c592b1109e16dd3eec7315fd1a290a179fc579d5
Olav Møyner <olav.moyner@gmail.com>
2020-03-19 19:07:30 +0100

    ad-eor: Add rough draft of a generic surfactant+polymer model

---
+++ ad-eor/models/components/PolymerComponent.m
@@ -0,0 +1,98 @@
+classdef PolymerComponent < GenericComponent
+    properties
+
+    end
+    
+    methods
+        function c = PolymerComponent()
+            c@GenericComponent('polymer');
+        end
+        
+        
+        function c = getComponentDensity(component, model, state, varargin)
+            cp = model.getProp(state, 'polymer');
+            rho = model.getProps(state, 'Density');
+            nph = numel(rho);
+            c = cell(1, nph);
+            c{1} = cp.*rho{1};
+        end
+        
+        function c = getComponentMass(component, model, state, varargin)
+            f = model.fluid;
+            cp = model.getProp(state, 'polymer');
+            rho = model.getProps(state, 'Density');
+            pv = model.getProp(state, 'PoreVolume');
+            
+            nph = numel(rho);
+            c = cell(1, nph);
+            
+            rhoW = rho{1};
+            sw = model.getProp(state, 'sW');
+            % In mobile water 
+            acc = (1-f.dps).*sw.*cp.*rhoW;
+            % Adsorbed part
+            poro = model.rock.poro;
+            ads = model.getProp(state, 'PolymerAdsorption');
+            adsorbed = f.rhoWS.*f.rhoR.*((1-poro)./poro).*ads;
+            
+            c{1} = pv.*(adsorbed + acc);
+        end
+        
+        function cmob = getComponentMobility(component, model, state, varargin)
+            mass = component.getComponentDensity(model, state, varargin{:});
+            [mob, rho, c] = model.getProps(state, 'Mobility', 'Density', 'polymer');
+            wIx = 1;
+            mobW = mob{wIx};
+            rhoW = rho{wIx};
+            fluid  = model.fluid;
+            mixpar = fluid.mixPar;
+            cpbar  = c/fluid.cpmax;
+            a  = fluid.muWMult(fluid.cpmax).^(1-mixpar);
+            mobP = c.*rhoW.*mobW./(a+(1-a)*cpbar);
+            
+            nphase = numel(mass);
+            cmob = cell(1, nphase);
+            cmob{wIx} = mobP;
+        end
+        
+        function c = getPhaseCompositionSurface(component, model, state, pressure, temperature)
+            % Polymer does not enter into any phase stream
+            nph = model.getNumberOfPhases();
+            c = cell(nph, 1);
+        end
+
+        function c = getPhaseComponentFractionInjection(component, model, state, force)
+            c = cell(model.getNumberOfPhases(), 1);
+            if isfield(force, 'compi')
+                comp_i = vertcat(force.compi);
+            else
+                comp_i = vertcat(force.sat);
+            end
+            wIx = 1;
+            cp = vertcat(force.cp);
+            ci = comp_i(:, wIx).*cp;
+            if any(ci ~= 0)
+                c{wIx} = ci;
+            end
+        end
+    end
+end
+
+%{
+Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
---
+++ ad-eor/models/experimental/GenericSurfactantPolymerModel.m
@@ -0,0 +1,131 @@
+classdef GenericSurfactantPolymerModel < ThreePhaseSurfactantPolymerModel & ExtendedReservoirModel
+    properties
+        
+    end
+    
+    methods
+        function model = GenericSurfactantPolymerModel(G, rock, fluid, varargin)
+            model = model@ThreePhaseSurfactantPolymerModel(G, rock, fluid, varargin{:});
+            model.OutputStateFunctions = {'ComponentTotalMass'};
+        end
+
+        function [problem, state] = getEquations(model, state0, state, dt, drivingForces, varargin)
+            [problem, state] = getEquations@PhysicalModel(model, state0, state, dt, drivingForces, varargin{:});
+        end
+
+        function [vars, names, origin] = getPrimaryVariables(model, state)
+            [vars, names, origin] = getPrimaryVariables@ThreePhaseBlackOilModel(model, state);
+            current = class(model);
+            
+            eor_names = {};
+            if model.polymer
+                eor_names{end+1} = 'polymer';
+            end
+            
+            if model.surfactant
+                eor_names{end+1} = 'surfactant';
+            end
+            [eor_vars, eor_origin] = deal(cell(1, numel(eor_names)));
+            [eor_vars{:}] = model.getProp(state, eor_names{:});
+            [eor_origin{:}] = deal(current);
+            
+            isRes = strcmp(origin, current);
+            
+            vars = [vars(isRes), eor_vars, vars(~isRes)];
+            names = [names(isRes), eor_names, names(~isRes)];
+            origin = [origin(isRes), eor_origin, origin(~isRes)];
+        end
+        
+        function [eqs, names, types, state] = getModelEquations(model, state0, state, dt, drivingForces)
+            [eqs, flux, names, types] = model.FluxDiscretization.componentConservationEquations(model, state, state0, dt);
+            src = model.FacilityModel.getComponentSources(state);
+
+            % Assemble equations and add in sources
+            for i = 1:numel(eqs)
+                if ~isempty(src.cells)
+                    eqs{i}(src.cells) = eqs{i}(src.cells) - src.value{i};
+                end
+                eqs{i} = model.operators.AccDiv(eqs{i}, flux{i});
+            end
+            % Get facility equations
+            [weqs, wnames, wtypes, state] = model.FacilityModel.getModelEquations(state0, state, dt, drivingForces);
+            eqs = [eqs, weqs];
+            names = [names, wnames];
+            types = [types, wtypes];
+        end
+
+        function names = getComponentNames(model)
+            names = cellfun(@(x) x.name, model.Components, 'UniformOutput', false);
+        end
+
+        function [state, report] = updateState(model, state, problem, dx, forces)
+            [state, report] = updateState@ThreePhaseSurfactantPolymerModel(model, state, problem, dx, forces);
+            if ~isempty(model.FacilityModel)
+                state = model.FacilityModel.applyWellLimits(state);
+            end
+        end
+
+        function model = validateModel(model, varargin)
+            % Validate model.
+            %
+            % SEE ALSO:
+            %   :meth:`ad_core.models.PhysicalModel.validateModel`
+            if isempty(model.FacilityModel) || ~isa(model.FacilityModel, 'ExtendedFacilityModel')
+                model.FacilityModel = ExtendedFacilityModel(model);
+            end
+            if isempty(model.Components)
+                nph = model.getNumberOfPhases();
+                names = model.getPhaseNames();
+                disgas = model.disgas;
+                vapoil = model.vapoil;
+                hasPoly = model.polymer;
+                hasSurf = model.surfactant;
+                model.Components = cell(1, nph+hasPoly+hasSurf);
+                for ph = 1:nph
+                    switch names(ph)
+                        case 'W'
+                            c = ImmiscibleComponent('water', ph);
+                        case 'O'
+                            if disgas || vapoil
+                                c = OilComponent('oil', ph, disgas, vapoil);
+                            else
+                                c = ImmiscibleComponent('oil', ph);
+                            end
+                        case 'G'
+                            if disgas || vapoil
+                                c = GasComponent('gas', ph, disgas, vapoil);
+                            else
+                                c = ImmiscibleComponent('gas', ph);
+                            end
+                        otherwise
+                            error('Unknown phase');
+                    end
+                    model.Components{ph} = c;
+                end
+                index = nph + 1;
+                if hasPoly
+                    c = PolymerComponent();
+                    model.Components{index} = c;
+                    index = index + 1;
+                end
+                if hasSurf
+                    % Not implemented - fill me in
+                    c = SurfactantComponent();
+                    model.Components{index} = c;
+                end
+            end
+            model = validateModel@ThreePhaseSurfactantPolymerModel(model, varargin{:});
+        end
+
+        function [state, report] = updateAfterConvergence(model, state0, state, dt, drivingForces)
+            [state, report] = updateAfterConvergence@ThreePhaseSurfactantPolymerModel(model, state0, state, dt, drivingForces);
+            if model.outputFluxes
+                state_flow = model.FluxDiscretization.buildFlowState(model, state, state0, dt);
+                f = model.getProp(state_flow, 'PhaseFlux');
+                nph = numel(f);
+                state.flux = zeros(model.G.faces.num, nph);
+                state.flux(model.operators.internalConn, :) = [f{:}];
+            end
+        end
+    end
+end
