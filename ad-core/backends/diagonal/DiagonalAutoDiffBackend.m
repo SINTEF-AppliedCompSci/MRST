4f4d3a169e25fc61c1f1bb944f997f911c4cfc9f
Olav Møyner <olav.moyner@gmail.com>
2020-04-13 18:40:22 +0200

    AMGCL/Diagonal-AD: Move block system and Schur complement to backend

--- ad-core/backends/diagonal/DiagonalAutoDiffBackend.m
+++ ad-core/backends/diagonal/DiagonalAutoDiffBackend.m
@@ -101,9 +101,153 @@ classdef DiagonalAutoDiffBackend < AutoDiffBackend
         function v = convertToAD(backend, v, sample)
            v = double2GenericAD(v, sample);
         end
+        
+        function [A_cc, b_c, A_nn, b_n, A_cn, A_nc] = getBlockSystemCSR(backend, problem, bz)
+            if nargin < 3 || bz == 0
+                bz = problem.countOfType('cell');
+            end
+            eqs = problem.equations;
+            ne = numel(eqs);
+            nj = numel(eqs{1}.jac);
+            c_sub = 1:bz; % Assuming cell equations come first
+            cell_eq = eqs(c_sub);
+            J = cellfun(@(x) x.jac{1}, cell_eq, 'UniformOutput', false);
+            
+            % First, get the matrix
+            opt = J{1}.divergenceOptions;
+            prelim = opt.mex;
+            
+            acc = cellfun(@(x) x.accumulation.diagonal, J, 'UniformOutput', false);
+            flux = cellfun(@(x) x.flux.diagonal, J, 'UniformOutput', false);
+            bad = cellfun(@isempty, flux);
+            if any(bad)
+                [flux{bad}] = deal(zeros(2*bz, size(model.operators.N, 1)));
+            end
+            
+            [colNo, rowPtr, values, n, m] = mexDiscreteDivergenceBlockJac(acc, flux, opt.N,...
+            prelim.facePos, prelim.faces, prelim.cells, prelim.cellIndex, bz, backend.rowMajor);
+            % Get the right hand side
+            b_c = getRHS(cell_eq);
+            assert(n == m)
+            A_cc = struct('col_no', colNo, 'row_ptr', rowPtr, 'val', values, 'n', n, 'block_size', bz, 'mapping', prelim);
+            if nargout > 2
+                if ne == bz
+                    % No extra equations, but they were requested. Just
+                    % output empty arrays.
+                    [A_nn, b_n, A_cn, A_nc] = deal([]);
+                else
+                    n_sub = (bz+1):ne;
+                    % Assume system:
+                    % A = [A_cc, A_cn
+                    %      A_nc, A_nn]
+                    % where c are cell equations/variables and n are non-cell.
+                    n_eq = eqs(n_sub);
+
+                    if nj > 1
+                        A_nn = getMatrix(n_eq, 2:nj);
+                        if nargout > 3
+                            b_n = getRHS(n_eq);
+                            if nargout > 4
+                                A_cn = getMatrix(cell_eq, 2:nj);
+                                if nargout > 5
+                                    A_nc = getMatrix(n_eq, 1);
+                                end
+                            end
+                        end
+                    else
+                        % "Slim" matrix, just two blocks
+                        [A_nn, b_n, A_cn] = deal([]);
+                        if nargout > 5
+                            A_nc = getMatrix(n_eq, 1);
+                        end
+                    end
+                end
+            end
+        end
+        
+        function [A, schur_diag, insert] = applySchurComplementBlockSystemCSR(backend, A, A_nn, A_nc, A_cn, schur_type, w)
+            if nargin < 7
+                w = 1;
+            end
+            if nargin < 6
+                doRow = true;
+                doSum = false;
+            else
+                switch schur_type
+                    case 'diagonal'
+                        doRow = true;
+                        doSum = false;
+                    case 'rowsum'
+                        doRow = true;
+                        doSum = true;
+                    case 'colsum'
+                        doRow = false;
+                        doSum = true;
+                    otherwise
+                        error('% is not supported.', schur_type);
+                end
+            end
+            bz = A.block_size;
+            n = size(A_nc, 2)/bz;
+            if isnumeric(A_nn)
+                n_to_c = A_nn\A_nc;
+            else
+                n_to_c = A_nn(A_nc);
+            end
+            fill = A_cn*n_to_c;
+            [row, col, vals] = find(fill);
+            cell_row = mod(row-1, n) + 1;
+            cell_col = mod(col-1, n) + 1;
+            keep = cell_row == cell_col;
+            ix = unique(cell_row(keep));
+            if ~doSum
+                row = row(keep);
+                col = col(keep);
+                vals = vals(keep);
+            end
+            schur_diag = zeros(bz*bz, numel(ix));
+
+            % Block
+            block_row = floor((row-1)/n) + 1;
+            block_col = floor((col-1)/n) + 1;
+            % Create block values to insert
+            for eqNo = 1:bz
+                for derNo = 1:bz
+                    act = block_row == eqNo & block_col == derNo;
+                    if doRow
+                        ii = row(act) - (eqNo-1)*n;
+                    else
+                        ii = col(act) - (derNo-1)*n;
+                    end
+                    rowsum = accumarray(ii, vals(act), [n, 1]);
+                    sub = (derNo-1)*bz + eqNo;
+                    schur_diag(sub, :) = schur_diag(sub, :) + rowsum(ix)';
+                end
+            end
+            prelim = A.mapping;
+            d_offset = prelim.cellIndex(ix);
+            insert = prelim.facePos(ix) + d_offset + ix;
+            if w ~= 0
+                A.val(:, insert) = A.val(:, insert) - w.*schur_diag;
+            end
+        end
     end
 end
 
+
+
+function A = getMatrix(eqs, j_ix)   
+    A = cell(1, numel(eqs));
+    for i = 1:numel(eqs)
+        A{i} = horzcat(eqs{i}.jac{j_ix});
+    end
+    A = vertcat(A{:});
+end
+
+function b = getRHS(eqs)
+    values = cellfun(@(x) -value(x), eqs, 'UniformOutput', false);
+    b = vertcat(values{:});
+end
 %{
 Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
 
--- ad-core/solvers/AMGCLSolverBlockAD.m
+++ ad-core/solvers/AMGCLSolverBlockAD.m
@@ -33,37 +33,15 @@ classdef AMGCLSolverBlockAD < AMGCLSolverAD
         
         function [dx, result, report] = solveLinearProblem(solver, problem, model)
             timer = tic();
-            backend = model.AutoDiffBackend;
-            eqs = problem.equations;
             bz = solver.amgcl_setup.block_size;
             if bz == 0
                 % Defaulted - we estimate
                 bz = problem.countOfType('cell');
                 solver.amgcl_setup.block_size = bz;
             end
-            ne = numel(eqs);
-            nj = numel(eqs{1}.jac);
-            needsReduction = ne > bz;
-            c_sub = 1:bz; % Assuming cell equations come first
-            cell_eq = eqs(c_sub);
-            J = cellfun(@(x) x.jac{1}, cell_eq, 'UniformOutput', false);
-            
-            % First, get the matrix
-            opt = J{1}.divergenceOptions;
-            prelim = opt.mex;
-            
-            acc = cellfun(@(x) x.accumulation.diagonal, J, 'UniformOutput', false);
-            flux = cellfun(@(x) x.flux.diagonal, J, 'UniformOutput', false);
-            bad = cellfun(@isempty, flux);
-            if any(bad)
-                [flux{bad}] = deal(zeros(2*bz, size(model.operators.N, 1)));
-            end
-            
-            [colNo, rowPtr, values, n, m] = mexDiscreteDivergenceBlockJac(acc, flux, opt.N,...
-            prelim.facePos, prelim.faces, prelim.cells, prelim.cellIndex, bz, backend.rowMajor);
-            % Get the right hand side
-            b = getRHS(cell_eq);
-            assert(n == m)
+            backend = model.AutoDiffBackend;
+            [A, b, A_nn, b_n, A_cn, A_nc] = backend.getBlockSystemCSR(problem, bz);
+            needsReduction = ~isempty(A_nn);
             if needsReduction
                 % We have non-cell equations. We try to do something about
                 % it.
@@ -74,38 +52,19 @@ classdef AMGCLSolverBlockAD < AMGCLSolverAD
                 solveSchur = strcmpi(s, 'schur');
                 
                 assert(solvePre || solvePost || solveSchur);
-                n_sub = (bz+1):ne;
-                % Assume system:
-                % A = [A_cc, A_cn
-                %      A_nc, A_nn]
-                % where c are cell equations/variables and n are non-cell.
-                n_eq = eqs(n_sub);
-                A_cn = getMatrix(cell_eq, 2:nj);
-                A_nn = getMatrix(n_eq, 2:nj);
-                if solvePost || solveSchur
-                    A_nc = getMatrix(n_eq, 1);
-                end
-                b_n = getRHS(n_eq);
                 % Factor remaining equations
                 [L, U] = lu(A_nn);
                 A_nn_inv = @(x) U\(L\x);
                 if solvePre || solveSchur
-                    % A_nn_inv = @(x) A_nn\x;
                     x_n = A_nn_inv(b_n);
                     b = b - A_cn*x_n;
-                    if solveSchur
-                        tic()
-                        [schur_diag, ix] = solver.getSchurBlockComplement(A_nn_inv, A_nc, A_cn, bz);
-                        d_offset = prelim.cellIndex(ix);
-                        insert = prelim.facePos(ix) + d_offset + ix;
-                        values(:, insert) = values(:, insert) - schur_diag;
-                        toc()
-                    end
+                    A = backend.applySchurComplementBlockSystemCSR(A, A_nn_inv, A_nc, A_cn, ...
+                                                                   solver.schurApproxType, solver.schurWeight);
                 end
             end
             t_prep = toc(timer);
             % Pass of specialized matrix to MEX file
-            [x_c, report] = solver.callAMGCL_MEX(colNo, rowPtr, values, b);
+            [x_c, report] = solver.callAMGCL_MEX(A.col_no, A.row_ptr, A.val, b);
             t_solve = toc(timer) - t_prep;
             if needsReduction
                 if solvePost || solveSchur
@@ -142,74 +101,10 @@ classdef AMGCLSolverBlockAD < AMGCLSolverAD
                 'Iterations', its);
         end
         
-        function [schur_diag, ix] = getSchurBlockComplement(solver, A_nn_inv, A_nc, A_cn, bz)
-            st = solver.schurApproxType;
-            switch st
-                case 'diagonal'
-                    doRow = true;
-                    doSum = false;
-                case 'rowsum'
-                    doRow = true;
-                    doSum = true;
-                case 'colsum'
-                    doRow = false;
-                    doSum = true;
-                otherwise
-                    error('% is not supported.', st);
-            end
-            n = size(A_nc, 2)/bz;
-            n_to_c = A_nn_inv(A_nc);
-            fill = A_cn*n_to_c;
-            [row, col, vals] = find(fill);
-            cell_row = mod(row-1, n) + 1;
-            cell_col = mod(col-1, n) + 1;
-            keep = cell_row == cell_col;
-            ix = unique(cell_row(keep));
-            if ~doSum
-                row = row(keep);
-                col = col(keep);
-                vals = vals(keep);
-            end
-            schur_diag = zeros(bz*bz, numel(ix));
 
-            % Block
-            block_row = floor((row-1)/n) + 1;
-            block_col = floor((col-1)/n) + 1;
-            % Create block values to insert
-            for eqNo = 1:bz
-                for derNo = 1:bz
-                    act = block_row == eqNo & block_col == derNo;
-                    if doRow
-                        ii = row(act) - (eqNo-1)*n;
-                    else
-                        ii = col(act) - (derNo-1)*n;
-                    end
-                    rowsum = accumarray(ii, vals(act), [n, 1]);
-                    sub = (derNo-1)*bz + eqNo;
-                    schur_diag(sub, :) = schur_diag(sub, :) + rowsum(ix)';
-                end
-            end
-            w = solver.schurWeight; % Pessimist factor for approximate schur
-            if w ~= 0
-                schur_diag = schur_diag.*w;
-            end
-            % l = 6; k = ix(l); full(fill(k + [0, 1, 2]*n, k + [0, 1, 2]*n)), schur_diag(:, l)
-        end
     end
 end
 
-function A = getMatrix(eqs, j_ix)   
-    A = cell(1, numel(eqs));
-    for i = 1:numel(eqs)
-        A{i} = horzcat(eqs{i}.jac{j_ix});
-    end
-    A = vertcat(A{:});
-end
-
-function b = getRHS(eqs)
-    values = cellfun(@(x) -value(x), eqs, 'UniformOutput', false);
-    b = vertcat(values{:});
-end
 
 %{
 Copyright 2009-2019 SINTEF Digital, Mathematics & Cybernetics.
