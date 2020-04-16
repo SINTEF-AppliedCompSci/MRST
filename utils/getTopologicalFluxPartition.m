35c40ac7e63efe2ba8e6114c1093ef1639cd7c76
Øystein S. Klemetsdal <oystein.klemetsdal@gmail.com>
2020-04-16 20:53:59 +0200

    Update getTopologicalFluxPartition.m

--- utils/getTopologicalFluxPartition.m
+++ utils/getTopologicalFluxPartition.m
@@ -1,13 +1,22 @@
-function partition = getTopologicalFluxPartition(model, state, state0, dt, drivingForces, varargin) %#ok
+function partition = getTopologicalFluxPartition(varargin)
+    require matlab_bgl
     opt = struct('blockSize'       , -inf, ...
                  'processPartition', false);
     opt = merge_options(opt, varargin{:});
     if opt.blockSize < 0
         opt.blockSize = model.G.cells.num/20;
     end
+    partition = @(varargin) get(varargin{:}, opt);
+end
+
+function partition = get(model, state, state0, dt, drivingForces, opt)
     rmodel = model;
     if isa(model, 'WrapperModel')
         rmodel = rmodel.getReservoirModel();
+    end
+    if nargin == 2
+        partition = ones(rmodel.G.cells.num,1);
+        return
     end
     v = state.flux(rmodel.operators.internalConn,:);
     order = getTopologicalPermutation(model.G, v, 'W', drivingForces.W);
