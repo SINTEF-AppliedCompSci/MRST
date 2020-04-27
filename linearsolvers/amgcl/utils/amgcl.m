20d00edaa03f2dc890d03a06a827a318980601b0
hnil <Halvor.M.Nilsen@sintef.no>
2020-04-27 18:18:47 +0200

    fixed amgcl_matlab_simple and amgcl.m to be able to use the update calls

--- linearsolvers/amgcl/utils/amgcl.m
+++ linearsolvers/amgcl/utils/amgcl.m
@@ -34,7 +34,8 @@ assert(mod(vs(1),opt.blocksize) == 0);
 if(isempty(opt.amgcloptions))
     solver = struct('type','gmres','M',20);
     precond = struct('class','relaxation','type','ilu0','damping',1);
-    options = struct('solver',solver,'precond',precond,'solver_type','regular','write_params',true,'block_size',opt.blocksize,'verbose',true);    
+    options = struct('solver',solver,'precond',precond,'solver_type','regular',...
+        'write_params',true,'block_size',opt.blocksize,'verbosity',0,'reuse_mode',1);    
 else
     options = opt.amgcloptions;
 end
