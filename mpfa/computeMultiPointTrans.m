b31adccb2dfb6b27b8e8a723e3f050d68c42d521
Xavier Raynaud <xavier.raynaud@sintef.no>
2020-05-06 12:25:11 +0200

    added few comments.

--- mpfa/computeMultiPointTrans.m
+++ mpfa/computeMultiPointTrans.m
@@ -1,5 +1,14 @@
 function [T, T_noflow] = computeMultiPointTrans(G, rock, varargin)
-
+% Compute multi-point transmissibilities. 
+%
+% Two versions available : 'legacy' (default) and 'tensor Assembly'.
+%
+% This legacy version is faster. It is limited to a mesh with grid cells where
+% corners have the same number of faces as the spatial dimension (this is always
+% the case in 2D but not in 3D). The tensor assembly version
+% (computeMultiPointTransTensorAssembly) can handle the other cases but is
+% slower (the implementation will be optimized in the future to run faster).
+%
    opt = struct('useTensorAssembly', false, ...
                 'neumann', false);
    
--- mpfa/incompMPFA.m
+++ mpfa/incompMPFA.m
@@ -1,13 +1,25 @@
 function state = incompMPFA(state, G, T, fluid, varargin)
-    
+% Solve incompressible problem with mpfa transmissibilities.
+%
+% Two versions available : 'legacy' (default) and 'tensor Assembly'.
+%
+% This legacy version is faster. It is limited to a mesh with grid cells where
+% corners have the same number of faces as the spatial dimension (this is always
+% the case in 2D but not in 3D). The tensor assembly version
+% (computeMultiPointTransTensorAssembly) can handle the other cases but is
+% slower (the implementation will be optimized in the future to run faster).
     opt = struct('useTensorAssembly', false,...
                  'mpfastruct', []); 
     [opt, extra] = merge_options(opt, varargin{:});
     
     if ~opt.useTensorAssembly
+        
         % use legacy implementation
         state = incompMPFAlegacy(state, G, T, fluid, varargin{:})
+    
+    
     else
+        
         % use tensor assembly based implementation
         assert(~isempty(opt.mpfastruct), 'option mpfastruct is required for tensor assembly');
         mpfastruct = opt.mpfastruct;
