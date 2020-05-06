64a831b5ce83857d927b1a70b9cff88127a71c08
Xavier Raynaud <xavier.raynaud@sintef.no>
2020-05-06 18:03:12 +0200

    same function incompMPFATensorAssembly for natural bc and neumann

--- mpfa/incompMPFA.m
+++ mpfa/incompMPFA.m
@@ -17,24 +17,13 @@ function state = incompMPFA(state, G, T, fluid, varargin)
         % use legacy implementation
         state = incompMPFAlegacy(state, G, T, fluid, varargin{:})
     
-    
     else
         
         % use tensor assembly based implementation
         assert(~isempty(opt.mpfastruct), 'option mpfastruct is required for tensor assembly');
         mpfastruct = opt.mpfastruct;
-        opt = struct('bc', [], ...
-                     'W', []);
-        [opt, extra] = merge_options(opt, extra{:});
-        if ~isempty(opt.bc)
-            bc = opt.bc;
-            state = incompMPFAbc(G, mpfastruct, bc, extra{:});
-        elseif ~isempty(opt.W)
-            W = opt.W;
-            state = incompMPFATensorAssembly(G, mpfastruct, W, extra{:});        
-        else
-            error('No stimulation input, either bc or W, given');
-        end
+        state = incompMPFATensorAssembly(G, mpfastruct, extra{:});        
+
     end
     
 
