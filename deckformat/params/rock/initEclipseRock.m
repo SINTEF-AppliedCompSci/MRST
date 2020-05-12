2b851fba1effe71e39b7f96ab91e2625ac458457
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2020-05-12 11:13:50 +0200

    Store GRID Section Transmissibility Multipliers in Rock
    This commit makes function 'initEclipseRock' aware of GRID section
    transmissibility multiplier keywords (MULTX .. MULTZ-).  If an input
    deck provides this data in the GRID section then the 'rock' value
    returned from 'initEclipseRock' will have a substructure named
    'multipliers' that contains those keywords, converted to lower case.
    
    For instance the 'MULTX-' keyword will be stored as
    
        rock.multipliers.multx_
    
    This is in preparation of making MRST's transmissibility calculation
    aware of multiplier information by default.

--- deckformat/params/rock/initEclipseRock.m
+++ deckformat/params/rock/initEclipseRock.m
@@ -19,6 +19,18 @@ function rock = initEclipseRock(deck)
 %            - pref -- Reference pressure for rock compressibility.  Data
 %                      from item one of 'ROCK' keyword.
 %
+%          If, additionally, the input deck's 'GRID' section contains
+%          transmissibility multiplier data (keywords 'MULTX', 'MULTY',
+%          'MULTZ', 'MULTX-', 'MULTY-', 'MULTZ-'), then those values will
+%          be stored as individual fields in a substructure 'multipliers'.
+%          The field names are lower case and hypens are replaced by
+%          underscores.  For instance, the 'MULTX-' data will be stored as
+%
+%             - rock.multipliers.multx_
+%
+%          This function is not aware of time-dependent multipliers that
+%          may be present in the 'SCHEDULE' section.
+%
 % NOTE:
 %   A given 'rock' field is only created if the corresponding keyword is
 %   present in the input deck.
@@ -51,29 +63,33 @@ You should have received a copy of the GNU General Public License
 along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
 
-
    % Check consistency.
    if ~consistent(deck)
       error(msgid('Tensor:Inconsistent'), ...
             'Input tensor is not structurally consistent.');
-   else
-      % Permeability tensor.
-      rock.perm = getTensor(deck);
    end
+
+   % Permeability tensor.
+   rock.perm = getTensor(deck);
+
+   extract_grid_prop = @(prop) reshape(deck.GRID.(prop), [], 1);
 
    % Other properties.
    rockprop = {'PORO', 'NTG'};
-   for i = 1 : numel(rockprop)
-      prop = regexprep(rockprop{i}, '\W', '_');
-      if isfield(deck.GRID, prop)
-         rock.(lower(prop)) = reshape(deck.GRID.(prop), [], 1);
-      end
+   for rockp = reshape(rockprop(isfield(deck.GRID, rockprop)), 1, [])
+      rock.(lower(rockp{1})) = extract_grid_prop(rockp{1});
+   end
+
+   multiplier = strcat('MULT', {'X', 'Y', 'Z'});
+   multiplier = [ multiplier, strcat(multiplier, '_') ];
+   for mult = reshape(multiplier(isfield(deck.GRID, multiplier)), 1, [])
+      rock.multipliers.(lower(mult{1})) = extract_grid_prop(mult{1});
    end
 
    if isfield(deck.PROPS, 'ROCK')
       [rock.cr, rock.pref] = rock_compressibility(deck.PROPS);
    end
-   
+
    rock = getRegions(rock, deck);
    rock = getScaling(rock, deck);
 end
@@ -109,8 +125,9 @@ function rock = getRegions(rock, deck)
        end
        rock.regions = regions;
    end
-
 end
+
+%--------------------------------------------------------------------------
 
 function rock = getScaling(rock, deck)
    if isfield(deck.RUNSPEC, 'ENDSCALE')
@@ -125,6 +142,7 @@ function rock = getScaling(rock, deck)
    end
 end
 
+%--------------------------------------------------------------------------
 
 function b = consistent(deck)
    k = false([3, 3]);
