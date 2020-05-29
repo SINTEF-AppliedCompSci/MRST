03dcd099c4de5d1816054849cacf93fb24327140
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2020-05-29 20:05:07 +0200

    MRST-Solvers: Insert Missing Copyright
    In preparation of MRST 2020a.

--- linearsolvers/amgcl/utils/getAMGCLDependencyPaths.m
+++ linearsolvers/amgcl/utils/getAMGCLDependencyPaths.m
@@ -19,7 +19,22 @@ function [amgclpath, boostpath] = getAMGCLDependencyPaths(varargin)
 %   `githubDownload`, `amgcl_matlab`, `callAMGCL`.
 
 %{
-#COPYRIGHT#
+Copyright 2009-2020 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
 
     opt = struct('amgcl_rev', 'a551614040f0a7b793b41a4a63386675ca61d8da');
