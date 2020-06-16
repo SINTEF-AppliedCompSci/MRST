4ad84ec7ad5ed7c418bc1461d710ede951398f88
Manuel Borregales <manuelantoniobr@gmail.com>
2020-06-16 09:21:11 +0200

    Adding code for setting up egg model

---
+++ examples/Egg_Model/Settup_Egg_simulation.m
@@ -0,0 +1,30 @@
+%% Example demonstrating the two-phase oil-water Egg model
+% This example sets up and runs the Egg model using the two-phase AD
+% solvers. 
+%
+% For details on the EggModel and the corresponding ensamble, see
+% Jansen, J. D., et al. "The egg model–a geological ensemble for reservoir
+% simulation." Geoscience Data Journal 1.2 (2014): 192-195.
+
+mrstModule add ad-core ad-blackoil deckformat diagnostics
+
+% Realizations can be set to 0 for base cae, or a number between 1 and 100
+% for different permeabilities.sa
+realization = 99;
+[G, rock, fluid, deck] = setupEGG('realization', realization);
+[state, model, schedule, nonlinear] = initEclipseProblemAD(deck, 'G', G, 'TimestepStrategy', 'none');
+
+
+model.getPhaseNames()
+
+
+problem = packSimulationProblem(state, model, schedule, 'EGG_realization_99', 'NonLinearSolver', nonlinear);
+
+%%
+[ok, status] = simulatePackedProblem(problem);
+
+
+%% Run simulation
+ [wellSols, states, reports] = getPackedSimulatorOutput(problem);
+    
+%  d = PostProcessDiagnosticsMRST(problem);
