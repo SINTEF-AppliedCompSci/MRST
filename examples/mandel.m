62f72a2e721d2b130808c731e1375c56213ffbfc
Xavier Raynaud <xavier.raynaud@sintef.no>
2020-06-24 13:03:29 +0200

    mandel problem uses last version of BiotModel

--- examples/mandel.m
+++ examples/mandel.m
@@ -1,3 +1,13 @@
+%% Mandel problem
+% Reference : (section 3.2)
+% @article{verruijt2013theory,
+%          title={Theory and problems of poroelasticity},
+%          author={Verruijt, Arnold},
+%          journal={Delft University of Technology},
+%          volume={71},
+%          year={2013}
+%         }
+
 
 clear all
 % close all
@@ -8,28 +18,46 @@ mrstModule add ad-mechanics ad-core ad-props ad-blackoil vemmech deckformat mrst
 
 %% Setup grid
 
-physdim = [20, 20] * meter;
-resolution = [40, 20];
+% physdim = [20, 20] * meter;
+physdim = [1, 1] * meter;
+nx = 40;, ny = 20;
+resolution = [nx, ny];
 G = cartGrid(resolution, physdim);
 G = computeGeometry(G);
 
 % flow parameters
-perm = 100 * milli * darcy;
-muW = 0.89 * milli * Pascal / second;
-poro = 0.25;
+% perm = 100 * milli * darcy;
+% muW = 0.89 * milli * Pascal / second;
+% poro = 0.25;
+perm = 1;
+muW  = 1;
+poro = 1;
 
 % elastic parameters
-young = 1 * giga * Pascal;
-poisson = 0.3;
+% young = 1 * giga * Pascal;
+% poisson = 0.3;
 
-E = young; nu = poisson; % shortcuts
+% Young's modulus
+E = 1;
+% Poisson's ratio
+nu = 0;
+
+% First Lamé parameter
 lambda = E*nu/((1 + nu)*(1 - 2*nu));
+% Second Lamé parameter (also called Shear modulus and denoted G)
 mu = E/(2*(1 + nu));
+Gm = mu;
+% Bulk modulus K
+K = E/(2*(1 - 2*nu)); 
+
+% consolidation coefficient
+cv = perm/muW*(K + 3*Gm); % see reference verruijt2013theory
 
 alpha = 1; % biot's coefficient
 
 % for at top
-top_force = 100 * mega * Pascal;
+% top_force = 100 * mega * Pascal;
+top_force = 1;
 
 rock.poro  = poro * ones(G.cells.num, 1);
 rock.perm  = (perm/muW) * ones(G.cells.num, 1);
@@ -37,6 +65,8 @@ rock.alpha = alpha * ones(G.cells.num, 1);
 
 % incompressible fluid
 cW = 0; 
+
+
 
 % reference pressure on the side
 pref = 0*barsa;
@@ -60,7 +90,6 @@ rightfaces = bc.face;
 bc = pside([], G, 'Ymin', dummy); 
 bottomfaces = bc.face;
 
-lateralfaces = [leftfaces; rightfaces];
 lateralfaces = leftfaces;
 
 % on the bottom, we have rolling condition in x-direction
@@ -110,7 +139,7 @@ cellcoltbl = tbls.cellcoltbl;
 force = zeros(cellcoltbl.num, 1);
 
 loadstruct.bc = bc;
-loadstruct.extforce = extforce;
+loadstruct.extforce = zeros(nodefacecoltbl.num, 1); % get the dimension right
 loadstruct.force = force;
 
 % setup mech structure 
@@ -122,7 +151,7 @@ mech.loadstruct = loadstruct;
 fluid.c = cW;
 fluid.src = [];
 
-% setup boundary condition for flow
+% Setup boundary conditions for flow
 
 bcfaces = rightfaces;
 bcvals = pref*ones(numel(bcfaces));
@@ -156,10 +185,10 @@ model =  BiotModel(G, rock, fluid, mech);
 
 %% Setup schedule
 tsteps = 100;
-duration = 1e3 * second;
+duration = 1;
 schedule.step.val = duration/tsteps * ones(tsteps, 1);
 schedule.step.control = ones(numel(schedule.step.val), 1);
-schedule.control = struct('W', []);
+schedule.control = struct('W', [], 'extforce', extforce);
 
 %% Setup initial state
 clear initState;
@@ -172,21 +201,48 @@ cellcoltbl = tbls.cellcoltbl;
 initState.u = zeros(cellcoltbl.num, 1);
 nlm = size(loadstruct.bc.linformvals, 1);
 initState.lambdamech = zeros(nlm, 1);
+initState.extforce = 0*extforce;
 
 solver = NonLinearSolver('maxIterations', 100);
 [wsol, states] = simulateScheduleAD(initState, model, schedule, 'nonlinearsolver', solver);
 
+
+%% Mandel plot
+figure
+clf
+ind = (1 : nx)';
+xc = G.cells.centroids(ind, 1);
+hold on
+
+trep = [0.00001; 0.01; 0.1; 0.5; 1];
+trep = trep/cv;
+tt = cumsum(schedule.step.val);
+legends = {};
+for i = 1 : numel(states);
+    if ismembertol(tt(i), trep, 1e-8);
+        p = states{i}.pressure;
+        p = p(ind);
+        plot(xc, p);
+        legends{end + 1} = sprintf('%g', tt(i)*cv);
+    end
+end
+legend(legends{:});
+
+
 %% plotting
 figure 
+clf
 plotToolbar(G, states);
 
 figure
+clf
 ind = 1;
 pmid = cellfun(@(state) state.pressure(ind), states);
 tt = cumsum(schedule.step.val);
 plot(tt, pmid, '*');
 
 figure
+clf
 inds = (1 : resolution(1))';
 xc = G.cells.centroids(inds, 1);
 pc = states{end}.pressure(inds);
