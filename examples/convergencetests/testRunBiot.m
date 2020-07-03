b57a82f3a05758284a804dabe417b97d50f6665e
Xavier Raynaud <xav.raynaud@gmail.com>
2020-07-03 07:23:02 +0200

    simple test for Biot convergence

--- examples/convergencetests/testRunBiot.m
+++ examples/convergencetests/testRunBiot.m
@@ -3,63 +3,25 @@ mrstModule add vemmech mpsaw vem mpfa
 clear all
 close all
 
-% Switch this on for perturbed grids
-pert = 0;
-% Test case number (see definitions below)
-testCase = 1;
-% Constant (between 0 and 1) in MPSA and MPFA which defines position of the continuity point
-eta = 1/3;
-
-switch testCase
-  case 1
-    % Cartesian grid
-    gridType = 1;
-  case 2
-    % Cartesian grid
-    gridType = 1;
-    eta = 0;
-  case 3
-    % Triangular grid, 90 degree angles
-    gridType = 2;
-    eta = 1/3;
-  case 4
-    % Equilateral triangles
-    gridType = 3;
-    eta = 1/3;
-end
-
-Nx = [30, 30];
-G = gridForConvTest(Nx, gridType);
-
-d = G.griddim;
-
-% Physical parameters
-mu     = 1;
-lambda = 0;
-alpha  = 1;
-K      = 1;
-tau    = 1; % (must be set to 1)
-rho    = 1;
-
-params = struct('mu'    , mu    , ...
-                'lambda', lambda, ...
-                'alpha' , alpha , ...
-                'K'     , K     , ...
-                'tau'   , tau   , ...
-                'rho'   , rho   , ...
-                'eta'   , eta);
-
-% Compute analytical solution
-output = analyticalBiot(d, params);
-
-params.u_fun      = output.u_fun;
-params.p_fun      = output.p_fun;
-params.stress_fun = output.stress_fun;
-params.force_fun  = output.force_fun;
-params.src_fun    = output.src_fun;
+params = struct('nref'    , 1, ...
+                'Nd'      , 2, ...
+                'gridtype', 1, ...
+                'eta'     , 0, ...
+                'mu'      , 1, ...
+                'lambda'  , 1, ...
+                'alpha'   , 1, ...
+                'K'       , 1, ...
+                'tau'     , 1, ...
+                'rho'     , 0);
 
 % Compute numerical solution
-output = runBiotConvSim(G, params);
+output1 = biotConvergenceFunc(params);
+output2 = biotConvergenceFunc(params, 'blocksize', 2);
+
+fprintf('Displacement error (standard) : %g\n', output1.uerrL2);
+fprintf('Displacement error (block assembly): %g\n', output2.uerrL2);
+
+return
 
 u = output.u;
 p = output.p;
