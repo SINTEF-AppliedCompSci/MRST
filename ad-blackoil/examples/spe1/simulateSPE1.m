025ace794801d7148e27b8c25e6b69d0883ba62a
Olav Møyner <olav.moyner@sintef.no>
2020-08-04 13:05:34 +0200

    ad-blackoil: Adding SPE3 minimal example for testing vapoil

--- ad-blackoil/examples/spe1/simulateSPE1.m
+++ ad-blackoil/examples/spe1/simulateSPE1.m
@@ -1,7 +1,7 @@
 %% Minimal example that runs SPE1
 mrstModule add ad-blackoil ad-core mrst-gui ad-props deckformat
-pth = getDatasetPath('spe1');
-fn  = fullfile(pth, 'BENCH_SPE1.DATA');
+pth = getDatasetPath('spe3');
+fn  = fullfile(pth, 'BENCH_SPE3.DATA');
 problem = initEclipsePackedProblemAD(fn);
 %% Simulate
 simulatePackedProblem(problem, 'restartStep', 1);
