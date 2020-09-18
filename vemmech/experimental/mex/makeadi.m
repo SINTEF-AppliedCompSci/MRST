bb3bb16c8626b7fe9907308fa8d2bffbb3f598b6
Elyes Ahmed <elyes.ahmed@sintef.no>
2020-09-18 15:55:47 +0200

    slight input change for makeadi

--- vemmech/experimental/mex/makeadi.m
+++ vemmech/experimental/mex/makeadi.m
@@ -2,7 +2,7 @@ function adi = makeadi(vals, jac)
 
    % three next lines to workaround mex memory issue with sparse arrays
    if isscalar(jac)
-      jac = sparse(numel(vals(:)), jac);
+      jac = sparse(numel(vals(:)), double(jac));
    end
    
    adi = ADI(vals(:), jac);
