c3251b4d7ef28a74e02e0b2fb08b1ac9215e92fd
Olav Møyner <olav.moyner@gmail.com>
2020-10-02 11:35:33 +0200

    Update initCompositionalState.m
    Clean up logic and interface

--- compositional/utils/initCompositionalState.m
+++ compositional/utils/initCompositionalState.m
@@ -1,18 +1,20 @@
-function state = initCompositionalState(G, p, T, s0, z0, eos)
+function state = initCompositionalState(model, p, T, s0, z0, eos)
 % Initialize a compositional state given initial composition
 %
 % SYNOPSIS:
-%   state = initCompositionalState(G, p, T, s0, z0, eos)
+%   state = initCompositionalState(model, p, T, s0, z0)
 %
 % PARAMETERS:
-%   G   - Grid for which the state is to be constructed.
-%   p   - Pressures as a column vector
-%   T   - Temperatures as a column vector
-%   s0  - Initial saturation. Any compositional phases will be flashed, so
-%         in that case only the sum matters.
-%   z   - Composition as a matrix with number of rows equal to the number
-%         of components.
-%   eos - EquationOfState derived class instance.
+%   model - Compositional model, or the reservoir grid itself.
+%   p     - Pressures as a column vector
+%   T     - Temperatures as a column vector
+%   s0    - Initial saturation. Any compositional phases will be flashed, so
+%           in that case only the sum matters. Only required for more than
+%           two phases present.
+%   z     - Composition as a matrix with number of rows equal to the number
+%           of components.
+%   eos   - EquationOfStateModel instance if model is not the first input
+%           argument (legacy syntax). Otherwise optional.
 %
 % RETURNS:
 %   state - Intialized state (after flash)
@@ -36,8 +38,19 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
-
-    state = initResSol(G, p, s0);
+    hasModel = ~isstruct(model);
+    if hasModel
+        % First argument is model
+        assert(isa(model, 'ThreePhaseCompositionalModel'));
+        if nargin == 5
+            eos = model.EOSModel;
+        end
+        G = model.G;
+    else
+        G = model;
+        assert(nargin > 5);
+    end
+    state = initResSol(G, p, 1);
     state.T = repmat(T, G.cells.num, 1);
     if size(z0, 1) == G.cells.num
         state.components = z0;
@@ -51,12 +64,37 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
         state = eos.updateAfterConvergence(state0, state, dt, struct());
     end
     [sL, sV] = eos.computeSaturations(nan, nan, state.x, state.y, state.L, state.Z_L, state.Z_V);
-    sHydrocarbon = [sL, sV];
-    if size(state.s, 2) == 3
-        % Water is present
-        sW = state.s(:, 1);
-        state.s = [sW, bsxfun(@times, sHydrocarbon, 1-sW)];
+    sz = size(s0, 2);
+    if hasModel
+        % We know the model and can then figure out what phase goes where.
+        nph = model.getNumberOfPhases();
+        ix = model.getEoSPhaseIndices();
+        if nph > 2
+            if sz == nph - 2
+                % We have value for the non-EoS-phases
+                fill = s0;
+                s0 = zeros(G.cells.num, nph);
+            elseif sz == nph
+                % We have one value per phase
+                fill = sum(s0, 2) - sum(s0(:, ix), 2);
+            else
+                error('Bad!');
+            end
+        else
+            fill = 0;
+            s0 = zeros(G.cells.num, nph);
+        end
+    elseif sz == 0
+        % Assume two-phase since we got no saturation and no model
+        fill = 0;
+        s0 = zeros(G.cells.num, 2);
     else
-        state.s = sHydrocarbon;
+        % We do not know what kind of model we have, just assume that the
+        % water phase is the non-EoS
+        assert(sz > 1, 'Must have multiple columns in saturations');
+        ix = [2, 3];
+        fill = sum(s0, 2) - sum(s0(:, ix), 2);
     end
+    s0(:, ix) = bsxfun(@times, 1 - fill, [sL, sV]);
+    state.s = s0;
 end
