bbc432184be77b74fbba9ad3e779c5e063aa65f5
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2020-10-13 23:39:54 +0200

    MRST-CO2Lab: Insert Copyright Notice Where Missing
    In preparation of MRST 2020b.

--- deprecated/computePlumeHeight.m
+++ deprecated/computePlumeHeight.m
@@ -1,6 +1,25 @@
-function [h, h_max] = computeHeightOfPlume(Gt, state, sw, sr)
+function [h, h_max] = computePlumeHeight(Gt, state, sw, sr)
+% computing height corresponding to the current saturation state
 
-    % computing height corresponding to the current saturation state
+%{
+Copyright 2009-2020 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
     if isfield(state, 'sGmax')
         % We operate with dissolution, implying a gradually receding 'max' saturation
         smax = state.sGmax;
