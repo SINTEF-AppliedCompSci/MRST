58d35ac25fae419cced6b9f34f062775e03a559e
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2020-10-13 23:30:55 +0200

    MRST-Model-IO: Update Copyright Years
    In preparation of MRST 2020b.

--- deckformat/deckinput/readFixedNumRecords.m
+++ deckformat/deckinput/readFixedNumRecords.m
@@ -46,7 +46,7 @@ function data = readFixedNumRecords(fid, template, nrec)
 %   `readDefaultedRecord`.
 
 %{
-Copyright 2020 SINTEF Digital, Mathematics & Cybernetics.
+Copyright 2020-2020 SINTEF Digital, Mathematics & Cybernetics.
 
 This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
 
