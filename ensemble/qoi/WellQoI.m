6c5add839decb76d084c54d1256cd12e117ac64c
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2020-10-27 09:04:16 +0100

    WellQoI: Add plotting routines. For time series: interpolate to base problem timesteps if problem has been run with different steps

--- ensemble/qoi/WellQoI.m
+++ ensemble/qoi/WellQoI.m
@@ -1,15 +1,15 @@
 classdef WellQoI < BaseQoI
     
     properties
-        fldname      = {'qOs'}
-        wellIndices
-        wellNames    
-                               
-        numTimesteps
+        fldname      = {'qOs'} % Well output field names
+        wellIndices            % Well indices
+        wellNames              % Well names
+
         cumulative   = false % Cumulative production
         total        = false % Total production
-        combined     = false % Combine all wells, or give per well values.
-        timesteps
+        combined     = false % Combine all wells, or give per well values
+        
+        dt % Timestep sizes
     end
     
     methods
@@ -43,6 +43,13 @@ classdef WellQoI < BaseQoI
                 % Assign well indices
                 qoi.wellIndices = cell2mat(ix);
             elseif ~isempty(qoi.wellIndices)
+                if strcmpi(qoi.wellIndices, ':')
+                    % All wells requested
+                    qoi.wellIndices = 1:numel(names);
+                elseif islogical(qoi.wellIndices)
+                    % Logical mask - find corresponding indices
+                    qoi.wellIndices = find(qoi.wellIndices);
+                end
                 % Verify well indices
                 assert(min(qoi.wellIndices) >= 1 && max(qoi.wellIndices) <= numel(names), ...
                     'Well indices must be in the range [1, numel(W)] = [1, %d]', numel(names));
@@ -53,91 +60,80 @@ classdef WellQoI < BaseQoI
                 error('Please provide either well names or well indices');
             end
             % Get timesteps
-            qoi.timesteps = problem.SimulatorSetup.schedule.step.val;
-            if ~isempty(qoi.numTimesteps)
-                qoi.timesteps = qoi.timesteps(1:qoi.numTimesteps);
-            else
-                qoi.numTimesteps = numel(qoi.timesteps);
-            end
+            qoi.dt = problem.SimulatorSetup.schedule.step.val;
         end
         
         %-----------------------------------------------------------------%
         function u = computeQoI(qoi, problem)
             % Reads the well solutions from the given problem and extract
             % the relevant data, which is then saved to file.
-            
             % Read well solutions
             wellSols = reshape(problem.OutputHandlers.wellSols(:), [], 1);
-
             % Organize as matrix with dimensions:
             % (numTimesteps, numWells, numFields)
-            wellOutputs = getWellOutput(wellSols, qoi.fldname, qoi.wellNames);
-
-            dt = getTimestepsFromProblem(problem);
-            
-            numAvailableTimesteps = size(dt, 1);
-            if qoi.numTimesteps < numAvailableTimesteps
-                wellOutputs = wellOutputs(1:qoi.numTimesteps,:,:);
-                dt = dt(1:qoi.numTimesteps);
-            end
-            
+            u = getWellOutput(wellSols, qoi.fldname, qoi.wellNames);
             % Compute total or cumulative if requested
+            dtProblem = getTimestepsFromProblem(problem);
             if qoi.total
-                wellOutputs = sum(wellOutputs.*dt,1);
+                u = sum(u.*dtProblem,1);
             elseif qoi.cumulative
-                wellOutputs = cumsum(wellOutputs.*dt,1);
+                u = cumsum(u.*dtProblem,1);
             end
-            
+            % Compute combined output from all wells if requested
             if qoi.combined && numel(qoi.wellNames) > 1
-                wellOutputs = sum(wellOutputs, 2);
+                u = sum(u, 2);
             end
-            
-            u = {wellOutputs};
-            
-        end
-
-        %-----------------------------------------------------------------%
-        function mean_u = getEnsembleMean(qoi, ensemble)
-            mean_u = zeros(numel(qoi.timesteps), numel(qoi.wellNames), numel(qoi.fldname));
-            for i = 1:ensemble.num
-                mean_u = mean_u + ensemble.qoi.ResultHandler{i}{1};
+            % If we have a time series and the problem was simulated with
+            % different timesteps than the base problem, interpolate well
+            % output onto base problem timesteps
+            if numel(qoi.dt) ~= numel(dtProblem) && ~qoi.total
+                u = qoi.interpolateWellOutput(dtProblem, u);
             end
-            mean_u = mean_u / ensemble.num;
+            u = {u};
         end
         
         %-----------------------------------------------------------------%
-        function plotEnsemble(qoi, ensemble)
-            % Plots well properties for the ensemble and ensemble mean.
-            % Creates one figure per field
-            % Organizes results from each well in a subplot
+        function wellOutput = interpolateWellOutput(qoi, dtProblem, wellOutput)
+            % Get base problem and current problem timestamps
+            timeProblem = [0; cumsum(dtProblem)];
+            timeBase    = [0; cumsum(qoi.dt)   ];
+            % Compute overlap
+            t   = bsxfun(@min, timeBase(2:end)  , timeProblem(2:end)'  );
+            t0  = bsxfun(@max, timeBase(1:end-1), timeProblem(1:end-1)');
+            psi = max(t - t0, 0)./qoi.dt;
+            % Integrate
+            wellOutput = psi*wellOutput;
+        end
             
-            num_fields = numel(qoi.fldname);
-            num_wells  = numel(qoi.wellNames);
-            
-            num_wells_horizontal = ceil(sqrt(num_wells));
-            num_wells_vertical   = ceil(num_wells/num_wells_horizontal);
-            
-            mean_qoi = qoi.getEnsembleMean(ensemble);
-            
-            t = cumsum(qoi.timesteps);
-            for fld = 1:num_fields
-                figure
-                for w = 1:num_wells
-                    subplot(num_wells_horizontal, num_wells_vertical, w)
-                    hold on
-                    for i = 1:ensemble.num
-                        plot(t, ensemble.qoi.ResultHandler{i}{1}(:,w,fld),   'color', [1 1 1]*0.6);
-                    end
-                    plot(t, mean_qoi(:, w, fld), 'color', 'red')
-                    title(strcat(qoi.fldname{fld}, " for well ", qoi.wellNames{w}));
+        %-----------------------------------------------------------------%
+        function plotQoI(qoi, ensemble, u, varargin) %#ok
+            % Plot a single well QoI u in current figure.
+            opt = struct('isMean'   , true, ...
+                         'timescale', day , ...
+                         'labels'   , true);
+            [opt, extra] = merge_options(opt, varargin{:});
+            color = [1,1,1]*0.8*(1-opt.isMean); % Plot mean in distinct color
+            is_timeseries = true;
+            if is_timeseries
+                time = cumsum(qoi.dt)./opt.timescale;
+                if qoi.cumulative
+                    u = cumsum(u.*qoi.timesteps).*opt.timescale;
+                else
+                    u = u.*opt.timescale;
+                end
+                plot(time, u, 'lineWidth', 2, 'color', color, extra{:});
+                xlim([time(1), time(end)]);
+                if opt.labels
+                    xlabel(sprintf('Time (%s)', formatTime(opt.timescale)));
+                    ylabel(sprintf('%s', qoi.fldname{1}));
                 end
             end
-            
         end
         
+        %-----------------------------------------------------------------%
         function n = norm(qoi, u)
             if ~qoi.total
-                n = cellfun(@(u) sum(u.*qoi.timesteps), u);
+                n = cellfun(@(u) sum(u.*qoi.dt), u);
             else
                 n = norm@BaseQoI(qoi, u);
             end
@@ -148,6 +144,8 @@ end
 
 %-------------------------------------------------------------------------%
 % Helpers
+%-------------------------------------------------------------------------%
+
 %-------------------------------------------------------------------------%
 function dt = getTimestepsFromProblem(problem)
     reports = reshape(problem.OutputHandlers.reports(:), [], 1);
@@ -160,4 +158,17 @@ function dt = getTimestepsFromProblem(problem)
         end
         dt(i) = dtstep;
     end     
+end
+
+%-------------------------------------------------------------------------%
+function str = formatTime(timescale)
+    t   = [second, minute, day, year];
+    str = {'Second', 'Minute', 'Day', 'Year'};
+    [d, ix] = min(abs(t - timescale));
+    if d == 0
+        str = str{ix};
+    else
+        warning('Non-standard timescale, using ''Second''');
+        str = str{1};
+    end
 end
