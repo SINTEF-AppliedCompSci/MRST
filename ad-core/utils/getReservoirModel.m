cf575051fdf2f68b80ea99df28ee5ff3f0992dee
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2020-11-02 14:21:15 +0100

    Improve function documentation

--- ad-core/utils/getReservoirModel.m
+++ ad-core/utils/getReservoirModel.m
@@ -1,7 +1,27 @@
 function rmodel = getReservoirModel(model)
-% Get the underlying reservoir model of a WrapperModel. If model is an
-% instance of SequentialPressureTransportModel, the function returns the
-% reservoir model of the pressure model.
+%Get the underlying reservoir model of a WrapperModel
+%
+% SYNOPSIS:
+%   rmodel = getReservoirModel(model)
+%
+% PARAMETERS:
+%   model - Instance of WrapperModel class.
+%
+% RETURNS:
+%   rmodel - ReservoirModel contained in wrapper model instance. If `model`
+%            is an instance of `SequentialPressureTransportModel` the
+%            function returns the `ReservoirModel` of the contained
+%            `PressureModel`.
+%
+%            Otherwise, this is the top-most `ReservoirModel` - the one
+%            that does not have a `parentModel`.
+%
+% EXAMPLE:
+%   tmodel = TransportModel(model);     % Make transport model
+%   rmodel = getReservoirModel(tmodel); % rmodel = model
+%
+% SEE ALSO:
+%   `WrapperModel`, `ReservoirModel`, `setReservoirModel`
     rmodel = model;
     if isa(rmodel, 'SequentialPressureTransportModel')
         % We have a sequential implicit model, use pressure model
--- ad-core/utils/setReservoirModel.m
+++ ad-core/utils/setReservoirModel.m
@@ -1,7 +1,29 @@
 function model = setReservoirModel(model, rmodel)
-% Replace the underlying reservoir model of a WrapperModel with rmdoel. If
-% model is an instance of SequentialPressureTransportModel, the function
-% replaces the reservoir model of the pressure and transport model.
+%Set the underlying reservoir model of a WrapperModel
+%
+% SYNOPSIS:
+%   model = setReservoirModel(model, rmodel)
+%
+% PARAMETERS:
+%   model  - Instance of WrapperModel class.
+%   rmodel - Instance of ReservoirModel class to replace the current
+%            underlying reservoir model of `model`
+%
+% RETURNS:
+%   model - WrapperModel with updated Reservoir model. If `model`
+%           is an instance of `SequentialPressureTransportModel` the
+%           function replaces the `ReservoirModel` of the contained
+%           `PressureModel` and `TransportModel`
+%
+% EXAMPLE:
+%   tmodel = TransportModel(model);             % Make transport model
+%   rmodel = getReservoirModel(tmodel);         % rmodel = model
+%   rmodel.rock.perm = rmodel.rock.perm*1e3;    % Multiply permeability
+%   rmodel = rmodel.setupOperators();           % Update model operators
+%   tmodel = setReservoirModel(tmodel, rmodel); % Update tmodel
+%
+% SEE ALSO:
+%   `WrapperModel`, `ReservoirModel`, `getReservoirModel`
     if isa(model, 'SequentialPressureTransportModel')
         % Sequential implicit model - set reservoir model for both pressure
         % and transport
