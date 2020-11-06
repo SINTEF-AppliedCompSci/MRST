78164633f3f612b6d958b41aabe359fe2ef3f3cd
Manuel A. Borregales R <manuelantoniobr@gmail.com>
2020-11-06 12:47:13 +0100

    Backup

--- examples/Egg_Model_Segregated/FlowNetwork.m
+++ examples/Egg_Model_Segregated/FlowNetwork.m
@@ -2,7 +2,7 @@ mrstModule add ad-core ad-blackoil deckformat diagnostics mrst-gui ad-props inco
 
  
 %% Run EGG field simulation
-Settup_Egg_simulation 
+%Settup_Egg_simulation 
  
 
 
@@ -12,66 +12,10 @@ states_ref   = states;
 schedule_ref = schedule;
 W_ref        = schedule_ref.control.W;
 
-%%  Compute diagnostics 
+%% Initializing parameters
 
- DD = WellPairNetwork(model_ref,schedule_ref,states_ref,state,wellSols_ref);
- DD =  DD.filter_wps(0.05*stb/day);
- DD.plotWellPairConnections()
- DD.plotWellPairsData('subplot',[4,4])
- 
- 
- %% Making the Graph
- Well_Indices = DD.Graph.Edges.EndNodes;
- 
- 
- n_wells =numel(W_ref);
-k = 1;
-            for i = 1: n_wells
-                Nodes(k,1) = k;
-                Nodes(k+1,1) = k+1;
-                Well(k,1) = i;
-                Well_name{k,1}= [W_ref(i).name,'-',num2str(1)];
-                Well(k+1,1) = i;
-                cell_number = W_ref(i).cells(1);
-                XData(k,1) = model.G.cells.centroids(cell_number,1);
-                YData(k,1) = model.G.cells.centroids(cell_number,2);
-                ZData(k,1) = model.G.cells.centroids(cell_number,3);
-                Well_cell(k,1) = cell_number;
-                SubWell(k,1) = 1;
-                
-                cell_number = W_ref(i).cells(end);
-                Well_name{k+1,1}= [W_ref(i).name,'-',num2str(2)];
-                XData(k+1,1) = model.G.cells.centroids(cell_number,1);
-                YData(k+1,1) = model.G.cells.centroids(cell_number,2);
-                ZData(k+1,1) = model.G.cells.centroids(cell_number,3);
-                Well_cell(k+1,1) = cell_number;
-                SubWell(k+1,1) = 2;
-                k = k +2;
-            end
-            
-            
-            
-            NodeWell_Indices = [];
-            NodeWell_Indices_2 = [];
-            k= 1;
-            for i = 1: size(Well_Indices,1)
-                    pv(k) = DD.wps{i}.volume;
-                    TT(k) = DD.wps{i}.Tr;
-                    pv(k+1) = DD.wps{i}.volume;
-                    TT(k+1) = DD.wps{i}.Tr;
-                    k=k+2;
-                I1_well = find( Well_Indices(i,1) == Well);
-                NodeWell_Indices = [NodeWell_Indices; I1_well];
-                
-                I2_well = find( Well_Indices(i,2) == Well);
-                NodeWell_Indices_2 = [NodeWell_Indices_2; I2_well];
-            end
-            
-        EdgeTable = table([NodeWell_Indices NodeWell_Indices_2],'VariableNames',{'EndNodes'});
-        NodeTable = table(Nodes,Well,SubWell,Well_name,Well_cell,XData,YData,ZData,'VariableNames',{'Nodes','Well','SubWell','Well_name','Well_cell','XData','YData','ZData'});
-        Graph = graph(EdgeTable,NodeTable);            
-
-   
+    pv = Mean_PV;
+    TT = Mean_Trans;   
 
 %% Creatting data driven model
 
@@ -107,3 +51,122 @@ obj.plotNetwork(model_ref.G,TT)
 
 figure
 obj.plotGPSNetGrid([])
+
+
+%% Preparing parameters and scaling values for each one
+
+WellIP = [];
+cell = [];
+well_index = []; 
+levels = 1;        
+for  i = 1:numel(W)
+    for l = 1 : size(W(i).WI)
+        well_index = [well_index; i,l];
+        WellIP = [WellIP; 2*W(i).WI(l)];
+    end
+end
+
+
+%     pv = 0*Mean_PV +Mean_PV(1) ;
+%     TT = 0*Mean_Trans+ Mean_Trans(1);
+
+Tr_boxlimits = [0.01*TT ;1.4*TT]';
+Pv_boxlimits = [0.01*pv/10 ; 2*pv/10]';
+well_boxlimits = [ 0.01*WellIP , ...
+                   7.0*WellIP];
+               
+
+               
+          well_IP = struct('name','conntrans',...
+                                   'type','value',...
+                                   'boxLims', well_boxlimits,...
+                                   'distribution','general',...
+                                   'Indx',well_index);
+
+
+          transmisibility_conection = struct('name','transmissibility',...
+                                   'type','value',...
+                                   'boxLims', Tr_boxlimits ,...
+                                   'distribution','connection',...
+                                   'Indx',{indexs.faces});
+                               
+
+          porevolume_conection = struct('name','porevolume',...
+                                   'type','value',...
+                                   'boxLims',Pv_boxlimits,...
+                                   'distribution','connection',...
+                                   'Indx',{indexs.cells});                                                                
+                               
+parameters =  {};                          
+parameters{1} = transmisibility_conection;
+parameters{2}  = porevolume_conection ;
+parameters{3}  = well_IP ;
+
+%% Prepare the model for simulation.
+lvl = 0;
+s0=zeros( numedges(obj.Graph) ,2);
+for i = 1: numedges(obj.Graph)
+    nodes= deal(obj.Graph.Edges.EndNodes(i,:));
+    
+    if (obj.Graph.Nodes.SubWell(nodes(1))==1)
+        s0((1:10)+lvl,1) = 0;
+        s0((1:10)+lvl,2) = 1;
+    else
+        s0((1:9)+lvl,1) = 1;
+        s0((1:9)+lvl,2) = 0;
+        s0((10:10)+lvl,1) = 0;
+        s0((10:10)+lvl,2) = 1;
+    end
+    lvl=lvl+10;
+end
+
+model = model.validateModel();
+state0 = initState(G, W , 400*barsa,s0); 
+dt = schedule.step.val;
+
+
+ %% Simulating the initial DD model
+
+ schedule = simpleSchedule(dt(1:48), 'W', W);
+
+         weighting =  {'WaterRateWeight',  1e5, ...
+                       'OilRateWeight',    1e5, ...
+                       'BHPWeight',        1e-5};
+ schedule_0=schedule;
+ 
+                  
+  val{1} = TT;
+  val{2} = pv/10;
+  val{3} = WellIP;
+  p0_fd = value2control(val,parameters);
+ 
+  
+ [misfitVal_0,gradient,wellSols_0,states_0] = Simulate_BFGS(p0_fd,parameters,model,schedule_0,state0, wellSols_ref,weighting,1);          
+  plotWellSols({wellSols_ref,wellSols_0},{schedule_ref.step.val,schedule_0.step.val})
+  
+    %% Optimization
+  
+obj_scaling     = abs(misfitVal_0);      % objective scaling  
+objh = @(p)Simulate_BFGS(p,parameters,model,schedule_0,state0,  wellSols_ref,weighting,obj_scaling);
+
+[v, p_opt, history] = unitBoxBFGS(p0_fd, objh,'gradTol',             1e-2, ...
+                                              'objChangeTol',        0.5e-3);
+
+
+%% Simulating all simulation time
+ schedule = simpleSchedule(dt, 'W', W);
+ 
+ [misfitVal_opt,gradient_opt,wellSols_opt] = Simulate_BFGS(p_opt,parameters,model,schedule,state0, wellSols_ref,weighting,obj_scaling);
+
+  [misfitVal_0,gradient_0,wellSols_0] = Simulate_BFGS(p0_fd,parameters,model,schedule,state0, wellSols_ref,weighting,obj_scaling);
+
+
+
+plotWellSols({wellSols_ref,wellSols_0,wellSols_opt},{schedule_ref.step.val,schedule.step.val,schedule.step.val})
+legend('reference model','initial DD model','optimize DD model')
+
+PlotEggmodel_results_2
+
+Graph= obj.Graph;
+
+Plot_Parameters
