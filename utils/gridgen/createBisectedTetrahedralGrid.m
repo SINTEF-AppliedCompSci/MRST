be6b8043c30aaa3816670a2729abe67722327b28
Xavier Raynaud <xavier.raynaud@sintef.no>
2020-11-06 11:17:03 +0100

    clean-up

--- utils/gridgen/createBisectedTetrahedralGrid.m
+++ utils/gridgen/createBisectedTetrahedralGrid.m
@@ -1,9 +1,9 @@
 function G = createBisectedTetrahedralGrid(Nx,alternate)
+    
+    [X,Y,Z] = meshgrid(linspace(0,1,Nx(1)+1), linspace(0,1,Nx(2)+1),linspace(0,1,Nx(3)+1));
+    p = [X(:), Y(:), Z(:)];
+    
+    G = mcomputeGeometry(tetrahedralGrid(p));
+    
+end
 
-[X,Y,Z] = meshgrid(linspace(0,1,Nx(1)+1), linspace(0,1,Nx(2)+1),linspace(0,1,Nx(3)+1));
-% X = X';
-% Y = Y';
-% Z = Z';
-p     = [X(:), Y(:), Z(:)];
-
-G = mcomputeGeometry(tetrahedralGrid(p));
