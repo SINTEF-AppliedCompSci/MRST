7591bc1b6f6cd1cf3a53ae17c2fa40b4bc4df830
Xavier Raynaud <xavier.raynaud@sintef.no>
2020-11-06 10:51:36 +0100

    changed refinement parameter in convergence test.

--- examples/convergencetests/mpsaConvergenceTest.m
+++ examples/convergencetests/mpsaConvergenceTest.m
@@ -5,19 +5,18 @@ close all
 
 %% params setting
 % nref     : degree of refinement
-nref = 3;  % default setting
-           % Nd       : dimension (2D or 3D)
-           % kappa    : Value of heterogenity in the domain (see paper)
-           % alpha    : Parameter to setup Lamé coefficient lambda (lambda = alpha*mu)
-           % gridtype : grid type (see mpsaPaperConvergenceFunc)
-           % eta      : Value used to set the position of the continuity point
+% Nd       : dimension (2D or 3D)
+% kappa    : Value of heterogenity in the domain (see paper)
+% alpha    : Parameter to setup Lamé coefficient lambda (lambda = alpha*mu)
+% gridtype : grid type (see mpsaPaperConvergenceFunc)
+% eta      : Value used to set the position of the continuity point
 
 % Possibility to run vem for comparison
 doVem = false;
 
 %% New Case
 dothiscase = true;
-params = struct('nref'    , 5, ...
+params = struct('nref'    , 4, ...
                 'Nd'      , 3, ...
                 'kappa'   , 10, ...
                 'alpha'   , 0, ...
