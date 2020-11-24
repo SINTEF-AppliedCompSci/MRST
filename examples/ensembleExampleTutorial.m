59d6e43d49c14b7c211782be702012407e738e65
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2020-11-24 23:58:49 +0100

    Insert Copyright Notice Where Missing
    In preparation of MRST 2020b.

--- examples/ensembleExampleTutorial.m
+++ examples/ensembleExampleTutorial.m
@@ -219,3 +219,31 @@ title('Water saturation after 300 days, ensemble avg');
 % [1] "Simulation of Stationary Gaussian Processes in [0, 1]^d", Andrew
 % Wood and Grace Chan, Journal of Computational and Graphical Statistics,
 % Vol.3 Number 4 (1994), pages 409-432
+
+%% Copyright Notice
+%
+% <html>
+% <p><font size="-1">
+% Copyright 2009-2020 SINTEF Digital, Mathematics & Cybernetics.
+% </font></p>
+% <p><font size="-1">
+% This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+% </font></p>
+% <p><font size="-1">
+% MRST is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% </font></p>
+% <p><font size="-1">
+% MRST is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% </font></p>
+% <p><font size="-1">
+% You should have received a copy of the GNU General Public License
+% along with MRST.  If not, see
+% <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses</a>.
+% </font></p>
+% </html>
