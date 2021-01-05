8f57f502093a49b1056ddca181036a60dd3b7703
Manuel A. Borregales R <manuelantoniobr@gmail.com>
2021-01-05 12:08:01 +0100

    Adding a nonlinear solver to run olympus

--- Simulate_BFGS.m
+++ Simulate_BFGS.m
@@ -14,7 +14,7 @@ function [misfitVal,varargout] = Simulate_BFGS(p,parameters,model,schedule,state
                 lsolve.setSolver('bicgstab');
              solver = NonLinearSolver('LinearSolver',AMGCL_CPRSolverAD(),'continueOnFailure',true);
              
-            [ wellSols,states] = simulateScheduleAD(state0, model, schedule);
+            [ wellSols,states] = simulateScheduleAD(state0, model, schedule,'NonLinearSolver',solver);
         
 % compute misfit function value (first each summand corresonding to each time-step)
     misfitVals = matchObservedOW(model.G, wellSols, schedule, wellSols_ref, weighting{:});
