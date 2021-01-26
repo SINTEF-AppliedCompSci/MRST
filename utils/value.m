5ee727d09c4cb0e16fe7d40dc3cf094060c6397b
Olav Møyner <olav.moyner@gmail.com>
2021-01-26 12:15:31 +0100

    value: Add a fallback for objects with .val field
    Band-aid fix to Octave issues

--- utils/value.m
+++ utils/value.m
@@ -54,6 +54,11 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
             end
         end
         xv = x;
+    elseif isprop(x, 'val')
+        % If the object somehow has the property '.val' we use that. Note
+        % that this is partially to fix an Octave bug when using function
+        % handles on classes.
+        xv = x.val;
     else
         xv = x;
     end
