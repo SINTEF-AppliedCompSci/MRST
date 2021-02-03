7a3d368e6842803243792beede15634884d7fae0
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2021-02-03 16:23:21 +0100

    Update makeWellPartition.m

--- utils/makeWellPartition.m
+++ utils/makeWellPartition.m
@@ -1,26 +1,50 @@
-function [pw, is_well] = makeWellPartition(model, W, varargin)
-    opt = struct('rWell', 20);
+function [partition, isWell] = makeWellPartition(G, W, varargin)
+
+    opt = struct('rWell'     , 20      , ...
+                 'type'      , 'metric', ...
+                 'splitWells', true    );
     opt = merge_options(opt, varargin{:});
-    G = model.G;
-    x = G.cells.centroids;
+    
     p = nan(G.cells.num, numel(W));
+    if strcmpi(opt.type, 'topological')
+        N = G.faces.neighbors;
+        N = N(all(N>0,2),:);
+        M = getConnectivityMatrix(N);
+    end
+        x = G.cells.centroids;
     for i = 1:numel(W)
-        xw = x(W(i).cells,:);
-        cells = any(pdist2(x, xw) < opt.rWell,2);
+        switch opt.type
+            case 'metric'
+                xw    = x(W(i).cells,:);
+                cells = any(pdist2(x, xw) < opt.rWell,2);
+            case 'topological'
+                cells = false(G.cells.num,1);
+                cells(W(i).cells) = true;
+                for j = 1:opt.rWell
+                    cells = cells | M*cells;
+                end
+        end
         p(:,i) = cells;
     end
-    pw = zeros(G.cells.num, 1);
-    for i = 1:numel(W)
-        pi = p(:,i);
-        for j = 1:numel(W)
-            pj = p(:,j);
-            if any(pi & pj)
-                cells = pi | pj;
-                pw(cells) = max(i,j);
-                [p(:,i), p(:,j)] = deal(cells);
+    
+    partition = zeros(G.cells.num, 1);
+    if opt.splitWells
+        for i = 1:numel(W)
+            pi = p(:,i);
+            partition(pi > 0) = i;
+            for j = 1:numel(W)
+                pj = p(:,j);
+                if any(pi & pj)
+                    cells = pi | pj;
+                    partition(cells) = max(i,j);
+                    [p(:,i), p(:,j)] = deal(cells);
+                end
             end
         end
+        isWell = partition > 0;
+        partition = compressPartition(partition);
+    else
+        partition(any(p,2)) = 1;
     end
-    is_well = pw > 0;
-    pw = compressPartition(pw);
+    
 end
