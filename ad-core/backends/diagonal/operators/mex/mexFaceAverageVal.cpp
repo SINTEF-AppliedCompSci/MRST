46e5db51a71c2d329e79f456c460a1be46163bf8
Olav Møyner <olav.moyner@gmail.com>
2021-02-24 09:26:26 +0100

    Update mexFaceAverageVal.cpp
    Move error checking out for OCT routine

--- ad-core/backends/diagonal/operators/mex/mexFaceAverageVal.cpp
+++ ad-core/backends/diagonal/operators/mex/mexFaceAverageVal.cpp
@@ -24,6 +24,29 @@ void faceAverage(const int nf, const int nc, const int dim, const double* value,
     }
 }
 
+const char* inputCheck(const int nin, const int nout, int & status_code){
+    if (nin == 0) {
+        if (nout > 0) {
+            status_code = -1;
+            return "Cannot give outputs with no inputs.";
+        }
+        // We are being called through compilation testing. Just do nothing.
+        // If the binary was actually called, we are good to go.
+        status_code = 1;
+        return "";
+    } else if (nin != 2) {
+        status_code = -2;
+        return "2 input arguments required.";
+    } else if (nout > 1) {
+        status_code = -3;
+        return "Wrong number of output arguments.";
+    } else {
+        // All ok.
+        status_code = 0;
+        return "";
+    }
+}
+
 #ifdef MRST_OCTEXT
     /* OCT gateway */
     DEFUN_DLD (mexFaceAverageVal, args, nargout,
@@ -31,26 +54,21 @@ void faceAverage(const int nf, const int nc, const int dim, const double* value,
     {
         const int nrhs = args.length();
         const int nlhs = nargout;
-        if (nrhs == 0) {
-            if (nlhs > 0) {
-                error("Cannot give outputs with no inputs.");
-            }
-            // We are being called through compilation testing. Just do nothing.
-            // If the binary was actually called, we are good to go.
+        int status_code = 0;
+        auto msg = inputCheck(nrhs, nlhs, status_code);
+        if(status_code < 0){
+            // Some kind of error
+            error(msg);
+        }else if (status_code == 1){
+            // Early return
             return octave_value_list();
-        } else if (nrhs != 2) {
-            error("2 input arguments required.");
-        } else if (nlhs > 1) {
-            error("Wrong number of output arguments.");
         }
-        
+
         const NDArray value_nd = args(0).array_value();
         const NDArray N_nd = args(1).array_value();
         
         const double * value = value_nd.data();
         const double * N = N_nd.data();
-        // double * value = mxGetPr(prhs[0]);
-        // double * N = mxGetPr(prhs[1]);
         const dim_vector sz = value_nd.dims();
         const dim_vector nsz = N_nd.dims();
         
@@ -58,15 +76,9 @@ void faceAverage(const int nf, const int nc, const int dim, const double* value,
         int nc = sz(0);
         
         int nf = nsz(0);
-        /*
-        int dim = mxGetN(prhs[0]);
-        int nc = mxGetM(prhs[0]);
-        int nf = mxGetM(prhs[1]);
-        */
+
         NDArray output({nf, dim}, 0);
         double * result = output.fortran_vec();
-        // plhs[0] = mxCreateDoubleMatrix(nf, dim, mxREAL);
-        // double * result = mxGetPr(plhs[0]);
 
        faceAverage(nf, nc, dim, value, N, result);
        return octave_value (output);
