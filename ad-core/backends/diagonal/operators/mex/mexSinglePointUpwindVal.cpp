62f0280dd4d64101deed8dd445509d8011d7931a
Olav Møyner <olav.moyner@gmail.com>
2021-02-25 10:54:34 +0100

    Update mexSinglePointUpwindVal.cpp
    Implement OCT-version

--- ad-core/backends/diagonal/operators/mex/mexSinglePointUpwindVal.cpp
+++ ad-core/backends/diagonal/operators/mex/mexSinglePointUpwindVal.cpp
@@ -2,12 +2,18 @@
 // include necessary system headers
 //
 #include <cmath>
-#include <mex.h>
 #include <array>
 #ifdef _OPENMP
     #include <omp.h>
 #endif
 #include <iostream>
+#ifdef MRST_OCTEXT
+    #include <octave/oct.h>
+    #include <octave/dMatrix.h>
+    #include <octave/boolMatrix.h>
+#else
+    #include <mex.h>
+#endif
 // In: Cell value (nc x d), N (nf x 2), flag (nf x 1) bool
 // Out: Face value of (nf x d)
 
@@ -52,34 +58,72 @@ const char* inputCheck(const int nin, const int nout, int & status_code){
     }
 }
 
-/* MEX gateway */
+#ifdef MRST_OCTEXT
+    /* OCT gateway */
+    DEFUN_DLD (mexSinglePointUpwindVal, args, nargout,
+               "Face upwind operator for MRST - value only.")
+    {
+        const int nrhs = args.length();
+        const int nlhs = nargout;
+        int status_code = 0;
+        auto msg = inputCheck(nrhs, nlhs, status_code);
+        if(status_code < 0){
+            // Some kind of error
+            error(msg);
+        }else if (status_code == 1){
+            // Early return
+            return octave_value_list();
+        }
 
-void mexFunction( int nlhs, mxArray *plhs[], 
-		  int nrhs, const mxArray *prhs[] )
-     
-{ 
-    int status_code = 0;
-    auto msg = inputCheck(nrhs, nlhs, status_code);
-    if(status_code < 0){
-        // Some kind of error
-        mexErrMsgTxt(msg);
-    }else if (status_code == 1){
-        // Early return
+        const NDArray value_nd = args(0).array_value();
+        const NDArray N_nd = args(1).array_value();
+        
+        const double * value = value_nd.data();
+        const double * N = N_nd.data();
+        const dim_vector sz = value_nd.dims();
+        const dim_vector nsz = N_nd.dims();
+        
+        int dim = sz(1);
+        int nc = sz(0);
+        int nf = nsz(0);
+
+        // Shouldn't this be boolNDArray or something? At least it does work.
+        const NDArray flag_nd = args(2).array_value();
+        const double * flag = flag_nd.data();
+
+        NDArray output({nf, dim});
+        double * result = output.fortran_vec();
+
+        upwind<double>(nf, nc, dim, value, N, flag, result);
+        return octave_value (output);
+    }
+#else
+    /* MEX gateway */
+    void mexFunction( int nlhs, mxArray *plhs[], 
+            int nrhs, const mxArray *prhs[] )
+        
+    { 
+        int status_code = 0;
+        auto msg = inputCheck(nrhs, nlhs, status_code);
+        if(status_code < 0){
+            // Some kind of error
+            mexErrMsgTxt(msg);
+        }else if (status_code == 1){
+            // Early return
+            return;
+        }
+        double * value = mxGetPr(prhs[0]);
+        double * N = mxGetPr(prhs[1]);
+        mxLogical * flag = mxGetLogicals(prhs[2]);
+
+        int dim = mxGetN(prhs[0]);
+        int nc = mxGetM(prhs[0]);
+        int nf = mxGetM(prhs[1]);
+            
+        plhs[0] = mxCreateUninitNumericMatrix(nf, dim, mxDOUBLE_CLASS, mxREAL);
+        double * result = mxGetPr(plhs[0]);
+        
+        upwind<mxLogical>(nf, nc, dim, value, N, flag, result);
         return;
     }
-    double * value = mxGetPr(prhs[0]);
-    double * N = mxGetPr(prhs[1]);
-    mxLogical * flag = mxGetLogicals(prhs[2]);
-
-    int dim = mxGetN(prhs[0]);
-    int nc = mxGetM(prhs[0]);
-    int nf = mxGetM(prhs[1]);
-        
-    plhs[0] = mxCreateUninitNumericMatrix(nf, dim, mxDOUBLE_CLASS, mxREAL);
-    double * result = mxGetPr(plhs[0]);
-    
-    upwind<mxLogical>(nf, nc, dim, value, N, flag, result);
-    return;
-}
-
-
+#endif
