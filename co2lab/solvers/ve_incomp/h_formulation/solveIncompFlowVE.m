bf81c6a68ead2f94f6f50afa9e8fb74e89b42274
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2021-02-25 13:46:34 +0100

    Remove end-of-line comma in if statements and unused argument in function call. No functional changes.

--- co2lab/solvers/ve_incomp/h_formulation/solveIncompFlowVE.m
+++ co2lab/solvers/ve_incomp/h_formulation/solveIncompFlowVE.m
@@ -177,9 +177,9 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
                 'MatrixOutput', false);
    opt = merge_options(opt, varargin{:});
 
-   do_solve = check_input(g, opt);
+   do_solve = check_input(opt);
 
-   if do_solve,
+   if do_solve
       cellNo  = rldecode(1:g.cells.num, diff(g.cells.facePos), 2) .';
       s.C     = sparse(1:numel(cellNo), cellNo, 1);
       s.D     = sparse(1:numel(cellNo), double(g.cells.faces(:,1)), 1, ...
@@ -200,7 +200,7 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
       % No driving forces, set flux to zero and remove pressure
       state.flux(:) = 0;
       state.pressure = [];
-      if isfield(state, 'facePressure'),
+      if isfield(state, 'facePressure')
          state.facePressure = [];
       end
    end
@@ -210,16 +210,16 @@ end
 % Helpers follow.
 %--------------------------------------------------------------------------
 
-function do_solve = check_input(g, opt)
+function do_solve = check_input(opt)
    pressure_bc = ~isempty(opt.bc) && ...
                  any(strcmpi('pressure', opt.bc.type));
    well_bhp    = ~isempty(opt.wells) && ...
                  any(strcmpi('bhp', { opt.wells.type }));
-   if ~(pressure_bc || well_bhp),
+   if ~(pressure_bc || well_bhp)
       sum_rates = 0;
       if ~isempty(opt.wells), sum_rates = sum(vertcat(opt.wells.val)); end
       if ~isempty(opt.bc), sum_rates = sum_rates + sum(opt.bc.value);  end
-      if abs(sum_rates) > eps,
+      if abs(sum_rates) > eps
          error(id('MassBalance:NotFulfilled'), ...
               ['Well rates and flux bc must sum up to 0 \n', ...
                'when there are no bhp constrained wells or pressure bc.\n']);
@@ -236,7 +236,7 @@ function do_solve = check_input(g, opt)
                             isempty(opt.src),   ...
                             isempty(opt.wells), ...
                             isempty(opt.rhs)]);
-   if ~do_solve,
+   if ~do_solve
       warning(id('DrivingForce:Missing'),                      ...
              ['\nNo external driving forces present in model--', ...
               'state remains unchanged: state.flux = 0, state.pressure = [].\n']);
@@ -306,7 +306,7 @@ function [A, b, dF, dC] = build_system(state, g, s, w, bc, src, fluid, rock, opt
    A{3} = blkdiag(Ar{3}, Aw{3});  b{3} = vertcat(br{3}, bw{2});
 
    % If rhs is supplied by user, replace b constructed above by given rhs
-   if ~isempty(opt.rhs),
+   if ~isempty(opt.rhs)
       assert (iscell(opt.rhs));
       assert (numel(b) == numel(opt.rhs));
       assert (all(cellfun(@(u,v) all(size(u) == size(v)), b, opt.rhs)));
@@ -327,14 +327,14 @@ end
 function solver = pick_solver(g, s, dF, dC, opt)
    regul = ~any(dF);  % Set zero level if no prescribed pressure values.
 
-   switch lower(opt.Solver),
-      case 'hybrid',
-         if ~any(strcmp(s.type, {'hybrid', 'comp_hybrid'})),
+   switch lower(opt.Solver)
+      case 'hybrid'
+         if ~any(strcmp(s.type, {'hybrid', 'comp_hybrid'}))
             error(id('SolverMode:Inconsistent'), ...
                  ['Solver mode ''hybrid'' is incompatible with ', ...
                   'linear system type ''%s''.'], s.type);
          end
-
+         
          solver = @solve_hybrid;
 %       case 'mixed',
 %          if ~any(strcmp(s.type, {'mixed', 'tpfa', 'comp_hybrid'})),
@@ -352,7 +352,7 @@ function solver = pick_solver(g, s, dF, dC, opt)
 %          end
 %
 %          solver = @(A,b) solve_mixed(A, b, @tpfSymm);
-      otherwise,
+       otherwise
          error(id('SolverMode:NotSupported'), ...
                'Solver mode ''%s'' is not supported.', opt.solver);
    end
@@ -396,19 +396,19 @@ end
 %--------------------------------------------------------------------------
 
 function state = pack_solution(state, g, s, flux, pres, lam, opt)
-   if ~isfield(state, 'facePressure'),
+   if ~isfield(state, 'facePressure')
       state.facePressure = zeros([g.faces.num, 1]);
    end
    state.pressure(:)     = pres;
    state.facePressure(:) = lam (1 : s.sizeD(2));
    state.flux(:)         = cellFlux2faceFlux(g, flux(1 : s.sizeB(1)));
 
-   if ~isempty(opt.wells),
+   if ~isempty(opt.wells)
       nw  = numel(opt.wells);
       i_f = s.sizeB(1);
       i_p = s.sizeD(2);
 
-      for k = 1 : nw,
+      for k = 1 : nw
          nperf = numel(opt.wells(k).cells);
 
          state.wellSol(k).flux     = -flux(i_f + 1 : i_f + nperf);
@@ -427,15 +427,15 @@ function [A, b, dF, dC] = syscomp_res(g, s, mob, omega, pc, state, bc, src, opt)
 
    mob = spdiags(s.C * mob, 0, s.sizeB(1), s.sizeB(2));
 
-   if strcmpi(opt.Solver, 'hybrid'),
-      if ~isfield(s, 'BI'),
+   if strcmpi(opt.Solver, 'hybrid')
+      if ~isfield(s, 'BI')
          error(id('SolverMode:Inconsistent'), ...
               ['Solver mode ''hybrid'' is incompatible with ', ...
                'linear system type ''%s''.'], s.type);
       end
       A{1} = mob * s.BI;
    else
-      if ~isfield(s, 'B'),
+      if ~isfield(s, 'B')
          error(id('SolverMode:Inconsistent'), ...
               ['Solver mode ''%s'' is incompatible with ', ...
                'linear system type ''%s''.'], opt.Solver, s.type);
@@ -459,7 +459,7 @@ function [A, b, dF, dC] = syscomp_wells(G, W, mob, omega,pc, state, opt)
    dF = logical([]);
    dC = [];
 
-   if ~isempty(W),
+   if ~isempty(W)
       % but fill in values when there nevertheless are some...
       nperf = cellfun(@numel, { W.cells });
       wc    = vertcat(W.cells);   n = numel(wc);   i = 1 : n;
@@ -506,7 +506,7 @@ function nF = neumann_faces(g, dF, opt)
    nF(any(g.faces.neighbors == 0, 2)) = true;  % All external faces ...
    nF(dF(1 : g.faces.num))            = false; % ... except Dirichlet cond.
 
-   if ~isempty(opt.wells),
+   if ~isempty(opt.wells)
       % Additionally include all rate-constrained wells.
       nF = [nF; strcmpi('rate', { opt.wells.type } .')];
    end
@@ -519,7 +519,7 @@ function Do = oriented_mapping(s, orient, opt)
 % the reduced, mixed system of linear equtions in linear system solver
 % functions 'mixedSymm' and 'tpfSymm'.
 %
-   if ~isempty(opt.wells),
+   if ~isempty(opt.wells)
       n  = sum(cellfun(@numel, { opt.wells.cells }));
       dw = speye(n);
       ow = ones([n, 1]);
