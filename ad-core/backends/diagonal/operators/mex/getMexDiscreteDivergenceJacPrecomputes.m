4d8dde3bb225c0e67df6e12591a9e30da6245ffa
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2021-03-05 23:46:41 +0100

    Divergence Mappings: Switch to Vectorised Approach
    This commit trades memory for speed using SORTROWS on a larger
    array to guarantee the appropriate per-cell grouping behaviour
    for the connecting cells and faces.  We also infer the local
    position of the diagonal element by a counting procedure instead
    of through FIND.
    
    This brings nearly a factor 6.5 reduction of the runtime setting
    up the mappings on a real field case with 372,588 active cells
    and 1,269,007 inter-cell connections.
    
    We nevertheless need to pay attention to the increased memory
    requirements and my have to refine this approach if that becomes
    a limiting resource for larger models.

--- ad-core/backends/diagonal/operators/mex/getMexDiscreteDivergenceJacPrecomputes.m
+++ ad-core/backends/diagonal/operators/mex/getMexDiscreteDivergenceJacPrecomputes.m
@@ -1,5 +1,44 @@
 function out = getMexDiscreteDivergenceJacPrecomputes(model)
-%Undocumented Utility Function
+%Compute Mapping Primitives for Accelerated Discrete Divergence Operator
+%
+% SYNOPSIS:
+%   map = getMexDiscreteDivergenceJacPrecomputes(model)
+%
+% PARAMETERS:
+%   model - Discretised physical model including neighbourship information
+%
+% RETURNS:
+%   map - Mapping primitives.  Structure with the following fields:
+%           - facePos   - (#cells + 1)-by-1 indirection map into connection
+%                         information.  Plays the same role as the field
+%                         with the same name in the `grid_structure`.
+%
+%           - faces     - Zero-based index of cells' connecting interfaces.
+%                         The connecting faces of cell 'i' are
+%
+%                            faces(facePos(i) : facePos(i + 1) - 1)
+%
+%           - cells     - One-based index of connecting/neighbouring cells.
+%                         The connecting/neighbouring cells of cell 'i' are
+%
+%                            cells(facePos(i) : facePos(i + 1) - 1)
+%
+%                         Moreover, the 'cells' array encodes the direction
+%                         of flow through a sign convention.  In particular
+%                         the neigbouring cell index is listed as POSITIVE
+%                         if the neighbour is a source of flow INTO cell
+%                         'i' and NEGATIVE if the flow is nominally OUT of
+%                         cell 'i'.
+%
+%           - cellIndex - Zero-based non-zero position of diagnoal matrix
+%                         element corresponding to each cell.  Integer array
+%                         of size #cells-by-1.
+%
+% NOTE:
+%   This function uses SORTROWS.
+%
+% SEE ALSO:
+%   `mexDiscreteDivergenceJac`, `grid_structure`, sortrows.
 
 %{
 Copyright 2009-2020 SINTEF Digital, Mathematics & Cybernetics.
@@ -20,55 +59,23 @@ You should have received a copy of the GNU General Public License
 along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
 
-    G = model.G;
-    N = model.operators.N;
-    % Use operators.N
-    f = (1:size(N, 1))';
-    pos = sortrows([[N(:, 1); N(:, 2)], [f; f]]); % Sort cell -> face maps
-    faces = pos(:, 2);
-    cellNo = pos(:, 1);
-    
-    lc = N(faces, 1);
-    rc = N(faces, 2);
-    left = (lc ~= cellNo);
-    cells = left.*lc + ~left.*rc;
-    cells(~left) = -cells(~left);
+    nc = model.G.cells.num;
+    N  = model.operators.N;
 
-    [c, ps] = rlencode(cellNo);
-    if numel(ps) ~= G.cells.num
-        % we have some neighborless cells, insert zeros in ps accordingly
-        cix = false(G.cells.num, 1);
-        cix(cellNo) = true;
-        tmp = ps;
-        ps = zeros(G.cells.num,1);
-        ps(cix) = tmp;
-    end
-        
-    facePos = cumsum([1; ps]);
+    f = (1 : size(N, 1)) .';
 
-    localCellIndex = zeros(G.cells.num, 1);
-    dispif(mrstVerbose(), 'Starting MEX divergence preparation.\n');
-    for i = 1:G.cells.num
-        if mod(i, 1000) == 0
-            dispif(mrstVerbose(), '%d of %d cells processed.\n', i, G.cells.num)
-        end
-        loc = facePos(i):facePos(i+1)-1;
-        ca = cells(loc);
-        [~, ix] = sort(abs(ca));
+    % [ self, other, faceID ]
+    cell_connections = sortrows([[N ; fliplr(N)], [f ; f]]);
+    sum_to_cell = @(x) accumarray(cell_connections(:, 1), x, [nc, 1]);
 
-        tmp = faces(loc);
-        faces(loc) = tmp(ix);
-        cells(loc) = ca(ix);
+    % Positive if 'self' is in second column of N, negative otherwise.
+    sign = 2*(N(cell_connections(:, 3), 1) ~= cell_connections(:, 1)) - 1;
 
-        tmp = find(abs(ca(ix)) > i, 1, 'first')-1;
-        if isempty(tmp)
-            tmp = numel(ca);
-        end
-        localCellIndex(i) = tmp;
-    end
-    dispif(mrstVerbose(), 'Done with MEX divergence preparation.\n');
-    % Need to sort cells as well
-    fpos0 = facePos-1;
-    faces = faces - 1;
-    out = struct('facePos', fpos0, 'faces', faces, 'cells', cells, 'cellIndex', localCellIndex);
+    localCellIndex = ...
+       sum_to_cell(cell_connections(:, 2) < cell_connections(:, 1));
+
+    out = struct('facePos'  , cumsum([0 ; sum_to_cell(1)]) , ...
+                 'faces'    , cell_connections(:,3)  - 1   , ...
+                 'cells'    , sign .* cell_connections(:,2), ...
+                 'cellIndex', localCellIndex);
 end
