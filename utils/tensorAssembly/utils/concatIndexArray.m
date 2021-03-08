69512e4de16a7832d5e79fe8d7df2fe817d63f32
Xavier Raynaud <xav.raynaud@gmail.com>
2021-03-08 17:18:06 +0100

    improved IndexArray concatenation

--- utils/tensorAssembly/utils/concatIndexArray.m
+++ utils/tensorAssembly/utils/concatIndexArray.m
@@ -1,6 +1,7 @@
 function tbl = concatIndexArray(tbl1, tbl2, fdnames, varargin)
     
-    opt = struct('checkUnique', true);
+    opt = struct('checkUnique', true, ...
+                 'removeDuplicates', false);
     opt = merge_options(opt, varargin{:}); 
     
     inds1 = tbl1.gets(fdnames);
@@ -8,10 +9,14 @@ function tbl = concatIndexArray(tbl1, tbl2, fdnames, varargin)
 
     inds = [inds1; inds2];
     
-    if opt.checkUnique
+    if (opt.checkUnique) & ~opt.removeDuplicates
         indstest = unique(inds, 'rows');
         assert(size(indstest, 1) == size(inds, 1), 'there are repeated indices');
     end    
+    
+    if opt.removeDuplicates
+        inds = unique(inds, 'rows');
+    end
     
     tbl = IndexArray([], 'inds', inds, 'fdnames', fdnames);
     
