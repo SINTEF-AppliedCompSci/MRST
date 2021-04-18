316a49bb5ef5122986c21dd21625711fc43959a6
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2021-04-18 21:51:39 +0200

    Autodiff: Insert Copyright Where Missing

--- hybrid-ve/utils/getWaterPressureFromHeight.m
+++ hybrid-ve/utils/getWaterPressureFromHeight.m
@@ -7,6 +7,25 @@ function pW_local = getWaterPressureFromHeight(pt, t, B, b, h, pW, g, rhow, rhog
     % pW is the water pressure at the bottom of the cell, g the gravity
     % magnitude and rhow/rhog the water and gas densities.
 
+%{
+Copyright 2009-2021 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
     dz_below = (b - pt);
 
     h_below = max(t + h - pt, 0);
