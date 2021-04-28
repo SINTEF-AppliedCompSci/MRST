58260578179f60d2e334cb3f4293df8cb7b9129f
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2021-04-28 19:19:29 +0200

    Update makeWellPartition.m

--- utils/makeWellPartition.m
+++ utils/makeWellPartition.m
@@ -5,38 +5,43 @@ function [partition, isWell] = makeWellPartition(G, W, varargin)
                  'use2d'     , false   , ...
                  'splitWells', true    );
     opt = merge_options(opt, varargin{:});
-    
+    % Initialize partition
     p = nan(G.cells.num, numel(W));
-    if strcmpi(opt.type, 'topological')
-        N = G.faces.neighbors;
-        N = N(all(N>0,2),:);
-        M = getConnectivityMatrix(N);
-    end
+    % Get grid connectivity
+    N = G.faces.neighbors;
+    N = N(all(N>0,2),:);
+    M = getConnectivityMatrix(N);
     if opt.use2d, cix = 1:2; else, cix = ':'; end
     x = G.cells.centroids(:, cix);
     for i = 1:numel(W)
+        % Add well cells for well i
+        cells = false(G.cells.num,1);
+        cells(W(i).cells) = true;
+        % Make sure we add at least one layer of cells
+        cells = cells | M*cells;
+        % Add extra cells according to type
         switch opt.type
             case 'metric'
                 xw    = x(W(i).cells,:);
-                cells = any(pdist2(x, xw) < opt.rWell,2);
+                cells = cells | any(pdist2(x, xw) < opt.rWell,2);
             case 'topological'
-                cells = false(G.cells.num,1);
-                cells(W(i).cells) = true;
-                for j = 1:opt.rWell
+                for j = 2:opt.rWell
                     cells = cells | M*cells;
                 end
         end
         p(:,i) = cells;
     end
-    
+    % Make partition
     partition = zeros(G.cells.num, 1);
     if opt.splitWells
         for i = 1:numel(W)
             pi = p(:,i);
             partition(pi > 0) = i;
             for j = 1:numel(W)
+                if i == j, continue; end
                 pj = p(:,j);
                 if any(pi & pj)
+                    % Merge intersecting domains
                     cells = pi | pj;
                     partition(cells) = max(i,j);
                     [p(:,i), p(:,j)] = deal(cells);
