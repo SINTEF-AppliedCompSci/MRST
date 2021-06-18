1fea76f671a060e05418256826c991b919e0f5f0
August Johansson <august.johansson@sintef.no>
2021-06-18 10:36:24 +0200

    Fix empty default

--- utils/inout/convertInputUnits.m
+++ utils/inout/convertInputUnits.m
@@ -42,7 +42,7 @@ You should have received a copy of the GNU General Public License
 along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
 
-    opt = struct('exclude', []);
+    opt = struct('exclude', {{}});
     opt = merge_options(opt, varargin{:});
 
     keywords = reshape(fieldnames(grdecl), 1, []);
