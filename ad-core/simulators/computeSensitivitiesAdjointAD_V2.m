2ef4c46bc2c10766dac52ef43753c5cf396e20ee
Manuel <you@example.com>
2021-06-23 10:14:56 +0200

    Adding sensitivity for multiplier

--- ad-core/simulators/computeSensitivitiesAdjointAD_V2.m
+++ ad-core/simulators/computeSensitivitiesAdjointAD_V2.m
@@ -100,10 +100,17 @@ for step = nt:-1:1
                     sens.(nm) = sens.(nm) + eqdth{nl}.jac{kp}'*lami{nl};
                 end
             end
-        end
+        end        
     end
 end
 
+% % multiply by value if type is multiplier:
+for kp = 1:numel(param)
+    nm = param{kp}.name;
+    if strcmp(param{kp}.type, 'multiplier')
+        sens.(nm) = (sens.(nm)).*param{kp}.getParameterValue(problem);
+    end
+end
 % compute partial derivative of first eq wrt init state and compute
 % initial state sensitivities
 if any(isInitStateParam)
@@ -130,6 +137,9 @@ if any(isInitStateParam)
         end
     end
 end
+       
+
+
 end
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
