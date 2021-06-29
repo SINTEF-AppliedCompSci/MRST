6b90113b5a4f99de32524796d67b609f6a0b11ca
Manuel <you@example.com>
2021-06-29 14:33:41 +0200

    Adding other gradients options

--- optimization/utils/evaluateMatch_simple.m
+++ optimization/utils/evaluateMatch_simple.m
@@ -1,4 +1,34 @@
-function [misfitVal,varargout] = evaluateMatch_simple(p, obj, state0_org ,model_org,schedule_org,objScaling,parameters, states_ref)
+function [misfitVal,varargout] = evaluateMatch_simple(p, obj, state0_org ,model_org,schedule_org,objScaling,parameters, states_ref,varargin)
+%Undocumented Utility Function
+
+%{
+Copyright 2009-2021 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
+opt = struct('Verbose',           mrstVerbose(),...
+    'Gradient',   'AdjointAD',...
+    'NonlinearSolver', [],...
+    'AdjointLinearSolver',[],...
+    'pertub',1e-4,...
+    'stepchange',false);
+
+opt = merge_options(opt, varargin{:});
+
 
 nparam = cellfun(@(x)x.n, parameters);
 p = max(0, min(1, p));
@@ -27,14 +57,33 @@ misfitVal  = - sum(vertcat(misfitVals{:}))/objScaling ;
 
 if nargout > 1
     objh = @(tstep,model,state) obj(model, states, SimulatorSetup.schedule, states_ref, true, tstep, state);
-    gradient = computeSensitivitiesAdjointAD_V2(SimulatorSetup, states, parameters, objh);
-    % do scaling of gradient
-    nms = applyFunction(@(x)x.name, parameters);
-    scaledGradient = cell(numel(nms), 1);
-    for k = 1:numel(nms)
-       scaledGradient{k} = parameters{k}.scaleGradient( gradient.(nms{k}), p{k});
+
+    switch opt.Gradient
+        case 'none'
+            if nargout > 2
+                [varargout{2:3}] = deal(wellSols, states);
+            end
+            return
+        case 'AdjointAD'
+            gradient = computeSensitivitiesAdjointAD_V2(SimulatorSetup, states, parameters, objh);        
+        case  'PerturbationAD'
+            %gradient = computeGradientPerturbationAD(state0, states, model, schedule, objh, ...
+            %    'Parameters'    , {params{:}}, ...
+            %    'ParameterTypes', {paramTypes{:}},...
+            %    'initStateSensitivity',initStateSensitivity);
+            error('Greadient method %s is not implemented',opt.Gradient)
+        case 'PerturbationADNUM'
+            error('Greadient method %s is not implemented',opt.Gradient)
+        otherwise
+            error('Greadient method %s is not implemented',opt.Gradient)
     end
-    varargout{1} = vertcat(scaledGradient{:})/objScaling;
+        % do scaling of gradient
+        nms = applyFunction(@(x)x.name, parameters);
+        scaledGradient = cell(numel(nms), 1);
+        for k = 1:numel(nms)
+           scaledGradient{k} = parameters{k}.scaleGradient( gradient.(nms{k}), p{k});
+        end
+        varargout{1} = vertcat(scaledGradient{:})/objScaling;
 end
 if nargout > 2
     [varargout{2:3}] = deal(wellSols, states);
