e7eff762f39ca9d2fbab2750a49d0b986a810e94
Stein Krogstad <stein.krogstad@sintef.no>
2021-07-06 13:26:58 +0200

    Various cleanup and fixes

--- optimization/utils/evaluateMatch.m
+++ optimization/utils/evaluateMatch.m
@@ -1,68 +1,45 @@
-function [misfitVal,varargout] = evaluateMatch(p, obj, SimulatorSetup_org,parameters, states_ref,varargin)
-% Simulate a model with parameters p and compute mistmatch with respect a reference state
+function [misfitVal,varargout] = evaluateMatch(pvec, obj, setup, parameters, states_ref, varargin)
+% Utility function (for optimization) that simulates a model with parameters obtained 
+% from the vector 'pvec' (scaled parameters) and computes mismatch with respect a 
+% reference output state 'states_ref'
 %
 % SYNOPSIS:
-%   misfitVal = evaluateMatch(p, obj, state0_org ,model_org,schedule_org,parameters, states_ref,varargin)
-%  
-%   [misfitVal,sesitivities] = evaluateMatch(p, obj, SimulatorSetup_org,parameters, states_ref,varargin)
-%  
-%   [misfitVal,sesitivities,wellSols,states] = evaluateMatch(p, obj, SimulatorSetup_org, parameters, states_ref,varargin)
-%
-%   [misfitVal,~,wellSols,states] = evaluateMatch(p, obj, SimulatorSetup_org ,parameters, states_ref,'Gradient','none',varargin)
+%   misfitVal = evaluateMatch(p, obj, setup, parameters, states_ref, ['pn', pv, ...]) 
+%  [misfitVal, sesitivities] = evaluateMatch(...) 
+%  [misfitVal, sesitivities, wellSols, states] = evaluateMatch(...) 
+%  [misfitVal, ~, wellSols, states] = evaluateMatch(...,'Gradient','none')
 %
 % DESCRIPTION:
 %   For a given parameter array p, compute mistmach and sesitivities with regards to parameter p
 %
 % REQUIRED PARAMETERS:
-%   p            - An array containing the parameters' values scaled in unit-interval [0 ,1]
-%
+%   pvec         - An array containing the parameters' values scaled in unit-interval [0 ,1]
 %   obj          - Objective function that evaluates the diferences/match between
 %                  states evaluated at parameters p (states(p)) and the reference state states_ref
-%   
-%   SimulatorSetup_org - structure containing:
-%       state0   - Physical model state at `t = 0`
-%       model    - Subclass of PhysicalModel class such as
-%                  `GenericBlackOilModel` that models the physical
-%                  effects we want to study.
-%
-%       schedule - Schedule suitable for `simulateScheduleAD`.
-%
-%   parameters   - array of parameters of class ModelParameter
-%
+%   setup        - Simulation setup structure containing: state0, model, and schedule.
+%   parameters   - cell-array of parameters of class ModelParameter
 %   states_ref   - Physical model states corresponding to the reference.
-%
 %
 % OPTIONAL PARAMETERS:
 %   'Gradient'       - Method to calculate the sensitivities/gradient:
 %                      'AdjointAD':        Compute parameter sensitivities
 %                                          using adjoint simulation  (default)
-%
 %                      'PerturbationADNUM': Compute parameter sensitivities
 %                                           using perturbations (first-order forward finite diferences)
-%
 %                      'none':              Avoind computing parameters sensitivities
-%
 %   'PerturbationSize'- small value <<1 to perturb parameter p (default = 1e-7)
-%
 %   'objScaling'   - scaling value for the objective function obj/objScaling.
-%
-%
 %   'AdjointLinearSolver' - Subclass of `LinearSolverAD` suitable for solving the
-%                      adjoint systems.
-%
+%                      adjoint linear systems.
 %   'NonlinearSolver'- Subclass of `NonLinearSolver` suitable for solving the
-%                      non linear sistem systems of the forward model.
-%
+%                      non linear systems of the forward model.
 %   'Verbose'         - Indicate if extra output is to be printed such as
 %                       detailed convergence reports and so on.
 % RETURNS:
 %   misfitVal        - Diference between states(p) and states_ref
-%
 %   sesitivities     - Gradient of misfitVal with respect p
-%
 %   wellSols         - Well solution at each control step (or timestep if
 %                      'OutputMinisteps' is enabled.)
-%
 %   states           - State at each control step (or timestep if
 %                      'OutputMinisteps' is enabled.)
 %
@@ -89,39 +66,40 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
 
 opt = struct('Verbose',           mrstVerbose(),...
-    'Gradient',   'AdjointAD',...
-    'NonlinearSolver', [],...
-    'AdjointLinearSolver',[],...
-    'PerturbationSize',1e-7,...
-    'objScaling',1);%,...
-    %'stepchange',false);
+             'Gradient',   'AdjointAD',...
+             'NonlinearSolver', [],...
+             'AdjointLinearSolver',[],...
+             'PerturbationSize',1e-7,...
+             'objScaling',1, ...
+             'enforceBounds',  true);
 
 opt = merge_options(opt, varargin{:});
 
-
-nparam = cellfun(@(x)x.n, parameters);
-p_org=p;
-p = max(0, min(1, p));
-p = mat2cell(p, nparam, 1);
-
-SimulatorSetup = SimulatorSetup_org;
-for k = 1:numel(parameters)
-    pval    = parameters{k}.unscale(p{k});
-    SimulatorSetup = parameters{k}.setParameterValue(SimulatorSetup, pval);
+nparam = cellfun(@(x)x.nParam, parameters);
+p_org = pvec;
+if opt.enforceBounds
+    pvec = max(0, min(1, pvec));
 end
-%SimulatorSetup.model.toleranceCNV = 1e-6;
-% figure(1), plot(SimulatorSetup.model.operators.pv)
-% figure(2), plot(SimulatorSetup.model.operators.T)
-% figure(3), plot(vertcat(SimulatorSetup.schedule.control.W.WI))
-[wellSols,states] = simulateScheduleAD(SimulatorSetup.state0, SimulatorSetup.model, SimulatorSetup.schedule,...
-    'NonLinearSolver',opt.NonlinearSolver,...
-    'Verbose',opt.Verbose);
+pvec = mat2cell(pvec, nparam, 1);
 
-misfitVals = obj(SimulatorSetup.model, states, SimulatorSetup.schedule, states_ref, false, [],[]);
+% Create new setup, and set parameter values
+pval = cell(size(parameters));
+setupNew = setup;
+setupNew.model.FlowDiscretization = [];
+setupNew.model.FlowPropertyFunctions = [];
+for k = 1:numel(parameters)
+    pval{k}  = parameters{k}.unscale(pvec{k});
+    setupNew = parameters{k}.setParameterValue(setupNew, pval{k});
+end
+[wellSols,states] = simulateScheduleAD(setupNew.state0, setupNew.model, setupNew.schedule,...
+                                       'NonLinearSolver',opt.NonlinearSolver,...
+                                       'Verbose',opt.Verbose);
+
+misfitVals = obj(setupNew.model, states, setupNew.schedule, states_ref, false, [],[]);
 misfitVal  = - sum(vertcat(misfitVals{:}))/opt.objScaling ;
 
 if nargout > 1
-    objh = @(tstep,model,state) obj(SimulatorSetup.model, states, SimulatorSetup.schedule, states_ref, true, tstep, state);
+    objh = @(tstep,model,state) obj(setupNew.model, states, setupNew.schedule, states_ref, true, tstep, state);
     nms = applyFunction(@(x)x.name, parameters);
     scaledGradient = cell(numel(nms), 1);
     
@@ -132,20 +110,20 @@ if nargout > 1
             end
             return
         case 'AdjointAD'
-            gradient = computeSensitivitiesAdjointAD(SimulatorSetup, states, parameters, objh,...
+            gradient = computeSensitivitiesAdjointAD(setupNew, states, parameters, objh,...
                                                         'LinearSolver',opt.AdjointLinearSolver);            
             % do scaling of gradient
             for k = 1:numel(nms)
-               scaledGradient{k} = parameters{k}.scaleGradient( gradient.(nms{k}), p{k});
+               scaledGradient{k} = parameters{k}.scaleGradient( gradient.(nms{k}), pval{k});
             end
         case 'PerturbationADNUM'
             % do manual pertubuation of the defiend control variabels
             eps_pert = opt.PerturbationSize;            
-            val=nan(size(p));
+            val=nan(size(pvec));
             try  % Try parallel loop
                 parfor i=1:numel(p_org)
                     val(i) = evaluateMatch(perturb(p_org,i,eps_pert),...
-                         obj,SimulatorSetup_org,parameters, states_ref,...
+                         obj,setup,parameters, states_ref,...
                         'Gradient', 'none',...
                         'NonlinearSolver',opt.NonlinearSolver,...
                         'objScaling',opt.objScaling);
