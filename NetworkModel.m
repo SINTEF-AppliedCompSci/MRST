86c099044ed8bd7f319ad0bcec041ad8357579bf
Manuel A. Borregales R <manuelantoniobr@gmail.com>
2021-07-06 13:29:40 +0200

    We extend the posibility to include more than one well-cell per well

--- NetworkModel.m
+++ NetworkModel.m
@@ -10,13 +10,15 @@ classdef NetworkModel
     end
     
     methods
-        function obj = NetworkModel(model,nc,Graph,W_in,Levels)
-            %GPSNet 
+        function obj = NetworkModel(model,nc,Graph,W_in,varargin)
+            
+            opt = struct('Verbose',  mrstVerbose());                   
+            opt = merge_options(opt, varargin{:});
+          
             
             %TODO: get numedges and numlevels and numcros and define and model
             % using fluid and rock
             
-            Levels =  1; % Variable non beeing used at the moment.
             
             %   Detailed explanation goes here
             obj.model = model;
@@ -28,33 +30,34 @@ classdef NetworkModel
             T(number_of_vertical_faces+1 :end) = 0;
             Graph.Nodes.Face_Indx =0*(1:numnodes(Graph))';%TODO: check this, include general counts for nodes
 
-
+            Well_cells = Graph.Nodes.Well_cells; %TODO Initialize better
             nf =  nc -1;
-            for i_ed =  1:size(Graph.Edges.EndNodes,1) % TODO: Add Total  number of edges
+            for i_ed =  1:numedges(Graph) 
                 
                 %% Savinf cell indexs
-                fprintf('Cell indexs Edge %i, %i-%i \n',i_ed,Graph.Edges.EndNodes(i_ed,1),Graph.Edges.EndNodes(i_ed,2));   
+                dispif(opt.Verbose,'Cell indexs Edge %i, %i-%i \n',i_ed,Graph.Edges.EndNodes(i_ed,1),Graph.Edges.EndNodes(i_ed,2));   
                 [Cell_1 , Cell_nc] = deal( 1+(i_ed-1)*nc  ,...
                                           nc+(i_ed-1)*nc) ;                                      
                 Graph.Edges.Cell_Indices{i_ed} = Cell_1+1 : Cell_nc-1;                                
                 %% Saving internal faces indexs
-                fprintf('in-Faces indexs Edge %i, %i-%i \n',i_ed,Graph.Edges.EndNodes(i_ed,1),Graph.Edges.EndNodes(i_ed,2));
+                dispif(opt.Verbose,'in-Faces indexs Edge %i, %i-%i \n',i_ed,Graph.Edges.EndNodes(i_ed,1),Graph.Edges.EndNodes(i_ed,2));
                 
                 [Node_1,Node_nc] = deal(Graph.Edges.EndNodes(i_ed,1),...
-                                       Graph.Edges.EndNodes(i_ed,2));
+                                        Graph.Edges.EndNodes(i_ed,2));
                 % Check if the firs node was already defined and has its
                 % face number index stored
                 if Graph.Nodes.Face_Indx(Node_1)== 0                    
                     
-                    fprintf(2,'Cell number %i, will have Well %s \n',Cell_1 ,W_in(Node_1).name); 
+                    dispif(opt.Verbose,2,'Cell number %i, will have Well %s \n',Cell_1 ,W_in(Graph.Nodes.Well(Node_1)).name); 
                         
                     First_Index =  1+(i_ed-1)*nf;  
                     Graph.Nodes.Face_Indx(Node_1) = First_Index;
-                    Graph.Nodes.Well_cells(Node_1)= Cell_1;
+                    Well_cells{Graph.Nodes.Well(Node_1)} = [Well_cells{Graph.Nodes.Well(Node_1)},Cell_1];
+                    Graph.Nodes.Well_cells{Node_1}= [Graph.Nodes.Well_cells{Node_1},Cell_1];
                 else                    
                     %Create New conectivity
                     N = [N;...
-                         Graph.Nodes.Well_cells(Node_1), Cell_1+1]; % New artificial face
+                         Graph.Nodes.Well_cells{Node_1}(end), Cell_1+1]; % New artificial face
                     T = [T;T(1)];
                     
                     First_Index = length(T); % This the indexa a new face  
@@ -69,14 +72,15 @@ classdef NetworkModel
                 % Check if the last node was already defined and has its
                 % face number index stored
                 if Graph.Nodes.Face_Indx(Node_nc) ==0
-                    fprintf(2,'Cell number %i, will have Well %s \n',Cell_nc ,W_in(Node_nc).name);                     
+                    dispif(opt.Verbose,2,'Cell number %i, will have Well %s \n',Cell_nc ,W_in(Graph.Nodes.Well(Node_nc)).name);                     
                     Last_Index  =  (nc-1)+(i_ed-1)*nf; 
                     Graph.Nodes.Face_Indx(Node_nc) = Last_Index;
-                    Graph.Nodes.Well_cells(Node_nc)= Cell_nc;
+                    Well_cells{Graph.Nodes.Well(Node_nc)} = [Well_cells{Graph.Nodes.Well(Node_nc)},Cell_nc];
+                    Graph.Nodes.Well_cells{Node_nc}= [Graph.Nodes.Well_cells{Node_nc},Cell_nc];
                 else
                      %Create New conectivity
                     N = [N;...
-                         Cell_nc-1, Graph.Nodes.Well_cells(Node_nc)]; % New artificial face
+                         Cell_nc-1, Graph.Nodes.Well_cells{Node_nc}(end)]; % New artificial face
                     T = [T;T(1)];
                     
                     Last_Index = length(T); % This the index of a new face  
@@ -84,56 +88,28 @@ classdef NetworkModel
                                              % Graph.Nodes.Well_cells(Node_1)
                                              % and  Cell_1+1
                 end
-                fprintf('\n');
+                dispif(opt.Verbose,'\n');
                 Graph.Edges.Face_Indices{i_ed} = [First_Index,Internal,Last_Index];                
             end
             
-            
+            % Creating the new well structure.
             W = [];
             for iw = 1 : numel(W_in)
-%                 W_in(iw).cells = Graph.Nodes.Well_cells(iw);
-%                 
-%                 %Reducing the size of well cells to 1
-%                 W_in(iw).r =W_in(iw).r(1) ;
-%                 W_in(iw).rR =W_in(iw).rR(1) ;
-%                 W_in(iw).WI =W_in(iw).WI(1) ;
-%                 W_in(iw).cstatus =W_in(iw).cstatus(1) ;
-%                 W_in(iw).cell_origin =W_in(iw).cell_origin(1) ;
-%                 
-                     W = verticalWell(W, model.G, model.rock, Graph.Nodes.Well_cells(iw),1, ...      
+                 W = addWell(W, model.G, model.rock,  Well_cells{iw}, ...      
                          'Type', W_in(iw).type,...
-                         'Val', W_in(iw).val, ... %  SAIGUP model
-                          'Radius',W_in(iw).r(1),...
+                         'Val', W_in(iw).val, ...
+                          'Radius',W_in(iw).r(1:numel(Well_cells{iw})),...
                           'Name',W_in(iw).name,...
-                          'Comp_i', W_in(iw).compi,...
-                          'sign', W_in(iw).sign)%,...
-                          %'refDepth',W_in(iw).refDepth);
-                      
-%                      W = addWell(W, model.G, model.rock,  Graph.Nodes.Well_cells(iw), ...      
-%                          'Type', W_in(iw).type, 'Val', W_in(iw).val, ... %  SAIGUP model
-%                           'Radius',W_in(iw).r(1),'Name',W_in(iw).name,'Comp_i', W_in(iw).compi,'sign', W_in(iw).sign);
-%                       
-
-                %W(iw).cells = Graph.Nodes.Well_cells(iw); %TODO: Fix the wells 
-
+                          'Comp_i',W_in(iw).compi,...
+                          'sign', W_in(iw).sign);
             end
             
-%             for i =  1: numel(Graph.Nodes.Well)
-%                 iw = Graph.Nodes.Well(i);
-%                 W(iw).cells(Graph.Nodes.SubWell(i)) =  Graph.Nodes.Well_cells(i);
-%             end
-%             
+
             
             obj.Graph =  Graph;
             obj.W     =  W;
             obj.W_in  =  W_in;
-            %obj.model.rock.perm = [0*model.rock.perm, model.rock.perm, 0*model.rock.perm];
-            
-            %obj.model.operators = setupOperatorsTPFA(obj.model.G, ...
-            %                                         obj.model.rock);
-                                                 
-           % obj.model = model.removeStateFunctionGroupings();
-            
+
             obj.model.operators =  setupOperatorsTPFA( obj.model.G,  obj.model.rock,...
                                                       'neighbors',N,...
                                                       'trans',T,...
@@ -149,7 +125,7 @@ classdef NetworkModel
             %   Detailed explanation goes here            
                 plotGrid(Grid, 'FaceColor', 'none', 'EdgeAlpha', 0.1), view(2); 
                 
-                plotWell(Grid,obj.W_in, 'color', 'k','fontsize',0)
+                plotWell(Grid,obj.W_in, 'color', 'k','fontsize',0);
                 
                 if isempty(data)
                    linewidth = 5;
@@ -164,14 +140,14 @@ classdef NetworkModel
                                     'YData',obj.Graph.Nodes.YData,...
                                     'ZData',obj.Graph.Nodes.ZData,...
                                     'LineWidth',linewidth);
-                        labelnode(pg,[1:numnodes(obj.Graph)],obj.Graph.Nodes.Well_name)
+                        labelnode(pg,[1:numnodes(obj.Graph)],obj.Graph.Nodes.Well_name);
                 hold off;
                 % To have text indicating values with text                
                 % plot(Graph,'EdgeLabel',compose("%5.2e",Graph.Edges.Weight),'LineWidth',10*TT/max(TT))
                
                 pg.NodeFontSize= 15;
                 axis off ; 
-                title("Initial Transmisibility")
+                title("Initial Transmisibility");
         end
         
         
