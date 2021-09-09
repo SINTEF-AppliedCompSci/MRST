46e14c7639c69c63a8d4ed2d4a1a61b093603a37
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2021-09-09 17:33:35 +0200

    Insert MRST Copyright Where Missing
    In preparation of MRST 2021b.

--- optimization/utils/addParameter.m
+++ optimization/utils/addParameter.m
@@ -3,6 +3,26 @@ function params = addParameter(params, SimulatorSetup, varargin)
 % parameters.
 % See  ModelParameter.m for more information about the parameters class.
 %
+
+%{
+Copyright 2009-2021 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
+
 p = ModelParameter(SimulatorSetup, varargin{:});
 if isempty(params)
     params = {p};
@@ -14,4 +34,3 @@ else
     error('Unknown format of input ''params''');
 end
 end
-        
