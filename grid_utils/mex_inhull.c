0bb2e4e2bc696010307a99b7dde5f6b82c3fff72
August Johansson <august.johansson@sintef.no>
2021-10-12 12:31:22 +0200

    Add verbose option

--- grid_utils/mex_inhull.c
+++ grid_utils/mex_inhull.c
@@ -1,5 +1,5 @@
 /*
-  mex version of inhull for 3D 
+  mex version of inhull for 3D
 */
 
 #include "mex.h"
@@ -22,7 +22,8 @@ void inhull(const double* const* testpts,
             const double* const* tess,
             mwSize ntess,
             double tol,
-            int *is_inside);
+            int *is_inside,
+            int verbose);
 
 void printer(double a[3])
 {
@@ -35,7 +36,7 @@ double** dalloc(int rows, int cols, double init)
     double** mat = (double **)malloc(rows * sizeof(double*));
     for (i = 0; i < rows; ++i) {
         mat[i] = (double *)malloc(cols * sizeof(double));
-        for (j = 0; j < cols; ++j) 
+        for (j = 0; j < cols; ++j)
             mat[i][j] = init;
     }
     return mat;
@@ -84,7 +85,7 @@ void mean(const double* const* a, int r, double* m)
     for (i = 0; i < r; ++i)
         for (d = 0; d < 3; ++d)
             m[d] += a[i][d];
-    
+
     for (d = 0; d < 3; ++d)
         m[d] /= r;
 }
@@ -108,7 +109,8 @@ void inhull(const double* const* testpts,
             const double* const* tess,
             mwSize ntess,
             double tol,
-            int *is_inside)
+            int *is_inside,
+            int verbose)
 {
     /* This function can be optimized */
     const int dim = 3;
@@ -121,7 +123,7 @@ void inhull(const double* const* testpts,
 
     /* Compute center of tesselation */
     mean(xyz, nxyz, center);
-    
+
     /* Compute normals for each face */
     for (i = 0; i < ntess; ++i)
     {
@@ -137,7 +139,7 @@ void inhull(const double* const* testpts,
         cross(ab, ac, nrmls[i]);
         length = norm(nrmls[i]);
         mean3(xyz[a], xyz[b], xyz[c], xyzmeans[i]);
-        
+
         if (length > tol)
         {
             /* Normalize and compute vector used for normal orientation */
@@ -152,17 +154,20 @@ void inhull(const double* const* testpts,
                     nrmls[i][d] *= -1;
         }
         else {
-            printf("\n\n\n\n Degenerate\n");
-            printer(ab);
-            printer(ac);
-            printer(nrmls[i]);
+            if (verbose)
+            {
+                printf("mex_inhull found degenerate triangle %d:\n", i);
+                printer(ab);
+                printer(ac);
+                printer(nrmls[i]);
+            }
         }
     }
 
     /* Check if the test points j are inside */
     for (j = 0; j < ntestpts; ++j) {
         /* Assume point inside */
-        is_inside[j] = 1; 
+        is_inside[j] = 1;
         for (i = 0; i < ntess; ++i)
         {
             double s[3];
@@ -198,9 +203,10 @@ void mexFunction(int nlhs, mxArray *plhs[],
     double **testpts_;
     double **xyz_;
     double ** tess_;
+    int verbose;
 
-    if (nrhs != 4) {
-        mexErrMsgTxt("Four inputs are required.");
+    if (nrhs < 4) {
+        mexErrMsgTxt("Four or five inputs are required.");
     }
     if (nlhs != 1) {
         mexErrMsgTxt("One output is required.");
@@ -212,32 +218,40 @@ void mexFunction(int nlhs, mxArray *plhs[],
     if (dim != 3) {
         mexErrMsgTxt("dim (number of columns) must be 3");
     }
-    /* printf("ntestpts %i\n", ntestpts); */
-    /* for (i = 0; i < ntestpts*dim; ++i) */
-    /*     printf("%f ", testpts[i]); */
-    /* printf("\n"); */
-    
+
     xyz = mxGetPr(prhs[1]);
     if (mxGetN(prhs[1]) != dim) {
         mexErrMsgTxt("Test points dimension and points defining the hull are not the same");
     }
     nxyz = mxGetM(prhs[1]);
-    /* printf("nxyz %i\n", nxyz); */
-    /* for (i = 0; i < nxyz*dim; ++i) */
-    /*     printf("%f ", xyz[i]); */
-    /* printf("\n"); */
 
     tess = mxGetPr(prhs[2]);
     if (mxGetN(prhs[2]) != dim) {
         mexErrMsgTxt("tesselation must be simplices in dimension d-1");
-    }        
+    }
     ntess = mxGetM(prhs[2]);
-    /* printf("ntess %i\n", ntess); */
-    /* for (i = 0; i < ntess*dim; ++i) */
-    /*     printf("%i ", (int)tess[i]); */
-    /* printf("\n"); */
-    
+
     tol = mxGetScalar(prhs[3]);
+    if (nrhs == 5)
+        verbose = mxGetScalar(prhs[4]);
+    else
+        verbose = 0;
+
+    if (verbose) {
+        printf("ntestpts %i\n", ntestpts);
+        for (i = 0; i < ntestpts*dim; ++i)
+            printf("%f ", testpts[i]);
+        printf("\n");
+        printf("nxyz %i\n", nxyz);
+        for (i = 0; i < nxyz*dim; ++i)
+            printf("%f ", xyz[i]);
+        printf("\n");
+        printf("ntess %i\n", ntess);
+        for (i = 0; i < ntess*dim; ++i)
+            printf("%i ", (int)tess[i]);
+        printf("\n");
+    }
+
 
     plhs[0] = mxCreateNumericMatrix((mwSize)ntestpts, 1, mxINT32_CLASS, mxREAL);
     is_inside = (int*)mxGetPr(plhs[0]);
@@ -260,7 +274,8 @@ void mexFunction(int nlhs, mxArray *plhs[],
            (const double* const*)xyz_, (mwSize)nxyz,
            (const double* const*)tess_, (mwSize)ntess,
            tol,
-           is_inside);
+           is_inside,
+           verbose);
 
     dfree(ntestpts, testpts_);
     dfree(nxyz, xyz_);
