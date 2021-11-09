43c9c6ad99daefd8bdbaed728b972d8148f7fa63
hnil <Halvor.M.Nilsen@sintef.no>
2021-11-09 20:22:35 +0100

    added  function to set verbosity level in MRST

--- utils/mrstDebug.m
+++ utils/mrstDebug.m
@@ -1,67 +1,38 @@
 function debug = mrstDebug(varargin)
-%Globally control default settings for MRST debugging information.
+%Globally control default settings for MRST verbose information.
 %
 % SYNOPSIS:
 %   Either of the modes
-%     1)     mrstDebug arg
-%     2) d = mrstDebug
+%     1) debuglevel = mrstDebug(debuglevel)
+%     2) v = mrstDebug
 %
 % DESCRIPTION:
 %   This function provides a centralised facility for individual MRST
-%   function to query debugging support default setting.  Specifically, if
-%   a function provides debugging support (e.g., additional state checks)
-%   in the form of a debugging option, then the function is encouraged to
-%   initialise the debugging flag with the output of this function.
-%   Debugging output may then be subsequently overridden on an individual,
-%   per-function basis.
+%   functions to query verbosity default settings.  Specifically, if a
+%   function provides verbose output (i.e., additional reporting during
+%   computational process) in the form of a 'debug' option, then the
+%   function is encouraged to initialise the verbose flag with the output
+%   of this function. Debug output may then be subsequently overridden on
+%   an individual, per-function basis.
 %
 % PARAMETERS:
-%     arg - Control mode for debugging support. OPTIONAL.  Must be one of
-%             - String, `{'off', 'on'}`, for globally disabling or enabling
-%               MRST debugging support.  Actual effects depends on specific
-%               setting in individual functions and may usually be controlled
-%               more targetly.  The default state is 'off'.
-%
-%             - String, 'reset', for restoring debugging setting to the
-%               default state: mrstDebug off
-%
-%             - Logical, `{false, true}`, for disabling or enabling MRST
-%               debugging support.
-%
-%             - Numeric (Real) scalar value.  Specifically set debugging
-%               level.  A debugging level exceeding zero enables debugging.
-%               Whether or not higher values produce more output is at the
-%               discretion of individual MRST functions.
+%   arg     - Numeric (Real) scalar value.  Specifically set debug
+%             level.  A verbosity level exceeding zero enables verbose
+%             output. Whether or not higher values produce more debug options is
+%             at the discretion of individual MRST functions.
 %
 %
 % RETURNS:
-%     d - A numeric scalar value.  D==0 turns default state of debugging
-%         support off, while d>0 signifies different levels of debugging
-%         support.  Individual callers of 'mrstDebug' (typically other
-%         MRST functions) must support a Boolean on/off state, but may,
-%         optionally, support a notion of debugging levels where higher
-%         levels signify more invasive (and expensive) checks.
+%     v - A numeric scalar value. 
 %
 % EXAMPLES:
-%   % 1) Demonstrate 'String' form (Command and Function syntax).
-%   mrstDebug  on    % Enable debugging support.
-%   mrstDebug('off') % Disable debugging (default state).
-%   mrstDebug reset  % Restore debugging defaults (off).
+%   mrstDebug(1)   % Enable verbose output.
+%   mrstDebug(0)   % Disable verbose output.
+%   mrstDebug(2)   % Set verbose output level to 2.
 %
-%   % 2) Demonstrate 'Logical' form of mrstDebug function.
-%   %    (only Function syntax supported in this case).
-%   mrstDebug(true)  % Enable debugging.
-%   mrstDebug(false) % Disable debugging (default state).
-%
-%   % 3) Demonstrate 'Numeric' form of mrstDebug function.
-%   %    (only Function syntax supported in this case).
-%   mrstDebug(1)   % Enable debugging.
-%   mrstDebug(0)   % Disable debugging.
-%   mrstDebug(2)   % Set debugging level to 2.
-%
-%   % 4) Retrieve current debugging setting.
+%   % Retrieve current verbosity setting.
 %   %    (Only function syntax supported in this case).
-%   d = mrstDebug()
+%   v = mrstDebug()
 
 %{
 Copyright 2009-2021 SINTEF Digital, Mathematics & Cybernetics.
@@ -86,23 +57,13 @@ along with MRST.  If not, see <http://www.gnu.org/licenses/>.
    persistent DEBUG;
 
    if isempty(DEBUG),
-      DEBUG = false;
+      DEBUG = 0;
    end
 
    if nargin > 0,
       arg = varargin{1};
-      if ischar(arg),
-         switch lower(arg),
-            case {'on' , 'yes', 'true' }         , DEBUG = true;
-            case {'off', 'no' , 'false', 'reset'}, DEBUG = false;
-            otherwise,
-               error(msgid('Mode:Unsupported'), ...
-                     'Unsupported debug control mode ''%s''.', arg);
-         end
-      elseif islogical(arg) || (isnumeric(arg) && isreal(arg)),
-         DEBUG = arg;
-      end
-   else
-      debug = DEBUG;
+      assert(isnumeric(arg) && isreal(arg))
+      DEBUG = arg;
    end
+   debug = DEBUG;
 end
