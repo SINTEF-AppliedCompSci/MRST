49ad3093e0693ce29c00085edaba9eb062e3f912
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2021-12-08 17:12:23 +0100

    Update testSuiteExampleSPE10.m

--- test-suite/examples/testSuiteExampleSPE10.m
+++ test-suite/examples/testSuiteExampleSPE10.m
@@ -11,10 +11,10 @@ test = TestCase('spe10_wo'); test.plot();
 %% Load subsets of the model
 test = TestCase('spe10_wo', 'layers', 'tarbert'   ); test.plot(); % Tarbert
 test = TestCase('spe10_wo', 'layers', 'upper_ness'); test.plot(); % Upper Ness
-test = TestCase('spe10_wo', 'layers', 10          ); test.plot(); % Layer 10
+test = TestCase('spe10_wo', 'layers', 13          ); test.plot(); % Layer 13
 
 %% Run the test
-% We run the test for layer 10 using a nonlinear solver with line search
+% We run the test for layer 13 using a nonlinear solver with line search
 nls = NonLinearSolver('useLineSearch', true);
 problem = test.getPackedSimulationProblem('NonLinearSolver', nls);
 simulatePackedProblem(problem);
@@ -25,7 +25,7 @@ simulatePackedProblem(problem);
 % that this is adifferent setup than the one above, and store the
 % simulation results in a different folder.
 test2 = test;                                  % Copy the test
-test2.schedule.control(1).W(1).status = false; % Shut in well P1
+test2.schedule.control(1).W(2).status = false; % Shut in well P2
 test2.schedule.control(1).W(5).val ...         % Reduce injection rate
     = test2.schedule.control(1).W(5).val*0.5;
 problem2 = test2.getPackedSimulationProblem('NonLinearSolver', nls);
@@ -33,6 +33,6 @@ simulatePackedProblem(problem2);
 
 %% Load and compare results
 [~, states ] = getPackedSimulatorOutput(problem );
+test.plot(states); colormap(bone);
 [~, states2] = getPackedSimulatorOutput(problem2);
-test.plot(states);
-test2.plot(states2);
+test2.plot(states2); colormap(bone);
