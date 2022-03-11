ccfb903590fd3cd0b5e1e050018468632688fbd2
Francesca Watson <francesca.watson@sintef.no>
2022-03-11 16:32:10 +0100

    More flexible isfield method for settingsStruct

--- utils/settings_manager/settingsStruct.m
+++ utils/settings_manager/settingsStruct.m
@@ -8,37 +8,39 @@ classdef settingsStruct < handle
         useMEX
         useOMP
     end
-    
+
     methods
-        function val = isfield(settings,fname)
-            switch fname
-                case 'allowDL'
-                    val = true;
-                    return
-                case 'dataDirectory'
-                    val = true;
-                    return    
-                case 'outputDirectory'
-                    val = true;
-                    return
-                case 'promptDL'
-                    val = true;
-                    return   
-                case 'promptMEX'
-                    val = true;
-                    return
-                case 'useMEX'
-                    val = true;
-                    return     
-                case 'useOMP'
-                    val = true;
-                    return                     
-                otherwise 
-                    val = false;
-            end
-                   
-        
+        function val = isfield(settings, fname)
+           if mrstPlatform('octave')
+              val = settings.isfield_octave(fname);
+           else
+              val = settings.isfield_matlab(fname);
+           end
         end
     end
-    
+
+   methods (Access = private)
+      function val = isfield_matlab(settings, fname)
+         val = settingsStruct.isfield_impl(properties(settings), fname);
+      end
+
+      function val = isfield_octave(~, fname)
+         props = {'allowDL', 'dataDirectory', 'outputDirectory', ...
+                  'promptDL', 'promptMEX', 'useMEX', 'useOMP'};
+
+         val = settingsStruct.isfield_impl(props, fname);
+      end
+   end
+
+   methods (Access = private, Static)
+      function val = isfield_impl(fields, fname)
+         fname = cellstr(fname);
+         [i, j] = blockDiagIndex(numel(fname), numel(fields));
+
+         m = strcmp(reshape(fname(i), numel(fname), []), ...
+                    reshape(fields(j), numel(fname), []));
+
+         val = reshape(any(m, 2), size(fname));
+      end
+   end
 end
