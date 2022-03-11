b4d7749debc2d946b5672edca2994c45f2a194d3
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2022-03-11 14:27:59 +0100

    Insert Copyright Notice Where Missing
    In preparation of MRST 2022a.

--- plotting/interpolateData.m
+++ plotting/interpolateData.m
@@ -1,5 +1,5 @@
 function data = interpolateData(G, data, varargin)
-%Interpolate data to corrdinates in a grid
+%Interpolate data to coordinates in a grid
 %
 % SYNOPSIS:
 %   data = interpolateData(G, data)
@@ -40,7 +40,26 @@ function data = interpolateData(G, data, varargin)
 %   should be used with care for actual computations.
 %
 % SEE ALSO:
-%   `unstructuredSurf`, unstructuredContour`
+%   `unstructuredSurf`, `unstructuredContour`.
+
+%{
+Copyright 2009-2021 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
 
     % Process input
     %---------------------------------------------------------------------%
