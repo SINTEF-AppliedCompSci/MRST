739644fe330066e878f5741582a1b51929347a5b
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2022-03-13 22:15:59 +0100

    Update Copyright Years
    In preparation of MRST 2022a

--- solvers/Hybrid.m
+++ solvers/Hybrid.m
@@ -50,7 +50,7 @@ classdef Hybrid
 end
 
 %{
-Copyright 2009-2021 SINTEF Digital, Mathematics & Cybernetics.
+Copyright 2009-2022 SINTEF Digital, Mathematics & Cybernetics.
 
 This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
 
