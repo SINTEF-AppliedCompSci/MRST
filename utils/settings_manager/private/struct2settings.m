dfee398f36e3fc0323c57ad0d73a247ab1d1fc2f
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2022-03-15 13:48:41 +0100

    Insert Copyright Notice in Settings GUI Implementation Files
    In preparation of MRST 2022a.

--- utils/settings_manager/private/struct2settings.m
+++ utils/settings_manager/private/struct2settings.m
@@ -1,4 +1,24 @@
 function settings = struct2settings(oldsettings)
+%Undocumented Utility Function
+
+%{
+Copyright 2009-2022 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
 
     settings = settingsStruct();
     fnames = fieldnames(oldsettings);
@@ -12,7 +32,5 @@ function settings = struct2settings(oldsettings)
 
     for setting = reshape(fnames, 1, [])
        settings.(setting{1}) = oldsettings.(setting{1});
-    end    
-
+    end
 end
-
--- utils/settings_manager/settingsStruct.m
+++ utils/settings_manager/settingsStruct.m
@@ -21,17 +21,14 @@ classdef settingsStruct < handle
     end
 
    methods (Access = private)
-
       function val = isfield_matlab(settings, fname)
          val = settingsStruct.isfield_impl(properties(settings), fname);
       end
 
       function val = isfield_octave(~, fname)
-
          props = settingsStruct.getSettingsProps();
          val = settingsStruct.isfield_impl(props, fname);
       end
-          
    end
 
    methods (Access = private, Static)
@@ -44,16 +41,32 @@ classdef settingsStruct < handle
 
          val = reshape(any(m, 2), size(fname));
       end
-      
-             
+
       function [allprops,names,folders] = getSettingsProps()
           % Current list of properties
-           names = {'allowDL', 'promptDL', 'promptMEX', 'useMEX', ...
-               'useOMP', 'useHash'};
+          names = {'allowDL', 'promptDL', 'promptMEX', 'useMEX', ...
+                   'useOMP', 'useHash'};
           folders = {'outputDirectory', 'dataDirectory'};
           allprops = [names,folders];
-      end      
-      
-      
+      end
    end
 end
+
+%{
+Copyright 2009-2022 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
