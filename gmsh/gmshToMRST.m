7c3d2c904f168c54b94751f50fe9ae2c331703b6
August Johansson <august.johansson@sintef.no>
2022-03-15 23:23:02 +0100

    Update copyright

--- gmsh/gmshToMRST.m
+++ gmsh/gmshToMRST.m
@@ -14,7 +14,7 @@ function [G, msh] = gmshToMRST(filename)
 %   .m.
 
 %{
-  Copyright 2009 - 2021 SINTEF Digital, Mathematics & Cybernetics.
+  Copyright 2009 - 2022 SINTEF Digital, Mathematics & Cybernetics.
 
   This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
 
