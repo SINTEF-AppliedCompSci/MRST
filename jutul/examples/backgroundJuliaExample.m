089f7935812ebed4a321b6ad975ee570fbf3f377
Olav Møyner <olav.moyner@gmail.com>
2022-04-07 11:15:44 +0200

    Update example to reflect new syntax
    Reflecting experiments done by @havahol

--- jutul/examples/backgroundJuliaExample.m
+++ jutul/examples/backgroundJuliaExample.m
@@ -8,24 +8,38 @@
 % closing the terminal window).
 mrstModule add ad-core ad-blackoil deckformat ad-props test-suite jutul
 %% How to set up
-% One time setup:
+% One time setup. Two options:
+% 1. Add DaemonMode to your base Julia environment (easiest)
+% 2. Create a separate environment with DaemonMode (advanced users).
+%
+% For option 1:
+% Run julia and run 
+% ]add DaemonMode
+% It should then be added to your base environment.
+%
+% Then, proceed with the following 
+%
 % mkdir jutul_daemon
 % cd .\julia-daemon\
 % julia
 % ]activate .
-% add DaemonMode # Require to run the Daemon
+% add DaemonMode # Required to run the Daemon, not needed if following (1)
 % dev Jutul      # Could be add once we get it into package manager
 % dev JutulDarcy
 % pwd() # will give you the absolute path of the folder
-
+%
 % Then the daemon can be spun up by calling
 %  julia --project="X" --startup-file=no -e 'using Revise; using DaemonMode; serve()'
 % where X is the absolute path of the jutul_daemon folder.
 % julia --project="C:\\Users\\olavm\\julia-daemon" --startup-file=no -e 'using Revise; using DaemonMode; serve()'
+%
+% If you were following path (2) you will need to specify the directory X of
+% your project via the 'project' optional input argument to
+% runJutulOnDaemon.
 %% We run a small example to demonstrate and verify that it is working
 setup = qfs_wo();
 [state0, model, schedule] = deal(setup.state0, setup.model, setup.schedule);
-[wells, states] = runJutulOnDaemon(state0, model, schedule, 'qfs_wo');
+[wells, states] = runJutulOnDaemon(state0, model, schedule, 'name', 'qfs_wo');
 %% Plot the results
 mrstModule add mrst-gui
 figure;
