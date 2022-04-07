2fb677e9ce2e7495bb4addc769b93174422879d0
Håvard Heitlo Holm <havard.heitlo.holm@sintef.no>
2022-04-07 10:34:17 +0200

    adding project options

--- jutul/utils/runJutulOnDaemon.m
+++ jutul/utils/runJutulOnDaemon.m
@@ -1,13 +1,13 @@
 function [wells, states] = runJutulOnDaemon(state0, model, schedule, varargin)
-    if numel(varargin) == 0
-        name = 'jutul_case';
-    else
-        name = varargin{1};
-        varargin = varargin(2:end);
+    opt = struct('name', 'jutul_case', ...
+                 'project', '');
+    [opt, extra] = merge_options(opt, varargin{:});
+    if ~isempty(opt.project)
+        opt.project = sprintf('--project="%s"', opt.project);
     end
-    pth = writeJutulInput(state0, model, schedule, name, 'printcmd', false);
+    pth = writeJutulInput(state0, model, schedule, opt.name, 'printcmd', false);
     % Create a julia script that runs the file
-    cmd_pth = fullfile(tempdir(), sprintf('run_%s.jl', name));
+    cmd_pth = fullfile(tempdir(), sprintf('run_%s.jl', opt.name));
     fprintf('Creating Julia runscript at %s... ', cmd_pth) 
     f = fopen(cmd_pth, 'w');
     if f == 0
@@ -27,7 +27,8 @@ function [wells, states] = runJutulOnDaemon(state0, model, schedule, varargin)
     fprintf(f, 'using JutulDarcy\nsimulate_mrst_case(\"%s\", write_mrst = true, info_level = %d, end_report = true, ascii_terminal = true)\n', pth, info);
     % Finally put together the command to invoke the daemon in client mode
     % and run the case.
-    cmd = sprintf('julia --startup-file=no --color=no -e "using DaemonMode; runargs()" %s', cmd_pth);
+    %cmd = sprintf('julia --startup-file=no --color=no -e "import Pkg; Pkg.add("DaemonMode"); using DaemonMode; runargs()" %s', cmd_pth);
+    cmd = sprintf('julia %s --startup-file=no --color=no -e "using DaemonMode; runargs()" %s', opt.project, cmd_pth);
     id = system(cmd);
     if id == 1
         error('Something went wrong.')
