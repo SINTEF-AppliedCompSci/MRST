6c70f4c3012341928d9e034bf8ed7564d874eb22
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2022-06-17 22:29:08 +0200

    Create geothermalExampleMSWells.m

---
+++ examples/geothermalExampleMSWells.m
@@ -0,0 +1,51 @@
+mrstModule add test-suite
+mrstModule add ad-core ad-props
+mrstModule add geothermal compositional
+mrstModule add mrst-gui
+
+%%
+close all
+test = TestCase('fractured_3d_slice_geothermal');
+test.plot(test.state0);
+
+%%
+problem = test.getPackedSimulationProblem();
+simulatePackedProblem(problem);
+
+%%
+[wellSols, states, reports] = getPackedSimulatorOutput(problem);
+
+test.plot(states);
+plotWellSols(wellSols);
+
+%%
+traj = linspace(0,max(test.model.G.nodes.coords(:,3)), 100)';
+traj = [repmat(min(test.model.G.cells.centroids(:,1:2), [], 1), 100, 1), traj];
+% traj = flipud(traj);
+
+W = MultisegmentWellModel(test.model, traj);
+
+wellState = initResSol(W.parentModel.G, 1, 1);
+W = W.validateModel;
+wellState = W.validateState(wellState);
+
+schedule = simpleSchedule(rampupTimesteps(10*day, 1*day));
+
+[~, states] = simulateScheduleAD(wellState, W, schedule);
+
+%%
+
+mm = MultiPhysicsModel({test.model, W});
+
+models = mm.getModels();
+names  = mm.getModelNames();
+state0 = struct();
+schedule = simpleSchedule(rampupTimesteps(10*day, 1*day));
+for i = 1:mm.numModels()
+    model = models{i};
+    name = names{i};
+    state0.(name) = initResSol(model.G, 1, 1);
+    schedule.control(1).(names{i}) = schedule.control(1);
+end
+schedule.control = rmfield(schedule.control, {'W', 'src', 'bc'});
+simulateScheduleAD(state0, mm, schedule);
