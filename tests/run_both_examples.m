f80beed0165d2017503cf6b86daaed4e3d3110fe
David Landa-Marbán <dmar@norceresearch.no>
2022-08-16 18:29:56 +0200

    Update run_both_examples.m

--- tests/run_both_examples.m
+++ tests/run_both_examples.m
@@ -1,3 +1,3 @@
 run("../mrst-core/startup.m")
-run("../mrst-thirdparty-modules/ad-micp/examples/runMICP1DCase.m")
 run("../mrst-thirdparty-modules/ad-micp/examples/runMICP3DCase.m")
+run("../mrst-thirdparty-modules/ad-micp/examples/runMICP1DCase.m")
