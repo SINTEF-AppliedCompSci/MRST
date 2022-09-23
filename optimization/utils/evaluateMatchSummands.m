b59512a26867d63d3f0c7cd1556afb71de93cafc
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2022-09-23 16:58:20 +0200

    Update Copyright Years
    Update Copyright Years
    
    In preparation of MRST 2022b.

--- optimization/utils/evaluateMatchSummands.m
+++ optimization/utils/evaluateMatchSummands.m
@@ -33,7 +33,7 @@ function [misfitVals ,varargout] = evaluateMatchSummands(pvec, obj, setup, param
 % `evalObjective`, `computeSensitivitiesAdjointAD`, `unitBoxBFGS` 
 
 %{
-Copyright 2009-2021 SINTEF Digital, Mathematics & Cybernetics.
+Copyright 2009-2022 SINTEF Digital, Mathematics & Cybernetics.
 
 This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
 
