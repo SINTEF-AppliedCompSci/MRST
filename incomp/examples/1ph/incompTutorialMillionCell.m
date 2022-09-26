18458f80d03c788977c45642ae1564880c90937f
Knut-Andreas Lie <Knut-Andreas.Lie@sintef.no>
2022-09-26 18:41:40 +0200

    Corrected error in logic for choosing AGMG introduced while testing

--- incomp/examples/1ph/incompTutorialMillionCell.m
+++ incomp/examples/1ph/incompTutorialMillionCell.m
@@ -17,7 +17,7 @@ mrstModule add incomp agmg linearsolvers
 % When working with large models, one cannot use the standard MLDIVIDE
 % ('\') solver in MATLAB. Here, we use the AGMG or AMGCL algebraic
 % multigrid solver.
-if ~exist('agmg', 'file') && ...
+if exist('agmg', 'file') && ...
       norm(agmg(speye(3), [ 1 ; 2 ; 3 ]) - [ 1 ; 2 ; 3 ]) < 1.0e-8
     linsolver = @(A,b) agmg(A,b,1);
     disp('Using AGMG as linear solver');
