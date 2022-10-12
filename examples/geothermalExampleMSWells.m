8dcf1b42a753eb1ae00820e72d05a040410d2c97
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2022-10-12 08:53:39 +0200

    Update geothermalExampleMSWells.m

--- examples/geothermalExampleMSWells.m
+++ examples/geothermalExampleMSWells.m
@@ -1,6 +1,7 @@
 mrstModule add test-suite
 mrstModule add ad-core ad-props
-mrstModule add geothermal compositional
+mrstModule add geothermal compositional dfm
+mrstModule add ruden-geothermal
 mrstModule add multiphysics-model wellpaths upr
 mrstModule add mrst-gui
 mrstModule add ecmor-2022-geothermal
@@ -9,23 +10,25 @@ mrstVerbose on
 
 %%
 close all
-test    = TestCase('fivespot_geothermal', 'cartDims', [5,5,2]);
+test    = TestCase('fivespot_geothermal', 'cartDims', [5, 5, 2]);
 test.model.FacilityModel = [];
 groups = [];
-groups = addFacilityGroup(groups, {'Hot'});
-groups = addFacilityGroup(groups, {'Cold-1-1', 'Cold-2-1', 'Cold-2-2', 'Cold-1-2'});
+groups = addFacilityGroup(groups, {'Hot'}, 'name', 'HotGroup');
+groups = addFacilityGroup(groups, {'Cold-1-1', 'Cold-2-1'}, 'name', 'ColdGroup');
+% groups = addFacilityGroup(groups, {'Cold-1-1', 'Cold-2-1', 'Cold-2-2', 'Cold-1-2'}, 'name', 'ColdGroup');
 test = convertToWBMultiModel(test, 'groups', groups);
-testRef = TestCase('fractured_3d_slice_geothermal', 'useWellboreModel', false, 'cartDims', cartDims);
+testRef = TestCase('fivespot_geothermal', 'cartDims', [31, 31, 32]);
 
 %%
 lsol = MultiPhysicsLinearSolver(test.model);
+% lsol = BackslashSolverAD();
 % lsol.solveSubproblems = true;
 nls = NonLinearSolver();
 nls.useLinesearch = true;
 nls.LinearSolver = lsol;
 % test.model.submodels.Wellbore.parentModel.operators.T = test.model.submodels.Wellbore.parentModel.operators.T*100;
 problem = test.getPackedSimulationProblem('LinearSolver', lsol);
-simulatePackedProblem(problem, 'restartStep', nan);
+simulatePackedProblem(problem, 'restartStep', 1);
 
 %%
 problemRef = testRef.getPackedSimulationProblem();
@@ -63,7 +66,6 @@ schedule = simpleSchedule(rampupTimesteps(10*day, 1*day));
 [~, states] = simulateScheduleAD(wellState, W, schedule);
 
 %%
-
 mm = MultiPhysicsModel({test.model, W});
 
 models = mm.getModels();
@@ -78,3 +80,5 @@ for i = 1:mm.numModels()
 end
 schedule.control = rmfield(schedule.control, {'W', 'src', 'bc'});
 simulateScheduleAD(state0, mm, schedule);
+
+%%
