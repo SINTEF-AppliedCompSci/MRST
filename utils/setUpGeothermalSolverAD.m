c2c057f4ad6e1eca73ae5b4cb60e139a56003c71
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2022-10-28 17:43:44 +0200

    Update setUpGeothermalSolverAD.m

--- utils/setUpGeothermalSolverAD.m
+++ utils/setUpGeothermalSolverAD.m
@@ -1,6 +1,6 @@
 function solver = setUpGeothermalSolverAD(model)
 
-    solver = AMGCLSolverAD('block_size', 1);
+    solver = AMGCLSolverAD('block_size', 2);
     solver.maxIterations = 100;
     solver.tolerance = 1e-4;
     solver.setPreconditioner('relaxation');
@@ -11,12 +11,12 @@ function solver = setUpGeothermalSolverAD(model)
     nc = model.G.cells.num;
     solver.keepNumber = 2*nc;
     solver.reduceToCell = false;
-%     order = [1:nc; (1:nc) + nc];
-%     order = order(:);
+    order = [1:nc; (1:nc) + nc];
+    order = order(:);
 %     
 %     o = getCellMajorReordering(nc, 2);
 %     
-%     [solver.equationOrdering, solver.variableOrdering] = deal(order);
+    [solver.equationOrdering, solver.variableOrdering] = deal(order);
     
     
 end
