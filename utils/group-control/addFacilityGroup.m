76145e8be2245497339b721f670c5464fa5162ae
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2022-12-16 11:14:02 +0100

    Update addFacilityGroup.m

--- utils/group-control/addFacilityGroup.m
+++ utils/group-control/addFacilityGroup.m
@@ -2,7 +2,7 @@ function groups = addFacilityGroup(groups, members, varargin)
     
 
     group = struct('name' , sprintf('G%d', numel(groups) + 1), ...
-                   'types', {{}}, ...
+                   'ftype', {{}}, ...
                    'type', 'none', ...
                    'val' , nan, ...
                    'sign', 0, ...
@@ -21,8 +21,11 @@ function group = checkInput(group)
 
     if ~iscell(group.members), group.members = {group.members}; end
     nm = numel(group.members);
-    if isempty(group.types), group.types = repmat({'well'}, nm, 1); end
-        
-    assert(numel(group.types) == nm);
+    if isempty(group.ftype), group.ftype = 'well'; end
+    if ~iscell(group.ftype), group.ftype = {group.ftype}; end
+    if numel(group.ftype) == 1
+        group.ftype = repmat(group.ftype, nm, 1);
+    end
+    assert(numel(group.ftype) == nm);
 
 end
