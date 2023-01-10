816f9de15726b75bcafdfd079d687ff7ecbba5b0
Øystein Klemetsdal <oystein.klemetsdal@gmail.com>
2023-01-10 11:54:26 +0100

    Update WellboreComponentPhaseFlux.m

--- statefunctions/wellbore/WellboreComponentPhaseFlux.m
+++ statefunctions/wellbore/WellboreComponentPhaseFlux.m
@@ -14,7 +14,28 @@ classdef WellboreComponentPhaseFlux < StateFunction
         %-----------------------------------------------------------------%
         function v = evaluateOnDomain(prop, model, state)
             
-            v = {state.massFlux};
+            vt = state.massFlux;
+            [mass, flag] = model.getProps(state, 'ComponentPhaseMass', 'PhaseUpwindFlag');
+            nc = model.getNumberOfComponents();
+            nph   = model.getNumberOfPhases();
+            massT = 0;
+            for c = 1:nc
+                for ph = 1:nph
+                    m = mass{c,ph};
+                    if isempty(m), continue; end
+                    massT = massT + m;
+                end
+            end
+            
+            v = cell(nc, nph);
+            for c = 1:nc
+                for ph = 1:nph
+                    m = mass{c,ph};
+                    if isempty(m), continue; end
+                    x = model.operators.faceUpstr(flag{ph}, m./massT);
+                    v{c, ph} = vt.*x;
+                end
+            end
             
         end
         %-----------------------------------------------------------------%
