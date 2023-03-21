4484cd95a6195d8a702ba22e1284b2ccf0533b49
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2023-03-21 14:02:21 +0100

    Move Copyright Notice Ahead of Implementation
    In preparation of MRST 2023a.

--- co2lab/utils/trapping/private/activeCellNodes.m
+++ co2lab/utils/trapping/private/activeCellNodes.m
@@ -2,13 +2,6 @@ function cnodes = activeCellNodes(Gt)
 % Return a (4 x m)-sized matrix where m is the number of active cells in Gt.  Each
 % column holds the indices of the 4 nodes that are corners of that cell.  
 % The algorithm presupposes that each cell has exactly 4 corner nodes.
-    
-    cnode_full_mat = cellNodes(Gt);
-    
-    % only keep the information we want
-    cnodes = reshape(cnode_full_mat(:, 3), 4, []);
-       
-end
 
 %{
 Copyright 2009-2023 SINTEF Digital, Mathematics & Cybernetics.
@@ -28,3 +21,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with MRST.  If not, see <http://www.gnu.org/licenses/>.
 %}
+
+    cnode_full_mat = cellNodes(Gt);
+
+    % only keep the information we want
+    cnodes = reshape(cnode_full_mat(:, 3), 4, []);
+end
