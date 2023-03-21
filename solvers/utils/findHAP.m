c0e1cf3e4790f969aa269149bbd164e8f91d575f
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2023-03-21 13:30:39 +0100

    Update Copyright Years
    In preparation of MRST 2023a.

--- solvers/utils/findHAP.m
+++ solvers/utils/findHAP.m
@@ -3,7 +3,7 @@ function interpFace = findHAP(G, rock)
 % Dirichlet and Neumann boundary conditions
 
 %{
-Copyright 2009-2022 SINTEF Digital, Mathematics & Cybernetics.
+Copyright 2009-2023 SINTEF Digital, Mathematics & Cybernetics.
 
 This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
 
