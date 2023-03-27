c724eb5d9fcbeb4f10973373f2aceb525af1b216
Olav Møyner <olav.moyner@gmail.com>
2023-03-27 13:36:56 +0200

    startup: Guard against pwd() outside MRST dir

--- startup.m
+++ startup.m
@@ -1,11 +1,13 @@
 function startup()
     run_startup();
     dirs = {'autodiff', 'model-io', 'multiscale', 'modules', 'solvers', 'visualization'};
+    [base_path, fn, fn] = fileparts(mfilename()); %#ok
+    clear fn
     for i = 1:numel(dirs)
-        mrstPath('addroot', dirs{i});
+        mrstPath('addroot', fullfile(base_path, dirs{i}));
     end
     % co2lab repo is organized differently
-    mrstPath('add', 'co2lab', fullfile('co2lab', 'co2lab'))
+    mrstPath('add', 'co2lab', fullfile(base_path, 'co2lab', 'co2lab'))
     run_local();
 end
 
