d5d28256af4f0b2b969d3485ffa28b3ce9b01122
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2023-09-20 13:11:08 +0200

    Insert Missing Copyright Notices
    In preparation of MRST 2023b.

--- examples/plotFinalPressure.m
+++ examples/plotFinalPressure.m
@@ -1,4 +1,24 @@
 function plotFinalPressure(G, states, name)
+%Undocumented Helper Function
+
+%{
+Copyright 2009-2023 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
 
     figure
     plotToolbar(G, states)
@@ -15,5 +35,4 @@ function plotFinalPressure(G, states, name)
     axis tight equal
     colorbar
     title([name, ' at endtime'])
-
 end
