9ae2a3bf8737d00ab5bf106283127affad58762c
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2023-09-20 13:07:31 +0200

    Fix Copyright Statements
    In preparation of MRST 2023b.

--- utils/makeStorageSchedule.m
+++ utils/makeStorageSchedule.m
@@ -1,4 +1,24 @@
 function schedule = makeStorageSchedule(W, chargeIx, varargin)
+% Undocumented Helper Function
+
+%{
+Copyright 2009-2023 SINTEF Digital, Mathematics & Cybernetics.
+
+This file is part of The MATLAB Reservoir Simulation Toolbox (MRST).
+
+MRST is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MRST is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MRST.  If not, see <http://www.gnu.org/licenses/>.
+%}
 
     month = year/12;
     opt = struct( ...
@@ -73,6 +93,4 @@ function [W, dt] = makeDischargeStage(W, chargeIx, opt)
     [W.T] = deal(opt.temperature(2));
     
     dt = rampupTimesteps(opt.time(1), opt.dt(1));
-
 end
-%-------------------------------------------------------------------------%
