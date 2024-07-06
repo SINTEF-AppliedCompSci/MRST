194db03d59a659aea82a07bbf68d627360c63ee9
Odd <odd.andersen@sintef.no>
2024-07-06 15:35:38 +0200

    Added sectino to exampleVEJohansen to reproduce 3D plots

--- co2lab-ve/examples/exampleVEJohansen.m
+++ co2lab-ve/examples/exampleVEJohansen.m
@@ -155,7 +155,7 @@ schedule.step.control = [ones(50, 1); ...
 model = CO2VEBlackOilTypeModel(Gt, rock2D, fluid);
 [wellSol, states] = simulateScheduleAD(initState, model, schedule);
 states = [{initState}; states];
-
+ 
 %% Animate the plume migration over the whole simulation period
 clf
 oG = generateCoarseGrid(Gt.parent, ones(Gt.parent.cells.num,1));
@@ -180,6 +180,29 @@ for i=1:numel(states)
     hs = plotCellData(Gt.parent, sat, ix); drawnow
 end
 
+%% Reproduce plots from 3D example (example3DJohansen)
+
+% Convert all VE states to 3D
+states3D = VEstates23D(states, Gt, fluid, 'poro3D', rock.poro);
+
+% Saturation in formation after injection period
+figure; plotCellData(Gt.parent, states3D{51}.s(:,2)); view(-63, 68); colorbar; 
+set(gcf, 'position', [531   337   923   356]); axis tight;
+
+% Saturation in formation at end of simulation
+figure; plotCellData(Gt.parent, states3D{end}.s(:,2)); view(-63, 68); colorbar; 
+set(gcf, 'position', [531   337   923   356]); axis tight;
+
+% saturation in selected vertical section, after injection period
+[i j k] = ind2sub(G.cartDims, G.cells.indexMap);
+figure; 
+plotCellData(extractSubgrid(G, j==48 & i>18 & i < 60), states3D{50}.s(j==48 & i>18 & i < 60,2)); 
+view(0,0); axis off;
+
+% saturation in selected vertical section, at end of simulation
+figure; 
+plotCellData(extractSubgrid(G, j==48 & i>18 & i < 60), states3D{end}.s(j==48 & i>18 & i < 60,2)); 
+view(0,0); axis off;
 
 %% Trapping inventory
 % The result is a more detailed inventory that accounts for six different categories of CO2:
