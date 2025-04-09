f2bdde7ea0bf98049996be089d990f6e6f2a002a
Xavier Raynaud <xav.raynaud@gmail.com>
2025-04-09 13:36:21 +0200

    added cpp mex function for product map

---
+++ core/utils/tensorAssembly/mapProduct.cpp
@@ -0,0 +1,69 @@
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+#include <typeinfo>
+#include <iostream>
+
+using namespace matlab::data;
+using matlab::mex::ArgumentList;
+
+class MexFunction : public matlab::mex::Function {
+public:
+  void operator()(ArgumentList outputs, ArgumentList inputs) {
+
+    // input[0] : integer, size of output
+    // input[1] : vector of double, input vector
+    // input[2] : vector of uint64, dispatch indices for input vector
+    // input[3] : vector of uint64, dispatch indices for output vector
+    // Validate arguments
+    
+    checkArguments(outputs, inputs);
+
+    uint n = inputs[0][0];
+
+    const TypedArray<double>&  u = inputs[1];
+
+    const TypedArray<uint64_t>&  dispind1 = inputs[2];
+    const TypedArray<uint64_t>&  dispind2 = inputs[3];
+    
+    ArrayDimensions dims = {n};
+    
+    ArrayFactory factory;
+    
+    TypedArray<double> v = factory.createArray<double>(dims);
+
+    
+    for (size_t i = 0; i < dispind1.getNumberOfElements(); ++i) {
+      
+      v[dispind2[i] - 1] +=  u[dispind1[i] - 1];
+      
+    }
+
+    // Assign outputs
+    outputs[0] = v;
+    
+  }
+
+  void checkArguments(ArgumentList outputs, ArgumentList inputs) {
+      
+    std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+        
+    ArrayFactory factory;
+
+    if (inputs[1].getType() != ArrayType::DOUBLE) {
+      matlabPtr->feval(u"error", 0, 
+                       std::vector<Array>({ factory.createScalar("Input must be double array") }));
+    }
+
+    for (size_t i = 2; i < 4; ++i) {
+      if (inputs[i].getType() != ArrayType::UINT64) {
+        matlabPtr->feval(u"error", 0, 
+                         std::vector<Array>({ factory.createScalar("Input must be uint64 array") }));
+      }
+    }
+
+    if (outputs.size() > 1) {
+      matlabPtr->feval(u"error", 0, 
+                       std::vector<Array>({ factory.createScalar("Only one output is returned") }));
+    }
+  }
+};
