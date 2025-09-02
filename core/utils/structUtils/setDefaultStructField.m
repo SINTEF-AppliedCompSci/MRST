a644e0b4ab94f96892d8139d561e321201267f0e
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-09-02 17:30:50 +0200

    more in setDefaultStructField

--- core/utils/structUtils/setDefaultStructField.m
+++ core/utils/structUtils/setDefaultStructField.m
@@ -1,5 +1,10 @@
 function mstruct = setDefaultStructField(mstruct, fieldnamelist, defaultValue)
 
+    if isUnAssigned(mstruct)
+        mstruct = setStructField([], fieldnamelist, defaultValue);
+        return
+    end
+    
     value = getStructField(mstruct, fieldnamelist);
 
     if isUnAssigned(value)
