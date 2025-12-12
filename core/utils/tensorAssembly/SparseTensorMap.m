7a0f7dc0c3b69e2bd55c8ece2b9130d112b25f58
Xavier Raynaud <xavier.raynaud@sintef.no>
2025-12-12 12:28:54 +0100

    more in SparseTensorMap

--- core/utils/tensorAssembly/SparseTensorMap.m
+++ core/utils/tensorAssembly/SparseTensorMap.m
@@ -73,10 +73,9 @@ classdef SparseTensorMap
                     'table that are neither merged or reduced.']);
 
             lA = fromTbl.gets(mergefds);
-            jA = fromTbl.gets(reducefds);
+            iA = fromTbl.gets(reducefds);
 
             lB = toTbl.gets(mergefds);
-            kB = toTbl.gets(ofds2);
 
             A = map.tensor.vals;
 
@@ -85,7 +84,6 @@ classdef SparseTensorMap
             b_lB = b_l(size(lA, 1) + (1 : size(lB, 1)));
 
             [m_i, ~, b_iA] = unique(iA, 'rows');
-            [m_j, ~, b_jA] = unique(jA, 'rows');
             [m_k, ~, b_kB] = unique(kB, 'rows');
 
             b_l = intersect(b_lA, b_lB, 'sorted');
@@ -93,82 +91,14 @@ classdef SparseTensorMap
             LIA = ismember(b_lA, b_l);
             b_lA = b_lA(LIA);
             b_iA = b_iA(LIA);
-            b_jA = b_jA(LIA);
             A    = A(LIA);
 
-            LIA = ismember(b_lB, b_l);
-            b_lB = b_lB(LIA);
-            b_kB = b_kB(LIA);
+            sA = sparse(b_lA, b_iA, A);
+            B = sum(sA, 2);
 
-            [b_lA, isort] = sort(b_lA);
-            b_iA = b_iA(isort);
-            b_jA = b_jA(isort);
-            A = A(isort);
- 
-            [b_lB, isort] = sort(b_lB);
-            b_kB = b_kB(isort);
+            B = B(b_lB);
 
-            b_lA_b_iA = unique([b_lA, b_iA], 'rows');
-            [~, nI] = rlencode(b_lA_b_iA(: , 1));
-
-            b_lB_b_kB = unique([b_lB, b_kB], 'rows');
-            [~, nK] = rlencode(b_lB_b_kB(: , 1));
-
-            nalloc = sum(nI.*nK);
-
-            [u_b_lA, n_lA] = rlencode(b_lA);
-            [u_b_lB, n_lB] = rlencode(b_lB);
-            
-            assert(all(u_b_lA == u_b_lB));
-            u_b_l = u_b_lA;
-            
-            startA   = 1;
-            endA     = n_lA(1);
-            
-            startB   = 1;
-            endB     = n_lB(1);
-            
-            b_iC_all = zeros(nalloc, 1, 'uint64');
-            b_kC_all = zeros(nalloc, 1, 'uint64');
-            B_all    = zeros(nalloc, 1, 'double');
-            
-            n_u_b_l = [];
-            
-            posB = 0;
-
-            for ind_u_b_l = 1 : numel(u_b_l)
-                
-                ind = (startA : endA);
-                sA = sparse(b_iA(ind), b_jA(ind), A(ind));
-                sB = sum(sA, 2)
-                
-                [b_iB, ~, B] = find(sB);                    
-
-                nB = size(B, 1);
-                
-                n_u_b_l(end + 1) = nB;
-                
-                b_iB_all(posB  + (1 : nB)) =  b_iB;
-                B_all(posB  + (1 : nB))    =  B ;
-                
-                posB = posB + nB;
-
-                if ind_u_b_l < numel(u_b_l)
-                    startA = endA + 1;
-                    endA   = startA + n_lA(ind_u_b_l + 1) - 1;                        
-                end
-
-            end
-    
-            fprintf('nalloc : %d, posC : %d, difference : %d\n', nalloc, posB, abs(nalloc - posB));
-            
-            B = B_all(1 : posB);
-
-            lB_all = rldecode(m_l(u_b_l, :), n_u_b_l);
-            fdnames = {mergefds{:}, ofds1{:}, ofds2{:}};
-            tbl = IndexArray([], 'fdnames', fdnames, 'inds', [lB_all, m_i(b_iB_all(1 : posB), :), m_k(b_kB_all(1 : posB), :)]);
-
-            B = SparseTensor(B, tbl);
+            B = SparseTensor(B, toTbl);
 
         end
         
